11851f194129291ed4646c64b8a8ad4a6b7e9bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:00 +0000

    Add package pyRedunda/1.1.1/pyRedunda-1.1.1.tar.gz

---
+++ packages/pyRedunda/pyRedunda-1.1.1.tar.gz/pyRedunda-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+    name = "pyRedunda",
+    packages = ["pyRedunda"],
+    version = "1.1.1",
+    description = "A python library for using Redunda.",
+    author = "Ashish Ahuja",
+    author_email = "ashish.ahuja@sobotics.org",
+    url = "https://github.com/SOBotics/Redunda-lib-Python",
+)
