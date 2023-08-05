94b1dc4e5e496e258a2b70560c0bec48bd5bb8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:04 +0000

    Add package YamlInfo/0.0.1.8/YamlInfo-0.0.1.8.tar.gz

---
+++ packages/YamlInfo/YamlInfo-0.0.1.8.tar.gz/YamlInfo-0.0.1.8/setup.py
@@ -0,0 +1,23 @@
+"""YamlInfo Install Script"""
+# coding=utf-8
+from setuptools import setup, find_packages
+from yaml_info.yamlinfo import YamlInfo
+
+project_data = YamlInfo("projectinfo.yml", "projectinfo", "LICENSE").get()
+project_info_data = project_data["info"]
+project_version_data = project_data["version"]
+__version__ = "%(major)s.%(minor)s.%(revision)s.%(release)s" % project_version_data
+
+setup(
+    name=project_data["info"]["name"],
+    version=__version__,
+    include_package_data=True,
+    packages=find_packages(exclude=['docs', 'tests*']),
+    install_requires=['PyYAML'],
+    url='https://github.com/AUCR/YamlInfo',
+    license=project_data["info"]["license"],
+    author=project_data["info"]["authors"],
+    author_email=project_data["info"]["authors_email"],
+    description=project_data["info"]["description"],
+    classifiers=project_data["info"]["classifiers"],
+)
