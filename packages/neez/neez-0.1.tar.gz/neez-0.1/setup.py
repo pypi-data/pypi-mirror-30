88213c7673286f66cb154e1b3a9f8b9c4ec8658d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:32 +0000

    Add package neez/0.1/neez-0.1.tar.gz

---
+++ packages/neez/neez-0.1.tar.gz/neez-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='neez',
+      version='0.1',
+      description='chff short message client',
+      url='http://github.com/mnes-io/neez',
+      author='Charles Perkins',
+      author_email='charlesap@gmail.com',
+      license='MIT',
+      packages=['neez'],
+      zip_safe=False)
