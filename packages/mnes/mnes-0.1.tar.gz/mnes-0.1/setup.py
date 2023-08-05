0e051df237e9681bb6fc11a641d0a3412e6b99aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:19 +0000

    Add package mnes/0.1/mnes-0.1.tar.gz

---
+++ packages/mnes/mnes-0.1.tar.gz/mnes-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mnes',
+      version='0.1',
+      description='metadata capture resistant secure messaging',
+      url='http://github.com/mnes-io/mnes',
+      author='Charles Perkins',
+      author_email='charlesap@gmail.com',
+      license='MIT',
+      packages=['mnes'],
+      zip_safe=False)
