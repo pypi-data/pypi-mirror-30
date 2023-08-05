e5b51c94637c1706173f1e0bd91e234857d1db37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:21 +0000

    Add package catcher/1.4.6/catcher-1.4.6-py3-none-any.whl

---
+++ packages/catcher/catcher-1.4.6-py3-none-any.whl/catcher/core/test.py
@@ -0,0 +1,63 @@
+from catcher.steps import step_factory
+from catcher.utils.logger import debug, info
+
+
+class Test:
+    def __init__(self, path: str, includes: dict, variables: dict, config: dict, steps: list) -> None:
+        self._includes = includes
+        self._variables = variables
+        self._config = config
+        self._steps = steps
+        self._path = path
+
+    @property
+    def includes(self) -> dict:
+        return self._includes
+
+    @property
+    def variables(self) -> dict:
+        return self._variables
+
+    @variables.setter
+    def variables(self, variables):
+        self._variables = variables
+
+    @property
+    def config(self) -> dict:
+        return self._config
+
+    @property
+    def steps(self) -> list:
+        return self._steps
+
+    @property
+    def path(self) -> str:
+        return self._path
+
+    def run(self, tag=None) -> dict:
+        for step in self.steps:
+            [action] = step.keys()
+            ignore_errors = get_or_default('ignore_errors', step[action], False)
+            if tag is not None:
+                step_tag = get_or_default('tag', step[action], None)
+                if step_tag != tag:
+                    continue
+            actions = step_factory.get_actions(self.path, step)
+            for action_object in actions:
+                try:
+                    self.variables = action_object.action(self.includes, self.variables)
+                    info('Step ' + action + ' OK')
+                except Exception as e:
+                    if ignore_errors:
+                        debug('Step ' + action + ' failed, but we ignore it')
+                        continue
+                    info('Step ' + action + ' failed: ' + str(e))
+                    raise e
+        return self.variables
+
+
+def get_or_default(key: str, body: dict or str, default: any) -> any:
+    if isinstance(body, dict):
+        return body.get(key, default)
+    else:
+        return default
---
+++ packages/catcher/catcher-1.4.6-py3-none-any.whl/catcher/steps/step_factory.py
@@ -0,0 +1,39 @@
+from catcher.steps.echo import Echo
+from catcher.steps.run import Run
+from catcher.steps.step import Step
+from catcher.steps.wait import Wait
+from catcher.steps.check import Check
+from catcher.steps.http import Http
+from catcher.steps.kafka import Kafka
+
+
+def get_actions(path: str, step: dict) -> [Step]:
+    [action] = step.keys()
+    body = step[action]
+    steps = []
+    if 'actions' in body:
+        for action_step in body['actions']:
+            steps.append(get_action(path, action, action_step))
+    else:
+        steps.append(get_action(path, action, body))
+    return steps
+
+
+#  TODO refactor me
+def get_action(path: str, action, body: dict or str) -> Step:
+    if action == 'echo':
+        return Echo(path, body)
+    if action == 'wait':
+        return Wait(body)
+    if action == 'run':
+        if isinstance(body, str):
+            return Run(**{'include': body})
+        else:
+            return Run(**body)
+    if action == 'check':
+        return Check(body)
+    if action == 'http':
+        return Http(body)
+    if action == 'kafka':
+        return Kafka(body)
+    return None
