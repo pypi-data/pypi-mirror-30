7c9790fe8d367461f4a3740798390e5e75a0e833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:19 +0000

    Add package slim/0.2.19/slim-0.2.19-py3-none-any.whl

---
+++ packages/slim/slim-0.2.19-py3-none-any.whl/slim/exception.py
@@ -0,0 +1,19 @@
+
+class SyntaxException(Exception):
+    pass
+
+
+class ParamsException(Exception):
+    pass
+
+
+class PermissionDeniedException(Exception):
+    pass
+
+
+class ResourceException(Exception):
+    pass
+
+
+class ValueHandleException(Exception):
+    pass
