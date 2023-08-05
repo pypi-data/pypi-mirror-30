43df26cbc6cfab3e87881437ecb8f0ec05c08f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:00 +0000

    Add package gtable/0.7.3/gtable-0.7.3.tar.gz

---
+++ packages/gtable/gtable-0.7.3.tar.gz/gtable-0.7.3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('gtable/version.py') as f:
+    exec(f.read())
+
+setup(name='gtable',
+      version=__version__,
+      description='A fast table-like container for data analytics',
+      packages=['gtable'],
+      author='Guillem Borrell',
+      author_email='guillemborrell@gmail.com',
+      install_requires=['numpy', 'pandas', 'numba'])
