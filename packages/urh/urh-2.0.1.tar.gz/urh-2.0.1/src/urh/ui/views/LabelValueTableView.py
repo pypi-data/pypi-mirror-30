eb9dcc559264250b7e9737ca6c73567b257aaf86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:40 +0000

    Add package urh/2.0.1/urh-2.0.1.tar.gz

---
+++ packages/urh/urh-2.0.1.tar.gz/urh-2.0.1/src/urh/ui/views/LabelValueTableView.py
@@ -0,0 +1,16 @@
+from PyQt5.QtWidgets import QTableView
+
+from urh.models.LabelValueTableModel import LabelValueTableModel
+from urh.signalprocessing.ProtocoLabel import ProtocolLabel
+from urh.ui.delegates.ComboBoxDelegate import ComboBoxDelegate
+
+
+class LabelValueTableView(QTableView):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setItemDelegateForColumn(1, ComboBoxDelegate(ProtocolLabel.DISPLAY_FORMATS, parent=self))
+        self.setItemDelegateForColumn(2, ComboBoxDelegate(ProtocolLabel.DISPLAY_BIT_ORDERS, parent=self))
+        self.setEditTriggers(QTableView.AllEditTriggers)
+
+    def model(self) -> LabelValueTableModel:
+        return super().model()
