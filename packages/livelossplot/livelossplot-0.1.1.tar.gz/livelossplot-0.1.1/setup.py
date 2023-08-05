7be637f472384ad3077f909ccb14ed87c1296500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:41 +0000

    Add package livelossplot/0.1.1/livelossplot-0.1.1.tar.gz

---
+++ packages/livelossplot/livelossplot-0.1.1.tar.gz/livelossplot-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(name='livelossplot',
+      version='0.1.1',
+      install_requires=['matplotlib', 'notebook'],
+      description='Live training loss plot in Jupyter Notebook for Keras, PyTorch and others.',
+      url='https://github.com/stared/livelossplot',
+      author='Piotr Migdał',
+      author_email='pmigdal@gmail.com',
+      keywords=['keras', 'pytorch', 'plot', 'chart'],
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Framework :: Jupyter',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+          'Topic :: Scientific/Engineering :: Visualization',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3'
+      ],
+      packages=find_packages(),
+      zip_safe=False)
