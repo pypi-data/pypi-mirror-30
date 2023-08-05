b6d2688db9882e4ff93daa0a8e9206e84dba8a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:43 +0000

    Add package pippackagepoc/1.1/pippackagepoc-1.1.tar.gz

---
+++ packages/pippackagepoc/pippackagepoc-1.1.tar.gz/pippackagepoc-1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pippackagepoc',
+    version='1.1',
+    description='The funniest joke in the world',
+    url='',
+    author='Flying Circus',
+    author_email='flyingcircus@example.com',
+    license='MIT',
+    packages=['pippackagepoc'],
+    install_requires=[
+        'markdown'
+    ],
+    zip_safe=False)
