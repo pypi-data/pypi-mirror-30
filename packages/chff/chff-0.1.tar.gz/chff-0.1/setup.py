c916127e8562f3396c89c7a6620402421486ddcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:33 +0000

    Add package chff/0.1/chff-0.1.tar.gz

---
+++ packages/chff/chff-0.1.tar.gz/chff-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='chff',
+      version='0.1',
+      description='Overlay Network Reference Implementation',
+      url='http://github.com/mnes-io/chff',
+      author='Charles Perkins',
+      author_email='charlesap@gmail.com',
+      license='MIT',
+      packages=['chff'],
+      zip_safe=False)
