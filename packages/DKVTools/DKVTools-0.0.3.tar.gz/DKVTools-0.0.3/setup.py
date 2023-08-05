1a807ee2cccd16198276db8dfa5275f38d1cfb15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:00 +0000

    Add package DKVTools/0.0.3/DKVTools-0.0.3.tar.gz

---
+++ packages/DKVTools/DKVTools-0.0.3.tar.gz/DKVTools-0.0.3/setup.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+from setuptools import find_packages
+from DKVTools import __version__
+
+setup(
+    name='DKVTools',
+    version=__version__,
+    description=(
+        u'一些常用方法'
+    ),
+    long_description=u'一些常用方法',
+    author='dekiven',
+    author_email='dekiven@163.com',
+    # maintainer='<维护人员的名字>',
+    # maintainer_email='<维护人员的邮件地址',
+    license='BSD License',
+    packages=find_packages(),
+    install_requires=[
+        # 'Pillow',
+        # 'other>=1.1'
+    ],
+    data_files=[
+    	# 'version.txt',
+    ],
+    platforms=["all"],
+    url='https://www.baidu.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
