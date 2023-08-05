fd90c48813f33378b011dfbac52b84cd7d5d8fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:14 +0000

    Add package aa-utility-package/0.1/aa%20utility%20package-0.1.tar.gz

---
+++ packages/aa-utility-package/aa%20utility%20package-0.1.tar.gz/aa utility package-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='aa utility package',
+      version='0.1',
+      description='Utility functions',
+      url='',
+      author='Gaurav Sharma',
+      author_email='gauravdbdev@gmail.com',
+      license='MIT',
+      packages=['aautilitypkg'],
+      zip_safe=False)
