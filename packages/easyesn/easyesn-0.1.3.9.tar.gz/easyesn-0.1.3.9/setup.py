25d62cc83991c3f0832376192ef2a4d0f5ba9f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:00 +0000

    Add package easyesn/0.1.3.9/easyesn-0.1.3.9.tar.gz

---
+++ packages/easyesn/easyesn-0.1.3.9.tar.gz/easyesn-0.1.3.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(name='easyesn',
+      version='0.1.3.9',
+      description='',
+      url='https://github.com/kalekiu/easyesn',
+      author='Roland Zimmermann, Luca Thiede',
+      author_email='support@flashtek.de',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+            'numpy',
+            'progressbar2',
+            'dill',
+            'multiprocess',
+            'sklearn'
+      ],
+      classifiers=[
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Developers',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+            'Programming Language :: Python :: 3'
+      ],
+      zip_safe=False)
