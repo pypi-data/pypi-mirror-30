40dc57f9d4d7bc96fe040f6370f1499027cd698f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:52 +0000

    Add package context-explorer/0.9.2/context_explorer-0.9.2.tar.gz

---
+++ packages/context-explorer/context_explorer-0.9.2.tar.gz/context_explorer-0.9.2/CONTRIBUTING.md
@@ -0,0 +1,18 @@
+Thanks for your interest in helping to improve this project!
+
+The best way to get in touch to discuss what you could help with is to [open
+a new issue](https://gitlab.com/joelostblom/context-explorer/issues/new) or
+comment on one in [the issue
+list](https://gitlab.com/joelostblom/context-explorer/issues). You can also Some
+ideas on how you can help:
+
+- General feedback and suggestions
+- Testing to use the software
+- Implementing new features
+- Improving existing features
+- Code improvements
+- Help setting up tests and continuous integrations
+
+If you don't already know what you want to help with, you can look at the issue
+list and keep an eye out for items labelled as
+[good-first-issue](https://gitlab.com/joelostblom/context-explorer/issues?label_name%5B%5D=good-first-issue).
---
+++ packages/context-explorer/context_explorer-0.9.2.tar.gz/context_explorer-0.9.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='context_explorer',
+    url='https://gitlab.com/stemcellbioengineering/context-explorer',
+    author='Joel Ostblom', author_email='joel.ostblom@gmail.com',
+    # Needed to actually package something
+    packages=find_packages(), #['context_explorer'],
+    # scripts=['bin/tile_wells'],
+    entry_points={'console_scripts':
+        ['context_explorer=context_explorer:main']},
+    # Needed for dependencies
+    install_requires=['matplotlib', 'joblib', 'scikit-learn', 'joblib', 'numpy',
+        'pandas', 'shapely', 'matplotlib', 'natsort', 'seaborn', 'scipy',
+        'pyqt', 'scikit-image'],
+    # Python version
+    python_requires='>=3.6',
+    # *strongly* suggested for sharing
+    version='0.9.2',
+    # The license can be anything you like
+    license='BSD-3',
+    description='''A tool that facilitates analyses of data extracted from
+        microscope images of cells''',
+    # We will also need a readme eventually (there will be a warning)
+    long_description=open('README.md').read()
+)
