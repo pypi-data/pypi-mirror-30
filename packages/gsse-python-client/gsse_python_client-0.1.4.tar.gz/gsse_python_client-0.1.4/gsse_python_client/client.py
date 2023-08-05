341bd1bc8a05c6e246543ddc40218f5ba4a77520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package gsse-python-client/0.1.4/gsse_python_client-0.1.4.tar.gz

---
+++ packages/gsse-python-client/gsse_python_client-0.1.4.tar.gz/gsse_python_client-0.1.4/gsse_python_client/client.py
@@ -0,0 +1,20 @@
+import requests
+
+class Client:
+    def __init__(self):
+        self.session = requests.session()
+        return
+
+    def addStock(self, ticker):
+        self.session.get("http://localhost:8000/add/stock", params = {"ticker": ticker})
+
+    def setExchange(self, exchange):
+        self.session.get("http://localhost:8000/set/exchange", params = {"exchange": exchange})
+
+    def requestTimeAdvance(self, duration):
+        r = self.session.get("http://localhost:8000/requestTimeAdvance", params= {"duration": duration})
+        return r.json()
+
+    def setDay(self, day):
+        r = self.session.get("http://localhost:8000/set/day", params = {"day": day})
+        return r.status_code;
