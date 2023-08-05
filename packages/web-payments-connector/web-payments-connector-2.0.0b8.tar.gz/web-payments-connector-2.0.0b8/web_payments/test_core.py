511164cabf2df38c1e059d866e955c534816e5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:33 +0000

    Add package web-payments-connector/2.0.0b8/web-payments-connector-2.0.0b8.tar.gz

---
+++ packages/web-payments-connector/web-payments-connector-2.0.0b8.tar.gz/web-payments-connector-2.0.0b8/web_payments/test_core.py
@@ -0,0 +1,179 @@
+from decimal import Decimal
+from unittest import TestCase
+from unittest.mock import patch, NonCallableMock
+
+from . import core
+from .status import PaymentStatus
+from .forms import CreditCardPaymentFormWithName, PaymentForm
+from .testcommon import create_test_payment
+
+BasePayment = create_test_payment()
+
+class TestProviderFactory(TestCase):
+
+    def test_provider_factory(self):
+        core.provider_factory('default')
+
+    def test_provider_does_not_exist(self):
+        self.assertRaises(ValueError, core.provider_factory, 'fake_provider')
+
+
+class TestBasePayment(TestCase):
+
+    def test_payment_attributes(self):
+        payment = BasePayment(
+            extra_data='{"attr1": "test1", "attr2": "test2"}')
+        self.assertEqual(payment.attrs.attr1, "test1")
+        self.assertEqual(payment.attrs.attr2, 'test2')
+        self.assertEqual(getattr(payment.attrs, "attr5", None), None)
+        self.assertEqual(hasattr(payment.attrs, "attr7"), False)
+
+    def test_capture_with_wrong_status(self):
+        payment = BasePayment(variant='default', status=PaymentStatus.WAITING)
+        self.assertRaises(ValueError, payment.capture)
+
+    @patch('web_payments_dummy.DummyProvider.capture')
+    def test_capture_preauth_successfully(self, mocked_capture_method):
+        amount = Decimal('20')
+        with patch.object(BasePayment, 'save') as mocked_save_method:
+            mocked_save_method.return_value = None
+            mocked_capture_method.return_value = amount
+
+            captured_amount = Decimal('0')
+            payment = BasePayment(variant='default', captured_amount=captured_amount,
+                                  status=PaymentStatus.PREAUTH)
+            payment.capture(amount)
+
+            self.assertEqual(payment.status, PaymentStatus.CONFIRMED)
+            self.assertEqual(payment.captured_amount, amount)
+        self.assertEqual(mocked_capture_method.call_count, 1)
+
+    @patch('web_payments_dummy.DummyProvider.capture')
+    def test_capture_preauth_without_amount(self, mocked_capture_method):
+        amount = None
+        with patch.object(BasePayment, 'save') as mocked_save_method:
+            mocked_save_method.return_value = None
+            mocked_capture_method.return_value = amount
+
+            captured_amount = Decimal('0')
+            status = PaymentStatus.PREAUTH
+            payment = BasePayment(variant='default', status=status,
+                                  captured_amount=captured_amount)
+            payment.capture(amount)
+
+            self.assertEqual(payment.status, status)
+            self.assertEqual(payment.captured_amount, captured_amount)
+        self.assertEqual(mocked_capture_method.call_count, 1)
+
+    def test_release_with_wrong_status(self):
+        payment = BasePayment(variant='default', status=PaymentStatus.WAITING)
+        self.assertRaises(ValueError, payment.release)
+
+    @patch('web_payments_dummy.DummyProvider.release')
+    def test_release_preauth_successfully(self, mocked_release_method):
+        with patch.object(BasePayment, 'save') as mocked_save_method:
+            mocked_save_method.return_value = None
+
+            payment = BasePayment(variant='default', status=PaymentStatus.PREAUTH)
+            payment.release()
+            self.assertEqual(payment.status, PaymentStatus.REFUNDED)
+        self.assertEqual(mocked_release_method.call_count, 1)
+
+    def test_refund_with_wrong_status(self):
+        payment = BasePayment(variant='default', status=PaymentStatus.WAITING)
+        self.assertRaises(ValueError, payment.refund)
+
+    def test_refund_too_high_amount(self):
+        payment = BasePayment(variant='default', status=PaymentStatus.CONFIRMED,
+                              captured_amount=Decimal('100'))
+        self.assertRaises(ValueError, payment.refund, Decimal('200'))
+
+    @patch('web_payments_dummy.DummyProvider.refund')
+    def test_refund_without_amount(self, mocked_refund_method):
+        refund_amount = None
+        with patch.object(BasePayment, 'save') as mocked_save_method:
+            mocked_save_method.return_value = None
+            mocked_refund_method.return_value = refund_amount
+
+            captured_amount = Decimal('200')
+            status = PaymentStatus.CONFIRMED
+            payment = BasePayment(variant='default', status=status,
+                                  captured_amount=captured_amount)
+            payment.refund(refund_amount)
+            self.assertEqual(payment.status, status)
+            self.assertEqual(payment.captured_amount, captured_amount)
+        self.assertEqual(mocked_refund_method.call_count, 1)
+
+    @patch('web_payments_dummy.DummyProvider.refund')
+    def test_refund_partial_success(self, mocked_refund_method):
+        refund_amount = Decimal('100')
+        captured_amount = Decimal('200')
+        status = PaymentStatus.CONFIRMED
+        with patch.object(BasePayment, 'save') as mocked_save_method:
+            mocked_save_method.return_value = None
+            mocked_refund_method.return_value = refund_amount
+
+            payment = BasePayment(variant='default', status=status,
+                                  captured_amount=captured_amount)
+            payment.refund(refund_amount)
+            self.assertEqual(payment.status, status)
+            self.assertEqual(payment.captured_amount, Decimal('100'))
+        self.assertEqual(mocked_refund_method.call_count, 1)
+
+    @patch('web_payments_dummy.DummyProvider.refund')
+    def test_refund_fully_success(self, mocked_refund_method):
+        refund_amount = Decimal('200')
+        captured_amount = Decimal('200')
+        with patch.object(BasePayment, 'save') as mocked_save_method:
+            mocked_save_method.return_value = None
+            mocked_refund_method.return_value = refund_amount
+
+            payment = BasePayment(variant='default', status=PaymentStatus.CONFIRMED,
+                                  captured_amount=captured_amount)
+            payment.refund(refund_amount)
+            self.assertEqual(payment.status, PaymentStatus.REFUNDED)
+            self.assertEqual(payment.captured_amount, Decimal('0'))
+        self.assertEqual(mocked_refund_method.call_count, 1)
+
+
+class TestCreditCardPaymentForm(TestCase):
+
+    def setUp(self):
+        self.credit_data = {
+                'name': 'John Doe',
+                'number': '4716124728800975',
+                'expiration': '2020-05',
+                'cvv2': '123'}
+
+    def test_form_verifies_card_number(self):
+        form = CreditCardPaymentFormWithName(data=self.credit_data)
+        self.assertTrue(form.validate())
+
+    def test_form_raises_error_for_invalid_card_number(self):
+        data = dict(self.credit_data)
+        data.update({'number': '1112223334445556'})
+        form = CreditCardPaymentFormWithName(data=data)
+        self.assertFalse(form.validate())
+        self.assertIn('number', form.errors)
+
+    def test_form_raises_error_for_invalid_cvv2(self):
+        data = dict(self.credit_data)
+        data.update({'cvv2': '12345'})
+        form = CreditCardPaymentFormWithName(data=data)
+        self.assertFalse(form.validate())
+        self.assertIn('cvv2', form.errors)
+
+"""
+class TestPaymentForm(TestCase):
+
+    def test_form_contains_hidden_fields(self):
+        data = {
+            'field1': 'value1',
+            'field2': 'value2',
+            'field3': 'value3',
+            'field4': 'value4'}
+
+        form = PaymentForm(data=data, hidden_inputs=True)
+        self.assertEqual(len(form.fields), len(data))
+        self.assertEqual(form.fields['field1'].initial, 'value1')
+"""
---
+++ packages/web-payments-connector/web-payments-connector-2.0.0b8.tar.gz/web-payments-connector-2.0.0b8/web_payments_dummy/forms.py
@@ -0,0 +1,23 @@
+
+from wtforms import SelectField, ValidationError, validators
+
+from web_payments.forms import PaymentForm
+from web_payments.status import FraudStatus, PaymentStatus
+
+
+class DummyForm(PaymentForm):
+    RESPONSE_CHOICES = (
+        ('3ds-disabled', '3DS disabled'),
+        ('3ds-redirect', '3DS redirect'),
+        ('failure', 'Gateway connection error'),
+        ('payment-error', 'Gateway returned unsupported response')
+    )
+    status = SelectField(choices=PaymentStatus.CHOICES, validators=[validators.Required()])
+    fraud_status = SelectField(choices=FraudStatus.CHOICES, validators=[validators.Required()])
+    gateway_response = SelectField(choices=RESPONSE_CHOICES, validators=[validators.Required()])
+    verification_result = SelectField(choices=PaymentStatus.CHOICES+[("", "")])
+
+    def validate(self):
+        if self.gateway_response.data == '3ds-redirect' and not self.verification_result.data:
+            self.errors["gateway_response"] = ['When 3DS is enabled you must set post validation status']
+        return super().validate()
