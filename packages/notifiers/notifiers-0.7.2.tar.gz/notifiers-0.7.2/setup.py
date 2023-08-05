28b45e22c6c25ec6c6c5edec444ab8a9c169ef20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:19 +0000

    Add package notifiers/0.7.2/notifiers-0.7.2.tar.gz

---
+++ packages/notifiers/notifiers-0.7.2.tar.gz/notifiers-0.7.2/setup.py
@@ -0,0 +1,54 @@
+import io
+from distutils.core import setup
+
+from setuptools import find_packages
+
+with io.open('README.MD', encoding='utf-8') as readme:
+    long_description = readme.read()
+
+
+def load_requirements(filename):
+    with io.open(filename, encoding='utf-8') as reqfile:
+        return [line.strip() for line in reqfile if not line.startswith('#')]
+
+
+# Populates __version__ without importing the package
+__version__ = None
+with io.open('notifiers/_version.py', encoding='utf-8') as ver_file:
+    exec(ver_file.read())  # pylint: disable=W0122
+if not __version__:
+    print('Could not find __version__ from notifiers/_version.py')
+    exit(1)
+
+setup(
+    name='notifiers',
+    version=__version__,
+    packages=find_packages(exclude=['notifiers.tests']),
+    url='https://github.com/liiight/notifiers',
+    license='MIT',
+    author='Or Carmi',
+    author_email='or.carmi82@gmail.com',
+    description='Easily send notifications everywhere',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=load_requirements('requirements.txt'),
+    tests_require=['pytest'],
+    extras_require={
+        'dev': load_requirements('dev-requirements.txt')
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop"
+    ],
+    entry_points="""
+        [console_scripts]
+        notifiers=notifiers_cli.core:entry_point
+    """,
+    python_requires='>=3.6'
+)
