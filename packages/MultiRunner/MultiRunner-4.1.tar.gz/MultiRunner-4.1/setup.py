0e1944995c56141d7efe3c01ff04e5818b6036b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:33 +0000

    Add package MultiRunner/4.1/MultiRunner-4.1.tar.gz

---
+++ packages/MultiRunner/MultiRunner-4.1.tar.gz/MultiRunner-4.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name = "MultiRunner",
+    version = "4.1",
+    description= "Add github url",
+    long_description= "This is a package for multi-process running, dedicated to my girlfriend 10L",
+    url='https://github.com/Statham-stone/MultiRunner',
+    author = "Statham",
+    author_email = "statham.stone@gmail.com",
+    packages=[""]
+)
