836f64e6bc3fc47dceb300661e3a562cd0b4207c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:39 +0000

    Add package clicol/0.1.10/clicol-0.1.10.tar.gz

---
+++ packages/clicol/clicol-0.1.10.tar.gz/clicol-0.1.10/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+PACKAGE = "clicol"
+NAME = "clicol"
+DESCRIPTION = "CLI colorizer"
+AUTHOR = "Viktor Kertesz"
+AUTHOR_EMAIL = "vkertesz2@gmail.com"
+URL = "https://github.com/realvitya/clicol"
+VERSION = __import__(PACKAGE).__version__
+LONGDESCRIPTION=open("README.rst", "rt").read()
+
+setup(
+    name='clicol',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONGDESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    license="GPLv3",
+    packages=find_packages(exclude=('tests')),
+    data_files=[
+        ('share/doc/clicol', ['doc/bashrc','doc/clicol.cfg']),
+    ],
+    install_requires=[
+        'pexpect',
+    ],
+    entry_points={
+            'console_scripts': [
+                    'clicol-telnet = clicol.clicol:main',
+                    'clicol-ssh    = clicol.clicol:main',
+                    'clicol-test   = clicol.clicol:main',
+                    'clicol-cmd    = clicol.clicol:main',
+                ],
+        },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+    ],
+    zip_safe=False,
+)
