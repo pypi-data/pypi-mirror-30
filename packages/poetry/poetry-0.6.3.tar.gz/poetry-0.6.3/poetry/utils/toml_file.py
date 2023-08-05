c3e6112233d0b60d6372c590264505b959c9b38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:37 +0000

    Add package poetry/0.6.3/poetry-0.6.3.tar.gz

---
+++ packages/poetry/poetry-0.6.3.tar.gz/poetry-0.6.3/poetry/utils/toml_file.py
@@ -0,0 +1,34 @@
+import toml
+
+from pathlib import Path
+
+from poetry.toml import dumps
+from poetry.toml import loads
+from poetry.toml import TOMLFile
+
+
+class TomlFile:
+
+    def __init__(self, path):
+        self._path = Path(path)
+
+    @property
+    def path(self):
+        return self._path
+
+    def read(self, raw=False) -> dict:
+        if raw:
+            return toml.loads(self._path.read_text())
+
+        return loads(self._path.read_text())
+
+    def write(self, data) -> None:
+        if not isinstance(data, TOMLFile):
+            data = toml.dumps(data)
+        else:
+            data = dumps(data)
+
+        self._path.write_text(data)
+
+    def __getattr__(self, item):
+        return getattr(self._path, item)
