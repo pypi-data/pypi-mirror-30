c6d1f81f79a8f45028b9f92f9f4d3ec8f2becda2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:36 +0000

    Add package django-docker-engine/0.0.41/django_docker_engine-0.0.41.tar.gz

---
+++ packages/django-docker-engine/django_docker_engine-0.0.41.tar.gz/django_docker_engine-0.0.41/django_docker_engine/proxy.py
@@ -0,0 +1,119 @@
+import errno
+import logging
+import os
+import socket
+from collections import namedtuple
+from httplib import BadStatusLine
+
+from django.conf.urls import url
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt as csrf_exempt_decorator
+from django.views.defaults import page_not_found
+from docker.errors import NotFound
+from httpproxy.views import HttpProxy
+
+from container_managers.docker_engine import DockerEngineManagerError
+from django_docker_engine.historian import NullHistorian
+from docker_utils import DockerClientWrapper
+
+try:
+    from urllib.error import HTTPError
+except ImportError:
+    from urllib2 import HTTPError
+
+try:
+    from django.views import View
+except ImportError:  # Support Django 1.7
+    from django.views.generic import View
+
+try:
+    from django.template.backends.django import DjangoTemplates
+except ImportError:  # Support Django 1.7
+    from django.template import Context, Template
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+
+UrlPatterns = namedtuple('UrlPatterns', ['urlpatterns'])
+
+
+class Proxy():
+    def __init__(self, docker_client_spec, historian=NullHistorian(),
+                 please_wait_title='Please wait',
+                 please_wait_body_html='<h1>Please wait</h1>',
+                 csrf_exempt=True):
+        self.docker_client_spec = docker_client_spec
+        self.historian = historian
+        self.csrf_exempt = csrf_exempt
+        self.content = self._render({
+            'title': please_wait_title,
+            'body_html': please_wait_body_html
+        })
+
+    def _render(self, context):
+        template_path = os.path.join(
+            os.path.dirname(__file__), 'please-wait.html')
+        template_code = open(template_path).read()
+
+        # Normally, we would use template loaders, but could there be
+        # interactions between the configs necessary here and in the parent app?
+        try:  # 1.11
+            engine = DjangoTemplates({
+                'OPTIONS': {}, 'NAME': None, 'DIRS': [], 'APP_DIRS': []
+            })
+            # All the keys are required, but the values don't seem to matter.
+            template = engine.from_string(template_code)
+        except NameError:  # 1.7
+            template = Template(template_code)
+            context = Context(context)
+
+        return template.render(context)
+
+    def url_patterns(self):
+        return [
+            url(
+                r'^(?P<container_name>[^/]*)/(?P<url>.*)$',
+                csrf_exempt_decorator(self._proxy_view) if self.csrf_exempt
+                else self._proxy_view
+            )
+        ]
+
+    def _proxy_view(self, request, container_name, url):
+        self.historian.record(container_name, url)
+        try:
+            client = DockerClientWrapper()
+            container_url = client.lookup_container_url(container_name)
+            view = HttpProxy.as_view(base_url=container_url)
+            return view(request, url=url)
+        except (DockerEngineManagerError, NotFound, BadStatusLine) as e:
+            # TODO: Should DockerEngineManagerError be sufficient by itself?
+            logger.info(
+                'Normal transient error. '
+                'Container: %s, Exception: %s', container_name, e)
+            view = self._please_wait_view_factory().as_view()
+            return view(request)
+        except socket.error as e:
+            if e.errno != errno.ECONNRESET:
+                raise
+            logger.info(
+                'Container not yet listening. '
+                'Container: %s, Exception: %s', container_name, e)
+            view = self._please_wait_view_factory().as_view()
+            return view(request)
+        except HTTPError as e:
+            logger.warn(e)
+            return page_not_found(request, e)
+            # The underlying error is not necessarily a 404,
+            # but this seems ok.
+
+    def _please_wait_view_factory(self):
+        class PleaseWaitView(View):
+            def get(inner_self, request, *args, **kwargs):
+                response = HttpResponse(self.content)
+                response.status_code = 503
+                response.reason_phrase = 'Container not yet available'
+                # Non-standard, but more clear than default;
+                # Also, before 1.9, this is not set just by changing status code.
+                return response
+            http_method_named = ['get']
+        return PleaseWaitView
---
+++ packages/django-docker-engine/django_docker_engine-0.0.41.tar.gz/django_docker_engine-0.0.41/tests/test_proxy.py
@@ -0,0 +1,82 @@
+import re
+import unittest
+from datetime import datetime
+
+from django.test import RequestFactory
+from mock import mock
+
+from django_docker_engine.docker_utils import DockerClientSpec
+from django_docker_engine.historian import FileHistorian
+from django_docker_engine.proxy import Proxy
+
+
+class CSRFTests(unittest.TestCase):
+
+    def setUp(self):
+        self.fake_get_kwargs = {
+            'request': RequestFactory().get('/fake-url'),
+            'container_name': 'fake-container',
+            'url': 'fake-url'
+        }
+        self.fake_post_kwargs = {
+            'request': RequestFactory().post('/fake-url'),
+            'container_name': 'fake-container',
+            'url': 'fake-url'
+        }
+
+    def _check_proxy_csrf(self, csrf_exempt=True):
+        with mock.patch("django_docker_engine.proxy.csrf_exempt_decorator") \
+                as csrf_exempt_mock:
+            spec = DockerClientSpec('/tmp/django-docker-data',
+                                    do_input_json_envvar=True)
+            Proxy(spec, csrf_exempt=csrf_exempt).url_patterns()
+            assert csrf_exempt_mock.called == csrf_exempt
+
+    def test_csrf_exempt_default_true(self):
+        self._check_proxy_csrf()
+
+    def test_csrf_exempt_false(self):
+        self._check_proxy_csrf(csrf_exempt=False)
+
+
+class ProxyTests(unittest.TestCase):
+
+    def test_proxy_please_wait(self):
+        history_path = '/tmp/django-docker-history-{}'.format(
+            re.sub(r'\D', '-', str(datetime.now()))
+        )
+        historian = FileHistorian(history_path)
+        title_text = 'test-title'
+        body_html = '<p>test-body</p>'
+        spec = DockerClientSpec('/tmp/django-docker-data',
+                                do_input_json_envvar=True)
+        proxy = Proxy(
+            docker_client_spec=spec,
+            historian=historian,
+            please_wait_title='<' + title_text + '>',
+            please_wait_body_html=body_html
+        )
+        urlpatterns = proxy.url_patterns()
+        self.assertEquals(len(urlpatterns), 1)
+
+        response = urlpatterns[0].callback(
+            request=RequestFactory().get('/fake-url'),
+            container_name='fake-container',
+            url='fake-url'
+        )
+
+        self.assertEqual(response.status_code, 503)
+        self.assertEqual(response.reason_phrase, 'Container not yet available')
+
+        # Title is escaped
+        self.assertIn('<title>&lt;' + title_text +
+                      '&gt;</title>', response.content)
+        # Body is not escaped
+        self.assertIn(body_html, response.content)
+
+        self.assertIn('http-equiv="refresh"', response.content)
+
+        self.assertEqual(
+            [line.split('\t')[1:] for line in historian.list()],
+            [['fake-container', 'fake-url\n']]
+        )
