a5ee8946a25b6f44e2fa0a8849b81ae49ec5e536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:33 +0000

    Add package biogrouper/0.1.3/biogrouper-0.1.3.tar.gz

---
+++ packages/biogrouper/biogrouper-0.1.3.tar.gz/biogrouper-0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='biogrouper',
+      python_requires="<3.0",
+      version='0.1.3',
+      scripts=['bin/Grouper'],
+      description='Graph-based clustering and annotation for improved de novo transcriptome analysis',
+      url='https://github.com/COMBINE-lab/Grouper',
+      author='Laraib Malik, Fatemeh Almodaresi, Rob Patro',
+      author_email='rob.patro@cs.stonybrook.edu',
+      license='BSD with attribution',
+      packages=['grouper'],
+      install_requires=[
+          'PyYAML',
+          'coloredlogs',
+          'click',
+          'networkx==1.11',
+          'numpy',
+          'pandas',
+          'tqdm'
+          'statistics'
+      ],
+      zip_safe=False)
