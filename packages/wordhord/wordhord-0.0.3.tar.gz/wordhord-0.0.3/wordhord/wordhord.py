4d52e3ef4ec1e529209e9f77ab671ad07e478573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:36 +0000

    Add package wordhord/0.0.3/wordhord-0.0.3.tar.gz

---
+++ packages/wordhord/wordhord-0.0.3.tar.gz/wordhord-0.0.3/wordhord/wordhord.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+################################################################################
+#    wordhord Copyright (C) 2018 Suizokukan
+#    Contact: suizokukan _A.T._ orange dot fr
+#
+#    This file is part of wordhord.
+#    wordhord is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    wordhord is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with wordhord.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+"""
+        wordhord by suizokukan (suizokukan AT orange DOT fr)
+
+        Toolbox for the analyse Old English texts.
+        ________________________________________________________________________
+
+        wordhord.py, global definitions for the project
+        ________________________________________________________________________
+
+        see README.md for more documentation.
+"""
+# ==============================================================================
+# project's settings
+#
+# o for __version__ format string, see https://www.python.org/dev/peps/pep-0440/ :
+#   e.g. "0.1.2.dev1" or "0.1a"
+#
+# o See also https://pypi.python.org/pypi?%3Aaction=list_classifiers
+#
+# ==============================================================================
+__projectname__ = "wordhord"
+__version__ = "0.0.3"
+__laststableversion__ = "0.0.3"
+__author__ = "Xavier Faure (suizokukan / 94.23.197.37)"
+__copyright__ = "Copyright 2018, suizokukan"
+__license__ = "GPL-3.0"
+__licensepypi__ = 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+__maintainer__ = "Xavier Faure (suizokukan)"
+__email__ = "suizokukan@orange.fr"
+__status__ = "Alpha"
+__statuspypi__ = 'Development Status :: 3 - Alpha'
