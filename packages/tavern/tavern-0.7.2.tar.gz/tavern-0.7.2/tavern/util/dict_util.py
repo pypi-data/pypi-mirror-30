5aa5856fe5f5ab0c6b1cf16e719b1ed377dc2f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:44 +0000

    Add package tavern/0.7.2/tavern-0.7.2.tar.gz

---
+++ packages/tavern/tavern-0.7.2.tar.gz/tavern-0.7.2/tavern/util/dict_util.py
@@ -0,0 +1,147 @@
+import collections
+import logging
+
+from future.utils import raise_from
+
+from tavern.util.loader import TypeConvertToken
+from . import exceptions
+
+
+logger = logging.getLogger(__name__)
+
+
+def format_keys(val, variables):
+    """recursively format a dictionary with the given values
+
+    Args:
+        val (dict): Input dictionary to format
+        variables (dict): Dictionary of keys to format it with
+
+    Returns:
+        dict: recursively formatted dictionary
+    """
+    formatted = val
+
+    if isinstance(val, dict):
+        formatted = {}
+        #formatted = {key: format_keys(val[key], variables) for key in val}
+        for key in val:
+            formatted[key] = format_keys(val[key], variables)
+    elif isinstance(val, (list, tuple)):
+        formatted = [format_keys(item, variables) for item in val]
+    elif isinstance(val, str):
+        try:
+            formatted = val.format(**variables)
+        except KeyError as e:
+            logger.error("Key(s) not found in format: %s", e.args)
+            raise_from(exceptions.MissingFormatError(e.args), e)
+    elif isinstance(val, TypeConvertToken):
+        value = format_keys(val.value, variables)
+        formatted = val.constructor(value)
+
+    return formatted
+
+
+def recurse_access_key(current_val, keys):
+    """ Given a list of keys and a dictionary, recursively access the dicionary
+    using the keys until we find the key its looking for
+
+    If a key is an integer, it will convert it and use it as a list index
+
+    Example:
+
+        >>> recurse_access_key({'a': 'b'}, ['a'])
+        'b'
+        >>> recurse_access_key({'a': {'b': ['c', 'd']}}, ['a', 'b', '0'])
+        'c'
+
+    Args:
+        current_val (dict): current dictionary we have recursed into
+        keys (list): list of str/int of subkeys
+
+    Returns:
+        str or dict: value of subkey in dict
+    """
+    if not keys:
+        return current_val
+    else:
+        current_key = keys.pop(0)
+
+        try:
+            current_key = int(current_key)
+        except ValueError:
+            pass
+
+        return recurse_access_key(current_val[current_key], keys)
+
+
+def deep_dict_merge(initial_dct, merge_dct):
+    """ Recursive dict merge. Instead of updating only top-level keys,
+    dict_merge recurses down into dicts nested to an arbitrary depth
+    and returns the merged dict. Keys values present in merge_dct take
+    precedence over values in initial_dct.
+    Modified from: https://gist.github.com/angstwad/bf22d1822c38a92ec0a9
+
+    Params:
+        initial_dct: dict onto which the merge is executed
+        merge_dct: dct merged into dct
+
+    Returns:
+        dict: recursively merged dict
+    """
+    dct = initial_dct.copy()
+
+    for k in merge_dct:
+        if (k in dct and isinstance(dct[k], dict)
+                and isinstance(merge_dct[k], collections.Mapping)):
+            dct[k] = deep_dict_merge(dct[k], merge_dct[k])
+        else:
+            dct[k] = merge_dct[k]
+
+    return dct
+
+
+def check_expected_keys(expected, actual):
+    """Check that a set of expected keys is a superset of the actual keys
+
+    Args:
+        expected (list, set, dict): keys we expect
+        actual (list, set, dict): keys we have got from the input
+
+    Raises:
+        UnexpectedKeysError: If not actual <= expected
+    """
+    expected = set(expected)
+    keyset = set(actual)
+
+    if not keyset <= expected:
+        unexpected = keyset - expected
+
+        logger.debug("Valid keys = %s, actual keys = %s", expected, keyset)
+
+        msg = "Unexpected keys {}".format(unexpected)
+        logger.error(msg)
+        raise exceptions.UnexpectedKeysError(msg)
+
+
+def yield_keyvals(block):
+    """Return indexes, keys and expected values for matching recursive keys
+
+    Given a list or dict, return a 3-tuple of the 'split' key (key split on
+    dots), the original key, and the expected value. If the input is a list, it
+    is enumerated so the 'keys' are just [0, 1, 2, ...]
+
+    Args:
+        block (dict, list): input matches
+
+    Yields:
+        (list, str, str): key split on dots, key, expected value
+    """
+    if isinstance(block, dict):
+        for joined_key, expected_val in block.items():
+            split_key = joined_key.split(".")
+            yield split_key, joined_key, expected_val
+    else:
+        for idx, val in enumerate(block):
+            sidx = str(idx)
+            yield [sidx], sidx, val
