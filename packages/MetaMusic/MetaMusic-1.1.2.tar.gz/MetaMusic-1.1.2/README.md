0c68b3425de33bd931ecdacdb01fe042d189aac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:26 +0000

    Add package MetaMusic/1.1.2/MetaMusic-1.1.2.tar.gz

---
+++ packages/MetaMusic/MetaMusic-1.1.2.tar.gz/MetaMusic-1.1.2/README.md
@@ -0,0 +1,27 @@
+       
+# Meta-Music
+Meta-Music is an open-source project that lets people add metadata to their Music library.
+
+# Using PIP
+        sudo pip install MetaMusic
+        sudo npm install -g random-material-color react react-dom
+        meta-music
+        # Or
+        meta-music /path/to/folder or song
+# Using source
+        
+        cd Meta-Music
+        git checkout pypi_package 
+        python3.6 setup.py install
+        sudo npm install -g random-material-color react react-dom
+        meta-music
+        # Or
+        meta-music /path/to/folder or song
+# Demo
+
+![demo](https://media.giphy.com/media/8PBFETWIZ39tme3vow/giphy.gif)
+
+
+**Note for MacOS users**: If you experience problems with fork in macOS, put `OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES` in your environment variable to fix the issue. This is an issue with macOS 10.13 where Apple changed the way fork() works on the OS which is incompatible with Python fork().
+ 
+**Note for Windows users**: Also install python-magic-bin using pip i.e `pip install python-magic-bin`
---
+++ packages/MetaMusic/MetaMusic-1.1.2.tar.gz/MetaMusic-1.1.2/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+
+def parse_requirements(requirements):
+    # load from requirements.txt
+    with open(requirements) as f:
+        lines = [l for l in f]
+        # remove spaces
+        stripped = map((lambda x: x.strip()), lines)
+        # remove comments
+        nocomments = filter((lambda x: not x.startswith('#')), stripped)
+        # remove empty lines
+        reqs = filter((lambda x: x), nocomments)
+        return reqs
+
+
+REQUIREMENTS = parse_requirements("requirements.txt")
+setuptools.setup(
+    name="MetaMusic",
+    version="1.1.2",
+    url="https://github.com/unique1o1/Meta-Music",
+    author="Yunik Maharjan",
+    author_email="yunik.maharjan@icloud.com",
+    license='MIT',
+    description="Metamusic",
+    platforms="Linux, MacOS, Windows",
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    install_requires=[i for i in REQUIREMENTS],
+
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Environment :: Console',
+        'Programming Language :: Python :: 3.6'
+    ],
+    entry_points="""
+    [console_scripts]
+    meta-music=metamusic.app:run
+    """,
+)
