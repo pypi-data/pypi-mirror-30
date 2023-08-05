b9570caa171989049f33c5753dec07fbbe8eee5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:43 +0000

    Add package math-braid/0.4/math_braid-0.4.tar.gz

---
+++ packages/math-braid/math_braid-0.4.tar.gz/math_braid-0.4/math_braid/braid.py
@@ -0,0 +1,397 @@
+#!/usr/bin/python
+
+from functools import reduce
+from sympy.combinatorics import Permutation
+from .canonical_factor import CanonicalFactor
+
+
+class Braid:
+    """
+    Implements a braid in left-greedy normal form, e.g. D^p A_1 A_2 ... A_k
+
+    Properties:
+        n: braid width (number of strands)
+        p: power of fundamental element D in left canonical form
+        k: number of canonical factors (implemented via a getter)
+        a: list of canonical factors
+
+    Check that we get the right number of factors
+        >>> Braid([1, 2, -1, -2], 5).k
+        2
+        >>> Braid([1, 2, -1, 2], 5).k
+        4
+
+    """
+
+    CanonicalFactor = CanonicalFactor
+
+    def __init__(self, obj=None, n=None, p=None, *args, **kwargs):
+        """
+        Initialize a braid.
+
+        Input can be any of:
+            * Another braid to copy (obj)
+            * A power of D (p) and a list of canonical factors (obj)
+            * A list of Artin generators, given as integers (obj)
+            * A list of band generators, given as 2-element lists (obj)
+            * The result of str(some braid)
+
+        Permutation list construction
+            >>> B[5]([[0,1,3,2,4],[0,3,2,4,1]])
+            B[5]([[0, 4, 3, 2, 1], [0, 3, 2, 1, 4]], 0)
+            >>> B[5]([[0,1,3,2,4],[0,3,2,4,1]], 1)
+            B[5]([[0, 4, 3, 2, 1], [0, 3, 2, 1, 4]], 1)
+            >>> B[5]([[0,1,3,2,4],[0,3,2,4,1]], 1) == B[5]([[0, 4, 3, 2, 1], [0, 3, 2, 1, 4]], 1)
+            True
+
+        Artin construction
+            >>> Braid([-3, 1], 5)
+            B[5]([[4, 0, 2, 1, 3], [1, 0, 2, 3, 4]], -1)
+
+        Band construction
+            >>> Braid([[2,1],[4,3]], 5)
+            B[5]([[1, 0, 3, 2, 4]], 0)
+            >>> Braid([[2,1],[4,4]], 5)
+            B[5]([[1, 0, 2, 3, 4]], 0)
+
+        """
+        super().__init__(*args, **kwargs)
+
+        # Easy: Copy braid properties
+        if isinstance(obj, Braid):
+            self.n = obj.n
+            self.p = obj.p
+            self.a = list(obj.a)
+        elif isinstance(obj, list) and n is not None:
+            self.n = n
+            # Quick exit for identity elements and powers of D
+            if not obj:
+                self.p = p or 0
+                self.a = []
+                return
+            if isinstance(obj[0], Braid.CanonicalFactor):
+                # A list of canonical factors? Copy so we can modify in place
+                if p is not None:
+                    self.p = p
+                    self.a = list(obj)
+                else:
+                    raise NotImplementedError
+            elif isinstance(obj[0], list) and 2 < len(obj[0]) > 2:
+                self.p = p or 0
+                self.a = [Braid.CanonicalFactor(x) for x in obj]
+            else:
+                # Starting from a word in generators
+                if isinstance(obj[0], list):
+                    # Copy list of band generators so we can modify in place
+                    if len(obj[0]) == 2:
+                        bandgens = list(obj)
+                    else:
+                        raise NotImplementedError
+                else:
+                    # Convert Artin generators to band generators
+                    if isinstance(obj[0], int):
+                        bandgens = []
+                        for x in obj:
+                            if 0 < x < n:
+                                bandgens.append([x + 1, x])
+                            elif -n < x < 0:
+                                bandgens.append([-x, 1 - x])
+                    else:
+                        raise NotImplementedError(repr(obj))
+                # Now, bandgens is guaranteed to be a list of band generators
+                # We convert to canonical factors
+                # First, self.p counts the occurrences of negative generators
+                self.p = 0
+                for i in range(len(bandgens) - 2, -1, -1):
+                    if bandgens[i + 1][0] < bandgens[i + 1][1]:
+                        self.p -= 1
+                    Braid._tauband(bandgens[i], self.n, self.p)
+                # Don't forget to check the first (leftmost) generator!
+                if bandgens[0][0] < bandgens[0][1]:
+                    self.p -= 1
+                self.a = [
+                    Braid.CanonicalFactor.createFromPair(
+                        x, self.n) for x in bandgens]
+
+            # Now, self.a is guaranteed to be a list of canonical factors
+            # Sort (sort of) this list
+            leftmost = -1
+            rightmost = len(self.a) - 2
+            while leftmost < rightmost:
+                newleft = rightmost
+                for j in range(rightmost, leftmost, -1):
+                    # I know the paper by Cha et al says d * ~self.a[j]
+                    # But I think our permutations mean different things
+                    # And the paper without pseudocode does it this way.
+                    b = (~self.a[j] * Braid.d(self.n)).meet(self.a[j + 1])
+                    if b:
+                        # Shift b one factor to the left
+                        newleft = j
+                        a_jplus = ~b * self.a[j + 1]
+                        # Keep track of identity elements
+                        if a_jplus:
+                            self.a[j + 1] = a_jplus
+                        else:
+                            if rightmost == j:
+                                rightmost -= 1
+                            self.a[j + 1] = Braid.CanonicalFactor()
+                        self.a[j] = self.a[j] * b
+                leftmost = newleft
+            # Clean up the list of canonical factors
+            # Cut out the identity elements from the right
+            a_len = rightmost + 2
+            del self.a[a_len:]
+            while a_len > 0 and not self.a[-1]:
+                del self.a[-1]
+                a_len -= 1
+            # Cut out copies of D from the left
+            while a_len > 0 and self.a[0] == Braid.d(self.n):
+                del self.a[0]
+                a_len -= 1
+                self.p += 1
+        elif obj is 1 or not obj:
+            self.n = n or 0
+            self.p = 0
+            self.a = []
+        else:
+            raise NotImplementedError
+
+    ####################
+    # Group Arithmetic #
+    ####################
+
+    def __mul__(self, other):
+        """
+        Multiplication of braids.
+
+        >>> one = Braid([1], 5)
+        >>> two = Braid([2], 5)
+        >>> one * two == Braid([1, 2], 5)
+        True
+
+        """
+        # Shortcut for identity elements
+        if not self:
+            try:
+                return Braid(other)
+            except NotImplementedError:
+                return NotImplemented
+        if other is 1 or not other:
+            return Braid(self)
+        # Ensure compatible braids
+        if not isinstance(other, Braid) or self.n != other.n:
+            return NotImplemented
+        # Combine information and construct the product
+        a = [x.tau(other.p) for x in self.a] + other.a
+        return Braid(a, n=self.n, p=self.p + other.p)
+
+    def __pow__(self, other):
+        """Compute self^other."""
+        try:
+            exponent = int(other)
+        except ValueError:
+            return NotImplemented
+        if exponent >= 0:
+            return reduce(
+                self.__class__.__mul__,
+                [self] * exponent,
+                self.__class__())
+        else:
+            return reduce(self.__class__.__mul__, [~self] * -exponent)
+
+    def __invert__(self):
+        """
+        Inverse of a braid.
+
+        >>> x = Braid([5, 1, -2, 4, 3, -1, -2], 6)
+        >>> not x * ~x
+        True
+
+        """
+        # Shortcut for identity elements
+        if not self:
+            return Braid(self)
+        # Transform and reverse the list of canonical factors
+        d = Braid.d(self.n)
+        k = self.k
+        a = [(~self.a[i] * d).tau(-self.p - i - 1)
+             for i in range(k - 1, -1, -1)]
+        # Construct the inverse
+        return Braid(a, n=self.n, p=-self.p - k)
+
+    def __eq__(self, other):
+        """
+        Equality test.
+        A quirk: identity elements from different B_n are considered equal.
+
+        Test the Artin relations
+        >>> Braid([1, 3], 4) == Braid([3, 1], 4)
+        True
+        >>> Braid([1, 2, 1], 3) == Braid([2, 1, 2], 3)
+        True
+
+        More tests, just for good measure
+        >>> Braid([1, 4, 4, 1], 7) == Braid([4, -5, 1, 1, 5, 4], 7)
+        True
+        >>> Braid([1, 4, 4, 1], 7) == Braid([4, -5, 1, 1, 6, 4], 7)
+        False
+
+        """
+        if other is 1 or not other:
+            return not self
+        if not isinstance(other, Braid):
+            return NotImplemented
+        if self.n != other.n:
+            return NotImplemented
+        return self.n == other.n and self.p == other.p and self.a == other.a
+
+    def __nonzero__(self):
+        """Override the default boolean casting, since we have a fast way."""
+        return self.p != 0 or self.k != 0
+    __bool__ = __nonzero__
+
+    ###########
+    # Helpers #
+    ###########
+
+    @staticmethod
+    def _tauband(generator, n, power=1):
+        """
+        Transformation: generator --> t^{power}(generator)
+
+        Where:          generator * D = D * t(generator)
+        Modifies a 2-element list in place. No error checks.
+
+        >>> a = [1, 5]
+        >>> Braid._tauband(a, 5)
+        >>> a
+        [1, 2]
+        >>> Braid._tauband(a, 7, -3)
+        >>> a
+        [5, 6]
+        """
+
+        if power == 0:
+            return
+        generator[0] += power
+        generator[1] += power
+        positivity = (generator[0] > generator[1])
+        generator[0] = (generator[0] - 1) % n + 1
+        generator[1] = (generator[1] - 1) % n + 1
+        if positivity != (generator[0] > generator[1]):
+            generator.reverse()
+
+    _d = {}
+
+    @classmethod
+    def d(cls, n):
+        """The fundamental factor D (lowercase delta here) in B_n."""
+        if n not in cls._d:
+            cls._d[n] = cls.CanonicalFactor([n - 1] + list(range(0, n - 1)))
+        return cls._d[n]
+
+    def _get_k(self):
+        """Return the length of the canonical factor list."""
+        return len(self.a)
+    k = property(_get_k)
+
+    def twist(self, i):
+        """
+        Make a new braid with one twist added.
+
+        >>> a = Braid([3, 2, -3, 1, 2], 5)
+        >>> a.twist(2) == a
+        False
+        >>> a.twist(2).twist(-2) == a
+        True
+        >>> a.twist(2).twist(-3) == a * Braid([2, -3], 5)
+        True
+
+        This tests that it leaves the original unchanged
+        >>> a == Braid([3, 2, -3, 1, 2], 5)
+        True
+
+        """
+        if self.n == 0:
+            return NotImplemented
+        if 0 < i < self.n:
+            p = self.p
+            a = self.a + \
+                [Braid.CanonicalFactor.createFromPair([i + 1, i], self.n)]
+        elif -self.n < i < 0:
+            p = self.p - 1
+            a = [x.tau(-1) for x in self.a] + \
+                [Braid.CanonicalFactor.createFromPair([-i, 1 - i], self.n)]
+        return Braid(a, self.n, p)
+
+    ###########
+    # Display #
+    ###########
+
+    def getPermutation(self):
+        """
+        A diagnostic function.
+
+        Test that we at least permute the strands correctly
+            >>> Braid([1], 5).getPermutation()
+            Permutation(4)(0, 1)
+
+        Test identity elements
+            >>> Braid([], 5).getPermutation()
+            Permutation(4)
+            >>> Braid().getPermutation()
+            Permutation()
+        """
+        if self.n == 0:
+            return Permutation()
+        d = Braid.d(self.n)
+        right = reduce(
+            lambda x, y: x * y,
+            self.a,
+            Permutation(
+                list(
+                    range(
+                        0,
+                        self.n))))
+        return (d ** self.p) * right
+
+    def numMixedTranspositions(self):
+        """ Number of transpositions in mixed canonical form. """
+        if self.p >= 0:
+            return (self.n - 1) * self.p + sum(a.numTranspositions()
+                                               for a in self.a)
+        elif self.p <= -self.k:
+            return (self.n - 1) * (-self.p) + sum(
+                self.n - 1 - a.numTranspositions() for a in self.a)
+        else:
+            return sum(self.n - 1 - a.numTranspositions()
+                       for a in self.a[:self.p]) + sum(a.numTranspositions() for a in self.a[self.p:])
+
+    def __str__(self):
+        '''
+        >>> str(Braid([-3,2], 5))
+        '[5] D^(-1) * [4, 0, 2, 1, 3] * [0, 2, 1, 3, 4]'
+        '''
+        return '[%s] D^(%s) * %s' % (self.n, self.p, " * ".join(map(str, self.a)))
+
+    def __repr__(self):
+        return 'B[%s]([%s], %i)' % (self.n, ", ".join(map(str, self.a)), self.p)
+
+
+class _BraidConstructorIndex:
+    """
+    >>> Braid([-3], 5) == B[5]([-3])
+    True
+    """
+
+    def __getitem__(self, key):
+        return lambda obj=None, p=None, * \
+            args, **kwargs: Braid(obj, *args, n=key, p=p, **kwargs)
+
+
+B = _BraidConstructorIndex()
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
---
+++ packages/math-braid/math_braid-0.4.tar.gz/math_braid-0.4/math_braid/canonical_factor.py
@@ -0,0 +1,285 @@
+#!/usr/bin/python
+
+"""
+Braid arithmetic.
+
+This implementation follows the description in the paper:
+J. Cha et al, "An Efficient Implementation of Braid Groups",
+Advances in Cryptology: Proceedings of ASIACRYPT 2001,
+Lecture Notes in Computer Science (2001), 144--156.
+
+"""
+
+from sympy.combinatorics import Permutation
+
+
+class CanonicalFactor(Permutation):
+    """
+    Canonical factor in the band-generator presentation.
+
+    >>> (CanonicalFactor([0, 2, 1]) * CanonicalFactor())
+    CanonicalFactor([0, 2, 1])
+    >>> (CanonicalFactor([0, 2, 1]) * CanonicalFactor([1,2,0]))
+    CanonicalFactor([2, 1, 0])
+
+    >>> x = CanonicalFactor([0, 1, 3, 2, 4])
+    >>> y = CanonicalFactor([2, 3, 4, 0, 1])
+    >>> z = CanonicalFactor([1, 4, 0, 3, 2])
+
+    >>> x == y
+    False
+    >>> y != z
+    True
+    >>> x == x
+    True
+
+    >>> x ** 2 == []
+    True
+    >>> y ** 5 == []
+    True
+
+    >>> x * y
+    CanonicalFactor([3, 2, 4, 0, 1])
+    >>> y * x
+    CanonicalFactor([2, 3, 0, 4, 1])
+    >>> (~z)
+    CanonicalFactor([2, 0, 4, 3, 1])
+
+    >>> (x * y) * z == x * (y * z)
+    True
+    >>> y * z == z * y
+    False
+
+    """
+
+    @classmethod
+    def createFromPair(cls, pair, n):
+        """
+        Given "pair"=[t,s], return the corresponding canonical factor.
+
+        The list [t,s] should denote the generator a_{ts}.
+        If a negative generator a^{-1} is represented,
+            then return the permutation corresponding to Da^{-1}.
+
+        >>> CanonicalFactor.createFromPair([5, 2], 7)
+        CanonicalFactor([0, 4, 2, 3, 1, 5, 6])
+        >>> CanonicalFactor.createFromPair([2, 5], 7)
+        CanonicalFactor([6, 3, 1, 2, 0, 4, 5])
+
+        """
+        if pair[0] > pair[1]:
+            # Positive: Just twist
+            lst = list(range(0, n))
+            lst[pair[0] - 1] = pair[1] - 1
+            lst[pair[1] - 1] = pair[0] - 1
+        elif pair[0] < pair[1]:
+            # Negative: Break into an inner cycle and an outer cycle
+            lst = [n - 1] + list(range(0, n - 1))
+            lst[pair[0] - 1] = (pair[1] - 2) % n
+            lst[pair[1] - 1] = (pair[0] - 2) % n
+        else:
+            lst = list(range(0, n))
+        return cls(lst, size=n)
+
+    @property
+    def n(self):
+        return self.size
+
+    @property
+    def table(self):
+        return self.array_form
+
+    def __str__(self):
+        return str(list(self))
+
+    def __repr__(self):
+        return "CanonicalFactor(%s)" % str(self)
+
+    def __mul__(self, other):
+        return Permutation.__mul__(other, self)
+
+    def __eq__(self, other):
+        """Equality test."""
+        try:
+            other = self.__class__(other)
+        except NotImplementedError:
+            return NotImplemented
+
+        if other.n == 0:
+            return self.n == 0 or self.table == list(range(0, self.n))
+        else:
+            return self.table == other.table
+
+    def __nonzero__(self):
+        """Nonzero test. Overridden because we can do it faster."""
+        return self.n != 0 and self.table != list(range(0, self.n))
+    __bool__ = __nonzero__
+
+    def __invert__(self):
+        """Inverse of an element."""
+
+        # Shortcut for identity
+        if self.n == 0:
+            return self.__class__(self)
+
+        # Initialize a list, and then permute it
+        mapping = [0] * self.n
+        for i in range(0, self.n):
+            # Break the abstraction barrier for some speed
+            mapping[self.table[i]] = i
+
+        return self.__class__(mapping)
+
+    def tau(self, power=1):
+        """
+        Transformation A --> t(A) where AD = Dt(A).
+
+        That is, t(A) = D^{-1} A D.
+        This function computes t^{power}(A).
+
+        >>> x = CanonicalFactor([0, 4, 2, 3, 1, 5, 6])
+        >>> d = CanonicalFactor([6, 0, 1, 2, 3, 4, 5])
+        >>> x.tau()
+        CanonicalFactor([0, 1, 5, 3, 4, 2, 6])
+        >>> x.tau(3) == d ** -3 * x * d ** 3
+        True
+        >>> x.tau(0) == x
+        True
+
+        """
+        return self.__class__([
+            (self.table[(i - power) % self.n] + power) % self.n
+            for i in range(0, self.n)])
+
+    def numTranspositions(self):
+        """
+        Count the band generators (transpositions) required to write this.
+
+        >>> from .braid import Braid
+        >>> b = Braid([1, 2, 1, 3, 3, 2, 3, 1, 2], 4)
+        >>> all(len(x.getTranspositions()) == x.numTranspositions() for x in b.a)
+        True
+
+        """
+        return len([None for i in range(0, self.n) if self.table[i] < i])
+
+    def getTranspositions(self):
+        """
+        Write a canonical factor as band generators.
+
+        >>> x = CanonicalFactor([0, 4, 2, 3, 1, 5, 6])
+        >>> x.getTranspositions()
+        [[5, 2]]
+
+        >>> from .braid import Braid
+        >>> b = Braid([1, 2, 1, 3, 3, 2, 3, 1, 2], 4)
+        >>> c = [(Braid(x.getTranspositions(), 4), x) for x in b.a]
+
+        Test that we get canonical factors back.
+        >>> all(x[0].p == 0 for x in c)
+        True
+        >>> all(x[0].k == 1 for x in c)
+        True
+
+        Test that we get the same canonical factor.
+        >>> all(x[0].a[0] == x[1] for x in c)
+        True
+
+        """
+        ans = []
+        for i, v in enumerate(self.table):
+            if v < i:
+                ans.append([i + 1, v + 1])
+        ans.reverse()
+        return ans
+
+    def computeCycles(self):
+        """
+        Compute the maxima of a permutation's descending cycles.
+
+        This method is a mutator.
+
+        >>> one = CanonicalFactor([0, 2, 1, 3, 4])
+        >>> one.computeCycles()
+        >>> one.d_cycles
+        [0, 2, 2, 3, 4]
+        >>> two = CanonicalFactor([6, 3, 1, 2, 0, 4, 5])
+        >>> two.computeCycles()
+        >>> two.d_cycles
+        [6, 3, 3, 3, 6, 6, 6]
+
+        """
+        # Break the abstraction barrier for some speed
+        self.d_cycles = list(range(0, self.n))
+        for i in range(self.n - 1, -1, -1):
+            if self.table[i] < i:
+                self.d_cycles[self.table[i]] = self.d_cycles[i]
+        return
+
+    def meet(self, other):
+        """
+        Compute the meet, self /\ other:
+        The max of all canonical factors smaller than both self and other.
+
+        >>> ident = CanonicalFactor()
+        >>> one = CanonicalFactor([0, 4, 2, 3, 1, 6, 5])
+        >>> two = CanonicalFactor([0, 4, 3, 2, 1, 5, 6])
+        >>> one.meet(two)
+        CanonicalFactor([0, 4, 2, 3, 1, 5, 6])
+        >>> two.meet(one)
+        CanonicalFactor([0, 4, 2, 3, 1, 5, 6])
+        >>> one.meet(one) == one
+        True
+        >>> two.meet(two) == two
+        True
+        >>> one.meet(ident) == ident
+        True
+
+        """
+        # These safeguards might not be necessary
+        # Time cost is about 1 part in 50
+        try:
+            other = self.__class__(other)
+        except NotImplementedError:
+            return NotImplemented
+
+        # Shortcut for an identity element
+        if self.n == 0 or other.n == 0:
+            return self.__class__()
+        if self.n != other.n:
+            return NotImplemented
+
+        # Compute descending-cycle maxima
+        self.computeCycles()
+        other.computeCycles()
+
+        # This part isn't written exactly as given in the paper
+        # That version seemed to have a few confusing redundancies
+        # Specifically, switching between 1...n and n...1 unnecessarily
+        order = list(range(0, self.n))
+        order.sort(key=lambda x: (self.d_cycles[x], other.d_cycles[x]))
+        order.reverse()
+
+        j = order[0]
+        d_cycles = [j] * self.n
+        for x in order[1:]:
+            if self.d_cycles[j] != self.d_cycles[x] or other.d_cycles[j] != other.d_cycles[x]:
+                j = x
+            d_cycles[x] = j
+
+        # Convert the cycles back into a permutation
+        prev = [-1] * self.n
+        lst = [-1] * self.n
+        for i in range(0, self.n):
+            if prev[d_cycles[i]] < 0:
+                lst[i] = d_cycles[i]
+            else:
+                lst[i] = prev[d_cycles[i]]
+            prev[d_cycles[i]] = i
+
+        return self.__class__(lst)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
