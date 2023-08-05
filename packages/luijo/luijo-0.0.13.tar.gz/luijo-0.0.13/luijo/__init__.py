b232f10d6beb59a1e417fa4cbe1d1af3dd6dff74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:05 +0000

    Add package luijo/0.0.13/luijo-0.0.13.tar.gz

---
+++ packages/luijo/luijo-0.0.13.tar.gz/luijo-0.0.13/luijo/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. currentmodule:: luiji
+.. moduleauthor:: Pat Daburu <pat@daburu.net>
+
+Extensions for Spotify Luigi
+"""
+
+__version__ = '0.0.13'  #: the working version
+__release__ = '0.0.13'  #: the release version
