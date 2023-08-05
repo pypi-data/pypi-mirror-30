d9fcd54a11dd49cb617d3a1f6661928d3c08f4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:27 +0000

    Add package pyformulas/0.1.3/pyformulas-0.1.3.tar.gz

---
+++ packages/pyformulas/pyformulas-0.1.3.tar.gz/pyformulas-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyformulas',
+  packages = ['pyformulas'], # this must be the same as the name above
+  version = '0.1.3',
+  description = 'A library of ready-to-go Python formulas',
+  author = 'pyformulas',
+  author_email = 'pyformulas@gmail.com',
+  url = 'https://github.com/pyformulas/pyformulas', # use the URL to the github repo
+  download_url = 'https://github.com/pyformulas/pyformulas/archive/0.1.3.tar.gz',
+  keywords = ['python', 'formulas', 'recipes', 'cookbook', 'lazy', 'easy', 'quick', 'shortcut'], # arbitrary keywords
+  classifiers = [],
+)
