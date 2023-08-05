5292a056d3469e3c976b486d332727531364819e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:49 +0000

    Add package indy-plenum-dev/1.2.278/indy-plenum-dev-1.2.278.tar.gz

---
+++ packages/indy-plenum-dev/indy-plenum-dev-1.2.278.tar.gz/indy-plenum-dev-1.2.278/plenum/test/script/test_change_non_primary_node_ha.py
@@ -0,0 +1,27 @@
+import pytest
+
+from plenum.test.script.helper import looper
+from stp_core.common.log import getlogger
+from plenum.test.script.helper import changeNodeHa
+
+
+logger = getlogger()
+
+whitelist = ['found legacy entry', "doesn't match", 'reconciling nodeReg',
+             'missing', 'conflicts', 'matches', 'nodeReg',
+             'conflicting address', 'unable to send message',
+             'got error while verifying message']
+
+
+@pytest.mark.skipif('sys.platform == "win32"', reason='SOV-330')
+def testChangeNodeHaForNonPrimary(looper, txnPoolNodeSet, tdirWithClientPoolTxns,
+                                  poolTxnData, poolTxnStewardNames, tconf, tdir):
+
+    changeNodeHa(looper,
+                 txnPoolNodeSet,
+                 tdirWithClientPoolTxns,
+                 poolTxnData,
+                 poolTxnStewardNames,
+                 tconf,
+                 shouldBePrimary=False,
+                 tdir=tdir)
