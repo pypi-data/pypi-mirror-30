277850c0f9384e963840880a2ffc4b85205e40bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:44 +0000

    Add package scikit-cycling/0.1.3/scikit_cycling-0.1.3-cp35-cp35m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl

---
+++ packages/scikit-cycling/scikit_cycling-0.1.3-cp35-cp35m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl/skcycling/utils/__init__.py
@@ -0,0 +1,12 @@
+"""
+The :mod:`skcycling.utils` module include utility functions.
+"""
+
+# Authors: Guillaume Lemaitre <g.lemaitre58@gmail.com>
+#          Cedric Lemaitre
+# License: BSD 3 clause
+
+from .validation import validate_filenames
+
+
+__all__ = ['validate_filenames']
