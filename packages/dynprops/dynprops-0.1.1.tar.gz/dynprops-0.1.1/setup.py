6fe17d134659497e6d369cb94b42b257dee12b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:38 +0000

    Add package dynprops/0.1.1/dynprops-0.1.1.tar.gz

---
+++ packages/dynprops/dynprops-0.1.1.tar.gz/dynprops-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='dynprops',
+    version='0.1.1',
+    url="http://github.com/hsolbrig/dynprops",
+    license='Apache 2.0',
+    author='Harold Solbrig',
+    author_email='solbrig@solbrig-informatics.com',
+    description='Dynamic Properties - support for complex tsv and sql values',
+    packages=['dynprops'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6']
+)
