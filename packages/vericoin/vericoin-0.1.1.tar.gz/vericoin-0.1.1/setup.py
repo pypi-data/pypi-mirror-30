231834bb5b84c70561de21254f21271731c7892c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:08 +0000

    Add package vericoin/0.1.1/vericoin-0.1.1.tar.gz

---
+++ packages/vericoin/vericoin-0.1.1.tar.gz/vericoin-0.1.1/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='vericoin',
+    version='0.1.1',
+    packages=['vericoin'],
+    url='https://github.com/sharebook-kr/vericoin',
+    author='jonghun.yoo',
+    author_email='jonghun.yoo@outlook.com',
+    description='암호화폐 Daily 가격 조회',
+)
