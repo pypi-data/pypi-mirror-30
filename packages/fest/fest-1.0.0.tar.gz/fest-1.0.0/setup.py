0199aa6e76a92db99309881029dc1d44c536d0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:38 +0000

    Add package fest/1.0.0/fest-1.0.0.tar.gz

---
+++ packages/fest/fest-1.0.0.tar.gz/fest-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import textwrap
+from setuptools import setup
+
+GOOGLE = ['google >= 1.9.3, < 2', 'google-api-python-client >= 1.6.4, < 2']
+WORDPRESS = ['python-wordpress-xmlrpc >= 2.3.0, < 2.4']
+ALL = GOOGLE + WORDPRESS
+
+
+setup(name='fest',
+      version='1.0.0',
+      author='amancevice',
+      author_email='smallweirdnum@gmail.com',
+      packages=['fest'],
+      url='https://github.com/amancevice/fest',
+      description='Sync Facebook events to other services',
+      long_description=textwrap.dedent(
+          '''See GitHub_ for documentation.
+          .. _GitHub: https://github.com/amancevice/fest'''),
+      install_requires=['facebook-sdk >= 2.0.0',
+                        'python-dateutil >= 2.6.1'],
+      extras_require={'all': ALL,
+                      'google': GOOGLE,
+                      'wordpress': WORDPRESS})
