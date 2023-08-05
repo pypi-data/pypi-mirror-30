010ab8ec632784978f8b47ed32f213fdd72c93d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:22 +0000

    Add package realtime-talib/0.25/realtime_talib-0.25-py2-none-any.whl

---
+++ packages/realtime-talib/realtime_talib-0.25-py2-none-any.whl/realtime_talib/Utilities.py
@@ -0,0 +1,29 @@
+import math
+from numpy import nan
+import itertools as itert
+
+#--------- LIST OPERATION METHODS------------
+
+def extendList(listToExtend, extenMultiplier): 
+	
+	extendedListTuple = tuple(itert.repeat(listToExtend, extenMultiplier))
+	return list(itert.chain.from_iterable(zip(*extendedListTuple)))
+
+#--------- TA-LIB OPERATION METHODS---------------------
+
+def removeNaN(listToChange):
+
+	index = 0
+	for i in listToChange:
+		if (math.isnan(i) == True):
+			index += 1
+		else: 
+			break
+	
+	return listToChange[index:]
+
+def getCurrentInds(indsToParse, lag):
+	
+	currentInds = [tuple([float(i[len(i)-1]) for i in indsToParse])]
+	laggedInds = extendList(currentInds, lag)
+	return laggedInds
