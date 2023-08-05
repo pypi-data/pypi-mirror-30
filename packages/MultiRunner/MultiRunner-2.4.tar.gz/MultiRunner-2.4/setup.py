c68c7f59cd6f743d3abf8d1871bc6fe4e2e5513d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:05 +0000

    Add package MultiRunner/2.4/MultiRunner-2.4.tar.gz

---
+++ packages/MultiRunner/MultiRunner-2.4.tar.gz/MultiRunner-2.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "MultiRunner",
+    version = "2.4",
+    description= "Add feature: choose the best gpu available only if there is gpu",
+    url='https://www.github.com/statham-stone/',
+    author = "Statham",
+    author_email = "statham.stone@gmail.com",
+    packages=[""]
+)
