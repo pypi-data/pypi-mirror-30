a44cac6e68a53ffd2a4d14bd2460072e1d129fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:03 +0000

    Add package expy/0.9.11.3/expy-0.9.11.3.tar.gz

---
+++ packages/expy/expy-0.9.11.3.tar.gz/expy-0.9.11.3/expy/shared.py
@@ -0,0 +1,131 @@
+# coding:utf-8
+import os
+import ctypes
+import serial
+import time
+import threading
+import platform
+
+import pyglet
+from pyglet import gl
+import pyglet.window.key as key_
+import pyglet.window.mouse as mouse_
+
+import librosa
+import pyaudio
+import numpy as np
+import math
+
+'''
+Some variable that will be used in many place.
+'''
+
+'environment varibles'
+path = os.path.dirname(os.path.abspath(__file__)) + '/'
+
+win_width = 0
+win_height = 0
+
+win = None
+need_update = False
+
+'event varibles'
+events = []
+events2 = []
+states = dict()
+
+allowed_keys = []
+allowed_keys_mapping = dict()
+
+allowed_mouse_clicks = []  # {'x':range(), 'y':range(), 'button':..}
+allowed_mouse_clicks_mapping = dict()
+
+suspending = False
+
+pressing = None
+figure_released = True
+
+start_tp = 0
+end_tp = 0
+
+onset = time.time()
+with open('log.txt','a') as f:
+    f.write('###\nOnset\t%s\n' %(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(onset)) ))
+
+'port'
+if platform.system() == "Windows":
+    if platform.architecture()[0] == '64bit':
+        port_dll = ctypes.windll.LoadLibrary(path + "inpoutx64.dll")
+    else:
+        port_dll = ctypes.windll.LoadLibrary(path + "inpout32.dll")
+else:
+    port_dll = None
+
+ser = serial.Serial(baudrate=115200)
+
+'threading'
+check_port = False
+port_result = ''
+def port_watcher():
+    global check_port
+    global start_tp
+    while 1:
+        time.sleep(0.0005)
+        if check_port:
+            port_result = ser.read()
+            start_tp = time.time()
+            check_port = False
+
+td = threading.Thread(target=port_watcher)
+td.start()
+
+lock = threading.Lock()
+
+def changeState(name, value):
+    '''
+    Change the value of a global state
+    Parameters
+    ----------
+    name: anything
+        The name of the target state
+    value: anything
+        The target value of the target state
+
+    Return
+    ---------
+    None
+    '''
+    lock.acquire()
+    try:
+        states[name] = value
+    finally:
+        lock.release()
+
+'experiment varibles'
+subject = ''
+start_block = 1
+
+setting = dict()
+timing = dict()
+
+font = dict()
+background_color = None
+font_color = None
+
+if platform.system() in ["Windows",'Linux']:
+    default_font = 'simhei'
+else:
+    default_font = 'hei'
+
+'sound'
+pa = pyaudio.PyAudio()
+# try:
+#     from pyglet.media.drivers.openal import lib_openal as al
+#     from pyglet.media.drivers.openal import lib_alc as alc
+# except:
+#     print('OpenAL not installed')
+#     has_openal = False
+# else:
+#     has_openal = True
+
+
---
+++ packages/expy/expy-0.9.11.3.tar.gz/expy-0.9.11.3/expy/stim/display.py
@@ -0,0 +1,78 @@
+from expy import shared
+from expy.colors import *
+from .draw import *
+from expy.response import *
+
+def clear(debugging=True):
+    '''
+    Clear the screen
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    '''
+    # shared.win.fill(shared.background_color)
+    # shared.pg.display.flip()
+    shared.win.clear()
+    shared.win.flip()
+    if debugging:
+        shared.win.clear()
+        shared.win.flip() 
+
+def show(out_time=False, clean_screen=True, backup=None, debugging=True):
+    '''
+    Display current canvas buffer, and keep the display during a limited period.
+
+    Parameters
+    ----------
+    out_time: num(>0), False(default)
+        The time limit of current function. (unit: second) 
+    clean_screen: True(default), False
+        Whether clear the screen after get the screen or not. 
+    backup: None, or a screen backup
+        Give a prepared screen to display
+
+    Returns
+    -------
+    None
+    '''
+    if backup:
+        shared.need_update = True
+        backup.blit(0,0,0)
+        # shared.win.blit(backup, (0, 0))
+    if shared.need_update:
+        shared.win.flip()
+        shared.need_update = False
+
+        # if not shared.start_tp:
+        #     shared.start_tp = shared.time.time()
+
+    # shared.pg.display.flip()
+    if out_time:
+        waitForResponse(shared.key_.ENTER, out_time)
+        if clean_screen:
+            clear(debugging)
+
+def getScreen(clean_screen=True):
+    '''
+    Get a backup of current canvas
+
+    Parameters
+    ----------
+    clean_screen: True(default), False
+        Whether clear the screen after get the screen or not. 
+
+    Returns
+    -------
+    None
+    '''
+
+    # backup = shared.pg.display.get_surface().convert()
+    backup = shared.pyglet.image.get_buffer_manager().get_color_buffer().get_image_data()
+    if clean_screen:
+        clear()
+    return backup
