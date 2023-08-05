d6ccfb7957988df0d17a37452c4b01b2b84c9eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:34 +0000

    Add package osupy/0.12/osupy-0.12.tar.gz

---
+++ packages/osupy/osupy-0.12.tar.gz/osupy-0.12/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+osu! Python API
+===================================
+copyright| (c) 2017-2018 CrowZ
+               - preta@crowz.r-e.kr
+license  | MIT
+===================================
+"""
+
+__title__ = 'osupy'
+__author__ = 'Preta'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017-2018 CrowZ'
+__version__ = '0.12'
+
+from .beatmap import *
+from .player import *
+from .event import *
+# from .util import *
+# from .error import *
---
+++ packages/osupy/osupy-0.12.tar.gz/osupy-0.12/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='osupy',
+      version='0.12',
+      url='https://github.com/Preta-Crowz/osupy',
+      license='MIT',
+      author='Preta',
+      author_email='preta@crowz.r-e.kr',
+      description='osu! Python API',
+      packages=[],
+      setup_requires=['requests>=2.13.0'],
+      keywords=['osu','osupy']
+     )
