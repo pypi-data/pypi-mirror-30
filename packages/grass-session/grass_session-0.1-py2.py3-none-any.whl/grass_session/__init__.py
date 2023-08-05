ddc36f9e6191462d2a3c3748e22c8e520601456e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:25 +0000

    Add package grass-session/0.1/grass_session-0.1-py2.py3-none-any.whl

---
+++ packages/grass-session/grass_session-0.1-py2.py3-none-any.whl/grass_session/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from grass_session.session import (get_platform_name,
+                                   get_grass_bin,
+                                   get_grass_gisbase,
+                                   set_grass_path_env,
+                                   write_gisrc,
+                                   grass_init,
+                                   grass_create,
+                                   Session,
+                                   GRASSBIN,
+                                   GISBASE)
+
+version = "0.1"
