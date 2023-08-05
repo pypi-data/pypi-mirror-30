ff2a19f0768d614d5b082438c7ebda2c4f8bda41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:52 +0000

    Add package isogeo-pysdk/2.19.4/isogeo_pysdk-2.19.4-py2.py3-none-any.whl

---
+++ packages/isogeo-pysdk/isogeo_pysdk-2.19.4-py2.py3-none-any.whl/isogeo_pysdk/checker.py
@@ -0,0 +1,258 @@
+# -*- coding: UTF-8 -*-
+#!/usr/bin/env python
+from __future__ import (absolute_import, unicode_literals)
+# ----------------------------------------------------------------------------
+
+"""
+Complementary set of tools to make some checks on requests to Isogeo API.
+"""
+
+# Created:      18/08/2017
+# ---------------------------------------------------------------------------
+
+# #############################################################################
+# ########## Libraries #############
+# ##################################
+
+# Standard library
+from collections import Counter
+import logging
+import socket
+from uuid import UUID
+
+# ##############################################################################
+# ########## Globals ###############
+# ##################################
+
+FILTER_KEYS = {"action": [],
+               "catalog": [],
+               "contact:group": [],
+               "contact:isogeo": [],
+               "coordinate-system": [],
+               "data-source": [],
+               "format": [],
+               "has-no": [],
+               "keyword:isogeo": [],
+               "keyword:inspire-theme": [],
+               "license:group": [],
+               "license:isogeo": [],
+               "owner": [],
+               "provider": [],
+               "text": [],
+               "type": []}
+
+FILTER_ACTIONS = ("download",
+                  "other",
+                  "view"
+                  )
+
+FILTER_PROVIDERS = ("manual",
+                    "auto"
+                    )
+
+FILTER_TYPES = ("dataset",
+                "raster-dataset",
+                "vector-dataset",
+                "resource",
+                "service"
+                )
+
+WG_KEYWORDS_CASING = ("capitalized",
+                      "lowercase",
+                      "mixedCase",
+                      "uppercase",
+                      )
+
+# ##############################################################################
+# ########## Classes ###############
+# ##################################
+
+
+class IsogeoChecker(object):
+    """Makes easier the translation of Isogeo API specific strings."""
+
+    def __init__(self, lang="FR"):
+        """Set text dictionary depending on language passed."""
+        super(IsogeoChecker, self).__init__()
+
+    def check_internet_connection(self, remote_server="https://www.isogeo.com"):
+        """Test if an internet connection is operational.
+
+        source: http://stackoverflow.com/a/20913928/2556577
+        """
+        try:
+            # see if we can resolve the host name -- tells us if there is
+            # a DNS listening
+            host = socket.gethostbyname(remote_server)
+            # connect to the host -- tells us if the host is actually
+            # reachable
+            socket.create_connection((host, 80), 2)
+            return True
+        except Exception as e:
+            logging.error(e)
+            pass
+        # end of method
+        return False
+
+    def check_bearer_validity(self, token, connect_mtd):
+        """Check API Bearer token validity.
+
+        Isogeo ID delivers authentication bearers which are valid during
+        24h, so this method checks the validity of the token (token in French)
+        with a 30 mn anticipation limit, and renews it if necessary.
+
+        token = must be a tuple like (bearer, expiration_date)
+
+        see: http://tools.ietf.org/html/rfc6750#section-2
+        FI: 24h = 86400 seconds, 30 mn = 1800, 5 mn = 300
+        """
+        if token[1] < 60:
+            token = connect_mtd
+            logging.debug("Token was about to expire, so has been renewed.")
+        else:
+            logging.debug("Token is still valid.")
+            pass
+
+        # end of method
+        return token
+
+    def check_api_response(self, response):
+        """Check API response and raise exceptions if needed."""
+        if response.status_code == 200:
+            logging.debug("Everything is OK dude, just go on!")
+            pass
+        elif response.status_code >= 400:
+            logging.error("Something's wrong Houston, check settings again!")
+            # logging.error(dir(response.request))
+            logging.error("{}: {} - {} - URL: {}"
+                          .format(response.status_code,
+                                  response.reason,
+                                  response.json().get("error"),
+                                  response.request.url))
+            # logging.error(dir(response))
+            return 0, response.status_code
+        else:
+            pass
+
+        # end of method
+        return 1
+
+    def check_request_parameters(self, parameters={}):
+        """Check parameters passed to avoid errors and help debug."""
+        # FILTERS
+        li_args = parameters.get("q").split()
+        logging.debug(li_args)
+        # li_values = [i.split(":")[1:] for i in li_args]
+
+        # Unicity
+        li_filters = [i.split(":")[0] for i in li_args]
+        filters_count = Counter(li_filters)
+        li_filters_must_be_unique = ("coordinate-system",
+                                     "format",
+                                     "owner",
+                                     "type")
+
+        for i in filters_count:
+            if i in li_filters_must_be_unique and filters_count.get(i) > 1:
+                raise ValueError("This query filter must be unique: {}"
+                                 " and it occured {} times."
+                                 .format(i, filters_count.get(i)))
+
+        # dict
+        dico_query = FILTER_KEYS.copy()
+        for i in li_args:
+            if i.startswith("action"):
+                dico_query["action"].append(i.split(":")[1:][0])
+                continue
+            elif i.startswith("catalog"):
+                dico_query["catalog"].append(i.split(":")[1:][0])
+                continue
+            elif i.startswith("contact") and i.split(":")[1] == "group":
+                dico_query["contact:group"].append(i.split(":")[1:][1])
+                continue
+            elif i.startswith("contact"):
+                dico_query["contact:isogeo"].append(i.split(":", 1)[1])
+                continue
+            elif i.startswith("coordinate-system"):
+                dico_query["coordinate-system"].append(i.split(":")[1:][0])
+                continue
+            elif i.startswith("data-source"):
+                dico_query["data-source"].append(i.split(":")[1:][0])
+                continue
+            elif i.startswith("format"):
+                dico_query["format"].append(i.split(":")[1:][0])
+                continue
+            elif i.startswith("has-no"):
+                dico_query["has-no"].append(i.split(":")[1:][0])
+                continue
+            elif i.startswith("keyword:isogeo"):
+                dico_query["keyword:isogeo"].append(i.split(":")[1:][1])
+                continue
+            elif i.startswith("keyword:inspire-theme"):
+                dico_query["keyword:inspire-theme"].append(i.split(":")[1:][1])
+                continue
+            elif i.startswith("license:isogeo"):
+                dico_query["license:isogeo"].append(i.split(":")[1:][1:])
+                continue
+            elif i.startswith("license"):
+                dico_query["license:group"].append(i.split(":", 1)[1:][0:])
+                continue
+            elif i.startswith("owner"):
+                dico_query["owner"].append(i.split(":")[1:][0])
+                continue
+            elif i.startswith("provider"):
+                dico_query["provider"].append(i.split(":")[1:][0])
+                continue
+            elif i.startswith("type"):
+                dico_query["type"].append(i.split(":")[1:][0])
+                continue
+            else:
+                logging.debug(i.split(":")[1], i.split(":")[1].isdigit())
+                dico_query["text"].append(i)
+                pass
+
+        # Values
+        dico_filters = {i.split(":")[0]: i.split(":")[1:] for i in li_args}
+        if dico_filters.get("type", ("dataset",))[0].lower() not in FILTER_TYPES:
+            raise ValueError("type value must be one of: {}"
+                             .format(" | ".join(FILTER_TYPES)))
+        elif dico_filters.get("action", ("download",))[0].lower() not in FILTER_ACTIONS:
+            raise ValueError("action value must be one of: {}"
+                             .format(" | ".join(FILTER_ACTIONS)))
+        elif dico_filters.get("provider", ("manual",))[0].lower() not in FILTER_PROVIDERS:
+            raise ValueError("provider value must be one of: {}"
+                             .format(" | ".join(FILTER_PROVIDERS)))
+        else:
+            logging.debug(dico_filters)
+
+    def check_is_uuid(self, uuid_str):
+        """
+            Check if it's an Isogeo UUID.
+
+            :param str uuid_str: UUID string to check
+        """
+        # handle Isogeo specific UUID in XML exports
+        if "isogeo:metadata" in uuid_str:
+            uuid_str = "urn:uuid:{}".format(uuid_str.split(":")[-1])
+        else:
+            pass
+        # test it
+        try:
+            uid = UUID(uuid_str)
+            return uid.hex == uuid_str.replace('-', '')\
+                                      .replace('urn:uuid:', '')
+        except ValueError as e:
+            logging.error("uuid ValueError. {} ({})  -- {}"
+                          .format(type(uuid_str), uuid_str, e))
+            return False
+        except TypeError:
+            logging.error("uuid must be a string. Not: {} ({})"
+                          .format(type(uuid_str), uuid_str))
+            return False
+
+
+# ##############################################################################
+# ##### Stand alone program ########
+# ##################################
+if __name__ == '__main__':
+    """Standalone execution."""
---
+++ packages/isogeo-pysdk/isogeo_pysdk-2.19.4-py2.py3-none-any.whl/isogeo_pysdk/utils.py
@@ -0,0 +1,203 @@
+# -*- coding: UTF-8 -*-
+#!/usr/bin/env python
+from __future__ import (absolute_import, unicode_literals)
+# ----------------------------------------------------------------------------
+
+"""
+    Complementary set of utils to use with Isogeo API.
+"""
+
+# ---------------------------------------------------------------------------
+
+# #############################################################################
+# ########## Libraries #############
+# ##################################
+
+# Standard library
+import logging
+import uuid
+
+# 3rd party
+import requests
+from six import string_types
+
+# modules
+try:
+    from . import checker
+except (ImportError, ValueError, SystemError):
+    import checker
+
+# ##############################################################################
+# ########## Globals ###############
+# ##################################
+
+checker = checker.IsogeoChecker()
+
+# ##############################################################################
+# ########## Classes ###############
+# ##################################
+
+
+class IsogeoUtils(object):
+    """Makes easier the translation of Isogeo API specific strings."""
+    API_URLS = {"prod": "api",
+                "qa": "api.qa",
+                # "int": "api.int.hq.isogeo.fr"
+                }
+
+    def __init__(self, proxies=dict()):
+        """Set text dictionary depending on language passed."""
+        self.platform, self.base_url = self.set_base_url()
+        self.proxies = proxies
+        super(IsogeoUtils, self).__init__()
+
+    def convert_uuid(self, in_uuid=str, mode=0):
+        """
+           Convert a metadata UUID to its URI equivalent. And conversely.
+
+           :param str in_uuid: UUID or URI to convert
+           :param int mode: 0 to HEX
+                            1 to URN (RFC4122)\
+                            2 to URN (Isogeo specific style)
+        """
+        # quick parameters check
+        if not isinstance(in_uuid, string_types):
+            raise TypeError("'in_uuid' expected a str value.")
+        else:
+            pass
+        if not checker.check_is_uuid(in_uuid):
+            raise ValueError("{} is not a correct UUID".format(in_uuid))
+        else:
+            pass
+        if not isinstance(mode, int) or mode not in (0, 1, 2):
+            raise TypeError("'mode' expected an integer value: 0 or 1")
+        else:
+            pass
+        # handle Isogeo specific UUID in XML exports
+        if "isogeo:metadata" in in_uuid:
+            in_uuid = "urn:uuid:{}".format(in_uuid.split(":")[-1])
+            logging.debug("Isogeo UUUID URN spotted: {}".format(in_uuid))
+        else:
+            pass
+        # operate
+        if mode == 0:
+            return uuid.UUID(in_uuid).hex
+        elif mode == 1:
+            return uuid.UUID(in_uuid).urn
+        elif mode == 2:
+            urn = uuid.UUID(in_uuid).urn
+            return "urn:isogeo:metadata:uuid:{}".format(urn.split(":")[2])
+        else:
+            pass
+
+    def get_isogeo_version(self, component="api", prot="https"):
+        """
+            Get Isogeo components versions.
+            Authentication is no required.
+
+            :param str component: one of api [default] | db | app
+        """
+        # which component
+        if component == "api":
+            version_url = "{}://v1.{}.isogeo.com/about"\
+                          .format(prot,
+                                  self.base_url
+                                  )
+        elif component == "db":
+            version_url = "{}://v1.{}.isogeo.com/about/database"\
+                          .format(prot,
+                                  self.base_url
+                                  )
+        elif component == "app" and self.platform == "prod":
+            version_url = "https://app.isogeo.com/about"
+        elif component == "app" and self.platform == "qa":
+            version_url = "https://qa-isogeo-app.azurewebsites.net/about"
+        elif component == "app" and self.platform == "int":
+            version_url = "{}://v1.api.int.hq.isogeo.fr/about"\
+                          .format(prot)
+        else:
+            raise ValueError("Component value is one theses values:"
+                             "api [default], db, app.")
+
+        # send request
+        try:
+            version_req = requests.get(version_url,
+                                       proxies=self.proxies
+                                       )
+        except requests.exceptions.SSLError as e:
+            logging.error(e)
+            version_req = requests.get(version_url,
+                                       proxies=self.proxies,
+                                       verify=False
+                                       )
+        # checking response
+        checker.check_api_response(version_req)
+
+        # end of method
+        return version_req.json().get("version")
+
+    def set_base_url(self, platform="prod"):
+        """
+            Set API base URLs according to platform.
+
+            :param str platform: one of prod | qa | int
+        """
+        platform = platform.lower()
+        self.platform = platform
+        if platform == "prod":
+            base_url = self.API_URLS.get(platform)
+            logging.debug("Using production platform.")
+        elif platform == "qa":
+            base_url = self.API_URLS.get(platform)
+            logging.debug("Using Quality Assurance platform (reduced perfs).")
+        else:
+            logging.error("Platform must be one of: {}"
+                          .format(" | ".join(self.API_URLS.keys())))
+            raise ValueError(3, "Platform must be one of: {}"
+                                .format(" | ".join(self.API_URLS.keys())))
+        # method ending
+        return platform.lower(), base_url
+
+    # -- API AUTH ------------------------------------------------------------
+    def credentials_loader(self, f_json, f_ini, e_vars):
+        """
+            Loads API credentials from a file or environment variables.
+
+            :param str f_json: path to the credentials JSON file:
+            :param str f_ini: path to the credentials INI file
+            :param dict e_vars: dict of environment variables names
+        """
+        if f_ini:
+            with open('client_secrets.json', "r") as j:
+                api = json.loads(j.read()).get("installed")
+            return api
+        elif f_json:
+            with open('client_secrets.json', "r") as j:
+                api = json.loads(j.read()).get("installed")
+            return api
+        elif e_vars:
+            with open('client_secrets.json', "r") as j:
+                api = json.loads(j.read()).get("installed")
+            return api
+        else:
+            raise ValueError()
+
+
+# ##############################################################################
+# ##### Stand alone program ########
+# ##################################
+if __name__ == '__main__':
+    """Standalone execution."""
+    utils = IsogeoUtils()
+    # from HEX
+    print("hex to hex - " + utils.convert_uuid(in_uuid="0269803d50c446b09f5060ef7fe3e22b", mode=0))
+    print("hex to urn (RFC4122) - " + utils.convert_uuid(in_uuid="0269803d50c446b09f5060ef7fe3e22b", mode=1))
+    print("hex to urn (isogeo style) - " + utils.convert_uuid(in_uuid="0269803d50c446b09f5060ef7fe3e22b", mode=2))
+    # from URN (RFC4122)
+    print("\nurn (RFC4122) to hex - " + utils.convert_uuid(in_uuid="urn:uuid:0269803d-50c4-46b0-9f50-60ef7fe3e22b", mode=0))
+    print("urn (RFC4122) to urn (RFC4122) - " + utils.convert_uuid(in_uuid="urn:uuid:0269803d-50c4-46b0-9f50-60ef7fe3e22b", mode=1))
+    print("urn (RFC4122) to urn (Isogeo style) - " + utils.convert_uuid(in_uuid="urn:uuid:0269803d-50c4-46b0-9f50-60ef7fe3e22b", mode=2))
+    # from URN (Isogeo style)
+    print("\nurn (Isogeo style) to hex - " + utils.convert_uuid(in_uuid="urn:isogeo:metadata:uuid:0269803d-50c4-46b0-9f50-60ef7fe3e22b", mode=0))
+    print("urn (Isogeo style) to urn (RFC4122) - " + utils.convert_uuid(in_uuid="urn:isogeo:metadata:uuid:0269803d-50c4-46b0-9f50-60ef7fe3e22b", mode=1))
+    print("urn (Isogeo style) to urn (Isogeo style) - " + utils.convert_uuid(in_uuid="urn:isogeo:metadata:uuid:0269803d-50c4-46b0-9f50-60ef7fe3e22b", mode=2))
