5ca746a277c1e2ec21c085a9c7744c1399077705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:30 +0000

    Add package cskv/1.0.0.dev1/cskv-1.0.0.dev1.tar.gz

---
+++ packages/cskv/cskv-1.0.0.dev1.tar.gz/cskv-1.0.0.dev1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'cskv',
+  packages = ['cskv'],
+  version = '1.0.0.dev1',
+  description = 'Config parser for INI and RAW formatted files',
+  author = 'Julen Larrucea',
+  author_email = 'julen@larrucea.eu',
+  url = 'https://github.com/julenl/cskv',
+  download_url = 'https://github.com/julenl/cskv/archive/0.1.tar.gz',
+  keywords = ['config', 'ini', 'raw', 'parser', 'non-interactive'],
+  classifiers = [],
+)
