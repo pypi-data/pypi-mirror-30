c0525b3924db5b3e96a2bb6820231ec4e1794a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:13 +0000

    Add package apollo-ml/0.3/apollo_ml-0.3.tar.gz

---
+++ packages/apollo-ml/apollo_ml-0.3.tar.gz/apollo_ml-0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'apollo_ml',
+  packages = ['apollo_ml'], # this must be the same as the name above
+  version = '0.3',
+  description = 'sklearn wrapper',
+  author = 'Joseph Farah',
+  author_email = 'jrfarah1999@gmail.com',
+  url = 'https://github.com/jrfarah/apollo_prod', # use the URL to the github repo
+  download_url = 'https://github.com/jrfarah/apollo_prod/archive/0.3.tar.gz', # I'll explain this in a second
+  keywords = ['machine', 'learning', 'sklearn', 'wrapper', 'simplicity', 'apollo_ml'], # arbitrary keywords
+  classifiers = [],
+)
