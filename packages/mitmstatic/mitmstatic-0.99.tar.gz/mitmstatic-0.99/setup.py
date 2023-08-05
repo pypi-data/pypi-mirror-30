8063c1128d1d118ef990915743b625bb63a56f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:55 +0000

    Add package mitmstatic/0.99/mitmstatic-0.99.tar.gz

---
+++ packages/mitmstatic/mitmstatic-0.99.tar.gz/mitmstatic-0.99/setup.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2017 David Preece, All rights reserved.
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+from setuptools import setup, find_packages
+
+setup(name='mitmstatic',
+      version='0.99',
+      author='David Preece',
+      author_email='davep@polymath.tech',
+      url='https://polymath.tech',
+      license='BSD',
+      packages=find_packages(),
+      install_requires=['mitmproxy'],
+      description='dms file dumper (for mitmproxy/mitmweb)',
+      long_description="Exports .dms files as created by mitmweb's export function "
+                       "into a directory structure suitable for webserving.",
+      keywords='mitmproxy mitmdump mitmweb dms html http static',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6'
+      ],
+      entry_points={
+          'console_scripts': ['mitmstatic=mitmstatic:main']
+      }
+      )
