32189059c2fd4c9e0117aa46e9255ce7e0b08552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:08 +0000

    Add package milachan/0.3.2/milachan-0.3.2.tar.gz

---
+++ packages/milachan/milachan-0.3.2.tar.gz/milachan-0.3.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from os import path
+
+setup(
+  name = 'milachan',
+  packages = ['milachan'],
+  version = '0.3.2',
+  description = 'A minimalistic framework for creating simple ImageBoards',
+  author = 'Luis Albizo',
+  author_email = 'albizo.luis@gmail.com',
+  url = 'https://github.com/LuisAlbizo/milachan',
+  keywords = ['imageboard', 'framework', 'chan','backend','database'],
+  license = 'Creative Commons Attribution-ShareAlike 4.0 International License',
+  setup_cfg = True,
+  classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: Other/Proprietary License ',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
+
