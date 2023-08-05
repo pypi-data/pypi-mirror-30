d6252eb3444cb45d15f820c3b10bfa615f8e4446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:11 +0000

    Add package treehopper/1.11.2/treehopper-1.11.2.tar.gz

---
+++ packages/treehopper/treehopper-1.11.2.tar.gz/treehopper-1.11.2/treehopper/libraries/__init__.py
@@ -0,0 +1,11 @@
+"""
+Drivers for external peripherals, like sensors, motors, displays, and IO.
+"""
+## @namespace treehopper.libraries
+
+from treehopper.libraries.Register import Register
+from treehopper.libraries.RegisterManager import RegisterManager
+from treehopper.libraries.SMBusDevice import SMBusDevice
+from treehopper.libraries.SpiDevice import SpiDevice
+from treehopper.libraries.Flushable import Flushable
+from treehopper.libraries.SpiDevice import SpiDevice
