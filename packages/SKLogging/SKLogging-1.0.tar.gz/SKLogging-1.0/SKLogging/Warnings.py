9b98d3c397507c863d33a1fcc180c50df37354a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:48 +0000

    Add package SKLogging/1.0/SKLogging-1.0.tar.gz

---
+++ packages/SKLogging/SKLogging-1.0.tar.gz/SKLogging-1.0/SKLogging/Warnings.py
@@ -0,0 +1,18 @@
+class DEBUG:
+    priority = 0
+    name = 'DEBUG'
+
+
+class INFO:
+    priority = 1
+    name = 'INFO'
+
+
+class WARNING:
+    priority = 2
+    name = 'WARNING'
+
+
+class ERROR:
+    priority = 3
+    name = 'ERROR'
