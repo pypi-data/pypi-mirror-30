ccb4aef81bf4249ad59edb5bc7694eb6653b85cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:27 +0000

    Add package splitnjoin/0.7/splitnjoin-0.7.tar.gz

---
+++ packages/splitnjoin/splitnjoin-0.7.tar.gz/splitnjoin-0.7/setup.py
@@ -0,0 +1,23 @@
+# Written by Sergio La Rosa (sergio.larosa89@gmail.com)
+# Part of "splitnjoin" package
+# https://github.com/SergioLaRosa/splitnjoin
+
+from setuptools import setup
+
+try:
+   import pypandoc
+   long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+   long_description = ''
+
+setup(
+    name='splitnjoin',
+    version='0.7',
+    description='Simple module for splitting binary files into multiple chunks and viceversa (from chunks to original file)',
+    long_description=long_description,
+    url='https://github.com/SergioLaRosa/splitnjoin',
+    author='Sergio La Rosa',
+    author_email='sergio.larosa89@gmail.com',
+    license='MIT',
+    packages=['splitnjoin'],
+    zip_safe=False)
