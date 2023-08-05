beca7799beacbd609cb2f64fb14484d74fcc6682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:34 +0000

    Add package onnx/1.0.1/onnx-1.0.1-py2.7-macosx-10.9-x86_64.egg

---
+++ packages/onnx/onnx-1.0.1-py2.7-macosx-10.9-x86_64.egg/onnx/backend/test/case/node/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from collections import namedtuple
+import sys
+
+from ..utils import import_recursive
+
+
+TestCases = []
+
+TestCase = namedtuple('TestCase', ['node', 'inputs', 'outputs', 'name'])
+
+def expect(*args, **kwargs):
+    TestCases.append(TestCase(*args, **kwargs))
+
+
+def collect_testcases():
+    '''Collect node test cases defined in python/numpy code.
+    '''
+    import_recursive(sys.modules[__name__])
+    return TestCases
