2a7604eb65bcf4997dc811b5f9d829887600fe25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:14 +0000

    Add package evenz/0.0.1/evenz-0.0.1.tar.gz

---
+++ packages/evenz/evenz-0.0.1.tar.gz/evenz-0.0.1/evenz/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. currentmodule:: evenz
+.. moduleauthor:: Pat Daburu <pat@daburu.net>
+
+Simple event-driven python.
+"""
+
+__version__ = '0.0.1'  #: the working version
+__release__ = '0.0.1'  #: the release version
