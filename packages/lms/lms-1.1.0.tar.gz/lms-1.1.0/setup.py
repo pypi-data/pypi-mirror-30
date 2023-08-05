2e4b07c6b14fd682c13ae223c22374bab396a304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:55 +0000

    Add package lms/1.1.0/lms-1.1.0.tar.gz

---
+++ packages/lms/lms-1.1.0.tar.gz/lms-1.1.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from setuptools import setup
+from lms import __version__
+
+
+setup(name='lms',
+      version=__version__,
+      description='Squeezebox server',
+      py_modules=['lms'],
+      url='https://github.com/molobrakos/lms',
+      license="",
+      scripts=["lms"],
+      author='',
+      author_email='',
+      install_requires=[
+          'requests', 'docopt'
+      ])
