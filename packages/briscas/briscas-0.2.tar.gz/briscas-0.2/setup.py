1781c629ae315fd5602a862b633d5a17381759cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:35 +0000

    Add package briscas/0.2/briscas-0.2.tar.gz

---
+++ packages/briscas/briscas-0.2.tar.gz/briscas-0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='briscas',
+    packages=['briscas'],
+    version='0.2',
+    description='Library to model the briscas card game.',
+    author='Bryan Collazo',
+    author_email='bcollazo2010@gmail.com',
+    license='MIT',
+    url='https://github.com/bcollazo/briscas',
+    download_url='https://github.com/bcollazo/briscas/archive/0.1.tar.gz',
+    keywords=['briscas', 'card', 'game', 'terminal', 'naipes'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Games/Entertainment',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[],
+)
