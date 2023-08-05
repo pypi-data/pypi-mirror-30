1ddfc32bb30f6c3abe102265f5b5a21d2c85e55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:38 +0000

    Add package onegov.winterthur/0.1.5/onegov.winterthur-0.1.5.tar.gz

---
+++ packages/onegov.winterthur/onegov.winterthur-0.1.5.tar.gz/onegov.winterthur-0.1.5/HISTORY.rst
@@ -0,0 +1,45 @@
+Changelog
+---------
+
+0.1.5 (2018-03-23)
+~~~~~~~~~~~~~~~~~~~~~
+
+- Fixes streetnames getting wrapped.
+  [href]
+
+0.1.4 (2018-03-20)
+~~~~~~~~~~~~~~~~~~~~~
+
+- Adjusts framed style.
+  [href]
+
+- Discards completely irrevant search results.
+  [href]
+
+- Adds support for streets without addresses.
+  [href]
+
+0.1.3 (2018-03-08)
+~~~~~~~~~~~~~~~~~~~~~
+
+- Fixes iframe height being calculated incorrectly.
+  [href]
+
+0.1.2 (2018-03-01)
+~~~~~~~~~~~~~~~~~~~~~
+
+- Applies Winterthur's CD to the framed version.
+  [href]
+
+0.1.1 (2018-02-06)
+~~~~~~~~~~~~~~~~~~~~~
+
+- Adds a frame-ancestors whitelist.
+  [href]
+
+0.1.0 (2018-01-31)
+~~~~~~~~~~~~~~~~~~~~~
+
+- Initial Release.
+  [href]
+
---
+++ packages/onegov.winterthur/onegov.winterthur-0.1.5.tar.gz/onegov.winterthur-0.1.5/onegov/winterthur/views/streets.py
@@ -0,0 +1,69 @@
+from itertools import groupby
+from onegov.core.cache import lru_cache
+from onegov.core.security import Public, Private
+from onegov.winterthur import WinterthurApp, _
+from onegov.winterthur.collections import AddressCollection
+from onegov.winterthur.collections import AddressSubsetCollection
+from onegov.winterthur.layout import AddressLayout
+from onegov.winterthur.layout import AddressSubsetLayout
+
+
+@WinterthurApp.html(
+    model=AddressCollection,
+    permission=Public,
+    template='streets.pt'
+)
+def view_streets(self, request):
+    request.app.enable_iframes(request)
+
+    request.include('street-search')
+    request.include('iframe-resizer')
+
+    by_letter = {
+        letter: tuple(streets) for letter, streets in groupby(
+            self.streets(), lambda s: s.letter
+        )
+    }
+
+    @lru_cache(maxsize=1)
+    def link_to_street(street):
+        return request.class_link(AddressSubsetCollection, {'street': street})
+
+    return {
+        'layout': AddressLayout(self, request),
+        'title': _("Streets Directory"),
+        'streets': by_letter,
+        'link_to_street': link_to_street
+    }
+
+
+@WinterthurApp.view(
+    model=AddressCollection,
+    permission=Private,
+    request_method='POST',
+    name="update"
+)
+def update_streets(self, request):
+    request.assert_valid_csrf_token()
+
+    self.update()
+
+    request.success(_("The streets directory has been updated"))
+
+
+@WinterthurApp.html(
+    model=AddressSubsetCollection,
+    permission=Public,
+    template='street.pt'
+)
+def view_street(self, request):
+    request.app.enable_iframes(request)
+
+    request.include('iframe-resizer')
+
+    return {
+        'layout': AddressSubsetLayout(self, request),
+        'title': self.street,
+        'addresses': self.subset(),
+        'parent': request.class_link(AddressCollection)
+    }
