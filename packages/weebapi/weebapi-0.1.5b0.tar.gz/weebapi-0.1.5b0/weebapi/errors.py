1969655e8888bb9345997444b175e88b11145d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:33 +0000

    Add package weebapi/0.1.5b0/weebapi-0.1.5b0.tar.gz

---
+++ packages/weebapi/weebapi-0.1.5b0.tar.gz/weebapi-0.1.5b0/weebapi/errors.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+class RequireFormatting(Exception):
+    pass
+
+
+class MissingRequiredArguments(Exception):
+    pass
+
+
+class WeirdResponse(Exception):
+    pass
+
+
+class SpecifyClient(Exception):
+    pass
+
+
+class Forbidden(Exception):
+    pass
+
+
+class DiscordPyNotInstalled(Exception):
+    pass
