1e3a0323b2f515ee5347f90857ce163d33ed264d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:32 +0000

    Add package base32k/0.1.5/base32k-0.1.5.zip

---
+++ packages/base32k/base32k-0.1.5.zip/base32k-0.1.5/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup \
+(
+    version="0.1.5",
+    name="base32k",
+    description="Convert decimal integers into a base 32768 number represented with characters.",
+    author="Equation Kid",
+    author_email="equationkid@gmail.com",
+    licence="MIT",
+    keywords="base32k Base32k base Base 32 32k binary bin string str hex hexadecimal encode compress number integer int compressiontools compact compression",
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    ],
+    zip_safe=False,
+    python_requires=">=3",
+)
