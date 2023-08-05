95f5b9ed2a6d1c946311294793155429b7540a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:22 +0000

    Add package python-overwatch/1.0.5/python-overwatch-1.0.5.tar.gz

---
+++ packages/python-overwatch/python-overwatch-1.0.5.tar.gz/python-overwatch-1.0.5/overwatch/test.py
@@ -0,0 +1,19 @@
+from __init__ import Overwatch
+
+def main():
+    overwatch = Overwatch(battletag="Okush#11324")
+
+    print(overwatch(hero="junkrat", filter="hero specific"))
+ 
+    # print(overwatch(hero="mei", filter="hero specific"))
+    # print(overwatch(hero="dva", filter="combat"))
+    # print(overwatch(hero='doomfist', filter="match awards"))
+    # print(overwatch(hero='roadhog', filter="average"))
+    # print(overwatch(hero='winston', filter="assists"))
+    # print(overwatch(hero='junkrat', filter="hero specific"))
+    # print(overwatch(hero='mccree', filter="hero specific"))
+    # print(overwatch(hero='soldier:76', filter="hero specific"))
+main()
+    
+
+
