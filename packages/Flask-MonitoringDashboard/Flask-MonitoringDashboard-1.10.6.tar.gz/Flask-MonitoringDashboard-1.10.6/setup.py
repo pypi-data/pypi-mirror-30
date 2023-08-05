f2400d51edb783fd35fccfe34eec7caa151915c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:52 +0000

    Add package Flask-MonitoringDashboard/1.10.6/Flask-MonitoringDashboard-1.10.6.tar.gz

---
+++ packages/Flask-MonitoringDashboard/Flask-MonitoringDashboard-1.10.6.tar.gz/Flask-MonitoringDashboard-1.10.6/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+
+def desc():
+    info = ''
+    with open('README.md', encoding='utf-8') as readme:
+        info = readme.read()
+        with open('CHANGELOG.rst', encoding='utf-8') as changelog:
+            info = info + '\n\n' + changelog.read()
+    return info
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="Flask-MonitoringDashboard",
+    version='1.10.6',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    platforms='Any',
+    zip_safe=False,
+    test_suite='flask_monitoringdashboard.test.start_testing',
+    url='https://github.com/flask-dashboard/Flask-MonitoringDashboard',
+    author="Patrick Vogel & Thijs Klooster",
+    author_email="p.p.vogel@student.rug.nl",
+    description="A dashboard for automatic monitoring of Flask web-services",
+    long_description=desc(),
+    install_requires=required,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'],
+    project_urls={
+        'Bug Reports': 'https://github.com/flask-dashboard/Flask-MonitoringDashboard/issues',
+        'PyPi': 'https://pypi.org/project/Flask-MonitoringDashboard/',
+        'Documentation': 'http://flask-monitoringdashboard.readthedocs.io/',
+        'Source': 'https://github.com/flask-dashboard/Flask-MonitoringDashboard/',
+    },
+)
