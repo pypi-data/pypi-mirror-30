e2bb3499a5f401976618b5ab30aeff8c93c1e7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:50 +0000

    Add package slackbot_ce/0.2.521761339/slackbot_ce-0.2.521761339.tar.gz

---
+++ packages/slackbot_ce/slackbot_ce-0.2.521761339.tar.gz/slackbot_ce-0.2.521761339/slackbot_ce/code_em/owen/slacklib.py
@@ -0,0 +1,26 @@
+# Put your commands here
+COMMAND1 = "is it iphone10 or iphonex"
+COMMAND2 = "do you like cake"
+COMMAND3 = "do you like the walking dead"
+COMMAND4 = "half-life 1 or half-life 2"
+COMMAND5 = "do you like me"
+
+# Your handling code goes in this function
+def handle_command(command):
+    """
+        Determine if the command is valid. If so, take action and return
+        a response, if necessary.
+    """
+    response = ""
+    if COMMAND1 in command:
+        response = "Iphone10."
+        response = " YEEEEEEEEEEEESSSSSSSSSSSSSSSSSS!" 
+        response = "The show stinks but the comic is geat."
+        response = "Half-life 1."
+        response = "No."
+        
+        
+        
+        
+        
+    return response
