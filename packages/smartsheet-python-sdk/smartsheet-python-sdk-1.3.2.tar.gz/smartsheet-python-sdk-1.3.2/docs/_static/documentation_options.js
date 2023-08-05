2b19be21bdd2096eb9c2a4500d4a409e98705427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:13 +0000

    Add package smartsheet-python-sdk/1.3.2/smartsheet-python-sdk-1.3.2.tar.gz

---
+++ packages/smartsheet-python-sdk/smartsheet-python-sdk-1.3.2.tar.gz/smartsheet-python-sdk-1.3.2/docs/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '1.3.2.dev5+gd4e533e',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
