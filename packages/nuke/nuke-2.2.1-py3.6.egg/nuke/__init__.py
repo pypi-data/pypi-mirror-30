e215208de0d962f78453e71dc5b5582410b416dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:13 +0000

    Add package nuke/2.2.1/nuke-2.2.1-py3.6.egg

---
+++ packages/nuke/nuke-2.2.1-py3.6.egg/nuke/__init__.py
@@ -0,0 +1,8 @@
+
+__author__ = "Varun Agrawal"
+__copyright__ = "Copyright 2017."
+__license__ = "MIT"
+__version__ = "2.2.1"
+__maintainer__ = "Varun Agrawal"
+__email__ = "varunagrawal@gatech.edu"
+__status__ = "Production"
