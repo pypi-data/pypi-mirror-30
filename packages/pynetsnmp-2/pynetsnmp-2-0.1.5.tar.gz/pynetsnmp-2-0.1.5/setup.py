cc47441e62d5ac55f5d6167109f8b8796b1d44d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:43 +0000

    Add package pynetsnmp-2/0.1.5/pynetsnmp-2-0.1.5.tar.gz

---
+++ packages/pynetsnmp-2/pynetsnmp-2-0.1.5.tar.gz/pynetsnmp-2-0.1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+# 'make build' will build the pynetsnmp and copy them to the needed locations.
+
+setup(
+    name="pynetsnmp-2",
+    version="0.1.5",
+    packages=find_packages(),
+    install_requires=[
+        'setuptools',
+        'ipaddr',
+        'Twisted',
+        'six',
+    ],
+    include_package_data=True,
+    # metadata for upload to PyPI
+    author="Zenoss",
+    author_email="support@zenoss.com",
+    maintainer='kalombo',
+    maintainer_email='nogamemorebrain@gmail.com',
+    description="ctypes wrapper for net-snmp.",
+    long_description="This repo is a fork of https://github.com/zenoss/pynetsnmp "
+                     "with opportunity to use set method.",
+    license="GPLv2 or later",
+    keywords=['snmp', 'twisted', 'pynetsnmp', 'netsnmp'],
+    url="https://github.com/kalombos/pynetsnmp",
+    download_url="https://github.com/kalombos/pynetsnmp",
+    zip_safe=False
+)
