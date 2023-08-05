70690725f2f971490630d25a12c9980e7302a117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:19 +0000

    Add package tapioca-ssllabs/0.2/tapioca_ssllabs-0.2-py3-none-any.whl

---
+++ packages/tapioca-ssllabs/tapioca_ssllabs-0.2-py3-none-any.whl/tapioca_ssllabs/tapioca_ssllabs.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+from tapioca import (
+    TapiocaAdapter, generate_wrapper_from_adapter, JSONAdapterMixin)
+
+from .resource_mapping import RESOURCE_MAPPING
+
+
+class SsllabsClientAdapter(JSONAdapterMixin, TapiocaAdapter):
+    api_root = 'https://api.ssllabs.com/api/{version}/'
+    resource_mapping = RESOURCE_MAPPING
+
+    def get_api_root(self, api_params):
+        version = api_params.get('protocol', 'v3')  # default https
+        return self.api_root.format(version=version)
+
+    def get_request_kwargs(self, api_params, *args, **kwargs):
+        params = super(SsllabsClientAdapter, self).get_request_kwargs(
+            api_params, *args, **kwargs)
+
+        return params
+
+    def get_iterator_list(self, response_data):
+        return response_data
+
+    def get_iterator_next_request_kwargs(self, iterator_request_kwargs,
+                                         response_data, response):
+        pass
+
+
+Ssllabs = generate_wrapper_from_adapter(SsllabsClientAdapter)
