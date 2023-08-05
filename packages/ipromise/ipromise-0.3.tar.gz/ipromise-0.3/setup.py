bf647ef867f122e5b234c34f9559655408f96278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:36 +0000

    Add package ipromise/0.3/ipromise-0.3.tar.gz

---
+++ packages/ipromise/ipromise-0.3.tar.gz/ipromise-0.3/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name = 'ipromise',
+    packages = ['ipromise'],
+    version = '0.3',
+    description = 'A Python base class that provides various decorators for specifying promises relating to inheritance.',
+    author = 'Neil Girdhar',
+    author_email = 'mistersheik@gmail.com',
+    url = 'https://github.com/NeilGirdhar/ipromise',
+    download_url = 'https://github.com/neilgirdhar/ipromise/archive/0.2.tar.gz',
+    keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    python_requires='>=3.6',
+)
