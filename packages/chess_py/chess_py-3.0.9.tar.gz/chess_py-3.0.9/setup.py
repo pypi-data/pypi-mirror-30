4852d13aa81f526e0e9718a6f7ff2829d9a7b041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:07 +0000

    Add package chess_py/3.0.9/chess_py-3.0.9.tar.gz

---
+++ packages/chess_py/chess_py-3.0.9.tar.gz/chess_py-3.0.9/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import setuptools
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='chess_py',
+    version='3.0.9',
+    description='Python chess client',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    platforms='MacOS X, Windows, Linux',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    author='Aubhro Sengupta',
+    author_email='aubhrosengupta@gmail.com',
+    url='https://github.com/LordDarkula/chess_py',
+    license='MIT',
+    packages=setuptools.find_packages()
+)
