70876b95b1ce2d03c4a5ffbd42be95271f66aa5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:46 +0000

    Add package pywebber/4.1/pywebber-4.1.tar.gz

---
+++ packages/pywebber/pywebber-4.1.tar.gz/pywebber-4.1/setup.py
@@ -0,0 +1,35 @@
+
+"""Setup"""
+
+from setuptools import setup
+
+def readme():
+    """Readme"""
+    with open("README.md") as rhand:
+        return rhand.read()
+
+setup(name='pywebber',
+      version='4.1',
+      description='Common tools employed in web development',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Programming Language :: Python :: 3.6',
+          'License :: OSI Approved :: MIT License',
+          'Intended Audience :: Developers',
+          'Environment :: Web Environment',
+          'Topic :: Internet :: WWW/HTTP :: Browsers',
+      ],
+      keywords='web crawler, lorem ipsum text generator, page indexer',
+      url='https://github.com/Parousiaic/py_webber',
+      download_url = 'https://github.com/Parousiaic/pywebber/archive/4.0.tar.gz',
+      author='Chidi Matthew Orji',
+      author_email='orjichidi95@gmail.com',
+      license='MIT',
+      packages=['pywebber'],
+      install_requires=[
+          ['beautifulsoup4', 'requests==2.18.4']
+      ],
+      zip_safe=False,
+      test_suite='nose2.collector.collector',)
