bf1501b98b8fc4dd8b11de96e88141f49529d2dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:17 +0000

    Add package waldur-openstack/0.41.1/waldur-openstack-0.41.1.tar.gz

---
+++ packages/waldur-openstack/waldur-openstack-0.41.1.tar.gz/waldur-openstack-0.41.1/src/waldur_openstack/openstack/tasks/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# Global import of all tasks from submodules.
+# Required for proper work of celery autodiscover
+# and adding all tasks to the registry.
+
+from .base import *
