13cf5b73556ef232a075b0d8a6cd2e60d840912f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:30 +0000

    Add package jsog3/2.0.1/jsog3-2.0.1.tar.gz

---
+++ packages/jsog3/jsog3-2.0.1.tar.gz/jsog3-2.0.1/setup.py
@@ -0,0 +1,37 @@
+
+
+from setuptools import setup, find_packages
+setup(
+	name='jsog3',
+	version='2.0.1',
+    packages=find_packages(),
+    scripts=['jsog.py'],
+	author='Simon Eggler',
+	author_email='simon.eggler@gmx.net',
+	url='https://github.com/simoneggler/jsog-python',
+	description='JSOG serializer and deserializer',
+	keywords='jsog json',
+	license='MIT License',
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3'
+	],
+
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.txt', '*.md'],
+    },
+
+    # metadata for upload to PyPI
+    project_urls={
+        "Bug Tracker": "https://github.com/simoneggler/jsog-python/issuespython setup.py",
+        "Documentation": "https://github.com/simoneggler/jsog-python/wiki",
+        "Source Code": "https://github.com/simoneggler/jsog-python",
+    }
+
+    # could also include long_description, download_url, classifiers, etc.
+
+)
