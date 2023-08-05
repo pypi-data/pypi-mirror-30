4c02f547c9b371246ab0dbc9d8f9601adc8b8054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:49 +0000

    Add package iscc/0.9.7/iscc-0.9.7.tar.gz

---
+++ packages/iscc/iscc-0.9.7.tar.gz/iscc-0.9.7/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='iscc',
+    version='0.9.7',
+    description="ISCC: Reference Implementation",
+    long_description=readme(),
+    author="Titusz Pan",
+    author_email='tp@py7.de',
+    url='https://github.com/coblo/iscc-specs',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=['xxhash', 'Pillow'],
+    license='BSD 2-clause',
+    zip_safe=False,
+    keywords='iscc identifier media content hash blockchain similarity',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ],
+)
