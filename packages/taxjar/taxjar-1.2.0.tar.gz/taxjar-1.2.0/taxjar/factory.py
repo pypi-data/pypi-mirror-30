8f804a8923d2ac99193b1687f3b5318916fc4da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:48 +0000

    Add package taxjar/1.2.0/taxjar-1.2.0.tar.gz

---
+++ packages/taxjar/taxjar-1.2.0.tar.gz/taxjar-1.2.0/taxjar/factory.py
@@ -0,0 +1,27 @@
+from taxjar.data.rate import TaxJarRate
+from taxjar.data.category import TaxJarCategories
+from taxjar.data.tax import TaxJarTax
+from taxjar.data.order import TaxJarOrder, TaxJarOrders
+from taxjar.data.region import TaxJarRegions
+from taxjar.data.validation import TaxJarValidation
+from taxjar.data.summary_rate import TaxJarSummaryRates
+from taxjar.exceptions import TaxJarTypeError
+
+class TaxJarTypeFactory(object):
+    @staticmethod
+    def build(data_type):
+        classes_by_type = {
+            'rate': TaxJarRate,
+            'categories': TaxJarCategories,
+            'tax': TaxJarTax,
+            'orders': TaxJarOrders,
+            'order': TaxJarOrder,
+            'refunds': TaxJarOrders,
+            'refund': TaxJarOrder,
+            'regions': TaxJarRegions,
+            'validation': TaxJarValidation,
+            'summary_rates': TaxJarSummaryRates
+        }
+        if data_type not in classes_by_type:
+            raise TaxJarTypeError("Unknown data type: " + data_type)
+        return classes_by_type[data_type]
