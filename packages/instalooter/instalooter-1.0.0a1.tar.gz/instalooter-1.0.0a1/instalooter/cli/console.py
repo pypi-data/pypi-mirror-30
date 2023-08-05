386a45500c36178b55427d0809af221c89ec3ea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:49 +0000

    Add package instalooter/1.0.0a1/instalooter-1.0.0a1.tar.gz

---
+++ packages/instalooter/instalooter-1.0.0a1.tar.gz/instalooter-1.0.0a1/instalooter/cli/console.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import functools
+import logging
+import warnings
+from typing import Callable
+
+logging.SPAM = 5
+logging.NOTICE = 25
+logging.SUCCESS = 35
+
+logging.addLevelName(logging.SPAM, "SPAM")
+logging.addLevelName(logging.NOTICE, "NOTICE")
+logging.addLevelName(logging.SUCCESS, "SUCCESS")
+
+
+def warn_logging(logger):
+    # type: (logging.Logger) -> Callable
+    """Create a `showwarning` function that uses the given logger.
+
+    Arguments:
+        logger (~logging.Logger): the logger to use.
+
+    Returns:
+        function: a function that can be used as the `warnings.showwarning`
+            callback.
+
+    """
+    def showwarning(message, category, filename, lineno, file=None, line=None):
+        logger.warning(message)
+    return showwarning
+
+
+def wrap_warnings(logger):
+    """Have the function patch `warnings.showwarning` with the given logger.
+
+    Arguments:
+        logger (~logging.logger): the logger to wrap warnings with when
+            the decorated function is called.
+
+    Returns:
+        `function`: a decorator function.
+
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def new_func(*args, **kwargs):
+            showwarning = warnings.showwarning
+            warnings.showwarning = warn_logging(logger)
+            try:
+                return func(*args, **kwargs)
+            finally:
+                warnings.showwarning = showwarning
+        return new_func
+    return decorator
