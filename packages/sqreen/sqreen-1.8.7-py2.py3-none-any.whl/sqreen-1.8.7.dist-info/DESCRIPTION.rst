24c15149ff348ce768fce9e337d4b94550ddbfb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:56 +0000

    Add package sqreen/1.8.7/sqreen-1.8.7-py2.py3-none-any.whl

---
+++ packages/sqreen/sqreen-1.8.7-py2.py3-none-any.whl/sqreen-1.8.7.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Sqreen is a SaaS based Application protection and monitoring platform that integrates directly into your Python applications.
+Learn more at `<https://www.sqreen.io/>`_.
+
---
+++ packages/sqreen/sqreen-1.8.7-py2.py3-none-any.whl/sqreen/rules_callbacks/sqreen_error_page.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016, 2017, 2018 Sqreen. All rights reserved.
+# Please refer to our terms for more information:
+#
+#     https://www.sqreen.io/terms.html
+#
+""" Base custom error page
+"""
+from os.path import dirname, join
+
+from ..rules import RuleCallback
+from ..runtime_storage import runtime
+
+
+class BaseSqreenErrorPage(RuleCallback):
+
+    def __init__(self, *args, **kwargs):
+        super(BaseSqreenErrorPage, self).__init__(*args, **kwargs)
+        with open(join(dirname(__file__), 'sqreen_error_page.html')) as f:
+            self.content = f.read()
+
+        rule_data = self.data["values"][0]
+        self.rule_type = rule_data["type"]
+
+        if self.rule_type == 'custom_error_page':
+            self.status_code = int(rule_data['status_code'])
+            runtime.attack_http_code = self.status_code
+        elif self.rule_type == 'redirection':
+            self.redirection_url = rule_data['redirection_url']
+        else:
+            raise ValueError("Invalid rule_type %s" % self.rule_type)
+
+    def pre(self, original, *args, **kwargs):
+        exception = self._get_exception(*args, **kwargs)
+
+        if exception:
+            if self.rule_type == 'custom_error_page':
+                response = self._get_response(exception, self.content, self.status_code)
+            elif self.rule_type == 'redirection':
+                headers = {'Location': self.redirection_url}
+                response = self._get_response(exception, "", 303, headers)
+
+            return {"status": "override", "new_return_value": response}
