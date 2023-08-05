dde3069c3a679c1a97a1300b07ba5defb96f7b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:05 +0000

    Add package naivepyrunner/0.1/naivepyrunner-0.1.tar.gz

---
+++ packages/naivepyrunner/naivepyrunner-0.1.tar.gz/naivepyrunner-0.1/naivepyrunner/handler.py
@@ -0,0 +1,6 @@
+class Handler():
+    def execute():
+        return -1
+
+    def stop():
+        pass
---
+++ packages/naivepyrunner/naivepyrunner-0.1.tar.gz/naivepyrunner-0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='naivepyrunner',
+      version='0.1',
+      description='Naive timed execution of custom handlers in multiple threads',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      keywords='schedule scheduling cron timing parallel',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+      ],
+      python='>=3.5',
+      url='http://github.com/henningjanssen/naivepyrunner',
+      author='Henning Janßen',
+      author_email='development@henning-janssen.net',
+      license='MIT',
+      packages=['naivepyrunner'],
+      install_requires=[],  # built-ins only
+      zip_safe=False)
