6c3751779f72c71ad13584eadd8b2d9cfc380716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:26 +0000

    Add package odoo11-addon-cms-form/11.0.1.0.2/odoo11_addon_cms_form-11.0.1.0.2-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-cms-form/odoo11_addon_cms_form-11.0.1.0.2-py2.py3-none-any.whl/odoo/addons/cms_form/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2017-2018 Simone Orsi
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'CMS Form',
+    'summary': """
+        Basic content type form""",
+    'version': '11.0.1.0.2',
+    'license': 'LGPL-3',
+    'author': 'Camptocamp, Odoo Community Association (OCA)',
+    'depends': [
+        'website',
+        'cms_status_message',
+    ],
+    'data': [
+        'security/cms_form.xml',
+        # FIXME: remove when fix merged
+        'templates/fix_odoo.xml',
+        'templates/assets.xml',
+        'templates/form.xml',
+        'templates/widgets.xml',
+    ],
+    'installable': True,
+}
