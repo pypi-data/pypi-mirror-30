dd1e6c939ecf381b1b1b88b15900ea6763f89cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:23 +0000

    Add package baycomp/1.0.1/baycomp-1.0.1.tar.gz

---
+++ packages/baycomp/baycomp-1.0.1.tar.gz/baycomp-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='baycomp',
+    version='1.0.1',
+    url='https://github.com/janezd/baycomp.git',
+    author='J. Demsar, A. Benavoli, G. Corani',
+    author_email='janez.demsar@fri.uni-lj.si',
+    description='Bayesian tests for comparison of classifiers',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib >= 2.1.2',
+        'numpy >= 1.13.1',
+        'scipy >= 0.19.1',
+        'seaborn >= 0.8.1'
+        ],
+    python_requires='>=3',
+    package_data={
+        'baycomp': ['hierarchical-t-test.stan']}
+)
