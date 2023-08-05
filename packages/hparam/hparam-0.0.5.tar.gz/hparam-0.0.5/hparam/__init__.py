931ce033924da8cfc091b4de12fdf685018a1441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:21 +0000

    Add package hparam/0.0.5/hparam-0.0.5.tar.gz

---
+++ packages/hparam/hparam-0.0.5.tar.gz/hparam-0.0.5/hparam/__init__.py
@@ -0,0 +1,2 @@
+from .Hparam import Hparam
+__version__ = '0.0.4'
---
+++ packages/hparam/hparam-0.0.5.tar.gz/hparam-0.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+ 
+setup(
+    name = 'hparam',
+    packages = ['hparam'],
+    version = '0.0.5',
+    description = 'A light-weight container for hyperparmeter management',
+    author='Qiyang Li',
+    author_email='colin.qiyang.li@gmail.com',
+    url='https://github.com/ColinQiyangLi/hparam',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: GIS'
+    ]
+)
