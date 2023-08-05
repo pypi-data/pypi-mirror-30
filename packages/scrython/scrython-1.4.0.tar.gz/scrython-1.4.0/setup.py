8e916d167b25b05ef1aeecbf36a536385dfe32ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:12 +0000

    Add package scrython/1.4.0/scrython-1.4.0.tar.gz

---
+++ packages/scrython/scrython-1.4.0.tar.gz/scrython-1.4.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+readme = ''
+with open('README.md') as f:
+	readme = f.read()
+
+setup(
+	name='scrython',
+	packages=['scrython', 'scrython.cards', 'scrython.rulings', 'scrython.catalog', 'scrython.sets', 'scrython.symbology'],
+	version='1.4.0',
+	description='A wrapper for using the Scryfall API.',
+	long_description=readme,
+	url='https://github.com/NandaScott/Scrython',
+	download_url='https://github.com/NandaScott/Scrython/archive/0.1.0.tar.gz',
+	author='Nanda Scott',
+	author_email='nanda1123@gmail.com',
+	license='MIT',
+	keywords=['Scryfall', 'magic', 'the gathering', 'scrython', 'wrapper'],
+	install_requires=['aiohttp', 'asyncio']
+	)
