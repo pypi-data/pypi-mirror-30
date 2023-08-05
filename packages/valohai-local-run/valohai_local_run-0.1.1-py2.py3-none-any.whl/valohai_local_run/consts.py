bbdaaf98a6b2a7a20e31fbba965409adf2118d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:16 +0000

    Add package valohai-local-run/0.1.1/valohai_local_run-0.1.1-py2.py3-none-any.whl

---
+++ packages/valohai-local-run/valohai_local_run-0.1.1-py2.py3-none-any.whl/valohai_local_run/consts.py
@@ -0,0 +1,6 @@
+volume_mount_targets = {
+    'repository': '/valohai/repository',
+    'inputs': '/valohai/inputs',
+    'outputs': '/valohai/outputs',
+}
+DEFAULT_OUTPUT_ROOT = './valohai-local-outputs/'
