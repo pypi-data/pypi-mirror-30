8b7b2b23388fa097fffd2bed1e30a7f2172e9b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:24 +0000

    Add package poetry/0.6.5/poetry-0.6.5.tar.gz

---
+++ packages/poetry/poetry-0.6.5.tar.gz/poetry-0.6.5/poetry/console/commands/command.py
@@ -0,0 +1,25 @@
+from cleo import Command as BaseCommand
+from cleo.inputs import ListInput
+
+from poetry.poetry import Poetry
+
+from ..styles.poetry import PoetryStyle
+
+
+class Command(BaseCommand):
+
+    @property
+    def poetry(self) -> Poetry:
+        return self.get_application().poetry
+
+    def reset_poetry(self) -> None:
+        self.get_application().reset_poetry()
+
+    def run(self, i, o) -> int:
+        """
+        Initialize command.
+        """
+        self.input = i
+        self.output = PoetryStyle(i, o)
+
+        return super(BaseCommand, self).run(i, o)
