43680beab1047ee7b1ae585ff065c0d6634d8da2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:10 +0000

    Add package stearns_cliff/0.0.1/stearns_cliff-0.0.1.tar.gz

---
+++ packages/stearns_cliff/stearns_cliff-0.0.1.tar.gz/stearns_cliff-0.0.1/stearns_cliff/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. currentmodule:: stearns_cliff
+.. moduleauthor:: Stearns Financial Services <softwaredevelopment@stearnsbank.com>
+
+A postal zones seed data loader for cooper.
+"""
+
+__version__ = '0.0.1'  #: the working version
+__release__ = '0.0.1'  #: the release version
