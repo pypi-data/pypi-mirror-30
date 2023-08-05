1211d2f1c55e4878a19effeac47a9ef601fc2366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:38 +0000

    Add package buildlib/2.0.0b35/buildlib-2.0.0b35-py3-none-any.whl

---
+++ packages/buildlib/buildlib-2.0.0b35-py3-none-any.whl/buildlib/kubernetes/__init__.py
@@ -0,0 +1,86 @@
+from base64 import b64encode
+import string
+import random
+import json
+import re
+import subprocess as sp
+from typing import Optional, List, Pattern
+from buildlib.kubernetes import cmd
+
+
+def parse_kubectl_option(
+    flag: str,
+    args: List[str],
+    sep: 'str',
+) -> list:
+    """"""
+    if type(args) == list:
+        return [flag, sep.join(args)]
+    else:
+        return []
+
+
+
+def generate_password(length: int = 32):
+    """
+    Generate a base64 encoded string consisting of numbers and upper/lower case
+    ascii letters.
+    Kubernetes requires base64 encoding.
+    """
+    rand_items = (
+        random.SystemRandom().choice(string.ascii_letters + string.digits)
+        for _
+        in range(length)
+    )
+    s = ''.join(rand_items)
+    s = b64encode(s.encode()).decode('utf8')
+
+    return s
+
+
+def get_item_names(
+    namespace: List[str],
+    kind: List[str],
+    label: List[str],
+    namefilter: Optional[Pattern] = None,
+    statusfilter: Optional[Pattern]=None,
+    **cmdargs,
+) -> Optional[List[str]]:
+
+
+    kind = parse_kubectl_option('', kind, sep=',')
+
+    options = [
+        *parse_kubectl_option('-n', namespace, sep=','),
+        *parse_kubectl_option('-l', label, sep=','),
+    ]
+
+    result = sp.run(
+        ['kubectl', 'get' ] + kind + options + ['-o', 'json'],
+        check=True,
+        stdout=sp.PIPE,
+    )
+
+    output = result.stdout.decode()
+    data = json.loads(output)
+    items = data.get('items')
+    item_names = []
+
+    for item in items:
+        name = item.get('metadata', {}).get('name')
+
+        try:
+            state = item.get('status', {}).get('containerStatuses', [])[0].get('state')
+        except KeyError:
+            state = {}
+
+        if namefilter and not re.search(namefilter, name):
+            continue
+
+        if statusfilter:
+            if not any([re.search(statusfilter, k) for k in state.keys()]):
+                continue
+
+        item_names.append(name)
+
+    return item_names
