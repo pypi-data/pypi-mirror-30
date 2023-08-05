c70dc408d4f5506ed93c4708c1c9242416b3191f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:48 +0000

    Add package vlinx-util/0.0.3/vlinx-util-0.0.3.tar.gz

---
+++ packages/vlinx-util/vlinx-util-0.0.3.tar.gz/vlinx-util-0.0.3/vlinxutil/process_util.py
@@ -0,0 +1,25 @@
+from subprocess import Popen, PIPE
+from threading import Thread
+import shlex
+
+
+def run_process(command, show_stdout=True, show_stderr=True):
+    with Popen(shlex.split(command), shell=False, stdout=PIPE, stderr=PIPE, bufsize=1, universal_newlines=True) as p:
+        def show_stdout():
+            for line in p.stdout:
+                print(line, end='')
+
+        def show_stderr():
+            for line in p.stderr:
+                print(line, end='')
+
+        if show_stdout:
+            t = Thread(target=show_stdout)
+            t.start()
+        if show_stderr:
+            t = Thread(target=show_stderr)
+            t.start()
+
+        p.wait()
+
+        return p.returncode
