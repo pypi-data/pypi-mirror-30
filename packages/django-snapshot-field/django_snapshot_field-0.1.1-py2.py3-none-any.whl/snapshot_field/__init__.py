e93b29689c1877234663b24988488a5b5d8c4789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:50 +0000

    Add package django-snapshot-field/0.1.1/django_snapshot_field-0.1.1-py2.py3-none-any.whl

---
+++ packages/django-snapshot-field/django_snapshot_field-0.1.1-py2.py3-none-any.whl/snapshot_field/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from .fields import SnapshotModelField
+
+__version__ = '0.1.1'
+
+default_app_config = 'snapshot_field.apps.SnapshotFieldConfig'
---
+++ packages/django-snapshot-field/django_snapshot_field-0.1.1-py2.py3-none-any.whl/snapshot_field/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig as BaseConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class SnapshotFieldConfig(BaseConfig):
+    name = 'snapshot_field'
+    verbose_name = _('Snapshot Field')
