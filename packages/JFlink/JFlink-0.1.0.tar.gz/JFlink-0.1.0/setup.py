c503eb997986f6706ad717f159bf780a6c588496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:47 +0000

    Add package JFlink/0.1.0/JFlink-0.1.0.tar.gz

---
+++ packages/JFlink/JFlink-0.1.0.tar.gz/JFlink-0.1.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+
+    name='JFlink',
+
+    package=['JFlink'],
+
+    version='0.1.0',
+
+    description='A collection of tools to improve workflow between JMCT and FISPACT',
+
+    license='MIT License',
+
+    install_requires=['lxml>=4.1.1'],
+
+
+
+    author='',
+
+    author_email='',
+
+    url='https://github.com/starmode/JFlink',
+
+    packages=find_packages(),
+
+    platforms='any',
+
+    classifiers=[  
+
+        "Development Status :: 3 - Alpha",  
+
+        "Environment :: Console",  
+
+        "Intended Audience :: Science/Research",  
+
+        "License :: OSI Approved :: MIT License",  
+
+        "Topic :: Scientific/Engineering",  
+
+    ]
+
+)
