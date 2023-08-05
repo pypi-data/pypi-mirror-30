b6f12090c2c6a256768c0109da6703a0b1ff020e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:14 +0000

    Add package just4demo/0.0.1/just4demo-0.0.1.tar.gz

---
+++ packages/just4demo/just4demo-0.0.1.tar.gz/just4demo-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='just4demo',
+    version='0.0.1',
+    description='some shit',
+    license='MIT',
+    packages=['dna'],
+    author='Emma Watson',
+    author_email='anshulk.it@gmail.com',
+    keywords=['example'],
+    url='https://github.com/bgse/hellostackoverflow'
+)
