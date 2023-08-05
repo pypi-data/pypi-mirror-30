4f8f9e625bbe9b5123af4d0fc0dea45738d8b3c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:45 +0000

    Add package doublethink/0.2.0.dev88/doublethink-0.2.0.dev88.tar.gz

---
+++ packages/doublethink/doublethink-0.2.0.dev88.tar.gz/doublethink-0.2.0.dev88/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import codecs
+
+setuptools.setup(
+    name='doublethink',
+    version='0.2.0.dev88',
+    packages=['doublethink'],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=['rethinkdb'],
+    url='https://github.com/internetarchive/doublethink',
+    author='Noah Levitt',
+    author_email='nlevitt@archive.org',
+    description='rethinkdb python library',
+    long_description=codecs.open(
+        'README.rst', mode='r', encoding='utf-8').read(),
+    entry_points={
+            'console_scripts': [
+                'doublethink-purge-stale-services=doublethink.cli:purge_stale_services',
+            ]
+    },
+)
