5f9f25790763829c8e5fb8b47e7b8a1f0c4acc9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:31 +0000

    Add package evtcantools/0.1.0/evtcantools-0.1.0.tar.gz

---
+++ packages/evtcantools/evtcantools-0.1.0.tar.gz/evtcantools-0.1.0/setup.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup , find_packages
+
+
+setup(name='evtcantools',
+      version='0.1.0',
+      description='Tools shared across software projects',
+      url='https://bitbucket.org/evt/evt-can-tools',
+      data_files = [],
+      author='EVT RIT',
+      author_email='EVT@RIT.EDU',
+      packages=find_packages(),
+      scripts= [],
+      license='MIT',
+      include_package_data=True,
+      package_data={ '': ['*.txt', '*.rst']},
+      zip_safe=False,
+      entry_points ={
+
+      })
+
+
+BIN_PATH = '/usr/local/bin'
