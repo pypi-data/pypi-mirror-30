9ba6351de6447381fe6b903080f0f7d7b9a360b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:22 +0000

    Add package txHTTP/0.1.1/txHTTP-0.1.1-py3-none-any.whl

---
+++ packages/txHTTP/txHTTP-0.1.1-py3-none-any.whl/txhttp/__init__.py
@@ -0,0 +1,7 @@
+from .application import Application
+from .handlers.generic.base import Handler
+from .server import Server
+
+__version__ = '0.1.1'
+
+__all__ = ['Application', 'Server', 'Handler']
---
+++ packages/txHTTP/txHTTP-0.1.1-py3-none-any.whl/txhttp/utils/decorators.py
@@ -0,0 +1,10 @@
+class classonlymethod(classmethod):
+    """
+    Shamelessly taken from Django's utils decorators
+    django.utils.decorators.classonlymethod
+    """
+
+    def __get__(self, instance, cls=None):
+        if instance is not None:
+            raise AttributeError("This method is available only on the class, not on instances.")
+        return super().__get__(instance, cls)
