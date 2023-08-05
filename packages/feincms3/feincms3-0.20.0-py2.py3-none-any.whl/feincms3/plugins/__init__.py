9e0d8307d7fccb566838a01203e3474bf9d365ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:18 +0000

    Add package feincms3/0.20.0/feincms3-0.20.0-py2.py3-none-any.whl

---
+++ packages/feincms3/feincms3-0.20.0-py2.py3-none-any.whl/feincms3/plugins/__init__.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+
+from .html import *
+from .snippet import *
+
+
+try:
+    import html_sanitizer
+except ImportError:  # pragma: no cover
+    pass
+else:
+    from .richtext import *
+
+try:
+    import requests
+except ImportError:  # pragma: no cover
+    pass
+else:
+    from .external import *
+
+try:
+    import versatileimagefield
+except ImportError:  # pragma: no cover
+    pass
+else:
+    from .versatileimage import *
