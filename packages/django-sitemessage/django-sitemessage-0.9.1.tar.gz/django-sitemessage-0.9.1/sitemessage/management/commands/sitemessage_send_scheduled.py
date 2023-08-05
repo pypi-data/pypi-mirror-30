87fb0beb1d67b3628c3f1bd2396e3f3596461f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:20 +0000

    Add package django-sitemessage/0.9.1/django-sitemessage-0.9.1.tar.gz

---
+++ packages/django-sitemessage/django-sitemessage-0.9.1.tar.gz/django-sitemessage-0.9.1/sitemessage/management/commands/sitemessage_send_scheduled.py
@@ -0,0 +1,37 @@
+from django.core.management.base import BaseCommand
+
+from sitemessage.toolbox import send_scheduled_messages
+from sitemessage.compat import CommandOption, options_getter
+
+
+get_options = options_getter((
+    CommandOption(
+        '--priority', action='store', dest='priority', default=None,
+        help='Allows to filter scheduled messages by a priority number. Defaults to None.'
+    ),
+))
+
+
+class Command(BaseCommand):
+
+    help = 'Sends scheduled messages (both in pending and error statuses).'
+
+    option_list = get_options()
+
+    def add_arguments(self, parser):
+        get_options(parser.add_argument)
+
+    def handle(self, *args, **options):
+        priority = options.get('priority', None)
+        priority_str = ''
+
+        if priority is not None:
+            priority_str = 'with priority %s ' % priority
+
+        self.stdout.write('Sending scheduled messages %s...\n' % priority_str)
+        try:
+            send_scheduled_messages(priority=priority)
+        except Exception as e:
+            self.stderr.write(self.style.ERROR('Error on send: %s\n' % e))
+        else:
+            self.stdout.write('Sending done.\n')
