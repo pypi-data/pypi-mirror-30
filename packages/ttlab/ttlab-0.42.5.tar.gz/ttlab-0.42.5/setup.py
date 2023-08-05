6f0fb50ae0f63da9f83edc3b8bf1acc5c4c02da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:02 +0000

    Add package ttlab/0.42.5/ttlab-0.42.5.tar.gz

---
+++ packages/ttlab/ttlab-0.42.5.tar.gz/ttlab-0.42.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+setup(name='ttlab',
+      version='0.42.5',
+      description='Next generation analysis software',
+      url='',
+      author='Christopher Tiburski and Johan Tenghamn',
+      author_email='info@ttlab.se',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=requirements,
+      zip_safe=False)
