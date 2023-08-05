925ad3cdfe7b61aa28660d7edde6027304922aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:10 +0000

    Add package string-distance/0.1.0/string_distance-0.1.0.tar.gz

---
+++ packages/string-distance/string_distance-0.1.0.tar.gz/string_distance-0.1.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages, Extension
+
+ext_modules = [
+    Extension(
+        "string_distance.edit_distance",
+        ["string_distance/edit_distance.pyx"],
+    ),
+    Extension(
+        "string_distance.float_edit_distance",
+        ["string_distance/float_edit_distance.pyx"]
+    ),
+]
+
+version = "0.1.0"
+
+setup(
+    name="string_distance",
+    version=version,
+    description="Minimum Edit Distance",
+    long_description=open('README.md').read(),
+    author="Brian Lester",
+    author_email="blester125@gmail.com",
+    url="https://github.com/blester125/string_distance",
+    download_url=f"https://github.com/blester125/string_distance/archive/{version}.tar.gz",
+    license="MIT",
+    packages=find_packages(),
+    package_data={
+        'string_distance': [
+            'string_distance/edit_distance.c',
+            'string_distance/float_edit_distance.c'
+        ],
+    },
+    include_package_data=True,
+    keywords=["NLP", "fuzzy", "matching"],
+    setup_requires=[
+        'cython',
+    ],
+    install_requires=[
+        'numpy',
+    ],
+    ext_modules=ext_modules
+)
