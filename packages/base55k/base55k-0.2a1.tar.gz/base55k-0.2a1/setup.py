d264edc1b29fb0078748f02dd326ee6e26720f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:15 +0000

    Add package base55k/0.2a1/base55k-0.2a1.tar.gz

---
+++ packages/base55k/base55k-0.2a1.tar.gz/base55k-0.2a1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup \
+(
+    version="0.2a1",
+    name="base55k",
+    description="Convert decimal integers into a base 55296 system.",
+    url="http://erbium.org",
+    author="Equation Kid",
+    author_email="equationkid@gmail.com",
+    packages=[],
+    licence="MIT",
+    keywords="base55k Base55k base Base 55 55k binary bin string str hex hexadecimal encode compress number integer int compressiontools compact",
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    ],
+    zip_safe=False,
+    python_requires=">=3",
+)
