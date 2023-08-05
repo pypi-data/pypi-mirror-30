e9d7273a26f48e0ce6c3912134a39090af401a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:48 +0000

    Add package threadedprocess/0.0.2/threadedprocess-0.0.2.tar.gz

---
+++ packages/threadedprocess/threadedprocess-0.0.2.tar.gz/threadedprocess-0.0.2/setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import setup
+
+try:
+    import concurrent.futures
+except ImportError:
+    CONCURRENT_FUTURES_PRESENT = False
+else:
+    CONCURRENT_FUTURES_PRESENT = True
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="threadedprocess",
+    version="0.0.2",
+    author="Roberto Abdelkader Martinez Perez",
+    author_email="robertomartinezp@gmail.com",
+    description=(
+        "A `ThreadedProcessPoolExecutor` is formed by a modified "
+        "`ProcessPoolExecutor` that generates processes that use a "
+        "`ThreadPoolExecutor` instance to run the given tasks."),
+    license="BSD",
+    keywords="concurrent futures executor process thread",
+    url="https://github.com/nilp0inter/threadedprocess",
+    py_modules=['threadedprocess'],
+    long_description=read('README.rst'),
+    install_requires=[] if CONCURRENT_FUTURES_PRESENT else ["futures"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
