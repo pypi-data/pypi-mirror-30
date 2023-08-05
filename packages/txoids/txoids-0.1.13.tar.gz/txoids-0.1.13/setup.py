5bbf456ec6688f19132c50b19f386d4cf0e8d6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:38 +0000

    Add package txoids/0.1.13/txoids-0.1.13.tar.gz

---
+++ packages/txoids/txoids-0.1.13.tar.gz/txoids-0.1.13/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+if __name__ == '__main__':
+    setup(name='txoids',
+          version='0.1.13',
+          url="https://github.com/kalombos/txoids",
+          download_url="https://github.com/kalombos/txoids",
+          description='Tool for parsing switches using SNMP',
+          long_description="Tools for parsing switches using SNMP protocol. "
+                           "It uses pynetsnmp library which works pretty fast with SNMP. ",
+          author='Nikolay Gorshkov',
+          author_email='nogamemorebrain@gmail.com',
+          maintainer='kalombo',
+          maintainer_email='nogamemorebrain@gmail.com',
+          packages=find_packages(),
+          install_requires=[
+              'pynetsnmp-2',
+          ],
+          keywords=['snmp', 'twisted', 'pynetsnmp', 'netsnmp'],
+          )
