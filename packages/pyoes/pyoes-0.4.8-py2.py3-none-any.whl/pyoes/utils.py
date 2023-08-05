4ab00affcbee3b8265f22abd73e8a95a3b501cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:46 +0000

    Add package pyoes/0.4.8/pyoes-0.4.8-py2.py3-none-any.whl

---
+++ packages/pyoes/pyoes-0.4.8-py2.py3-none-any.whl/pyoes/utils.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Contains some extra filters for Jinja2.
+
+To use these, they need to be registered in your pyramid config.
+"""
+from dateutil import parser
+
+
+def set_attr_filter(target, key, value):
+    """
+    Jinja2 filter that sets an attribute of an object.
+    """
+    target[key] = value
+    return target
+
+
+def datetime_format_filter(value, format='%d-%m-%Y %H:%M'):
+    """
+    Jinja 2 filter to print a datetime object in a template.
+
+    :param value: Eiter a :class:`datetime.datetime` or a string that can be \
+        parsed by :class:`dateutil.parser`. In case of ambiguity between the \
+        day and the month, the parser will assume the day precedes the month \
+        (ie. European way)
+    :param format: The format to print the date in.
+    :return: The formatted date, eg. `07-09-2014 20:00`.
+    """
+    if value:
+        try:
+            return value.strftime(format)
+        except AttributeError:
+            if value[0:4].find('-') == -1 and value[0:4].find('/') == -1:
+                date = parser.parse(value, yearfirst=True, dayfirst=False)
+            else:
+                date = parser.parse(value, dayfirst=True)
+            return date.strftime(format)
+    else:
+        return ''
