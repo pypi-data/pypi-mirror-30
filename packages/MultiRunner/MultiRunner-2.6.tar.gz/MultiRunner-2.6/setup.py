7569a6f2215616775900b554437934bbb639e1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:22 +0000

    Add package MultiRunner/2.6/MultiRunner-2.6.tar.gz

---
+++ packages/MultiRunner/MultiRunner-2.6.tar.gz/MultiRunner-2.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "MultiRunner",
+    version = "2.6",
+    description= "Fix a little little bug",
+    url='https://www.github.com/statham-stone/',
+    author = "Statham",
+    author_email = "statham.stone@gmail.com",
+    packages=[""]
+)
