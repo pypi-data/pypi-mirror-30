569adf76899b4004759c24d88ea59ae6a26f3611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:10 +0000

    Add package gotenshita/2.2.4/gotenshita-2.2.4.tar.gz

---
+++ packages/gotenshita/gotenshita-2.2.4.tar.gz/gotenshita-2.2.4/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+__version__ = '2.2.4'
+
+# publish helper
+if sys.argv[-1] == 'publish':
+    for cmd in [
+            'python setup.py sdist upload',
+            'git tag {}'.format(__version__),
+            'git push origin master --tag']:
+        subprocess.check_call(cmd, shell=True)
+    sys.exit(0)
+
+setup(
+    name='gotenshita',
+    version=__version__,
+    packages=find_packages(),
+    description='Gotenshita notifier.',
+    long_description=open('README.rst').read(),
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    url='http://github.com/wkentaro/gotenshita',
+    install_requires=open('requirements.txt').readlines(),
+    license='MIT',
+    keywords='utility',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    entry_points={'console_scripts': ['gotenshita=gotenshita:main']},
+)
