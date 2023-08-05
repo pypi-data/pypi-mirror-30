5c9ba4ee5c08dc168558d39a4bde2325103df0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:57 +0000

    Add package eggit/0.0.1/eggit-0.0.1.tar.gz

---
+++ packages/eggit/eggit-0.0.1.tar.gz/eggit-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eggit',
+    version='0.0.1',
+    description='a python lib',
+    author='JoiT',
+    author_email='myjoit@outlook.com',
+    url='https://github.com/MyJoiT/eggit',
+    download_url='https://github.com/MyJoiT/eggit/archive/0.0.1.tar.gz',
+    packages=find_packages(exclude=[]),
+    keywords=('eggit, lib, tool'),
+    install_requires=[],
+    license='GPL3',
+    zip_safe=True
+)
