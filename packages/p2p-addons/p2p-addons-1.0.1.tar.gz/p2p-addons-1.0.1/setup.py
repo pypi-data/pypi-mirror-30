05ef86820f8bcb7d7ba67ddc1db63a083b3ad7b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package p2p-addons/1.0.1/p2p-addons-1.0.1.tar.gz

---
+++ packages/p2p-addons/p2p-addons-1.0.1.tar.gz/p2p-addons-1.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+setup(name='p2p-addons',
+      version='1.0.1',
+      description='peer2peer add ons',
+      py_modules=['addons'],
+      license='MIT',
+      platforms='any',
+      install_requires=['peer2peer']
+      )
