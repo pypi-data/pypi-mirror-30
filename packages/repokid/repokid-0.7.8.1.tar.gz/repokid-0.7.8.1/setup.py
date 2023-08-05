b7eb0b9eef04d7ac56d944ff1c0b6d3f044f6cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:58 +0000

    Add package repokid/0.7.8.1/repokid-0.7.8.1.tar.gz

---
+++ packages/repokid/repokid-0.7.8.1.tar.gz/repokid-0.7.8.1/setup.py
@@ -0,0 +1,39 @@
+#     Copyright 2017 Netflix, Inc.
+#
+#     Licensed under the Apache License, Version 2.0 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+import re
+import ast
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    REQUIRED = f.read().splitlines()
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+with open('repokid/__init__.py', 'rb') as f:
+    REPOKID_VERSION = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    name='repokid',
+    version=REPOKID_VERSION,
+    description=' AWS Least Privilege for Distributed, High-Velocity Deployment.',
+    long_description=__doc__,
+    packages=find_packages(),
+    install_requires=REQUIRED,
+    entry_points={
+        'console_scripts': [
+            'repokid = repokid.cli.repokid_cli:main',
+            'dispatcher = repokid.cli.dispatcher_cli:main'
+        ],
+    }
+)
