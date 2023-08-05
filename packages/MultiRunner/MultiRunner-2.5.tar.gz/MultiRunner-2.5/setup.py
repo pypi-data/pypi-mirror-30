cc0347e127c51385aa38c31a89da8bdcae056485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:21 +0000

    Add package MultiRunner/2.5/MultiRunner-2.5.tar.gz

---
+++ packages/MultiRunner/MultiRunner-2.5.tar.gz/MultiRunner-2.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "MultiRunner",
+    version = "2.5",
+    description= "Fix a little bug",
+    url='https://www.github.com/statham-stone/',
+    author = "Statham",
+    author_email = "statham.stone@gmail.com",
+    packages=[""]
+)
