f5a3caf60a06e306d884b736896eff5afea0bdd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:06 +0000

    Add package ccw-utilities/1.0/ccw_utilities-1.0.tar.gz

---
+++ packages/ccw-utilities/ccw_utilities-1.0.tar.gz/ccw_utilities-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ccw_utilities',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Greg Ward',
+      author_email='gward@python.net',
+      url='',
+     )
