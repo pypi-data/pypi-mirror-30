d6b63d01f8a292355c85f4b002025c49474ca64d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:16 +0000

    Add package vr.cli/2.10/vr.cli-2.10.tar.gz

---
+++ packages/vr.cli/vr.cli-2.10.tar.gz/vr.cli-2.10/setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+# Project skeleton maintained at https://github.com/jaraco/skeleton
+
+import io
+
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+    long_description = readme.read()
+
+name = 'vr.cli'
+description = 'Command-line client for Velociraptor'
+nspkg_technique = 'managed'
+"""
+Does this package use "native" namespace packages or
+pkg_resources "managed" namespace packages?
+"""
+
+params = dict(
+    name=name,
+    use_scm_version=True,
+    author="Jason R. Coombs",
+    author_email="jaraco@jaraco.com",
+    description=description or name,
+    long_description=long_description,
+    url="https://github.com/yougov/" + name,
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    namespace_packages=name.split('.')[:-1],
+    install_requires=[
+        'jaraco.functools>=1.0,<2dev',
+        'vr.common>=5.1',
+        'more_itertools',
+        'six',
+        'datadiff>=1.1.6',
+        'jaraco.ui>=1.2',
+        'jaraco.timing',
+        'jaraco.logging',
+    ],
+    extras_require={
+        'testing': [
+            # upstream
+            'pytest>=2.8',
+            'pytest-sugar>=0.9.1',
+            'collective.checkdocs',
+            'pytest-flake8',
+
+            # local
+        ],
+        'docs': [
+            # upstream
+            'sphinx',
+            'jaraco.packaging>=3.2',
+            'rst.linker>=1.9',
+
+            # local
+        ],
+    },
+    setup_requires=[
+        'setuptools_scm>=1.15.0',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    entry_points={
+        'console_scripts': [
+            'vr.cli = vr.cli:handle_command_line',
+        ],
+    },
+)
+if __name__ == '__main__':
+    setuptools.setup(**params)
