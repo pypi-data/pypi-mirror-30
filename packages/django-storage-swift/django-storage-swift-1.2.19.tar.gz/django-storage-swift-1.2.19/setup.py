4ff7f919ad70b92dff8a40311c5b7c3bb4b97b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:54 +0000

    Add package django-storage-swift/1.2.19/django-storage-swift-1.2.19.tar.gz

---
+++ packages/django-storage-swift/django-storage-swift-1.2.19.tar.gz/django-storage-swift-1.2.19/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+setup(
+    name='django-storage-swift',
+    version='1.2.19',
+    description='OpenStack Swift storage backend for Django',
+    long_description=open('README.rst').read(),
+    url='https://github.com/dennisv/django-storage-swift',
+    author='Dennis Vermeulen',
+    author_email='blacktorn@gmail.com',
+    license='MIT',
+    packages=['swift'],
+    install_requires=[
+        'python-swiftclient>=2.2.0',
+        'python-keystoneclient>=0.2.3',
+        'six',
+        'python-magic>=0.4.10',
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+)
