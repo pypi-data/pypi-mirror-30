186401831603b28322e70f391b9fa3d927573d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:38 +0000

    Add package web-crawler-plus/0.9.11/web-crawler-plus-0.9.11.tar.gz

---
+++ packages/web-crawler-plus/web-crawler-plus-0.9.11.tar.gz/web-crawler-plus-0.9.11/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='web-crawler-plus',
+      version='0.9.11',
+      description='A micro-framework to crawl the web pages with crawlers configs.'
+                  ' It can use MongoDB, Elasticsearch and Solr databases to cache and save the extracted data.',
+      author='Ravi Raja Merugu',
+      author_email='rrmerugu@gmail.com',
+      url='https://github.com/invanatech/web-crawler-plus',
+      packages=['webcrawler', 'tests',
+                'webcrawler.spiders', 'webcrawler.pipelines', 'webcrawler.httpcache',
+                'webcrawler.spiders.search_engines', 'webcrawler.utils'],
+      install_requires=['Scrapy==1.5.0', 'pysolr==3.7.0', 'pymongo==3.6.1', 'elasticsearch-dsl==6.1.0',
+                        "feedparser==5.2.1"]
+      )
