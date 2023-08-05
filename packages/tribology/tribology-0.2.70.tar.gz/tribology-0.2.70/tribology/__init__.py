b11e88a90b2b2a486ff39189228693dc4b6ff8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:14 +0000

    Add package tribology/0.2.70/tribology-0.2.70.tar.gz

---
+++ packages/tribology/tribology-0.2.70.tar.gz/tribology-0.2.70/tribology/__init__.py
@@ -0,0 +1,29 @@
+"""
+
+Python init file for tribology package
+
+"""
+
+__all__ = ['tribology',
+           'boundary_element',
+           'constants',
+           'data_import',
+           'dowson_hamrock',
+           'hertz',
+           'lubrication',
+           'roller_bearings',
+           'p3can']
+
+from tribology.tribology import *
+from tribology.boundary_element import *
+from tribology.constants import *
+from tribology.data_import import *
+from tribology.dowson_hamrock import *
+from tribology.hertz import *
+from tribology.lubrication import *
+from tribology.roller_bearings import *
+
+import sys
+sys.path.insert(0, "tribology")
+sys.path.insert(0, "tribology/p3can")
+from tribology.p3can import *
