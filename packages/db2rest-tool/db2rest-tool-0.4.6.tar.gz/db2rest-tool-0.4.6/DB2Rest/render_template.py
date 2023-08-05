292eb53972616ea4bbe59bb95fec54bd980bcc1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:34 +0000

    Add package db2rest-tool/0.4.6/db2rest-tool-0.4.6.tar.gz

---
+++ packages/db2rest-tool/db2rest-tool-0.4.6.tar.gz/db2rest-tool-0.4.6/DB2Rest/render_template.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import os
+from jinja2 import Environment, FileSystemLoader
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_ENVIRONMENT = Environment(
+    autoescape=False,
+    loader=FileSystemLoader(os.path.join(BASE_DIR, 'templates')),
+    trim_blocks=False)
+
+def render_template(template, data):
+    return TEMPLATE_ENVIRONMENT.get_template(template).render(dict(data=data))
+
+def render_to_template(file_name_arg, template_name, data_to_render):
+    file_name = file_name_arg
+    with open(file_name, 'w') as file:
+        template_render = render_template(template_name, data_to_render)
+        file.write(template_render)
