acbe204aa75b1f18cf3aa0930dde92a129171434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:09 +0000

    Add package orchestra/0.2.46/orchestra-0.2.46.tar.gz

---
+++ packages/orchestra/orchestra-0.2.46.tar.gz/orchestra-0.2.46/orchestra/todos/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from orchestra.models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Todo
+        fields = (
+            'id',
+            'created_at',
+            'task',
+            'description',
+            'completed',
+            'start_by_datetime',
+            'due_datetime')
+        read_only_fields = ('id',)
---
+++ packages/orchestra/orchestra-0.2.46.tar.gz/orchestra-0.2.46/orchestra/utils/decorators.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+
+
+def run_if(*args):
+    """
+    Decorator prevents function run if required feature flag is False and
+    raises exception if it doesn't exist.
+    """
+    feature_flags = args
+
+    def require_settings_decorator(func):
+        def wrapper(*args, **kwargs):
+            for flag in feature_flags:
+                flag_value = getattr(settings, flag, None)
+                if not isinstance(flag_value, bool):
+                    raise Exception('Setting {} must be either True or '
+                                    'False.'.format(flag))
+                elif not flag_value:
+                    # Don't run the function if the flag is false.
+                    return
+            return func(*args, **kwargs)
+        return wrapper
+    return require_settings_decorator
