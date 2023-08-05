16ff9a17004d8d8901feaaebba9728aed9f3d70c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:29 +0000

    Add package itsjustcode/0.1/itsjustcode-0.1.tar.gz

---
+++ packages/itsjustcode/itsjustcode-0.1.tar.gz/itsjustcode-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='itsjustcode',
+      version='0.1',
+      description='Database First Code Generator that uses the SQL Alchemy and Jinja2',
+      url='https://github.com/ellerbus/itsjustcode',
+      license='MIT',
+      packages=['itsjustcode'],
+      zip_safe=False)
