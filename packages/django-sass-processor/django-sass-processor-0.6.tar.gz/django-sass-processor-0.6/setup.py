140337025ffa6662cb1fd4b9908589e75b44d324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:22 +0000

    Add package django-sass-processor/0.6/django-sass-processor-0.6.tar.gz

---
+++ packages/django-sass-processor/django-sass-processor-0.6.tar.gz/django-sass-processor-0.6/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from setuptools import setup, find_packages
+from sass_processor import __version__
+try:
+    from pypandoc import convert
+except ImportError:
+    import io
+
+    def convert(filename, fmt):
+        with io.open(filename, encoding='utf-8') as fd:
+            return fd.read()
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Framework :: Django :: 1.9',
+    'Framework :: Django :: 1.10',
+    'Framework :: Django :: 1.11',
+]
+
+setup(
+    name='django-sass-processor',
+    version=__version__,
+    description='SASS processor to compile SCSS files into *.css, while rendering, or offline.',
+    author='Jacob Rief',
+    author_email='jacob.rief@gmail.com',
+    url='https://github.com/jrief/django-sass-processor',
+    packages=find_packages(),
+    install_requires=[],
+    license='LICENSE-MIT',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    long_description=convert('README.md', 'rst'),
+    include_package_data=True,
+    zip_safe=False,
+)
