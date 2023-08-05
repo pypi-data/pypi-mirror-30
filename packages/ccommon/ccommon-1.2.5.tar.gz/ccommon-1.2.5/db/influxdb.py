20eb247a3ca2db5e434b9364bb640104b4f443e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:56 +0000

    Add package ccommon/1.2.5/ccommon-1.2.5.tar.gz

---
+++ packages/ccommon/ccommon-1.2.5.tar.gz/ccommon-1.2.5/db/influxdb.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/2/21 13:52
+# @Author  : chenjw
+# @Site    : 
+# @File    : influxdb.py
+# @Software: PyCharm Community Edition
+# @Desc    :  do what
+
+from influxdb import InfluxDBClient
+import logging
+import time
+
+
+class InfluxDB:
+    def __init__(self, host, port, username, pwd, database):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.pwd = pwd
+        self.database = database
+        self.client = None
+
+    def getClient(self):
+        if self.client is None:
+            self.connect()
+        return self.client
+
+    def connect(self):
+        self.client = InfluxDBClient(self.host, self.port, self.username, self.pwd, self.database)
+        self.client.create_database(self.database)
+
+    def close(self):
+        try:
+            self.client.close()
+        except:
+            pass
+
+    def query_one(self, value='value', measurement='measurement', where=[]):
+        '''
+        :param value: key of fields
+        :param measurement: measurement
+        :param where: conditions
+        :return: value of None
+        '''
+        _query = 'select %s from %s' % (value, measurement)
+        if len(where) > 0:
+            def combine_where(ss):
+                if len(ss) == 1:
+                    return ' where ' + ss
+                else:
+                    tmp = ''
+                    for index in range(len(ss)):
+                        if index == len(ss) - 1:
+                            tmp += ss[index]
+                        else:
+                            tmp += ss[index] + ' and '
+                    return ' where ' + tmp
+
+            _query += combine_where(where)
+        _query += ' ORDER BY time DESC LIMIT 1'
+        result = self.getClient().query(_query)
+        try:
+            for tmp in result[result.keys()[0]]:
+                return tmp[value]
+        except:
+            return None
+        return None
+
+    def recordinfo(self, measurement, date=None, _time=None, fields=None, tags={}):
+        '''
+        :param client: influxdb client
+        :param measurement: key to measure a series of data
+        :param date: date like '2018-01-01'
+        :param fields:  common like {'value':12.34}
+        :param tags: like {'a':'a','b':'b'} use tags to group by data
+        :return: nothing ,sometimes print msg if raise Exception
+        '''
+        try:
+            record_map = {
+                "measurement": measurement,
+                "tags": tags,
+                "fields": fields
+            }
+            if date is not None:
+                record_map['time'] = "%sT01:00:00Z" % date,
+            elif _time is not None:
+                record_map['time'] = "%s" % _time,
+            self.getClient().write_points([record_map])
+        except Exception as e:
+            logging.error('call recordinfo raise exception %s' % e)
+
+
+if __name__ == '__main__':
+    pass
