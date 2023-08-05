195834ab47ba40e62a69ef1f337f231778d648a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:51 +0000

    Add package pystm/0.1/pystm-0.1.tar.gz

---
+++ packages/pystm/pystm-0.1.tar.gz/pystm-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pystm',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/MSusik/stm',
+    license='BSD',
+    author='msusik',
+    author_email='mateuszsusik@gmail.com',
+    description='Structural Topic Model'
+)
---
+++ packages/pystm/pystm-0.1.tar.gz/pystm-0.1/stm.py
@@ -0,0 +1,13 @@
+
+class StructuralTopicModel(BaseEstimator, TransformerMixin):
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+
+        return self
+
+    def transform(self, X):
+
+        return X
