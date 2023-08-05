2ba75930baa50fdc42d01706de936907280009dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:33 +0000

    Add package apertium-apy/0.11.0rc1/apertium-apy-0.11.0rc1.tar.gz

---
+++ packages/apertium-apy/apertium-apy-0.11.0rc1.tar.gz/apertium-apy-0.11.0rc1/apertium_apy/keys.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+
+if False:
+    from typing import Dict  # noqa: F401
+
+
+def get_key(key):
+    return keys[key]
+
+
+keys_raw = {
+    # add keys here
+}  # type: Dict
+keys = defaultdict(lambda: 'null', keys_raw)
