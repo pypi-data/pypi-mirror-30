b39f3729530713d41e249913d96f25476229f67f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:14 +0000

    Add package oriole-test/0.6.0/oriole-test-0.6.0.tar.gz

---
+++ packages/oriole-test/oriole-test-0.6.0.tar.gz/oriole-test-0.6.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('HISTORY.rst') as f:
+    history = f.read()
+
+requirements = [
+    'Click>=6.0',
+    'oriole>=7.1.0',
+]
+
+setup(
+    author="Eric.Zhou",
+    author_email='xiaoxiang.cn@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="Support tdd for oriole-service.",
+    entry_points={
+        'console_scripts': [
+            'oriole_test=oriole_test.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='oriole-test',
+    name='oriole-test',
+    packages=find_packages(include=['oriole_test']),
+    test_suite='tests',
+    url='https://github.com/zhouxiaoxiang/oriole-test',
+    version='0.6.0',
+    zip_safe=False,
+)
