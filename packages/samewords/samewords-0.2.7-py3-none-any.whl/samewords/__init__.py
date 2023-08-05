bdf024562345ef10a8e572bc8e950ae10c97bdba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:16 +0000

    Add package samewords/0.2.7/samewords-0.2.7-py3-none-any.whl

---
+++ packages/samewords/samewords-0.2.7-py3-none-any.whl/samewords/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Package for annotating references to potentially ambiguous words in critical
+editions made with LaTeX and reledmac.
+"""
+__all__ = ['core', 'document', 'matcher', 'cli', 'settings', 'tokenize',
+           'brackets']
+__version__ = '0.2.7'
+
+import samewords.core
