4d5d7977cd2bf0c2ed63b772eb779c7f3ed60fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:15 +0000

    Add package codacy-coverage/1.3.11/codacy-coverage-1.3.11.tar.gz

---
+++ packages/codacy-coverage/codacy-coverage-1.3.11.tar.gz/codacy-coverage-1.3.11/setup.py
@@ -0,0 +1,69 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Default version
+__version__ = '1.3.0'
+
+# Get the correct version from file
+try:
+    import version
+    __version__ = version.__version__
+except ImportError:
+    pass
+
+setup(
+    name='codacy-coverage',
+
+    version=__version__,
+
+    description='Codacy coverage reporter for Python',
+    long_description=long_description,
+
+    url='https://github.com/codacy/python-codacy-coverage',
+
+    author='Codacy',
+    author_email='team@codacy.com',
+
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+
+    keywords='development coverage',
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'}, include_package_data=True,
+
+    install_requires=['requests>=2.9.1'],
+
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['nosetests', 'coverage'],
+    },
+
+    entry_points={
+        'console_scripts': [
+            'python-codacy-coverage=codacy:main',
+        ],
+    },
+    test_suite='tests'
+)
