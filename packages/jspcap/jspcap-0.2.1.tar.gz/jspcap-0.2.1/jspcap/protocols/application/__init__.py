3c730cda5a62ec9cc0c72216789e52af51ad9b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:58 +0000

    Add package jspcap/0.2.1/jspcap-0.2.1.tar.gz

---
+++ packages/jspcap/jspcap-0.2.1.tar.gz/jspcap-0.2.1/jspcap/protocols/application/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""application layer protocols
+
+``jspcap.protocols.application`` is collection of all
+protocols in application layer, with detailed
+implementation and methods.
+
+"""
+# Base Class for Internet Layer
+from jspcap.protocols.application.application import Application
+
+# Utility Classes for Protocols
+from jspcap.protocols.application.httpv1 import HTTPv1
+
+# Deprecated / Base Classes
+from jspcap.protocols.application.http import HTTP
+
+
+__all__ = ['HTTPv1']
