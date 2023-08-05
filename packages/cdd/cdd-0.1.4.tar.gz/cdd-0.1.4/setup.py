f69189ce7388e44a0a3a167ec9e8d489b0744fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:51 +0000

    Add package cdd/0.1.4/cdd-0.1.4.tar.gz

---
+++ packages/cdd/cdd-0.1.4.tar.gz/cdd-0.1.4/setup.py
@@ -0,0 +1,18 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='cdd',
+    version='0.1.4',
+    packages=['cdd',],
+    license='GNU Affero General Public License v3',
+    description='improved file system navigation with cd',
+    long_description=open('README.txt').read(),
+    url='https://github.com/daltonserey/cdd',
+    author='Dalton Serey',
+    author_email='daltonserey@gmail.com',
+    scripts=['bin/cdd', 'bin/cdd.sh'],
+    entry_points={
+        'console_scripts': ['foo = cdd.teste:main']
+    }
+)
