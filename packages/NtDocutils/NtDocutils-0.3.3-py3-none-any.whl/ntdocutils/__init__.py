f7f8768fa56855b8e36781b9de89b98435ace2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:42 +0000

    Add package NtDocutils/0.3.3/NtDocutils-0.3.3-py3-none-any.whl

---
+++ packages/NtDocutils/NtDocutils-0.3.3-py3-none-any.whl/ntdocutils/__init__.py
@@ -0,0 +1,13 @@
+# NtDocutils https://blog.nt.web.ve/en/articles/ntdocutils/
+# Copyright (c) 2017 Miguel Angel Rivera Notararigo
+
+"""NtDocutils."""
+
+__version__ = "0.3.3"
+__docformat__ = "reStructuredText"
+
+COPYRIGHT = """NtDocutils v{} https://blog.nt.web.ve/en/articles/ntdocutils/
+Copyright (c) 2017 Miguel Angel Rivera Notararigo
+Licensed under The MIT License""".format(__version__)
+
+DESCRIPTION = """NtDocutils is a Material Design Lite theme for Docutils."""
