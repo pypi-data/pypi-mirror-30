124395f9240e90f118842323cbdf1da4d3ecfe64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:48 +0000

    Add package stationdiy/1.96/stationdiy-1.96.tar.gz

---
+++ packages/stationdiy/stationdiy-1.96.tar.gz/stationdiy-1.96/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+  name = 'stationdiy',
+  packages = ['stationdiy'], # this must be the same as the name above
+  version = '1.96',
+  description = 'Custom Controller for StationDiY IoT platform',
+  author = 'Baurin Leza',
+  author_email = 'baurin.lg@gmail.com',
+  url = 'https://github.com/trpill/stationdiy', # use the URL to the github repo
+  download_url = 'https://github.com/trpill/stationdiy/archive/master.zip', # I'll explain this in a second
+  keywords = ['stationdiy', 'iot', 'internet'], #  keywords
+  classifiers = [],
+  install_requires=[
+        'paho-mqtt',
+        'requests',
+    ]
+)
