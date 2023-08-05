000db2ca52ac6e9bb0b799ca58d7416a071ede4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:38 +0000

    Add package onegov.search/1.3.2/onegov.search-1.3.2-py3-none-any.whl

---
+++ packages/onegov.search/onegov.search-1.3.2-py3-none-any.whl/onegov/search/cli.py
@@ -0,0 +1,36 @@
+""" Provides commands related to the onegov.search. """
+
+import click
+
+from onegov.core.cli import command_group, pass_group_context
+from sedate import utcnow
+
+
+cli = command_group()
+
+
+@cli.command(context_settings={'default_selector': '*'})
+@pass_group_context
+def reindex(group_context):
+    """ Reindexes all objects in the elasticsearch database. """
+
+    def run_reindex(request, app):
+        if not hasattr(request.app, 'es_client'):
+            return
+
+        title = f"Reindexing {request.app.application_id}"
+        print(click.style(title, underline=True))
+
+        start = utcnow()
+
+        session = request.session
+        request.app.es_perform_reindex()
+
+        print(f"took {utcnow() - start}")
+
+        @request.after
+        def cleanup(response):
+            session.invalidate()
+            session.bind.dispose()
+
+    return run_reindex
