b4f237f9ac21c7952adcbeadfd8d90ab102a2afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:25 +0000

    Add package kastore/0.0.1/kastore-0.0.1.tar.gz

---
+++ packages/kastore/kastore-0.0.1.tar.gz/kastore-0.0.1/README.rst
@@ -0,0 +1,7 @@
+kastore
+=======
+
+This software is not ready for use; this is a holding page for the
+soon-to-be-released package.
+
+
---
+++ packages/kastore/kastore-0.0.1.tar.gz/kastore-0.0.1/kastore/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+from __future__ import division
+
+from .store import *  # NOQA
+
+
+def main():
+    print("Main entry point for kastore command")
