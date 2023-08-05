acae443851f40e28ea6b0c146314c8b8fc3fed81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:30 +0000

    Add package contextvars/2.0/contextvars-2.0.tar.gz

---
+++ packages/contextvars/contextvars-2.0.tar.gz/contextvars-2.0/setup.py
@@ -0,0 +1,31 @@
+import os.path
+import setuptools
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    readme = f.read()
+
+
+setuptools.setup(
+    name='contextvars',
+    version="2.0",
+    description='PEP 567 Backport',
+    long_description=readme,
+    author='MagicStack Inc',
+    author_email='hello@magic.io',
+    packages=['contextvars'],
+    provides=['contextvars'],
+    license='Apache License, Version 2.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+    ],
+    include_package_data=True,
+    test_suite='tests.suite',
+)
