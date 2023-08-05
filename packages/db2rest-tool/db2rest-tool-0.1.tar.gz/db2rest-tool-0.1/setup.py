7b2ebfdb4b0fd2955e30c12e8ff404f85aff4073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:10 +0000

    Add package db2rest-tool/0.1/db2rest-tool-0.1.tar.gz

---
+++ packages/db2rest-tool/db2rest-tool-0.1.tar.gz/db2rest-tool-0.1/setup.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='db2rest-tool',
+      version='0.1',
+      author='oliveirabrunoa',
+      author_email='oliveirabrunoa@gmail.com',
+      url='https://github.com/oliveirabrunoa/db2rest',
+
+      py_modules=['DB2Rest'],
+
+
+)
