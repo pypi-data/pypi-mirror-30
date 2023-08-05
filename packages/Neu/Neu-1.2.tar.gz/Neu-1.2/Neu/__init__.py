3be6d16d067df9317724720cb6ed124ab4dca0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:07 +0000

    Add package Neu/1.2/Neu-1.2.tar.gz

---
+++ packages/Neu/Neu-1.2.tar.gz/Neu-1.2/Neu/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from .SegModel import SegModel
+from .PosModel import PosModel
+from .NerModel import NerModel
+from .ParserModel import ParserModel
+
+
+# cut = SegModel().cut
+# pos = PosModel().pos
+ner = NerModel().ner
+# parser = ParserModel().parser
+
+
