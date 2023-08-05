eec6c06ac0cbb2840b3483879f29287281fd980a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:21 +0000

    Add package MultiRunner/2.2/MultiRunner-2.2.tar.gz

---
+++ packages/MultiRunner/MultiRunner-2.2.tar.gz/MultiRunner-2.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "MultiRunner",
+    version = "2.2",
+    description= "Add feature: choose the best gpu available",
+    url='https://www.github.com/statham-stone/',
+    author = "Statham",
+    author_email = "statham.stone@gmail.com",
+    packages=[""]
+)
