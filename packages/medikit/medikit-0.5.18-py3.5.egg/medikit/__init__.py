1cbe78d08214ddc6f84d5a5fee0a929d9f6697a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:37 +0000

    Add package medikit/0.5.18/medikit-0.5.18-py3.5.egg

---
+++ packages/medikit/medikit-0.5.18-py3.5.egg/medikit/__init__.py
@@ -0,0 +1,18 @@
+from medikit._version import __version__
+
+
+def listen(*args, **kwargs):
+    raise NotImplementedError('This is a stub.')
+
+
+def require(*args):
+    raise NotImplementedError('This is a stub.')
+
+
+def pipeline(*args):
+    raise NotImplementedError('This is a stub.')
+
+
+__all__ = [
+    '__version__',
+]
