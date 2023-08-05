fde1b61cdd74128a28ca7d6f086d1c5c2d46e825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:08 +0000

    Add package kallikrein/0.22.0a3/kallikrein-0.22.0a3.tar.gz

---
+++ packages/kallikrein/kallikrein-0.22.0a3.tar.gz/kallikrein-0.22.0a3/kallikrein/matchers/comparison.py
@@ -0,0 +1,69 @@
+import abc
+import operator
+from typing import Tuple, Callable
+from numbers import Number
+
+from kallikrein.match_result import MatchResult, SimpleMatchResult, BadNestedMatch
+from kallikrein.matcher import Matcher, BoundMatcher
+from amino import Boolean, L, _, List
+
+
+class Comparison(Matcher[Number]):
+
+    @abc.abstractproperty
+    def operator(self) -> Callable[[Number, Number], Boolean]:
+        ...
+
+    @abc.abstractproperty
+    def operator_reprs(self) -> Tuple[str, str]:
+        ...
+
+    def match(self, exp: Number, target: Number) -> MatchResult[Number]:
+        result = Boolean(self.operator(exp, target))
+        op_s, op_f = self.operator_reprs
+        op = op_s if result else op_f
+        message = '{} {} {}'.format(exp, op, target)
+        return SimpleMatchResult(result, List(message))
+
+    def match_nested(self, exp: Number, target: BoundMatcher) -> MatchResult[Number]:
+        return BadNestedMatch(self)
+
+
+class SimpleComparison(Comparison):
+
+    def __init__(self, op: Callable[[Number, Number], bool], op_s: str, op_f: str) -> None:
+        self.op = op
+        self.op_s = op_s
+        self.op_f = op_f
+
+    @property
+    def operator(self) -> Callable[[Number, Number], Boolean]:
+        return L(self.op)(_, _) >> Boolean
+
+    @property
+    def operator_reprs(self) -> Tuple[str, str]:
+        return self.op_s, self.op_f
+
+
+def comparison(op: Callable[[Number, Number], bool], op_s: str, op_f: str) -> Matcher[Number]:
+    return SimpleComparison(op, op_s, op_f)
+
+equal = comparison(operator.eq, '==', '/=')
+eq = equal
+
+not_equal = comparison(operator.ne, '/=', '==')
+ne = not_equal
+
+greater_equal = comparison(operator.ge, '>=', '<')
+ge = greater_equal
+
+greater = comparison(operator.gt, '>', '<=')
+gt = greater
+
+less_equal = comparison(operator.le, '<=', '>')
+le = less_equal
+
+less = comparison(operator.lt, '<', '>=')
+lt = less
+
+__all__ = ('equal', 'greater_equal', 'eq', 'ge', 'greater', 'gt', 'less_equal', 'le', 'less', 'lt')
---
+++ packages/kallikrein/kallikrein-0.22.0a3.tar.gz/kallikrein-0.22.0a3/kallikrein/util/string.py
@@ -0,0 +1,47 @@
+from typing import Any
+
+from amino.string.hues import huestr
+
+from amino import __
+
+
+def green(msg: Any) -> str:
+    return huestr(str(msg)).green.colorized
+
+
+def red(msg: Any) -> str:
+    return huestr(str(msg)).red.colorized
+
+
+def yellow(msg: Any) -> str:
+    return huestr(str(msg)).yellow.colorized
+
+
+def blue(msg: Any) -> str:
+    return huestr(str(msg)).blue.colorized
+
+
+def magenta(msg: Any) -> str:
+    return huestr(str(msg)).magenta.colorized
+
+indent = __.map(' {}'.format)
+
+green_check = green('✓')
+
+red_cross = red('✗')
+
+yellow_clock = yellow('⌚')
+
+green_plus = green('+')
+
+red_minus = red('-')
+
+spec_sign = '⛤'
+
+
+def spec_headline(mod: str) -> str:
+    return magenta(f'{spec_sign} {mod}')
+
+
+__all__ = ('indent', 'green_check', 'red_cross', 'green', 'red', 'yellow_clock', 'green_plus', 'red_minus', 'blue',
+           'magenta', 'spec_sign', 'spec_headline')
