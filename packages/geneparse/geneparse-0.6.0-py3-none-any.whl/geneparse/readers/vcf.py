71f896201af23d16a75f48b3ece1a1d91a69f68c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:09 +0000

    Add package geneparse/0.6.0/geneparse-0.6.0-py3-none-any.whl

---
+++ packages/geneparse/geneparse-0.6.0-py3-none-any.whl/geneparse/readers/vcf.py
@@ -0,0 +1,113 @@
+"""Reader for VCF files."""
+
+# This file is part of geneparse.
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2017 Pharmacogenomics Centre
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+from ..core import Variant, ImputedVariant, Genotypes, GenotypesReader
+
+from cyvcf2 import VCF
+import numpy as np
+
+
+class VCFReader(GenotypesReader):
+    def __init__(self, filename, quality_field=None):
+        self.get_vcf = lambda: VCF(filename)
+        self.quality_field = quality_field
+
+    def iter_genotypes(self):
+        for v in self.get_vcf():
+            alleles = set(v.REF) | set(v.ALT)
+
+            if self.quality_field:
+                variant = ImputedVariant(v.ID, v.CHROM, v.start, alleles,
+                                         getattr(v, self.quality_field))
+            else:
+                variant = Variant(v.ID, v.CHROM, v.start, alleles)
+
+            for coded_allele, g in self._make_genotypes(v.ALT, v.genotypes):
+                yield Genotypes(variant, g, v.REF, coded_allele)
+
+    @staticmethod
+    def _make_genotypes(alleles, genotypes):
+        out = []
+        for allele_symbol, allele in enumerate(alleles):
+            variant_geno = np.empty(len(genotypes))
+
+            for i, g in enumerate(genotypes):
+                variant_geno[i] = 0
+                a1, a2, phase = g
+
+                if a1 == allele_symbol + 1:
+                    variant_geno[i] += 1
+
+                if a2 == allele_symbol + 1:
+                    variant_geno[i] += 1
+
+            out.append((allele, variant_geno))
+
+        return out
+
+    def iter_variants(self):
+        for v in self.get_vcf():
+            yield Variant(v.ID, v.CHROM, v.start, {v.REF} | set(v.ALT))
+
+    def get_variant_genotypes(self, variant):
+        region = self.get_vcf()(
+            "{}:{}-{}".format(variant.chrom, variant.pos, variant.pos)
+        )
+        genotypes = []
+
+        for v in region:
+            for coded_allele, g in self._make_genotypes(v.ALT, v.genotypes):
+                alleles = {v.REF.upper(), coded_allele.upper()}
+                match = (
+                    alleles.issubset(set(variant.alleles)) or
+                    variant.alleles is None
+                )
+
+                if match:
+                    genotypes.append(Genotypes(g, v.REF, coded_allele))
+
+        return genotypes
+
+    def get_variants_in_region(self, chrom, start, end):
+        region = self.get_vcf()(
+            "{}:{}-{}".format(chrom, start, end)
+        )
+        for v in region:
+            for coded_allele, g in self._make_genotypes(v.ALT, v.genotypes):
+                variant = Variant(
+                    v.ID, v.CHROM, v.start, [v.ref, coded_allele]
+                )
+                yield Genotypes(variant, g, v.REF, coded_allele)
+
+    def get_samples(self):
+        return self.get_vcf().samples
+
+    def get_number_samples(self):
+        return len(self.get_samples())
+
+    def get_number_variants(self):
+        raise NotImplementedError("Don't know how to do this using cyvcf2.")
---
+++ packages/geneparse/geneparse-0.6.0-py3-none-any.whl/geneparse/utils.py
@@ -0,0 +1,148 @@
+"""Utilities"""
+
+# This file is part of geneparse.
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2017 Pharmacogenomics Centre
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+import urllib
+import json
+import logging
+
+import numpy as np
+import pandas as pd
+
+from .core import Variant
+
+
+logger = logging.getLogger(__name__)
+
+
+def flip_alleles(genotypes):
+    """Flip the alleles of an Genotypes instance."""
+    genotypes.reference, genotypes.coded = (genotypes.coded,
+                                            genotypes.reference)
+    genotypes.genotypes = 2 - genotypes.genotypes
+    return genotypes
+
+
+def code_minor(genotypes):
+    """Encode the genotypes with respect to the minor allele.
+
+    This confirms that "reference" is the major allele and that "coded" is
+    the minor allele.
+
+    In other words, this function can be used to make sure that the genotype
+    value is the number of minor alleles for an individual.
+
+    """
+    _, minor_coded = maf(genotypes)
+    if not minor_coded:
+        return flip_alleles(genotypes)
+
+    return genotypes
+
+
+def maf(genotypes):
+    """Computes the MAF and returns a boolean indicating if the minor allele
+    is currently the coded allele.
+
+    """
+    g = genotypes.genotypes
+    maf = np.nansum(g) / (2 * np.sum(~np.isnan(g)))
+    if maf > 0.5:
+        maf = 1 - maf
+        return maf, False
+
+    return maf, True
+
+
+def rsids_to_variants(li):
+    url = "http://grch37.rest.ensembl.org/variation/homo_sapiens"
+
+    req = urllib.request.Request(
+        url=url,
+        data=json.dumps({"ids": li}).encode("utf-8"),
+        headers={
+            "Content-type": "application/json",
+            "Accept": "application/json",
+        },
+        method="POST"
+    )
+
+    with urllib.request.urlopen(req) as f:
+        data = json.loads(f.read().decode("utf-8"))
+
+    out = {}
+    for name, info in data.items():
+        # Check the mappings.
+        found = False
+        for mapping in info["mappings"]:
+            chrom = mapping.get("seq_region_name")
+            pos = mapping.get("start")
+            alleles = mapping.get("allele_string").split("/")
+
+            assembly = mapping.get("assembly_name")
+
+            valid = (assembly == "GRCh37" and
+                     chrom is not None and
+                     pos is not None and
+                     len(alleles) >= 2)
+
+            if found and valid:
+                logger.warning("Multiple mappings for '{}'.".format(name))
+            elif valid:
+                found = True
+                out[name] = Variant(name, chrom, pos, alleles)
+
+        if not found:
+            logger.warning(
+                "Could not find mappings for '{}'.".format(name)
+            )
+
+    return out
+
+
+def genotype_to_df(g, samples, as_string=False):
+    """Convert a genotype object to a pandas dataframe.
+
+    By default, the encoded values are stored, but the as_string argument can
+    be used to represent it as characters (alleles) instead.
+
+    """
+    name = g.variant.name if g.variant.name else "genotypes"
+    df = pd.DataFrame(g.genotypes, index=samples, columns=[name])
+
+    if as_string:
+        df["alleles"] = None
+
+        hard_calls = df[name].round()
+        df.loc[hard_calls == 0, "alleles"] = "{0}/{0}".format(g.reference)
+        df.loc[hard_calls == 1, "alleles"] = "{0}/{1}".format(g.reference,
+                                                              g.coded)
+        df.loc[hard_calls == 2, "alleles"] = "{0}/{0}".format(g.coded)
+
+        df = df[["alleles"]]
+        df.columns = [name]
+
+    return df
