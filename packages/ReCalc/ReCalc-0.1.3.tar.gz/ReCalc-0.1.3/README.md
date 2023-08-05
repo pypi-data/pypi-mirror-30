30a243cfbbdbd1bbada1e2fa0e8f63cf9002b732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:18 +0000

    Add package ReCalc/0.1.3/ReCalc-0.1.3.tar.gz

---
+++ packages/ReCalc/ReCalc-0.1.3.tar.gz/ReCalc-0.1.3/README.md
@@ -0,0 +1,12 @@
+# ReCalc
+This is a graphing calculator written in python.
+
+prerequisites:
+
+To run this program you will need python 3.6, Sympy and tkinter.
+I have tested it on Windows 10 and Fedora 26-27.
+
+Installation:
+
+Once you have installed the above get the files on github.
+
---
+++ packages/ReCalc/ReCalc-0.1.3.tar.gz/ReCalc-0.1.3/setup.py
@@ -0,0 +1,47 @@
+#   setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "ReCalc",
+    version = "0.1.3",
+    packages = find_packages(),
+	scripts = [
+		"ReCalc.py", "ReCalc_testing.py",
+		"ReCalc_tkinter_testing.py"
+	],
+	
+	classifiers = [
+		"Programming Language :: Python :: 3.6",
+		"Environment :: X11 Applications :: Gnome",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+		"Natural Language :: English",
+		"Operating System :: Microsoft :: Windows :: Windows 10",
+		"Operating System :: POSIX :: Linux",
+		"Programming Language :: Python :: 3 :: Only",
+		"Programming Language :: Python :: Implementation :: CPython",
+		"Topic :: Scientific/Engineering :: Mathematics"
+	],
+	install_requires = [
+		"sympy>=1.1.1",
+		"pillow>=5.0.0"
+	],
+	
+	package_data = {
+	"": ["*.ico", "*.txt", "*.py"]
+	},
+	zip_safe = False,
+	
+	url = "https://github.com/RedKnite5/ReCalc.git",
+	
+	author = "Max Friedman",
+	author_email = "mr.awesome10000@gmail.com",
+	# desciption = "This is a basic graphing calculator.",
+	# licencse = "GPLv3",
+	keywords = ["Calculator"]
+)
+
+
+
+
+
