a0edbd91e9fac2ec7b7c3659e51010e9cbd07aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:26 +0000

    Add package aztk/0.7.0b11/aztk-0.7.0b11.tar.gz

---
+++ packages/aztk/aztk-0.7.0b11.tar.gz/aztk-0.7.0b11/aztk_cli/spark/endpoints/cluster/cluster_get.py
@@ -0,0 +1,29 @@
+import argparse
+import typing
+import aztk
+from aztk_cli import log
+from aztk_cli import utils, config
+
+
+def setup_parser(parser: argparse.ArgumentParser):
+    parser.add_argument('--id',
+                        dest='cluster_id',
+                        required=True,
+                        help='The unique id of your spark cluster')
+    parser.add_argument('--show-config',
+                        dest='show_config',
+                        action='store_true',
+                        help='Show the cluster configuration')
+
+
+def execute(args: typing.NamedTuple):
+    spark_client = aztk.spark.Client(config.load_aztk_secrets())
+    cluster_id = args.cluster_id
+    cluster = spark_client.get_cluster(cluster_id)
+    utils.print_cluster(spark_client, cluster)
+
+    configuration = spark_client.get_cluster_config(cluster_id)
+    if configuration and args.show_config:
+        log.info("-------------------------------------------")
+        log.info("Cluster configuration:")
+        utils.print_cluster_conf(configuration, False)
