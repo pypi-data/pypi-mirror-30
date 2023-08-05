e9f89ca67ed28cc19dde16dc51ab7fd7e33c5934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:13 +0000

    Add package serial/0.0.43/serial-0.0.43.tar.gz

---
+++ packages/serial/serial-0.0.43.tar.gz/serial-0.0.43/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='serial',
+    version='0.0.43',
+    description='A library for serializing python objects as JSON/YAML/XML, and deserializing JSON/YAML/XML.',
+    url='https://bitbucket.com/davebelais/serial.git',
+    author='David Belais',
+    author_email='david@belais.me',
+    license='MIT',
+    classifiers=[
+        # 'Development Status :: 1 - Planning',
+        'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='rest api serialization serialize',
+    packages=find_packages(),
+    install_requires=[
+        'future>=0.16.0',
+        'pyyaml>=3.12',
+        'iso8601>=0.1.12',
+    ],
+)
