92a22468150ef03234d559b089051e9399ba1f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:12 +0000

    Add package invoice2data/0.2.79/invoice2data-0.2.79-py2.7.egg

---
+++ packages/invoice2data/invoice2data-0.2.79-py2.7.egg/invoice2data/output/to_csv.py
@@ -0,0 +1,17 @@
+import csv
+
+def write_to_file(data, path):
+    if path.endswith('.csv'):
+        filename = path
+    else:
+        filename = path + '.csv'
+        
+    with open(filename, "w") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+
+        writer.writerow(['date', 'desc', 'amount'])
+        for line in data:
+            writer.writerow([
+                line['date'].strftime('%d/%m/%Y'),
+                line['desc'],
+                line['amount']])
