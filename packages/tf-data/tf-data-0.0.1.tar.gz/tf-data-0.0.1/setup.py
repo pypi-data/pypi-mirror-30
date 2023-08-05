674936027f08584fbefffd7e19fabba98476ea67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:29 +0000

    Add package tf-data/0.0.1/tf-data-0.0.1.tar.gz

---
+++ packages/tf-data/tf-data-0.0.1.tar.gz/tf-data-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'tf-data',
+  packages = ['tf-data'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'Easy datasets for tensorflow',
+  author = 'Florian Rueberg',
+  author_email = 'florian.rueberg@gmail.com',
+  url = 'https://github.com/fru/tf-data',
+  download_url = 'https://github.com/fru/tf-data/archive/0.0.1.tar.gz',
+  keywords = ['datasets', 'tensorflow'],
+  classifiers = [],
+)
