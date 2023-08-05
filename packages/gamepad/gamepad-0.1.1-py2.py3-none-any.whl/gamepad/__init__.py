1f2ebf7a820b3fb633d9bb6e4f947c3e570711f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:58 +0000

    Add package gamepad/0.1.1/gamepad-0.1.1-py2.py3-none-any.whl

---
+++ packages/gamepad/gamepad-0.1.1-py2.py3-none-any.whl/gamepad/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Python Gamepad API."""
+
+__author__ = """Darius Montez"""
+__email__ = 'darius.montez@gmail.com'
+__version__ = '0.1.1'
+
+from gamepad.gamepad import Gamepad
+
+__all__ = [
+    'Gamepad'
+]
---
+++ packages/gamepad/gamepad-0.1.1-py2.py3-none-any.whl/gamepad/cli.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for gamepad."""
+import sys
+import click
+
+from gamepad import Gamepad
+
+
+@click.command()
+def main(args=None):
+    """Console script for gamepad."""
+
+    controller = Gamepad()
+    controller.watch_all()
+
+    click.echo("Connect a gamepad and start mashing buttons!")
+
+    input("Press ENTER to quit\n\n")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
