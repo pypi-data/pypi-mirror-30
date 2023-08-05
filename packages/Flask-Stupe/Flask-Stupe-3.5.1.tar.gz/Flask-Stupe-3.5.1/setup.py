06fea73e6035a0cc4149e04388391a97f67514d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:30 +0000

    Add package Flask-Stupe/3.5.1/Flask-Stupe-3.5.1.tar.gz

---
+++ packages/Flask-Stupe/Flask-Stupe-3.5.1.tar.gz/Flask-Stupe-3.5.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+def get_description():
+    with open("README.rst") as file:
+        return file.read()
+
+
+setup(
+    name='Flask-Stupe',
+    version='3.5.1',
+    url='https://github.com/numberly/flask-stupe',
+    license='MIT',
+    author='Guillaume Gelin',
+    author_email='ramnes@1000mercis.com',
+    description='a.k.a. « Flask on steroids »',
+    long_description=get_description(),
+    py_modules=['flask_stupe'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=['Flask>=0.11'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
