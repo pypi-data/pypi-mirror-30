e131066713559bc9d53af9020ee20973caf06955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:42 +0000

    Add package ecotron-app/0.2/ecotron-app-0.2.tar.gz

---
+++ packages/ecotron-app/ecotron-app-0.2.tar.gz/ecotron-app-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+     name='ecotron-app',    # This is the name of your PyPI-package.
+     version='0.2',                          # Update the version number for new releases
+     scripts=['app'],                  # The name of your scipt, and also the command you'll be using for calling it
+     url='https://github.com/nanusefue/econtro-app',
+     author='Esteban Gimenez - nanusefue',
+     author_email='nanusefue@gmail.com',
+
+
+)
