29d86f1388acc9f37d9c9d1597e157cd3076a687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:23 +0000

    Add package anybadge/1.1.1/anybadge-1.1.1.tar.gz

---
+++ packages/anybadge/anybadge-1.1.1.tar.gz/anybadge-1.1.1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+from setuptools import setup
+from mister_bump import bump
+from m2r import parse_from_file
+import restructuredtext_lint
+
+# Parser README.md into reStructuredText format
+rst_readme = parse_from_file('README.md')
+
+# Validate the README, checking for errors
+errors = restructuredtext_lint.lint(rst_readme)
+
+# Raise an exception for any errors found
+if errors:
+    print(rst_readme)
+    raise ValueError('README.md contains errors: ',
+                     ', '.join([e.message for e in errors]))
+
+setup(
+    name='anybadge',
+    description='Simple, flexible badge generator for project badges.',
+    long_description=rst_readme,
+    version=bump(),
+    author='Jon Grace-Cox',
+    author_email='jongracecox@gmail.com',
+    py_modules=['anybadge', 'anybadge_server'],
+    setup_requires=['setuptools', 'wheel'],
+    tests_require=[],
+    install_requires=[],
+    data_files=[],
+    options={
+        'bdist_wheel': {'universal': True}
+    },
+    url='https://github.com/jongracecox/anybadge',
+    entry_points={
+        'console_scripts': ['anybadge=anybadge:main',
+                            'anybadge-server=anybadge_server:main'],
+    }
+)
