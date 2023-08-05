8096ab8eecd5d53cce3d8aba33b8e010379714ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:05 +0000

    Add package geno/0.0.0.3/geno-0.0.0.3.tar.gz

---
+++ packages/geno/geno-0.0.0.3.tar.gz/geno-0.0.0.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name='geno',
+    packages=['geno'],
+    version='0.0.0.3',
+    description='A solver for non-linear optimization problems.',
+    author='Soeren Laue',
+    author_email='soeren.laue@uni-jena.de',
+    # I'll explain this in a second
+    # download_url='https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',
+    package_data={'': ['license.txt'],
+                  "geno": ["*.so"]},
+    include_package_data=True,
+    keywords=['solver', 'optimization'],
+)
