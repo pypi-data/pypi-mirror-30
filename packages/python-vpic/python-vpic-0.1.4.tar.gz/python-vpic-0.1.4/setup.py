c28c0614a0e275aad4211f406af78b2a8d89202e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:38 +0000

    Add package python-vpic/0.1.4/python-vpic-0.1.4.tar.gz

---
+++ packages/python-vpic/python-vpic-0.1.4.tar.gz/python-vpic-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='python-vpic',
+    version='0.1.4',
+    description='Simple wrapper to the vPic API.',
+    long_description=readme(),
+    keywords='vPic',
+    url='https://github.com/OmniRisk/python-vpic',
+    author='Damian Hites',
+    author_email='damian@omnirisk.co',
+    license='MIT',
+    packages=['vpic'],
+    install_requires=[
+        'requests'
+    ],
+    include_package_data=True,
+    zip_safe=True,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
---
+++ packages/python-vpic/python-vpic-0.1.4.tar.gz/python-vpic-0.1.4/vpic/models.py
@@ -0,0 +1,12 @@
+import re
+
+
+def camel_to_snake(s):
+    return re.sub("([A-Z]+)", "_\\1", s).lower().lstrip("_")
+
+
+class Vehicle(object):
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, camel_to_snake(key), value.strip())
