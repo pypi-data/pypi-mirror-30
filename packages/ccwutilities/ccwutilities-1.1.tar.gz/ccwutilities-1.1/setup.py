fc40a879708f3006eed0452a3fc302fda095b63e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:08 +0000

    Add package ccwutilities/1.1/ccwutilities-1.1.tar.gz

---
+++ packages/ccwutilities/ccwutilities-1.1.tar.gz/ccwutilities-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ccwutilities',
+      version='1.1',
+      description='CCW Utilities',
+      author='Michael van der Veeke',
+      author_email='mva@ccw.vic.edu.au',
+      url='https://ccw.vic.edu.au',
+     )
