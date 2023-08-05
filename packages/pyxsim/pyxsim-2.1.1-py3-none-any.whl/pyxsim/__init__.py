463bd26177274948c0a8e52d9866bd6052284e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:00 +0000

    Add package pyxsim/2.1.1/pyxsim-2.1.1-py3-none-any.whl

---
+++ packages/pyxsim/pyxsim-2.1.1-py3-none-any.whl/pyxsim/__init__.py
@@ -0,0 +1,36 @@
+__version__ = "2.1.1"
+
+from pyxsim.event_list import \
+    EventList, \
+    ConvolvedEventList
+
+from pyxsim.instruments import \
+    InstrumentSimulator, \
+    ACIS_I, ACIS_S, \
+    Lynx_Imager, Lynx_Calorimeter, \
+    Hitomi_SXS, Athena_WFI, \
+    Athena_XIFU
+
+from pyxsim.photon_list import \
+    PhotonList
+
+from pyxsim.source_generators import \
+    make_xrb_particles, \
+    make_xrb_photons, \
+    XrayLightCone, \
+    make_background, \
+    make_point_sources
+
+from pyxsim.source_models import \
+   SourceModel, \
+   ThermalSourceModel, \
+   LineSourceModel, \
+   PowerLawSourceModel
+
+from pyxsim.spectral_models import \
+    TableApecModel, \
+    AbsorptionModel, \
+    TBabsModel, WabsModel
+
+from pyxsim.utils import \
+    merge_files
