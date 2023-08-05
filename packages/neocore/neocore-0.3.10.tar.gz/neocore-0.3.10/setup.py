4ff50b5aeb03639c0e434fd4894b674a35025faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:14 +0000

    Add package neocore/0.3.10/neocore-0.3.10.tar.gz

---
+++ packages/neocore/neocore-0.3.10.tar.gz/neocore-0.3.10/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+from pip.download import PipSession
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+install_reqs = parse_requirements('requirements.txt', session=PipSession())
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='neocore',
+    python_requires='>=3.4',
+    version='0.3.10',
+    description="Core functionality of neo-python",
+    long_description=readme + '\n\n' + history,
+    author="City of Zion",
+    author_email='python@cityofzion.io',
+    url='https://github.com/CityOfZion/neo-python-core',
+    packages=find_packages(include=['neocore']),
+    include_package_data=True,
+    install_requires=reqs,
+    license="MIT license",
+    zip_safe=False,
+    keywords='neocore, neo, python, node',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
