93da72719b7a79432b3f48d9a01256eecb2a291d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:06 +0000

    Add package pycda/0.1.0/pycda-0.1.0.tar.gz

---
+++ packages/pycda/pycda-0.1.0.tar.gz/pycda-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='pycda',
+    version='0.1.0',
+    description='Python Crater Detection Algorithm (PyCDA) is a pipeline for crater detection; go from image to annotated crater stats in minutes.',
+    url='https://github.com/AlliedToasters/PyCDA',
+    download_url='https://github.com/AlliedToasters/PyCDA/archive/0.1.0.tar.gz',
+    keywords = ['crater detection', 'astronomy', 'planetary science', 'planetary geology'],
+    author='Michael Klear',
+    author_email='michael.klear@colorado.edu',
+    license='MIT',
+    packages=['pycda'],
+    classifiers=[]
+)
