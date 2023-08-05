b6f6ad4727ca1f391a61d66241b71ee2c0f5d696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:57 +0000

    Add package zen-py/0.0.0.17/zen-py-0.0.0.17.tar.gz

---
+++ packages/zen-py/zen-py-0.0.0.17.tar.gz/zen-py-0.0.0.17/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zen-py',
+    keywords=['serverless', 'automation', 'aws'],
+    description='An AWS service automation tools for the missing pieces of CloudFormation',
+    license='Apache License 2.0',
+    install_requires=['boto3', 'pymysql', 'requests'],
+    version='0.0.0.17',
+    entry_points={
+          'console_scripts': [
+              'zen = zenpy.zen:master_command'
+          ]
+      },
+    author='Chen Cheng',
+    author_email='ccwukong@gmail.com',
+    packages=find_packages(),
+    platforms='any',
+)
