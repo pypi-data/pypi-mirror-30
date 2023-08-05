4480e012cc6829091bc01e351eff74b740c6c3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:12 +0000

    Add package cfly/0.9.6/cfly-0.9.6.tar.gz

---
+++ packages/cfly/cfly-0.9.6.tar.gz/cfly-0.9.6/cfly/__init__.py
@@ -0,0 +1,9 @@
+'''
+    Build python extensions on-the-fly.
+'''
+
+from .core import compile_module, module_from_source
+from . import utils
+
+__all__ = ['compile_module', 'module_from_source', 'utils']
+__version__ = '0.9.6'
