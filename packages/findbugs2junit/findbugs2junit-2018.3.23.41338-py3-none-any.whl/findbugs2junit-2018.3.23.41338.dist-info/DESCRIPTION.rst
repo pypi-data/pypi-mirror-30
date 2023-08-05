277fdc6132fbf9412f1cfadb13ef5b602856c41c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:48 +0000

    Add package findbugs2junit/2018.3.23.41338/findbugs2junit-2018.3.23.41338-py3-none-any.whl

---
+++ packages/findbugs2junit/findbugs2junit-2018.3.23.41338-py3-none-any.whl/findbugs2junit-2018.3.23.41338.dist-info/DESCRIPTION.rst
@@ -0,0 +1,12 @@
+|Build Status|
+
+findbugs2junit
+==============
+
+Simple tool to convert a findbugs xml file to junit xml for reporting to
+Jenkins.
+
+.. |Build Status| image:: https://travis-ci.org/hughsaunders/findbugs2junit.svg?branch=master
+   :target: https://travis-ci.org/hughsaunders/findbugs2junit
+
+
---
+++ packages/findbugs2junit/findbugs2junit-2018.3.23.41338-py3-none-any.whl/findbugs2junit/findbugs2junit.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import logging
+import xml.etree.ElementTree as ET
+
+import click
+
+from junit_xml import TestCase, TestSuite
+
+logging.basicConfig()
+logger = logging.getLogger("findbugs2junit")
+
+
+@click.command()
+@click.argument('infile', type=click.File('r'))
+@click.argument('outfile', type=click.File('w'))
+@click.option('--debug/--no-debug', default=False)
+def cli(infile, outfile, debug):
+    if debug:
+        logger.setLevel(logging.DEBUG)
+    logger.info("infile: {inf} outfile: {outf}".format(
+        inf=infile.name, outf=outfile.name))
+    tree = ET.parse(infile)
+    root = tree.getroot()
+
+    test_cases = []
+    files = root.findall("file")
+    for f in files:
+        for b in f.findall("BugInstance"):
+            classname = f.get("classname")
+            tc = TestCase(
+                b.get("type"),  # name
+                classname,  # classname
+            )
+            # mark test case as a failure
+            tc.add_failure_info("{0} line:{1}".format(b.get("message"),
+                                                      b.get("lineNumber")))
+            test_cases.append(tc)
+    ts = TestSuite("Findbugs", test_cases)
+    ts.to_file(outfile, [ts])
+
+
+if __name__ == "__main__":
+    cli()
