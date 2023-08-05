72f190c0d31afbc0cb3d3e7f0e9c7acaa4490111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:14 +0000

    Add package aapippackage/0.1/aapippackage-0.1.tar.gz

---
+++ packages/aapippackage/aapippackage-0.1.tar.gz/aapippackage-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='aapippackage',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='',
+      author='gaurav',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['aapippackage'],
+      zip_safe=False)
