377764d653a92974ae4683dff380cde67baf861c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:21 +0000

    Add package boto3-batch-utils/1.0.22/boto3_batch_utils-1.0.22.tar.gz

---
+++ packages/boto3-batch-utils/boto3_batch_utils-1.0.22.tar.gz/boto3_batch_utils-1.0.22/boto3_batch_utils/Cloudwatch.py
@@ -0,0 +1,52 @@
+import logging
+from datetime import datetime
+
+from boto3_batch_utils.Base import BaseDispatcher
+
+
+logger = logging.getLogger('boto3-batch-utils')
+
+
+def cloudwatch_dimension(name, value):
+    """ Structure for forming aCloudwatch dimension """
+    return {'Name': str(name), 'Value': str(value)}
+
+
+class CloudwatchBatchDispatcher(BaseDispatcher):
+    """
+    Manage the batch 'put' of Cloudwatch metrics
+    """
+
+    def __init__(self, namespace, max_batch_size=25, flush_payload_on_max_batch_size=True):
+        self.namespace = namespace
+        super().__init__('cloudwatch', 'put_metric_data', batch_size=max_batch_size,
+                         flush_payload_on_max_batch_size=flush_payload_on_max_batch_size)
+
+    def _send_individual_payload(self, payload, retry=4):
+        """ Send an individual metric to Cloudwatch """
+        pass
+
+    def _process_batch_send_response(self, response):
+        """ Process the response data from a batch put request (N/A) """
+        pass
+
+    def _batch_send_payloads(self, batch=None, **nested_batch):
+        """ Attempt to send a single batch of metrics to Cloudwatch """
+        super()._batch_send_payloads({'Namespace': self.namespace, 'MetricData': batch})
+
+    def flush_payloads(self):
+        """ Push all metrics in the payload list to Cloudwatch """
+        super().flush_payloads()
+
+    def submit_payload(self, metric_name=None, timestamp=datetime.now(), dimensions=None, value=None, unit='Count'):
+        """ Submit a metric ready to be batched up and sent to Cloudwatch """
+        payload = {
+                'MetricName': metric_name,
+                'Timestamp': timestamp,
+                'Value': value,
+                'Unit': unit
+            }
+        logger.debug("Payload submitted to {} dispatcher: {}".format(self._subject_name, payload))
+        if dimensions:
+            payload['Dimensions'] = dimensions if isinstance(dimensions, list) else [dimensions]
+        super().submit_payload(payload)
