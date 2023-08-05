1b230f459b80b3acff0e20f32bbc065f74272ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:31 +0000

    Add package politico-civic-geography/0.1.5/politico_civic_geography-0.1.5-py3-none-any.whl

---
+++ packages/politico-civic-geography/politico_civic_geography-0.1.5-py3-none-any.whl/geography/admin.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from geography.models import (Division, DivisionLevel, Geometry,
+                              IntersectRelationship)
+
+
+class IntersectRelationshipInline(admin.StackedInline):
+    model = IntersectRelationship
+    fk_name = 'from_division'
+    extra = 0
+
+
+class DivisionAdmin(admin.ModelAdmin):
+    inlines = (IntersectRelationshipInline,)
+    list_display = ('label', 'level', 'code')
+    list_filter = ('level',)
+    search_fields = ('code', 'label')
+    readonly_fields = ('parent', 'uid',)
+
+
+class GeometryAdmin(admin.ModelAdmin):
+    list_display = ('division', 'map_level', 'small_preview')
+    list_filter = ('subdivision_level',)
+    search_fields = ('division__name',)
+    readonly_fields = ('file_size', 'large_preview', 'source', 'series',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('division', 'subdivision_level', 'source', 'series',)
+        }),
+        ('Geo data', {
+            'fields': (
+                'topojson', 'simplification',
+                'file_size', 'large_preview',)
+        }),
+        ('In effect', {
+            'fields': (
+                'effective', 'effective_start', 'effective_end',
+            )
+        }),
+    )
+
+    def map_level(self, obj):
+        return obj.subdivision_level.name
+
+
+admin.site.register(DivisionLevel)
+admin.site.register(Division, DivisionAdmin)
+admin.site.register(Geometry, GeometryAdmin)
