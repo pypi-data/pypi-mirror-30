089bbb54b9ec98a3762f3c15ca87ebe8b9909883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:30 +0000

    Add package mocksftp/1.0.1/mocksftp-1.0.1-py2.py3-none-any.whl

---
+++ packages/mocksftp/mocksftp-1.0.1-py2.py3-none-any.whl/mocksftp/decorators.py
@@ -0,0 +1,29 @@
+import logging
+
+import paramiko
+
+logger = logging.getLogger(__name__)
+
+
+def returns_sftp_error(func):
+
+    def wrapped(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except OSError as err:
+            logger.debug(
+                "Error calling %s(%s, %s): %s",
+                func, args, kwargs, err, exc_info=True)
+            return paramiko.SFTPServer.convert_errno(err.errno)
+        except AttributeError as err:
+            logger.warning(
+                "Error calling %s(%s, %s): %s",
+                func, args, kwargs, err, exc_info=True)
+            return paramiko.SFTP_OP_UNSUPPORTED
+        except Exception as err:
+            logger.warning(
+                "Error calling %s(%s, %s): %s",
+                func, args, kwargs, err, exc_info=True)
+            return paramiko.SFTP_FAILURE
+
+    return wrapped
