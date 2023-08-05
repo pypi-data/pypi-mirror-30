67ae39a21309ae1d78a3d5b0a73a41702edc48ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:15 +0000

    Add package dqa-commonlib/0.5.3/dqa_commonlib-0.5.3.tar.gz

---
+++ packages/dqa-commonlib/dqa_commonlib-0.5.3.tar.gz/dqa_commonlib-0.5.3/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Created by lijian on 2017/9/1.
+
+from setuptools import setup, find_packages
+
+setup(
+    name="dqa_commonlib",
+    version="0.5.3",
+    url="https://github.com/xiaoyaoyuyi/python_package_test",
+    description="common script operations liabray about python",
+    author="xiaoyaoyuyi",
+    author_email="389105015@qq.com",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=['Programming Language :: Python :: 2.7'],
+    python_requires='>=2.7',
+    install_requires=['MySQL-python'],
+    package_data={
+        'dqa_commonlib': ['setup.cfg', 'LICENSE.txt', 'MANIFEST.in'] 
+    },
+    keywords="dada qa commonlib",
+)
