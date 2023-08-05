d70c235bb73352e23ae66c6d243f27dca152fad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:24 +0000

    Add package MultiRunner/3.0/MultiRunner-3.0.tar.gz

---
+++ packages/MultiRunner/MultiRunner-3.0.tar.gz/MultiRunner-3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name = "MultiRunner",
+    version = "3.0",
+    description= "",
+    long_description= "This is a package for multi-process running, dedicated to my girlfriend 10L",
+    url='https://www.github.com/statham-stone/',
+    author = "Statham",
+    author_email = "statham.stone@gmail.com",
+    packages=[""]
+)
