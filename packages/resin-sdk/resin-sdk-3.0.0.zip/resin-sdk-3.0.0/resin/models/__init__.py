529385a6ac7ab786dfb434ac38a6449f978cf399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:30 +0000

    Add package resin-sdk/3.0.0/resin-sdk-3.0.0.zip

---
+++ packages/resin-sdk/resin-sdk-3.0.0.zip/resin-sdk-3.0.0/resin/models/__init__.py
@@ -0,0 +1,26 @@
+"""
+This module implements all models for Resin Python SDK.
+
+"""
+
+from .device import Device
+from .application import Application
+from .config import Config
+from .environment_variables import EnvironmentVariable
+from .key import Key
+from .device_os import DeviceOs
+from .supervisor import Supervisor
+from .build import Build
+
+
+class Models(object):
+
+    def __init__(self):
+        self.device = Device()
+        self.application = Application()
+        self.config = Config()
+        self.environment_variables = EnvironmentVariable()
+        self.key = Key()
+        self.device_os = DeviceOs()
+        self.supervisor = Supervisor()
+        self.build = Build()
