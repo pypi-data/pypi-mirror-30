190cc0dd95c320f0841d781c392154c2f244b9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:48 +0000

    Add package vr.imager/1.4.1/vr.imager-1.4.1.tar.gz

---
+++ packages/vr.imager/vr.imager-1.4.1.tar.gz/vr.imager-1.4.1/README.rst
@@ -0,0 +1,13 @@
+.. image:: https://img.shields.io/pypi/v/vr.imager.svg
+   :target: https://pypi.org/project/vr.imager
+
+.. image:: https://img.shields.io/pypi/pyversions/vr.imager.svg
+
+.. image:: https://img.shields.io/travis/yougov/vr.imager/master.svg
+   :target: https://travis-ci.org/yougov/vr.imager
+
+vr.imager is a component of the `Velociraptor
+<https://github.com/yougov/velociraptor>`_ project.
+
+The supported interface is the command-line API, invoked with
+``vimage`` or ``python -m vr.imager``.
---
+++ packages/vr.imager/vr.imager-1.4.1.tar.gz/vr.imager-1.4.1/setup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+# Project skeleton maintained at https://github.com/jaraco/skeleton
+
+import io
+
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+    long_description = readme.read()
+
+name = 'vr.imager'
+description = 'Command line tool to create system image tarballs.'
+nspkg_technique = 'managed'
+"""
+Does this package use "native" namespace packages or
+pkg_resources "managed" namespace packages?
+"""
+
+params = dict(
+    name=name,
+    use_scm_version=True,
+    author="Brent Tubbs",
+    author_email="brent.tubbs@gmail.com",
+    description=description or name,
+    long_description=long_description,
+    url="https://github.com/yougov/" + name,
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    namespace_packages=(
+        name.split('.')[:-1] if nspkg_technique == 'managed'
+        else []
+    ),
+    python_requires='>=2.7',
+    install_requires=[
+        'vr.runners>=2.10',
+        'jaraco.ui',
+        'path.py>=7.2',
+        'vr.common>=4.3',
+    ],
+    extras_require={
+        'testing': [
+            'pytest>=2.8',
+            #'pytest-sugar',
+            'collective.checkdocs',
+        ],
+        'docs': [
+            'sphinx',
+            'jaraco.packaging>=3.2',
+            'rst.linker>=1.9',
+        ],
+    },
+    setup_requires=[
+        'setuptools_scm>=1.15.0',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    entry_points={
+        'console_scripts': [
+            'vimage = vr.imager.command:invoke',
+        ],
+    },
+)
+if __name__ == '__main__':
+    setuptools.setup(**params)
