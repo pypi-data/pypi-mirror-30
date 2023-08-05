eba15169947890bd2cacf17720518b2080dad939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:11 +0000

    Add package progeny/0.1.0/progeny-0.1.0.tar.gz

---
+++ packages/progeny/progeny-0.1.0.tar.gz/progeny-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='progeny',
+    version='0.1.0',
+    description='Simple but powerful management for complex class hierarchies',
+    long_description=readme,
+    url='https://github.com/GoodRx/progeny',
+    author='Lyndsy Simon',
+    author_email='lyndsy@goodrx.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    required=('six', ),
+)
