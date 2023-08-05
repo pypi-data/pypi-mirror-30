fdb70ea713178d893b572135eca469e0f1badf7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:29 +0000

    Add package template-file-parser/0.1/template-file-parser-0.1.tar.gz

---
+++ packages/template-file-parser/template-file-parser-0.1.tar.gz/template-file-parser-0.1/README.rst
@@ -0,0 +1,8 @@
+template-file-parser
+====================
+
+|Build Status|
+
+.. |Build Status| image:: https://travis-ci.org/david-gang/template-file-parser.svg?branch=master
+   :target: https://travis-ci.org/david-gang/template-file-parser
+
---
+++ packages/template-file-parser/template-file-parser-0.1.tar.gz/template-file-parser-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='template-file-parser',
+      version='0.1',
+      description='Tiny wrapper over string templates which works with files',
+      url='https://github.com/david-gang/template-file-parser',
+      download_url='https://github.com/david-gang/template-file-parser/archive/0.1.tar.gz',
+      author='David Gang',
+      author_email='michaelgang@gmail.com',
+      license='MIT',
+      packages=['template_file_parser'],
+      python_requires=">=3.4",
+      zip_safe=False)
