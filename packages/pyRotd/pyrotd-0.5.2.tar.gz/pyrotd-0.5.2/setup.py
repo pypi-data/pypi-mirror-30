bd53b678c04b00a29da41fe02f25097de110c980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:00 +0000

    Add package pyRotd/0.5.2/pyrotd-0.5.2.tar.gz

---
+++ packages/pyRotd/pyrotd-0.5.2.tar.gz/pyrotd-0.5.2/setup.py
@@ -0,0 +1,37 @@
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+# Get the long description from the README file
+with open(os.path.join(here, 'README.rst')) as fp:
+    long_description = fp.read()
+
+setup(
+    name='pyrotd',
+    license='MIT',
+    version='0.5.2',
+    description='Rotated response spectrum calculation implemented in Python.',
+    long_description=long_description,
+    url='http://github.com/arkottke/pyrotd',
+    author='Albert Kottke',
+    author_email='albert.kottke@gmail.com',
+    install_requires=[
+        'numpy',
+        'setuptools',
+    ],
+    test_suite='tests',
+    keywords=['response spectrum', 'earthquake ground motion'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Science/Research',
+    ],
+    zip_safe=True,
+    include_package_data=True, )
