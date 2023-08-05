404164df3bcfb770bf8a5ee4dd98f1a24cf546e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:35 +0000

    Add package PriceDb/1.3.2/PriceDb-1.3.2.tar.gz

---
+++ packages/PriceDb/PriceDb-1.3.2.tar.gz/PriceDb-1.3.2/pricedb/utils.py
@@ -0,0 +1,22 @@
+""" Various utilities """
+from typing import List
+
+def split_symbol(symbol: str) -> (str, str):
+    """ Splits the symbol into namespace, symbol tuple """
+    symbol_parts = symbol.split(":")
+    namespace = None
+    mnemonic = symbol
+
+    if len(symbol_parts) > 1:
+        namespace = symbol_parts[0]
+        mnemonic = symbol_parts[1]
+
+    return (namespace, mnemonic)
+
+def read_lines_from_file(file_path: str) -> List[str]:
+    """ Read text lines from a file """
+    # check if the file exists?
+    with open(file_path) as csv_file:
+        content = csv_file.readlines()
+    return content
+    
