9c07f12bb8df1e6105049def28592aed79a1d907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:18 +0000

    Add package xswitch/1.0.2/xswitch-1.0.2.tar.gz

---
+++ packages/xswitch/xswitch-1.0.2.tar.gz/xswitch-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="xswitch",
+	version="1.0.2",
+	packages=find_packages(),
+	description="My usefull functions",
+	url="https://0xswitch.fr",
+	author="switch",
+	license="WTFPL",
+	python_requires=">=2.7"
+	)
---
+++ packages/xswitch/xswitch-1.0.2.tar.gz/xswitch-1.0.2/xswitch/__init__.py
@@ -0,0 +1,1 @@
+from xswitch import *
