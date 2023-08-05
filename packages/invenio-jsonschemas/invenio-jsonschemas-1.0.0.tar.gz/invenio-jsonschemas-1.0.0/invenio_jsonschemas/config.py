7bd56cc5dd9816dd4fea49a3f92ba97b9cb22f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:01 +0000

    Add package invenio-jsonschemas/1.0.0/invenio-jsonschemas-1.0.0.tar.gz

---
+++ packages/invenio-jsonschemas/invenio-jsonschemas-1.0.0.tar.gz/invenio-jsonschemas-1.0.0/invenio_jsonschemas/config.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015-2018 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Default configuration."""
+
+JSONSCHEMAS_HOST = 'localhost'
+"""Default json schema host."""
+
+JSONSCHEMAS_ENDPOINT = '/schemas'
+"""Default schema endpoint."""
+
+JSONSCHEMAS_URL_SCHEME = 'https'
+"""Default url scheme for schemas."""
+
+JSONSCHEMAS_REPLACE_REFS = False
+"""Whether to resolve $ref before serving a schema."""
+
+JSONSCHEMAS_RESOLVE_SCHEMA = False
+"""Whether to resolve schema using the Resolver Class.
+
+If is ``True``, will replace $ref and run the
+:py:const:`invenio_jsonschemas.config.JSONSCHEMAS_RESOLVER_CLS` class
+before serving a schema.
+"""
+
+JSONSCHEMAS_LOADER_CLS = None
+"""Loader class used in ``JSONRef`` when replacing ``$ref``."""
+
+JSONSCHEMAS_RESOLVER_CLS = 'invenio_jsonschemas.utils.resolve_schema'
+"""Resolver used to resolve the schema.
+
+if :py:const:`invenio_jsonschemas.config.JSONSCHEMAS_RESOLVE_SCHEMA` is
+``True`` or there is ``?resolved=1`` parameter on the request the resolver
+will run over the schema. This can be used for custom schemas resolver.
+"""
+
+JSONSCHEMAS_REGISTER_ENDPOINTS_API = True
+"""Register the endpoints on the API app."""
+
+JSONSCHEMAS_REGISTER_ENDPOINTS_UI = True
+"""Register the endpoints on the UI app."""
