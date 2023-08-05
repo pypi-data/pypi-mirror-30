3622cc5e8ff41d9e5d9a452db9c0c14591a63ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:42 +0000

    Add package wagtail-modelchooser-1.13.1/1.0.0/wagtail-modelchooser-1.13.1-1.0.0.tar.gz

---
+++ packages/wagtail-modelchooser-1.13.1/wagtail-modelchooser-1.13.1-1.0.0.tar.gz/wagtail-modelchooser-1.13.1-1.0.0/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+Install wagtailmodelchooser using setuptools
+"""
+from setuptools import find_packages, setup
+
+with open('wagtailmodelchooser/version.py', 'r') as f:
+    version = None
+    exec(f.read())
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='wagtail-modelchooser-1.13.1',
+    version=version,
+    description='Model choosers for Wagtail admin',
+    long_description=readme,
+    author='Tim Heap',
+    author_email='tim@takeflight.com.au',
+    url='https://github.com/nebulosar/wagtailmodelchooser/',
+
+    install_requires=['wagtail>=1.13.'],
+    zip_safe=False,
+    license='BSD License',
+
+    packages=find_packages(exclude=['tests*']),
+
+    include_package_data=True,
+    package_data={},
+
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
