15320bbae4c82ae37df0197549f75ab2a39f72ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:48 +0000

    Add package Moustache-fusion.beta/0.0.1.post16728/Moustache-fusion.beta-0.0.1.post16728.tar.gz

---
+++ packages/Moustache-fusion.beta/Moustache-fusion.beta-0.0.1.post16728.tar.gz/Moustache-fusion.beta-0.0.1.post16728/moustache_fusion/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Module de fusion documentaire
+"""
+__version__ = "0.0.1-16728"
+
+
+def launch():
+    from moustache_fusion import skittlesws
+    skittlesws.default_app().run(debug=True, host='0.0.0.0')
