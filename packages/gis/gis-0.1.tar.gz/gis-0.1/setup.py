882752615ee292d32bbd140b9da832e8215cf8f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:53 +0000

    Add package gis/0.1/gis-0.1.tar.gz

---
+++ packages/gis/gis-0.1.tar.gz/gis-0.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='gis',
+    version='0.1',
+    url='http://github.com/terranodo/gis',
+    license='MIT',
+    author='Ariel Nunez',
+    author_email='ingenieroariel@gmail.com',
+    description='async + http2 + postgis + openapi + geo+json-seq',
+    long_description=open('README').read(),
+    py_modules=['gis'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)
