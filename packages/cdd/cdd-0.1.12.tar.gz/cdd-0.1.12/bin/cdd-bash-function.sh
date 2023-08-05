f3481ed77b56de1f7ef855232f4f40d959c971eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:31 +0000

    Add package cdd/0.1.12/cdd-0.1.12.tar.gz

---
+++ packages/cdd/cdd-0.1.12.tar.gz/cdd-0.1.12/bin/cdd-bash-function.sh
@@ -0,0 +1,26 @@
+function cd {
+    if [[ $# == 0 ]]; then
+        # no argument used
+        command cd
+        return 0
+    fi
+
+    if [[ $# == 1 ]] && [[ -d "$1" ]]; then
+        # argument is a directory in current directory
+        command cd "$1"
+
+        # add directory to the database
+        cdd -a $PWD
+
+        # and that's all
+        return 0
+    fi
+
+    # assume argument $1 is a pattern
+    cddmatch=$(cdd $1 | head -1)
+    if [[ -d "$cddmatch" ]]; then
+        command cd $cddmatch
+    else
+        cdd -d "$cddmatch"
+    fi
+}
