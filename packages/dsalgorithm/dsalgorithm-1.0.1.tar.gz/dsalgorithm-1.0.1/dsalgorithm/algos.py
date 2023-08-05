13b8d00be00e2b7b5c8fc4e276f90c8505c5c357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:29 +0000

    Add package dsalgorithm/1.0.1/dsalgorithm-1.0.1.tar.gz

---
+++ packages/dsalgorithm/dsalgorithm-1.0.1.tar.gz/dsalgorithm-1.0.1/dsalgorithm/algos.py
@@ -0,0 +1,6 @@
+def hellofunction(text):
+    """
+    :param text: some text
+    :return: This function returns the sample "hello world" with the text.
+    """
+    print("Hello world ",text)
---
+++ packages/dsalgorithm/dsalgorithm-1.0.1.tar.gz/dsalgorithm-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='dsalgorithm',
+    version='1.0.1',
+    packages=['dsalgorithm'],
+    url='https://github.com/SSK0001/Algorithms-DS',
+    license='MIT License',
+    author='Santhosh ',
+    author_email='santhoshksuram@gmail.com',
+    description='Repository contains the solutions to most common CS Algorithm problems.'
+)
