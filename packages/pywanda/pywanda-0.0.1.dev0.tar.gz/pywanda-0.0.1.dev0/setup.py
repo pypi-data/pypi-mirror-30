99eaf66ece6bb06cfc3087ad49d51d9953a3bd43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:03 +0000

    Add package pywanda/0.0.1.dev0/pywanda-0.0.1.dev0.tar.gz

---
+++ packages/pywanda/pywanda-0.0.1.dev0.tar.gz/pywanda-0.0.1.dev0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'pywanda',
+    version = '0.0.1.dev0',
+    author = 'Deltares',
+    description = "Placeholder",
+    long_description = '''Placeholder''',
+    url = 'https://www.deltares.nl/en/software/wanda/',
+    license = 'Proprietary License',
+    platforms = ['windows'],
+    zip_safe=False
+)
