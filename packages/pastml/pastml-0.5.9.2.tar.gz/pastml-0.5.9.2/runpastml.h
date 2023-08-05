6bb29230d838d8c3bd4bdf5de07831c0dad126c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:14 +0000

    Add package pastml/0.5.9.2/pastml-0.5.9.2.tar.gz

---
+++ packages/pastml/pastml-0.5.9.2.tar.gz/pastml-0.5.9.2/runpastml.h
@@ -0,0 +1,9 @@
+//
+// Created by azhukova on 1/24/18.
+//
+
+#ifndef PASTML_PASTML_H
+#define PASTML_PASTML_H
+int runpastml(char *annotation_name, char *tree_name, char *out_annotation_name, char *out_tree_name,
+              char *out_parameter_name, char *model, char *prob_method);
+#endif //PASTML_PASTML_H
