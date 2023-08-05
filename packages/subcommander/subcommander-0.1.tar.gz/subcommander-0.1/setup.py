1cd28270990e4890b134585dcf40789f0be9be4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:32 +0000

    Add package subcommander/0.1/subcommander-0.1.tar.gz

---
+++ packages/subcommander/subcommander-0.1.tar.gz/subcommander-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'subcommander',
+  packages = ['subcommander'],
+  version = '0.1',
+  description = 'A plugable commandline app command management module',
+  author = 'Moritz Moxter',
+  author_email = 'moritzmoxter@7tupel.net',
+  url = 'https://github.com/7tupel/subcommander',
+  download_url = 'https://github.com/7tupel/subcommander/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['argparse', 'commandline', 'commands'],
+  classifiers = [],
+)
