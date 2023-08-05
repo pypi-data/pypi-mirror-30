719208f4239858b1713249132eb55e74cea9ab51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:16 +0000

    Add package simple_paths_algorithm/0.1.1/simple_paths_algorithm-0.1.1-py2.py3-none-any.whl

---
+++ packages/simple_paths_algorithm/simple_paths_algorithm-0.1.1-py2.py3-none-any.whl/simple_paths_algorithm/examples/examples.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import pkg_resources
+
+def load_csv(csv_name):
+    '''
+    inputs:
+        csv_name - str of csv to load from examples/data/
+    -------
+    returns:
+        csv_df - dataframe of csv loaded
+    '''
+    csv_path = pkg_resources.resource_filename(__name__, 'data/' + csv_name)
+    csv_df = pd.read_csv(csv_path, header=None)
+
+    return csv_df
+
+def example_graph_1():
+    '''
+    small 4 node example
+    '''
+    return load_csv('ex_1_nodes.csv'), load_csv('ex_1_edges.csv')
+
+def example_graph_2():
+    '''
+    small 5 node example
+    '''
+    return load_csv('ex_2_nodes.csv'), load_csv('ex_2_edges.csv')
+
+def ncaa_2009_data():
+    '''
+    2009 ncaa football data, data from:
+        http://www.masseyratings.com/
+    explaination of fields:
+        http://www.masseyratings.com/scorehelp.htm
+    '''
+    return load_csv('ncaa_2009_teams.csv'), load_csv('ncaa_2009_games.csv')
+
+
+def ncaa_2016_data():
+    '''
+    2016 ncaa football data, data from:
+        http://www.masseyratings.com/
+    explaination of fields:
+        http://www.masseyratings.com/scorehelp.htm
+    '''
+    return load_csv('ncaa_2016_teams.csv'), load_csv('ncaa_2016_games.csv')
