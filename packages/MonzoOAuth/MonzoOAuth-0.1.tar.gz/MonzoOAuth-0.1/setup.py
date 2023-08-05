a8a1fcc99026c3570f4b6055df58c621e6689ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:23 +0000

    Add package MonzoOAuth/0.1/MonzoOAuth-0.1.tar.gz

---
+++ packages/MonzoOAuth/MonzoOAuth-0.1.tar.gz/MonzoOAuth-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='MonzoOAuth',
+    version='0.1',
+    description='An OAuth2 Monzo Client',
+    url='https://github.com/jafacakes2011/MonzoOAuth',
+    author='Cameron',
+    author_email='',
+    license='MIT',
+    packages=['MonzoOAuth'],
+    zip_safe=False
+)
