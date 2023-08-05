f0894410c382ee472137439c1cb4f6506d50da30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:45 +0000

    Add package cdf2cim/0.2.2.2/cdf2cim-0.2.2.2.tar.gz

---
+++ packages/cdf2cim/cdf2cim-0.2.2.2.tar.gz/cdf2cim-0.2.2.2/runtests.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Import utils.
+source $CDF2CIM_CLIENT_HOME/sh/utils.sh
+
+# Main entry point.
+main()
+{
+    log "ERRATA-TESTS : execution starts ..."
+
+    nosetests -v -s $CDF2CIM_CLIENT_HOME/tests
+    # nosetests -v -s $CDF2CIM_CLIENT_HOME/tests/test_scan.py
+
+    log "ERRATA-TESTS : execution complete ..."
+}
+
+# Invoke entry point.
+main
