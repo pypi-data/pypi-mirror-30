1649892d25808ec306b0a668c4f05fa90790120e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:45 +0000

    Add package qwertyui/1.0.21/qwertyui-1.0.21.tar.gz

---
+++ packages/qwertyui/qwertyui-1.0.21.tar.gz/qwertyui-1.0.21/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+
+setup(name='qwertyui',
+      version='1.0.21',
+      description='Some common Python functions and algorithms',
+      author='Przemyslaw Kaminski',
+      author_email='cgenie@gmail.com',
+      url='https://github.com/CGenie/qwertyui',
+      packages=find_packages(exclude=['tests.py']),
+      install_requires=[
+          'minio==2.2.4',
+          'requests==2.18.4',
+          'requests-toolbelt==0.8.0',
+      ]
+)
