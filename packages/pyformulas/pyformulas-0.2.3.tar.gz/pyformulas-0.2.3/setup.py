922b04e87aa7b404c00149bd92712eb5e2b0cf11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:25 +0000

    Add package pyformulas/0.2.3/pyformulas-0.2.3.tar.gz

---
+++ packages/pyformulas/pyformulas-0.2.3.tar.gz/pyformulas-0.2.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+setup(
+  name = 'pyformulas',
+  packages = ['pyformulas'],
+  python_requires='>=3.0',
+  install_requires=[
+          'opencv-python',
+		  'numpy',
+		  'urllib3'
+      ],
+  version = '0.2.3',
+  description = 'A library of ready-to-go Python formulas',
+  author = 'pyformulas',
+  author_email = 'pyformulas@gmail.com',
+  url = 'https://github.com/pyformulas/pyformulas',
+  download_url = 'https://github.com/pyformulas/pyformulas/archive/0.2.3.tar.gz',
+  keywords = ['python', 'formulas', 'recipes', 'cookbook', 'lazy', 'easy', 'quick', 'shortcut'],
+  classifiers = [],
+)
