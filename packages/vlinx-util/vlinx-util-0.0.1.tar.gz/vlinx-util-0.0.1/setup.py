2d82ff44883048fc0e13f015ea8606bc5e8bcb1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:47 +0000

    Add package vlinx-util/0.0.1/vlinx-util-0.0.1.tar.gz

---
+++ packages/vlinx-util/vlinx-util-0.0.1.tar.gz/vlinx-util-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vlinx-util',
+    version='0.0.1',
+    description='VLINX Python Util',
+    long_description='VLINX Python Util Provided by vlinx.io',
+    author='vlinx',
+    author_email='vlinx@vlinx.io',
+    url='https://vlinx.io/',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[],
+)
