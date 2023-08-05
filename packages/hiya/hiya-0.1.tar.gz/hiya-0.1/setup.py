37a69636c2839fe6b299ab312484938e3abb63d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:32 +0000

    Add package hiya/0.1/hiya-0.1.tar.gz

---
+++ packages/hiya/hiya-0.1.tar.gz/hiya-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hiya',
+      version='0.1',
+      description='Identity Services over chff',
+      url='http://github.com/mnes-io/hiya',
+      author='Charles Perkins',
+      author_email='charlesap@gmail.com',
+      license='MIT',
+      packages=['hiya'],
+      zip_safe=False)
