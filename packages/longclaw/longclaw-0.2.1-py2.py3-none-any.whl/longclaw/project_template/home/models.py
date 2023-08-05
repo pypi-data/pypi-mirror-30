4eba6f53ff9dd21b84b894d157e141849dacea4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:39 +0000

    Add package longclaw/0.2.1/longclaw-0.2.1-py2.py3-none-any.whl

---
+++ packages/longclaw/longclaw-0.2.1-py2.py3-none-any.whl/longclaw/project_template/home/models.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import, unicode_literals
+from wagtail.wagtailcore.models import Page
+
+
+class HomePage(Page):
+    pass
---
+++ packages/longclaw/longclaw-0.2.1-py2.py3-none-any.whl/longclaw/tests/settings.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8
+from __future__ import unicode_literals, absolute_import
+import os
+import django
+
+DEBUG = True
+USE_TZ = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+ROOT_URLCONF = 'longclaw.tests.urls'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sites',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+
+    'wagtail.wagtailforms',
+    'wagtail.wagtailredirects',
+    'wagtail.wagtailembeds',
+    'wagtail.wagtailsites',
+    'wagtail.wagtailusers',
+    'wagtail.wagtailsnippets',
+    'wagtail.wagtaildocs',
+    'wagtail.wagtailimages',
+    'wagtail.wagtailsearch',
+    'wagtail.wagtailadmin',
+    'wagtail.wagtailcore',
+    'wagtail.contrib.modeladmin',
+    'wagtail.contrib.settings',
+
+    'modelcluster',
+    'taggit',
+    'rest_framework',
+    'django_extensions',
+
+    'longclaw.longclawcore',
+    'longclaw.longclawsettings',
+    'longclaw.longclawshipping',
+    'longclaw.longclawproducts',
+    'longclaw.longclaworders',
+    'longclaw.longclawcheckout',
+    'longclaw.longclawbasket',
+    'longclaw.longclawstats',
+    'longclaw.contrib.productrequests',
+    'longclaw.tests.products',
+]
+
+SITE_ID = 1
+
+MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'wagtail.wagtailcore.middleware.SiteMiddleware',
+    'wagtail.wagtailredirects.middleware.RedirectMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(os.path.dirname(__file__), 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'longclaw.longclawsettings.context_processors.currency',
+            ],
+        },
+    },
+]
+
+if django.VERSION >= (1, 10):
+    MIDDLEWARE = MIDDLEWARE
+else:
+    MIDDLEWARE_CLASSES = MIDDLEWARE
+
+STATIC_URL = '/static/'
+
+PRODUCT_VARIANT_MODEL = 'products.ProductVariant'
