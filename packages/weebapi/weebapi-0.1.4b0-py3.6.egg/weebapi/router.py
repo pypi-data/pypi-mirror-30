b286e5c8d48df7063b1a330ea6cb9ca0c65f6398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:51 +0000

    Add package weebapi/0.1.4b0/weebapi-0.1.4b0-py3.6.egg

---
+++ packages/weebapi/weebapi-0.1.4b0-py3.6.egg/weebapi/router.py
@@ -0,0 +1,37 @@
+from weebapi.errors import RequireFormatting
+
+
+class Route(object):
+    def __init__(self, url: str, method: str, require_format: bool = False):
+        self.url = url
+        self.method = method
+        self.require_format = require_format
+
+    def __str__(self) -> str:
+        if self.require_format:
+            raise RequireFormatting
+        return self.url
+
+    def __get__(self, instance, owner) -> str:
+        if self.require_format:
+            raise RequireFormatting
+        return self.url
+
+    def format_url(self, *args) -> str:
+        return self.url.format(*args)
+
+
+class Router(object):
+    def __init__(self, base_url: str):
+        if not base_url.endswith("/"):
+            base_url += "/"
+        self.base_url = base_url
+        self.base_image = base_url + "images/"
+        self.upload = Route(self.base_image + "upload", "POST")
+        self.types = Route(self.base_image + "types", "GET")
+        self.tags = Route(self.base_image + "tags", "GET")
+        self.random = Route(self.base_image + "random", "GET")
+        self.image = Route(self.base_image + "info/{}", "GET", True)
+        self.image_add_tags = Route(self.base_image + "info/{}/tags", "POST", True)
+        self.image_remove_tags = Route(self.base_image + "info/{}/tags", "DELETE", True)
+        self.image_remove = Route(self.base_image + "info/{}", "DELETE", True)
