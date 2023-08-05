63eeaa6b1b92d89f4f1513f2537af6d0016fda12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:50 +0000

    Add package malicense/0.0.6/malicense-0.0.6-py3-none-any.whl

---
+++ packages/malicense/malicense-0.0.6-py3-none-any.whl/malicense/__init__.py
@@ -0,0 +1,10 @@
+''' malicense.__init__: executed when malicense is imported '''
+
+__version__ = '0.0.6'
+version = __version__
+
+# Example of use in __init__.py file
+# import malicense
+# from .user import main
+# main(package=malicense, licfilename='LICENSE.txt')
+
---
+++ packages/malicense/malicense-0.0.6-py3-none-any.whl/malicense/__main__.py
@@ -0,0 +1,4 @@
+''' malicense.__main__: executed when malicense called as a script '''
+
+from .user import cmMain
+cmMain()
