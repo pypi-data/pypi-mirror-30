2e0341128b795d2e337b390f2d56b7a358eb1c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:46 +0000

    Add package laurelin-ldap/1.2.0/laurelin-ldap-1.2.0.tar.gz

---
+++ packages/laurelin-ldap/laurelin-ldap-1.2.0.tar.gz/laurelin-ldap-1.2.0/tests/utils.py
@@ -0,0 +1,52 @@
+from laurelin.ldap import attributetype, objectclass, rules
+from importlib import import_module
+
+
+def get_reload():
+    try:
+        return reload
+    except NameError:
+        try:
+            from imp import reload
+            return reload
+        except ImportError:
+            from importlib import reload
+            return reload
+
+
+reload = get_reload()
+
+
+def clear_attribute_types():
+    attributetype._name_attribute_types.clear()
+    attributetype._oid_attribute_types.clear()
+
+
+def clear_object_classes():
+    objectclass._name_object_classes.clear()
+    objectclass._oid_object_classes.clear()
+
+
+def clear_rules():
+    rules._oid_syntax_rules.clear()
+    rules._oid_syntax_rule_objects.clear()
+    rules._oid_matching_rules.clear()
+    rules._name_matching_rules.clear()
+    rules._oid_matching_rule_objects.clear()
+    rules._name_matching_rule_objects.clear()
+
+
+def clear_schema_registrations():
+    clear_attribute_types()
+    clear_object_classes()
+    clear_rules()
+
+
+def load_schema():
+    schema_mod = 'laurelin.ldap.schema'
+    clear_schema_registrations()
+    schema = import_module(schema_mod)
+    clear_schema_registrations()
+    reload(schema)
+    return import_module(schema_mod)
+
