74beaf41d0bb866ef0a59a09349ebdfd6b4fead8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:50 +0000

    Add package weebapi/0.1.4b0/weebapi-0.1.4b0-py3-none-any.whl

---
+++ packages/weebapi/weebapi-0.1.4b0-py3-none-any.whl/weebapi/errors.py
@@ -0,0 +1,18 @@
+class RequireFormatting(Exception):
+    pass
+
+
+class MissingRequiredArguments(Exception):
+    pass
+
+
+class WeirdResponse(Exception):
+    pass
+
+
+class SpecifyClient(Exception):
+    pass
+
+
+class Forbidden(Exception):
+    pass
