603a681ef2e6bb1c601b14165aec8bc08e273ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:17 +0000

    Add package RTool/0.0.2.1.3/RTool-0.0.2.1.3.tar.gz

---
+++ packages/RTool/RTool-0.0.2.1.3.tar.gz/RTool-0.0.2.1.3/setup.py
@@ -0,0 +1,17 @@
+tag = '0.0.2.1.3'
+from setuptools import setup, find_packages
+#from distutils.core import setup
+setup(
+  name = 'RTool',
+  packages = find_packages(),#['RTool'], # this must be the same as the name above
+  version = '%s'%tag,
+  description = 'A random test lib',
+  author = 'Ron Nofar',
+  author_email = 'ronnofar2@gmail.com',
+  url = 'https://github.com/RonNofar/RToolPackage', # use the URL to the github repo
+  download_url = 'https://github.com/RonNofar/RToolPackage/archive/%s.tar.gz'%tag, # I'll explain this in a second
+  keywords = ['RTool', 'win32', 'Maya'], # arbitrary keywords
+  classifiers = [],
+)
+
+#http://wiki.python.org/moin/Distutils/Cookbook/AutoPackageDiscovery
