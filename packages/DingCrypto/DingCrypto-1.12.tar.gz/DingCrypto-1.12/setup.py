19122458361e39b7e0e9e19f95d8036f128109f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:25 +0000

    Add package DingCrypto/1.12/DingCrypto-1.12.tar.gz

---
+++ packages/DingCrypto/DingCrypto-1.12.tar.gz/DingCrypto-1.12/setup.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='DingCrypto',
+    version='1.12',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='xingege',
+    author_email='shuizhengqi1@163.com',
+    description=u'dingtalk message encrypt and decrypt'
+)
