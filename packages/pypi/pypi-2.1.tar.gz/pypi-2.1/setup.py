d5cf620b3fa638f4867631ba236b16ccd120ddb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:12 +0000

    Add package pypi/2.1/pypi-2.1.tar.gz

---
+++ packages/pypi/pypi-2.1.tar.gz/pypi-2.1/setup.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+#
+# $Id$
+
+from distutils.core import setup
+
+# perform the setup action
+setup(
+    name = "pypi",
+    version = '2.1',
+    description = "PyPI is the Python Package Index at http://pypi.org/",
+    long_description = '''
+PyPI has a new home at <http://pypi.org/>. Users should need not need to change
+anything, as the old "www" address should still work. Use of the new
+address in preference is encouraged.
+
+Developers interested in how PyPI works, or in contributing to the project,
+should visit http://wiki.python.org/moin/CheeseShopDev
+    ''',
+    author = "Warehouse Maintainers",
+    author_email = 'richard@python.org',
+    url = 'http://wiki.python.org/moin/CheeseShopDev',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
+
+# vim: set filetype=python ts=4 sw=4 et si
