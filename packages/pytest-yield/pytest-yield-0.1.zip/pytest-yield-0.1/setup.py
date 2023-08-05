c7e639e6d889a7868fcd49e83d73a5874109d19d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:06 +0000

    Add package pytest-yield/0.1/pytest-yield-0.1.zip

---
+++ packages/pytest-yield/pytest-yield-0.1.zip/pytest-yield-0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='pytest-yield',
+    version='0.1',
+    description='PyTest plugin to run tests concurrently, each `yield` switch context to other test',
+    long_description=open("README.md").read(),
+    author='Volodymyr Trotsyshyn',
+    author_email='devova@gmail.com',
+    url='https://github.com/devova/pytest-yield',
+    download_url='https://github.com/devova/pytest-yield/archive/0.1.tar.gz',
+    py_modules=['pytest_yield'],
+    install_requires=["pytest>=3.0"],
+    keywords=['testing'],
+    classifiers=[],
+    entry_points={
+        'pytest11': [
+            'pytest_yield = pytest_yield.plugin',
+        ]
+    }
+)
