51d96d85437aa067e8796b47f44f77d5b62dcba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:39 +0000

    Add package oulinbao/0.0.8/oulinbao-0.0.8.tar.gz

---
+++ packages/oulinbao/oulinbao-0.0.8.tar.gz/oulinbao-0.0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from os import path
+from codecs import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='oulinbao',
+    version='0.0.8',
+    description='oulinbao test pypi',
+    long_description=long_description,
+    author='oulinbao',
+    url='https://pypi.org/project/oulinbao/',
+    scripts=[],
+    packages=find_packages(),
+    install_requires=[],
+
+    classifiers=[  # Optional
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+)
