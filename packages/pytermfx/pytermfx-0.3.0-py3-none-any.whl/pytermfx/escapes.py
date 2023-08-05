649f567e6b07347c11d23a9c2d950c0917131b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:25 +0000

    Add package pytermfx/0.3.0/pytermfx-0.3.0-py3-none-any.whl

---
+++ packages/pytermfx/pytermfx-0.3.0-py3-none-any.whl/pytermfx/escapes.py
@@ -0,0 +1,121 @@
+from pytermfx.pytermfx import *
+from pytermfx.keys import *
+
+"""Supports escape sequences for keyboard input.
+Be forewarned: this is messy and not portable.
+It supports most of the keys I have on my keyboard.
+"""
+
+KEY_MAP = {}
+KEY_MAP.update({chr(i + 1): Key(chr(97 + i), ctrl=True) for i in range(26)})
+del KEY_MAP[chr(9)] # unfortunately ctrl+i is the same as tab and we have no other way
+                    # to recognize this.
+
+ESC_MAP = {
+	"[": {
+		"A": KEY_UP,
+		"B": KEY_DOWN,
+		"C": KEY_RIGHT,
+		"D": KEY_LEFT,
+		"a": KEY_UP + MOD_SHIFT,
+		"b": KEY_DOWN + MOD_SHIFT,
+		"c": KEY_RIGHT + MOD_SHIFT,
+		"d": KEY_LEFT + MOD_SHIFT,
+		"Z": KEY_TAB + MOD_SHIFT,
+		"1": {
+			"1": {"~": KEY_F1, "^": KEY_F1 + MOD_CTRL},
+			"2": {"~": KEY_F2, "^": KEY_F2 + MOD_CTRL},
+			"3": {"~": KEY_F3, "^": KEY_F3 + MOD_CTRL},
+			"4": {"~": KEY_F4, "^": KEY_F4 + MOD_CTRL},
+			"5": {"~": KEY_F5, "^": KEY_F5 + MOD_CTRL},
+			"7": {"~": KEY_F6, "^": KEY_F6 + MOD_CTRL},
+			"8": {"~": KEY_F7, "^": KEY_F7 + MOD_CTRL},
+			"9": {"~": KEY_F8, "^": KEY_F8 + MOD_CTRL}
+		},
+		"2": {
+			"~": KEY_INS,
+			"$": KEY_INS + MOD_SHIFT,
+			"^": KEY_INS + MOD_CTRL,
+			"@": KEY_INS + MOD_CTRL + MOD_SHIFT,
+			"0": {"~": KEY_F9, "^": KEY_F9 + MOD_CTRL},
+			"1": {"~": KEY_F10, "^": KEY_F10 + MOD_CTRL},
+			"3": {"~": KEY_F11, "^": KEY_F11 + MOD_CTRL},
+			"4": {"~": KEY_F12, "^": KEY_F12 + MOD_CTRL}
+		},
+		"3": {
+			"~": KEY_DEL, 
+			"$": KEY_DEL + MOD_SHIFT,
+			"^": KEY_DEL + MOD_CTRL,
+			"@": KEY_DEL + MOD_CTRL + MOD_SHIFT,
+		},
+		"7": {
+			"~": KEY_HOME,
+			"$": KEY_HOME + MOD_SHIFT,
+			"^": KEY_HOME + MOD_CTRL,
+			"@": KEY_HOME + MOD_CTRL + MOD_SHIFT,
+		},
+		"8": {
+			"~": KEY_END,
+			"$": KEY_END + MOD_SHIFT,
+			"^": KEY_END + MOD_CTRL,
+			"@": KEY_END + MOD_CTRL + MOD_SHIFT,
+		},
+		"5": {
+			"~": KEY_PGUP,
+			"$": KEY_PGUP + MOD_SHIFT,
+			"^": KEY_PGUP + MOD_CTRL,
+			"@": KEY_PGUP + MOD_CTRL + MOD_SHIFT,
+		},
+		"6": {
+			"~": KEY_PGDN,
+			"$": KEY_PGDN + MOD_SHIFT,
+			"^": KEY_PGDN + MOD_CTRL,
+			"@": KEY_PGDN + MOD_CTRL + MOD_SHIFT,
+		}
+	},
+	"O": {
+		"a": KEY_UP + MOD_CTRL,
+		"b": KEY_DOWN + MOD_CTRL,
+		"c": KEY_RIGHT + MOD_CTRL,
+		"d": KEY_LEFT + MOD_CTRL,
+	}
+}
+ESC_MAP[ESC] = MOD_ALT
+
+def read_escape(file):
+	first = file.read(1)
+	if first != ESC:
+		if first in KEY_MAP:
+			return KEY_MAP[first]
+		return Key(first)
+
+	mod = MOD_NONE
+	skip_once = True
+	c = file.read(1)
+	c_buffer = []
+	m = ESC_MAP
+
+	# special case alt+alpha
+	if c not in m:
+		if c in KEY_MAP:
+			return KEY_MAP[c] + MOD_ALT
+		return Key(c) + MOD_ALT
+
+	# iterative case: escape sequence
+	while isinstance(m, dict):
+		if not skip_once:
+			c = file.read(1)
+		else:
+			skip_once = False
+		c_buffer.append(c)
+		if c not in m:
+			break
+
+		if type(m[c]) == Key:
+			return m[c] + mod
+		elif type(m[c]) == Mod:
+			mod += m[c]
+		else:
+			m = m[c]
+
+	raise ValueError("Unsupported escape sequence: " + "".join(c_buffer))
---
+++ packages/pytermfx/pytermfx-0.3.0-py3-none-any.whl/pytermfx/terminal.py
@@ -0,0 +1,251 @@
+from pytermfx.pytermfx import *
+from pytermfx.color import Color, ColorMode
+from pytermfx.escapes import read_escape
+import subprocess
+import signal
+import sys
+import termios
+import tty
+
+class Terminal:
+	def __init__(self, input_file=sys.stdin, output_file=sys.stdout):
+		self.in_file = input_file
+		self.out_file = output_file
+		self._buffer = []
+		
+		signal.signal(signal.SIGWINCH, self._handle_resize)
+		self._resize_handlers = []
+		self.add_resize_handler(self.update_size)
+		self.update_size()
+		
+		try:
+			self._original_attr = termios.tcgetattr(sys.stdin)
+		except termios.error:
+			self._original_attr = None
+		self._cbreak = False
+		self._color_mode = ColorMode.MODE_256
+		self._cursor_visible = False
+
+	def set_cbreak(self, cbreak=True):
+		"""Enable or disable cbreak mode.
+		"""
+		assert(self._original_attr != None)
+		termios.tcsetattr(self.in_file, termios.TCSADRAIN, self._original_attr)
+		if cbreak:
+			tty.setcbreak(self.in_file.fileno())
+		self._cbreak = cbreak
+
+	def set_color_mode(self, mode):
+		"""Change the color mode of the terminal.
+		The color mode determines what kind of ANSI sequences are used to
+		set colors. See ColorMode for more details.
+		"""
+		if not isinstance(mode, ColorMode):
+			raise ValueError("mode must be a ColorMode.")
+		self._color_mode = mode
+
+	def _handle_resize(self, signum=0, frame=None):
+		for h in self._resize_handlers:
+			h()
+
+	def add_resize_handler(self, func):
+		"""Adds a handler for terminal resize.
+		"""
+		self._resize_handlers.append(func)
+
+	def update_size(self, defaults=None):
+		"""Retrieve and store the dimensions of the terminal window.
+		Sets self.w and self.h with current data if possible.
+		Raises an exception if no size detection method works.
+		"""
+		try:
+			size = subprocess.check_output("stty size", shell=True).split(" ")
+			self.w = int(size[0])
+			self.h = int(size[1])
+			return
+		except:
+			pass
+
+		try:
+			self.w = int(subprocess.check_output("tput cols", shell=True))
+			self.h = int(subprocess.check_output("tput lines", shell=True))
+			return
+		except:
+			pass
+
+		if defaults:
+			self.w, self.h = defaults
+			return
+		
+		raise RuntimeError("No suitable method to get terminal size.")
+
+	def getch(self):
+		"""Get a single character from stdin in cbreak mode.
+		Blocks until the user performs an input. Only works if cbreak is on.
+		"""
+		if not self._cbreak:
+			raise ValueError("Must be in cbreak mode.")
+		return read_escape(self.in_file)
+
+	def getch_raw(self):
+		"""Get a single character from stdin in cbreak mode.
+		Does not decode escape sequences.
+		Blocks until the user performs an input. Only works if cbreak is on.
+		"""
+		if not self._cbreak:
+			raise ValueError("Must be in cbreak mode.")
+		return self.in_file.read(1)
+
+	def write(self, *things):
+		"""Write an arbitrary number of things to the buffer.
+		"""
+		self._buffer += map(lambda i: str(i), things)
+		return self
+
+	def writeln(self, *things):
+		"""Writes an arbitrary number of things to the buffer with a newline.
+		"""
+		self.write(*things, "\n")
+		return self
+
+	def flush(self):
+		"""Flush the buffer to the terminal.
+		"""
+		print("".join(self._buffer), end="", file=self.out_file, flush=True)
+		self._buffer = []
+
+	def print(self, *things, sep="", end="\n"):
+		"""Acts like Python's print(). Forces a flush.
+		"""
+		self.write(sep.join(things), end).flush()
+
+	def clear(self):
+		"""Clear the screen.
+		"""
+		self.write(CSI, "2J")
+		return self
+
+	def clear_box(self, x, y, w, h):
+		"""Clears a region of the terminal
+		"""
+		for i in range(int(y), int(y+h)):
+			self.cursor_to(int(x), i)
+			self.write(" " * w)
+		return self
+
+	def clear_line(self):
+		"""Clear the line and move cursor to start
+		"""
+		self.cursor_to_start()
+		self.write(CSI, "2K")
+		return self
+
+	def clear_to_end(self):
+		"""Clear to the end of the line
+		"""
+		self.write(CSI, "0K")
+		return self
+
+	def reset(self):
+		"""Clean up the terminal state before exiting.
+		"""
+		self.write(ESC, "c") # reset state
+		self.cursor_to(0, 0)
+		self.clear()
+		self.flush()
+
+	def cursor_set_visible(self, visible=True):
+		"""Change the cursor visibility.
+		visible may be True or False.
+		"""
+		self.write(CSI, "?25", "h" if visible else "l")
+		self._cursor_visible = visible
+		return self
+
+	def cursor_get_pos(self):
+		"""Retrieve the current (x,y) cursor position.
+		This is slow, so avoid using when unnecessary.
+		"""
+		old_status = self._cbreak
+		if not self._cbreak:
+			self.set_cbreak(True)
+		
+		# write DSR (device status report)
+		self.write(CSI, "6n")
+		self.flush()
+
+		# read result from stdin
+		buf = []
+		while self.getch_raw() != "[": # skip start
+			pass
+		c = ""
+		while c != "R": # read until end
+			buf.append(c)
+			c = self.getch_raw()
+		parts = "".join(buf).split(";")
+
+		# restore old cbreak
+		if not old_status:
+			self.set_cbreak(False)
+
+		return (int(parts[1]) - 1, int(parts[0]) - 1)
+
+	def cursor_save(self):
+		"""Save the cursor position
+		"""
+		self.write(CSI, "s")
+		return self
+
+	def cursor_restore(self):
+		"""Restore the cursor position
+		"""
+		self.write(CSI, "u")
+		return self
+
+	def cursor_to(self, x, y):
+		"""Move the cursor to an absolute position.
+		"""
+		self.write(CSI, int(y+1), ";", int(x+1), "H")
+		return self
+
+	def cursor_to_x(self, x):
+		"""Move the cursor to a given column on the same line.
+		"""
+		self.write(CSI, int(x+1), "G")
+		return self
+
+	def cursor_move(self, x, y):
+		"""Move the cursor by a given amount.
+		"""
+		if x < 0:
+			self.write(CSI, abs(int(x)), "D")
+		elif x > 0:
+			self.write(CSI, int(x), "C")
+		if y < 0:
+			self.write(CSI, abs(int(x)), "A")
+		elif y > 0:
+			self.write(CSI, int(x), "B")
+		return self
+
+	def cursor_to_start(self):
+		"""Move the cursor to the start of the line.
+		"""
+		self.write(CSI, "1G")
+		return self
+
+	def style(self, *styles):
+		"""Apply styles, which may be a Color or something with .ansi()
+		Accepts a Color or a Style.
+		"""
+		for style in styles:
+			if isinstance(style, Color):
+				self.write(style.to_mode(self._color_mode))
+			else:
+				self.write(style.ansi())
+		return self
+
+	def style_reset(self):
+		"""Reset style.
+		"""
+		self.write(CSI, "0m")
+		return self
