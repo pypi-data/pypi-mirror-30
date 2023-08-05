d6af67aef02ac9279ccd375aff94b6b2e14c90f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:51 +0000

    Add package scikit-cycling/0.1.3/scikit_cycling-0.1.3-cp35-cp35m-win32.whl

---
+++ packages/scikit-cycling/scikit_cycling-0.1.3-cp35-cp35m-win32.whl/skcycling/_version.py
@@ -0,0 +1,5 @@
+# Authors: Guillaume Lemaitre <g.lemaitre58@gmail.com>
+#          Cedric Lemaitre
+# License: BSD 3 clause
+
+__version__ = '0.1.3'
---
+++ packages/scikit-cycling/scikit_cycling-0.1.3-cp35-cp35m-win32.whl/skcycling/io/__init__.py
@@ -0,0 +1,11 @@
+"""
+The :mod:`skcycling.io` module includes utility to load data.
+"""
+
+# Authors: Guillaume Lemaitre <g.lemaitre58@gmail.com>
+#          Cedric Lemaitre
+# License: BSD 3 clause
+
+from .base import bikeread
+
+__all__ = ['bikeread']
