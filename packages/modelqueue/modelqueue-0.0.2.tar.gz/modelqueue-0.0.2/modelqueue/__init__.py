ffacd02906e32d773b81a538705de24b4c9e2078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:14 +0000

    Add package modelqueue/0.0.2/modelqueue-0.0.2.tar.gz

---
+++ packages/modelqueue/modelqueue-0.0.2.tar.gz/modelqueue-0.0.2/modelqueue/__init__.py
@@ -0,0 +1,34 @@
+"ModelQueue: task queue based on Django models."
+
+class StatusField:
+    """Model queue status field.
+
+    TODO
+
+    """
+    # pylint: disable=too-few-public-methods
+    pass
+
+
+def run(queryset, field_name, func):
+    """Queue runner.
+
+    TODO
+
+    """
+    # pylint: disable=unused-argument
+    pass
+
+
+__all__ = [
+    'StatusField',
+    'run',
+]
+
+
+__title__ = 'modelqueue'
+__version__ = '0.0.2'
+__build__ = 0x000002
+__author__ = 'Grant Jenks'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2018 Grant Jenks'
