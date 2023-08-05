8e25bf3d64feb1fb39ea1f89ee1f2ee9fc0fd61e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:34 +0000

    Add package amazon_review_scraper/0.1.2/amazon_review_scraper-0.1.2.tar.gz

---
+++ packages/amazon_review_scraper/amazon_review_scraper-0.1.2.tar.gz/amazon_review_scraper-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'amazon_review_scraper',
+  packages = ['amazon_review_scraper'],
+  version = '0.1.2',
+  description = 'A library to scrape Amazon Reviews',
+  author = 'Shreyas S',
+  author_email = 'hello@shreyas.im',
+  url = 'https://github.com/shreyas707/Amazon-Review-Scraper',
+  download_url = 'https://github.com/shreyas707/Amazon-Review-Scraper/archive/0.1.1.tar.gz',
+  keywords = ['testing'],
+  classifiers = [],
+)
