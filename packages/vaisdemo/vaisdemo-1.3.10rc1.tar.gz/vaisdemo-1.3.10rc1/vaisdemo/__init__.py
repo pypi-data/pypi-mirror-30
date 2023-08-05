612ae988d91f22c72901dd56619cb9422af53436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:56 +0000

    Add package vaisdemo/1.3.10rc1/vaisdemo-1.3.10rc1.tar.gz

---
+++ packages/vaisdemo/vaisdemo-1.3.10rc1.tar.gz/vaisdemo-1.3.10rc1/vaisdemo/__init__.py
@@ -0,0 +1,8 @@
+import sys
+def runapp():
+    import vaisdemo.main_mic as main_mic
+    main_mic.main()
+
+def runapp_video():
+    import vaisdemo.main_video as main_video
+    main_video.main()
---
+++ packages/vaisdemo/vaisdemo-1.3.10rc1.tar.gz/vaisdemo-1.3.10rc1/vaisdemo/info/info.py
@@ -0,0 +1,45 @@
+import sys
+import os
+import time
+import ctypes
+import platform
+import traceback
+
+class SystemInformation:
+    def __init__(self):
+        self.os = self._os_version().strip()
+        self.cpu = self._cpu().strip()
+        self.node = platform.node()
+
+    def _os_version(self):
+        return platform.platform()
+
+    def _cpu(self):
+        return platform.machine()
+
+def get_system_info(api_key):
+    import pygsheets
+    import ipgetter
+    import io
+
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    key_file = os.path.join(root_dir, "key.json")
+
+    f = io.StringIO()
+
+    for i in range(4):
+        try:
+            gc = pygsheets.authorize(service_file=key_file, no_cache=True)
+            sh = gc.open_by_key("1sVjzu5WasiOKXBOjMWrPzKcXFd-NZ4Gt6CVyyrZa15Y").worksheet('index', 0)
+
+            data = [api_key]
+            ip = ipgetter.myip()
+            data.append(ip)
+            s = SystemInformation()
+            data += [s.os, s.cpu]
+            sh.insert_rows(row=1, values=data)
+            break
+        except Exception as e:
+            traceback.print_exc(file=sys.stdout)
+            print("=== retry ===")
+            time.sleep(1)
