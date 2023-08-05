650c2d9a7919cf11dc0d948b7cab9751e71ae40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:42 +0000

    Add package bulma/1.0.1/bulma-1.0.1.tar.gz

---
+++ packages/bulma/bulma-1.0.1.tar.gz/bulma-1.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/24 上午9:11
+# @Author  : Dirk Zhao
+from distutils.core import setup
+
+setup(
+    name='bulma',
+    version='1.0.1',
+    py_modules=['HttpClient', 'Log'],
+    author='Dirk Zhao',
+    author_email='732015168@qq.com',
+    description='修改logger传入参数',
+)
