a97d29858256f8f9fc4d8191c69276d8e468bb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:10 +0000

    Add package django-prices-openexchangerates/1.0.1/django-prices-openexchangerates-1.0.1.tar.gz

---
+++ packages/django-prices-openexchangerates/django-prices-openexchangerates-1.0.1.tar.gz/django-prices-openexchangerates-1.0.1/setup.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+import os
+from setuptools import setup
+
+CLASSIFIERS = [
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Software Development :: Libraries :: Python Modules']
+
+setup(
+    name='django-prices-openexchangerates',
+    author='Mirumee Software',
+    author_email='hello@mirumee.com',
+    description='openexchangerates.org support for django-prices',
+    license='BSD',
+    version='1.0.1',
+    url='https://github.com/mirumee/django-prices-openexchangerates',
+    packages=[
+        'django_prices_openexchangerates',
+        'django_prices_openexchangerates.management',
+        'django_prices_openexchangerates.management.commands',
+        'django_prices_openexchangerates.migrations',
+        'django_prices_openexchangerates.templatetags'],
+    include_package_data=True,
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'Django>=1.11', 'django-prices>=1.0.0', 'prices>=1.0.0'],
+    platforms=['any'],
+    tests_require=['mock==1.0.1', 'pytest'],
+    zip_safe=False)
