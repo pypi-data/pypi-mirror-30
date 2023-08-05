4ac4fa80a35744fd18aff0e1bc59228407ef23a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:17 +0000

    Add package skelethon/0.0.1.dev1/skelethon-0.0.1.dev1.tar.gz

---
+++ packages/skelethon/skelethon-0.0.1.dev1.tar.gz/skelethon-0.0.1.dev1/skelethon/__main__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Primary entrypoint into the package."""
+
+from __future__ import absolute_import
+
---
+++ packages/skelethon/skelethon-0.0.1.dev1.tar.gz/skelethon-0.0.1.dev1/skelethon/version.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Define the package version.
+
+The version set here will be automatically incorporated into setup.py
+and also set as the __version__ attribute for the package.
+"""
+
+from __future__ import unicode_literals
+
+__version_info__ = (0, 0, 1)
+__version__ = '.'.join([str(ver) for ver in __version_info__])
