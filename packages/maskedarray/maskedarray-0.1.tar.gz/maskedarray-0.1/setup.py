ea0de7b9e8dd1aeaf269063bc971e4a5211db33b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:11 +0000

    Add package maskedarray/0.1/maskedarray-0.1.tar.gz

---
+++ packages/maskedarray/maskedarray-0.1.tar.gz/maskedarray-0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+  name = 'maskedarray',
+  packages = ['maskedarray'],
+  version = '0.1',
+  description = 'NumPy Masked Array',
+  author = 'NumPy maintainers',
+  author_email = 'stefanv@berkeley.edu',
+  url = 'http://www.numpy.org',
+  classifiers = [],
+)
