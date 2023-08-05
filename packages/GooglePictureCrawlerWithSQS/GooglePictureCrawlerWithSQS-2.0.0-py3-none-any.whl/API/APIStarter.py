4687706869f9bc9a436228a385dbec0ab77aaebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:10 +0000

    Add package GooglePictureCrawlerWithSQS/2.0.0/GooglePictureCrawlerWithSQS-2.0.0-py3-none-any.whl

---
+++ packages/GooglePictureCrawlerWithSQS/GooglePictureCrawlerWithSQS-2.0.0-py3-none-any.whl/API/APIStarter.py
@@ -0,0 +1,20 @@
+#https://www.codementor.io/sagaragarwal94/building-a-basic-restful-api-in-python-58k02xsiq
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+from API.APICrawlerManager import APICrawlerManager
+from API.APICrawlerManager import APIConcreteCrawlerManager
+
+
+class APIStarter(object):
+
+     @staticmethod
+     def start():
+          app = Flask(__name__)
+          api = Api(app)
+
+          api.add_resource(APICrawlerManager, '/crawler')
+          api.add_resource(APIConcreteCrawlerManager, '/crawler/<crawler_id>')
+
+          #if __name__ == '__main__':
+          app.run(port=5002)
