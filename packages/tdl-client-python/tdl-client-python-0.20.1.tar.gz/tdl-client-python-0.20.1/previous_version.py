034950cb3045a576e9e01f9bd14670cff476cf99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:15 +0000

    Add package tdl-client-python/0.20.1/tdl-client-python-0.20.1.tar.gz

---
+++ packages/tdl-client-python/tdl-client-python-0.20.1.tar.gz/tdl-client-python-0.20.1/previous_version.py
@@ -0,0 +1,10 @@
+PREVIOUS_VERSION = '0.19.3'
+
+
+def main():
+    print PREVIOUS_VERSION
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ packages/tdl-client-python/tdl-client-python-0.20.1.tar.gz/tdl-client-python-0.20.1/src/tdl/runner/runner_action.py
@@ -0,0 +1,16 @@
+class RunnerAction:
+    def __init__(self, short_name, name, client_action):
+        self.short_name = short_name
+        self.name = name
+        self.client_action = client_action
+
+
+class RunnerActions:
+
+    get_new_round_description = RunnerAction("new", "get_new_round_description", "stop")
+    deploy_to_production = RunnerAction("deploy", "deploy_to_production", "publish")
+
+    all = [
+        get_new_round_description,
+        deploy_to_production,
+    ]
