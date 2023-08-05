fcb23b81748d3cafb5368aaba610a9692739df30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:30 +0000

    Add package splitnjoin/0.65/splitnjoin-0.65.tar.gz

---
+++ packages/splitnjoin/splitnjoin-0.65.tar.gz/splitnjoin-0.65/setup.py
@@ -0,0 +1,16 @@
+# Written by Sergio La Rosa (sergio.larosa89@gmail.com)
+# Part of "splitnjoin" package
+# https://github.com/SergioLaRosa/splitnjoin
+
+from setuptools import setup
+
+setup(
+    name='splitnjoin',
+    version='0.65',
+    description='Simple module for splitting binary files into multiple chunks and viceversa (from chunks to original file)',
+    url='https://github.com/SergioLaRosa/splitnjoin',
+    author='Sergio La Rosa',
+    author_email='sergio.larosa89@gmail.com',
+    license='MIT',
+    packages=['splitnjoin'],
+    zip_safe=False)
