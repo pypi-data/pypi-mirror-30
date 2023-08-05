5bd45edb7aa542e109501a8041de7f713312f692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:38 +0000

    Add package coinsmarketcap/0.15/coinsmarketcap-0.15.tar.gz

---
+++ packages/coinsmarketcap/coinsmarketcap-0.15.tar.gz/coinsmarketcap-0.15/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'coinsmarketcap',
+  packages = ['coinsmarketcap'],
+  version = '0.15',
+  description = 'CoinMarketCap.COM parser',
+  author = 'Giorgi Anakidze',
+  author_email = 'giorgianakidze@outlook.com',
+  url = 'https://github.com/gpskwlkr/coinmarketcap-parser-py', 
+  download_url = 'https://github.com/gpskwlkr/coinmarketcap-parser-py/releases',
+  keywords = ['parsing','bitcoin','cryptocurrency'],
+  classifiers = [],
+)
