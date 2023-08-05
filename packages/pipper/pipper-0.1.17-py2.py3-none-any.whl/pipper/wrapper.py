df6a7cef5552f6ecb3b5e540a9c1af1b8bd3463b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:29 +0000

    Add package pipper/0.1.17/pipper-0.1.17-py2.py3-none-any.whl

---
+++ packages/pipper/pipper-0.1.17-py2.py3-none-any.whl/pipper/wrapper.py
@@ -0,0 +1,44 @@
+import sys
+import pip
+import pkg_resources
+import semver
+
+from pipper import versioning
+
+
+def update_required(package_name: str, install_version: str) -> bool:
+    """ """
+
+    existing = status(package_name)
+
+    if not existing:
+        return True
+
+    version = (
+        versioning.deserialize(install_version)
+        if install_version.startswith('v') else
+        install_version
+    )
+
+    result = semver.compare(existing.version, version)
+    return result != 0
+
+
+def status(package_name: str):
+    """ """
+
+    try:
+        return pkg_resources.get_distribution(package_name)
+    except pkg_resources.DistributionNotFound:
+        return None
+    except Exception:
+        raise
+
+
+def install_wheel(wheel_path: str, to_user: bool = False):
+    """ """
+
+    cmd = ['install', wheel_path]
+    cmd += ['--user'] if to_user else []
+    print('COMMAND:', ' '.join(cmd))
+    pip.main(cmd)
