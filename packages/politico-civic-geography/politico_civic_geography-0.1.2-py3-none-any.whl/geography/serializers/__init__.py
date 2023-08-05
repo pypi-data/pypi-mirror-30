3eb5391bba7891bde8d30949bde91afb46d811a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:23 +0000

    Add package politico-civic-geography/0.1.2/politico_civic_geography-0.1.2-py3-none-any.whl

---
+++ packages/politico-civic-geography/politico_civic_geography-0.1.2-py3-none-any.whl/geography/serializers/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+from .division import DivisionSerializer
+from .division_level import DivisionLevelSerializer
+from .geometry import GeometrySerializer
+from .geometry_list import GeometryListSerializer
