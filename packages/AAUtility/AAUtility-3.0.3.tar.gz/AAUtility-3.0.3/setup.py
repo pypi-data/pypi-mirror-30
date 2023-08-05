6a1b9fd632cd80f2ea144fc025f15a168f1e6d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:05 +0000

    Add package AAUtility/3.0.3/AAUtility-3.0.3.tar.gz

---
+++ packages/AAUtility/AAUtility-3.0.3.tar.gz/AAUtility-3.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'AAUtility',
+    version='3.0.3',
+    description='package utility function in common package',
+    url='https://github.com/gauravccloud/AAUtility.git',
+    author='Gaurav Sharma',
+    author_email='gauravdbdev@gmail.com',
+    license='MIT',
+    packages=['AAUtility'],
+    install_requires=[],
+    zip_safe=False
+)
