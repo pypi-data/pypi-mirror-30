385a771d1129c3545bc6934e6171a82cb3ac8b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:36 +0000

    Add package demultiplex/0.1.1/demultiplex-0.1.1.tar.gz

---
+++ packages/demultiplex/demultiplex-0.1.1.tar.gz/demultiplex-0.1.1/demultiplex/__init__.py
@@ -0,0 +1,32 @@
+"""demultiplex: Demultiplex any number of FASTA or a FASTQ files based on a
+list of barcodes.
+
+
+Format of the barcode file:
+name barcode
+
+Copyright (c) 2015 Leiden University Medical Center <humgen@lumc.nl>
+Copyright (c) 2015 Jeroen F.J. Laros <J.F.J.Laros@lumc.nl>
+
+Licensed under the MIT license, see the LICENSE file.
+"""
+from .demultiplex import Extractor, count, demultiplex
+
+
+__version_info__ = ('0', '1', '1')
+
+__version__ = '.'.join(__version_info__)
+__author__ = 'Jeroen F.J. Laros'
+__contact__ = 'J.F.J.Laros@lumc.nl'
+__homepage__ = 'https://github.com/jfjlaros/demultiplex'
+
+usage = __doc__.split('\n\n\n')
+
+
+def doc_split(func):
+    return func.__doc__.split('\n\n')[0]
+
+
+def version(name):
+    return '{} version {}\n\nAuthor   : {} <{}>\nHomepage : {}'.format(
+        name, __version__, __author__, __contact__, __homepage__)
