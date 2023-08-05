e9795dcb6c90e0451e9737760df70f9f5a391ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:15 +0000

    Add package pwclip/1.2.1/pwclip-1.2.1.linux-x86_64.tar.gz

---
+++ packages/pwclip/pwclip-1.2.1.linux-x86_64.tar.gz/usr/local/lib/python3.6/dist-packages/pwclip/lib/system/__init__.py
@@ -0,0 +1,9 @@
+from system.clips import clips, copy, paste
+
+from system.user import userfind, whoami
+
+from system.path import filetime, setfiletime, filerotate
+
+from system.xlib import xinput, xnotify, xyesno, xmsgok, xgetpass
+
+from system.which import which
