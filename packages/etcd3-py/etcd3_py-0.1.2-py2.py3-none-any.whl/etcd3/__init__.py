d2a82899a7274e1c8b3bc924a8353b45f0776ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:55 +0000

    Add package etcd3-py/0.1.2/etcd3_py-0.1.2-py2.py3-none-any.whl

---
+++ packages/etcd3-py/etcd3_py-0.1.2-py2.py3-none-any.whl/etcd3/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+"""Top-level package for etcd3-py."""
+import six
+
+from .version import __version__, __author__, __email__
+
+__all__ = ['__version__', '__author__', '__email__']
+
+from .client import Client
+
+AioClient = None
+if six.PY3: # pragma: no cover
+    from .aio_client import AioClient
+
+__all__.extend([
+    'Client',
+    'AioClient'
+])
+
+from .stateful import Txn
+
+__all__.extend([
+    'Txn'
+])
