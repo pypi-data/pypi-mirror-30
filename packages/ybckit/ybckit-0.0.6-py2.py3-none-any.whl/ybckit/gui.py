b4b17f5fa9c213b751f758d92e9b9edae200b217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:25 +0000

    Add package ybckit/0.0.6/ybckit-0.0.6-py2.py3-none-any.whl

---
+++ packages/ybckit/ybckit-0.0.6-py2.py3-none-any.whl/ybckit/gui.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+import os
+
+import easygui as eg
+import time
+
+from . import protocol
+
+
+def _is_under_ybc_env():
+    """
+    判断当前环境是否在猿编程环境下
+    :return:
+    """
+    return 'YBC_ENV' in os.environ and os.environ['YBC_ENV'] is not None
+
+
+def _wrap(method):
+    if not _is_under_ybc_env():
+        return method
+
+    def wrapped(*args, **kwargs):
+        _locals = locals()
+        request_id = protocol.send_request(method, _locals['args'], _locals['kwargs'])
+
+        while True:
+            raw_response = protocol.get_raw_response(request_id)
+            if raw_response is False:
+                time.sleep(0.1)
+                continue
+
+            return protocol.parse_response(raw_response)
+
+    return wrapped
+
+
+for _method in ['buttonbox', 'enterbox', 'passwordbox', 'msgbox', 'diropenbox', 'fileopenbox']:
+    globals()[_method] = _wrap(getattr(eg, _method))
+
+if __name__ == '__main__':
+    globals()['buttonbox'](msg='hello world')
