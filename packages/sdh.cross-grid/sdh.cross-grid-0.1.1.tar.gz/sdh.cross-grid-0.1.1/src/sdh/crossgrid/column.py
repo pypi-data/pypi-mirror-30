cad97e4e7719b20dc8fc5240acf22ac483abe9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:40 +0000

    Add package sdh.cross-grid/0.1.1/sdh.cross-grid-0.1.1.tar.gz

---
+++ packages/sdh.cross-grid/sdh.cross-grid-0.1.1.tar.gz/sdh.cross-grid-0.1.1/src/sdh/crossgrid/column.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+
+class Column(object):
+    def __init__(self, report_row, key):
+        self.key = key
+        self.report_row = report_row
+        self.value = None
+
+    def append(self, obj):
+        self.value = self.report_row.report.agg_function(obj, self.value)
