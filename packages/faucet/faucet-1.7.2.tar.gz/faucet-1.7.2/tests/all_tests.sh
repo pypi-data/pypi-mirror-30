fcc2dbe5edefc6a1b5205e4f7a04e2ae851401e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:34 +0000

    Add package faucet/1.7.2/faucet-1.7.2.tar.gz

---
+++ packages/faucet/faucet-1.7.2.tar.gz/faucet-1.7.2/tests/all_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+./sysctls_for_tests.sh
+
+export OVS_LOGDIR=/usr/local/var/log
+rm -rf /tmp/faucet*log /tmp/gauge*log /tmp/faucet-tests* /var/tmp/faucet-tests* $OVS_LOGDIR/* ; killall ryu-manager ; ./faucet_mininet_test.py -c ; /usr/local/share/openvswitch/scripts/ovs-ctl stop ; /usr/local/share/openvswitch/scripts/ovs-ctl start ; ./faucet_mininet_test.py $*
