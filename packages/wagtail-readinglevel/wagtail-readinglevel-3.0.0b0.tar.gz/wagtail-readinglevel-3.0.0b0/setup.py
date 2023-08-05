2ff3f9a59b57963f4e3b13960e921b1ea1f77865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:13 +0000

    Add package wagtail-readinglevel/3.0.0b0/wagtail-readinglevel-3.0.0b0.tar.gz

---
+++ packages/wagtail-readinglevel/wagtail-readinglevel-3.0.0b0.tar.gz/wagtail-readinglevel-3.0.0b0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+Installs the Wagtail readinglevel plugin that adds reading level check
+functionality to rich text editors.This helps when trying to create content
+that conforms to the WCAG 2.0
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='wagtail-readinglevel',
+      version='3.0.0b',
+      description='Determine reading age of a piece of text.',
+      url='http://github.com/vixdigital/wagtail-readinglevel',
+      author='VIX Digital',
+      author_email='info@vix.digital',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+          'wagtail>=1.0',
+      ],
+      zip_safe=False)
