d3b97b7571e2efccebb36d5c71fa6be6eecd609a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:11 +0000

    Add package openmesh/1.0.0.post28/openmesh-1.0.0.post28.tar.gz

---
+++ packages/openmesh/openmesh-1.0.0.post28.tar.gz/openmesh-1.0.0.post28/OpenMesh/CI/ci-cppcheck.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Exit script on any error
+set -e 
+
+#=====================================
+# Color Settings:
+#=====================================
+NC='\033[0m'
+OUTPUT='\033[0;32m'
+WARNING='\033[0;93m'
+
+echo -e "${OUTPUT}"
+echo "=============================================================================="
+echo "Running cppcheck"
+echo "=============================================================================="
+echo -e "${NC}"
+echo "Please Wait ..."
+
+# Run cppcheck and output into file
+cppcheck --enable=all . -I src -i Doc/ --force --suppress=unusedFunction --suppress=missingIncludeSystem --quiet -Umin -Umax -UBMPOSTFIX -DOPENMESHDLLEXPORT="" &> cppcheck.log
+
+echo -e "${OUTPUT}"
+echo "=============================================================================="
+echo "CPPCHECK Messages"
+echo "=============================================================================="
+echo -e "${NC}"
+
+
+# Echo output to command line for simple analysis via gitlab
+cat cppcheck.log
+
+COUNT=$(wc -l < cppcheck.log )
+
+echo -e "${OUTPUT}"
+echo "=============================================================================="
+echo "CPPCHECK Summary"
+echo "=============================================================================="
+echo -e "${NC}"
+
+MAX_COUNT=6
+
+if [ $COUNT -gt $MAX_COUNT ]; then
+  echo -e ${WARNING}
+  echo "Total CPPCHECK error Count is $COUNT, which is too High (max is $MAX_COUNT)! CPPCHECK Run failed";
+  echo -e "${NC}"
+  exit 1;
+else
+  echo "Total CPPCHECK error Count is $COUNT ... OK"
+fi
+
+ 
