e7b5cd142e21cb926896d52c51b6b9c3c208c286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:36 +0000

    Add package ArithSmcho/0.1/ArithSmcho-0.1-py2-none-any.whl

---
+++ packages/ArithSmcho/ArithSmcho-0.1-py2-none-any.whl/ArithSmcho/arithPython.py
@@ -0,0 +1,11 @@
+def add(x, y):
+    """
+    Adds values from two inputs and return the sum
+    :param x:
+    :param y:
+    :return: (x + y)
+
+    >>> add(10, 20)
+    30
+    """
+    return (x + y)
