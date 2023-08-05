6cb66fd373d224e1f1469af2d22c011bec371ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:21 +0000

    Add package thoth-storages/0.0.11/thoth-storages-0.0.11.tar.gz

---
+++ packages/thoth-storages/thoth-storages-0.0.11.tar.gz/thoth-storages-0.0.11/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+def get_install_requires():
+    with open('requirements.txt', 'r') as requirements_file:
+        # TODO: respect hashes in requirements.txt file
+        res = requirements_file.readlines()
+        return [req.split(' ', maxsplit=1)[0] for req in res if req]
+
+
+setup(
+    name='thoth-storages',
+    version='0.0.11',
+    description='Storage and database adapters available in project Thoth',
+    long_description='Storage and database adapters available in project Thoth',
+    author='Fridolin Pokorny',
+    author_email='fridolin@redhat.com',
+    license='GPLv2+',
+    packages=[
+        'thoth.storages',
+        'thoth.storages.graph'
+    ],
+    zip_safe=False,
+    install_requires=get_install_requires()
+)
