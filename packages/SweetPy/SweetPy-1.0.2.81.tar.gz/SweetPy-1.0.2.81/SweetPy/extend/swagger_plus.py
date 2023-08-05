53fc90d4c17d7cadcb218f813b5243d859853620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:46 +0000

    Add package SweetPy/1.0.2.81/SweetPy-1.0.2.81.tar.gz

---
+++ packages/SweetPy/SweetPy-1.0.2.81.tar.gz/SweetPy-1.0.2.81/SweetPy/extend/swagger_plus.py
@@ -0,0 +1,89 @@
+from django.conf.urls import url,RegexURLResolver,RegexURLPattern
+from rest_framework_swagger.views import get_swagger_view
+from django import conf
+from django.conf import settings
+from django.core.checks.urls import check_resolver
+from django.core.checks.registry import register,Tags
+import django.core.checks.urls
+
+
+schema_view = get_swagger_view(title=settings.SWEET_CLOUD_APPNAME + ' Restful API Documentation')
+
+conf.settings.INSTALLED_APPS.append('rest_framework_swagger')
+swagger_regex = RegexURLPattern('^swagger-ui.html$',schema_view)
+
+@register(Tags.urls)
+def check_url_config(app_configs, **kwargs):
+    if getattr(settings, 'ROOT_URLCONF', None):
+        from django.urls import get_resolver
+        resolver = get_resolver()
+        global swagger_regex
+        resolver.url_patterns.append(swagger_regex)
+        return check_resolver(resolver)
+    return []
+
+django.core.checks.urls.check_url_config = check_url_config
+
+
+import coreapi
+from coreapi.compat import force_bytes
+from openapi_codec import OpenAPICodec as _OpenAPICodec
+from openapi_codec.encode import generate_swagger_object
+from rest_framework.renderers import BaseRenderer, JSONRenderer
+from rest_framework import status
+import simplejson as json
+from rest_framework_swagger.settings import swagger_settings
+from ..func_plus import FuncHelper
+import platform
+import os
+class OpenAPICodec(_OpenAPICodec):
+    def encode(self, document, extra=None, **options):
+        if hasattr(settings, 'SWEET_SWAGGER_JSON_FILE') and (settings.SWEET_SWAGGER_JSON_FILE):
+            if platform.system().lower() == 'windows':
+                path = os.getcwd() + settings.SWEET_SWAGGER_JSON_FILE
+                path = path.replace('/','\\')
+            else:
+                path = os.getcwd() + settings.SWEET_SWAGGER_JSON_FILE
+                path = path.replace('\\', '/')
+            if FuncHelper.check_file_exists(path):
+                with open(path, 'r', encoding='utf8') as f:
+                    json_str = f.read()
+                    return force_bytes(json_str)
+            else:
+                raise Exception('SwaggerJsonFile ' + settings.SWEET_SWAGGER_JSON_FILE + ' Not Found!')
+        else:
+            if not isinstance(document, coreapi.Document):
+                raise TypeError('Expected a `coreapi.Document` instance')
+
+            data = generate_swagger_object(document)
+            if isinstance(extra, dict):
+                data.update(extra)
+
+            return force_bytes(json.dumps(data))
+
+class OpenAPIRenderer(BaseRenderer):
+    media_type = 'application/openapi+json'
+    charset = None
+    format = 'openapi'
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        if renderer_context['response'].status_code != status.HTTP_200_OK:
+            return JSONRenderer().render(data)
+        extra = self.get_customizations()
+        if platform.system().lower() == 'windows':
+            return OpenAPICodec().encode(data['data'], extra=extra)
+        else:
+            return OpenAPICodec().encode(data, extra=extra)
+
+    def get_customizations(self):
+        """
+        Adds settings, overrides, etc. to the specification.
+        """
+        data = {}
+        if swagger_settings.SECURITY_DEFINITIONS:
+            data['securityDefinitions'] = swagger_settings.SECURITY_DEFINITIONS
+
+        return data
+
+import rest_framework_swagger.renderers
+rest_framework_swagger.renderers.OpenAPIRenderer.render = OpenAPIRenderer.render
