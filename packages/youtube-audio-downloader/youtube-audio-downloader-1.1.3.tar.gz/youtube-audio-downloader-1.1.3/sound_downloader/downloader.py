c541d15221f8132b2327fb4ba3416c1dc6e28741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:07 +0000

    Add package youtube-audio-downloader/1.1.3/youtube-audio-downloader-1.1.3.tar.gz

---
+++ packages/youtube-audio-downloader/youtube-audio-downloader-1.1.3.tar.gz/youtube-audio-downloader-1.1.3/sound_downloader/downloader.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/python
+
+import pafy
+from .file_manager import FileManager
+
+
+class YoutubeAudioDownloader(object):
+    PATH_TO_SAVE = "."
+
+    def __init__(self, path_file, path_to_save=None):
+        self.path_file = path_file
+        self.path_to_save = path_to_save or self.PATH_TO_SAVE
+
+    @staticmethod
+    def _get_links(path_file):
+        return FileManager.get_links(path_file)
+
+    def download_audios(self):
+        links = self._get_links(self.path_file)
+        for link in links:
+            video = pafy.new(link)
+            bestaudio = video.getbestaudio()
+            bestaudio.download(quiet=False, filepath=self.path_to_save)
