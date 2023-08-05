d2fd0d2448074ab248b798678c8999523741dc24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:23 +0000

    Add package rafter/1.0.2/rafter-1.0.2.tar.gz

---
+++ packages/rafter/rafter-1.0.2.tar.gz/rafter-1.0.2/rafter/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '1.0.2'
+
+from .app import *         # noqa
+from .exceptions import *  # noqa
+from .http import *        # noqa
+from .schema import *      # noqa
