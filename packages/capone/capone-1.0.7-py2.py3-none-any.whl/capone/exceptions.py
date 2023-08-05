21fa0a683754f63aa9fac4d22b09c362a3adfb96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:31 +0000

    Add package capone/1.0.7/capone-1.0.7-py2.py3-none-any.whl

---
+++ packages/capone/capone-1.0.7-py2.py3-none-any.whl/capone/exceptions.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+
+
+class TransactionException(Exception):
+    pass
+
+
+class TransactionBalanceException(TransactionException):
+    pass
+
+
+class UnvoidableTransactionException(TransactionException):
+    pass
+
+
+class UnmodifiableTransactionException(TransactionException):
+    pass
+
+
+class PrimaryRelatedObjectException(TransactionException):
+    pass
+
+
+class NoLedgerEntriesException(TransactionException):
+    pass
+
+
+class ExistingLedgerEntriesException(TransactionException):
+    pass
