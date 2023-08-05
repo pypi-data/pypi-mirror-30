bcce87b957cd6c32925069468281f6e6d8e53799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:16 +0000

    Add package qt5reactor/0.5/qt5reactor-0.5.tar.gz

---
+++ packages/qt5reactor/qt5reactor-0.5.tar.gz/qt5reactor-0.5/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: X11 Applications :: Qt',
+    'Framework :: Twisted',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Natural Language :: English',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+setup(
+    name='qt5reactor',
+    version='0.5',
+    license='MIT',
+    classifiers=classifiers,
+    author='Christopher R. Wood',
+    author_email='chris@leastauthority.com',
+    description='Twisted Qt Integration',
+    long_description=read('README.rst'),
+    url='https://github.com/sunu/qt5reactor',
+    packages=find_packages(),
+    py_modules=['qt5reactor'],
+    keywords=['Qt', 'twisted'],
+    install_requires=['twisted']
+)
