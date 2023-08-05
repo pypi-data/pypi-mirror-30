86741e8bc56f8d2eb7574fe96f9df95308868e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:29 +0000

    Add package inlineplz/0.25.0/inlineplz-0.25.0-py2.py3-none-any.whl

---
+++ packages/inlineplz/inlineplz-0.25.0-py2.py3-none-any.whl/inlineplz/parsers/yamllint.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from inlineplz.parsers.base import ParserBase
+
+
+class YAMLLintParser(ParserBase):
+    """Parse yaml-lint output."""
+
+    def parse(self, lint_data):
+        messages = set()
+        for file_path, output in lint_data:
+            try:
+                if file_path.strip() and output.strip():
+                    path = file_path
+                    line = int(output.split('at line')[1].split()[0].strip())
+                    msgbody = output
+                    messages.add((path, line, msgbody))
+            except (ValueError, IndexError):
+                print('Invalid message: {0}'.format(output))
+        return messages
