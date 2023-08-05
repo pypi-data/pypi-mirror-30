07f88f60e31762e2133bd3229c18fc7c0ddebd37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:37 +0000

    Add package file-sorting/2.0/file_sorting-2.0.tar.gz

---
+++ packages/file-sorting/file_sorting-2.0.tar.gz/file_sorting-2.0/file_sorting.py
@@ -0,0 +1,38 @@
+﻿import os
+import os.path
+import shutil
+import time
+import datetime
+import stat
+from PIL import Image
+from PIL.ExifTags import TAGS
+import csv
+
+def file_move(frfolder,file1,type,dest,file2):
+    log = dest + '\\'+'process.log'
+
+    if os.path.exists(dest + '\\' + type + '\\' + file2 + '.' + type):
+        file_move(frfolder,file1,type,dest,file2+'-1')
+    else:
+        shutil.move(frfolder + '\\' + file1 + '.' + type, dest + '\\' + type + '\\' + file2 + '.' + type)
+        out = open(log,'a')
+        wt = csv.writer(out)            
+        wt.writerow([frfolder + '\\' + file1 + '.'+ type, dest + '\\' +  type +  '\\'+ file2 + '.' + type])
+        out.close()
+                    
+def class_files(folder,dstfold):
+       
+    for i in os.listdir(folder):
+        if os.path.isdir(os.path.join(folder,i)):
+            class_files(os.path.join(folder,i),dstfold)
+        else:
+            type = i.split(".")[-1]
+            file_name = i[0:i.rindex('.')]
+            if not (os.path.exists(dstfold + '\\' + type) and not os.path.isfile(dstfold + '\\' + type)):
+                os.makedirs(dstfold + '\\' + type)
+            file_move(folder,file_name,type,dstfold,file_name)
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ packages/file-sorting/file_sorting-2.0.tar.gz/file_sorting-2.0/setup.py
@@ -0,0 +1,6 @@
+﻿from distutils.core import setup
+
+setup(name = 'file_sorting',
+      version = '2.0',
+      py_modules = ['file_sorting'],
+      )
