408470471feac6e56d8b57434562e3702e062fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:04 +0000

    Add package weeb.py/1.1.0/weeb.py-1.1.0.tar.gz

---
+++ packages/weeb.py/weeb.py-1.1.0.tar.gz/weeb.py-1.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='weeb.py',
+      author='Desiiii',
+      author_email='godavaru@gmail.com',
+      description='An API wrapper for weeb.sh',
+      url='https://github.com/Desiiii/weeb.py',
+      version='1.1.0',
+      packages=["weeb"],
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.6',
+      ],
+      python_requires=">=3.4, <4",
+      include_package_date=True)
