3c851f04db4ee80a77d34748c1fff9c42cea07d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:07 +0000

    Add package pyt/0.7.27/pyt-0.7.27.tar.gz

---
+++ packages/pyt/pyt-0.7.27.tar.gz/pyt-0.7.27/pyt/__main__.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import argparse
+import os
+
+from pyt import tester, __version__
+
+
+def console():
+    '''
+    cli hook
+
+    return -- integer -- the exit code
+    '''
+    parser = argparse.ArgumentParser(description='Easy Python Testing')
+    parser.add_argument('names', metavar='NAME', nargs='*', default=[], help='the test(s) you want to run')
+    parser.add_argument('--basedir', dest='basedir', default=os.curdir, help='run from this directory')
+    parser.add_argument('--debug', "-d", dest='debug', action='store_true', help='print debugging info')
+    parser.add_argument(
+        "--version", "-V",
+        action='version',
+        version="%(prog)s {}, Python {}".format(__version__, sys.version)
+    )
+    parser.add_argument('--all', "-a", dest='run_all', action='store_true', help='run all tests with buffer')
+    #parser.add_argument('--fad', dest='daf', action='store_true', help='run with --all --no-faifast --debug')
+
+    # https://docs.python.org/2/library/unittest.html#command-line-options
+    #parser.add_argument('--no-failfast', dest='no_failfast', action='store_true', help='turns off fail fast')
+    #parser.add_argument('--no-buffer', dest='no_buffer', action='store_true', help='turns off buffer when more than one test is ran')
+    parser.add_argument('--buffer', "-b", dest='buffer', action='store_true', help='Buffer stdout and stderr during test runs')
+
+    args, test_args = parser.parse_known_args()
+
+    test_args.insert(0, sys.argv[0])
+    ret_code = 0
+
+    if args.run_all:
+        args.names = ['']
+        args.buffer = True
+
+    # create the singleton
+    environ = tester.TestEnviron.get_instance(args)
+
+    if not args.names:
+        args.names.append('')
+
+    if args.names:
+        for name in args.names:
+            ret_code |= tester.run_test(
+                name,
+                args.basedir,
+                argv=test_args,
+            )
+
+    else:
+        environ.unbuffer()
+        # http://unix.stackexchange.com/a/8815/118750
+        parser.print_help()
+        ret_code = 1
+
+    sys.exit(ret_code)
+
+
+if __name__ == "__main__":
+    # allow both imports of this module, for entry_points, and also running this module using python -m pyt
+    console()
+
---
+++ packages/pyt/pyt-0.7.27.tar.gz/pyt-0.7.27/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import re
+import os
+import sys
+
+_ver = sys.version_info
+is_py2 = (_ver[0] == 2)
+is_py3 = (_ver[0] == 3)
+
+name = 'pyt'
+with open(os.path.join(name, "__init__.py")) as f:
+    version = re.search("^__version__\s*=\s*[\'\"]([^\'\"]+)", f.read(), flags=re.I | re.M).group(1)
+
+
+kwargs = dict(
+    name=name,
+    version=version,
+    description='easily run python unit tests',
+    author='Jay Marcyes',
+    author_email='jay@marcyes.com',
+    url='http://github.com/Jaymon/{}'.format(name),
+    packages=[name],
+    license="MIT",
+    classifiers=[ # https://pypi.python.org/pypi?:action=list_classifiers
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python :: 2.7',
+        #'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3',
+    ],
+    #test_suite = "test_pout",
+)
+
+if is_py2:
+    kwargs['entry_points'] = {
+        'console_scripts': [
+            '{} = {}.__main__:console'.format(name, name),
+            '{}2 = {}.__main__:console'.format(name, name)
+        ]
+    }
+
+elif is_py3:
+    kwargs['entry_points'] = {
+        'console_scripts': [
+            '{}3 = {}.__main__:console'.format(name, name)
+        ]
+    }
+
+setup(**kwargs)
+
