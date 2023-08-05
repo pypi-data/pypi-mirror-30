d2b6036947fbad637caa6b5866f1bcc481633b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:56 +0000

    Add package requests-viewer/0.0.12/requests_viewer-0.0.12.tar.gz

---
+++ packages/requests-viewer/requests_viewer-0.0.12.tar.gz/requests_viewer-0.0.12/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages
+from setuptools import setup
+
+MAJOR_VERSION = '0'
+MINOR_VERSION = '0'
+MICRO_VERSION = '12'
+VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
+
+setup(name='requests_viewer',
+      version=VERSION,
+      description="requests_viewer!",
+      url='https://github.com/kootenpv/requests_viewer',
+      author='Pascal van Kooten',
+      author_email='kootenpv@gmail.com',
+      license='MIT',
+      install_requires=[
+          'requests',
+      ],
+      extras_require={
+          'fancy': ['lxml', 'bs4', 'tldextract']
+      },
+      packages=find_packages(),
+      zip_safe=False,
+      platforms='any')
