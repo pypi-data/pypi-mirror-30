65eb48e142260db7c6a6c9bdf65f1979913896a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:26 +0000

    Add package tapioca-ssllabs/0.4/tapioca_ssllabs-0.4-py3-none-any.whl

---
+++ packages/tapioca-ssllabs/tapioca_ssllabs-0.4-py3-none-any.whl/tapioca_ssllabs/resource_mapping.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+RESOURCE_MAPPING = {
+    'info': {
+        'resource': 'info',
+        'docs': 'https://github.com/ssllabs/ssllabs-scan/blob/master/ssllabs-api-docs-v3.md',
+        'methods': ['GET']
+    },
+    'analyze': {
+        'resource': 'analyze',
+        'docs': 'https://github.com/ssllabs/ssllabs-scan/blob/master/ssllabs-api-docs-v3.md',
+        'methods': ['GET']
+    },
+    'get_endpoint_data': {
+        'resource': 'getEndpointData',
+        'docs': 'https://github.com/ssllabs/ssllabs-scan/blob/master/ssllabs-api-docs-v3.md',
+        'methods': ['GET']
+    },
+    'get_status_codes': {
+        'resource': 'getStatusCodes',
+        'docs': 'https://github.com/ssllabs/ssllabs-scan/blob/master/ssllabs-api-docs-v3.md',
+        'methods': ['GET']
+    },
+}
