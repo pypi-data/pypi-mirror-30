6965d31a70fbcacfd0d6c5e8b6ec20f9e111c416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:34 +0000

    Add package onnx/1.1.0/onnx-1.1.0-py2.7-macosx-10.6-x86_64.egg

---
+++ packages/onnx/onnx-1.1.0-py2.7-macosx-10.6-x86_64.egg/onnx/defs/__init__.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import onnx.onnx_cpp2py_export.defs as C
+
+
+ONNX_DOMAIN = ""
+
+
+def has(op_type):
+    return C.has_schema(op_type)
+
+
+def get_schema(op_type):
+    return C.get_schema(op_type)
+
+
+def get_all_schemas():
+    return C.get_all_schemas()
+
+
+def get_all_schemas_with_history():
+    return C.get_all_schemas_with_history()
+
+
+def onnx_opset_version():
+    return C.schema_version_map()[ONNX_DOMAIN][1]
+
+
+OpSchema = C.OpSchema
