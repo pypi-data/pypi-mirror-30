e9d803657e901f958ff53928ca801fa6b9ba514c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:44 +0000

    Add package homematicip/0.9/homematicip-0.9.tar.gz

---
+++ packages/homematicip/homematicip-0.9.tar.gz/homematicip-0.9/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+VERSION = '0.9'
+
+setup(
+    name='homematicip',
+    packages=find_packages(exclude='tests'),
+    version=VERSION,
+    description='An API for the homematicip cloud',
+    author='Heimo Stieg',
+    author_email='stieg@corona-bytes.net',
+    url='https://github.com/coreGreenberet/homematicip-rest-api',
+    download_url='https://github.com/coreGreenberet/homematicip-rest-api/tarball/' + VERSION,
+    keywords=['homematicip'],  # arbitrary keywords
+    classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3 :: Only'],
+    install_requires=["requests", "websocket-client", "future", "websockets", "aiohttp"],
+    scripts=['hmip_cli.py', 'hmip_generate_auth_token.py']
+)
