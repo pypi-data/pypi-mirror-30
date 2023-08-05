ab20ac9e928b42c05a4e4d5465d5d02c6e6f4a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:07 +0000

    Add package github-console/1.0.6/github-console-1.0.6.tar.gz

---
+++ packages/github-console/github-console-1.0.6.tar.gz/github-console-1.0.6/README.md
@@ -0,0 +1,21 @@
+## github-console
+#### Manage github with console method.
+
+
+#### Pypi [github-console](https://pypi.python.org/pypi/github-console)
+### How to install ?
+```bash
+ cd ~
+ git clone https://github.com/luoyeah/github-console
+ cd github-console
+ ./setup.py install
+ 
+ github -h
+```
+### or
+
+```bash
+ pip install github-console
+ 
+ github -h
+```
---
+++ packages/github-console/github-console-1.0.6.tar.gz/github-console-1.0.6/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+home_page = 'https://github.com/luoyeah/github-console'
+
+requires = [
+    'github3.py>=1.0.1',
+    'docopt>=0.6.2'
+]
+
+packages = [
+    'github_console'
+]
+
+setup(
+    name='github-console',
+    version='1.0.6',
+    keywords=(
+        'github-console',
+        'github',
+        'git'
+    ),
+    author='luoyeah',
+    author_email='luoyeah_ilku@foxmail.com',
+    url=home_page,
+    license='GPLv3',
+    description='Github console tool.',
+    long_description='HomePage: %s' % home_page,
+    include_package_data=True,
+    zip_safe=True,
+    packages=packages,
+    platforms='any',
+    install_requires=requires,
+    entry_points={
+        'console_scripts':[
+            'github = github_console.github:main'
+        ]
+    },
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+    ]
+)
