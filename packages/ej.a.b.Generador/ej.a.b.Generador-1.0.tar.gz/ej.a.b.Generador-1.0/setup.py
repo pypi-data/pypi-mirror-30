a046597bfbc54cfd00d5c8cc1acd846878599d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:41 +0000

    Add package ej.a.b.Generador/1.0/ej.a.b.Generador-1.0.tar.gz

---
+++ packages/ej.a.b.Generador/ej.a.b.Generador-1.0.tar.gz/ej.a.b.Generador-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='ej.a.b.Generador',    # This is the name of your PyPI-package.
+    version='1.0',                          # Update the version number for new releases
+    description = "ejemplo",
+    py_modules=['Generador']                  # The name of your scipt, and also the command you'll be using for calling it
+)
