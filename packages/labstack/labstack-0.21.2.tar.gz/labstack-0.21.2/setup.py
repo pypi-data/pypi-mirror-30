5ad859e4b729fb36c9f1059eb53955a089ac2a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:09 +0000

    Add package labstack/0.21.2/labstack-0.21.2.tar.gz

---
+++ packages/labstack/labstack-0.21.2.tar.gz/labstack-0.21.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+  name='labstack',
+  version='0.21.2',
+  description='Official Python client library for the LabStack platform',
+  long_description='`<https://github.com/labstack/labstack-python>`_',
+  keywords='api, testing, monitoring, analytics',
+  url='https://github.com/labstack/labstack-python',
+  author='Vishal Rana',
+  author_email='vr@labstack.com',
+  license='MIT',
+  packages=['labstack'],
+  install_requires=[
+    'requests==2.18.4',
+    'psutil==5.4.3',
+    'APScheduler==3.5.1'
+  ],
+  extra_requires=[
+    'Flask==0.12.2'
+  ],
+  classifiers=[
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6'
+  ]
+)
