8a97cfa006cdeb61aea55d8dc6d5d213bd8d5f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:41 +0000

    Add package zmail/0.0.8/zmail-0.0.8.tar.gz

---
+++ packages/zmail/zmail-0.0.8.tar.gz/zmail-0.0.8/setup.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+PROJECT_NAME = 'zmail'
+MODULE_NAME = 'zmail'
+
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == 'publish':
+    os.system('python3 setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
+
+setup(
+    name='zmail',
+    version='0.0.8',
+
+    author='ZYunH',
+    author_email='workvl@163.com',
+
+    description='Zmail allows you to send and get emails as possible as it can be in python',
+    long_description='Zmail allows you to send and get emails as possible as it can be in python.There is no need to check server address or make your own MIME string.With zmail, you only need to care about your mail content.',
+    keywords='email python3 package',
+
+    url='https://github.com/ZYunH/zmail',
+    license="MIT Licence",
+
+    platforms='all',
+
+    packages=find_packages(),
+    include_package_data=True,
+
+)
