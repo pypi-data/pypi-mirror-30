bdc4ba47833fbbbed367fbee399c1f6d0e7aec61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:28 +0000

    Add package Terrene/0.1.0/Terrene-0.1.0.tar.gz

---
+++ packages/Terrene/Terrene-0.1.0.tar.gz/Terrene-0.1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+required = []
+try:
+    with open('requirements.txt') as f:
+        required = f.read().splitlines()
+except FileNotFoundError:
+    pass
+
+setup(
+    name="Terrene",
+    version="0.1.0",
+    description="Terrene's Python SDK",
+    url="https://docs.terrene.co",
+    author="Terrene",
+    author_email="developers@terrene.co",
+    license='MIT',
+    keywords="terrene",
+    packages=find_packages(exclude=["test"]),
+    install_requires=required,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
