14d9a15d9fbd922bfc3c5ea661ec18a740ba190e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:52 +0000

    Add package pysp2tf-demo/0.5/pysp2tf-demo-0.5.tar.gz

---
+++ packages/pysp2tf-demo/pysp2tf-demo-0.5.tar.gz/pysp2tf-demo-0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+  name = 'pysp2tf-demo',
+  version = '0.5',
+  description = 'PySpark and TF demo',
+  author = 'Andy Feng',
+  author_email = 'andy.feng@gmail.com',
+  url = 'https://github.com/anfeng/py-demo',
+  keywords = ['tensorflow', 'spark'],
+  packages = ['py2tf', 'py2tf.jars'],
+  package_data={'py2tf.jars': ['*.jar']},
+  include_package_data=True, 
+  license = 'Apache 2.0',
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: Apache Software License',
+    'Topic :: Software Development :: Libraries',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6'
+  ]
+)
