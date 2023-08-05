aa64abefd9d611c40d5307211de9aa936aa1f41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:48 +0000

    Add package scikit-optimize/0.5.2/scikit-optimize-0.5.2.tar.gz

---
+++ packages/scikit-optimize/scikit-optimize-0.5.2.tar.gz/scikit-optimize-0.5.2/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='scikit-optimize',
+      version='0.5.2',
+      description='Sequential model-based optimization toolbox.',
+      long_description=open('README.rst').read(),
+      url='https://scikit-optimize.github.io/',
+      license='BSD',
+      author='The scikit-optimize contributors',
+      packages=['skopt', 'skopt.learning', 'skopt.optimizer', 'skopt.space',
+                'skopt.learning.gaussian_process'],
+      install_requires=["numpy", "scipy>=0.14.0", "scikit-learn>=0.19.1"],
+      extras_require={
+        'plots':  ["matplotlib"]
+        }
+      )
