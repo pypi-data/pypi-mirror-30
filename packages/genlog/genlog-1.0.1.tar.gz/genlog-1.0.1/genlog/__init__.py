5e6053ebe99cad2acb2f9485f91fc6c3e17e02af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:27 +0000

    Add package genlog/1.0.1/genlog-1.0.1.tar.gz

---
+++ packages/genlog/genlog-1.0.1.tar.gz/genlog-1.0.1/genlog/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author  : ilpan
+@contact : pna.dev@outlook.com
+@file    : __init__.py.py
+@desc    :
+@time    : 18-3-16 上午11:15
+"""
+
+__description__ = "genlog: a tool that can randomly generate logs and send them to specified host:port"
+
+__version__ = '1.0.1'
+__author__  = 'ilpan'
+__license__   = 'MIT'
+__author_email__ = 'pna.dev@outlook.com'
