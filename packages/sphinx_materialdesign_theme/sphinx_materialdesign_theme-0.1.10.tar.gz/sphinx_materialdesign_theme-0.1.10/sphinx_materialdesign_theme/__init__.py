21993ccc40f107b7bee9646c0635a496a4dea57a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:59 +0000

    Add package sphinx_materialdesign_theme/0.1.10/sphinx_materialdesign_theme-0.1.10.tar.gz

---
+++ packages/sphinx_materialdesign_theme/sphinx_materialdesign_theme-0.1.10.tar.gz/sphinx_materialdesign_theme-0.1.10/sphinx_materialdesign_theme/__init__.py
@@ -0,0 +1,9 @@
+from os import path
+
+package_dir = path.dirname(path.abspath(__file__))
+
+def get_path():
+    return package_dir
+
+def setup(app):
+    app.add_html_theme('sphinx_materialdesign_theme', package_dir)
