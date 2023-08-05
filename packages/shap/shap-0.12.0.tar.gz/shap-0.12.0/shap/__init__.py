c9e90436ab867fd33c36dc94e943c66f0c5c4a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:11 +0000

    Add package shap/0.12.0/shap-0.12.0.tar.gz

---
+++ packages/shap/shap-0.12.0.tar.gz/shap-0.12.0/shap/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+from iml.explanations import Explanation, AdditiveExplanation
+from iml.datatypes import Data, DenseData
+from iml.links import Link, IdentityLink, LogitLink
+from iml.common import Instance, Model
+from .explainer import KernelExplainer, kmeans
+from .plots import visualize, plot, summary_plot, joint_plot, interaction_plot, dependence_plot, force_plot
+from iml.visualizers import initjs, SimpleListVisualizer, SimpleListVisualizer, AdditiveForceVisualizer, \
+    AdditiveForceArrayVisualizer
+from . import datasets
