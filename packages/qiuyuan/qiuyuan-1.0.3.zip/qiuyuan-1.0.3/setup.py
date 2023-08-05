47302ea052ac622ea6f4009d54ebba3abe3274c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:37 +0000

    Add package qiuyuan/1.0.3/qiuyuan-1.0.3.zip

---
+++ packages/qiuyuan/qiuyuan-1.0.3.zip/qiuyuan-1.0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='qiuyuan',
+    version='1.0.3',
+    packages=['qiuyuan'],
+    url='http://www.chenqiuyuan.com',
+    license='',
+    author='Qiuyuan Chen',
+    author_email='380133194@qq.com',
+    description='Test'
+)
