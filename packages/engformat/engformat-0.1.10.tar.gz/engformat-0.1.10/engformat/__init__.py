f8e2956193bf1c2c1ca550d8f1fa24b71797e77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:15 +0000

    Add package engformat/0.1.10/engformat-0.1.10.tar.gz

---
+++ packages/engformat/engformat-0.1.10.tar.gz/engformat-0.1.10/engformat/__init__.py
@@ -0,0 +1,3 @@
+from engformat.plot import *
+from engformat.plot_tools import *
+from engformat.plot_outputs import *
---
+++ packages/engformat/engformat-0.1.10.tar.gz/engformat-0.1.10/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+about = {}
+with open("engformat/__about__.py") as fp:
+    exec(fp.read(), about)
+
+setup(name='engformat',
+      version=about['__version__'],
+      description='Tools for displaying engineering calculations according to the Engineering Standard Format',
+      url='',
+      author='Maxim Millen',
+      author_email='millen@fe.up.pt',
+      license='MIT',
+      packages=[
+        'engformat',
+    ],
+    install_requires=[
+        "numpy",
+        "bwplot>=0.2.10",
+        "sfsimodels"
+    ],
+      zip_safe=False)
