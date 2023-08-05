435eb8b1968db8a37c9bd4b9efe901a9a69d1d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:59 +0000

    Add package script-venv/0.1.0/script_venv-0.1.0-py2.py3-none-any.whl

---
+++ packages/script-venv/script_venv-0.1.0-py2.py3-none-any.whl/script_venv/config.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+""" Config file processing """
+from configparser import ConfigParser
+from pathlib import Path
+from types import MappingProxyType
+
+from typing import Mapping, Set, Dict  # noqa: F401
+
+from os import path
+
+from .venv import VEnv
+
+# noinspection SpellCheckingInspection
+"""
+Config file structure:
+[SCRIPTS]
+cookiecutter=cc
+[PACKAGES]
+jupyter
+[cc]
+requirements:
+    cookiecutter
+    pipdeptree
+"""
+
+SCRIPTS = "SCRIPTS"
+
+
+class VenvConfig(object):
+    def __init__(self) -> None:
+        self.configs = set()  # type: Set[str]
+        self._scripts = {}  # type: Dict[str, str]
+        self._venvs = {}  # type: Dict[str, VEnv]
+        self._scripts_proxy = MappingProxyType(self._scripts)
+        self._venvs_proxy = MappingProxyType(self._venvs)
+
+    def load(self, config_path: Path, local: bool) -> None:
+        config_file = config_path / '.sv_cfg'
+        config_file_path = Path(path.expanduser(str(config_file))).absolute()
+
+        if config_file_path.exists():
+            self.configs.add(config_file.as_posix())
+            config = ConfigParser()
+            config.read_file(config_file_path.open())
+
+            for v in config:
+                if v.islower():
+                    venv = config[v]
+                    req = venv.get('requirements', '').splitlines()
+                    self._venvs.setdefault(v, VEnv(v, local=local, requirements=req))
+
+            scripts = config[SCRIPTS]
+            for s in scripts:
+                v = scripts[s] or s
+                self._scripts[s] = v
+                self._venvs.setdefault(v, VEnv(v, local=local))
+
+    @property
+    def scripts(self) -> Mapping[str, str]:
+        return self._scripts_proxy
+
+    @property
+    def venvs(self) -> Mapping[str, VEnv]:
+        return self._venvs_proxy
---
+++ packages/script-venv/script_venv-0.1.0-py2.py3-none-any.whl/script_venv/venv.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+"""Virtual environment handling"""
+
+import os
+from pathlib import Path
+import subprocess
+import sys
+from typing import Iterable, List, Dict  # noqa: F401
+import venv
+
+
+_r = 'requirements'
+
+if os.name == 'nt':  # pragma: no cover
+    _bin = 'Scripts'
+    _exe = '.exe'
+    _quote = '"%s"'
+else:  # pragma: no cover
+    _bin = 'bin'
+    _exe = ''
+    _quote = '%s'
+
+
+class VEnv(object):
+    def __init__(self, name, requirements: Iterable[str] = None, local=False) -> None:
+        self.name = name
+        self.requirements = requirements
+        self.env_path = (Path('.sv') if local else Path('~') / '.sv') / name
+        self.abs_path = Path(os.path.expanduser(str(self.env_path))).absolute()
+
+    def __str__(self) -> str:
+        return "%s (%s%s)" % (self.name, self.env_path, '' if self.abs_path.exists() else ' !MISSING')
+
+    def exists(self) -> bool:
+        return self.env_path.exists()
+
+    def run(self, cmd_name: str, args: List[str], runner=None) -> int:
+        runner = runner if callable(runner) else subprocess.call
+
+        bin_path = self.abs_path / _bin
+        cmd_path = bin_path / (cmd_name + _exe)
+        new_env = dict()  # type: Dict[str,str]
+        new_env.update(os.environ,
+                       VIRTUAL_ENV=str(self.abs_path),
+                       PATH=''.join([_quote % self.abs_path, os.pathsep, os.environ['PATH']])
+                       )
+        if cmd_path.exists():  # pragma: no cover
+            cmd = [str(cmd_path)]
+        else:
+            cmd = [str(bin_path / os.path.basename(sys.executable)), cmd_name]
+
+        return runner(cmd + args, env=new_env)
+
+    def create(self, creator=None) -> bool:
+        if not self.requirements:
+            return False
+
+        creator = creator if callable(creator) else venv.create
+
+        creator(str(self.abs_path), with_pip=True)
+        return True
