03aa3acc6299173ee5e0631ecae1c47edd5b82ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:12 +0000

    Add package ompp/1.0.0/ompp-1.0.0.tar.gz

---
+++ packages/ompp/ompp-1.0.0.tar.gz/ompp-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ompp',
+      version='1.0.0',
+      description='ompp python support',
+      url='https://github.com/PasinduSenanayake/Rigel-FYP/tree/ompppackage',
+      author='Rigel',
+      author_email='pasinduath@gmail.com',
+      license='MIT',
+      packages=['ompp'],
+      include_package_data=True,
+      zip_safe=False)
