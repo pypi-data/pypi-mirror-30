61ad9abcee5e9d8155b75ac81f7adf2539c1ae52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:53 +0000

    Add package pygogogate2/0.0.1/pygogogate2-0.0.1.tar.gz

---
+++ packages/pygogogate2/pygogogate2-0.0.1.tar.gz/pygogogate2-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='pygogogate2',
+    version='0.0.1',
+    description='Python package for controlling Gogogate2-Enabled Garage Door',
+    author='David Broadfoot',
+    author_email='dbroadfoot@gmail.com',
+    url='https://github.com/dlbroadfoot/pygogogate2',
+    license=license,
+    packages=['pygogogate2'],
+    package_dir={'pygogogate2': 'pygogogate2'},
+    install_requires=[
+        'pycryptodomex'
+    ]
+)
