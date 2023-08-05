b1a33a7a31793eb3c8448b5558bda6104a84a1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:40 +0000

    Add package cdisp/0.0.7/cdisp-0.0.7.tar.gz

---
+++ packages/cdisp/cdisp-0.0.7.tar.gz/cdisp-0.0.7/setup.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import codecs
+
+from setuptools import setup
+
+
+version = '0.0.7'
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    readme = f.read()
+
+
+setup_requires = ['pytest-runner'] if \
+    {'pytest', 'test', 'ptr'}.intersection(sys.argv) else []
+
+setup(
+    name='cdisp',
+    version=version,
+    author='Felippe Barbosa',
+    author_email='felippe.barbosa@gmail.com',
+    license='MIT v1.0',
+    url='https://github.com/fbarbosa/cdisp',
+    description='My personal support packages for dispersion and nonlinear optics analysis using COMSOL simulations',
+    long_description=readme,
+    keywords='dispersion COMSOL',
+    packages=['cdisp'],
+    package_dir={'cdisp': 'cdisp'},
+    ext_modules=[],
+    provides=['cdisp'],
+    install_requires=['numpy', 'scipy', 'pandas'] + (['future']
+                                  if sys.version_info.major < 3 else []),
+    setup_requires=setup_requires,
+    tests_require=[],
+    platforms='OS Independent',
+    classifiers=[
+        'Topic :: Scientific/Engineering :: Physics',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Boost Software License 1.0 (BSL-1.0)'
+    ],
+    zip_safe = False,
+    include_package_data = True)
