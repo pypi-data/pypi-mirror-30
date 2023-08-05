2556bb20f4eea18110ecd0ea702343d5f6ed7244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:40 +0000

    Add package kerncraft/0.6.5/kerncraft-0.6.5.tar.gz

---
+++ packages/kerncraft/kerncraft-0.6.5.tar.gz/kerncraft-0.6.5/tests/all_tests.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import unittest
+import sys
+
+
+suite = unittest.TestLoader().loadTestsFromNames(
+    [
+        'test_kerncraft',
+        'test_intervals',
+        'test_kernel',
+        'test_layer_condition',
+        'test_likwid_bench_auto',
+        'test_iaca_marker',
+        'test_cacheprediction',
+    ]
+)
+
+testresult = unittest.TextTestRunner(verbosity=2).run(suite)
+sys.exit(0 if testresult.wasSuccessful() else 1)
