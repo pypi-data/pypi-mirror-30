47552dd7e588e7b234f716ddc27069761ca78f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:25 +0000

    Add package sqlservice/0.20.0/sqlservice-0.20.0-py2.py3-none-any.whl

---
+++ packages/sqlservice/sqlservice-0.20.0-py2.py3-none-any.whl/sqlservice/__pkg__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+"""
+
+__package_name__ = 'sqlservice'
+__description__ = 'The missing SQLAlchemy ORM interface'
+__url__ = 'https://github.com/dgilland/sqlservice'
+__version__ = '0.20.0'
+__author__ = 'Derrick Gilland'
+__email__ = 'dgilland@gmail.com'
+__license__ = 'MIT License'
