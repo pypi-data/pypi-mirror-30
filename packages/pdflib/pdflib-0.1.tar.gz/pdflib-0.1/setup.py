f64021954e5a3809a5d94ff1f582114909b40cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:47 +0000

    Add package pdflib/0.1/pdflib-0.1.tar.gz

---
+++ packages/pdflib/pdflib-0.1.tar.gz/pdflib-0.1/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import os
+import sys
+
+from setuptools import Extension, setup
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    print('You need to install cython first - pip install cython',
+          file=sys.stderr)
+    sys.exit(1)
+
+
+POPPLER_ROOT = os.environ.get('POPPLER_ROOT', '')
+if not POPPLER_ROOT:
+    print('Please clone and compile poppler from source and set POPPLER_ROOT '
+          'environment variable. See README for more info.', file=sys.stderr)
+    sys.exit(1)
+POPPLER_CPP_LIB_DIR = os.path.join(POPPLER_ROOT, 'cpp/')
+
+if sys.platform == 'darwin':
+    # OS X extras
+    extra_compile_args = [
+        "-std=c++11", "-stdlib=libc++", "-mmacosx-version-min=10.7"
+    ]
+else:
+    extra_compile_args = ["-std=c++11"]
+
+poppler_ext = Extension('pdflib',
+                        ['pdflib.pyx', 'poppler/utils/ImageOutputDev.cc'],
+                        language='c++',
+                        extra_compile_args=extra_compile_args,
+                        include_dirs=[
+                            POPPLER_ROOT, os.path.join(POPPLER_ROOT, 'poppler')
+                        ],
+                        library_dirs=[POPPLER_ROOT, POPPLER_CPP_LIB_DIR],
+                        runtime_library_dirs=['$ORIGIN'],
+                        libraries=['poppler', 'poppler-cpp'])
+
+setup(
+    name='pdflib',
+    version='0.1',
+    description="python bindings for poppler",
+    install_requires=['cython', ],
+    include_package_data=True,
+    ext_modules=cythonize([poppler_ext]),
+    zip_safe=False
+)
