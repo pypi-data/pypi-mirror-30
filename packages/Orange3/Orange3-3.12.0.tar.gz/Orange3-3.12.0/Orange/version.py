d8b082acfc6f6936fe84b16f950c200f4405a403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:18 +0000

    Add package Orange3/3.12.0/Orange3-3.12.0.tar.gz

---
+++ packages/Orange3/Orange3-3.12.0.tar.gz/Orange3-3.12.0/Orange/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '3.12.0'
+version = '3.12.0'
+full_version = '3.12.0'
+git_revision = 'cdd2135d8b48cad750343d01903e7d1826b91ce2'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
