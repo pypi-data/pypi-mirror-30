e254bb298888d7e352c15b5cbf491feebc419ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:29 +0000

    Add package multiflexxlib/0.2.9/multiflexxlib-0.2.9.tar.gz

---
+++ packages/multiflexxlib/multiflexxlib-0.2.9.tar.gz/multiflexxlib-0.2.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+requires = ['pyclipper>=1.1.0',
+            'numpy>=1.14',
+            'pandas>=0.22.0',
+            'matplotlib>=2.1.0',
+            'scipy>=1.0.0']
+
+setup(name='multiflexxlib',
+      version='0.2.9',
+      description='Tools library for CAMEA neutron detector MultiFLEXX',
+      url='http://github.com/yumemi5k/multiflexxlib',
+      author='Siqin Meng',
+      author_email='mengsq04@gmail.com',
+      license='',
+      packages=['multiflexxlib'],
+      install_requires=requires,
+      zip_safe=False,
+      include_package_data=True)
