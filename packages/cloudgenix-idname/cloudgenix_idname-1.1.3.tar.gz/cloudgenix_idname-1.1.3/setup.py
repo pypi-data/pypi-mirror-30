5f82befea442eebecfb5bc0360c19568e0a459be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:45 +0000

    Add package cloudgenix-idname/1.1.3/cloudgenix_idname-1.1.3.tar.gz

---
+++ packages/cloudgenix-idname/cloudgenix_idname-1.1.3.tar.gz/cloudgenix_idname-1.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='cloudgenix_idname',
+      version='1.1.3',
+      description='ID -> Name translator for the CloudGenix Python SDK',
+      url='https://github.com/ebob9/cloudgenix-idname',
+      author='Aaron Edwards',
+      author_email='cloudgenix_idname@ebob9.com',
+      license='MIT',
+      install_requires=[
+            'cloudgenix >= 4.5.5b3'
+      ],
+      packages=['cloudgenix_idname'],
+      classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3"
+      ]
+      )
