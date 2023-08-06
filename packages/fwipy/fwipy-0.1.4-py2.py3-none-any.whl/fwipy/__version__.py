86bfd9f42709d951897f5dd22da355341ce19044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:49 +0000

    Add package fwipy/0.1.4/fwipy-0.1.4-py2.py3-none-any.whl

---
+++ packages/fwipy/fwipy-0.1.4-py2.py3-none-any.whl/fwipy/__version__.py
@@ -0,0 +1,18 @@
+
+#   ___________    __    ____  __  .______   ____    ____
+#  |   ____\   \  /  \  /   / |  | |   _  \  \   \  /   /
+#  |  |__   \   \/    \/   /  |  | |  |_)  |  \   \/   /
+#  |   __|   \            /   |  | |   ___/    \_    _/
+#  |  |       \    /\    /    |  | |  |          |  |
+#  |__|        \__/  \__/     |__| | _|          |__|
+
+
+__title__ = 'fwipy'
+__description__ = 'Fwipy - Python Wrapper for Content Player Socket Operations'
+__url__ = 'https://github.com/FourWindsInteractive/fwipy'
+__version__ = '0.0.1.dev0'
+__author__ = "Erin O'Connell"
+__author_email__ = 'erin.oconnell@fourwindsinteractive.com'
+__license__ = 'Apache 2.0'
+__copyright__ = "Copyright 2017 Erin O'Connell"
+__download_url__ = 'https://github.com/fourwindsinteractive/fwipy/archive/{0}.tar.gz'.format(__version__)
---
+++ packages/fwipy/fwipy-0.1.4-py2.py3-none-any.whl/fwipy/compat.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""
+fwipy.compat
+~~~~~~~~~~~~~~~
+
+This module handles import compatibility issues between Python 2 and
+Python 3.  (Thanks Kenneth and requests ✨🍰✨)
+"""
+
+import chardet
+
+import sys
+
+# -------
+# Pythons
+# -------
+
+# Syntax sugar.
+_ver = sys.version_info
+
+#: Python 2.x?
+is_py2 = (_ver[0] == 2)
+
+#: Python 3.x?
+is_py3 = (_ver[0] == 3)
+
+
+# ---------
+# Specifics
+# ---------
+
+if is_py2:
+    from urllib import (quote, unquote)
+
+elif is_py3:
+    from urllib.parse import (quote, unquote)
