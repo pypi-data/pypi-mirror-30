7870c90e9e4cd82f518bd839b7b2bc84387069fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:02 +0000

    Add package de/0.1/de-0.1.tar.gz

---
+++ packages/de/de-0.1.tar.gz/de-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='de',
+      version='0.1',
+      description='de',
+      url='https://github.com/desantis/python-de',
+      author='Andrew T. DeSantis',
+      author_email='atd@deos.org',
+      license='MIT',
+      packages=['de'],
+      zip_safe=False)
