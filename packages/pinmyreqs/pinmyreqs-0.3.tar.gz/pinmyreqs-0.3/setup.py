080a8acce8973dbc48810ed33e7a3de7c5e4c62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:31 +0000

    Add package pinmyreqs/0.3/pinmyreqs-0.3.tar.gz

---
+++ packages/pinmyreqs/pinmyreqs-0.3.tar.gz/pinmyreqs-0.3/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import sys
+from setuptools import setup
+
+setup(
+    name='pinmyreqs',
+    version='0.3',
+    description='Pin your requirements.txt without adding new dependencies',
+    long_description=open('README.rst').read(),
+    url='http://github.com/mdamien/pinmyreqs',
+    author='Damien MARIÉ',
+    author_email='damien@dam.io',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'pinmyreqs=pinmyreqs:pinmyreqs',
+            'unpinmyreqs=pinmyreqs:unpinmyreqs',
+        ],
+    },
+    packages=['pinmyreqs'],
+    zip_safe=False
+)
