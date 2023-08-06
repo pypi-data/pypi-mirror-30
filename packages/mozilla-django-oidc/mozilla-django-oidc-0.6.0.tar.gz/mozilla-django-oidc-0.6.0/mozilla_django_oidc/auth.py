08f2c1115bd570c11df6f498b7d2ef2390dc3c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:03 +0000

    Add package mozilla-django-oidc/0.6.0/mozilla-django-oidc-0.6.0.tar.gz

---
+++ packages/mozilla-django-oidc/mozilla-django-oidc-0.6.0.tar.gz/mozilla-django-oidc-0.6.0/mozilla_django_oidc/auth.py
@@ -0,0 +1,227 @@
+import base64
+import hashlib
+import json
+import logging
+import requests
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+from django.core.exceptions import SuspiciousOperation, ImproperlyConfigured
+try:
+    from django.urls import reverse
+except ImportError:
+    # Django < 2.0.0
+    from django.core.urlresolvers import reverse
+from django.utils.encoding import force_bytes, smart_text, smart_bytes
+from django.utils.module_loading import import_string
+from django.utils import six
+
+from josepy.jwk import JWK
+from josepy.jws import JWS
+
+from mozilla_django_oidc.utils import absolutify, import_from_settings
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def default_username_algo(email):
+    """Generate username for the Django user.
+
+    :arg str/unicode email: the email address to use to generate a username
+
+    :returns: str/unicode
+
+    """
+    # bluntly stolen from django-browserid
+    # store the username as a base64 encoded sha224 of the email address
+    # this protects against data leakage because usernames are often
+    # treated as public identifiers (so we can't use the email address).
+    username = base64.urlsafe_b64encode(
+        hashlib.sha1(force_bytes(email)).digest()
+    ).rstrip(b'=')
+
+    return smart_text(username)
+
+
+class OIDCAuthenticationBackend(ModelBackend):
+    """Override Django's authentication."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize settings."""
+        self.OIDC_OP_TOKEN_ENDPOINT = import_from_settings('OIDC_OP_TOKEN_ENDPOINT')
+        self.OIDC_OP_USER_ENDPOINT = import_from_settings('OIDC_OP_USER_ENDPOINT')
+        self.OIDC_RP_CLIENT_ID = import_from_settings('OIDC_RP_CLIENT_ID')
+        self.OIDC_RP_CLIENT_SECRET = import_from_settings('OIDC_RP_CLIENT_SECRET')
+        self.OIDC_RP_SIGN_ALGO = import_from_settings('OIDC_RP_SIGN_ALGO', 'HS256')
+        self.OIDC_RP_IDP_SIGN_KEY = import_from_settings('OIDC_RP_IDP_SIGN_KEY', None)
+
+        if self.OIDC_RP_SIGN_ALGO.startswith('RS') and self.OIDC_RP_IDP_SIGN_KEY is None:
+            raise ImproperlyConfigured('IDP Signing key not provided with RS signing algorithm')
+
+        self.UserModel = get_user_model()
+
+    def filter_users_by_claims(self, claims):
+        """Return all users matching the specified email."""
+        email = claims.get('email')
+        if not email:
+            return self.UserModel.objects.none()
+        return self.UserModel.objects.filter(email__iexact=email)
+
+    def verify_claims(self, claims):
+        """Verify the provided claims to decide if authentication should be allowed."""
+        return True
+
+    def create_user(self, claims):
+        """Return object for a newly created user account."""
+        # bluntly stolen from django-browserid
+        # https://github.com/mozilla/django-browserid/blob/master/django_browserid/auth.py
+
+        username_algo = import_from_settings('OIDC_USERNAME_ALGO', None)
+        email = claims.get('email')
+        if not email:
+            return None
+
+        if username_algo:
+            if isinstance(username_algo, six.string_types):
+                username_algo = import_string(username_algo)
+            username = username_algo(email)
+        else:
+            username = default_username_algo(email)
+
+        return self.UserModel.objects.create_user(username, email)
+
+    def _verify_jws(self, payload, key):
+        """Verify the given JWS payload with the given key and return the payload"""
+
+        jws = JWS.from_compact(payload)
+        jwk = JWK.load(key)
+        if not jws.verify(jwk):
+            msg = 'JWS token verification failed.'
+            raise SuspiciousOperation(msg)
+
+        try:
+            alg = jws.signature.combined.alg.name
+            if alg != self.OIDC_RP_SIGN_ALGO:
+                msg = 'The specified alg value is not allowed'
+                raise SuspiciousOperation(msg)
+        except KeyError:
+            msg = 'No alg value found in header'
+            raise SuspiciousOperation(msg)
+
+        return jws.payload
+
+    def verify_token(self, token, **kwargs):
+        """Validate the token signature."""
+        nonce = kwargs.get('nonce')
+
+        if self.OIDC_RP_SIGN_ALGO.startswith('RS'):
+            key = self.OIDC_RP_IDP_SIGN_KEY
+        else:
+            key = self.OIDC_RP_CLIENT_SECRET
+
+        # Verify the token
+        verified_token = self._verify_jws(
+            force_bytes(token),
+            # Use smart_bytes here since the key string comes from settings.
+            smart_bytes(key),
+        )
+        # The 'verified_token' will always be a byte string since it's
+        # the result of base64.urlsafe_b64decode().
+        # The payload is always the result of base64.urlsafe_b64decode().
+        # In Python 3 and 2, that's always a byte string.
+        # In Python3.6, the json.loads() function can accept a byte string
+        # as it will automagically decode it to a unicode string before
+        # deserializing https://bugs.python.org/issue17909
+        token_nonce = json.loads(verified_token.decode('utf-8')).get('nonce')
+
+        if import_from_settings('OIDC_USE_NONCE', True) and nonce != token_nonce:
+            msg = 'JWT Nonce verification failed.'
+            raise SuspiciousOperation(msg)
+        return True
+
+    def authenticate(self, **kwargs):
+        """Authenticates a user based on the OIDC code flow."""
+
+        self.request = kwargs.pop('request', None)
+        if not self.request:
+            return None
+
+        state = self.request.GET.get('state')
+        code = self.request.GET.get('code')
+        nonce = kwargs.pop('nonce', None)
+        session = self.request.session
+
+        if not code or not state:
+            raise SuspiciousOperation('Code or state not found.')
+
+        token_payload = {
+            'client_id': self.OIDC_RP_CLIENT_ID,
+            'client_secret': self.OIDC_RP_CLIENT_SECRET,
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': absolutify(
+                self.request,
+                reverse('oidc_authentication_callback')
+            ),
+        }
+
+        # Get the token
+        response = requests.post(self.OIDC_OP_TOKEN_ENDPOINT,
+                                 data=token_payload,
+                                 verify=import_from_settings('OIDC_VERIFY_SSL', True))
+        response.raise_for_status()
+
+        # Validate the token
+        token_response = response.json()
+        id_token = token_response.get('id_token')
+        if self.verify_token(id_token, nonce=nonce):
+            access_token = token_response.get('access_token')
+
+            if import_from_settings('OIDC_STORE_ACCESS_TOKEN', False):
+                session['oidc_access_token'] = access_token
+
+            if import_from_settings('OIDC_STORE_ID_TOKEN', False):
+                session['oidc_id_token'] = id_token
+
+            user_response = requests.get(self.OIDC_OP_USER_ENDPOINT,
+                                         headers={
+                                             'Authorization': 'Bearer {0}'.format(access_token)
+                                         },
+                                         verify=import_from_settings('OIDC_VERIFY_SSL', True))
+            user_response.raise_for_status()
+
+            user_info = user_response.json()
+            email = user_info.get('email')
+
+            claims_verified = self.verify_claims(user_info)
+            if not claims_verified:
+                LOGGER.debug('Login failed: Claims verification for %s failed.', email)
+                return None
+
+            # email based filtering
+            users = self.filter_users_by_claims(user_info)
+
+            if len(users) == 1:
+                return users[0]
+            elif len(users) > 1:
+                # In the rare case that two user accounts have the same email address,
+                # log and bail. Randomly selecting one seems really wrong.
+                LOGGER.warn('Multiple users with email address %s.', email)
+                return None
+            elif import_from_settings('OIDC_CREATE_USER', True):
+                user = self.create_user(user_info)
+                return user
+            else:
+                LOGGER.debug('Login failed: No user with email %s found, and '
+                             'OIDC_CREATE_USER is False', email)
+                return None
+        return None
+
+    def get_user(self, user_id):
+        """Return a user based on the id."""
+
+        try:
+            return self.UserModel.objects.get(pk=user_id)
+        except self.UserModel.DoesNotExist:
+            return None
---
+++ packages/mozilla-django-oidc/mozilla-django-oidc-0.6.0.tar.gz/mozilla-django-oidc-0.6.0/mozilla_django_oidc/middleware.py
@@ -0,0 +1,129 @@
+import logging
+import time
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    # Python < 3
+    from urllib import urlencode
+
+import django
+try:
+    from django.urls import reverse
+except ImportError:
+    # Django < 2.0.0
+    from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.utils.crypto import get_random_string
+from django.utils.functional import cached_property
+
+from mozilla_django_oidc.utils import (
+    absolutify,
+    import_from_settings,
+    is_authenticated
+)
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+# Django 1.10 makes changes to how middleware work. In Django 1.10+, we want to
+# use the mixin so that our middleware works as is.
+if django.VERSION >= (1, 10):
+    from django.utils.deprecation import MiddlewareMixin
+else:
+    class MiddlewareMixin(object):
+        pass
+
+
+class RefreshIDToken(MiddlewareMixin):
+    """Renews id_tokens after expiry seconds
+
+    For users authenticated with an id_token, we need to check that it's still
+    valid after a specific amount of time and if not, force them to
+    re-authenticate silently.
+
+    """
+
+    @cached_property
+    def exempt_urls(self):
+        """Generate and return a set of url paths to exempt from RefreshIDToken
+
+        This takes the value of ``settings.OIDC_EXEMPT_URLS`` and appends three
+        urls that mozilla-django-oidc uses. These values can be view names or
+        absolute url paths.
+
+        :returns: list of url paths (for example "/oidc/callback/")
+
+        """
+        exempt_urls = list(import_from_settings('OIDC_EXEMPT_URLS', []))
+        exempt_urls.extend([
+            'oidc_authentication_init',
+            'oidc_authentication_callback',
+            'oidc_logout',
+        ])
+
+        return set([
+            url if url.startswith('/') else reverse(url)
+            for url in exempt_urls
+        ])
+
+    def is_refreshable_url(self, request):
+        """Takes a request and returns whether it triggers a refresh examination
+
+        :arg HttpRequest request:
+
+        :returns: boolean
+
+        """
+        return (
+            request.method == 'GET' and
+            is_authenticated(request.user) and
+            not request.is_ajax() and
+            request.path not in self.exempt_urls
+        )
+
+    def process_request(self, request):
+        if not self.is_refreshable_url(request):
+            LOGGER.debug('request is not refreshable')
+            return
+
+        expiration = request.session.get('oidc_id_token_expiration', 0)
+        now = time.time()
+        if expiration > now:
+            # The id_token is still valid, so we don't have to do anything.
+            LOGGER.debug('id token is still valid (%s > %s)', expiration, now)
+            return
+
+        LOGGER.debug('id token has expired')
+        # The id_token has expired, so we have to re-authenticate silently.
+        auth_url = import_from_settings('OIDC_OP_AUTHORIZATION_ENDPOINT')
+        client_id = import_from_settings('OIDC_RP_CLIENT_ID')
+        state = get_random_string(import_from_settings('OIDC_STATE_SIZE', 32))
+
+        # Build the parameters as if we were doing a real auth handoff, except
+        # we also include prompt=none.
+        params = {
+            'response_type': 'code',
+            'client_id': client_id,
+            'redirect_uri': absolutify(
+                request,
+                reverse('oidc_authentication_callback')
+            ),
+            'state': state,
+            'scope': import_from_settings('OIDC_RP_SCOPES', 'openid email'),
+            'prompt': 'none',
+        }
+
+        if import_from_settings('OIDC_USE_NONCE', True):
+            nonce = get_random_string(import_from_settings('OIDC_NONCE_SIZE', 32))
+            params.update({
+                'nonce': nonce
+            })
+            request.session['oidc_nonce'] = nonce
+
+        request.session['oidc_state'] = state
+        request.session['oidc_login_next'] = request.get_full_path()
+
+        query = urlencode(params)
+        redirect_url = '{url}?{query}'.format(url=auth_url, query=query)
+        return HttpResponseRedirect(redirect_url)
