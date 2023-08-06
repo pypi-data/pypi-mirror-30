9bea8d362ca9c9c16d77de909130ae39627395e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:24 +0000

    Add package ybc-creditcard-ocr/1.0.1/ybc_creditcard_ocr-1.0.1.tar.gz

---
+++ packages/ybc-creditcard-ocr/ybc_creditcard_ocr-1.0.1.tar.gz/ybc_creditcard_ocr-1.0.1/ybc_creditcard_ocr/__init__.py
@@ -0,0 +1,1 @@
+from .ybc_creditcard_ocr import *
---
+++ packages/ybc-creditcard-ocr/ybc_creditcard_ocr-1.0.1.tar.gz/ybc_creditcard_ocr-1.0.1/ybc_creditcard_ocr/ybc_creditcard_ocr.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import base64
+import os
+
+
+
+def creditcard_info(filename=''):
+    '''识别银行卡信息'''
+    if not filename:
+        return -1
+
+    url = 'https://www.yuanfudao.com/tutor-ybc-course-api/creditCardOcr.php'
+    filepath = os.path.abspath(filename)
+    b64img= base64.b64encode(open(filepath, 'rb').read()).rstrip().decode('utf-8')
+    data = {}
+    data['b64img'] = b64img
+    r = requests.post(url, data=data)
+    res = r.json()
+    if res['ret'] == 0 and res['data']:
+        res_dict = {}
+        for val in res['data']['item_list'] :
+            res_dict[val['item']] = val['itemstring']
+        return res_dict
+    else:
+        return -1
+
+
+def main():
+    res = creditcard_info('1.jpg')
+    print(res)
+
+
+
+if __name__ == '__main__':
+    main()
