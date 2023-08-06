2904bbe3a83ba133bbffb9b15c772b8c51054b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:40 +0000

    Add package django-nap/0.30.11/django-nap-0.30.11.tar.gz

---
+++ packages/django-nap/django-nap-0.30.11.tar.gz/django-nap-0.30.11/nap/utils/__init__.py
@@ -0,0 +1,94 @@
+import json
+from cgi import parse_header, parse_multipart
+from inspect import isgenerator
+
+from django import VERSION
+from django.core.serializers.json import DjangoJSONEncoder
+from django.http import QueryDict
+
+from .. import http
+
+try:
+    from json import JSONDecodeError
+except ImportError:
+    JSONDecodeError = ValueError
+
+
+class JsonMixin:
+    '''
+    Common methods for handling JSON request data.
+    '''
+    CONTENT_TYPES = ['application/json', 'text/json']
+    JSON_DECODER = None
+
+    def get_request_data(self, default=None):
+        '''Retrieve data from request'''
+        from django.conf import settings
+        encoding = self.request.encoding or settings.DEFAULT_CHARSET
+
+        if VERSION < (1, 10):
+            content_type, content_params = parse_header(self.request.META.get('CONTENT_TYPE', ''))
+        else:
+            content_type = self.request.content_type
+            content_params = self.request.content_params
+
+        if content_type in self.CONTENT_TYPES:
+            if not self.request.body:
+                return default
+            try:
+                return self.loads(self.request.body.decode(encoding))
+            except JSONDecodeError:
+                raise http.BadRequest()
+
+        if self.request.method in ('PUT', 'PATCH'):
+            if content_type == 'application/x-www-form-urlencoded':
+                return QueryDict(self.request.body, encoding=encoding)
+            elif content_type == 'multipart/form-data':
+                return parse_multipart(self.request.body, content_params)
+
+        return self.request.POST
+
+    def loads(self, data, **kwargs):
+        '''Serialise data for responses.'''
+        kwargs.setdefault('cls', self.JSON_DECODER)
+        return json.loads(data, **kwargs)
+
+
+def flatten_errors(errors):
+    '''
+    Utility function to turn an ErrorDict into a dict of lists of strings.
+    '''
+    return {
+        field: [
+            error if isinstance(error, str) else (
+                error.message % error.params if error.params else error.message
+            )
+            for error in errors
+        ]
+        for field, errors in errors.items()
+    }
+
+
+class List(list):
+    def __init__(self, gen):
+        self.gen = gen
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self.gen)
+
+    def __bool__(self):
+        return True
+
+
+class NapJSONEncoder(DjangoJSONEncoder):
+
+    def default(self, o):
+        if isgenerator(o):
+            return List(o)
+        m = getattr(o, '__json__', None)
+        if callable(m):
+            return m()
+        return super().default(o)
---
+++ packages/django-nap/django-nap-0.30.11.tar.gz/django-nap-0.30.11/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as fin:
+    readme = fin.read()
+
+setup(
+    name='django-nap',
+    version='0.30.11',
+    description='A light REST tool for Django',
+    long_description=readme,
+    author='Curtis Maloney',
+    author_email='curtis@tinbrain.net',
+    url='http://github.com/funkybob/django-nap',
+    keywords=['django', 'json', 'rest', 'api'],
+    packages = find_packages(exclude=('tests*',)),
+    zip_safe=False,
+    classifiers = [
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        # 'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    requires = [
+        'Django (>=1.8)',
+    ],
+    install_requires = [
+        'Django>=1.8',
+    ],
+)
