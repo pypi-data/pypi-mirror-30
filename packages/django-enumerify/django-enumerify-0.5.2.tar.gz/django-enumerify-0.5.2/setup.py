33b9d6a2710b740f239930318947a4f878832031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:13 +0000

    Add package django-enumerify/0.5.2/django-enumerify-0.5.2.tar.gz

---
+++ packages/django-enumerify/django-enumerify-0.5.2.tar.gz/django-enumerify-0.5.2/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-enumerify',
+    version='0.5.2',
+    author=u'Faisal Mahmud',
+    author_email='faisal@willandskill.se',
+    packages=find_packages(),
+    url='http://bitbucket.org/willandskill/django-enumerify',
+    license='BSD licence, see LICENCE.txt',
+    description='Simple Enums for Django when working with choices in model fields.',
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    test_suite="testproject.runtests.runtests",
+)
