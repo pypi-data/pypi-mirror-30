1f94f46303772991290ded1f5d852a6f558bc931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:48 +0000

    Add package py-edamam/0.1.1/py_edamam-0.1.1.tar.gz

---
+++ packages/py-edamam/py_edamam-0.1.1.tar.gz/py_edamam-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='py_edamam',
+    version='0.1.1',
+    packages=['py_edamam'],
+    url='https://github.com/JarbasAl/py_edamam',
+    license='MIT',
+    author='jarbasAi',
+    author_email='jarbasai@mailfence.com',
+    description='edamam api'
+)
