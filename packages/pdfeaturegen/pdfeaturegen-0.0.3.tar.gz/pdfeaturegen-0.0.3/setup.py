fea2bf69485c11a4afbb61541da9b449b5e956ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:04 +0000

    Add package pdfeaturegen/0.0.3/pdfeaturegen-0.0.3.tar.gz

---
+++ packages/pdfeaturegen/pdfeaturegen-0.0.3.tar.gz/pdfeaturegen-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(name='pdfeaturegen',
+      version='0.0.3',
+      description='Supporting functions to generate features in Pandas Dataframe for Machine Learning',
+      url='https://github.com/ivanfoong/pdfeaturegen',
+      author='Ivan Foong',
+      author_email='vonze21@gmail.com',
+      license='MIT',
+      packages=['pdfeaturegen'],
+      install_requires=[
+          'pandas',
+      ],
+      zip_safe=False)
+
