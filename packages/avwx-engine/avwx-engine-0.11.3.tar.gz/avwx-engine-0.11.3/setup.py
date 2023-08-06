1949153f3822df0d475235bc7adb6ec453c16abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:53 +0000

    Add package avwx-engine/0.11.3/avwx-engine-0.11.3.tar.gz

---
+++ packages/avwx-engine/avwx-engine-0.11.3.tar.gz/avwx-engine-0.11.3/setup.py
@@ -0,0 +1,26 @@
+"""
+Michael duPont - michael@mdupont.com
+AVWX-Engine : setup.py
+
+avwx package metadata
+"""
+
+from setuptools import setup
+
+setup(
+    name='avwx-engine',
+    version='0.11.3',
+    description='Aviation weather report parsing library',
+    url='https://github.com/flyinactor91/AVWX-Engine',
+    author='Michael duPont',
+    author_email='michael@mdupont.com',
+    license='MIT',
+    packages=[
+        'avwx'
+    ],
+    package_data={
+        'avwx': [
+            'stations.json'
+        ]
+    }
+)
