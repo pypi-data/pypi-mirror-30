dfd528da25e367436f62c0d200b2d396dea9d39b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:08 +0000

    Add package http-test-client/0.3.2/http_test_client-0.3.2.tar.gz

---
+++ packages/http-test-client/http_test_client-0.3.2.tar.gz/http_test_client-0.3.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+setup(
+    name='http_test_client',
+    version='0.3.2',
+    description='Library to simplify writing HTTP REST service integration tests',
+    long_description=open('README.rst').read(),
+    author='Maxim Kulkin',
+    author_email='maxim.kulkin@gmail.com',
+    url='https://github.com/maximkulkin/http-test-client',
+    license='MIT',
+    py_modules=['http_test_client'],
+    install_requires=[
+        'requests>=2.14',
+        'six>=1.10',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=[
+        'pytest',
+        'mockito>=1.0',
+        'responses>=0.5',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
