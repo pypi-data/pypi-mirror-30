8406e859956053486cfa70092a2ddf3ee694bd1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:35 +0000

    Add package django-ultracache/1.11.10/django_ultracache-1.11.10-py2.7.egg

---
+++ packages/django-ultracache/django_ultracache-1.11.10-py2.7.egg/ultracache/tasks.py
@@ -0,0 +1,42 @@
+import json
+import urllib
+import urlparse
+
+from celery import shared_task
+try:
+    import pika
+    DO_TASK = True
+except ImportError:
+    DO_TASK = False
+
+from django.conf import settings
+
+
+@shared_task(max_retries=3, ignore_result=True)
+def broadcast_purge(path, headers=None):
+    if not DO_TASK:
+        raise RuntimeError("Library pika==0.10.0 not found")
+
+    try:
+        url = settings.ULTRACACHE["rabbitmq-url"]
+    except (AttributeError, KeyError):
+        # Use same host as celery. Pika requires the path to be url
+        # encoded. A typical broker URL setting remains effectively
+        # unchanged but as soon as sub-paths are encountered this encoding
+        # becomes necessary.
+        parsed = urlparse.urlparse(settings.CELERY_BROKER_URL)
+        url = "%s://%s/%s" % (
+            parsed.scheme,
+            parsed.netloc,
+            urllib.quote(parsed.path[1:], safe="")
+        )
+    connection = pika.BlockingConnection(pika.URLParameters(url))
+    channel = connection.channel()
+    channel.exchange_declare(exchange="purgatory", type="fanout")
+    channel.basic_publish(
+        exchange="purgatory",
+        routing_key="",
+        body=json.dumps({"path": path, "headers": headers or {}})
+    )
+    connection.close()
+    return True
