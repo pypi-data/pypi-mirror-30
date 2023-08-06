d07bdc0974dc411894837d3619cd06be2f328ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:43 +0000

    Add package nunit/0.0.2a0/nunit-0.0.2a0.tar.gz

---
+++ packages/nunit/nunit-0.0.2a0.tar.gz/nunit-0.0.2a0/nunit/constant.py
@@ -0,0 +1,10 @@
+"""Collection of physical constants"""
+
+## Define or import fundamental constants
+c = 299792458.0
+m_e = 9.10938291e-31
+e = 1.602176565e-19
+hbar = 1.054571726e-34
+k_e = 8.9875517873681e9
+from math import pi
+epsil0 = 8.854187817620e-12
