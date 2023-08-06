411f9744bc9c07e00cad77f7227cb460460bfa65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:38 +0000

    Add package dockerstache/0.0.14/dockerstache-0.0.14.tar.gz

---
+++ packages/dockerstache/dockerstache-0.0.14.tar.gz/dockerstache-0.0.14/src/dockerstache/__init__.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""
+_dockerstache_
+
+util package for rendering docker files from mustache templates
+
+"""
+import sys
+import logging
+import logging.handlers
+__version__ = "0.0.14"
+
+
+_LOGGER = {
+    "logger": None
+}
+
+
+def get_logger():
+    """
+    _get_logger_
+
+    Get package logger instance
+
+    """
+    if _LOGGER['logger'] is None:
+        _LOGGER['logger'] = setup_logger()
+    return _LOGGER['logger']
+
+
+def setup_logger():
+    """
+    setup basic logger
+    """
+    logger = logging.getLogger('dockerstache')
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    return logger
