45855b3288eafa33742b978c9bf68ea8e57474be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:03 +0000

    Add package micromagneticmodel/0.8/micromagneticmodel-0.8.tar.gz

---
+++ packages/micromagneticmodel/micromagneticmodel-0.8.tar.gz/micromagneticmodel-0.8/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.rst") as f:
+    readme = f.read()
+
+setuptools.setup(
+    name="micromagneticmodel",
+    version="0.8",
+    description="A Python-based micromagnetic model.",
+    long_description=readme,
+    url="https://joommf.github.io",
+    author="Marijan Beg and Hans Fangohr",
+    author_email="jupyteroommf@gmail.com",
+    packages=setuptools.find_packages(),
+    install_requires=["discretisedfield"],
+    classifiers=["Development Status :: 3 - Alpha",
+                 "License :: OSI Approved :: BSD License",
+                 "Topic :: Scientific/Engineering :: Physics",
+                 "Intended Audience :: Science/Research",
+                 "Programming Language :: Python :: 3 :: Only"]
+)
