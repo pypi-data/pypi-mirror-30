e5990e606345734db6350d0dfc8c89eb0e239c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:27 +0000

    Add package TrainHelper/0.0.3.18/TrainHelper-0.0.3.18-py3-none-any.whl

---
+++ packages/TrainHelper/TrainHelper-0.0.3.18-py3-none-any.whl/TrainHelper/callbacks/repeater.py
@@ -0,0 +1,27 @@
+from keras.callbacks import Callback
+import datetime
+import logging
+
+
+class Repeater(Callback):
+    def __init__(self, config, queuer):
+        super(Callback, self).__init__()
+        self.start = datetime.datetime.now()
+        self.config = config
+        self.queuer = queuer
+
+    def on_train_begin(self, logs=None):
+        logging.info('SETTING START TIME')
+        self.start = datetime.datetime.now()
+        logging.info('Start time {}'.format(self.start))
+
+    def on_epoch_end(self, epoch, logs=None):
+        now = datetime.datetime.now()
+        delta = (now - self.start).seconds // 60
+        time_max = self.config.Repeater.Time
+        print(time_max)
+
+        if delta >= time_max * 60:  # time_max hrs
+            logging.info('Delta is bigger than expected')
+            self.queuer.queue(self.config, self.config.config_name, self.config.script, 'Y')
+            self.model.stop_training = True
---
+++ packages/TrainHelper/TrainHelper-0.0.3.18-py3-none-any.whl/TrainHelper/configuration/reader.py
@@ -0,0 +1,75 @@
+import os
+import configparser
+import logging
+import ntpath
+from .config import Config, ConfigSection
+
+
+class Reader:
+
+    @staticmethod
+    def path_leaf(path):
+        head, tail = ntpath.split(path)
+        return tail or ntpath.basename(head)
+
+    @staticmethod
+    def validate_path(path):
+        possible = ["", "config", "configs", "conf"]
+        for p in possible:
+            src = "{}/{}".format(p, path)
+            if os.path.exists(src):
+                return src
+        return path
+
+    @staticmethod
+    def read(config_path):
+        config_data = configparser.ConfigParser()
+        config_data.optionxform = str
+        config_path = Reader.validate_path(config_path)
+        config_data.read(config_path)
+        file_name = Reader.path_leaf(config_path)
+        base_text = 'Reading configuration from {}'.format(config_path)
+        logging.info('{} as object'.format(base_text))
+        configuration = Config(file_name)
+        sections = config_data.sections()
+        for section_name in sections:
+            options = config_data.options(section_name)
+            section = ConfigSection()
+            for option in options:
+                value = config_data[section_name][option]
+                val = Reader.__parse(value)
+                print('Seetting for {}'.format(option.lower()))
+                setattr(section, option.lower(), val)
+            print('SETTING {}'.format(section_name.lower()))
+            setattr(configuration, section_name.lower(), section)
+        return configuration
+
+    @staticmethod
+    def __parse(s: str):  # TODO: support more types
+        if s == 'True':
+            return True
+        if s == 'False':
+            return False
+        try:
+            return int(s)
+        except ValueError:
+            try:
+                return float(s)
+            except ValueError:
+                if s[0] == '[' and s[-1] == ']':
+                    return map(float, s.split(','))
+                else:
+                    return s  # return as a string
+
+    # @staticmethod
+    # def __configSectionMap(config, section):
+    #     _dict = {}
+    #     options = config.options(section)
+    #     for option in options:
+    #         try:
+    #             _dict[section+'.'+option] = Reader.__parse(config.get(section, option))
+    #         except Exception as ex:
+    #             logging.error(ex)
+    #             _dict[option] = None
+    #     return _dict
+
