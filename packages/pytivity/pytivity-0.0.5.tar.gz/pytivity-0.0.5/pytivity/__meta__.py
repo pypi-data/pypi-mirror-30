f0958f3594d2d47f308db0b45da0919463444df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:35 +0000

    Add package pytivity/0.0.5/pytivity-0.0.5.tar.gz

---
+++ packages/pytivity/pytivity-0.0.5.tar.gz/pytivity-0.0.5/pytivity/__meta__.py
@@ -0,0 +1,12 @@
+METADATA = {
+    "author": (
+        'Quentin Dawans <python@quentindawans.com>, '
+    ),
+    "author_email": 'python@quentindawans.com',
+    "description": 'Pytivity is a python3 command line manager for your KDE'
+                   ' activities.',
+    "url": "https://github.com/Ovvovy/pytivity",
+    "license": 'MIT',
+    "name": 'pytivity',
+    "version": '0.0.5',
+}
