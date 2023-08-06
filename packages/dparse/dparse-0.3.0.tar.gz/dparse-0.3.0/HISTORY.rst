e58a9e683e7716a869cc23c2a9142c88934455d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:40 +0000

    Add package dparse/0.3.0/dparse-0.3.0.tar.gz

---
+++ packages/dparse/dparse-0.3.0.tar.gz/dparse-0.3.0/HISTORY.rst
@@ -0,0 +1,32 @@
+=======
+History
+=======
+
+0.3.0 (2018-03-01)
+------------------
+
+* Added support for setup.cfg files (thanks @kexepal)
+* Dependencies from Pipfiles now include the section (thanks @paulortman)
+* Multiline requirements are now ignored if they are marked
+* Added experimental support for Pipfiles
+
+0.2.1 (2017-07-19)
+------------------
+
+* Internal refactoring
+
+0.2.0 (2017-07-19)
+------------------
+
+* Removed setuptools dependency
+
+
+0.1.1 (2017-07-14)
+------------------
+
+* Fixed a bug that was causing the parser to throw errors on invalid requirements.
+
+0.1.0 (2017-07-11)
+------------------
+
+* Initial, not much to see here.
---
+++ packages/dparse/dparse-0.3.0.tar.gz/dparse-0.3.0/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+import sys
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    "packaging",
+    "six",
+    "pyyaml",
+    "pipenv"
+]
+
+# make pytest-runner a conditional requirement, per: https://pypi.org/project/pytest-runner/
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+setup_requirements = [
+    # other setup requirements
+] + pytest_runner
+
+test_requirements = [
+    'pytest',
+]
+
+setup(
+    name='dparse',
+    version='0.3.0',
+    description="A parser for Python dependency files",
+    long_description=readme + '\n\n' + history,
+    author="Jannis Gebauer",
+    author_email='ja.geb@me.com',
+    url='https://github.com/jayfk/dparse',
+    packages=find_packages(include=['dparse']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='dparse',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
