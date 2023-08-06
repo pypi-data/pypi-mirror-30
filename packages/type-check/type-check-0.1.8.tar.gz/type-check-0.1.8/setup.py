a449240ded8cf7feef8f5e97bf1f59a8c3e9d0bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:34 +0000

    Add package type-check/0.1.8/type-check-0.1.8.tar.gz

---
+++ packages/type-check/type-check-0.1.8.tar.gz/type-check-0.1.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='type-check',
+    version='0.1.8',
+    description='Type check decorator for python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='type check safe',
+    packages=find_packages(),
+)
