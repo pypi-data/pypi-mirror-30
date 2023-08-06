7d2f06b0483b99b2b68eb364b3471ad7928c2fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:27 +0000

    Add package radis/0.1.11/radis-0.1.11-py2.py3-none-any.whl

---
+++ packages/radis/radis-0.1.11-py2.py3-none-any.whl/radis/tools/parser/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 26 11:52:15 2015
+
+Erwan Pannier
+EM2C, CentraleSupélec, 2015
+CNRS UPR 288
+
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from .hiparser import hit2df, get_molecule, get_molecule_identifier
+from .cdsd import cdsd2df
