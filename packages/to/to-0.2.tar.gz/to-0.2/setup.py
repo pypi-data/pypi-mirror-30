268cf18df3b6a11dd7496dbca2462ef8c033d72b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:16 +0000

    Add package to/0.2/to-0.2.tar.gz

---
+++ packages/to/to-0.2.tar.gz/to-0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name='to',
+    packages=['to'],  # this must be the same as the name above
+    package_dir={'to': 'to'},
+    package_data={'to': ['utils/*.py', 'data/*.py', 'net/*.py', 'layers/*.py']},
+    version='0.2',
+    description='A trainer for PyTorch',
+    author='Jacob Zhang',
+    author_email='r.c.cham@gmail.com',
+    url='https://github.com/chamrc/to',  # use the URL to the github repo
+    download_url='https://github.com/chamrc/to/archive/0.2.tar.gz',  # I'll explain this in a second
+    keywords=['testing', 'logging', 'example'],  # arbitrary keywords
+    classifiers=[],
+)
