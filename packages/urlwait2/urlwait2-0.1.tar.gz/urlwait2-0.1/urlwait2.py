538891bbe439ad3c9b69de8a22d0c84ff443db2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:53 +0000

    Add package urlwait2/0.1/urlwait2-0.1.tar.gz

---
+++ packages/urlwait2/urlwait2-0.1.tar.gz/urlwait2-0.1/urlwait2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import time
+
+import click
+from requests import get
+
+
+@click.command()
+@click.argument('url')
+def main(url):
+    if not url.startswith('http'):
+        url = 'http://' + url
+    print(f'urlwait2: waiting for {url}')
+
+    attempt = 1
+    while True:
+        try:
+            get(url, allow_redirects=True).raise_for_status()
+        except Exception as e:
+            print(f'{e} Attempt {attempt} failed, retrying in 2 seconds')
+            attempt += 1
+            time.sleep(2)
+        else:
+            print(f'urlwait2: successfully connected to {url}')
+            break
+
+
+if __name__ == '__main__':
+    main()
