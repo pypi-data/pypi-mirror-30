679c2ceadee00fb5760a6b45b6661ab084dfed9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:10 +0000

    Add package stripy/0.4.0/stripy-0.4.0.tar.gz

---
+++ packages/stripy/stripy-0.4.0.tar.gz/stripy-0.4.0/stripy/__init__.py
@@ -0,0 +1,21 @@
+"""
+Copyright 2017 Louis Moresi, Ben Mather
+
+Stripy is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or any later version.
+
+Stripy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Stripy.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from .spherical import sTriangulation
+from .cartesian import Triangulation
+from . import spherical_meshes
+from . import cartesian_meshes
+from . import documentation
