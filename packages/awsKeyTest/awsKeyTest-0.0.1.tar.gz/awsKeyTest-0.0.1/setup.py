14984f51f0641867a7b38acd36d0bd1e75b01e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:19 +0000

    Add package awsKeyTest/0.0.1/awsKeyTest-0.0.1.tar.gz

---
+++ packages/awsKeyTest/awsKeyTest-0.0.1.tar.gz/awsKeyTest-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='awsKeyTest',
+    version='0.0.1',
+    description='This is an aws key.',
+    url='https://github.com/awskey',
+    author='Dylan Ayrey',
+    author_email='dylanayrey@gmail.com',
+    license='GNU',
+    packages = find_packages(),
+)
