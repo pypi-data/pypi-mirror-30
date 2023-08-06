ea08fe35665458d0746e130b26d07d8672059131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:47 +0000

    Add package xp-last/1.0.0/xp-last-1.0.0.tar.gz

---
+++ packages/xp-last/xp-last-1.0.0.tar.gz/xp-last-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'xp-last',
+    version= '1.0.0',
+    url= 'http://www.baidu.com',
+    author= 'xiaopeng',
+    author_email='zhangxiao_peng@163.com',
+    description= 'start study publish.',
+    py_modules = ['xp-last']
+)
