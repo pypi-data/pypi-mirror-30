66473a7ebea85e9e0faebe84741d5bc3d735a14b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:14 +0000

    Add package klab-autotime/0.0.2/klab-autotime-0.0.2.tar.gz

---
+++ packages/klab-autotime/klab-autotime-0.0.2.tar.gz/klab-autotime-0.0.2/setup.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import, division, print_function
+
+from setuptools import setup, find_packages
+
+import versioneer
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+
+setup(
+    name='klab-autotime',
+    version='0.0.2',
+    # cmdclass=versioneer.get_cmdclass(),
+    author='Yiran Tao',
+    author_email='yt2487@columbia.edu',
+    url='https://github.com/gerard0315/klab-autotime',
+    description='Modified from Phillip Cloud project Time everything in IPython',
+    long_description=long_description,
+    license='Apache',
+    install_requires=['ipython'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Utilities',
+    ],
+    packages=find_packages(),
+)
