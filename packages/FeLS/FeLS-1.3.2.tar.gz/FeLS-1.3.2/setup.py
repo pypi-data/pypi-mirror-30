727b3f47582e64342694bd9eae21e1aa04cfa553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:31 +0000

    Add package FeLS/1.3.2/FeLS-1.3.2.tar.gz

---
+++ packages/FeLS/FeLS-1.3.2.tar.gz/FeLS-1.3.2/setup.py
@@ -0,0 +1,24 @@
+try:
+    from setuptools import setup
+
+    setup_kwargs = {'entry_points': {'console_scripts': ['fels=fels.fels:main']}}
+except ImportError:
+    from distutils.core import setup
+
+    setup_kwargs = {'scripts': ['bin/fels']}
+
+tag = '1.3.2'
+
+setup(name='FeLS',
+      version=tag,
+      py_modules=['fels'],
+      description='Fetch Landsat & Sentinel Data from google cloud',
+      url='https://github.com/vascobnunes/fetchLandsatSentinelFromGoogleCloud',
+      author='dgketchum',
+      author_email='dgketchum@gmail.com',
+      license='GPL',
+      zip_safe=False,
+      packages=['fels'],
+      install_requires=['numpy'],
+      dependency_links=['https://www.conan.io/source/Gdal/2.1.3/osechet/stable'],
+      **setup_kwargs)
