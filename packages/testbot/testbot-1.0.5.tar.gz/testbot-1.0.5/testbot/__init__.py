0d2a21561aa5fccbbe75267a0fc49ce9c1e5c33b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:46 +0000

    Add package testbot/1.0.5/testbot-1.0.5.tar.gz

---
+++ packages/testbot/testbot-1.0.5.tar.gz/testbot-1.0.5/testbot/__init__.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import time
+from os import makedirs
+from os.path import join, dirname
+
+from testbot.settings import settings
+
+__version__ = "1.0.5"
+
+try:
+    makedirs("/log")
+except FileExistsError:
+    pass
+
+TOKEN = ""
+try:
+    TOKEN = settings["bots"]["vesh_bot"]
+except:
+    pass
+
+
+def bot_command(func):
+    """
+    Декоратор-интерфейс для команд бота
+    """
+    if "_id" not in func.__code__.co_varnames:
+        raise NameError(
+            "Интерфейс функции не соотетствует bot_command функции")
+    annotations = func.__annotations__
+    if "_id" in annotations:
+        if 'int' not in str(annotations["_id"]):
+            raise NameError("аннотация не соответствует int*")
+    else:
+        raise NameError("нет аннотации для параметра _id")
+
+    async def wrapped(self, _id, **kwargs):
+        await func(self, _id, **kwargs)
+
+    wrapped.__name__ = func.__name__
+    wrapped.__doc__ = "<bot_command>"
+    return wrapped
+
+
+def bot_callback(func):
+    """
+    Декоратор-интерфейс для callback-функций бота
+    """
+    if "msg" not in func.__code__.co_varnames:
+        raise NameError(
+            "Интерфейс функции не соотетствует bot_callback функции")
+    annotations = func.__annotations__
+    if "msg" in annotations:
+        if 'dict' not in str(annotations["msg"]):
+            raise NameError("аннотация не соответствует dict*")
+    else:
+        raise NameError("нет аннотации для параметра msg")
+
+    async def wrapped(self, msg, **kwargs):
+        await func(self, msg, **kwargs)
+
+    wrapped.__name__ = func.__name__
+    wrapped.__doc__ = "<bot_callback>"
+    return wrapped
+
+
+def async_logger(file: str):
+    def wrapper(func):
+        async def wrapped(*args, **kwargs):
+            with open(join(dirname(__file__), file), "a", encoding="utf-16") as f:
+                f.write(time.ctime(time.time()) + "\n")
+                f.write(func.__name__ + " ")
+                f.write("args: " + str(args) + " kwargs: " + str(kwargs) + "\n")
+                try:
+                    r = await func(*args, **kwargs)
+                    f.write(f"returned: {r}")
+                    f.write("\nOk\n\n")
+                except Exception as e:
+                    f.write("exception: " + str(e))
+                    f.write("\nFailed\n\n")
+                    raise e
+        wrapped.__name__ = func.__name__
+        wrapped.__annotations__ = func.__annotations__
+        wrapped.__doc__ = func.__doc__
+        return wrapped
+    return wrapper
---
+++ packages/testbot/testbot-1.0.5.tar.gz/testbot-1.0.5/testbot/wolfram_alpha.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+import asyncio
+import xml.etree.ElementTree as ET
+import aiohttp
+
+from testbot.settings import settings
+
+try:
+    APP_ID = settings["wolfram_alpha"]["id"]
+except:
+    pass
+
+
+@asyncio.coroutine
+def wolfram_xml2dict(xml_string: str):
+    """
+    Преобразует xml-строку, полученную от API wolfram alpha в словарь
+    :param xml_string: xml-строка
+    :return: преобразованный словарь
+    """
+    root = ET.fromstring(xml_string)
+    res = root.attrib
+    res["pods"] = []
+    for pod in root:
+        res["pods"].append({pod.tag: pod.attrib})
+        res["pods"][-1][pod.tag]["subpod"] = []
+        for subpod in pod:
+            res["pods"][-1][pod.tag]["subpod"] = subpod.attrib
+            for child in subpod:
+                res["pods"][-1][pod.tag]["subpod"][child.tag] = child.attrib
+    return res
+
+
+async def get_plot_link(query_string: str):
+    """
+    Получает график функции с помощью wolfram alpha API
+    :param query_string: строка, содержащая функцию
+    :return: ссылка на график
+    """
+    app_id = "V9K3LJ-Y3WJHTRWYL"
+    link = f"http://api.wolframalpha.com/v2/query?input=Plot[{query_string}]&appid={APP_ID}".replace("+", "%2B")
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                text = await resp.text()
+                data = await wolfram_xml2dict(text)
+    except aiohttp.client_exceptions.ClientConnectorError as e:
+        return None
+    if "pods" in data:
+        for pod in data["pods"]:
+            if "pod" in pod and "title" in pod["pod"] and (pod["pod"]["title"] == "Plot" or
+                                                           pod["pod"]["title"] == "Plots" or
+                                                           pod["pod"]["title"] == "Implicit plot"):
+
+                try:
+                    return pod["pod"]["subpod"]["img"]["src"]
+                except KeyError:
+                    return None
+    return None
+
+
+if __name__ == "__main__":
+    async def test():
+        print(await get_plot_link("y=x^3"))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test())
