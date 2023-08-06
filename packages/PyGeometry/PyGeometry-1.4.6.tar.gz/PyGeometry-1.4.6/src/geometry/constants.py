2372a16934882e896f8b6f45eca4972a562a7854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:15 +0000

    Add package PyGeometry/1.4.6/PyGeometry-1.4.6.tar.gz

---
+++ packages/PyGeometry/PyGeometry-1.4.6.tar.gz/PyGeometry-1.4.6/src/geometry/constants.py
@@ -0,0 +1,6 @@
+
+
+class GeometryConstants(object):
+    atol_zero_norm = 1e-7
+    rtol_SE2_from_SE3 = 1e-4
+
---
+++ packages/PyGeometry/PyGeometry-1.4.6.tar.gz/PyGeometry-1.4.6/src/geometry/distances/__init__.py
@@ -0,0 +1,3 @@
+''' Utils for dealing with distance matrices '''
+from .spherical import *
+from .generic import *
