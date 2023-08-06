6d5c5c9767cd647424a46d761f702880877c3815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:23 +0000

    Add package Royal-Mail-Rest-API/0.0.3/Royal%20Mail%20Rest%20API-0.0.3.tar.gz

---
+++ packages/Royal-Mail-Rest-API/Royal%20Mail%20Rest%20API-0.0.3.tar.gz/Royal Mail Rest API-0.0.3/royal_mail_rest_api/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Royal Mail Rest API."""
+
+__author__ = """Alex Hellier"""
+__email__ = 'alex.hellier@gmail.com'
+__version__ = '0.0.1'
+
+
+# from .api import RoyalMailBaseClass
+# from .tracking import TrackingApi
+# from .cli import cli
---
+++ packages/Royal-Mail-Rest-API/Royal%20Mail%20Rest%20API-0.0.3.tar.gz/Royal Mail Rest API-0.0.3/royal_mail_rest_api/cli.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for royal_mail_rest_api."""
+import sys
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for royal_mail_rest_api."""
+    click.echo("Replace this message by putting your code into "
+               "royal_mail_rest_api.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
