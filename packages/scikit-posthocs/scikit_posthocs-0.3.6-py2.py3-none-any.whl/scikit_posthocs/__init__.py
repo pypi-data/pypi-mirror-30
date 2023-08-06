1953a9f2fe5bd7dd11b86e0663c9827f0e256919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:12 +0000

    Add package scikit-posthocs/0.3.6/scikit_posthocs-0.3.6-py2.py3-none-any.whl

---
+++ packages/scikit-posthocs/scikit_posthocs-0.3.6-py2.py3-none-any.whl/scikit_posthocs/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.3.5'
+
+from scikit_posthocs._posthocs \
+    import posthoc_conover, posthoc_dunn, posthoc_nemenyi,\
+           posthoc_ttest, posthoc_tukey_hsd, posthoc_mannwhitney,\
+           posthoc_nemenyi_friedman, posthoc_conover_friedman,\
+           posthoc_quade, posthoc_durbin, posthoc_vanwaerden, posthoc_wilcoxon,\
+           posthoc_anderson
+
+from scikit_posthocs._plotting \
+    import sign_array, sign_plot, sign_table
+
+from scikit_posthocs._outliers \
+    import outliers_iqr, outliers_grubbs, outliers_tietjen, outliers_gesd
