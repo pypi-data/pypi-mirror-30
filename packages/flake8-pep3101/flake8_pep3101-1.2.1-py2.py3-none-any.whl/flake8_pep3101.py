c66a6e214f79caeca17c991051d8f2bad41ae96a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:54 +0000

    Add package flake8-pep3101/1.2.1/flake8_pep3101-1.2.1-py2.py3-none-any.whl

---
+++ packages/flake8-pep3101/flake8_pep3101-1.2.1-py2.py3-none-any.whl/flake8_pep3101.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import ast
+import pycodestyle
+
+
+class Flake8Pep3101(object):
+
+    name = 'flake8_pep3101'
+    version = '1.2.1'
+    message = 'S001 found modulo formatter'
+
+    def __init__(self, tree, filename):
+        self.filename = filename
+        self.tree = tree
+
+    def run(self):
+        if self.filename == 'stdin':
+            lines = pycodestyle.stdin_get_value()
+            tree = ast.parse(lines)
+        elif self.tree:
+            tree = self.tree
+        else:
+            with open(self.filename) as f:
+                tree = ast.parse(f.read())
+
+        for stmt in ast.walk(tree):
+            if isinstance(stmt, ast.BinOp) and \
+                    isinstance(stmt.op, ast.Mod):
+                if isinstance(stmt.left, ast.Num) or \
+                        isinstance(stmt.right, ast.Num):
+                    continue
+
+                if isinstance(stmt.left, (ast.Str, ast.Name)):
+                    yield (
+                        stmt.lineno,
+                        stmt.col_offset,
+                        self.message,
+                        type(self),
+                    )
