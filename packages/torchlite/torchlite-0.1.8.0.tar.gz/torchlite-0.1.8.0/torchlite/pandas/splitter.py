626e6945d26a2d5c03b455dd058808c06f8bff72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:26 +0000

    Add package torchlite/0.1.8.0/torchlite-0.1.8.0.tar.gz

---
+++ packages/torchlite/torchlite-0.1.8.0.tar.gz/torchlite-0.1.8.0/torchlite/pandas/splitter.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import numpy as np
+
+
+def time_split(df, val_start_date, val_stop_date, split_field=None):
+    """
+    Split on a datetime64 field. Take the date between val_start_date and val_stop_date as validation split
+    and leave the rest for as the train set
+    Args:
+        df (DataFrame): A pandas DataFrame
+        val_start_date (datetime): A datetime date (E.g datetime.datetime(2014, 8, 1))
+        val_stop_date (datetime): A datetime date (E.g datetime.datetime(2014, 9, 17))
+        split_field (str, None): The column name contained the datetime64 values, or None to use the index
+
+    Returns:
+
+    """
+    if split_field is None:
+        val_df = df[(df.index <= val_stop_date) & (df.index >= val_start_date)]
+    else:
+        val_df = df[(df[split_field] <= val_stop_date) & (df[split_field] >= val_start_date)]
+
+    train_df = df.drop(val_df.index)
+    return train_df, val_df
