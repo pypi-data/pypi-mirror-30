08d033c168ed70d18478401156ff9f3c52bbc2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:21 +0000

    Add package shap/0.14.0/shap-0.14.0.tar.gz

---
+++ packages/shap/shap-0.14.0.tar.gz/shap-0.14.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, Extension
+import numpy
+
+# to publish use:
+# > python setup.py sdist upload
+# which depends on ~/.pypirc
+
+setup(
+    name='shap',
+    version='0.14.0',
+    description='A unified approach to explain the output of any machine learning model.',
+    url='http://github.com/slundberg/shap',
+    author='Scott Lundberg',
+    author_email='slund1@cs.washington.edu',
+    license='MIT',
+    packages=['shap', 'shap.explainers'],
+    install_requires=['numpy', 'scipy', 'iml>=0.6.0', 'scikit-learn', 'matplotlib', 'pandas', 'tqdm'],
+    test_suite='nose.collector',
+    tests_require=['nose', 'xgboost'],
+    ext_modules = [Extension('shap._cext', sources=['shap/_cext.cc'], include_dirs=[numpy.get_include()])],
+    zip_safe=False
+)
