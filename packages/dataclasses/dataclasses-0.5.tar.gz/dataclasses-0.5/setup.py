d9dc3c0fe4458961f4c80007e239ee370ccb7ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:34 +0000

    Add package dataclasses/0.5/dataclasses-0.5.tar.gz

---
+++ packages/dataclasses/dataclasses-0.5.tar.gz/dataclasses-0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='dataclasses',
+    version='0.5',
+    description='An implementation of PEP 557: Data Classes',
+    url='https://github.com/ericvsmith/dataclasses',
+    author='Eric V. Smith',
+    author_email='eric@python.org',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    py_modules=['dataclasses']
+)
