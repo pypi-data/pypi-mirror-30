bbc7f7ecc2f35da9ad12d57e607ce9bd5c5cabc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:44 +0000

    Add package django-aadhaar/1.0.0.dev1/django-aadhaar-1.0.0.dev1.zip

---
+++ packages/django-aadhaar/django-aadhaar-1.0.0.dev1.zip/django-aadhaar-1.0.0.dev1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-aadhaar',
+    version='1.0.0.dev1',
+    packages=['django_aadhaar'],
+    url='https://github.com/pupattan/django-aadhaar.git',
+    license='',
+    author='pulakpattanayak',
+    author_email='pulak.pattanayak@gmail.com',
+    description='django extension for aadhaar'
+)
