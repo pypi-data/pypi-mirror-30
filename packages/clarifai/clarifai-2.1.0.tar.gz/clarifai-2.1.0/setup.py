7c3aa06c024cb3b031724964128af361a9145b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:27 +0000

    Add package clarifai/2.1.0/clarifai-2.1.0.tar.gz

---
+++ packages/clarifai/clarifai-2.1.0.tar.gz/clarifai-2.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="clarifai",
+    description='Clarifai API Python Client',
+    version='2.1.0',
+    author='Clarifai',
+    maintainer='Robert Wen',
+    maintainer_email='robert@clarifai.com',
+    url='https://github.com/clarifai/clarifai-python',
+    author_email='support@clarifai.com',
+    install_requires=['future==0.15.2',
+                       'requests==2.13.0',
+                       'configparser==3.5.0',
+                       'jsonschema==2.5.1',
+                       'enum34==1.1.6',
+                       'Pillow==2.9.0'],
+    packages=find_packages(),
+    license="Apache 2.0",
+    scripts=['scripts/clarifai'],
+)
