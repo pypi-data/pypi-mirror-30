87eabd70495a73cb1d38e35703c05a42c4b503be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:34 +0000

    Add package pyRestTable/2018.4.0/pyRestTable-2018.4.0.tar.gz

---
+++ packages/pyRestTable/pyRestTable-2018.4.0.tar.gz/pyRestTable-2018.4.0/README.rst
@@ -0,0 +1,24 @@
+pyRestTable
+===========
+
+Format a nice table in reST (reStructuredText ) from Python.
+
+Each cell may have multiple lines, separated by a newline.
+The content of each cell will be rendered as str(cell).
+At present, *pyRestTable* only supports tables with content 
+that does not span any cells (no rowspans or columnspans).
+
+:author:    Pete R. Jemian
+:email:     prjemian@gmail.com
+:copyright: 2014-2018, Pete R. Jemian
+:license:   Creative Commons Attribution 4.0 International Public License (see *LICENSE.txt*)
+:docs:      http://pyRestTable.readthedocs.io
+:URL:       https://github.com/prjemian/pyRestTable
+:PyPI:      https://pypi.python.org/pypi/pyRestTable/
+:TODO list: https://github.com/prjemian/pyRestTable/issues
+:build:
+    .. image:: https://travis-ci.org/prjemian/pyRestTable.svg?branch=master
+               :target: https://travis-ci.org/prjemian/pyRestTable
+:coverage:
+   .. image:: https://coveralls.io/repos/github/prjemian/pyRestTable/badge.svg?branch=master
+              :target: https://coveralls.io/github/prjemian/pyRestTable?branch=master
---
+++ packages/pyRestTable/pyRestTable-2018.4.0.tar.gz/pyRestTable-2018.4.0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+#-----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     prjemian@gmail.com
+# :copyright: (c) 2014-2016, Pete R. Jemian
+#
+# Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+import os
+import re
+import sys
+
+import versioneer
+
+# pull in some definitions from the package's __init__.py file
+sys.path.insert(0, os.path.join('.', 'src', ))
+import pyRestTable
+
+
+verbose=1
+long_description = open('README.rst', 'r').read()
+
+
+setup (name =             pyRestTable.__package_name__,        # pyRestTable
+       version =          versioneer.get_version(),
+       cmdclass =         versioneer.get_cmdclass(),
+       license =          pyRestTable.__license__,
+       description =      pyRestTable.__description__,
+       long_description = long_description,
+       author =           pyRestTable.__author_name__,
+       author_email =     pyRestTable.__author_email__,
+       url =              pyRestTable.__url__,
+       download_url =     pyRestTable.__download_url__,
+       keywords =         pyRestTable.__keywords__,
+       platforms =        'any',
+       package_dir =      {'': 'src'},
+       packages =         ['pyRestTable', ],
+       #packages=find_packages(),
+       package_data     = {'pyRestTable': ['LICENSE.txt',]},
+       classifiers =      pyRestTable.__classifiers__,
+       test_suite  =      "tests",
+      )
