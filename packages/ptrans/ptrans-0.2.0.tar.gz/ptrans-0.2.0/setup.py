5ef047267745e37e0ab112437eec1fb02a7c30e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:51 +0000

    Add package ptrans/0.2.0/ptrans-0.2.0.tar.gz

---
+++ packages/ptrans/ptrans-0.2.0.tar.gz/ptrans-0.2.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.2.0'
+
+setup(name='ptrans',
+      version=VERSION,
+      description="A command line tool help you trans other lauguages to Chinese each other",
+      long_description='',
+      keywords='trans terminal',
+      author='linrz',
+      author_email='1246533834@qq.com',
+      url='https://github.com/linrz/trans-tool',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+        'requests',
+      ],
+      entry_points={
+        'console_scripts': [
+            'trans = trans:translate'
+        ]
+      },
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.5'
+      ]
+)
