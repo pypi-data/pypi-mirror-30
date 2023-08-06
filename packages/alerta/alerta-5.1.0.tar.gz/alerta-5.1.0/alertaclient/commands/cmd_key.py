2a7e07c8c1a48fa7746699cecb76c25763ec871a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:32 +0000

    Add package alerta/5.1.0/alerta-5.1.0.tar.gz

---
+++ packages/alerta/alerta-5.1.0.tar.gz/alerta-5.1.0/alertaclient/commands/cmd_key.py
@@ -0,0 +1,26 @@
+import sys
+from datetime import datetime, timedelta
+
+import click
+
+
+@click.command('key', short_help='Create API key')
+@click.option('--username', '-u', help='User (Admin only)')
+@click.option('--scope', 'scopes', multiple=True, help='List of permissions eg. admin:keys, write:alerts')
+@click.option('--duration', metavar='SECONDS', type=int, help='Duration API key is valid')
+@click.option('--text', help='Description of API key use')
+@click.option('--delete', '-D', metavar='ID', help='Delete API key using ID or KEY')
+@click.pass_obj
+def cli(obj, username, scopes, duration, text, delete):
+    """Create or delete an API key."""
+    client = obj['client']
+    if delete:
+        client.delete_key(delete)
+    else:
+        try:
+            expires = datetime.utcnow() + timedelta(seconds=duration) if duration else None
+            key = client.create_key(username, scopes, expires, text)
+        except Exception as e:
+            click.echo('ERROR: {}'.format(e))
+            sys.exit(1)
+        click.echo(key.key)
