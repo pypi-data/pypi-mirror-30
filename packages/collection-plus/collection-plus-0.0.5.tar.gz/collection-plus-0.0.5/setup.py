d501e53498e3c5cce93ef173914bc04e6c236003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:09 +0000

    Add package collection-plus/0.0.5/collection-plus-0.0.5.tar.gz

---
+++ packages/collection-plus/collection-plus-0.0.5.tar.gz/collection-plus-0.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='collection-plus',
+    version='0.0.5',
+    description='Collection utils',
+    long_description=open('README.rst').read(),
+    url='https://github.com/bruca/collections',
+    author='Bruca Lock',
+    author_email='lockshi@hotmail.com',
+    keywords='collection collections',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    include_package_data=False,
+    zip_safe=False
+)
