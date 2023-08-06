8408e89d2c0231dbca7bc33ba0008573b2f366fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:21 +0000

    Add package eidreader/0.0.3/eidreader-0.0.3.tar.gz

---
+++ packages/eidreader/eidreader-0.0.3.tar.gz/eidreader-0.0.3/README.rst
@@ -0,0 +1,16 @@
+=========================
+The ``eidreader`` package
+=========================
+
+
+
+
+eidreader is a command-line script which reads data from Belgian eID
+cards and writes the data to stdout or posts it to a web server.
+
+- The central project homepage is
+  http://eidreader.lino-framework.org
+- Please report issues to
+  https://github.com/lino-framework/eidreader/issues
+
+
---
+++ packages/eidreader/eidreader-0.0.3.tar.gz/eidreader-0.0.3/setup_info.py
@@ -0,0 +1,30 @@
+SETUP_INFO = dict(
+    name='eidreader',
+    version='0.0.3',
+    install_requires=['requests', 'PyKCS11'],
+    scripts=['eidreader.py'],
+    description="Read data from Belgian eId card via command-line",
+    license='Free BSD',
+    author='Luc Saffre',
+    author_email='luc@saffre-rumma.net')
+
+SETUP_INFO.update(long_description="""\
+
+eidreader is a command-line script which reads data from Belgian eID
+cards and writes the data to stdout or posts it to a web server.
+
+- The central project homepage is
+  http://eidreader.lino-framework.org
+- Please report issues to
+  https://github.com/lino-framework/eidreader/issues
+
+""")
+SETUP_INFO.update(classifiers="""\
+Programming Language :: Python
+Programming Language :: Python :: 3
+Development Status :: 4 - Beta
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Natural Language :: English
+Operating System :: OS Independent""".splitlines())
+
