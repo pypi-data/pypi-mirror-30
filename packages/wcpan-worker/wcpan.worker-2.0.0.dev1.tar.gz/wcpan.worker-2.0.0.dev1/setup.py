e2fa5f9a73205228f29bb1cde0d2845a2b1ddcb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:59 +0000

    Add package wcpan-worker/2.0.0.dev1/wcpan.worker-2.0.0.dev1.tar.gz

---
+++ packages/wcpan-worker/wcpan.worker-2.0.0.dev1.tar.gz/wcpan.worker-2.0.0.dev1/setup.py
@@ -0,0 +1,26 @@
+import os.path as op
+
+from setuptools import setup
+
+
+with open(op.join(op.dirname(__file__), './README.rst')) as fin:
+    long_description = fin.read()
+
+setup(
+        name='wcpan.worker',
+        version='2.0.0.dev1',
+        description='An asynchronous task queue with priority support.',
+        long_description=long_description,
+        author='Wei-Cheng Pan',
+        author_email='legnaleurc@gmail.com',
+        url='https://github.com/legnaleurc/wcpan.worker',
+        packages=[
+            'wcpan.worker',
+        ],
+        install_requires=[
+            'wcpan.logger',
+        ],
+        classifiers=[
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.5',
+        ])
