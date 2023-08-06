886faac3347f90b55eae2a068f4a12a1dc42bb22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:26 +0000

    Add package bitcoin.sh/0.1/bitcoin.sh-0.1.tar.gz

---
+++ packages/bitcoin.sh/bitcoin.sh-0.1.tar.gz/bitcoin.sh-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bitcoin.sh',
+      version='0.1',
+      description='bitcoin.sh',
+      url='https://github.com/desantis/python-bitcoin.sh',
+      author='Andrew T. DeSantis',
+      author_email='atd@deos.org',
+      license='MIT',
+      packages=['bitcoin.sh'],
+      zip_safe=False)
