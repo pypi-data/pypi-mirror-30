aad299c4d1dad03407dd49b56755c4b22fa9b5c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:08 +0000

    Add package ThreadDownloader8460/1.1.0/ThreadDownloader8460-1.1.0.tar.gz

---
+++ packages/ThreadDownloader8460/ThreadDownloader8460-1.1.0.tar.gz/ThreadDownloader8460-1.1.0/HtmlProcessor/grabber.py
@@ -0,0 +1,24 @@
+from urllib import request
+from bs4 import BeautifulSoup
+from SimpleThreading import ThreadPool
+
+def url_to_string(url):
+    return request.urlopen(url).read()
+
+def find_links(url):
+    data = url_to_string(url)
+    soup = BeautifulSoup(data, "html.parser")
+    anchors = soup.find_all('a',href=True)
+    dirty = list(map(lambda item : item["href"], anchors))
+    return list(filter(lambda link : link.startswith("http"),dirty))
+
+def download_links(url, path):
+    links = find_links(url)
+    i=0
+    for link in links:
+        ThreadPool.downloader(link, path + str(i) + ".html")
+        i+=1
+
+
+if __name__ =="__main__":
+    download_links("https://pypi.org/project/ThreadDownloader8460/", "downloads/")
---
+++ packages/ThreadDownloader8460/ThreadDownloader8460-1.1.0.tar.gz/ThreadDownloader8460-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+packages = find_packages()
+
+setup(
+    name='ThreadDownloader8460',
+    version='1.1.0',
+    description="My first python distribution",
+    packages=packages,
+    install_requires=["BeautifulSoup4"]
+)
