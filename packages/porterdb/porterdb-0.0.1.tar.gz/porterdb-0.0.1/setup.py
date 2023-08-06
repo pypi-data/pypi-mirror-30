01e0e47399ab2b2e656fd1daa73692d5c35708c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:20 +0000

    Add package porterdb/0.0.1/porterdb-0.0.1.tar.gz

---
+++ packages/porterdb/porterdb-0.0.1.tar.gz/porterdb-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name="porterdb",
+	version="0.0.1",
+	description="a simple document based database manager",
+	url="https://github.com/wrecodde/porterdb",
+	author="Deji Joseph",
+	author_email="wrecodde@gmail.com",
+	license="MIT",
+        packages=["porterdb"],
+        long_description=open("README.txt").read()
+)
