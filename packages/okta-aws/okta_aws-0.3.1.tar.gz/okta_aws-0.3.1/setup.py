1b239e698115a29e0be1b3c656f63b017cfcbb71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:43 +0000

    Add package okta-aws/0.3.1/okta_aws-0.3.1.tar.gz

---
+++ packages/okta-aws/okta_aws-0.3.1.tar.gz/okta_aws-0.3.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='okta_aws',
+    version='0.3.1',
+    description='Use the AWS API via an account using Okta',
+    author='Mark Harrison',
+    author_email='mharrison@chef.io',
+    license='Apache-2.0',
+    scripts=['okta_aws'],
+    url='https://github.com/chef/okta_aws',
+    python_requires='>=3',
+    install_requires=['requests>=2.18.4', 'toml>=0.9.4']
+)
