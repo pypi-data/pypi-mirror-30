a4028c3616b5934e7bd8eb98fd51e133627efbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:07 +0000

    Add package jsdaily/0.9.0/jsdaily-0.9.0.tar.gz

---
+++ packages/jsdaily/jsdaily-0.9.0.tar.gz/jsdaily-0.9.0/jsdaily/libupdate/update_apm.sh
@@ -0,0 +1,204 @@
+#!/bin/bash
+
+
+# clear potential terminal buffer
+sript -q /dev/null tput clear > /dev/null 2>&1
+
+
+# preset terminal output colours
+blush="tput setaf 1"    # blush / red
+green="tput setaf 2"    # green
+reset="tput sgr0"       # reset
+
+
+################################################################################
+# Check Atom updates.
+#
+# Parameter list:
+#   1. Log Date
+#   2. Quiet Flag
+#   3. Verbose Flag
+#   4. Outdated Flag
+#   5. Package
+#       ............
+################################################################################
+
+
+# parameter assignment
+logdate=$1
+arg_q=$2
+arg_v=$3
+arg_o=$4
+arg_pkg=${*:5}
+
+
+# log file prepare
+logfile="/Library/Logs/Scripts/update/$logdate.log"
+tmpfile="/tmp/log/update.log"
+
+
+# remove /tmp/log/update.log
+rm -f $tmpfile
+
+
+# create /tmp/log/update.log & /Library/Logs/Scripts/update/logdate.log
+touch $logfile
+touch $tmpfile
+
+
+# log current status
+echo "- /bin/bash $0 $@" >> $tmpfile
+
+
+# log commands
+logprefix="script -aq $tmpfile"
+if ( $arg_q ) ; then
+    logsuffix="grep ^$"
+else
+    logsuffix="grep ^.*$"
+fi
+
+
+# if no outdated packages found
+if ( ! $arg_o ) ; then
+    $green
+    $logprefix echo "update: apm: all packages have been up-to-date" | $logsuffix
+    $reset
+else
+    # if quiet flag set
+    if ( $arg_q ) ; then
+        quiet="--quiet"
+    else
+        quiet=""
+    fi
+
+    # if verbose flag set
+    if ( $arg_v ) ; then
+        verbose="--verbose"
+    else
+        verbose=""
+    fi
+
+    # update procedure
+    for name in $arg_pkg ; do
+        flag=`apm list --bare --no-color | sed "s/@.*//" | awk "/^$name$/"`
+        if [[ -nz $flag ]] ; then
+            if ( $arg_q ) ; then
+                $logprefix echo "+ apm upgrade $name $verbose $quiet" > /dev/null 2>&1
+                $logprefix apm upgrade $name $verbose $quiet -y > /dev/null 2>&1
+                $logprefix echo > /dev/null 2>&1
+            else
+                # ask for confirmation
+                while true ; do
+                    read -p "Would you like to install ${name}? (yes)" answer
+
+                    # check answer
+                    case $answer in
+                        [yY]*)
+                            $logprefix echo "+ apm upgrade $name $verbose $quiet"
+                            $logprefix apm upgrade $name $verbose $quiet -y
+                            $logprefix echo
+                            break ;;
+                        [nN]*)
+                            $blush
+                            $logprefix echo "Update procedure for ${name} declined."
+                            $reset
+                            break ;;
+                        * )
+                            echo "Invalid choice." ;;
+                    esac
+                done
+            fi
+        else
+            $blush
+            $logprefix echo "update: apm: no Atom package names $name installed" | $logsuffix
+            $reset
+
+            # did you mean
+            dym=`apm list --bare --no-color | sed "s/@.*//" | grep $name | xargs | sed "s/ /, /g"`
+            if [[ -nz $dym ]] ; then
+                $blush
+                $logprefix echo "update: apm: did you mean any of the following packages: $dym?" | $logsuffix
+                $reset
+            fi
+            $logprefix echo | $logsuffix
+        fi
+    done
+fi
+
+
+# read /tmp/log/update.log line by line then migrate to log file
+while read -r line ; do
+    # plus `+` proceeds in line
+    if [[ $line =~ ^(\+\+*\ )(.*)$ ]] ; then
+        # add "+" in the beginning, then write to /Library/Logs/Scripts/update/logdate.log
+        echo "+$line" >> $logfile
+    # minus `-` proceeds in line
+    elif [[ $line =~ ^(-\ )(.*)$ ]] ; then
+        # replace "-" with "+", then write to /Library/Logs/Scripts/update/logdate.log
+        echo "$line" | sed "y/-/+/" >> $logfile
+    # colon `:` in line
+    elif [[ $line =~ ^([[:alnum:]][[:alnum:]]*)(:)(.*)$ ]] ; then
+        # if this is a update logging message
+        if [[ $line =~ ^(update: )(.*)$ ]] ; then
+            echo "LOG: $line"
+        # if this is a warning
+        elif [[ $( tr "[:upper:]" "[:lower:]" <<< $line ) =~ ^([[:alnum:]][[:alnum:]]*:\ )(.*)(warning:\ )(.*) ]] ; then
+            # log tag
+            prefix="WAR"
+            # log content
+            suffix=`echo $line | sed "s/\[[0-9][0-9]*m//g" | sed "s/warning: //"`
+        # if this is an error
+        elif [[ $( tr "[:upper:]" "[:lower:]" <<< $line ) =~ ^([[:alnum:]][[:alnum:]]*:\ )(.*)(error:\ )(.*)$ ]] ; then
+            # log tag
+            prefix="ERR"
+            # log content
+            suffix=`echo $line | sed "s/\[[0-9][0-9]*m//g" | sed "s/error: //"`
+        # if this is asking for password
+        elif [[ $line =~ ^(Password:)(.*) ]] ; then
+            # log tag
+            prefix="PWD"
+            # log content
+            suffix="content hidden due to security reasons"
+        # otherwise, extract its own tag
+        else
+            # log tag
+            prefix=`echo $line | sed "s/\[[0-9][0-9]*m//g" | sed "s/\(.*\)*:\ .*/\1/" | cut -c 1-3 | tr "[:lower:]" "[:upper:]"`
+            # log content
+            suffix=`echo $line | sed "s/\[[0-9][0-9]*m//g" | sed "s/.*:\ \(.*\)*.*/\1/"`
+        fi
+        # write to /Library/Logs/Scripts/update/logdate.log
+        echo "$prefix: $suffix" >> $logfile
+    # colourised `[??m` line
+    elif [[ $line =~ ^(.*)(\[[0-9][0-9]*m)(.*)$ ]] ; then
+        # error (red/[31m) line
+        if [[ $line =~ ^(.*)(\[31m)(.*)$ ]] ; then
+            # add `ERR` tag and remove special characters then write to /Library/Logs/Scripts/update/logdate.log
+            echo "ERR: $line" | sed "s/\[[0-9][0-9]*m//g" >> $logfile
+        # warning (yellow/[33m)
+        elif [[ $line =~ ^(.*)(\[33m)(.*)$ ]] ; then
+            # add `WAR` tag and remove special characters then write to /Library/Logs/Scripts/update/logdate.log
+            echo "WAR: $line" | sed "s/\[[0-9][0-9]*m//g" >> $logfile
+        # other colourised line
+        else
+            # add `INF` tag and remove special characters then write to /Library/Logs/Scripts/update/logdate.log
+            echo "INF: $line" | sed "s/\[[0-9][0-9]*m//g" >> $logfile
+        fi
+    # empty / blank line
+    elif [[ $line =~ ^([[:space:]]*)$ ]] ; then
+        # directlywrite to /Library/Logs/Scripts/update/logdate.log
+        echo $line >> $logfile
+    # non-empty line
+    else
+        # add `OUT` tag, remove special characters and discard flushed lines then write to /Library/Logs/Scripts/update/logdate.log
+        echo "OUT: $line" | sed "s/\[\?[0-9][0-9]*[a-zA-Z]//g" | sed "/\[[A-Z]/d" | sed "/##*\ \ *.*%/d" >> $logfile
+    fi
+done < $tmpfile
+
+
+# remove /tmp/log/update.log
+rm -f $tmpfile
+
+
+# clear potential terminal buffer
+sript -q /dev/null tput clear > /dev/null 2>&1
---
+++ packages/jsdaily/jsdaily-0.9.0.tar.gz/jsdaily-0.9.0/jsdaily/libupdate/update_cask.sh
@@ -0,0 +1,207 @@
+#!/bin/bash
+
+
+# clear potential terminal buffer
+sript -q /dev/null tput clear > /dev/null 2>&1
+
+
+# preset terminal output colours
+blush="tput setaf 1"    # blush / red
+green="tput setaf 2"    # green
+reset="tput sgr0"       # reset
+
+
+################################################################################
+# Check Caskroom updates.
+#
+# Parameter list:
+#   1. Log Date
+#   2. Quiet Flag
+#   3. Verbose Flag
+#   4. Force Flag
+#   5. Greedy Flag
+#   6. Outdated Flag
+#   7. Package
+#       ............
+################################################################################
+
+
+# parameter assignment
+logdate=$1
+arg_q=$2
+arg_v=$3
+arg_f=$4
+arg_g=$5
+arg_o=$6
+arg_pkg=${*:7}
+
+
+# log file prepare
+logfile="/Library/Logs/Scripts/update/$logdate.log"
+tmpfile="/tmp/log/update.log"
+
+
+# remove /tmp/log/update.log
+rm -f $tmpfile
+
+
+# create /tmp/log/update.log & /Library/Logs/Scripts/update/logdate.log
+touch $logfile
+touch $tmpfile
+
+
+# log current status
+echo "- /bin/bash $0 $@" >> $tmpfile
+
+
+# log commands
+logprefix="script -aq $tmpfile"
+if ( $arg_q ) ; then
+    logsuffix="grep ^$"
+else
+    logsuffix="grep ^.*$"
+fi
+
+
+# if no outdated packages found
+if ( ! $arg_o ) ; then
+    $green
+    $logprefix echo "update: cask: all Casks have been up-to-date" | $logsuffix
+    $reset
+else
+    # if quiet flag set
+    if ( $arg_q ) ; then
+        quiet="--quiet"
+    else
+        quiet=""
+    fi
+
+    # if verbose flag set
+    if ( $arg_v ) ; then
+        verbose="--verbose"
+    else
+        verbose=""
+    fi
+
+    # if force flag set
+    if ( $arg_f ) ; then
+        force="--force"
+    else
+        force=""
+    fi
+
+    # if greedy flag set
+    if ( $arg_g ) ; then
+        $logprefix echo "+ brew cask upgrade --greedy $force $verbose $quiet"
+        $logprefix brew cask upgrade --greedy $verbose $forc $quiet
+        $logprefix echo
+    else
+        # update procedure
+        for name in $arg_pkg ; do
+            flag=`brew cask list -1 | awk "/^$name$/"`
+            if [[ -nz $flag ]] ; then
+                if ( $arg_q ) ; then
+                    $logprefix echo "+ brew cask upgrade $name $verbose $quiet" > /dev/null 2>&1
+                    $logprefix brew cask uninstall --force $name $verbose $quiet > /dev/null 2>&1
+                    $logprefix brew cask install --force $name $verbose $quiet > /dev/null 2>&1
+                    $logprefix echo > /dev/null 2>&1
+                else
+                    $logprefix echo "+ brew cask upgrade $name $verbose $quiet"
+                    $logprefix brew cask uninstall --force $name $verbose $quiet
+                    $logprefix brew cask install --force $name $verbose $quiet
+                    $logprefix echo
+                fi
+            else
+                $blush
+                $logprefix echo "update: cask: no Cask names $name installed" | $logsuffix
+                $reset
+
+                # did you mean
+                dym=`brew cask list -1 | grep $name | xargs | sed "s/ /, /g"`
+                if [[ -nz $dym ]] ; then
+                    $blush
+                    $logprefix echo "update: appstore: did you mean any of the following Casks: $dym?" | $logsuffix
+                    $reset
+                fi
+                $logprefix echo
+            fi
+        done
+    fi
+fi
+
+
+# read /tmp/log/update.log line by line then migrate to log file
+while read -r line ; do
+    # plus `+` proceeds in line
+    if [[ $line =~ ^(\+\+*\ )(.*)$ ]] ; then
+        # add "+" in the beginning, then write to /Library/Logs/Scripts/update/logdate.log
+        echo "+$line" >> $logfile
+    # minus `-` proceeds in line
+    elif [[ $line =~ ^(-\ )(.*)$ ]] ; then
+        # replace "-" with "+", then write to /Library/Logs/Scripts/update/logdate.log
+        echo "$line" | sed "y/-/+/" >> $logfile
+    # colon `:` in line
+    elif [[ $line =~ ^([[:alnum:]][[:alnum:]]*)(:)(.*)$ ]] ; then
+        # if this is a update logging message
+        if [[ $line =~ ^(update: )(.*)$ ]] ; then
+            echo "LOG: $line"
+        # if this is a warning
+        elif [[ $( tr "[:upper:]" "[:lower:]" <<< $line ) =~ ^([[:alnum:]][[:alnum:]]*:\ )(.*)(warning:\ )(.*) ]] ; then
+            # log tag
+            prefix="WAR"
+            # log content
+            suffix=`echo $line | sed "s/\[[0-9][0-9]*m//g" | sed "s/warning: //"`
+        # if this is an error
+        elif [[ $( tr "[:upper:]" "[:lower:]" <<< $line ) =~ ^([[:alnum:]][[:alnum:]]*:\ )(.*)(error:\ )(.*)$ ]] ; then
+            # log tag
+            prefix="ERR"
+            # log content
+            suffix=`echo $line | sed "s/\[[0-9][0-9]*m//g" | sed "s/error: //"`
+        # if this is asking for password
+        elif [[ $line =~ ^(Password:)(.*) ]] ; then
+            # log tag
+            prefix="PWD"
+            # log content
+            suffix="content hidden due to security reasons"
+        # otherwise, extract its own tag
+        else
+            # log tag
+            prefix=`echo $line | sed "s/\[[0-9][0-9]*m//g" | sed "s/\(.*\)*:\ .*/\1/" | cut -c 1-3 | tr "[:lower:]" "[:upper:]"`
+            # log content
+            suffix=`echo $line | sed "s/\[[0-9][0-9]*m//g" | sed "s/.*:\ \(.*\)*.*/\1/"`
+        fi
+        # write to /Library/Logs/Scripts/update/logdate.log
+        echo "$prefix: $suffix" >> $logfile
+    # colourised `[??m` line
+    elif [[ $line =~ ^(.*)(\[[0-9][0-9]*m)(.*)$ ]] ; then
+        # error (red/[31m) line
+        if [[ $line =~ ^(.*)(\[31m)(.*)$ ]] ; then
+            # add `ERR` tag and remove special characters then write to /Library/Logs/Scripts/update/logdate.log
+            echo "ERR: $line" | sed "s/\[[0-9][0-9]*m//g" >> $logfile
+        # warning (yellow/[33m)
+        elif [[ $line =~ ^(.*)(\[33m)(.*)$ ]] ; then
+            # add `WAR` tag and remove special characters then write to /Library/Logs/Scripts/update/logdate.log
+            echo "WAR: $line" | sed "s/\[[0-9][0-9]*m//g" >> $logfile
+        # other colourised line
+        else
+            # add `INF` tag and remove special characters then write to /Library/Logs/Scripts/update/logdate.log
+            echo "INF: $line" | sed "s/\[[0-9][0-9]*m//g" >> $logfile
+        fi
+    # empty / blank line
+    elif [[ $line =~ ^([[:space:]]*)$ ]] ; then
+        # directlywrite to /Library/Logs/Scripts/update/logdate.log
+        echo $line >> $logfile
+    # non-empty line
+    else
+        # add `OUT` tag, remove special characters and discard flushed lines then write to /Library/Logs/Scripts/update/logdate.log
+        echo "OUT: $line" | sed "s/\[\?[0-9][0-9]*[a-zA-Z]//g" | sed "/\[[A-Z]/d" | sed "/##*\ \ *.*%/d" >> $logfile
+    fi
+done < $tmpfile
+
+
+# remove /tmp/log/update.log
+rm -f $tmpfile
+
+
+# clear potential terminal buffer
+sript -q /dev/null tput clear > /dev/null 2>&1
