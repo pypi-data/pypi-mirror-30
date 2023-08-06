62970f26d4d0de7d677e1db871346f72daa37762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:36 +0000

    Add package aiosqlite/0.2.1/aiosqlite-0.2.1.tar.gz

---
+++ packages/aiosqlite/aiosqlite-0.2.1.tar.gz/aiosqlite-0.2.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+from os import path
+import shutil
+
+if path.isfile('README.rst'):
+    shutil.copyfile('README.rst', 'README')
+
+setup(
+    name='aiosqlite',
+    description='asyncio bridge to the standard sqlite3 module',
+    version='0.2.1',
+    author='John Reese',
+    author_email='john@noswap.com',
+    url='https://github.com/jreese/aiosqlite',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: AsyncIO',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+    ],
+    license='MIT License',
+    packages=['aiosqlite'],
+    install_requires=[
+    ],
+)
