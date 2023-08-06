3a2f1dbefd06c77bb834eaff47532badea165a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:58 +0000

    Add package sqlservice/0.22.0/sqlservice-0.22.0.tar.gz

---
+++ packages/sqlservice/sqlservice-0.22.0.tar.gz/sqlservice-0.22.0/src/sqlservice/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""The sqlservice package.
+"""
+
+from .__pkg__ import (
+    __description__,
+    __url__,
+    __version__,
+    __author__,
+    __email__,
+    __license__)
+
+from .client import SQLClient
+from .core import destroy, make_identity, save, transaction
+from .model import ModelBase, as_declarative, declarative_base
+from .query import SQLQuery
