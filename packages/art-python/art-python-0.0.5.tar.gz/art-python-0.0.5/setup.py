03fee526042f83c93a3f09c5af60058ebb96e332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:36 +0000

    Add package art-python/0.0.5/art-python-0.0.5.tar.gz

---
+++ packages/art-python/art-python-0.0.5.tar.gz/art-python-0.0.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from distutils.core import Extension
+
+DISTNAME = 'art-python'
+VERSION = '0.0.5'
+PACKAGES = ['art']
+EXTENSIONS = []
+DESCRIPTION = 'Python package'
+AUTHOR = 'ART Developers'
+MAINTAINER_EMAIL = 'cbirkjones@gmail.com'
+LICENSE = 'MIT'
+URL = 'https://github.com/cbirkj/art-python'
+
+setuptools_kwargs = {
+    'zip_safe': False,
+    'install_requires': ['numpy >= 1.10.4',
+			'pandas >= 0.18.0'],
+    'scripts': [],
+    'include_package_data': True
+}
+
+setup(name=DISTNAME,
+      version=VERSION,
+      packages=PACKAGES,
+      ext_modules=EXTENSIONS,
+      description=DESCRIPTION,
+      author=AUTHOR,
+      maintainer_email=MAINTAINER_EMAIL,
+      license=LICENSE,
+      url=URL,
+      **setuptools_kwargs)
