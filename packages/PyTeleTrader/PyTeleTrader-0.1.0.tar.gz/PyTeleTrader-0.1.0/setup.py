24f685a0d7e1aab858fd8a5af4ea5c4be187c6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:11 +0000

    Add package PyTeleTrader/0.1.0/PyTeleTrader-0.1.0.tar.gz

---
+++ packages/PyTeleTrader/PyTeleTrader-0.1.0.tar.gz/PyTeleTrader-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='PyTeleTrader',
+    version='0.1.0',
+    author='Marc Beder',
+    author_email='marcbeder@yahoo.de',
+    packages=['PyTeleTrader'],
+    url='http://pypi.python.org/pypi/PyTeleTrader/',
+    license='LICENSE.txt',
+    description='Extraction tool for stocks info from www.teletrader.com site',
+    long_description=open('README.txt').read(),
+
+)
+
+
