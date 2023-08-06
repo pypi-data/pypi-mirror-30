a9c1f6bcbc351526e5de7ed7459f09d0efcc24ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:14 +0000

    Add package firstpypiFab/0.1.1/firstpypiFab-0.1.1.tar.gz

---
+++ packages/firstpypiFab/firstpypiFab-0.1.1.tar.gz/firstpypiFab-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='firstpypiFab',
+    packages=['firstpypiFab'],
+    version='0.1.1',
+    description='First pypi Fab',
+    author='Fabian Flores',
+    author_email='favian.flone92@gmail.com',
+    url='https://github.com/Fabfm4/firstpypi',
+    download_url='https://github.com/Fabfm4/firstpypi/tarball/0.1.1',
+    keywords=['HelloWorld', 'firstpypiFab'],
+    classifiers=[],
+)
