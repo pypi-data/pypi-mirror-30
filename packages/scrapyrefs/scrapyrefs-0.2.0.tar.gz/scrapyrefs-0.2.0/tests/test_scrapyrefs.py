a88fd57adb3536f4fe7b43db45aa5ad64ca6a144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:27 +0000

    Add package scrapyrefs/0.2.0/scrapyrefs-0.2.0.tar.gz

---
+++ packages/scrapyrefs/scrapyrefs-0.2.0.tar.gz/scrapyrefs-0.2.0/tests/test_scrapyrefs.py
@@ -0,0 +1,14 @@
+import unittest
+
+from scrapyrefs import scrapyrefs
+
+
+class ScrapyrefsTest(unittest.TestCase):
+
+    def test_stripspaces(self):
+
+        result = scrapyrefs.strip_spaces('KAY , Martin , The Proper Place of Men and Machines in Language Translation , Palo Alto , Xerox Palo Alto Research Center [ en ligne ], 1980, <http://www.mt-archive.info/Kay-1980. pdf> ( page consultée le 10 janvier2013 ) .')
+
+        expected = 'KAY, Martin, The Proper Place of Men and Machines in Language Translation, Palo Alto, Xerox Palo Alto Research Center [en ligne], 1980, <http://www.mt-archive.info/Kay-1980. pdf> (page consultée le 10 janvier2013).'
+
+        self.assertEqual(result, expected)
