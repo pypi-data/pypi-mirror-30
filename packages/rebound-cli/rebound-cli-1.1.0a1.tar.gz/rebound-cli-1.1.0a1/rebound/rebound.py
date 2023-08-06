b4ca709896de112358aa6176bc7d1264a6f77980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:27 +0000

    Add package rebound-cli/1.1.0a1/rebound-cli-1.1.0a1.tar.gz

---
+++ packages/rebound-cli/rebound-cli-1.1.0a1.tar.gz/rebound-cli-1.1.0a1/rebound/rebound.py
@@ -0,0 +1,37 @@
+"""
+Name: Rebound
+Version: 1.1.0
+Author: @shobrook
+Description: Command-line tool that automatically searches Stack Overflow and displays
+results in your terminal when you get a compiler error.
+"""
+
+import sys
+import utilities as util
+
+
+def main(command):
+    language = util.get_language(command[0].lower()) # Gets the language name
+    if language == '': # Unknown language
+        sys.stdout.write("\n%s%s%s" % (util.RED, "Sorry, Rebound doesn't support this file type.", util.END))
+        return
+
+    util.get_question_and_answers("https://stackoverflow.com/questions/39182074/urwid-colored-text-simplified")
+
+    output, error = util.execute([language] + command[0:]) # Executes the command and pipes stdout
+    error_msg = util.get_error_message(error, language) # Prepares error message for search
+
+    if error_msg != None:
+        query = "%s %s" % (language, error_msg)
+        search_results, captcha = util.search_stackoverflow(query)
+
+        if search_results != []:
+            if captcha:
+                sys.stdout.write("\n%s%s%s" % (util.RED, "Sorry, Stack Overflow blocked our request. Try again in a minute.", util.END))
+                return
+            elif util.confirm("\nDisplay Stack Overflow results?"):
+                return util.App(search_results)
+        else:
+            sys.stdout.write("\n%s%s%s" % (util.RED, "No Stack Overflow results found.", util.END))
+    else:
+        sys.stdout.write("\n%s%s%s" % (util.CYAN, "No error detected :)", util.END))
