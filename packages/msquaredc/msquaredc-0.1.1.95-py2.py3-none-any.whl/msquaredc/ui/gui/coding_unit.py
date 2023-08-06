cccc607a472fa60e979c54d3b875c5fe33bccd02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:13 +0000

    Add package msquaredc/0.1.1.95/msquaredc-0.1.1.95-py2.py3-none-any.whl

---
+++ packages/msquaredc/msquaredc-0.1.1.95-py2.py3-none-any.whl/msquaredc/ui/gui/coding_unit.py
@@ -0,0 +1,17 @@
+from sys import version_info
+
+if version_info[0] == 2:
+    # We are using Python 2.x
+    import Tkinter as tk
+elif version_info[0] == 3:
+    # We are using Python 3.x
+    import tkinter as tk
+
+
+class Paper(tk.Frame):  # pragma : no cover
+    def __init__(self, master, coding, data):
+        super(Paper, self).__init__(master)
+
+
+if __name__ == "__main__":
+    Paper("config.yml", None)
