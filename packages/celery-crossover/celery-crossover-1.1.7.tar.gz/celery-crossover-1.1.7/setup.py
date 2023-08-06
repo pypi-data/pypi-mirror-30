7fe7dcb2f945c5b2a42bcafbd2e4901c9a11a05f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:09 +0000

    Add package celery-crossover/1.1.7/celery-crossover-1.1.7.tar.gz

---
+++ packages/celery-crossover/celery-crossover-1.1.7.tar.gz/celery-crossover-1.1.7/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup
+
+requires = [
+    'celery[redis]>=3.1.20',
+    'requests-celery-adapters>=2.0.9',
+    'six>=1.11.0'
+            ]
+
+extras_require = {
+    'test': [
+        'pytest>=3.2.2'
+    ],
+}
+
+
+setup(name='celery-crossover',
+      version='1.1.7',
+      description='Celery Crossover aims to make it really easy to execute tasks in another service.',
+      author='Daniel Debonzi',
+      author_email='debonzi@gmail.com',
+      install_requires=requires,
+      extras_require=extras_require,
+      url='https://github.com/debonzi/celery_crossover',
+      packages=['crossover', 'examples'],
+      )
