c6bf6cc1da995b909f26b8c44d8b3b1a4de20646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:53 +0000

    Add package noesis/0.2.2/noesis-0.2.2.tar.gz

---
+++ packages/noesis/noesis-0.2.2.tar.gz/noesis-0.2.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from noesis import __version__
+
+setup(name='noesis',
+    version=__version__,
+    description='An open source framework for network data mining that provides a large collection of network analysis techniques',
+    url='http://noesis.ikor.org/',
+    author='The NOESIS project team',
+    author_email='noesis-support@ikor.org',
+    license='MIT',
+    packages=['noesis'],
+    install_requires=['numpy','javabridge'],
+    package_data={'noesis': ['data/*']},
+    include_package_data=True,
+    test_suite="noesis.tests.test",
+    keywords=['noesis', 'network'],
+)
