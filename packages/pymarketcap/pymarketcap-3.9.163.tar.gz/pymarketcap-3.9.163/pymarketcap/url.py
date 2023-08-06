ac612519f3aecc8d08a38943c50244d764d1e96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:47 +0000

    Add package pymarketcap/3.9.163/pymarketcap-3.9.163.tar.gz

---
+++ packages/pymarketcap/pymarketcap-3.9.163.tar.gz/pymarketcap-3.9.163/pymarketcap/url.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""Urllib implementation for pymarketcap."""
+
+from urllib.error import HTTPError
+from urllib.request import urlopen, Request
+from socket import timeout
+
+from pymarketcap import __version__
+from pymarketcap.errors import CoinmarketcapHTTPError408
+
+class Response:
+    """Internal response object for encapsulate responses
+    getted by requests with urllib module."""
+    def __init__(self, text, status_code, url):
+        self.text = text
+        self.status_code = status_code
+        self.url = url
+
+def get_to_memory(url, timeout, debug):
+    """GET request stored in memory.
+
+    Args:
+        timeout (int): Number of seconds until
+            expiration time cancels the request.
+        debug (bool): See code response or not.
+    """
+    req = Request(
+        url.decode(),
+        headers={"User-Agent": "pymarketcap %s" % __version__}
+    )
+    try:
+        req = urlopen(req, timeout=timeout)
+    except HTTPError as err:
+        return Response(b"", err.code, url)
+    except OSError:
+        return Response(b"", 404, url)
+    except timeout:
+        raise CoinmarketcapHTTPError408("Request timeout exceed (%d seconds)" % timeout)
+    else:
+        data = req.read()
+        if debug:
+            print(data)
+        res = Response(data, req.getcode(), url)
+        req.close()
+        return res
