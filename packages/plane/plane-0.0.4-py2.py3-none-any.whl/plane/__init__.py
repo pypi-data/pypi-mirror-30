edaf9156283dd4bef62294fb25dd9d3801cdb051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:29 +0000

    Add package plane/0.0.4/plane-0.0.4-py2.py3-none-any.whl

---
+++ packages/plane/plane-0.0.4-py2.py3-none-any.whl/plane/__init__.py
@@ -0,0 +1,11 @@
+from .punctuation import remove_punctuation
+from .main import replace, extract, build_new_regex
+from .pattern import (
+    HTML,
+    EMAIL,
+    SPACE,
+    TELEPHONE,
+    RESTRICT_URL,
+    CHINESE,
+    CJK,
+)
