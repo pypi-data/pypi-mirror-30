92e3ef089c7b4b1c4ea4c81bd0898f6c9f840329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:36 +0000

    Add package PkgScript/0.2.5/PkgScript-0.2.5.tar.gz

---
+++ packages/PkgScript/PkgScript-0.2.5.tar.gz/PkgScript-0.2.5/pkgscript/version.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+__author__ = "Padraic Shafer PShafer@lbl.gov"
+__date__ = "2018-04-14T10:54:25-0700"
