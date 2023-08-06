7f171badc480d775622502025e6d324edeac4c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:32 +0000

    Add package pybdsim/1.4/pybdsim-1.4-py2-none-any.whl

---
+++ packages/pybdsim/pybdsim-1.4-py2-none-any.whl/pybdsim/Compare/__init__.py
@@ -0,0 +1,27 @@
+#explicit imports to keep namespace clean
+from _MadxBdsimComparison import MadxVsBDSIM
+from _MadxBdsimComparison import MadxVsBDSIMOrbit
+from _MadxBdsimComparison import MadxVsBDSIMFromGMAD
+
+from _TransportBdsimComparison import TransportVsBDSIM
+
+try:
+    import pymad8 as _pymad8
+    from _Mad8BdsimComparison import Mad8VsBDSIM
+except ImportError:
+    import warnings
+    msg = "Missing pymad8 dependency.  MAD8 comparison facilities excluded."
+    warnings.warn(msg)
+    del warnings
+
+from _BdsimBdsimComparison import BDSIMVsBDSIM
+from _BdsimBdsimComparison import PTCVsBDSIM
+
+try:
+    import pysad as _pysad
+    from _SadComparison import SadComparison
+except ImportError:
+    pass
+
+
+from _MadxMadxComparison import MadxVsMadx
