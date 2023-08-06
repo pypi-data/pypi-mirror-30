f70afcf23f5053930ef04e81a7ac246263935c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:34 +0000

    Add package python-ses/0.2.2.1/python-ses-0.2.2.1.tar.gz

---
+++ packages/python-ses/python-ses-0.2.2.1.tar.gz/python-ses-0.2.2.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(name='python-ses',
+      version='0.2.2.1',
+      description='Simple usefull lib to send email using AWS SES',
+      long_description='Really, the best around.',
+      url='http://github.com/storborg/funniest',
+      author='Aaron Dominguez - Destacame Spa',
+      author_email='dominguezaaron75@gmail.com',
+      license='MIT',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      keywords='aws ses python email emailing',
+      packages=['python_ses'],
+      install_requires=[
+          'jinja2',
+          'boto3',
+          'email'
+      ],
+      #dependency_links=['http://github.com/user/repo/tarball/master#egg=package-1.0'],
+      zip_safe=False)
