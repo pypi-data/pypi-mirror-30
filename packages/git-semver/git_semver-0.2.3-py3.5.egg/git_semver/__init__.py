0ef1f85d34395a591c55c3833f17dfc799a781a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:32 +0000

    Add package git-semver/0.2.3/git_semver-0.2.3-py3.5.egg

---
+++ packages/git-semver/git_semver-0.2.3-py3.5.egg/git_semver/__init__.py
@@ -0,0 +1,22 @@
+from git_semver._version import __version__
+from semantic_version import Version as _Version
+
+
+def get_current_version(repo, Version=_Version):
+    latest = None
+    for tag in repo.tags:
+        v = tag.name
+        if v.startswith('v.'):
+            v = v[2:]
+        elif v.startswith('v'):
+            v = v[1:]
+
+        v = Version.coerce(v, partial=True)
+
+        if not latest:
+            latest = v
+        else:
+            if v > latest:
+                latest = v
+
+    return latest
