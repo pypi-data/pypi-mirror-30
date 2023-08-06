87d4ef4bae60195fa43f3eb6a5cdc8add434a92b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:30 +0000

    Add package persistable/0.3.5/persistable-0.3.5.tar.gz

---
+++ packages/persistable/persistable-0.3.5.tar.gz/persistable-0.3.5/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='persistable',
+    version='0.3.5',
+    packages=['persistable', 'persistable/util'],
+    url='https://github.com/DataReply/persistable',
+    download_url='https://github.com/DataReply/persistable/archive/0.3.5.tar.gz',
+    license='',
+    author='Alex Loosley',
+    author_email='a.loosley@reply.de',
+    description='An inheritable superclass with logging, and tools for persisting and '
+                'loading models with parameter tracking',
+    keywords = ['persisting', 'models', 'pipeline'],
+    install_requires=[
+        "cytoolz>=0.8.2",
+        "wrapt>=1.10.11"
+    ]
+)
