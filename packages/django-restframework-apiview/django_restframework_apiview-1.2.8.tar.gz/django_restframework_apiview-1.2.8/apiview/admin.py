916021ab3937e7f0dcbebbc371cbd3f82828dc87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:23 +0000

    Add package django-restframework-apiview/1.2.8/django_restframework_apiview-1.2.8.tar.gz

---
+++ packages/django-restframework-apiview/django_restframework_apiview-1.2.8.tar.gz/django_restframework_apiview-1.2.8/apiview/admin.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+from __future__ import absolute_import, unicode_literals
+
+from apiview import admintools
+
+
+class BaseAdmin(admintools.ProxyModelAdmin):
+    pass
