84dbbe7216db14793e3210253a545d2d7e774782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:06 +0000

    Add package msquaredc/0.1.1.158/msquaredc-0.1.1.158-py2.py3-none-any.whl

---
+++ packages/msquaredc/msquaredc-0.1.1.158-py2.py3-none-any.whl/msquaredc/ui/gui/dialogs.py
@@ -0,0 +1,18 @@
+from sys import version_info
+
+if version_info[0] == 2:
+    # We are using Python 2.x
+    import tkSimpleDialog as sdg
+    import tkFileDialog as fdg
+elif version_info[0] == 3:
+    # We are using Python 3.x
+    from tkinter import simpledialog as sdg
+    from tkinter import filedialog as fdg
+
+
+def NameDialog():  # pragma : no cover
+    return sdg.askstring("Name dialog", "Please insert Name:")
+
+
+def FileDialog():  # pragma : no cover
+    return fdg.askopenfilename()
---
+++ packages/msquaredc/msquaredc-0.1.1.158-py2.py3-none-any.whl/msquaredc/ui/gui/menu.py
@@ -0,0 +1,40 @@
+from sys import version_info
+
+from msquaredc.ui.interfaces import AbstractMenu
+
+if version_info[0] == 2:
+    # We are using Python 2.x
+    import Tkinter as tk
+elif version_info[0] == 3:
+    # We are using Python 3.x
+    import tkinter as tk
+
+
+class GUIMenu(AbstractMenu):
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        self.root = parent
+        while self.root.parent is not None:
+            self.root = self.root.parent
+        self.tk = self.parent.tk
+        super(GUIMenu, self).__init__(self.parent, name)
+
+        if not isinstance(self.parent, GUIMenu):
+            self.menu = tk.Menu(self.tk, tearoff=False)
+            self.tk.config(menu=self.menu)
+        else:
+            self.menu = tk.Menu(self.root.menu.tk, tearoff=False)
+
+    def addEntry(self, entry, handle, *args, **kwargs):
+        label = tk.StringVar(self.tk, entry, entry)
+        super(GUIMenu, self).addEntry(entry, handle)
+        self.root.add_label(entry, label.set)
+        self.menu.add_command(label=label, command=handle)
+
+    def addSeparator(self):
+        self.menu.add_separator()
+
+    def addSubmenu(self, entry, *args, **kwargs):
+        super(GUIMenu, self).addSubmenu(entry)
+        self.menu.add_cascade(label=entry.name, menu=entry.menu)
