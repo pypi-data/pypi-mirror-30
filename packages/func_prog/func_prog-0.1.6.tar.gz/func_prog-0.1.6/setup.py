9538540e1329436111511ae957513c137c3b67b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:00 +0000

    Add package func_prog/0.1.6/func_prog-0.1.6.tar.gz

---
+++ packages/func_prog/func_prog-0.1.6.tar.gz/func_prog-0.1.6/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='func_prog',
+    packages=['func_prog'],
+    version='0.1.6',
+    description='A library supporting functional programming',
+    author='Duy Tin Truong',
+    author_email='',
+    url='https://github.com/duytintruong/func_prog',
+    download_url='https://github.com/duytintruong/func_prog/archive/0.1.6.tar.gz',
+    keywords=['functional programming', 'pipeline', 'data'],
+    classifiers=[],
+)
