654048c70bf1e231609faf2522a76e816c49a961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:03 +0000

    Add package django-addons-formlib/2.0.1/django-addons-formlib-2.0.1.tar.gz

---
+++ packages/django-addons-formlib/django-addons-formlib-2.0.1.tar.gz/django-addons-formlib-2.0.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from django_addons_formlib import __version__
+
+setup(
+    name='django-addons-formlib',
+    version=__version__,
+    description='django-addons Framework form library',
+    author='Divio AG',
+    author_email='info@divio.ch',
+    url='https://github.com/django-addons/django-addons-formlib',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django :: 2.0',
+    ],
+)
