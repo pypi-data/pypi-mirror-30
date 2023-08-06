871834ec29aafcf49701e4510479e4eeee31326c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:59 +0000

    Add package youtube-audio-downloader/1.1.7/youtube-audio-downloader-1.1.7.tar.gz

---
+++ packages/youtube-audio-downloader/youtube-audio-downloader-1.1.7.tar.gz/youtube-audio-downloader-1.1.7/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" setup.py for youtube-audio-downloader."""
+
+from setuptools import setup
+
+setup(
+    name="youtube-audio-downloader",
+    version="1.1.7",
+    description="Download audio from YouTube",
+    keywords=["youtube-downloader", "downloader", "YouTube", "youtube", "download", "audio"],
+    author="marcosschroh",
+    author_email="marcos.06sch@gmail.com",
+    url="https://github.com/marcosschroh/youtube-audio-downloader",
+    download_url="https://github.com/marcosschroh/youtube-audio-downloader.git",
+    install_requires=[
+        "youtube-dl==2017.11.15",
+        "pafy==0.5.0",
+        "click==6.7",
+        "PyYAML==3.12"
+    ],
+    packages=["sound_downloader"],
+    license='GPLv3',
+    entry_points='''
+        [console_scripts]
+        sound-down=scripts.sound_down:main
+    ''',
+)
