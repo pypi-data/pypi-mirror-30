2aa58b7316ed96a71f165069d1242eb2d47f1155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:31 +0000

    Add package tensorcomlib/1.1.0/tensorcomlib-1.1.0-py3-none-any.whl

---
+++ packages/tensorcomlib/tensorcomlib-1.1.0-py3-none-any.whl/tensorcomlib/decomposition.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def cp(X, n_components, tol, max_iter, init_type, random_state=None):
+    pass
+
+def tucker(X, n_components=None, tol=1E-6, max_iter=500, init_type="hosvd",random_state=None):
+    pass
