41f5495d632fb6d768684f19220794c8cf40df23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:37 +0000

    Add package elitsdk/0.0.4/elitsdk-0.0.4.tar.gz

---
+++ packages/elitsdk/elitsdk-0.0.4.tar.gz/elitsdk-0.0.4/elitsdk/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.0.4'
+version = '0.0.4'
+full_version = '0.0.4'
+git_revision = '4cc5593fc83691dd02fceced2b7bbbd76bc7502c'
+release = True
+if not release:
+    version = full_version
