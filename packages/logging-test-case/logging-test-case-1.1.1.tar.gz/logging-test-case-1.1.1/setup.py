0ca4811b2176b2efd0cc38ad9ed6d2775cc50064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:47 +0000

    Add package logging-test-case/1.1.1/logging-test-case-1.1.1.tar.gz

---
+++ packages/logging-test-case/logging-test-case-1.1.1.tar.gz/logging-test-case-1.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+"""
+To upload to PyPI:
+https://packaging.python.org/tutorials/distributing-packages/
+
+python setup.py sdist bdist_wheel
+twine upload dist/*
+"""
+setup(name='logging-test-case',
+      version='1.1.1',
+      description='Provides class LoggingTestCase to help test log files.',
+      keywords='unit testing log files logging regression logging-test-case loggingtestcase',
+      author='Chad Rosenquist',
+      author_email='chadrosenquist@gmail.com',
+      url='https://github.com/chadrosenquist/logging-test-case',
+      packages=['loggingtestcase'],
+      license='MIT',
+      python_requires='>=3'
+)
