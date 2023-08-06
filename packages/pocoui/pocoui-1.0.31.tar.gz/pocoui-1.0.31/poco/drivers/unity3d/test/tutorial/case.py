4bb910a2e416e0f4b1bf084c079ee7e530a64b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:40 +0000

    Add package pocoui/1.0.31/pocoui-1.0.31.tar.gz

---
+++ packages/pocoui/pocoui-1.0.31.tar.gz/pocoui-1.0.31/poco/drivers/unity3d/test/tutorial/case.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+import time
+from pocounit.case import PocoTestCase
+from pocounit.addons.poco.action_tracking import ActionTracker
+
+from poco.drivers.unity3d import UnityPoco
+
+
+class TutorialCase(PocoTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.poco = UnityPoco()
+        action_tracker = ActionTracker(cls.poco)
+        cls.register_addin(action_tracker)
+
+    @classmethod
+    def tearDownClass(cls):
+        time.sleep(1)
