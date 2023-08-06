7f529988bb3a19593a97c56c4b2a0fe95af42799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:02 +0000

    Add package atdlib/0.1/atdlib-0.1.tar.gz

---
+++ packages/atdlib/atdlib-0.1.tar.gz/atdlib-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='atdlib',
+      version='0.1',
+      description='atdlib python library',
+      url='https://github.com/desantis/python-atdlib',
+      author='Andrew T. DeSantis',
+      author_email='atd@deos.org',
+      license='MIT',
+      packages=['atdlib'],
+      zip_safe=False)
