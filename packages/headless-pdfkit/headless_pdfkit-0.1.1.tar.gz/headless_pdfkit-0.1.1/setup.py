83aa720b112f53e71a29773a9b1cfa9c28aec51d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:02 +0000

    Add package headless-pdfkit/0.1.1/headless_pdfkit-0.1.1.tar.gz

---
+++ packages/headless-pdfkit/headless_pdfkit-0.1.1.tar.gz/headless_pdfkit-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='headless_pdfkit',
+      version='0.1.1',
+      description='A headless version of pdfkit which makes use of xvfb-run',
+      long_description=long_description,
+      url='http://github.com/mrshu/headless_pdfkit',
+      author='Marek Suppa (mr.Shu)',
+      author_email='mr@shu.io',
+      license='MIT',
+      packages=['headless_pdfkit'],
+      zip_safe=False)
