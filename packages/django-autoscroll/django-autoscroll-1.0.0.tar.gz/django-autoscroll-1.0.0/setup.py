897edf693cb08e151477b0e08d75e15270ac28cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:17 +0000

    Add package django-autoscroll/1.0.0/django-autoscroll-1.0.0.tar.gz

---
+++ packages/django-autoscroll/django-autoscroll-1.0.0.tar.gz/django-autoscroll-1.0.0/setup.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name="django-autoscroll",
+      version="1.0.0",
+      description="Abstract automatic pagination.",
+      packages=["autoscroll", 'autoscroll.templatetags'],
+      author="Iury O. G. Figueiredo",
+      author_email="ioliveira@id.uff.br",
+      url='https://github.com/iogf/django-autoscroll',
+      download_url='https://github.com/iogf/django-autoscroll',
+      keywords=['django', 'autoscroll', 'scrolling'],
+      classifiers=[])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
