2f33cb548571c696cb63d59fe5c7c8c8719cd72c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:26 +0000

    Add package widdy/0.2.10/widdy-0.2.10-py2.py3-none-any.whl

---
+++ packages/widdy/widdy-0.2.10-py2.py3-none-any.whl/widdy/examples/counter.py
@@ -0,0 +1,55 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from builtins import int
+from builtins import super
+from future import standard_library
+standard_library.install_aliases()
+import widdy
+from widdy import styles
+from random import random
+import time, sys
+
+class CounterApp(widdy.App):
+    tpl = "{time: <20} | {counter: <7}"
+
+    def __init__(app):
+        header      = app.tpl.format(time='time', counter='counter')
+        app.headers = [header, len(header) * "-"]
+        app.lines   = []
+        app.counter = 0
+        txt, app.update_text = widdy.Text("")
+        box = widdy.LineBox(txt)
+        menu     = [('+', styles.BLUE_BOLD,   'increase'),
+                    ('-', styles.CYAN_BOLD,   'decrease'),
+                    ('R', styles.YELLOW_BOLD, 'random'),
+                    ('C', styles.RED_BOLD,    'clear')]
+        handlers = [('+', app.inc),
+                    ('-', app.dec),
+                    ('R', app.random),
+                    ('C', app.clear)]
+        f = widdy.Frame(widdy.Header("EmptyApp"), box, widdy.Menu(*menu))
+        super().__init__(f, handlers=widdy.Handlers(*handlers))
+        app.redraw()
+
+    def inc(app):    app.counter += 1; app.add_value()
+    def dec(app):    app.counter -= 1; app.add_value()
+    def random(app): app.counter = int(100 * random()); app.add_value()
+    def clear(app):  app.lines = []; app.redraw()
+
+    def add_value(app, value=None):
+        if value is None: value = app.counter
+        app.lines.append(app.tpl.format(time='{:.2f}'.format(time.time()), counter=value))
+        app.redraw()
+
+    def redraw(app):
+        app.update_text('\n'.join(app.headers + app.lines))
+
+def main(argv=None): CounterApp().run()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+    sys.exit(0)
+
+
