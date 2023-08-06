05ec44c0302f8d97b51ec689741f645ac13599d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:12 +0000

    Add package pybbn/0.0.2/pybbn-0.0.2.tar.gz

---
+++ packages/pybbn/pybbn-0.0.2.tar.gz/pybbn-0.0.2/tests/pptc/test_triangulator.py
@@ -0,0 +1,28 @@
+from nose import with_setup
+
+from pybbn.graph.dag import BbnUtil
+from pybbn.pptc.moralizer import Moralizer
+from pybbn.pptc.potentialinitializer import PotentialInitializer
+from pybbn.pptc.triangulator import Triangulator
+
+
+def setup():
+    pass
+
+
+def teardown():
+    pass
+
+
+@with_setup(setup, teardown)
+def test_triangulator():
+    bbn = BbnUtil.get_huang_graph()
+    PotentialInitializer.init(bbn)
+
+    ug = Moralizer.moralize(bbn)
+    cliques = Triangulator.triangulate(ug)
+
+    # assert later
+    # for clique in cliques:
+    #     print(clique)
+    # assert 1 == 2
