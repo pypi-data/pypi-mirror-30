79787adda1c2213cdb9d5edeba6f6d0df4e3ac7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:31 +0000

    Add package grpc_wrapper/0.1/grpc_wrapper-0.1.tar.gz

---
+++ packages/grpc_wrapper/grpc_wrapper-0.1.tar.gz/grpc_wrapper-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup_requires = []
+
+install_requires = [
+      'grpcio == 1.8.4'
+]
+dependency_links = []
+
+setup(name='grpc_wrapper',
+      version='0.1',
+      description='simple client and server for grpc',
+      url='',
+      author='loveloper',
+      author_email='loveloper44@gmail.com',
+      license='MIT',
+      packages=['grpc_wrapper'],
+      zip_safe=False)
