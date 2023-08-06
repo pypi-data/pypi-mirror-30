9c0af6a5cfa53e24b25837047d88c5e108af6ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:32 +0000

    Add package Python-TBA/0.1/Python-TBA-0.1.tar.gz

---
+++ packages/Python-TBA/Python-TBA-0.1.tar.gz/Python-TBA-0.1/Python-TBA/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'Python-TBA',
+  packages = ['Python-TBA'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A BlueAlliance API Libary for Python',
+  author = 'techied',
+  author_email = 'deejnelson711@gmail.com',
+  url = 'https://github.com/techied/Python-TBA',
+  download_url = 'https://github.com/techied/Python-TBA/archive/0.1.tar.gz',
+  keywords = ['api', '3.6', 'thebluealliance', 'frc'], # arbitrary keywords
+  classifiers = [],
+)
