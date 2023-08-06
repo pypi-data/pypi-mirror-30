bc80351867013ba87cf65c7db3172ed81ea1be51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:08 +0000

    Add package radvel/1.1.8/radvel-1.1.8-cp36-cp36m-macosx_10_7_x86_64.whl

---
+++ packages/radvel/radvel-1.1.8-cp36-cp36m-macosx_10_7_x86_64.whl/radvel/cli.py
@@ -0,0 +1,185 @@
+"""
+Command Line Interface
+"""
+import os
+from argparse import ArgumentParser
+import imp
+import warnings
+import pickle
+
+import radvel.driver
+
+warnings.filterwarnings("ignore")
+warnings.simplefilter('once', DeprecationWarning)
+
+def main():
+    psr = ArgumentParser(
+        description="RadVel: The Radial Velocity Toolkit", prog='RadVel'
+    )
+    psr.add_argument('--version',
+        action='version',
+        version="%(prog)s {}".format(radvel.__version__),
+        help="Print version number and exit."
+    )
+
+    subpsr = psr.add_subparsers(title="subcommands", dest='subcommand')
+
+    # In the parent parser, we define arguments and options common to
+    # all subcommands.
+    psr_parent = ArgumentParser(add_help=False)
+    psr_parent.add_argument(
+        '-d', dest='outputdir', type=str,
+        help="Working directory. Default is the same as the \
+        configuration file (without .py)"
+    )
+    psr_parent.add_argument('-s','--setup',
+        dest='setupfn', type=str, 
+        help="Setup file."
+    )
+    psr_parent.add_argument('--decorr',
+        dest='decorr',
+        action='store_true',
+        default=False,
+        help="Include decorrelation in likelihood."
+    )
+
+
+
+    # Fitting    
+    psr_fit = subpsr.add_parser(
+        'fit', parents=[psr_parent],
+        description="Perform max-likelihood fitting"
+    )
+    psr_fit.set_defaults(func=radvel.driver.fit)
+
+    
+    # Plotting
+    psr_plot = subpsr.add_parser('plot', parents=[psr_parent],)
+    psr_plot.add_argument('-t','--type',
+        type=str, nargs='+',
+        choices=['rv','corner','trend', 'derived'],
+        help="type of plot(s) to generate"
+    )
+    psr_plot.add_argument(
+        '--plotkw', dest='plotkw',action='store', default="{}", type=eval,
+        help='''
+        Dictionary of keywords sent to rv_multipanel_plot. 
+        E.g. --plotkw "{'yscale_auto': True}"'
+        ''',
+    )
+    
+    psr_plot.set_defaults(func=radvel.driver.plots)
+
+    
+    # MCMC
+    psr_mcmc = subpsr.add_parser(
+        'mcmc', parents=[psr_parent],
+        description="Perform MCMC exploration"
+    )
+    psr_mcmc.add_argument(
+        '--nsteps', dest='nsteps', action='store',default=10000, type=float, 
+        help='Number of steps per chain [10000]',)
+    psr_mcmc.add_argument(
+        '--nwalkers', dest='nwalkers', action='store', default=50, type=int,
+        help='Number of walkers. [50]', 
+    )
+    psr_mcmc.add_argument(
+    '--nensembles', dest='ensembles', action='store', default=8, type=int,
+    help="Number of ensembles. Will be run in parallel on separate CPUs [8]"
+    )
+    psr_mcmc.add_argument(
+    '--maxGR', dest='maxGR', action='store', default=1.01, type=float,
+    help="Maximum G-R statistic for chains to be deemed well-mixed and halt the MCMC run [1.01]"
+    )
+    psr_mcmc.add_argument(
+    '--burnGR', dest='burnGR', action='store', default=1.03, type=float,
+    help="Maximum G-R statistic to stop burn-in period [1.03]"
+    )
+    psr_mcmc.add_argument(
+    '--minTz', dest='minTz', action='store', default=1000, type=int,
+    help="Minimum Tz to consider well-mixed [1000]"
+    )
+    psr_mcmc.add_argument(
+    '--minsteps', dest='minsteps', action='store', default=1000, type=int,
+    help="Minimum number of steps per walker before convergence tests are performed [1000]"
+    )
+    psr_mcmc.add_argument(
+    '--thin', dest='thin', action='store', default=1, type=int,
+    help="Save one sample every N steps [default=1, save all samples]"
+    )
+    psr_mcmc.add_argument(
+    '--serial', dest='serial', action='store', default=False, type=bool,
+    help='''\
+If True, run MCMC in serial instead of parallel. [False]
+'''
+    )
+    psr_mcmc.set_defaults(func=radvel.driver.mcmc)
+
+    # Derive physical parameters
+    psr_physical = subpsr.add_parser(
+        'derive', parents=[psr_parent],
+        description="Multiply MCMC chains by physical parameters. MCMC must"
+        + "be run first"
+    )
+
+    psr_physical.set_defaults(func=radvel.driver.derive)
+    
+    # BIC 
+    psr_bic = subpsr.add_parser('bic', parents=[psr_parent],)
+    psr_bic.add_argument('-t',
+        '--type', type=str, nargs='+', 
+        choices=['nplanets'],
+        help="type of BIC comparison to perform"
+    )
+    psr_bic.set_defaults(func=radvel.driver.bic)
+
+    # Tables
+    psr_table = subpsr.add_parser('table', parents=[psr_parent],)
+    psr_table.add_argument('-t','--type',
+        type=str, nargs='+',
+        choices=['params', 'priors', 'nplanets', 'rv'],
+        help="type of plot(s) to generate"
+    )
+    psr_table.add_argument(
+        '--header', action='store_true',
+        help="included latex column header. Default just prints data rows"
+    )
+    
+    psr_table.set_defaults(func=radvel.driver.tables)
+
+    
+    # Report
+    psr_report = subpsr.add_parser(
+        'report', parents=[psr_parent], 
+        description="Merge output tables and plots into LaTeX report"
+    )
+    psr_report.add_argument(
+        '--comptype', dest='comptype', action='store',
+        default='nplanets', type=str, 
+        help='Type of BIC model comparison table to include. \
+        Default: nplanets')
+
+    psr_report.add_argument(
+        '--latex-compiler', default='pdflatex', type=str, 
+        help='Path to latex compiler'
+        )
+
+    psr_report.set_defaults(func=radvel.driver.report)
+    
+    args = psr.parse_args()
+
+    if args.outputdir is None:
+        setupfile = args.setupfn
+        print(setupfile)
+        system_name = os.path.basename(setupfile).split('.')[0]
+        outdir = os.path.join('./', system_name)
+        args.outputdir = outdir
+            
+    if not os.path.isdir(args.outputdir):
+        os.mkdir(args.outputdir)
+        
+    args.func(args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/radvel/radvel-1.1.8-cp36-cp36m-macosx_10_7_x86_64.whl/radvel/fitting.py
@@ -0,0 +1,124 @@
+import scipy.optimize
+import numpy as np
+import copy
+import collections
+
+
+def maxlike_fitting(post, verbose=True, method='Powell'):
+    """Maximum Likelihood Fitting
+
+    Perform a maximum likelihood fit.
+
+    Args:
+        post (radvel.Posterior): Posterior object with initial guesses
+        verbose (bool [optional]): Print messages and fitted values?
+        method (string [optional]): Minimization method. See documentation for `scipy.optimize.minimize` for available
+            options.
+
+    Returns: 
+        radvel.Posterior : Posterior object with parameters
+        updated to their maximum likelihood values
+
+    """
+
+    if verbose:
+        print("Initial loglikelihood = %f" % post.logprob())
+        print("Performing maximum likelihood fit...")
+    res = scipy.optimize.minimize(
+        post.neglogprob_array, post.get_vary_params(), method=method,
+        options=dict(xatol=1e-8, maxiter=200, maxfev=100000)
+    )
+    synthparams = post.params.basis.to_synth(post.params, noVary = True) # setting "noVary" assigns each new parameter a vary attribute
+    post.params.update(synthparams)                                 # of '', for printing purposes
+
+    if verbose:
+        print("Final loglikelihood = %f" % post.logprob())
+        print("Best-fit parameters:")
+        print(post)
+        
+    return post
+    
+
+def model_comp(post, verbose=False):
+    """Model Comparison
+
+    Fit for planets adding one at a time.  Save results as list of
+    posterior objects.
+
+    Args:
+        post (radvel.Posterior): posterior object for final best-fit solution 
+            with all planets
+        verbose (bool): (optional) print out statistics
+        
+    Returns:
+        list of dictionaries: 
+            List of dictionaries with fit statistics. Each value in the 
+            dictionary is a tuple with the statistic value as the first 
+            element and a description of that statistic in the second element.
+    """
+    iparams = copy.deepcopy(post.params)
+    
+    num_planets = post.likelihood.model.num_planets
+
+    statsdict = []
+    
+    for n in range(num_planets+1):
+        pdict = collections.OrderedDict()
+        if verbose:
+            print("Testing %d planet model.\n\n" % n)
+
+        for par in post.params.keys():
+            try:
+                num = int(par[-1])
+                if num > n:
+                    if par.startswith('k') or par.startswith('logk'):
+                        post.params[par].value = 0.0
+                    post.params[par].vary = False
+                else:
+                    post.params[par].value = iparams[par].value
+                    post.params[par].vary = iparams[par].vary
+            except (ValueError, KeyError):
+                pass
+
+        for par in post.params:
+            if par.startswith('jit'):
+                post.params[par].vary = False
+            
+        post = maxlike_fitting(post, verbose=False)
+
+        ndata = len(post.likelihood.y)
+        nfree = len(post.get_vary_params())
+        # chi = np.sum((post.likelihood.residuals()/post.likelihood.yerr)**2)
+        chi = np.sum((post.likelihood.residuals()/post.likelihood.errorbars())**2)
+        chi_red = chi / (ndata - nfree)
+
+        if verbose:
+            print(post)
+            print("N_free = %d" % nfree)
+            print("RMS = %4.2f" % np.std(post.likelihood.residuals()))
+            print("logprob (jitter fixed) = %4.2f" % post.logprob())
+            print("chi (jitter fixed) = %4.2f" % chi)
+            print("chi_red (jitter fixed) = %4.2f" % chi_red)
+            print("BIC (jitter fixed) = %4.2f" % post.bic())
+        
+        pdict['$N_{\\rm data}$'] = (ndata, 'number of measurements')
+        pdict['$N_{\\rm free}$'] = (nfree, 'number of free parameters')
+        pdict['RMS'] = (
+            np.round(np.std(post.likelihood.residuals()), 2), 
+            'RMS of residuals in m s$^{-1}$'
+        )
+        pdict['$\\chi^{2}$'] = (np.round(chi,2), "jitter fixed")
+        pdict['$\\chi^{2}_{\\nu}$'] = (
+            np.round(chi_red,2), "jitter fixed"
+        )
+        pdict['$\\ln{\\mathcal{L}}$'] = (
+            np.round(post.logprob(),2), "natural log of the likelihood"
+        )
+        pdict['BIC'] = (
+            np.round(post.bic(),2), 
+            'Bayesian information criterion'
+        )
+        
+        statsdict.append(pdict)
+
+    return statsdict
