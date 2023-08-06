409c5237aeca87f266de9d023b382629ae5bc37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:03 +0000

    Add package pubplot/0.1.2/pubplot-0.1.2.tar.gz

---
+++ packages/pubplot/pubplot-0.1.2.tar.gz/pubplot-0.1.2/setup.py
@@ -0,0 +1,40 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='pubplot',
+    version='0.1.2',
+    description='Seamless LaTeX and Matplotlib integration for publication plots',
+    long_description=readme,
+    packages=find_packages(),
+    url='',
+    download_url='',
+    license='ISC',
+    author='Hugo Sadok',
+    author_email='hugo@sadok.com.br',
+    keywords=['matplotlib', 'latex', 'pgf'],
+    include_package_data=True,
+    install_requires=[
+        'matplotlib',
+        'pylatex',
+        'numpy'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+)
