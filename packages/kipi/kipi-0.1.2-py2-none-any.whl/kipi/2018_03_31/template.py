381e50da46da4fe20813e8d40cad14069d32f656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:52 +0000

    Add package kipi/0.1.2/kipi-0.1.2-py2-none-any.whl

---
+++ packages/kipi/kipi-0.1.2-py2-none-any.whl/kipi/2018_03_31/template.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+# -*- coding: ascii -*- 
+"""
+module - short description
+
+long descruption
+
+Usage
+
+Authors
+
+License
+
+Copyright Bob Cousins 2018
+
+"""
+
+__author__ = 'Bob Cousins' 
+__copyright__ = 'Copyright 2018 Bob Cousins' 
+__license__ = 'GPL 3.0' 
+__version__ = '0.1.0'
+
+
+import os
+
+class Aclass:
+
+    def __init__():
+        self.init = True
+
+
+
