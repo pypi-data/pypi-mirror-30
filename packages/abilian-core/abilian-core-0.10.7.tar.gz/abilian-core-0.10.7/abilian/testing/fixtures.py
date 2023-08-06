6de39959087542408ea853b8dd60d76bef8b72ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:27 +0000

    Add package abilian-core/0.10.7/abilian-core-0.10.7.tar.gz

---
+++ packages/abilian-core/abilian-core-0.10.7.tar.gz/abilian-core-0.10.7/abilian/testing/fixtures.py
@@ -0,0 +1,86 @@
+# coding=utf-8
+"""Configuration and injectable fixtures for Pytest.
+
+Can be reused (and overriden) by adding::
+
+   pytest_plugins = ['abilian.testing.fixtures']
+
+to your `conftest.py`.
+
+Supposed to replace the too-complex current UnitTest-based testing
+framework. -> DI and functions over complex inheritance hierarchies FTW!
+"""
+
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
+
+from pytest import fixture
+
+from abilian.app import create_app
+from abilian.testing.util import cleanup_db, ensure_services_started, \
+    stop_all_services
+
+
+class TestConfig:
+    TESTING = True
+    SERVER_NAME = 'localhost'
+    CELERY_ALWAYS_EAGER = True  # run tasks locally, no async
+    CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+    MAIL_SENDER = 'tester@example.com'
+    SITE_NAME = "Abilian Test"
+    CSRF_ENABLED = True
+    WTF_CSRF_ENABLED = True
+    BABEL_ACCEPT_LANGUAGES = ['en', 'fr']
+
+
+@fixture
+def config():
+    return TestConfig
+
+
+@fixture
+def app(config):
+    # We currently return a fresh app for each test.
+    # Using session-scoped app doesn't currently work.
+    # Note: the impact on speed is minimal.
+    return create_app(config=config)
+
+
+@fixture
+def app_context(app):
+    with app.app_context() as ctx:
+        yield ctx
+
+
+@fixture
+def test_request_context(app):
+    with app.test_request_context() as ctx:
+        yield ctx
+
+
+@fixture
+def db(app, app_context):
+    """Return a fresh db for each test."""
+    from abilian.core.extensions import db
+
+    stop_all_services(app)
+    ensure_services_started(['repository', 'session_repository'])
+
+    cleanup_db(db)
+    db.create_all()
+    yield db
+
+    db.session.remove()
+    cleanup_db(db)
+    stop_all_services(app)
+
+
+@fixture
+def session(db):
+    return db.session
+
+
+@fixture
+def client(app, db):
+    """Return a Web client, used for testing, bound to a DB session."""
+    return app.test_client()
