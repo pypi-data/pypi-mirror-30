b8045209beee91e9bf277ea7431677cac501a8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:54 +0000

    Add package Chern/2.1.0/Chern-2.1.0-py3.6.egg

---
+++ packages/Chern/Chern-2.1.0-py3.6.egg/Chern/kernel/VDirectory.py
@@ -0,0 +1,56 @@
+"""
+A module
+"""
+import os
+import subprocess
+from Chern.utils import utils
+from Chern.utils import git
+from Chern.kernel.VObject import VObject
+class VDirectory(VObject):
+    """
+    Nothing more to do for this VDirectory.
+    """
+    def helpme(self, command):
+        from Chern.kernel.Helpme import directory_helpme
+        print(directory_helpme.get(command, "No such command, try ``helpme'' alone."))
+
+    def add_parameter(self, parameter, value):
+        """
+        Add a parameter to the parameters file
+        """
+        if parameter == "parameters":
+            print("A parameter is not allowed to be called parameters")
+        parameters_file = utils.CondfigFile(self.path+"/.chern/parameters.py")
+        parameters_file.write_varialbe(parameter, value)
+        parameters = parameters_file.read_variable("parameters")
+        if parameters is None:
+            parameters = []
+        parameters.append(parameter)
+        parameters_file.write_variable("parameters", parameters)
+
+    def remove_parameter(self, parameter):
+        """
+        Remove a parameter to the parameters file
+        """
+        if parameter == "parameters":
+            print("parameters is not allowed to remove")
+            return
+        parameters_file = utils.ConfigFile(self.path+"/.chern/parameters.py")
+        parameters = parameters_file.read_variable("parameters")
+        if parameter not in parameters:
+            print("Parameter not found")
+            return
+        parameters.remove(parameter)
+        parameters_file.write_variable(parameter, None)
+        parameters_file.write_variable("parameters", parameters)
+        self.set_update_time()
+
+def create_directory(path, inloop=False):
+    path = utils.strip_path_string(path)
+    os.mkdir(path)
+    os.mkdir(path+"/.chern")
+    with open(path + "/.chern/config.py", "w") as f:
+        f.write("object_type = \"directory\"")
+    with open(path + "/README.md", "w") as f:
+        f.write("Please write README for this directory")
+    subprocess.call("vim %s/README.md"%path, shell=True)
