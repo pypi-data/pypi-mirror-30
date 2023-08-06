41e75f6fbfd34dc95bbdad733cbb298ed01157c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:57 +0000

    Add package pyhttpintercept/0.9.2/pyhttpintercept-0.9.2-py2.py3-none-any.whl

---
+++ packages/pyhttpintercept/pyhttpintercept-0.9.2-py2.py3-none-any.whl/pyhttpintercept/_constants.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+from ._metadata import __module_name__
+
+PROFILER_PROFILE_ID = __module_name__
+
+THREADED = u'Threaded'
+SERVER_THREADING_KEY = u'PyHTTPIntercept/Threading'
+
+START_SERVERS = u'Start WebServers'
+STOP_SERVERS = u'Stop WebServers'
+
+SSL_CERT_CA = u'PyHTTPInterceptCA'
+SSL_CERT_SERVER = u'PyHTTPInterceptSERVER'
+SSL_PROFILE_NAME = u'PyHTTPIntercept'
