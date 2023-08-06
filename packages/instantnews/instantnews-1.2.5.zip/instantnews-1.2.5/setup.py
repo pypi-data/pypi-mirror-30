9d1a335df1dddefd2ce5c4e59e621d2cde34f2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:36 +0000

    Add package instantnews/1.2.5/instantnews-1.2.5.zip

---
+++ packages/instantnews/instantnews-1.2.5.zip/instantnews-1.2.5/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+
+with open('requirements.txt') as fobj:
+    install_requires = [line.strip() for line in fobj]
+
+
+setup(
+      name='instantnews',
+      version='1.2.5',
+      description='Get live news instantaneously',
+      author='shivam singh',
+      author_email='shivam043@gmail.com',
+      url='https://github.com/shivam043/instantnews',
+      license='MIT',
+      py_modules=['instantnews'],
+      install_requires=install_requires,
+      entry_points='''
+      [console_scripts]
+      instantnews=instantnews:parser
+      ''',
+)
