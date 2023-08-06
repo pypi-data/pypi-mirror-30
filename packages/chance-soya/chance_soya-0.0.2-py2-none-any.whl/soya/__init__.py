04e9cc8c8a4c0515279091007a060512e5ee1547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:17 +0000

    Add package chance-soya/0.0.2/chance_soya-0.0.2-py2-none-any.whl

---
+++ packages/chance-soya/chance_soya-0.0.2-py2-none-any.whl/soya/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File: soya/__init__.py
+# Author: Jimin Huang <huangjimin@whu.edu.cn>
+# Date: 19.03.2018
+from process import Soya
+
+
+__all__ = ['Soya']
