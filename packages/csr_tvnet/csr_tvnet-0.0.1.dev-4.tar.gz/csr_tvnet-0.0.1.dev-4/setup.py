d7147480ccdbd1ff7f5efaf8d78223c6fd7079be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:57 +0000

    Add package csr_tvnet/0.0.1.dev-4/csr_tvnet-0.0.1.dev-4.tar.gz

---
+++ packages/csr_tvnet/csr_tvnet-0.0.1.dev-4.tar.gz/csr_tvnet-0.0.1.dev-4/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+project_name = 'csr_tvnet'
+project_ver = '0.0.1.dev-4'
+setup(
+    name=project_name,
+    packages=['csr_tvnet', 'cloud'],
+    version=project_ver,
+    description='Transit Vnet Package',
+    author='Vamsi Kalapala',
+    author_email='vakalapa@cisco.com',
+    scripts=["bin/setup_tvnet.py"],
+    # use the URL to the github repo
+    url='https://github4-chn.cisco.com/csr1000v-cloud/' + project_name,
+    download_url='https://pypi.python.org/pypi?:action=display&name=%s&version=%s' % (
+        project_name, project_ver),
+    keywords=['cisco', 'azure', 'guestshell', 'dmvpn', 'csr1kv', 'csr1000v'],
+    classifiers=[],
+    license="MIT",
+    install_requires=[
+        'azure_storage',
+        'futures',
+        'requests[security]'
+    ],
+)
