4ae518a063ca4e7fc4c7f10d0cbc046657442588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:45 +0000

    Add package pytelegraf/0.3.2/pytelegraf-0.3.2.tar.gz

---
+++ packages/pytelegraf/pytelegraf-0.3.2.tar.gz/pytelegraf-0.3.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pytelegraf',
+    version='0.3.2',
+    description='Telegraf client',
+    author='paksu',
+    url='https://github.com/paksu/pytelegraf',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    extras_require={
+        'http': ['requests-futures==0.9.7']
+    },
+    test_suite='telegraf.tests',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
