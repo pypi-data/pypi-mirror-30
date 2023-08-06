ca4ff96a0d93dc44c69b802fbf2abbc69af01395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:51 +0000

    Add package SpamScope/2.4.0/SpamScope-2.4.0.tar.gz

---
+++ packages/SpamScope/SpamScope-2.4.0.tar.gz/SpamScope-2.4.0/src/bolts/tokenizer.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Copyright 2016 Fedele Mantuano (https://www.linkedin.com/in/fmantuano/)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+import datetime
+import random
+import six
+from collections import deque
+
+from streamparse import Stream
+import mailparser
+from mailparser import get_header
+from modules import AbstractBolt, MAIL_PATH, MAIL_STRING, MAIL_PATH_OUTLOOK
+from modules.attachments import fingerprints, MailAttachments
+
+
+class Tokenizer(AbstractBolt):
+    """Split the mail in token parts (body, attachments, etc.) and sends
+    these parts to others bolts."""
+
+    outputs = [
+        Stream(
+            fields=['sha256_random', 'mail', 'is_filtered'],
+            name='mail'),
+        Stream(
+            fields=['sha256_random', 'raw_mail', 'mail_type', 'is_filtered'],
+            name='raw_mail'),
+        Stream(
+            fields=['sha256_random', 'body', 'is_filtered'],
+            name='body'),
+        Stream(
+            fields=['sha256_random', 'network', 'is_filtered'],
+            name='network'),
+        Stream(
+            fields=['sha256_random', 'with_attachments', 'attachments'],
+            name='attachments')]
+
+    def initialize(self, stormconf, context):
+        super(Tokenizer, self).initialize(stormconf, context)
+
+        self.mailparser = {
+            MAIL_PATH: mailparser.parse_from_file,
+            MAIL_PATH_OUTLOOK: mailparser.parse_from_file_msg,
+            MAIL_STRING: mailparser.parse_from_string}
+
+        self.mails_analyzed = deque(maxlen=self.conf["maxlen_mails"])
+        self.network_analyzed = deque(maxlen=self.conf["maxlen_network"])
+        self.attachments_analyzed = deque(
+            maxlen=self.conf["maxlen_attachments"])
+
+        self._load_filters()
+
+    def _load_filters(self):
+        self.filter_mails_enabled = self.conf["filter_mails"]
+        self.filter_network_enabled = self.conf["filter_network"]
+        self.filter_attachments_enabled = self.conf["filter_attachments"]
+
+    def _make_mail(self, tup):
+        raw_mail = tup.values[0]
+        mail_type = tup.values[5]
+        rand = '_' + ''.join(random.choice('0123456789') for i in range(10))
+        self.parser = self.mailparser[mail_type](raw_mail)
+        mail = self.parser.mail
+
+        # Data mail sources
+        mail["mail_server"] = tup.values[1]
+        mail["mailbox"] = tup.values[2]
+        mail["priority"] = tup.values[3]
+        mail["sender_ip"] = self.parser.get_server_ipaddress(tup.values[4])
+        mail["to_domains"] = self.parser.to_domains
+
+        # Fingerprints of body mail
+        (mail["md5"], mail["sha1"], mail["sha256"], mail["sha512"],
+            mail["ssdeep"]) = fingerprints(self.parser.body.encode('utf-8'))
+        sha256_rand = mail["sha256"] + rand
+
+        # Add path to result
+        if mail_type == MAIL_PATH:
+            mail["path_mail"] = raw_mail
+
+        # Dates
+        if mail.get('date'):
+            mail["date"] = mail.get('date').isoformat()
+        else:
+            mail["date"] = datetime.datetime.utcnow().isoformat()
+
+        mail["analisys_date"] = datetime.datetime.utcnow().isoformat()
+
+        # Adding custom headers
+        for h in tup.values[6]:
+            mail["custom_" + h] = get_header(self.parser.message, h)
+
+        # Remove attachments
+        mail.pop("attachments", None)
+
+        return sha256_rand, mail
+
+    def process_tick(self, freq):
+        """Every freq seconds you reload configuration. """
+        super(Tokenizer, self).process_tick(freq)
+        self._load_filters()
+
+    def process(self, tup):
+        try:
+            sha256_rand, mail = self._make_mail(tup)
+            with_attachments = False
+            attachments = []
+            body = self.parser.body
+            raw_mail = tup.values[0]
+            mail_type = tup.values[5]
+
+            # If filter network is enabled
+            is_filtered_net = False
+            if self.filter_network_enabled:
+                if mail["sender_ip"] in self.network_analyzed:
+                    is_filtered_net = True
+
+                # Update database ip addresses analyzed
+                self.network_analyzed.append(mail["sender_ip"])
+
+            # If filter mails is enabled
+            is_filtered_mail = False
+            if self.filter_mails_enabled:
+                if mail["sha1"] in self.mails_analyzed:
+                    mail.pop("body", None)
+                    body = six.text_type()
+                    raw_mail = six.text_type()
+                    is_filtered_mail = True
+
+                # Update database mails analyzed
+                self.mails_analyzed.append(mail["sha1"])
+
+            if self.parser.attachments:
+                with_attachments = True
+                attachments = MailAttachments.withhashes(
+                    self.parser.attachments)
+
+                # If filter attachments is enabled
+                if self.filter_attachments_enabled:
+                    hashes = attachments.filter(self.attachments_analyzed)
+                    self.attachments_analyzed.extend(hashes)
+
+        except TypeError, e:
+            self.raise_exception(e, tup)
+
+        except UnicodeDecodeError, e:
+            self.raise_exception(e, tup)
+
+        else:
+            # Emit mail
+            self.emit([sha256_rand, mail, is_filtered_mail], stream="mail")
+
+            # Emit raw_mail
+            self.emit([
+                sha256_rand, raw_mail, mail_type, is_filtered_mail],
+                stream="raw_mail")
+
+            # Emit body
+            self.emit([sha256_rand, body, is_filtered_mail], stream="body")
+
+            # Emit network
+            self.emit([
+                sha256_rand, mail["sender_ip"], is_filtered_net],
+                stream="network")
+
+            # Emit attachments
+            self.emit([
+                sha256_rand, with_attachments, list(attachments)],
+                stream="attachments")
---
+++ packages/SpamScope/SpamScope-2.4.0.tar.gz/SpamScope-2.4.0/src/spouts/files_mails.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Copyright 2016 Fedele Mantuano (https://www.linkedin.com/in/fmantuano/)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from __future__ import absolute_import, print_function, unicode_literals
+from datetime import date
+
+import Queue
+import glob
+import os
+import shutil
+
+import six
+
+from modules import AbstractSpout, MailItem, MAIL_PATH, MAIL_PATH_OUTLOOK
+
+
+class FilesMailSpout(AbstractSpout):
+    outputs = ['raw_mail', 'mail_server', 'mailbox',
+               'priority', 'trust', 'mail_type', 'headers']
+
+    def initialize(self, stormconf, context):
+        super(FilesMailSpout, self).initialize(stormconf, context)
+
+        self._check_conf()
+        self._queue = Queue.PriorityQueue()
+        self._queue_tail = set()
+        self._count = 1
+        self._what = self.conf["post_processing"]["what"].lower()
+        self._load_mails()
+
+    def _check_conf(self):
+        self._where = self.conf["post_processing"]["where"]
+        if not self._where:
+            raise RuntimeError(
+                "where in {!r} is not configurated".format(
+                    self.component_name))
+
+        self._where_failed = self.conf["post_processing"]["where.failed"]
+        if not self._where_failed:
+            raise RuntimeError(
+                "where.failed in {!r} is not configurated".format(
+                    self.component_name))
+
+        if not os.path.exists(self._where_failed):
+            os.makedirs(self._where_failed)
+
+    def _load_mails(self):
+        """This function load mails in a priority queue. """
+
+        self.log("Loading new mails for {!r}".format(self.component_name))
+
+        mailboxes = self.conf["mailboxes"]
+        for k, v in mailboxes.iteritems():
+            if not os.path.exists(v["path_mails"]):
+                raise RuntimeError(
+                    "Mail path {!r} does not exist".format(v["path_mails"]))
+
+            all_mails = set(
+                glob.glob(os.path.join(v["path_mails"], v["files_pattern"])))
+
+            # put new mails in queue
+            for mail in (all_mails - self._queue_tail):
+                mail_type = MAIL_PATH_OUTLOOK \
+                    if v.get("outlook", False) else MAIL_PATH
+
+                self._queue_tail.add(mail)
+                self._queue.put(
+                    MailItem(
+                        filename=mail,
+                        mail_server=v["mail_server"],
+                        mailbox=k,
+                        priority=v["priority"],
+                        trust=v["trust_string"],
+                        mail_type=mail_type,
+                        headers=v.get("headers", [])))
+
+    def next_tuple(self):
+
+        # After reload.mails next_tuple reload spout config
+        if (self._count % self.conf["reload.mails"]):
+            self._count += 1
+
+        # put new mails in priority queue
+        else:
+            # Reload general spout conf
+            self._conf_loader()
+
+            # Reload new mails
+            self._load_mails()
+            self._count = 1
+
+        # If queue is not empty
+        if not self._queue.empty():
+            mail = self._queue.get(block=True)
+
+            self.emit([
+                mail.filename,  # 0
+                mail.mail_server,  # 1
+                mail.mailbox,  # 2
+                mail.priority,  # 3
+                mail.trust,  # 4
+                mail.mail_type,  # 5
+                mail.headers],  # 6
+                tup_id=mail.filename)
+
+    def ack(self, tup_id):
+        """Acknowledge tup_id, that is the path_mail. """
+
+        if os.path.exists(tup_id):
+            if self._what == "remove":
+                os.remove(tup_id)
+            else:
+                try:
+                    now = six.text_type(date.today())
+                    mail_path = os.path.join(self._where, now)
+                    if not os.path.exists(mail_path):
+                        os.makedirs(mail_path)
+                    # this chmod is useful to work under
+                    # nginx directory listing
+                    os.chmod(tup_id, 0o775)
+                    shutil.move(tup_id, mail_path)
+                except shutil.Error:
+                    os.remove(tup_id)
+
+        try:
+            # Remove from tail analyzed mail
+            self._queue.task_done()
+            self._queue_tail.remove(tup_id)
+            self.log("Mails to process: {}".format(len(self._queue_tail)))
+        except KeyError, e:
+            self.raise_exception(e, tup_id)
+
+    def fail(self, tup_id):
+        try:
+            os.chmod(tup_id, 0o775)
+            shutil.move(tup_id, self._where_failed)
+
+            # Remove from tail analyzed mail
+            self._queue.task_done()
+            self._queue_tail.remove(tup_id)
+
+        except IOError, e:
+            self.raise_exception(e, tup_id)
+
+        finally:
+            self.log("Mail {!r} failed. Check it".format(tup_id))
