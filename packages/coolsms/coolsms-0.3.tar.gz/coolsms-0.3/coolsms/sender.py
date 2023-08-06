2c63741ab7374281f89d5fb29ff9c534ebb4ca47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:39 +0000

    Add package coolsms/0.3/coolsms-0.3.tar.gz

---
+++ packages/coolsms/coolsms-0.3.tar.gz/coolsms-0.3/coolsms/sender.py
@@ -0,0 +1,35 @@
+import hashlib
+import hmac
+import secrets
+import requests
+from datetime import datetime
+
+
+class Sender(object):
+    def __init__(self, key, secret):
+        self._key = key
+        self._secret = secret
+
+    def _auth_params(self):
+        ts = int(datetime.now().timestamp())
+        salt = secrets.token_hex(8)
+        key = self._secret.encode('utf8')
+        msg = '{}{}'.format(ts, salt).encode('utf8')
+        sign = hmac.new(key, msg, hashlib.md5)
+        return {
+            'api_key': self._key,
+            'timestamp': ts,
+            'salt': salt,
+            'signature': sign.hexdigest(),
+        }
+
+    def send(self, sender, receiver, text):
+        params = self._auth_params()
+        params.update({
+            'from': sender,
+            'to': receiver,
+            'text': text,
+        })
+        print(params)
+        response = requests.post('https://api.coolsms.co.kr/sms/2/send', data=params)
+        return response.json()
