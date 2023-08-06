9edd68fee5fd657da0d3f2ba158323d32ced929e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:40 +0000

    Add package edurov/0.0.4/edurov-0.0.4.tar.gz

---
+++ packages/edurov/edurov-0.0.4.tar.gz/edurov-0.0.4/edurov/core.py
@@ -0,0 +1,97 @@
+import os
+import subprocess
+import time
+from multiprocessing import Process
+
+import Pyro4
+
+from edurov.sync import start_sync_classes
+from edurov.utils import warning, preexec_function
+from edurov.web import start_http_server
+
+
+
+class WebMethod(object):
+    def __init__(self, video_resolution='1024x768', fps=30, server_port=8000,
+                 debug=False, runtime_functions=None, index_file=None,
+                 custom_response=None):
+
+        self.res = video_resolution
+        self.fps = fps
+        self.server_port = server_port
+        self.debug = debug
+        self.run_funcs = self.valid_runtime_functions(runtime_functions)
+        self.cust_resp = self.valid_custom_response(custom_response)
+        self.index_file = self.valid_index_file(index_file)
+
+    def valid_custom_response(self, custom_response):
+        if custom_response:
+            if not callable(custom_response):
+                warning('custom_response parameter has to be a callable '
+                        'function, not type {}'.format(type(custom_response)))
+                return None
+        return custom_response
+
+    def valid_runtime_functions(self, runtime_functions):
+        if runtime_functions:
+            if callable(runtime_functions):
+                runtime_functions = [runtime_functions]
+            elif isinstance(runtime_functions, list):
+                for f in runtime_functions:
+                    if not callable(f):
+                        warning(
+                            'Parameter runtime_functions has to be a function '
+                            'or a list of functions, not {}'.format(type(f)))
+            else:
+                warning('Parameter runtime_functions has to be a function '
+                        'or a list of functions, not {}'
+                        .format(type(runtime_functions)))
+        return runtime_functions
+
+    def valid_index_file(self, file_path):
+        if not 'index.html' in file_path:
+            warning('The index files must be called "index.html')
+        if os.path.isfile(file_path):
+            return os.path.abspath(file_path)
+        else:
+            warning('could not find "{}", needs absolute path'
+                    .format(file_path))
+        return None
+
+    def serve(self, timeout=None):
+        start = time.time()
+        name_server = subprocess.Popen('pyro4-ns', shell=False,
+                                       preexec_fn=preexec_function)
+        time.sleep(2)
+        pyro_classes = Process(target=start_sync_classes)
+        pyro_classes.start()
+        time.sleep(4)
+        web_server = Process(
+            target=start_http_server,
+            args=(self.res, self.fps, self.server_port, self.index_file,
+                  self.debug, self.cust_resp))
+        web_server.daemon = True
+        web_server.start()
+        processes = []
+        for f in self.run_funcs:
+            p = Process(target=f)
+            p.daemon = True
+            p.start()
+            processes.append(p)
+
+        with Pyro4.Proxy("PYRONAME:ROVSyncer") as rov:
+            try:
+                while rov.run:
+                    if timeout:
+                        if time.time() - start >= timeout:
+                            break
+            except KeyboardInterrupt:
+                pass
+            finally:
+                print('Shutting down')
+                web_server.terminate()
+                rov.run = False
+                for p in processes:
+                    p.join(3)
+                pyro_classes.terminate()
+                name_server.terminate()
---
+++ packages/edurov/edurov-0.0.4.tar.gz/edurov-0.0.4/setup.py
@@ -0,0 +1,68 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+from edurov.utils import detect_pi
+
+CURRENT_PYTHON = sys.version_info[:2]
+REQUIRED_PYTHON = (3, 0)
+
+if CURRENT_PYTHON < REQUIRED_PYTHON:
+    sys.stderr.write(
+        """
+        ==========================
+        Unsupported Python version
+        ==========================
+        
+        eduROV requires python 3, try using pip3 instead of pip.
+        """)
+    sys.exit(1)
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='edurov',
+    version='0.0.4',
+    description='A educational project for remotely operated vehicles',
+    long_description=read('README.rst'),
+    license='GPLv3',
+    url='https://github.com/trolllabs/eduROV',
+
+    author='trolllabs',
+    author_email='martinloland@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Education',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Multimedia :: Video :: Display',
+        'Framework :: Robot Framework',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='video education ROV picamera',
+    install_requires=[
+                         'pygame',
+                         'Pyro4'] +
+                     (['picamera==1.13' if detect_pi() else []]),
+    python_requires='>=3',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'edurov-web = examples_edurov.entry_points:edurov_web',
+            'edurov-duo = examples_edurov.entry_points:edurov_duo'
+        ],
+    },
+    project_urls={
+        'Documentation': 'http://http://edurov.no/',
+        'Source': 'https://github.com/trolllabs/eduROV/',
+        'Tracker': 'https://github.com/trolllabs/eduROV/issues',
+    }
+)
