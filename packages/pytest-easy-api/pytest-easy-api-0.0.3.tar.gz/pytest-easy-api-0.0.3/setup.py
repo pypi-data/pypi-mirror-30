7d336f25c2b651f551771b0a7d682d644c9c6074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:58 +0000

    Add package pytest-easy-api/0.0.3/pytest-easy-api-0.0.3.tar.gz

---
+++ packages/pytest-easy-api/pytest-easy-api-0.0.3.tar.gz/pytest-easy-api-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from os import path
+
+# Get the long description from the README file
+with open(path.join(path.dirname(__file__), 'README.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name='pytest-easy-api',
+    version='0.0.3',
+    description='Simple API testing with pytest',
+    long_description=long_description,
+    url='https://github.com/thaffenden/pytest-easy-api',
+    author='Tristan Haffenden',
+    author_email="tristanehaffenden@gmail.com",
+    packages=['pytest_easy_api'],
+    install_requires=['pytest', 'requests'],
+    tests_require=['pytest', 'flask', 'pytest-cov'],
+    entry_points={
+        'pytest11': ['pytest_easy_api = pytest_easy_api.fixtures']
+    },
+    classifiers=[
+        "Framework :: Pytest",
+    ],
+    keywords=['testing', 'pytest', 'api']
+)
