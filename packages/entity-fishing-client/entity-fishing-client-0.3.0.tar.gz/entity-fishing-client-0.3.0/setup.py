12277c040a015398427791e0d93e942ce8c605aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:52 +0000

    Add package entity-fishing-client/0.3.0/entity-fishing-client-0.3.0.tar.gz

---
+++ packages/entity-fishing-client/entity-fishing-client-0.3.0.tar.gz/entity-fishing-client-0.3.0/setup.py
@@ -0,0 +1,34 @@
+from codecs import open
+from os import path
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file.
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='entity-fishing-client',
+    version='0.3.0',
+    description='A minimal client for entity-fishing service.',
+    long_description=long_description,
+    url='https://github.com/Hirmeos/entity-fishing-client-python',
+    author='Francesco de Virgilio, Luca Foppiano',
+    author_email='francesco.devirgilio@ubiquitypress.com, luca.foppiano@inria.fr',
+    license='GNU GPL v3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        # 'Topic :: Text processing',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords=['named entity recognition', 'entity matching', 'entity recognition'],
+    install_requires=['requests', 'zenlog'],
+    packages=['nerd'],
+    package_dir={
+        'nerd': 'entity-fishing_client'
+    }
+)
