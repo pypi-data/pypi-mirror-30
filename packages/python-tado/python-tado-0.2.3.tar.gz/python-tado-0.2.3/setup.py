44145a2e0c432a4940c8faf6a7bfa2030f0a1226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:55 +0000

    Add package python-tado/0.2.3/python-tado-0.2.3.tar.gz

---
+++ packages/python-tado/python-tado-0.2.3.tar.gz/python-tado-0.2.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='python-tado',
+      version='0.2.3',
+      description='PyTado from chrism0dwk, modfied by w.malgadey',
+      keywords='tado',
+      author='chrism0dwk, w.malgadey',
+      author_email='chrism0dwk@gmail.com, w.malgadey@gmail.com',
+      url='https://github.com/wmalgadey/PyTado',
+      zip_safe=False,
+      platforms=["any"],
+      packages=find_packages()
+     )
