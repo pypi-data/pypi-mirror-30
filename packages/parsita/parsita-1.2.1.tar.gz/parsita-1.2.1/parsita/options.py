b7c23387a546c438ac2e5e0a8d03395b66cbbe56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:19 +0000

    Add package parsita/1.2.1/parsita-1.2.1.tar.gz

---
+++ packages/parsita/parsita-1.2.1.tar.gz/parsita-1.2.1/parsita/options.py
@@ -0,0 +1,68 @@
+import re
+from typing import Any, Sequence
+
+from .state import Input, Output, SequenceReader, StringReader, Result, Success, Failure, Continue
+
+# Global mutable state
+
+default_whitespace = re.compile(r'\s*')
+whitespace = None
+
+
+# PyCharm does not understand type hint on handle_literal, so this signature is more general
+def default_handle_literal(literal: Any):
+    from .parsers import LiteralStringParser
+    return LiteralStringParser(literal, whitespace)
+
+
+def wrap_literal(literal: Input):
+    from .parsers import LiteralParser
+    return LiteralParser(literal)
+
+
+handle_literal = default_handle_literal
+
+
+def default_parse_method():
+    freeze_whitespace = whitespace
+
+    def regex_parse(self, source: str) -> Result:
+        reader = StringReader(source)
+        if freeze_whitespace is None:
+            result = self.consume(reader)
+        else:
+            result = (self << freeze_whitespace).consume(reader)
+
+        if isinstance(result, Continue):
+            if result.remainder.finished:
+                return Success(result.value)
+            elif result.farthest is None:
+                return Failure(result.remainder.expected_error('end of source'))
+            else:
+                return Failure(result.message())
+        else:
+            return Failure(result.message())
+
+    return regex_parse
+
+
+def basic_parse(self, source: Sequence[Input]) -> Result[Output]:
+    reader = SequenceReader(source)
+    result = self.consume(reader)
+
+    if isinstance(result, Continue):
+        if result.remainder.finished:
+            return Success(result.value)
+        elif result.farthest is None:
+            return Failure(result.remainder.expected_error('end of source'))
+        else:
+            return Failure(result.message())
+    else:
+        return Failure(result.message())
+
+
+parse_method = default_parse_method()
+
+
+__all__ = ['default_whitespace', 'whitespace', 'default_handle_literal', 'wrap_literal', 'handle_literal',
+           'default_parse_method', 'basic_parse', 'parse_method']
