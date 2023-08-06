14d8c2caac48a1fc323c17afed9017e6be3f5cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:05 +0000

    Add package pnutpy/0.2.3/pnutpy-0.2.3.tar.gz

---
+++ packages/pnutpy/pnutpy-0.2.3.tar.gz/pnutpy-0.2.3/HISTORY.rst
@@ -0,0 +1,32 @@
+.. :changelog:
+
+Release History
+---------------
+
+0.1.0 (2016-12-02)
+++++++++++++++++++
+
+* initial release
+
+
+0.2.0 (2017-08-19)
+++++++++++++++++++
+
+* add most channel and message endpoints
+
+
+0.2.1 (2017-11-03)
+++++++++++++++++++
+
+* update missing parameter constants
+
+
+0.2.2 (2018-01-29)
+++++++++++++++++++
+
+* add many missing endpoints, testing, documentation
+
+0.2.3 (2018-03-31)
+++++++++++++++++++
+
+* add poll endpoints
---
+++ packages/pnutpy/pnutpy-0.2.3.tar.gz/pnutpy-0.2.3/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name='pnutpy',
+      version='0.2.3',
+      description='pnut.io API library for python',
+      long_description=open('README.rst').read(),
+      license='MIT',
+      author='33MHz, pnut.io',
+      author_email='support@pnut.io',
+      url='https://github.com/pnut-api/PNUTpy',
+      packages=find_packages(exclude=['tests']),
+      install_requires=[
+          'python-dateutil>=2.2',
+          'requests>=2.4.3',
+      ],
+      keywords='pnut.io api library',
+      zip_safe=True)
