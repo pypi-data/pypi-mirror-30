bd485026f920e7cd2d802cf85fd6495fd9c90714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:31 +0000

    Add package matialvarezs-handlers-easy/0.1.10/matialvarezs_handlers_easy-0.1.10.tar.gz

---
+++ packages/matialvarezs-handlers-easy/matialvarezs_handlers_easy-0.1.10.tar.gz/matialvarezs_handlers_easy-0.1.10/matialvarezs_handlers_easy/utils.py
@@ -0,0 +1,10 @@
+import simplejson as json
+
+def delete_db_object(db_object,**kwargs,**options):
+    # method_delete => return object if exists or none
+    message_ok_delete = options.get('message_ok_delete','delete ok')
+    message_error_on_delete = options.get('message_error_on_delete','error on delete')
+    if db_object:
+        db_object.delete()
+        return json.dumps({"results": message_ok_delete, "error": None})
+    return json.dumps({"results": None, "error": message_error_on_delete})
