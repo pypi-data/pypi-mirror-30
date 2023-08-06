b5a28aba4910c01e9bfbdbeaf0737a723e88eb6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:28 +0000

    Add package iaajon/0.1.2/iaajon-0.1.2.tar.gz

---
+++ packages/iaajon/iaajon-0.1.2.tar.gz/iaajon-0.1.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 24 00:06:51 2018
+
+@author: NewType
+"""
+
+from setuptools import setup
+long_description = 'Detects potential corrupt entries in a dataframe.'
+setup(name='iaajon',
+      version='0.1.2',
+      description='Detects potential corrupt entries in a dataframe.',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
+      url='https://github.com/lionely/iaa',
+      author='Jonathan Scott',
+      author_email='lionelscotty95@gmail.com',
+      license='BSD',
+      packages=['iaajon'],
+      install_requires=['googlemaps','numpy','pandas'],
+      zip_safe=False)
