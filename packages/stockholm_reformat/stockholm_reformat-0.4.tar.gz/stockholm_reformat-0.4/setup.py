19de94a8fa70afdd53a22420de1c61ffc7ac3b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:34 +0000

    Add package stockholm_reformat/0.4/stockholm_reformat-0.4.tar.gz

---
+++ packages/stockholm_reformat/stockholm_reformat-0.4.tar.gz/stockholm_reformat-0.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf8 -*-
+import setuptools
+from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+
+ext = Extension('stockholm_reformat.creformat',
+                sources=['stockholm_reformat/creformat.c'],
+                extra_compile_args=['-O2', '-mtune=native', '-funroll-loops',
+                                    '-fpic'])
+
+setup(name='stockholm_reformat', version='0.4',
+      ext_modules=cythonize([ext]),
+      description='Fast Stockholm to other formats Multiple Sequence Alignment reformater.',
+      url='https://github.com/Dapid/stockholm_reformat',
+      author='David Menéndez Hurtado',
+      author_email='davidmenhur@gmail.com',
+      license='BSD 3-clause',
+      packages=setuptools.find_packages(),
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      install_requires=['Cython'],
+      setup_requires=['Cython'],
+      scripts=['bin/stockholm_to_a3m', 'bin/stockholm_to_fasta', 'bin/stockholm_to_aln'],
+      classifiers=['Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Cython',
+                   'Programming Language :: Python :: Implementation :: CPython',
+                   'Topic :: Scientific/Engineering :: Bio-Informatics',
+                   'Intended Audience :: Science/Research',
+                   'Development Status :: 3 - Alpha',
+                   'License :: OSI Approved :: BSD License']
+    )
