6af239c7353725e7f556ee1202d2c3a121445ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:25 +0000

    Add package deos.org/0.1/deos.org-0.1.tar.gz

---
+++ packages/deos.org/deos.org-0.1.tar.gz/deos.org-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='deos.org',
+      version='0.1',
+      description='deos.org',
+      url='https://github.com/desantis/python-deos.org',
+      author='Andrew T. DeSantis',
+      author_email='atd@deos.org',
+      license='MIT',
+      packages=['deos.org'],
+      zip_safe=False)
