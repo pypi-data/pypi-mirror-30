da36f307a3fcbbe6699e5b937bdbf714527f5fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:01 +0000

    Add package s3-dumps/0.1.6/s3_dumps-0.1.6.tar.gz

---
+++ packages/s3-dumps/s3_dumps-0.1.6.tar.gz/s3_dumps-0.1.6/setup.py
@@ -0,0 +1,42 @@
+import os
+
+from setuptools import setup
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
+
+setup(
+    name='s3_dumps',
+    packages=['s3_dumps'],
+    version=__import__('s3_dumps').__version__,
+    description='A package for backup DB and store in s3',
+    long_description=readme,
+    author='Reck31',
+    author_email='rakesh.gunduka@gmail.com',
+    url='https://github.com/rakeshgunduka/s3_dumps',
+    include_package_data=True,
+    install_requires=[
+        "boto3"
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    entry_points={
+        'console_scripts': [
+            'postgres_to_s3=s3_dumps.postgres_to_s3:main',
+            'redis_to_s3=s3_dumps.redis_to_s3:main'
+        ],
+    },
+    scripts=[
+        's3_dumps/postgres_to_s3.py',
+        's3_dumps/redis_to_s3.py'
+    ],
+    keywords = ['s3', 'backup', 'postgres', 'redis', 'digitaloceanspaces', 'aws'],
+    zip_safe=False
+)
