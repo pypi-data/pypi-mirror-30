39c2052d05f91af4952d0a5b807c04f3059b47bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:08 +0000

    Add package radvel/1.1.8/radvel-1.1.8-cp27-cp27m-macosx_10_6_x86_64.whl

---
+++ packages/radvel/radvel-1.1.8-cp27-cp27m-macosx_10_6_x86_64.whl/radvel/__init__.py
@@ -0,0 +1,40 @@
+from .model import *
+from .likelihood import *
+from . import posterior
+from .mcmc import *
+from .prior import *
+from .utils import *
+from .report import *
+from .plotting import *
+from .fitting import *
+import os 
+import sys
+
+__all__=['model', 'likelihood', 'posterior', 'mcmc', 'prior', 'utils',
+         'fitting', 'report', 'cli', 'driver', 'gp']
+
+__version__ = '1.1.8'
+
+MODULEDIR, filename = os.path.split(__file__)
+DATADIR = os.path.join(sys.prefix, 'radvel_example_data')
+
+# tell python how to pickle methods and fucntions; necessary for running MCMC in multi-
+#   threaded mode.
+def _pickle_method(method):
+    func_name = method.im_func.__name__
+    obj = method.im_self
+    cls = method.im_class
+    return _unpickle_method, (func_name, obj, cls)
+
+def _unpickle_method(func_name, obj, cls):
+    for cls in cls.mro():
+        try:
+            func = cls.__dict__[func_name]
+        except KeyError:
+            pass
+        else:
+            break
+    return func.__get__(obj, cls)
+if sys.version_info[0] < 3:
+    import copy_reg
+    copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
