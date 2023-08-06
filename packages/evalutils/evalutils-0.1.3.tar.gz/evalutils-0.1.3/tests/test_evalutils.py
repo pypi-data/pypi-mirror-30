28faf20c99c7c55737a5020cce1493bd38a474ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:46 +0000

    Add package evalutils/0.1.3/evalutils-0.1.3.tar.gz

---
+++ packages/evalutils/evalutils-0.1.3.tar.gz/evalutils-0.1.3/tests/test_evalutils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import evalutils
+
+def test_class_creation():
+    # noinspection PyUnusedLocal
+    class TestEval(evalutils.Evaluation):
+        pass
+
