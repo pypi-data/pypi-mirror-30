38bff9d42d6e288436ab6c657c8466af822e8247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:16 +0000

    Add package EvilSample/0.1/EvilSample-0.1.tar.gz

---
+++ packages/EvilSample/EvilSample-0.1.tar.gz/EvilSample-0.1/EvilSample/__init__.py
@@ -0,0 +1,2 @@
+def joke():
+    return (u'English language is easy, german is useless')
---
+++ packages/EvilSample/EvilSample-0.1.tar.gz/EvilSample-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='EvilSample',
+      version='0.1',
+      description='Save it.',
+      url='http://mad.com',
+      author='Flying Circus',
+      author_email='joe@example.com',
+      license='BPUT',
+      packages=['EvilSample'],
+      zip_safe=False)
