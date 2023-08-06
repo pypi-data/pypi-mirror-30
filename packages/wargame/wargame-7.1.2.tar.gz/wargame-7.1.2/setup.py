ec0c1ecdf093f7a294207d73b126ffdfcdea27ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:18 +0000

    Add package wargame/7.1.2/wargame-7.1.2.tar.gz

---
+++ packages/wargame/wargame-7.1.2.tar.gz/wargame-7.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('README') as file:
+	readme=file.read()
+setup(
+	name='wargame',
+	version='7.1.2',
+	packages=['wargame'],
+	url='https://tespypi.python.org/pypi/ataqueaOrcos1112/',
+	license='LICENSE.txt',
+	description='fantasiaAtaqueaOrcos1113',
+	long_description=readme,
+	author='jmartinacero',
+	author_email='javiermartin@jmrobotica.com'
+)
+
+
