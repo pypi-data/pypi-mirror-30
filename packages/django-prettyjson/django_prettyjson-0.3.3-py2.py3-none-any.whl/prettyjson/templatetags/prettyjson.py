2e7273dd6f1d16f060c1afac826d0277cbfda77f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:35 +0000

    Add package django-prettyjson/0.3.3/django_prettyjson-0.3.3-py2.py3-none-any.whl

---
+++ packages/django-prettyjson/django_prettyjson-0.3.3-py2.py3-none-any.whl/prettyjson/templatetags/prettyjson.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+from django.template import Library
+from django.utils.safestring import mark_safe
+
+import six
+
+from ..widgets import PrettyJSONWidget
+from standardjson import StandardJSONEncoder
+
+register = Library()
+
+
+@register.simple_tag
+def prettyjson_setup(jquery=True):
+    widget = PrettyJSONWidget()
+    media = widget.media
+    if not jquery:
+        media._js = media._js[2:]
+    return mark_safe(str(media))
+
+
+@register.simple_tag
+def prettyjson(obj):
+    data = obj
+    if isinstance(obj, six.string_types):
+        data = json.loads(obj)
+    widget = PrettyJSONWidget()
+    return mark_safe(widget.render(name=None,
+                     value=(json.dumps(data, ensure_ascii=False, cls=StandardJSONEncoder))))
