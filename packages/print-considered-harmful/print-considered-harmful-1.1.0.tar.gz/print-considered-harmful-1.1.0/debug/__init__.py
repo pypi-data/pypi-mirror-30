b05af2531b10e564b79118bd9315e329917b36d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:24 +0000

    Add package print-considered-harmful/1.1.0/print-considered-harmful-1.1.0.tar.gz

---
+++ packages/print-considered-harmful/print-considered-harmful-1.1.0.tar.gz/print-considered-harmful-1.1.0/debug/__init__.py
@@ -0,0 +1,31 @@
+# Import this module into your script to enable debugging.
+
+
+def breakpoint(value=None, up=1):
+    if breakpoint.enabled:
+        import sys
+        import ipdb
+        frame = sys._getframe()
+        for __ in range(up): frame = frame.f_back
+        # Call sset_trace (starting with double s) rather than set_trace
+        # in order to break free from nosetests's captured stdout!
+        ipdb.sset_trace(frame, context=7)
+    return value
+
+
+def disable_all_breakpoints():
+    breakpoint.enabled = False
+
+
+breakpoint.enabled = True
+breakpoint.disable = disable_all_breakpoints
+
+
+__builtins__['debugger'] = breakpoint
+
+
+def wrap(function):
+    def wrapper(*args, **quarks):
+        breakpoint(up=2)
+        return function(*args, **quarks)
+    return wrapper
