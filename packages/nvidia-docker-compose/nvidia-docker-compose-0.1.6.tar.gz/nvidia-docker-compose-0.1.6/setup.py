bf1481e7c87303b47e8a69577e4adcb1553fc27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:29 +0000

    Add package nvidia-docker-compose/0.1.6/nvidia-docker-compose-0.1.6.tar.gz

---
+++ packages/nvidia-docker-compose/nvidia-docker-compose-0.1.6.tar.gz/nvidia-docker-compose-0.1.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='nvidia-docker-compose',
+      version='0.1.6',
+      description='GPU enabled docker-compose wrapper',
+      url='https://github.com/eywalker/nvidia-docker-compose',
+      author='Edgar Y. Walker',
+      author_email='edgar.walker@gmail.com',
+      license='MIT',
+      packages=[],
+      install_requires=['docker-compose', 'pyyaml', 'jinja2'],
+      scripts=['bin/nvidia-docker-compose']
+      )
