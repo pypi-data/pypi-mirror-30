c4876f263f592464235f4fe463bc0a5b34b8e1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:40 +0000

    Add package django-react-templatetags/5.0.0/django_react_templatetags-5.0.0.tar.gz

---
+++ packages/django-react-templatetags/django_react_templatetags-5.0.0.tar.gz/django_react_templatetags-5.0.0/django_react_templatetags/encoders.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import django
+
+from django.core.serializers.json import DjangoJSONEncoder
+from django_react_templatetags.mixins import RepresentationMixin
+
+
+def json_encoder_cls_factory(context):
+    class ReqReactRepresentationJSONEncoder(ReactRepresentationJSONEncoder):
+        context = None
+
+    ReqReactRepresentationJSONEncoder.context = context
+    return ReqReactRepresentationJSONEncoder
+
+
+class ReactRepresentationJSONEncoder(DjangoJSONEncoder):
+    '''
+    Custom json encoder that adds support for RepresentationMixin
+    '''
+
+    def default(self, o):
+        if isinstance(o, RepresentationMixin):
+            if not hasattr(o, 'to_react_representation'):
+                return o.react_representation
+
+            args = [self.context if hasattr(self, 'context') else None]
+            args = [x for x in args if x is not None]
+
+            return o.to_react_representation(*args)
+
+        return super(ReactRepresentationJSONEncoder, self).default(o)
