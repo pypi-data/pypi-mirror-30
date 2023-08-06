fb1d6fd5e2f5058685137af6c5c0e73ba201da0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:15 +0000

    Add package dar/0.1.0/dar-0.1.0.tar.gz

---
+++ packages/dar/dar-0.1.0.tar.gz/dar-0.1.0/dar/cli.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Console script for dar."""
+import sys
+import click
+
+from dar import dar
+
+
+@click.group()
+def main(args=None):
+    """Console script for dar."""
+
+
+@main.command()
+@click.argument("key")
+@click.argument("command", nargs=-1)
+def register(key, command):
+    config = dar.get_config()
+    dar.register_alias(config, key, command)
+
+
+@main.command()
+@click.argument("key")
+def run(key):
+    config = dar.get_config()
+    dar.call_alias(config, key)
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
