d42c185a96b3d4d9b382f5850fed57b8668eab0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:56 +0000

    Add package compoda/0.3.3/compoda-0.3.3.tar.gz

---
+++ packages/compoda/compoda-0.3.3.tar.gz/compoda-0.3.3/compoda/utils.py
@@ -0,0 +1,177 @@
+"""Utility functions."""
+
+# Part of Compoda library
+# Copyright (C) 2016-2018  Omer Faruk Gulban
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import division
+import sys
+import numpy as np
+
+
+def truncate_range(data, percMin=0.25, percMax=99.75, discard_zeros=True):
+    """Truncate too low and too high values.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        Image to be truncated.
+    percMin : float
+        Percentile minimum.
+    percMax : float
+        Percentile maximum.
+    discard_zeros : bool
+        Discard voxels with value 0 from truncation.
+
+    Returns
+    -------
+    data : np.ndarray
+
+    """
+    if discard_zeros:
+        msk = ~np.isclose(data, 0)
+        pMin, pMax = np.nanpercentile(data[msk], [percMin, percMax])
+    else:
+        pMin, pMax = np.nanpercentile(data, [percMin, percMax])
+    temp = data[~np.isnan(data)]
+    temp[temp < pMin], temp[temp > pMax] = pMin, pMax  # truncate min and max
+    data[~np.isnan(data)] = temp
+    if discard_zeros:
+        data[~msk] = 0  # put back masked out voxels
+    return data
+
+
+def scale_range(data, scale_factor=500, delta=0, discard_zeros=True):
+    """Scale values as a preprocessing step.
+
+    Parameters
+    ----------
+    data : np.ndarray
+        Image to be scaled.
+    scale_factor : float
+        Lower scaleFactors provides faster interface due to loweing the
+        resolution of 2D histogram ( 500 seems fast enough).
+    delta : float
+        Delta ensures that the max data points fall inside the last bin
+        when this function is used with histograms.
+    discard_zeros : bool
+        Discard voxels with value 0 from truncation.
+
+    Returns
+    -------
+    data: np.ndarray
+        Scaled image.
+
+    """
+    if discard_zeros:
+        msk = ~np.isclose(data, 0)
+    else:
+        msk = np.ones(data.shape, dtype=bool)
+    scale_factor = scale_factor - delta
+    data[msk] = data[msk] - np.nanmin(data[msk])
+    data[msk] = scale_factor / np.nanmax(data[msk]) * data[msk]
+    if discard_zeros:
+        data[~msk] = 0  # put back masked out voxels
+    return data
+
+
+def cart_to_quasipolar(cart_coord):
+    """Convert n-dimensional cartesian to quasipolar coordinates [1].
+
+    Parameters
+    ----------
+    cart_coord : 2d numpy array, shape [n_samples, n_coordinates]
+        Cartesian coordinates.
+
+    Returns
+    -------
+    qpol_coord : 2d numpy array, shape [n_samples, n_coordinates]
+        Quasi-polar coordinates. First coordinate is always 'r'
+        calculated with euclidean norm.
+
+    Reference
+    ---------
+    [1] Nguyen, T. M. (2014). N-Dimensional Quasipolar Coordinates -
+        Theory and Application, pg 13. University of Las Vegas. URL:
+        http://digitalscholarship.unlv.edu/thesesdissertations/2125
+
+    """
+    dims = cart_coord.shape
+    qpol_coord = np.zeros(dims)
+    r = np.linalg.norm(cart_coord, axis=1)
+    qpol_coord[:, 0] = r
+
+    def rec_mult(qpol_inter, n, nr_mult):
+        """Iterative multiplication of sines [1]."""
+        temp = 1.0
+        for count in range(nr_mult):
+            temp *= np.sin(qpol_inter[:, n+1+count])
+        return temp
+
+    # This for loop is the literal implementation of [1].
+    # It starts from last quasi-polar (qpol) coordinate and finds n-1 thetas.
+    # In other words, it starts to fill the qpol coordinates end to beginning.
+    # Multiplication by 2 is done for range correction.
+    for i, n in enumerate(range(dims[1]-1, 0, -1)):
+        if n == dims[1]-1:
+            qpol_coord[:, n] = np.arccos(cart_coord[:, n] / r)
+        elif n == 1:
+            qpol_coord[:, n] = np.arcsin(cart_coord[:, n]
+                                         / (r * rec_mult(qpol_coord, n, i)))
+        else:
+            qpol_coord[:, n] = np.arccos(cart_coord[:, n]
+                                         / (r * rec_mult(qpol_coord, n, i)))
+    return qpol_coord
+
+
+def progress_output(input1, input2, text=''):
+    """Handy progress output procedure for command line.
+
+    Parameters
+    ----------
+    input1, input2 : Int
+        input1 out of input2
+
+    """
+    sys.stdout.write(("\r%i/%i " + text) % (input1, input2))
+    sys.stdout.flush()
+
+
+def isometric_projection(data, alpha=45, beta=35.5):
+    """Do isometric projection.
+
+    Parameters
+    ----------
+    TODO
+    """
+    alpha, beta = np.deg2rad(alpha), np.deg2rad(beta)
+    # trans = (data.min() + data.max()) / 2.
+    # data = data - trans
+    r_1 = np.array([[1., 0., 0.],
+                    [0., np.cos(alpha), np.sin(alpha)],
+                    [0., -np.sin(alpha), np.cos(alpha)]])
+    r_2 = np.array([[np.cos(beta), 0., -np.sin(beta)],
+                    [0., 1., 0.],
+                    [np.sin(beta), 0., np.cos(beta)]])
+    rot = np.dot(r_1, r_2)
+    projection = np.array([[1., 0., 0.], [0., 1., 0.]]).T
+    dims = data.shape
+    out = np.zeros((dims[0], dims[1]-1))
+    temp = np.zeros(dims)
+    for i in range(data.shape[0]):
+        temp[i, :] = 1./np.sqrt(6) * np.dot(data[i, :], rot)
+    for i in range(data.shape[0]):
+        out[i, :] = np.dot(temp[i, :], projection)
+    return out
