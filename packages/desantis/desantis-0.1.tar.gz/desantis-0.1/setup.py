0266c3763584a81f0e8d1344ef9faed595f45443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:08 +0000

    Add package desantis/0.1/desantis-0.1.tar.gz

---
+++ packages/desantis/desantis-0.1.tar.gz/desantis-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='desantis',
+      version='0.1',
+      description='desantis',
+      url='https://github.com/desantis/python-desantis',
+      author='Andrew T. DeSantis',
+      author_email='atd@deos.org',
+      license='MIT',
+      packages=['desantis'],
+      zip_safe=False)
