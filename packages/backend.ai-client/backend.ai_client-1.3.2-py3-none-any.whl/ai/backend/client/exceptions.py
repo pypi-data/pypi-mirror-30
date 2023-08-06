914d56669ebd25e894914546c75c9a91948e46d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:20 +0000

    Add package backend.ai-client/1.3.2/backend.ai_client-1.3.2-py3-none-any.whl

---
+++ packages/backend.ai-client/backend.ai_client-1.3.2-py3-none-any.whl/ai/backend/client/exceptions.py
@@ -0,0 +1,45 @@
+from typing import Any
+import json
+
+__all__ = (
+    'BackendError',
+    'BackendAPIError',
+    'BackendClientError',
+)
+
+
+class BackendError(Exception):
+    '''Exception type to catch all ai.backend-related errors.'''
+
+    def __str__(self):
+        return repr(self)
+
+
+class BackendAPIError(BackendError):
+    '''Exceptions returned by the API gateway.'''
+
+    def __init__(self, status: int, reason: str, data: Any):
+        if isinstance(data, (str, bytes)):
+            data = json.loads(data)
+        super().__init__(status, reason, data)
+
+    @property
+    def status(self) -> int:
+        return self.args[0]
+
+    @property
+    def reason(self) -> str:
+        return self.args[1]
+
+    @property
+    def data(self) -> Any:
+        return self.args[2]
+
+
+class BackendClientError(BackendError):
+    '''
+    Exceptions from the client library, such as argument validation
+    errors.
+    '''
+
+    pass
