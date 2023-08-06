d764f25ce6c41f6e0e67c3ba2c502ce607eceb69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:24 +0000

    Add package clashroyale/3.2.1/clashroyale-3.2.1.tar.gz

---
+++ packages/clashroyale/clashroyale-3.2.1.tar.gz/clashroyale-3.2.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='clashroyale',
+    packages=['clashroyale'],
+    version='v3.2.1',
+    description='An (a)sync wrapper for royaleapi.com',
+    author='kyb3r',
+    license='MIT',
+    url='https://github.com/cgrok/clashroyale',
+    keywords=['clashroyale', 'wrapper', 'cr', 'royaleapi'],
+    install_requires=['aiohttp>=2.0.0,<2.3.0', 'python-box==3.1.1', 'requests==2.18.4', 'asynctest==0.12.0'],
+)
