5dfafd30e8bdeb2d530bb013e556df9dd8c5e85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:36 +0000

    Add package owping-parser/0.0.1/owping-parser-0.0.1.tar.gz

---
+++ packages/owping-parser/owping-parser-0.0.1.tar.gz/owping-parser-0.0.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+setup(
+    name='owping-parser',
+    version='0.0.1',
+    author='mirgleich',
+    author_email='dev.david@familie-gleich.de',
+    description='A program to parse the output of owping',
+    url='https://github.com/mirgleich/owping-parser',
+    license='',  # TODO: license
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Topic :: System :: Networking',
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
+        # TODO: License
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    py_modules=['owping'],
+    keywords='owping network ping',
+    project_urls={
+        'Tracker': 'https://github.com/mirgleich/owping-parser/issues',
+        'Source': 'https://github.com/mirgleich/owping-parser',
+    },
+    python_requires='>=3',
+)
