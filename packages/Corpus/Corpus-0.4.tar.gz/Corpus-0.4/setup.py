039a1c19234233981a77b970d3dff05e4b81d75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:34 +0000

    Add package Corpus/0.4/Corpus-0.4.tar.gz

---
+++ packages/Corpus/Corpus-0.4.tar.gz/Corpus-0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+def readme():
+    with open('README.md') as f:
+        return f.read()
+		
+setup(name='Corpus',
+      version='0.4',
+      description='A simple English dictionary for Python',
+	  long_description='This can be used to check spelling of processed text for furthur use.',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Indexing',
+		'Natural Language :: English',
+		'Programming Language :: Python :: 2.7',
+      ],
+      keywords='dictionary spelling english linguistic',
+      url='https://github.com/aneeshb413/Corpus',
+      author='Aneesh Bhat',
+      author_email='aneeshb413@gmail.com',
+      license='MIT',
+      packages=['Corpus'],test_suite='nose.collector',
+	  tests_require=['nose'],
+	  include_package_data=True,
+      zip_safe=False)
