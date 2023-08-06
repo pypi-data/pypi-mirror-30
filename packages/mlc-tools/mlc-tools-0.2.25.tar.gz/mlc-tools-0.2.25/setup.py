72b87c4bc421fc45d8099f55cfa225b05527d5a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:30 +0000

    Add package mlc-tools/0.2.25/mlc-tools-0.2.25.tar.gz

---
+++ packages/mlc-tools/mlc-tools-0.2.25.tar.gz/mlc-tools-0.2.25/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+import src.version
+
+setup(
+    name='mlc-tools',
+    version=src.__version__,
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    install_requires=[
+    ]
+    # entry_points={
+    #     'console_scripts':
+    #         ['mlc-tools = src.main:console']
+    # },
+    # test_suite='tests'
+)
+
+
+# find_packages(package_dir)
