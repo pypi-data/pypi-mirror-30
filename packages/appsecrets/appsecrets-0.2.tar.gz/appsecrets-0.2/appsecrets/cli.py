ccf9bb8bcdd6327091f94283b89c218b10e34ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:16 +0000

    Add package appsecrets/0.2/appsecrets-0.2.tar.gz

---
+++ packages/appsecrets/appsecrets-0.2.tar.gz/appsecrets-0.2/appsecrets/cli.py
@@ -0,0 +1,57 @@
+import argparse
+import sys
+
+from .api import Secrets
+from .exc import Error
+
+
+def encrypt(args):
+    Secrets(args.path).encrypt_all()
+    print("Secrets in %s are now all encrypted" % args.path)
+
+
+def decrypt(args):
+    plaintext = Secrets(args.path).decrypt(name=args.name)
+    print(plaintext.decode('utf-8'), end='')
+
+
+def check(args):
+    names = Secrets(args.path).list_unencrypted()
+    if names:
+        print("Error: secrets in plaintext: %s" % ', '.join(names))
+        sys.exit(1)
+
+
+def build_parser():
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(dest='command')
+
+    sp = subparsers.add_parser('encrypt', help='Encrypt all secrets in plaintext')
+    sp.add_argument('path')
+    sp.set_defaults(func=encrypt)
+
+    sp = subparsers.add_parser('decrypt', help='Decrypt a secret and print to stdout')
+    sp.add_argument('path')
+    sp.add_argument('name')
+    sp.set_defaults(func=decrypt)
+
+    sp = subparsers.add_parser('check', help='Succeed only if all secrets are encrypted')
+    sp.add_argument('path')
+    sp.set_defaults(func=check)
+
+    return parser
+
+
+def main():
+    parser = build_parser()
+    args = parser.parse_args()
+
+    if not hasattr(args, 'func'):
+        parser.print_help()
+        sys.exit(1)
+
+    try:
+        args.func(args)
+    except Error as err:
+        sys.exit(err)
