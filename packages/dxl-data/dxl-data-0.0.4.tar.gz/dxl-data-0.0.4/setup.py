83cd6353dcb8c021234e682a808e3f4cd803b303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:40 +0000

    Add package dxl-data/0.0.4/dxl-data-0.0.4.tar.gz

---
+++ packages/dxl-data/dxl-data-0.0.4.tar.gz/dxl-data-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name='dxl-data',
+    version='0.0.4',
+    description='Data Processing Library.',
+    url='https://github.com/Hong-Xiang/dxdata',
+    author='Hong Xiang',
+    author_email='hx.hongxiang@gmail.com',
+    license='MIT',
+    packages=[
+        'dxl.data', 'dxl.data.core', 'dxl.data.core.image',
+        'dxl.data.core.numpy_ops', 'dxl.data.io',
+        'dxl.data.statistics.image'
+    ],
+    package_dir={'': 'src/python'},
+    install_requires=['dxl-core', 'dxl-fs', 'pandas==0.20.3'],
+    scripts=[],
+    #   namespace_packages = ['dxl'],
+    zip_safe=False)
