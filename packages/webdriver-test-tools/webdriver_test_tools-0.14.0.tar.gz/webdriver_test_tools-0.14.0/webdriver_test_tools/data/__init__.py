5a6a12c7d58e00a3cc16a388bf1215900688e64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:46 +0000

    Add package webdriver-test-tools/0.14.0/webdriver_test_tools-0.14.0.tar.gz

---
+++ packages/webdriver-test-tools/webdriver_test_tools-0.14.0.tar.gz/webdriver_test_tools-0.14.0/webdriver_test_tools/data/__init__.py
@@ -0,0 +1,21 @@
+# TODO: add loremipsum documentation (and contents?)
+"""
+The `data` subpackage includes some external packages for generating fake user info and
+placeholder text. For more information, see their documentation pages:
+
+    - `randomuser package <http://connordelacruz.com/python-randomuser/>`__
+    - `loremipsum <http://connordelacruz.com/py-loremipsum/>`__
+
+.. autoclass:: randomuser.randomuser.RandomUser
+    :members:
+    :undoc-members:
+
+    .. automethod:: __init__
+
+.. automodule:: loremipsum
+    :members:
+    :undoc-members:
+    :show-inheritance:
+"""
+from randomuser import RandomUser
+import loremipsum
