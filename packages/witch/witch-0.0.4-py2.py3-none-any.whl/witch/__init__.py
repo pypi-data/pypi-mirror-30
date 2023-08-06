c8049bd1043daffde6200ae5bfb41a5008f71aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:08 +0000

    Add package witch/0.0.4/witch-0.0.4-py2.py3-none-any.whl

---
+++ packages/witch/witch-0.0.4-py2.py3-none-any.whl/witch/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for witch."""
+
+__author__ = """James Draper"""
+__email__ = 'draperjames@github.io'
+__version__ = '0.0.4'
+
+import os
+
+# def find_path():
+#     """Find the location of package in any given file system."""
+#     __dir_path__ = os.path.dirname(os.path.realpath(__file__))
+#     return __dir_path__
+#
+# with open(os.path.join(find_path(), '__version__')) as f:
+#     __version__ = f.read().strip()
