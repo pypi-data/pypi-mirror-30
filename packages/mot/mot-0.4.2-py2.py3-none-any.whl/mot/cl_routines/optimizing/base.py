40d00088e41d7c117cc039ddbc6d7e1e73c9f225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:35 +0000

    Add package mot/0.4.2/mot-0.4.2-py2.py3-none-any.whl

---
+++ packages/mot/mot-0.4.2-py2.py3-none-any.whl/mot/cl_routines/optimizing/base.py
@@ -0,0 +1,393 @@
+import logging
+import numpy as np
+import pyopencl as cl
+
+from mot.cl_routines.mapping.objective_function_calculator import ObjectiveFunctionCalculator
+from ...utils import get_float_type_def, KernelInputDataManager
+from ...cl_routines.base import CLRoutine
+from ...load_balance_strategies import Worker
+from ...__version__ import __version__
+
+__author__ = 'Robbert Harms'
+__date__ = "2014-05-18"
+__license__ = "LGPL v3"
+__maintainer__ = "Robbert Harms"
+__email__ = "robbert.harms@maastrichtuniversity.nl"
+
+
+return_code_labels = {
+    0: ['default', 'no return code specified'],
+    1: ['found zero', 'sum of squares/evaluation below underflow limit'],
+    2: ['converged', 'the relative error in the sum of squares/evaluation is at most tol'],
+    3: ['converged', 'the relative error of the parameter vector is at most tol'],
+    4: ['converged', 'both errors are at most tol'],
+    5: ['trapped', 'by degeneracy; increasing epsilon might help'],
+    6: ['exhausted', 'number of function calls exceeding preset patience'],
+    7: ['failed', 'ftol<tol: cannot reduce sum of squares any further'],
+    8: ['failed', 'xtol<tol: cannot improve approximate solution any further'],
+    9: ['failed', 'gtol<tol: cannot improve approximate solution any further'],
+    10: ['NaN', 'Function value is not-a-number or infinite'],
+    11: ['exhausted', 'temperature decreased to 0.0']
+}
+
+
+class AbstractOptimizer(CLRoutine):
+
+    def __init__(self, patience=1, optimizer_settings=None, **kwargs):
+        """Create a new optimizer that will minimize the given model using the given environments.
+
+        If the environment is None, a suitable default environment is created.
+
+        Args:
+            cl_environments (list of CLEnvironment): a list with the cl environments to use
+            load_balancer (SimpleLoadBalanceStrategy): the load balance strategy to use
+            patience (int): The patience is used in the calculation of how many iterations to iterate the optimizer.
+                The exact usage of this value of this parameter may change per optimizer.
+            optimizer_options (dict): extra options one can set for the optimization routine. These are routine
+                dependent.
+        """
+        super(AbstractOptimizer, self).__init__(**kwargs)
+
+        self._optimizer_settings = optimizer_settings or {}
+        if not isinstance(self._optimizer_settings, dict):
+            raise ValueError('The given optimizer settings is not a dictionary.')
+
+        self.patience = patience or 1
+
+        if 'patience' in self._optimizer_settings:
+            self.patience = self._optimizer_settings['patience']
+        self._optimizer_settings['patience'] = self.patience
+
+    @property
+    def optimizer_settings(self):
+        """Get the optimization flags and settings set to the optimizer.
+
+        This ordinarily should contain all the extra flags and options set to the optimization routine. Even those
+        set additionally in the constructor.
+
+        Returns:
+            dict: the optimization options (settings)
+        """
+        return self._optimizer_settings
+
+    def minimize(self, model, starting_positions):
+        """Minimize the given model using the given environments.
+
+        Args:
+            model (mot.model_interfaces.OptimizeModelInterface): The model to minimize
+            starting_points (ndarray): The starting starting_positions for every problem in the model, it should be
+                an matrix of shape (d, p) with d problems and p parameter.
+
+        Returns:
+            OptimizationResults: the container with the optimization results
+        """
+        raise NotImplementedError()
+
+
+class OptimizationResults(object):
+
+    def get_optimization_result(self):
+        """Get the optimization result, that is, the matrix with the optimized parameter values.
+
+        Returns:
+            ndarray: the optimized parameter maps, an (d, p) array with for d problems a value for every p parameters
+        """
+        raise NotImplementedError()
+
+    def get_return_codes(self):
+        """Get the return codes, that is, the matrix with for every problem the return code of the optimizer
+
+        Returns:
+            ndarray: the return codes, an (d,) vector with for d problems a return code
+        """
+        raise NotImplementedError()
+
+    def get_objective_values(self):
+        """Get the objective values for each of the problem instances.
+
+        Returns:
+            ndarray: (d,) matrix with for every problem d, the objective value
+        """
+        raise NotImplementedError()
+
+
+class SimpleOptimizationResult(OptimizationResults):
+
+    def __init__(self, model, optimization_results, return_codes):
+        """Simple optimization results container which computes some values only when requested.
+
+        Args:
+            model (mot.model_interfaces.OptimizeModelInterface): the model we used to get these results
+            optimization_results (ndarray): a (d, p) matrix with for every d problems the estimated value for
+                every parameter p
+            return_codes (ndarray): the return codes as a (d,) vector for every d problems
+        """
+        self._model = model
+        self._optimization_results = optimization_results
+        self._return_codes = return_codes
+        self._objective_values = None
+
+    def get_optimization_result(self):
+        return self._optimization_results
+
+    def get_return_codes(self):
+        return self._return_codes
+
+    def get_objective_values(self):
+        if self._objective_values is None:
+            self._objective_values = np.nan_to_num(ObjectiveFunctionCalculator().calculate(
+                self._model, self._optimization_results))
+        return self._objective_values
+
+
+class AbstractParallelOptimizer(AbstractOptimizer):
+
+    def __init__(self, **kwargs):
+        """
+        Args:
+            optimizer_settings (dict): extra options one can set for the optimization routine. These are routine
+                dependent.
+        """
+        super(AbstractParallelOptimizer, self).__init__(**kwargs)
+        self._logger = logging.getLogger(__name__)
+
+    def minimize(self, model, starting_positions):
+        self._logger.info('Entered optimization routine.')
+        self._logger.info('Using MOT version {}'.format(__version__))
+        self._logger.info('We will use a {} precision float type for the calculations.'.format(
+            'double' if self._double_precision else 'single'))
+        for env in self.load_balancer.get_used_cl_environments(self.cl_environments):
+            self._logger.info('Using device \'{}\'.'.format(str(env)))
+        self._logger.info('Using compile flags: {}'.format(self.get_compile_flags_list()))
+        self._logger.info('We will use the optimizer {} '
+                          'with optimizer settings {}'.format(self.__class__.__name__,
+                                                              self._optimizer_settings))
+
+        self._logger.info('Starting optimization preliminaries')
+
+        if len(starting_positions.shape) < 2:
+            starting_positions = starting_positions[..., None]
+
+        parameters = np.require(starting_positions, self._mot_float_dtype, requirements=['C', 'A', 'O', 'W'])
+
+        if parameters.shape[0] != model.get_nmr_problems():
+            raise ValueError('The number of problems in the model does not match the number of starting points given.')
+
+        if parameters.shape[1] != model.get_nmr_parameters():
+            raise ValueError('The number of parameters in the model does not match the number of '
+                             'starting points given.')
+
+        nmr_params = model.get_nmr_parameters()
+        return_codes = np.zeros((model.get_nmr_problems(),), dtype=np.int8, order='C')
+
+        self._logger.info('Finished optimization preliminaries')
+        self._logger.info('Starting optimization')
+        workers = self._create_workers(self._get_worker_generator(self, model, parameters,
+                                                                  nmr_params, return_codes, self._mot_float_dtype,
+                                                                  self._optimizer_settings, ))
+        self.load_balancer.process(workers, model.get_nmr_problems())
+        self._logger.info('Finished optimization')
+
+        parameters = model.finalize_optimized_parameters(parameters)
+        return SimpleOptimizationResult(model, parameters, return_codes)
+
+    def _get_worker_generator(self, *args):
+        """Generate the worker generator callback for the function _create_workers()
+
+        This is supposed to be overwritten by the implementing optimizer.
+
+        Returns:
+            the python callback for generating the worker
+        """
+        return lambda cl_environment: AbstractParallelOptimizerWorker(cl_environment, self._double_precision, *args)
+
+
+class AbstractParallelOptimizerWorker(Worker):
+
+    def __init__(self, cl_environment, double_precision, parent_optimizer, model, starting_points,
+                 nmr_params, return_codes, mot_float_dtype, optimizer_settings=None):
+        super(AbstractParallelOptimizerWorker, self).__init__(cl_environment)
+
+        self._optimizer_settings = optimizer_settings
+
+        self._parent_optimizer = parent_optimizer
+
+        self._model = model
+        self._data_info = self._model.get_kernel_data()
+        self._data_struct_manager = KernelInputDataManager(self._data_info, mot_float_dtype)
+        self._double_precision = double_precision
+        self._nmr_params = nmr_params
+
+        self._return_codes = return_codes
+
+        self._starting_points = starting_points
+        self._all_buffers, self._params_buffer, self._return_code_buffer = self._create_buffers()
+        self._kernel = self._build_kernel(self._get_kernel_source(), self._parent_optimizer.get_compile_flags_list())
+
+    def calculate(self, range_start, range_end):
+        nmr_problems = range_end - range_start
+
+        kernel_func = self._kernel.minimize
+        kernel_func.set_scalar_arg_dtypes(self._get_buffer_scalar_args())
+
+        kernel_func(self._cl_queue, (nmr_problems, ), None, *self._all_buffers,
+                    global_offset=(range_start,))
+        self._enqueue_readout(self._params_buffer, self._starting_points, range_start, range_end)
+        self._enqueue_readout(self._return_code_buffer, self._return_codes, range_start, range_end)
+
+    def _get_buffer_scalar_args(self):
+        scalar_args = [None, None]
+        scalar_args.extend(self._data_struct_manager.get_scalar_arg_dtypes())
+        return scalar_args
+
+    def _create_buffers(self):
+        all_buffers = []
+
+        parameters_buffer = cl.Buffer(self._cl_context,
+                                      cl.mem_flags.READ_WRITE | cl.mem_flags.USE_HOST_PTR,
+                                      hostbuf=self._starting_points)
+        all_buffers.append(parameters_buffer)
+
+        return_code_buffer = cl.Buffer(self._cl_context,
+                                       cl.mem_flags.WRITE_ONLY | cl.mem_flags.USE_HOST_PTR,
+                                       hostbuf=self._return_codes)
+        all_buffers.append(return_code_buffer)
+        all_buffers.extend(self._data_struct_manager.get_kernel_inputs(self._cl_context, 1))
+
+        return all_buffers, parameters_buffer, return_code_buffer
+
+    def _get_kernel_source(self):
+        """Generate the kernel source for this optimization routine.
+
+        By default this returns a full kernel source using information from _get_optimizer_cl_code()
+        and _get_optimizer_call_name().
+
+        One could overwrite this function to completely generate the kernel source, but most likely
+        you would want to implement _get_optimizer_cl_code() and _get_optimizer_call_name().
+
+        Returns:
+            str: The kernel source for this optimization routine.
+        """
+        nmr_params = self._nmr_params
+
+        kernel_source = ''
+        kernel_source += get_float_type_def(self._double_precision)
+        kernel_source += self._data_struct_manager.get_struct_definition()
+
+        kernel_source += self._get_optimizer_cl_code()
+        kernel_source += '''
+            __kernel void minimize(
+                ''' + ",\n".join(self._get_kernel_param_names()) + '''
+                ){
+                    ulong gid = get_global_id(0);
+        '''
+
+        kernel_source += '''
+                    mot_float_type x[''' + str(nmr_params) + '''];
+                    for(uint i = 0; i < ''' + str(nmr_params) + '''; i++){
+                        x[i] = params[gid * ''' + str(nmr_params) + ''' + i];
+                    }
+
+                    mot_data_struct data = ''' + self._data_struct_manager.get_struct_init_string('gid') + ''';
+                    return_codes[gid] = (char) ''' + self._get_optimizer_call_name() + '(' + \
+                         ', '.join(self._get_optimizer_call_args()) + ''');
+
+                    for(uint i = 0; i < ''' + str(nmr_params) + '''; i++){
+                        params[gid * ''' + str(nmr_params) + ''' + i] = x[i];
+                    }
+                }
+        '''
+        return kernel_source
+
+    def _get_kernel_param_names(self):
+        """Get the list of kernel parameter names.
+
+        This is useful if a subclass extended the buffers with an additional buffer.
+
+        Returns:
+            list of str: the list of kernel parameter names
+        """
+        kernel_param_names = ['global mot_float_type* restrict params',
+                              'global char* restrict return_codes']
+        kernel_param_names.extend(self._data_struct_manager.get_kernel_arguments())
+
+        return kernel_param_names
+
+    def _get_optimizer_call_args(self):
+        """Get the optimizer calling arguments.
+
+        This is useful if a subclass extended the buffers with an additional buffer.
+
+        Returns:
+            list of str: the list of optimizer call arguments
+        """
+        call_args = ['x', '(void*)&data']
+        return call_args
+
+    def _get_optimizer_cl_code(self):
+        """Get the optimization CL code that is called during optimization for each problem.
+
+        This is normally called by the default implementation of _get_ll_calculating_kernel().
+
+        By default this creates a CL function named 'evaluation' that can be called by the optimization routine.
+
+        Returns:
+            str: The kernel source for the optimization routine.
+        """
+        kernel_source = self._get_evaluate_function()
+        kernel_source += self._get_optimization_function()
+        return kernel_source
+
+    def _get_evaluate_function(self):
+        """Get the CL code for the evaluation function. This is called from _get_optimizer_cl_code.
+
+        Implementing optimizers can change this if desired.
+
+        Returns:
+            str: the evaluation function.
+        """
+        objective_function = self._model.get_objective_per_observation_function()
+        param_modifier = self._model.get_pre_eval_parameter_modifier()
+
+        kernel_source = ''
+        kernel_source += objective_function.get_cl_code()
+        kernel_source += param_modifier.get_cl_code()
+        kernel_source += '''
+            double evaluate(mot_float_type* x, void* data_void){
+
+                mot_data_struct* data = (mot_data_struct*)data_void;
+
+                mot_float_type x_model[''' + str(self._model.get_nmr_parameters()) + '''];
+                for(uint i = 0; i < ''' + str(self._model.get_nmr_parameters()) + '''; i++){
+                    x_model[i] = x[i];
+                }
+
+                ''' + param_modifier.get_cl_function_name() + '''(data, x_model);
+
+                double sum = 0;
+                for(uint i = 0; i < ''' + str(self._model.get_nmr_inst_per_problem()) + '''; i++){
+                    sum += ''' + objective_function.get_cl_function_name() + '''(data, x_model, i);
+                }
+                return sum;
+            }
+        '''
+        return kernel_source
+
+    def _get_optimization_function(self):
+        """Return the optimization function as a CL string for the implementing optimizer.
+
+        This is a convenience function to avoid boilerplate in implementing _get_optimizer_cl_code().
+
+        Returns:
+            str: The optimization routine function
+        """
+
+    def _get_optimizer_call_name(self):
+        """Get the call name of the optimization routine.
+
+        This name is the name of the function called by the kernel to optimize a single problem.
+
+        Returns:
+            str: The function name of the optimization function.
+        """
+        return ''
---
+++ packages/mot/mot-0.4.2-py2.py3-none-any.whl/mot/cl_routines/optimizing/levenberg_marquardt.py
@@ -0,0 +1,177 @@
+import os
+from pkg_resources import resource_filename
+import pyopencl as cl
+from mot.utils import get_float_type_def
+from .base import AbstractParallelOptimizer, AbstractParallelOptimizerWorker
+
+__author__ = 'Robbert Harms'
+__date__ = "2014-02-05"
+__license__ = "LGPL v3"
+__maintainer__ = "Robbert Harms"
+__email__ = "robbert.harms@maastrichtuniversity.nl"
+
+
+class LevenbergMarquardt(AbstractParallelOptimizer):
+
+    default_patience = 250
+
+    def __init__(self, patience=None, step_bound=None, scale_diag=None, optimizer_settings=None, **kwargs):
+        """Use the Levenberg-Marquardt method to calculate the optimimum.
+
+        Args:
+            patience (int): Used to set the maximum number of iterations to patience*(number_of_parameters+1)
+        """
+        patience = patience or self.default_patience
+
+        optimizer_settings = optimizer_settings or {}
+
+        keyword_values = {}
+        keyword_values['step_bound'] = step_bound
+        keyword_values['scale_diag'] = scale_diag
+
+        option_defaults = {'step_bound': 100.0, 'scale_diag': 1}
+
+        def get_value(option_name):
+            value = keyword_values.get(option_name)
+            if value is None:
+                value = optimizer_settings.get(option_name)
+            if value is None:
+                value = option_defaults[option_name]
+            return value
+
+        for option in option_defaults:
+            optimizer_settings.update({option: get_value(option)})
+
+        super(LevenbergMarquardt, self).__init__(patience=patience, optimizer_settings=optimizer_settings, **kwargs)
+
+    def _get_worker_generator(self, *args):
+        return lambda cl_environment: LevenbergMarquardtWorker(cl_environment, self._double_precision, *args)
+
+
+class LevenbergMarquardtWorker(AbstractParallelOptimizerWorker):
+
+    def __init__(self, *args, **kwargs):
+        super(LevenbergMarquardtWorker, self).__init__(*args, **kwargs)
+
+        if self._model.get_nmr_inst_per_problem() < self._nmr_params:
+            raise ValueError('The number of instances per problem must be greater than the number of parameters')
+
+    def _create_buffers(self):
+        all_buffers, parameters_buffer, return_code_buffer = super(LevenbergMarquardtWorker, self)._create_buffers()
+
+        fjac_items = self._nmr_params * self._model.get_nmr_inst_per_problem() * self._starting_points.shape[0]
+        fjac_buffer_size = fjac_items * self._starting_points.dtype.itemsize
+        fjac_buffer = cl.Buffer(self._cl_context, cl.mem_flags.READ_WRITE, size=fjac_buffer_size)
+        all_buffers.append(fjac_buffer)
+
+        return all_buffers, parameters_buffer, return_code_buffer
+
+    def _get_buffer_scalar_args(self):
+        scalar_args = super(LevenbergMarquardtWorker, self)._get_buffer_scalar_args()
+        scalar_args.insert(2, None)
+        return scalar_args
+
+    def _get_kernel_source(self):
+        """Overwrite the default kernel source generation.
+
+        We need to add a few extra buffers to the kernel and the lmmin call. This makes the optimizer
+        more robust when using datasets with a high number of observations per problem instance.
+
+        If we inline these buffers the CL compiler may throw a 'out of resources' error since it can fail to find
+        a contiguous block of memory large enough for any of these buffers. We circumvent that now by creating them
+        in global memory space.
+        """
+        nmr_params = self._nmr_params
+
+        kernel_param_names = ['global mot_float_type* restrict params',
+                              'global char* restrict return_codes']
+        kernel_param_names.extend(self._data_struct_manager.get_kernel_arguments())
+        kernel_param_names.append('global mot_float_type* restrict fjac_all')
+
+        optimizer_call_args = 'x, (void*)&data'
+
+        kernel_source = ''
+        kernel_source += get_float_type_def(self._double_precision)
+        kernel_source += self._data_struct_manager.get_struct_definition()
+
+        kernel_source += self._get_optimizer_cl_code()
+        kernel_source += '''
+            __kernel void minimize(
+                ''' + ",\n".join(kernel_param_names) + '''
+                ){
+                    ulong gid = get_global_id(0);
+
+                    mot_float_type x[''' + str(nmr_params) + '''];
+                    for(uint i = 0; i < ''' + str(nmr_params) + '''; i++){
+                        x[i] = params[gid * ''' + str(nmr_params) + ''' + i];
+                    }
+
+                    global mot_float_type* fjac = fjac_all + gid * ''' \
+                         + str(self._nmr_params * self._model.get_nmr_inst_per_problem()) + ''';
+
+                    mot_data_struct data = ''' + self._data_struct_manager.get_struct_init_string('gid') + ''';
+                    return_codes[gid] = (char) ''' + self._get_optimizer_call_name() + '''(''' \
+                         + optimizer_call_args + ''', fjac);
+
+                    for(uint i = 0; i < ''' + str(nmr_params) + '''; i++){
+                        params[gid * ''' + str(nmr_params) + ''' + i] = x[i];
+                    }
+                }
+        '''
+        return kernel_source
+
+    def _get_evaluate_function(self):
+        """Get the CL code for the evaluation function. This is called from _get_optimizer_cl_code.
+
+        Implementing optimizers can change this if desired.
+
+        Returns:
+            str: the evaluation function.
+        """
+        objective_func = self._model.get_objective_per_observation_function()
+        param_modifier = self._model.get_pre_eval_parameter_modifier()
+
+        kernel_source = ''
+        kernel_source += objective_func.get_cl_code()
+        kernel_source += param_modifier.get_cl_code()
+        kernel_source += '''
+            void evaluate(mot_float_type* x, void* data_void, mot_float_type* result){
+                mot_data_struct* data = (mot_data_struct*)data_void;
+                
+                mot_float_type x_model[''' + str(self._model.get_nmr_parameters()) + '''];
+                for(uint i = 0; i < ''' + str(self._model.get_nmr_parameters()) + '''; i++){
+                    x_model[i] = x[i];
+                }
+                ''' + param_modifier.get_cl_function_name() + '''(data, x_model);
+                
+                for(uint i = 0; i < ''' + str(self._model.get_nmr_inst_per_problem()) + '''; i++){
+                    // the model expects the L1 norm, while the LM method takes the L2 norm. Taking square root here.
+                    result[i] = sqrt(fabs(''' + objective_func.get_cl_function_name() + '''(data, x_model, i)));
+                }
+            }
+        '''
+        return kernel_source
+
+    def _get_optimization_function(self):
+        params = {'NMR_PARAMS': self._nmr_params,
+                  'PATIENCE': self._parent_optimizer.patience,
+                  'NMR_INST_PER_PROBLEM': self._model.get_nmr_inst_per_problem(),
+                  'USER_TOL_MULT': 30}
+
+        optimizer_settings = self._optimizer_settings or {}
+        option_defaults = {'step_bound': 100.0, 'scale_diag': 1, 'usertol_mult': 30}
+        option_converters = {'scale_diag': lambda val: int(bool(val))}
+
+        for option, default in option_defaults.items():
+            v = optimizer_settings.get(option, default)
+            if option in option_converters:
+                v = option_converters[option](v)
+            params.update({option.upper(): v})
+
+        body = open(os.path.abspath(resource_filename('mot', 'data/opencl/lmmin.cl')), 'r').read()
+        if params:
+            body = body % params
+        return body
+
+    def _get_optimizer_call_name(self):
+        return 'lmmin'
