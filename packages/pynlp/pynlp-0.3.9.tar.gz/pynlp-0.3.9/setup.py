74e9d4bd9c8631751ae8e6f2c6f4c1dd6adbee13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:22 +0000

    Add package pynlp/0.3.9/pynlp-0.3.9.tar.gz

---
+++ packages/pynlp/pynlp-0.3.9.tar.gz/pynlp-0.3.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pynlp',
+    version='0.3.9',
+    packages=['pynlp'],
+    install_requires=['requests'],
+    url='http://github.com/sina-al/pynlp',
+    license='MIT',
+    author='Sina',
+    author_email='s.aleyaasin@gmail.com',
+    description='Python wrapper for Stanford CoreNLP',
+
+)
