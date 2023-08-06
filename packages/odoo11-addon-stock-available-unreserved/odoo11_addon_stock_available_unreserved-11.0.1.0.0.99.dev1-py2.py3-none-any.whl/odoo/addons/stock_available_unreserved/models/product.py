00044cdb916f7b7cebec820b2919c9c80fb54a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:41 +0000

    Add package odoo11-addon-stock-available-unreserved/11.0.1.0.0.99.dev1/odoo11_addon_stock_available_unreserved-11.0.1.0.0.99.dev1-py2.py3-none-any.whl

---
+++ packages/odoo11-addon-stock-available-unreserved/odoo11_addon_stock_available_unreserved-11.0.1.0.0.99.dev1-py2.py3-none-any.whl/odoo/addons/stock_available_unreserved/models/product.py
@@ -0,0 +1,103 @@
+# Copyright 2018 Camptocamp SA
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+from odoo.addons import decimal_precision as dp
+from odoo.tools.float_utils import float_round
+
+UNIT = dp.get_precision('Product Unit of Measure')
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    qty_available_not_res = fields.Float(
+        string='Quantity On Hand Unreserved',
+        digits=UNIT,
+        compute='_compute_product_available_not_res',
+    )
+
+    @api.multi
+    @api.depends('product_variant_ids.qty_available_not_res')
+    def _compute_product_available_not_res(self):
+        for tmpl in self:
+            if isinstance(tmpl.id, models.NewId):
+                continue
+            tmpl.qty_available_not_res = sum(
+                tmpl.mapped('product_variant_ids.qty_available_not_res')
+            )
+
+    @api.multi
+    def action_open_quants_unreserved(self):
+        products_ids = self.mapped('product_variant_ids').ids
+        quants = self.env['stock.quant'].search([
+            ('product_id', 'in', products_ids),
+        ])
+        quant_ids = quants.filtered(
+            lambda x: x.product_id.qty_available_not_res > 0
+        ).ids
+        result = self.env.ref('stock.product_open_quants').read()[0]
+        result['domain'] = [('id', 'in', quant_ids)]
+        result['context'] = {
+            'search_default_locationgroup': 1,
+            'search_default_internal_loc': 1,
+        }
+        return result
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    qty_available_not_res = fields.Float(
+        string='Qty Available Not Reserved',
+        digits=UNIT,
+        compute='_compute_qty_available_not_reserved',
+    )
+
+    @api.multi
+    def _prepare_domain_available_not_reserved(self):
+        domain_quant = [
+            ('product_id', 'in', self.ids),
+        ]
+        domain_quant_locations = self._get_domain_locations()[0]
+        domain_quant.extend(domain_quant_locations)
+        return domain_quant
+
+    @api.multi
+    def _compute_product_available_not_res_dict(self):
+
+        res = {}
+
+        domain_quant = self._prepare_domain_available_not_reserved()
+        quants = self.env['stock.quant'].with_context(lang=False).read_group(
+            domain_quant,
+            ['product_id', 'location_id', 'quantity', 'reserved_quantity'],
+            ['product_id', 'location_id'],
+            lazy=False)
+        product_sums = {}
+        for quant in quants:
+            # create a dictionary with the total value per products
+            product_sums.setdefault(quant['product_id'][0], 0.)
+            product_sums[quant['product_id'][0]] += (
+                quant['quantity'] - quant['reserved_quantity']
+            )
+        for product in self.with_context(prefetch_fields=False, lang=''):
+            available_not_res = float_round(
+                product_sums.get(product.id, 0.0),
+                precision_rounding=product.uom_id.rounding
+            )
+            res[product.id] = {
+                'qty_available_not_res': available_not_res,
+            }
+        return res
+
+    @api.multi
+    def _compute_qty_available_not_reserved(self):
+        res = self._compute_product_available_not_res_dict()
+        for prod in self:
+            qty = res[prod.id]['qty_available_not_res']
+            prod.qty_available_not_res = qty
+        return res
