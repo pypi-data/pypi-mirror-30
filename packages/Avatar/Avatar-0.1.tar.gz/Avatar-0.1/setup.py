8914b2ea3b9fc31a1d99c19bd564299ab31d87b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:20 +0000

    Add package Avatar/0.1/Avatar-0.1.tar.gz

---
+++ packages/Avatar/Avatar-0.1.tar.gz/Avatar-0.1/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='Avatar',
+    version='0.1',
+    description='Avatar generator.',
+    url='https://github.com/greyli/avatar',
+    author='Grey Li',
+    author_email='withlihui@gmail.com',
+    license='MIT',
+    include_package_data=True,
+    install_requires=[
+        'pillow',
+    ],
+)
