6ff4154b09685730eaf997469130d06091b71511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:47 +0000

    Add package py2sms/1.0/py2sms-1.0.tar.gz

---
+++ packages/py2sms/py2sms-1.0.tar.gz/py2sms-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'py2sms',
+  packages = ['py2sms'],
+  version = '1.0',
+  description = 'A package to send SMS using Way2Sms',
+  author = 'varunotelli',
+  author_email = 'varun15122@cse.ssn.edu.in',
+  url = 'https://github.com/varunotelli/Py2SMS', 
+  download_url = 'https://github.com/varunotelli/py2sms/archive/1.0.tar.gz',
+  long_description=open('readme.txt').read(), 
+  keywords = ['SMS', 'Messaging', 'Way2SMS']
+)
