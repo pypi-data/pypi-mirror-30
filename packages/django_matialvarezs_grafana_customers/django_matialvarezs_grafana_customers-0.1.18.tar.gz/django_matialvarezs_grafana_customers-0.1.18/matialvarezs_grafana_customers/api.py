753fdc8f1793066d205d7f7ca09e1ad254550582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:47 +0000

    Add package django_matialvarezs_grafana_customers/0.1.18/django_matialvarezs_grafana_customers-0.1.18.tar.gz

---
+++ packages/django_matialvarezs_grafana_customers/django_matialvarezs_grafana_customers-0.1.18.tar.gz/django_matialvarezs_grafana_customers-0.1.18/matialvarezs_grafana_customers/api.py
@@ -0,0 +1,30 @@
+from . import settings
+from matialvarezs_request_handler import utils as matialvarezs_request_handler_utils
+from . import models
+import json
+import requests
+def create_organisation(business_id, name):
+    data = {
+        "name": name
+    }
+    res = matialvarezs_request_handler_utils.send_post_and_get_response(settings.GRAFANA_API_BASE_URL + 'orgs',
+                                                                        data=data, headers=settings.GRAFANA_API_HEADERS,
+                                                                        convert_data_to_json=True)
+    # res = requests.post(settings.GRAFANA_API_BASE_URL + 'orgs',data=data, headers=settings.GRAFANA_API_HEADERS)
+    # res.
+    print("res antes de guardar organisation en grafana",res.content)
+    if res:
+        print("res al guardar organisation en grafana",res.content)
+        res_data = json.loads(res.content.decode('utf-8'))
+        organisation_id = res_data['orgId']
+        customer_org_grafana = models.BusinessCustomerOrganisationGrafana(business=business_id, organisation_id=organisation_id)
+        customer_org_grafana.save()
+
+
+
+def create_user():
+    pass
+
+
+def create_dashboard():
+    pass
