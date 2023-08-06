610248c1466e0fc0cab4bb0934e14604d6e3cb91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:32 +0000

    Add package hrpc/1.0.4/hrpc-1.0.4.tar.gz

---
+++ packages/hrpc/hrpc-1.0.4.tar.gz/hrpc-1.0.4/hrpc/transport/http.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+__author__ = 'lxn3032'
+
+
+import json
+import requests
+
+
+from hrpc.transport import Transport
+
+
+class HttpTransport(Transport):
+    def __init__(self, endpoint, client):
+        super(HttpTransport, self).__init__(endpoint, client)
+        self.connect()
+
+    def send(self, req):
+        req['session_id'] = self.session_id
+        r = requests.post(self.endpoint, data=json.dumps(req), headers={'Content-Type': 'application/json'})
+        self.rpc_client.put_response(r.json())
+
+    def connect(self):
+        self.connected = True
+
+    def disconnect(self):
+        pass
+
+    def ping(self):
+        try:
+            r = requests.post(self.endpoint, data='', timeout=1)
+            return True
+        except:
+            return False
+
+    def __str__(self):
+        msg = '<hrpc.__builtin__.transport over http> endpoint: {}'.format(self.endpoint)
+        return msg
