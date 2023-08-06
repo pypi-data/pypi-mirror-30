7720405a9f935acb32d14080833a1fd29a57a99c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:46 +0000

    Add package qextract/0.0.3/qextract-0.0.3-py2.py3-none-any.whl

---
+++ packages/qextract/qextract-0.0.3-py2.py3-none-any.whl/qextract/__init__.py
@@ -0,0 +1,6 @@
+"""Right now this is empty as this library is a command line tool only at this
+point.  If this changes, some methods will be moved here."""
+
+from .utils import *
+import spacy
+nlp = spacy.load('en')
