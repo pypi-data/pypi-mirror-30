7211effdf36ea2eb9358ee47ad54858179908986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:58 +0000

    Add package virtualenv2/0.0.1/virtualenv2-0.0.1.tar.gz

---
+++ packages/virtualenv2/virtualenv2-0.0.1.tar.gz/virtualenv2-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "virtualenv2",
+    version = "0.0.1",
+    author = "Kale Franz",
+    author_email = "kale@franz.io",
+    description = ("virtualenv alternative"),
+)
