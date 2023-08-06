a0a08c4079a33ffc4b79ff8dc140050622c0008e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:57 +0000

    Add package BiblioPixel/3.4.10/BiblioPixel-3.4.10.tar.gz

---
+++ packages/BiblioPixel/BiblioPixel-3.4.10.tar.gz/BiblioPixel-3.4.10/bibliopixel/animation/collection.py
@@ -0,0 +1,96 @@
+import os, traceback
+from .. project import aliases, construct, load, project
+from . import animation
+from .. util import json, log
+
+
+class Collection(animation.Animation):
+    """
+    A ``Collection`` is a list of ``Animation``s
+    """
+
+    @staticmethod
+    def pre_recursion(desc):
+        animations = []
+
+        for a in desc['animations']:
+            loaded = load.load_if_filename(a)
+            if loaded:
+                animation = loaded.get('animation', {})
+                run = loaded.get('run', {})
+
+            elif callable(a) or isinstance(a, str) or 'animation' not in a:
+                animation = a
+                run = {}
+
+            else:
+                animation = a.pop('animation', {})
+                run = a.pop('run', {})
+                if a:
+                    raise ValueError(
+                        'Extra fields in animation: ' + ', '.join(a))
+
+            animation = construct.to_type_constructor(
+                animation, 'bibliopixel.animation')
+
+            run.update(animation.get('run', {}))
+            animation['run'] = run
+
+            # Children without fps or sleep_time get it from their parents.
+            if not ('fps' in run or 'sleep_time' in run):
+                if 'fps' in desc['run']:
+                    run.update(fps=desc['run']['fps'])
+                elif 'sleep_time' in desc['run']:
+                    run.update(sleep_time=desc['run']['sleep_time'])
+
+            animations.append(animation)
+
+        desc['animations'] = animations
+        return desc
+
+    CHILDREN = 'animations',
+
+    def __init__(self, layout, animations=None, **kwds):
+        super().__init__(layout, **kwds)
+        self.animations = animations or []
+        self.internal_delay = 0  # never wait
+
+    # Override to handle all the animations
+    def cleanup(self, clean_layout=True):
+        self.state = animation.STATE.canceled
+        for a in self.animations:
+            a.cleanup()
+        super().cleanup(clean_layout)
+
+    def add_animation(self, anim, **kwds):
+        from .. util import deprecated
+        deprecated.deprecated('Collection.add_animation')
+
+        anim._set_runner(kwds)
+        self.animations.append(anim)
+
+    def pre_run(self):
+        self.animations = _AnimationList(self.animations)
+        for a in self.animations:
+            a.pre_run()
+
+
+class _AnimationList:
+    def __init__(self, animations):
+        self._animations = list(animations)
+        self._names = {a.name: i for i, a in enumerate(self._animations)}
+
+    def _index(self, i):
+        return i if isinstance(i, int) else self._names[i]
+
+    def __getitem__(self, i):
+        return self._animations[self._index(i)]
+
+    def __getattr__(self, i):
+        return self[i]
+
+    def __iter__(self):
+        return iter(self._animations)
+
+    def __len__(self):
+        return len(self._animations)
