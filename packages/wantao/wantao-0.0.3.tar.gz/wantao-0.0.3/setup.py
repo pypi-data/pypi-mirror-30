d37033e1af5b8a99ec93756bac89fb3620b99b78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:28 +0000

    Add package wantao/0.0.3/wantao-0.0.3.tar.gz

---
+++ packages/wantao/wantao-0.0.3.tar.gz/wantao-0.0.3/setup.py
@@ -0,0 +1,17 @@
+﻿from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name = 'wantao',
+    keyword='wantao',
+    version ='0.0.3',
+    description = 'just a simple test',
+    license = 'MIT License',
+    url='https://github.com/wantao666/wantao',
+    author = 'wantao',
+    author_email = '895484122@qq.com',
+    include_package_data=True,
+    packages = find_packages(),
+    install_requires=['requests>=2.0','httplib2>=0.9'],
+    zip_safe=False,
+)
