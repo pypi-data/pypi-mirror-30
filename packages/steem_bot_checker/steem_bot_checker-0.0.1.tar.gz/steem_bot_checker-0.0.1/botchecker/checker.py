54e236d97e4918c8018e389986687aee84b36e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:42 +0000

    Add package steem_bot_checker/0.0.1/steem_bot_checker-0.0.1.tar.gz

---
+++ packages/steem_bot_checker/steem_bot_checker-0.0.1.tar.gz/steem_bot_checker-0.0.1/botchecker/checker.py
@@ -0,0 +1,12 @@
+from .bots import bid_bots, other_bots
+from .datastructures import Bot
+
+
+def check(account: str) -> (bool, Bot):
+    if account in bid_bots:
+        return True, Bot(account=account, type="bid_bot")
+    if account in other_bots:
+        return True, Bot(account=account, type="other_bot")
+
+    return False, None
+
---
+++ packages/steem_bot_checker/steem_bot_checker-0.0.1.tar.gz/steem_bot_checker-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='steem_bot_checker',
+    version='0.0.1',
+    packages=["botchecker",],
+    url='https://github.com/emre/botchecker',
+    license='MIT',
+    author='emre yilmaz',
+    author_email='mail@emreyilmaz.me',
+    description='A simple Python library to check a STEEM account operators a bot or not',
+)
