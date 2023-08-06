a3dd224151ad44f6e923e323ae0f3119d91afdd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:19 +0000

    Add package ibird/1.2.0/ibird-1.2.0.tar.gz

---
+++ packages/ibird/ibird-1.2.0.tar.gz/ibird-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name="ibird", 
+	version="1.2.0", 
+	py_modules=['ibird'],
+	author="Aiqi.Xu", 
+	author_email='xu_aiqi@126.com',
+	description="A simple printer of nested lists",
+	)
