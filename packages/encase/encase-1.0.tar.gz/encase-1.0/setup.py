10cfba08fa82171a6bd3167d5c2acbc06ff72764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:23 +0000

    Add package encase/1.0/encase-1.0.tar.gz

---
+++ packages/encase/encase-1.0.tar.gz/encase-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'encase',
+  packages = ['encase'],
+  version = '1.0',
+  description = 'A Better Dictionary Class',
+  author = 'Ryan Miller',
+  author_email = 'ryan@devopsmachine.com',
+  url = 'https://github.com/RyanMillerC/encase',
+  download_url = 'https://github.com/RyanMillerC/encase/archive/1.0.tar.gz',
+  keywords = ['data', 'dictionary'],
+  classifiers = [],
+)
