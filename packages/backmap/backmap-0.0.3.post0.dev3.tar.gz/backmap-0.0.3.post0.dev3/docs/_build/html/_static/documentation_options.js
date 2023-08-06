b7172671be6919c025128e4433e70bbd40d6b73d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:12 +0000

    Add package backmap/0.0.3.post0.dev3/backmap-0.0.3.post0.dev3.tar.gz

---
+++ packages/backmap/backmap-0.0.3.post0.dev3.tar.gz/backmap-0.0.3.post0.dev3/docs/_build/html/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: 'v0.0.3+3.ge0c834c.dirty',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
