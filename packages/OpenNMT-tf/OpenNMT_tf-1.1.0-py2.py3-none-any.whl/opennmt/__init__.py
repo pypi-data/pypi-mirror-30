b82036b2de9c96b5553c686e5cc718209e7d7ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:50 +0000

    Add package OpenNMT-tf/1.1.0/OpenNMT_tf-1.1.0-py2.py3-none-any.whl

---
+++ packages/OpenNMT-tf/OpenNMT_tf-1.1.0-py2.py3-none-any.whl/opennmt/__init__.py
@@ -0,0 +1,10 @@
+"""OpenNMT module."""
+
+from opennmt import decoders
+from opennmt import encoders
+from opennmt import inputters
+from opennmt import layers
+from opennmt import models
+from opennmt import tokenizers
+
+from opennmt.runner import Runner
