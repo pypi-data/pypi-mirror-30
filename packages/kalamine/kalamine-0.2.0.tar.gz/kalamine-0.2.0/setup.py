c0eee7d21255a9ed8e16f7ee6feb77d65ae464cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:35 +0000

    Add package kalamine/0.2.0/kalamine-0.2.0.tar.gz

---
+++ packages/kalamine/kalamine-0.2.0.tar.gz/kalamine-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='kalamine',
+      version='0.2.0',
+      description='Keyboard Layout Maker',
+      url='http://github.com/fabi1cazenave/kalamine',
+      author='Fabien Cazenave',
+      author_email='fabien@cazenave.cc',
+      license='MIT',
+      packages=['kalamine'],
+      scripts=['bin/kalamine'],
+      install_requires=['pyyaml'],
+      include_package_data=True,
+      zip_safe=False)
