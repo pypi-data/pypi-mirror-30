123fe26f4e851a4bf4392c9a5a1ee1962fbb5d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:05 +0000

    Add package universal_interferometer/0.2/universal-interferometer-0.2.tar.gz

---
+++ packages/universal_interferometer/universal-interferometer-0.2.tar.gz/universal-interferometer-0.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 16:16:20 2018
+
+@author: clementsw
+"""
+
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='universal-interferometer',
+      version='0.2',
+      description='Algorithms for universal interferometers',
+      long_description='Decompose unitary matrices into interferometers using two different algorithms, and visualize the interferometers',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Physics',
+      ],      
+      url='http://github.com/clementsw/universal-interferometer',
+      author='William R. Clements',
+      author_email='mail@william-clements.com',
+      license='MIT',
+      packages=['universal-interferometer'],
+      install_requires=[
+          'numpy',
+          'matplotlib',
+      ],
+      include_package_data=True,
+      zip_safe=False)
