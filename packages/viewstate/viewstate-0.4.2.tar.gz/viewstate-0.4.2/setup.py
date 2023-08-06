4008852632fae4d63310bbe1028a43ab9c43a3c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:52 +0000

    Add package viewstate/0.4.2/viewstate-0.4.2.tar.gz

---
+++ packages/viewstate/viewstate-0.4.2.tar.gz/viewstate-0.4.2/setup.py
@@ -0,0 +1,28 @@
+from html import escape
+from pathlib import Path
+from setuptools import setup, find_packages
+
+with open(str(Path(__file__).resolve().parents[0] / 'README.rst'), encoding='utf-8') as f:
+    # HTML escape to ensure rendering on PyPI
+    long_description = escape(f.read())
+
+setup(
+    name='viewstate',
+    author='Yuval Adam',
+    author_email='_@yuv.al',
+    version='0.4.2',
+    description='.NET viewstate decoder',
+    long_description=long_description,
+    url='https://github.com/yuvadm/viewstate',
+    license='MIT',
+    python_requires='>=3.5.0',
+    packages=find_packages(exclude=['docs', 'tests']),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
