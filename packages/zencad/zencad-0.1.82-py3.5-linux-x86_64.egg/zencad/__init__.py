9cef1c701d6bc68359441d88d08b03252e00d2ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:51 +0000

    Add package zencad/0.1.82/zencad-0.1.82-py3.5-linux-x86_64.egg

---
+++ packages/zencad/zencad-0.1.82-py3.5-linux-x86_64.egg/zencad/__init__.py
@@ -0,0 +1,42 @@
+import math
+
+#widget
+from zencad.widget import display
+from zencad.widget import show
+
+#cache
+from zencad.cache import enable as enable_cache
+
+#solid
+from zencad.solid import box
+from zencad.solid import sphere
+from zencad.solid import torus
+from zencad.solid import cylinder
+from zencad.solid import cone
+
+from zencad.solid import linear_extrude
+
+#face
+from zencad.face import circle
+from zencad.face import ngon
+from zencad.face import square
+from zencad.face import rectangle
+from zencad.face import polygon
+
+#wire
+from zencad.wire import segment
+from zencad.wire import polysegment
+from zencad.wire import circle as wcircle
+from zencad.wire import arc_by_points
+
+
+def error(str):
+	print("ZenCadError: " + str)
+	exit(-1)
+
+from zencad.math3 import point
+from zencad.math3 import points
+#from zencad.zenlib import ZenVertex as vertex
+
+def gr(rad): return rad / 180 * math.pi
+from zencad.math3 import point as pnt
