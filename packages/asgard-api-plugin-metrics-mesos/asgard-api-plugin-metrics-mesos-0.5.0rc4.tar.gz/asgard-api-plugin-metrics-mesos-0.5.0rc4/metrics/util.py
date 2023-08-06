a537bb94d210d2c0be597c808ded34d4fcd837ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:47 +0000

    Add package asgard-api-plugin-metrics-mesos/0.5.0rc4/asgard-api-plugin-metrics-mesos-0.5.0rc4.tar.gz

---
+++ packages/asgard-api-plugin-metrics-mesos/asgard-api-plugin-metrics-mesos-0.5.0rc4.tar.gz/asgard-api-plugin-metrics-mesos-0.5.0rc4/metrics/util.py
@@ -0,0 +1,14 @@
+
+import requests
+
+from asgard.sdk import mesos
+
+from metrics import config
+
+def get_mesos_slaves():
+    leader_address = mesos.get_mesos_leader_address()
+    url = f"{leader_address}/slaves"
+    config.logger.debug({"action": "pre-fetch", "fetch-url": url})
+    response = requests.get(url, timeout=2)
+    config.logger.debug({"action": "post-fetch", "fetch-url": url, "fetch-status": response.status_code})
+    return response.json()
