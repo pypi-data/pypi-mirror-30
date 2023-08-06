2d49caceed647c257a02056a3eabb886cd307dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:39 +0000

    Add package goonj/0.1.4.1/goonj-0.1.4.1-py3-none-any.whl

---
+++ packages/goonj/goonj-0.1.4.1-py3-none-any.whl/goonj/entities.py
@@ -0,0 +1,45 @@
+class AlertSource(object):
+    def __init__(self, name, default_channels, severity):
+        self.name = name
+        self.default_channels = default_channels
+        self.severity = severity
+
+
+class Severity(object):
+    def __init__(self, channels):
+        self.channels = channels
+
+
+class Tag(object):
+    def __init__(self, name):
+        self.name = name
+
+
+class CustomMessage(object):
+    __slots__ = ['tags', 'severity', 'message', 'error', 'error_id', 'subject']
+
+    def __init__(self, tags, severity, message, error, error_id, subject):
+        self.tags = tags
+        self.severity = severity
+        self.message = message
+        self.error = error
+        self.error_id = error_id
+        self.subject = subject
+
+    def __str__(self):
+        return 'tags:{} , severity:{} ,subject:{}, message:{} , error_id:{} , error:{}'.format(str(
+            self.tags),
+            self.severity,
+            self.subject,
+            self.message,
+            self.error,
+            self.error_id
+        )
+
+
+class SmtpServerConfig(object):
+    def __init__(self, host, port, username, password):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
