a744bbfd16c633346fee2afd2ac47fa545e6490b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:24 +0000

    Add package lektor-natural-language/0.1/lektor-natural-language-0.1.tar.gz

---
+++ packages/lektor-natural-language/lektor-natural-language-0.1.tar.gz/lektor-natural-language-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='lektor-natural-language',
+    version='0.1',
+    author='Terminal Labs',
+    author_email='solutions@terminallabs.com',
+    description='Adds NLTK based template filters.',
+    license='BSD-3-Clause',
+    py_modules=['lektor_natural_language'],
+    url='https://github.com/terminal-labs/lektor-natural-language',
+    entry_points={
+        'lektor.plugins': [
+            'natural-language = lektor_natural_language:NaturalLanguagePlugin',
+        ]
+    },
+    install_requires=[
+        'nltk',
+        'rake-nltk'
+    ]
+)
