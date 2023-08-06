64150243cbe380136f12750c64e3f659813390c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:31 +0000

    Add package chainer-cyclegan/1.2.3/chainer-cyclegan-1.2.3.tar.gz

---
+++ packages/chainer-cyclegan/chainer-cyclegan-1.2.3.tar.gz/chainer-cyclegan-1.2.3/setup.py
@@ -0,0 +1,42 @@
+import subprocess
+import sys
+
+from pip.req import parse_requirements
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = '1.2.3'
+
+
+if sys.argv[-1] == 'release':
+    commands = [
+        'python setup.py sdist upload',
+        'git tag v{0}'.format(version),
+        'git push origin master --tag',
+    ]
+    for cmd in commands:
+        subprocess.call(cmd, shell=True)
+    sys.exit(0)
+
+
+try:
+    import cv2  # NOQA
+except ImportError:
+    print('Please install OpenCV.')
+    quit(1)
+
+
+install_requires = parse_requirements('requirements.txt', session=False)
+
+setup(
+    name='chainer-cyclegan',
+    description='Chainer Implementation of CycleGAN.',
+    version=version,
+    packages=find_packages(),
+    install_requires=[str(ir.req) for ir in install_requires],
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    url='https://github.com/wkentaro/chainer-cyclegan',
+    license='MIT',
+)
