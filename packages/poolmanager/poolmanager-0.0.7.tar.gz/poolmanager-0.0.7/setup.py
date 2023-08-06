7e09f6757b130e76d9e858ae6cad96ec7b7f06ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:04 +0000

    Add package poolmanager/0.0.7/poolmanager-0.0.7.tar.gz

---
+++ packages/poolmanager/poolmanager-0.0.7.tar.gz/poolmanager-0.0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+
+setup(name='poolmanager',
+      version='0.0.7',
+      description='Simple pool manager',
+      classifiers=[],
+      keywords='',
+      author='Loic Gasser',
+      author_email='loicgasser4@gmail.com',
+      license='MIT',
+      url='https://github.com/geoadmin/lib-poolmanager',
+      packages=find_packages(exclude=['tests']),
+      package_dir={'poolmanager': 'poolmanager'},
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='nose.collector',
+      install_requires=[],
+      )
+
