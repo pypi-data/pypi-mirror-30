09465013ac4db858a3b507a4268b49c806391f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:37 +0000

    Add package mastercard-blockchain-b2bxb/0.0.2/mastercard_blockchain_b2bxb-0.0.2-py2-none-any.whl

---
+++ packages/mastercard-blockchain-b2bxb/mastercard_blockchain_b2bxb-0.0.2-py2-none-any.whl/mastercardblockchainb2bxb/__init__.py
@@ -0,0 +1,10 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardblockchainb2bxb.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardblockchainb2bxb.claims import *
+from mastercardblockchainb2bxb.fx import *
+from mastercardblockchainb2bxb.payments import *
