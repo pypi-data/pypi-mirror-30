5f821d683eb8153e9bc875dd4856e0c5eb3e22bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:43 +0000

    Add package jardin/0.18.2/jardin-0.18.2.tar.gz

---
+++ packages/jardin/jardin-0.18.2.tar.gz/jardin-0.18.2/jardin/__init__.py
@@ -0,0 +1,14 @@
+import sys
+
+if not hasattr(sys, 'jardin_setup') or (hasattr(sys, 'jardin_setup') and not sys.jardin_setup):
+    from jardin.model import Model, Collection
+    import jardin.config
+
+__author__ = 'Emmanuel Turlay'
+__copyright__ = 'Copyright 2017, Instacart'
+__credits__ = ['Emmanuel Turlay', 'Mathieu Ripert']
+__license__ = 'MIT'
+__version__ = '0.18.2'
+__maintainer__ = 'Emmanuel Turlay'
+__email__ = 'emmanuel@instacart.com'
+__status__ = 'Prototype'
