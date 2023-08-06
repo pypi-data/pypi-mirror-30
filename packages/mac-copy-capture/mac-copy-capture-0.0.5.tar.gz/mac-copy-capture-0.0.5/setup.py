ea8f9dd222fc15410fea9a2671ae04ec2a6794c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:55 +0000

    Add package mac-copy-capture/0.0.5/mac-copy-capture-0.0.5.tar.gz

---
+++ packages/mac-copy-capture/mac-copy-capture-0.0.5.tar.gz/mac-copy-capture-0.0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='mac-copy-capture',
+      version='0.0.5',
+      author="cijianzy",
+      author_email="cijianzy@gmail.com",
+      url="http://cijianzy.com",
+      packages=find_packages(),
+      #package_data={'setuptools_app': ['contents/hello']},
+      # scripts=['bin/say_hello'],
+      description=("A tool to capture Mac clipboard image"),
+      long_description=open('README.md').read(),
+      install_requires=['Pillow'],
+      classifiers=[
+          "Programming Language :: Python",
+          "License :: OSI Approved :: MIT License",
+          "Development Status :: 3 - Alpha"
+          ]
+      )
