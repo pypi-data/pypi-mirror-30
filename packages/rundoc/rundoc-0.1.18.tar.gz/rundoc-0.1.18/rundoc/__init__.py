9379615d74d3840b11960cfb20ff465fe9e129bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:28 +0000

    Add package rundoc/0.1.18/rundoc-0.1.18.tar.gz

---
+++ packages/rundoc/rundoc-0.1.18.tar.gz/rundoc-0.1.18/rundoc/__init__.py
@@ -0,0 +1,23 @@
+"""
+Tool that let's you run code blocks from a markdown file in controlled manner.
+"""
+
+__version__ = "0.1.18"
+__licence__ = "BSD"
+__year__ = "2017"
+__author__ = "EclecticIQ"
+__author_email__ = "info@eclecticiq.com"
+
+
+class RundocException(Exception):
+    """Generic rundoc exception."""
+    pass
+
+class BadEnv(RundocException):
+    """Attempt to load invalid environment name/value"""
+    pass
+
+class CodeFailed(RundocException):
+    """Raise this when code block returns non-zero exit status."""
+    pass
+
