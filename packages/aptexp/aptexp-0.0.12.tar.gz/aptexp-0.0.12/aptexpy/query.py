ef5b359165c3fdece6c0bc7d3447e3a467b9bbdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:17 +0000

    Add package aptexp/0.0.12/aptexp-0.0.12.tar.gz

---
+++ packages/aptexp/aptexp-0.0.12.tar.gz/aptexp-0.0.12/aptexpy/query.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import pymongo
+
+class MongoQuery:
+
+    __client = None
+    __db = None
+    __fields = None
+
+    head = []
+
+    def __init__(self, fields={'stix_report':{"Title" : 1, "_id" : 1, "Date" : 1, "Tag" : 1,"vendors" : 1, "URL": 1}},
+                                params={'host':'127.0.0.1:27017','db':'mongo_engine_db'}):
+        
+        self.__client = pymongo.MongoClient(params['host'])
+        self.__db = self.__client[params['db']]
+        self.__fields = fields
+
+    def test(self):
+        print(self.parse_collection())
+
+
+    def parse_collection(self):
+
+        result = []
+        for k, v in self.__fields.items():
+            db_coll = self.__db[k]
+            self.__parse_field_txt(db_coll, v, result)
+
+        # reverse result
+        # result = list(zip(*result))
+
+        return result
+
+    def __parse_field_txt(self, db_coll, text, res):
+        isempty = True if(res == []) else False
+        cursor = db_coll.find({},text)
+
+        n = 0
+        keys = []
+
+        for x in cursor:
+            record = []
+            for k,v in x.items():
+                record.append(v)
+            if(isempty):
+                res.append(record)
+            else:
+                res[n].extend(record)
+                n = n+1
+
+            if(keys==[]):
+                keys = x.keys() 
+                print(keys)
+        self.head.extend(keys)
+
+if __name__ == '__main__':
+
+    query = MongoQuery(fields={'stix_report':{"_id" : 0, "Title" : 1, "vendors" : 1, "URL": 1}})
+
+    query.test()
+
+
