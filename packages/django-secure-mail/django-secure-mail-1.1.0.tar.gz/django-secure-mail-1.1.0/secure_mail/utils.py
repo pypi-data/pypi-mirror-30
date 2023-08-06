421cad86c7feeb448d42fcdf78b4b6daabecd15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:31 +0000

    Add package django-secure-mail/1.1.0/django-secure-mail-1.1.0.tar.gz

---
+++ packages/django-secure-mail/django-secure-mail-1.1.0.tar.gz/django-secure-mail-1.1.0/secure_mail/utils.py
@@ -0,0 +1,31 @@
+from gnupg import GPG
+
+from secure_mail.settings import (ALWAYS_TRUST, GNUPG_ENCODING, GNUPG_HOME,
+                                  SIGNING_KEY_FINGERPRINT)
+
+
+# Used internally
+encrypt_kwargs = {
+    'always_trust': ALWAYS_TRUST,
+    'sign': SIGNING_KEY_FINGERPRINT,
+}
+
+
+def get_gpg():
+    gpg = GPG(gnupghome=GNUPG_HOME)
+    if GNUPG_ENCODING is not None:
+        gpg.encoding = GNUPG_ENCODING
+    return gpg
+
+
+class EncryptionFailedError(Exception):
+    pass
+
+
+def addresses_for_key(gpg, key):
+    """
+    Takes a key and extracts the email addresses for it.
+    """
+    return [address.split("<")[-1].strip(">")
+            for address in gpg.list_keys().key_map[key['fingerprint']]["uids"]
+            if address]
---
+++ packages/django-secure-mail/django-secure-mail-1.1.0.tar.gz/django-secure-mail-1.1.0/setup.py
@@ -0,0 +1,53 @@
+import sys
+from shutil import rmtree
+from setuptools import setup, find_packages
+
+
+if sys.argv[:2] == ["setup.py", "bdist_wheel"]:
+    # Remove previous build dir when creating a wheel build,
+    # since if files have been removed from the project,
+    # they'll still be cached in the build dir and end up
+    # as part of the build, which is unexpected.
+    try:
+        rmtree("build")
+    except:
+        pass
+
+
+setup(
+    name="django-secure-mail",
+    version=__import__("secure_mail").__version__,
+    author="blag",
+    author_email="blag@users.noreply.github.com",
+    description="A Django reusable app providing the ability to send PGP "
+                "encrypted and multipart emails using the Django templating "
+                "system.",
+    long_description=open("README.rst").read(),
+    url="https://github.com/blag/django-secure-mail",
+    packages=find_packages(exclude=("tests",)),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        "python-gnupg"
+    ],
+    extras_require={
+        'dev': [
+            "sphinx-me",
+        ]
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Framework :: Django",
+        "Topic :: Communications :: Email",
+        "Topic :: Security :: Cryptography",
+    ]
+)
