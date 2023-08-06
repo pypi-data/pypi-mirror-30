40eb5092dd271812f946a9b875e4810e6b71cad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:39 +0000

    Add package vip-hci/0.9.5/vip_hci-0.9.5.tar.gz

---
+++ packages/vip-hci/vip_hci-0.9.5.tar.gz/vip_hci-0.9.5/vip_hci/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import (absolute_import)
+
+from . import preproc
+from . import conf
+from . import fits
+from . import frdiff
+from . import leastsq
+from . import llsg
+from . import madi
+from . import negfc
+from . import nmf
+from . import pca
+from . import phot
+from . import stats
+from . import var
+from .hci_cube import *
+from .vip_ds9 import *
+
+__version__ = "0.9.5"
+
+
+
+
