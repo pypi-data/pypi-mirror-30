d73d5e03b55ab87a899eeedeb30d9060fd55908e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:39 +0000

    Add package django_matialvarezs_grafana_customers/0.1.1/django-matialvarezs-grafana-customers-0.1.1.tar.gz

---
+++ packages/django_matialvarezs_grafana_customers/django-matialvarezs-grafana-customers-0.1.1.tar.gz/django-matialvarezs-grafana-customers-0.1.1/matialvarezs-grafana-customers/api.py
@@ -0,0 +1,24 @@
+from . import settings
+from matialvarezs_request_handler import utils as matialvarezs_request_handler_utils
+from . import models
+
+
+def create_organisation(customer_id, name):
+    data = {
+        "name": name
+    }
+    res = matialvarezs_request_handler_utils.send_post_and_get_response(settings.GRAFANA_API_BASE_URL + 'orgs',
+                                                                        data=data, headers=settings.GRAFANA_API_HEADERS,
+                                                                        convert_data_to_json=True)
+    if res:
+        organisation_id = res['orgId']
+        customer_org_grafana = models.CustomerOrganisationGrafana(customer=customer_id, organisation_id=organisation_id)
+        customer_org_grafana.save()
+
+
+def create_user():
+    pass
+
+
+def create_dashboard():
+    pass
---
+++ packages/django_matialvarezs_grafana_customers/django-matialvarezs-grafana-customers-0.1.1.tar.gz/django-matialvarezs-grafana-customers-0.1.1/matialvarezs-grafana-customers/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GrafanaConfig(AppConfig):
+    name = 'matialvarezs-grafana-customers'
