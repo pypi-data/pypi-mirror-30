003675aa9d134b05311a6175e27e0a0e5fb3a35a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:40 +0000

    Add package mdt/0.11.3/mdt-0.11.3-py2.py3-none-any.whl

---
+++ packages/mdt/mdt-0.11.3-py2.py3-none-any.whl/mdt/data/components/standard/cascade_models/relaxometry.py
@@ -0,0 +1,23 @@
+from mdt.component_templates.cascade_models import CascadeTemplate
+
+__author__ = 'Robbert Harms'
+__date__ = '2017-08-26'
+__maintainer__ = 'Robbert Harms'
+__email__ = 'robbert.harms@maastrichtuniversity.nl'
+__licence__ = 'LGPL v3'
+
+
+class S0_T1_GRE(CascadeTemplate):
+
+    description = 'Cascade for S0_T1_GRE model.'
+    models = ('S0',
+              'S0_T1_GRE',
+              'S0_T1_GRE')
+    fixes = {1: [('ExpT1DecIR.Efficiency', 1)]}
+
+
+class S0_T2(CascadeTemplate):
+
+    description = 'Cascade for S0-T2 model.'
+    models = ('S0',
+              'S0-T2')
