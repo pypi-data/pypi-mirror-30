5e60de0bb9629a5faa62782c57f4cb30107e93be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:39 +0000

    Add package yabealice-test/1.0/yabealice_test-1.0.zip

---
+++ packages/yabealice-test/yabealice_test-1.0.zip/yabealice_test-1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'yabealice_test',
+    packages = ['test1'],
+    version = '1.0',
+    description = 'My test project',
+    author = 'yabealice',
+    author_email = 'aliceliu0106@gmail.com',
+    keywords = ['test'],
+    classifiers = [],
+)
---
+++ packages/yabealice-test/yabealice_test-1.0.zip/yabealice_test-1.0/test1/test1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  3 09:28:32 2018
+
+@author: tsjhlhj
+"""
+
+import tensorflow as tf
+
+print('hi!')
