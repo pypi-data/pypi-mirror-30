6e0d21dbc3aff59184fe0d32a14d4fde7b10827b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:08 +0000

    Add package webtest-docgen/0.6.2/webtest-docgen-0.6.2.tar.gz

---
+++ packages/webtest-docgen/webtest-docgen-0.6.2.tar.gz/webtest-docgen-0.6.2/webtest_docgen/__init__.py
@@ -0,0 +1,5 @@
+from .models import *
+from .providers import *
+from .app import TestDocumentApp
+
+__version__ = '0.6.2'
---
+++ packages/webtest-docgen/webtest-docgen-0.6.2.tar.gz/webtest-docgen-0.6.2/webtest_docgen/providers/html.py
@@ -0,0 +1,10 @@
+# def _generate_index(self):
+#     f = self._ensure_file(self.get_index_filename())
+#     f.write('# %s `%s`\n' % (self._docs_root.title or 'untitled', self._docs_root.version))
+#     if len(self._docs_root.documents) > 0:
+#         f.write('## Documents\n')
+#         for document in self._docs_root.documents:
+#             f.write('- ')
+#
+#     for resource_key, resource in self._docs_root.resources.items():
+#         f.write('## Resources\n' %)
