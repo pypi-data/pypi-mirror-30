571c8ca1eccc1899e7ade782c30c0947c199eff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:16 +0000

    Add package poetry/0.7.0b3/poetry-0.7.0b3-py3-none-any.whl

---
+++ packages/poetry/poetry-0.7.0b3-py3-none-any.whl/poetry/console/commands/check.py
@@ -0,0 +1,15 @@
+from .command import Command
+
+
+class CheckCommand(Command):
+    """
+    Checks the validity of the <comment>pyproject.toml</comment> file.
+
+    check
+    """
+
+    def handle(self):
+        # Load poetry and display errors, if any
+        _ = self.poetry
+
+        self.info('All set!')
