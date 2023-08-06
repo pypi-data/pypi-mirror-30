b950d9690bcace775bc90c319b344c67af259da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:14 +0000

    Add package scrapyrefs/0.3.0/scrapyrefs-0.3.0.tar.gz

---
+++ packages/scrapyrefs/scrapyrefs-0.3.0.tar.gz/scrapyrefs-0.3.0/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+from scrapyrefs import VERSION
+
+test_requires = []
+
+install_requires = [
+    'lxml',
+    'crossrefapi'
+]
+
+setup(
+    name="scrapyrefs",
+    version=VERSION,
+    description="Library that implements the endpoints of the Crossref API",
+    author="Erudit",
+    author_email="fabio.batalha@erudit.org",
+    maintainer="Fabio Batalha",
+    maintainer_email="fabio.batalha@erudit.org",
+    url="http://github.com/erudit/scrapyrefs",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+    dependency_links=[],
+    tests_require=test_requires,
+    test_suite='tests',
+    install_requires=install_requires,
+    entry_points="""
+    [console_scripts]
+    scrapyrefs=scrapyrefs.scrapyrefs:main
+    """
+)
