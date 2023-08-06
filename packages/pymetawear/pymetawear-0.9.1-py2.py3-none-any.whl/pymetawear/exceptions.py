84fa2569a73c9c40d72693de86f9ac8811e572b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:17 +0000

    Add package pymetawear/0.9.1/pymetawear-0.9.1-py2.py3-none-any.whl

---
+++ packages/pymetawear/pymetawear-0.9.1-py2.py3-none-any.whl/pymetawear/exceptions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+.. moduleauthor:: hbldh <henrik.blidh@nedomkull.com>
+Created on 2016-03-30, 10:11
+
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+
+class PyMetaWearException(Exception):
+    """Main exception."""
+    pass
+
+
+class PyMetaWearConnectionTimeout(PyMetaWearException):
+    """Connection could not be established in time."""
+    pass
