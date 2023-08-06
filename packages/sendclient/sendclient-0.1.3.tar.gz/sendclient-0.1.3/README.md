036484099b1b945c4603a7918948293407ab2898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:27 +0000

    Add package sendclient/0.1.3/sendclient-0.1.3.tar.gz

---
+++ packages/sendclient/sendclient-0.1.3.tar.gz/sendclient-0.1.3/README.md
@@ -0,0 +1,63 @@
+# send-cli [![Build Status](https://travis-ci.org/ehuggett/send-cli.svg?branch=master)](https://travis-ci.org/ehuggett/send-cli)
+Unofficial command line (Firefox) Send client
+
+![tty_crop](https://user-images.githubusercontent.com/8090731/30059626-c1daf4d4-9237-11e7-97a1-0f53456a293c.gif)
+
+Warning, Sharp Edges!
+- will overwrite the output file without warning, but does prompt for the output filename
+- Uses temp files to avoid using large amount of memory, you will need at least 2GB of free disk space to upload a 2GB file
+- Very limited error checking
+- expect serious issues such as data loss if precautions are not taken (you are using a service where destroying your data is a feature, use common sense)
+
+Issues for bugs/features/mistakes/comments on coding style are all very welcome, please also feel free to open pull requests for nits/petty changes.
+
+## Installation
+You can install the package "[sendclient](https://pypi.python.org/pypi/sendclient)" from the python package index
+```shell
+pip3 install sendclient
+```
+or from a clone of this repository
+```shell
+pip3 install /path/to/cloned/send-cli/
+```
+_tip: use `pip install -e` to install in 'editable' mode if your intending to modify the client_
+
+## Basic usage
+```shell
+$ send-cli -h
+!!! Experimental/alpha quality suitable for testing at your own risk only !!!
+usage: send-cli [-h]
+                [--service-local | --service-dev | --service-stage | --service-live | --service SERVICE]
+                [--file FILE | --stdin FILENAME | --delete URL TOKEN | --download-limit URL TOKEN LIMIT | --url URL]
+                [--ignore-version]
+                [input]
+
+Unofficial (Firefox) Send client
+
+positional arguments:
+  input                 Uploads or downloads a file specified by path or url,
+                        no other options required
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --ignore-version      Disable server version checks (MAY CAUSE LOSS OF DATA)
+
+Set the Send service to used:
+  --service-local       Use a Send service at http://localhost:8080
+  --service-dev         Use the Send development server for upload
+  --service-stage       Use the Send staging server for upload
+  --service-live        Use the Send production server for upload (Default)
+  --service SERVICE     Specify the url of a Send service to use for upload,
+                        can also be set with the environment variable
+                        SEND_SERVICE
+
+Action:
+  --file FILE           Upload the specified file to Firefox Send
+  --stdin FILENAME      Upload data read from standard input with this
+                        filename
+  --delete URL TOKEN    Delete a file hosted on a Send server
+  --download-limit URL TOKEN LIMIT
+                        Change the download limit for a file hosted on a Send
+                        server, LIMIT must be <=20
+  --url URL             Download a file with a Send link
+```
---
+++ packages/sendclient/sendclient-0.1.3.tar.gz/sendclient-0.1.3/sendclient/common.py
@@ -0,0 +1,105 @@
+# Constants
+CHUNK_SIZE = 8192
+SPOOL_SIZE = 16 * 1024 * 1024
+
+from tqdm import tqdm
+import requests
+import base64
+
+def splitkeyurl(url):
+    '''
+       Splits a Send url into key, urlid and 'prefix' for the Send server
+       Should handle any hostname, but will brake on key & id length changes
+    '''
+    key = url[-22:]
+    urlid = url[-34:-24]
+    service = url[:-43]
+    return service, urlid, key
+
+def checkServerVersion(service, ignoreVersion=False):
+    if ignoreVersion == True:
+        return True
+
+    r = requests.get(service + '__version__')
+    r.raise_for_status()
+    b = r.json()
+    if b['version'] == 'v2.5.3' and b['commit'] == 'cda38f9':
+        return True
+    else:
+        return False
+
+def fileSize(f):
+    '''returns the size of a file like object in bytes/octets'''
+    offset = f.tell()
+
+    f.seek(0, 2)
+    size = f.tell()
+
+    f.seek(offset)  # return to the previous position in the file
+    return size
+
+def progbar(total):
+    return tqdm(unit='B', unit_scale=True, total=total)
+
+def unpadded_urlsafe_b64encode(b):
+    ''' base64 encode bytes with url-safe alphabet and strip padding '''
+    return base64.urlsafe_b64encode(b).decode().replace('=','')
+
+def unpadded_urlsafe_b64decode(s):
+    ''' Decode url-safe base64 string, with or without padding, to bytes'''
+    return base64.urlsafe_b64decode(s + '=' * (4 - len(s) % 4))
+
+import Cryptodome.Hash
+import Cryptodome.Protocol.KDF
+import Cryptodome.Random
+
+class secretKeys:
+    def __init__(self, secretKey=None, password=None, url=None):
+        self.secretKey = secretKey if secretKey is not None else self.randomSecretKey()
+        self.encryptKey = self.deriveEncryptKey()
+        self.encryptIV = self.randomEncryptIV()
+        self.authKey = self.deriveAuthKey()
+        self.metaKey = self.deriveMetaKey()
+        self.metaIV = bytes(12) # Send uses a 12 byte all-zero IV when encrypting metadata
+        if password != None and url != None:
+            self.deriveNewAuthKey(password, url)
+
+    def randomSecretKey(self):
+        return Cryptodome.Random.get_random_bytes(16)
+
+    def randomEncryptIV(self):
+        return Cryptodome.Random.get_random_bytes(12)
+
+    def deriveEncryptKey(self):
+        master = self.secretKey
+        key_len = 16
+        salt = b''
+        hashmod = Cryptodome.Hash.SHA256
+        num_keys = 1
+        context = b'encryption'
+        return Cryptodome.Protocol.KDF.HKDF(master, key_len, salt, hashmod, num_keys=num_keys, context=context)
+
+    def deriveAuthKey(self):
+        master = self.secretKey
+        key_len = 64
+        salt = b''
+        hashmod = Cryptodome.Hash.SHA256
+        num_keys = 1
+        context = b'authentication'
+        return Cryptodome.Protocol.KDF.HKDF(master, key_len, salt, hashmod, num_keys=num_keys, context=context)
+
+    def deriveMetaKey(self):
+        master = self.secretKey
+        key_len = 16
+        salt = b''
+        hashmod = Cryptodome.Hash.SHA256
+        num_keys = 1
+        context = b'metadata'
+        return Cryptodome.Protocol.KDF.HKDF(master, key_len, salt, hashmod, num_keys=num_keys, context=context)
+
+    def deriveNewAuthKey(self, password, url):
+        self.password = password
+        self.url = url
+        hmac_sha256 = lambda key, msg: Cryptodome.Hash.HMAC.new(key, msg, Cryptodome.Hash.SHA256).digest()
+        self.newAuthKey = Cryptodome.Protocol.KDF.PBKDF2(password.encode('utf8'), url.encode('utf8'), dkLen=64, count=100, prf=hmac_sha256)
+        return self.newAuthKey
