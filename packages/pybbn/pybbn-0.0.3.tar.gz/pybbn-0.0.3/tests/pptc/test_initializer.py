7456c54de8edb380a680fdd195ac5445edf457e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:18 +0000

    Add package pybbn/0.0.3/pybbn-0.0.3.tar.gz

---
+++ packages/pybbn/pybbn-0.0.3.tar.gz/pybbn-0.0.3/tests/pptc/test_initializer.py
@@ -0,0 +1,38 @@
+from nose import with_setup
+
+from pybbn.graph.dag import BbnUtil
+from pybbn.pptc.initializer import Initializer
+from pybbn.pptc.moralizer import Moralizer
+from pybbn.pptc.potentialinitializer import PotentialInitializer
+from pybbn.pptc.transformer import Transformer
+from pybbn.pptc.triangulator import Triangulator
+
+
+def setup():
+    pass
+
+
+def teardown():
+    pass
+
+
+@with_setup(setup, teardown)
+def test_initializer():
+    bbn = BbnUtil.get_huang_graph()
+    PotentialInitializer.init(bbn)
+
+    ug = Moralizer.moralize(bbn)
+    cliques = Triangulator.triangulate(ug)
+
+    join_tree = Transformer.transform(cliques)
+
+    Initializer.initialize(join_tree)
+
+    # assert later
+    # for clique in join_tree.get_cliques():
+    #     if isinstance(clique, SepSet):
+    #         continue
+    #     potential = join_tree.potentials[clique.id]
+    #     print(clique)
+    #     print(potential)
+    # assert 1 == 2
