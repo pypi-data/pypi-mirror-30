9d857c1af39714ee989048c9f91f691b1ec5e996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:01 +0000

    Add package lsm-db/0.6.2/lsm-db-0.6.2.tar.gz

---
+++ packages/lsm-db/lsm-db-0.6.2.tar.gz/lsm-db-0.6.2/setup.py
@@ -0,0 +1,33 @@
+import glob
+import os
+
+from distutils.core import setup, Extension
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    import warnings
+    cython_installed = False
+    warnings.warn('Cython not installed, using pre-generated C source file.')
+else:
+    cython_installed = True
+
+
+if cython_installed:
+    python_source = 'lsm.pyx'
+else:
+    python_source = 'lsm.c'
+    cythonize = lambda obj: obj
+
+library_source = glob.glob('src/*.c')
+lsm_extension = Extension(
+    'lsm',
+    sources=[python_source] + library_source)
+
+setup(
+    name='lsm-db',
+    version='0.6.2',
+    description='Python bindings for the SQLite4 LSM database.',
+    author='Charles Leifer',
+    author_email='',
+    ext_modules=cythonize([lsm_extension]),
+)
