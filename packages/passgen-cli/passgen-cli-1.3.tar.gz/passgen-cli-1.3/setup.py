20a391310e59224fe34523e11221d7c2c2adddd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:35 +0000

    Add package passgen-cli/1.3/passgen-cli-1.3.tar.gz

---
+++ packages/passgen-cli/passgen-cli-1.3.tar.gz/passgen-cli-1.3/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+ 
+ 
+"""setup.py: setuptools control."""
+ 
+from setuptools import setup, find_packages
+ 
+version = "1.3"
+descr = "This is simple CLI random password generator. It combines punctuations, letters (upper and lower cases) and digits.\n\n"
+descr += "Installation:\n `pip install passgen-cli`\n\n"
+descr += "Options:\n `-l INTEGER  length of the password.`\n\n"
+descr += "Without the `-l` option, it returns 12 characters by default.\n\nExample: `passgen-cli -l 13`\n\n"
+
+def readme():
+    with open("README.rst") as f:
+        return f.read() 
+
+ 
+setup(
+    name = "passgen-cli",
+    packages = find_packages(),
+    version = version,
+    description = "Python command line password generator.",
+    long_description = descr,
+    author = "Colby Alladaye",
+    keywords= "password generator",
+    author_email = "colbyter@gmail.com",
+    url = "https://github.com/Colbyter/passgen-cli",
+    install_requires=['click'],
+    license='MIT',
+     entry_points = {
+        'console_scripts': [
+        'passgen-cli = passgencli.passgencli:main',
+        ],
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Natural Language :: English',
+],
+    )
