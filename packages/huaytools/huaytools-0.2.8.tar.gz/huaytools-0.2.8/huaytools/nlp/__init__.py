67fc3933ca1b46ca4c1513649dfab4cbc57eb050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:30 +0000

    Add package huaytools/0.2.8/huaytools-0.2.8.tar.gz

---
+++ packages/huaytools/huaytools-0.2.8.tar.gz/huaytools-0.2.8/huaytools/nlp/__init__.py
@@ -0,0 +1,1 @@
+from .corpus import remove_stopwords
---
+++ packages/huaytools/huaytools-0.2.8.tar.gz/huaytools-0.2.8/huaytools/test.py
@@ -0,0 +1,11 @@
+import doctest
+
+
+def test_bunch():
+    from huaytools.utils import bunch
+    doctest.testmod(bunch)
+
+
+if __name__ == '__main__':
+    """"""
+    test_bunch()
