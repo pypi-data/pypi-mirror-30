09b398ab85255223e06a9c6b378ffc4ba73dbd78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:25 +0000

    Add package pysoundio/1.0.0/pysoundio-1.0.0.tar.gz

---
+++ packages/pysoundio/pysoundio-1.0.0.tar.gz/pysoundio-1.0.0/setup.py
@@ -0,0 +1,40 @@
+"""
+PySoundIo
+
+A robust, cross-platform solution for real-time audio.
+"""
+import re
+from setuptools import setup, Extension
+
+vstr = open('pysoundio/__init__.py', 'r').read()
+regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
+version = re.search(regex, vstr, re.M)
+
+soundio = Extension('_soundiox',
+                    sources=['pysoundio/_soundiox.c'],
+                    include_dirs=['./pysoundio', '/usr/local/include'],
+                    library_dirs=['/usr/local/lib'],
+                    libraries=['soundio'])
+
+setup(
+    name='pysoundio',
+    version=version.group(1),
+    description='Python wrapper for libsoundio',
+    long_description='A robust, cross-platform solution for real-time audio',
+    license='MIT',
+    author='Joe Todd',
+    author_email='joextodd@gmail.com',
+    url='http://pysoundio.readthedocs.io/en/latest/',
+    download_url='https://github.com/joextodd/pysoundio/archive/' + version.group(1) + '.tar.gz',
+    include_package_data=True,
+    packages=['pysoundio'],
+    ext_modules=[soundio],
+    test_suite='tests',
+    keywords=('audio', 'sound', 'stream'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License'
+    ],
+)
