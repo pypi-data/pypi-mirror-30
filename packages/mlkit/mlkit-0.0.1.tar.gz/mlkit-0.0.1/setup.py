f3efddc4e6ce003406ba2c1f553424368de552eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:44 +0000

    Add package mlkit/0.0.1/mlkit-0.0.1.tar.gz

---
+++ packages/mlkit/mlkit-0.0.1.tar.gz/mlkit-0.0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='mlkit',
+    version='0.0.1',
+    packages=['mlkit'],
+    url='',
+    license='MIT',
+    description='Machine Learning Toolkit',
+    install_requires=[
+    ],
+    extras_require={
+    },
+)
