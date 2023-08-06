143e921247291806b5c1332aca020a9d9a4a16c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:41 +0000

    Add package CommonUtilities/0.1/CommonUtilities-0.1.tar.gz

---
+++ packages/CommonUtilities/CommonUtilities-0.1.tar.gz/CommonUtilities-0.1/CommonUtilities/MathUtils.py
@@ -0,0 +1,8 @@
+class MathUtils:
+    def __init__(self):
+        ''' Constructor for this class. '''
+        pass
+
+
+    def printData(self):
+        print('test...')
---
+++ packages/CommonUtilities/CommonUtilities-0.1.tar.gz/CommonUtilities-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='CommonUtilities',
+      version='0.1',
+      description='Test...',
+      url='http://github.com/shankar-ray/CommonUtilities',
+      author='Sangram Kesari Ray',
+      author_email='shankar.ray030@gmail.com',
+      license='MIT',
+      packages=['CommonUtilities'],
+      zip_safe=False)
