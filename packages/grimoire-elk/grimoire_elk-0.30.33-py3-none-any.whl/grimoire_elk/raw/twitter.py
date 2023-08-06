5324a88ea8520d23d3bbb4ed5199f0d3d6827ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:51 +0000

    Add package grimoire-elk/0.30.33/grimoire_elk-0.30.33-py3-none-any.whl

---
+++ packages/grimoire-elk/grimoire_elk-0.30.33-py3-none-any.whl/grimoire_elk/raw/twitter.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#   Alvaro del Castillo San Felix <acs@bitergia.com>
+#
+
+from .elastic import ElasticOcean
+from ..elastic_mapping import Mapping as BaseMapping
+
+
+class Mapping(BaseMapping):
+
+    @staticmethod
+    def get_elastic_mappings(es_major):
+        """Get Elasticsearch mapping.
+
+        :param es_major: major version of Elasticsearch, as string
+        :returns:        dictionary with a key, 'items', with the mapping
+        """
+
+        mapping = '''
+         {
+            "dynamic":true,
+            "properties": {
+                "user": {
+                    "dynamic":false,
+                    "properties": {
+                        "entities": {
+                            "properties": {}
+                        }
+                    }
+                },
+                "hashtags": {
+                    "dynamic":false,
+                    "properties": {
+                        "indices": {
+                            "properties": {}
+                        }
+                    }
+                },
+                "metadata": {
+                    "dynamic":false,
+                    "properties": {}
+                },
+                "entities": {
+                    "properties": {
+                        "symbols": {
+                            "properties": {}
+                        },
+                        "urls": {
+                            "properties": {}
+                        },
+                        "user_mentions": {
+                            "properties": {}
+                        }
+                    }
+                }
+            }
+        }
+        '''
+
+        return {"items": mapping}
+
+
+class TwitterOcean(ElasticOcean):
+    """Twitter Ocean feeder"""
+
+    mapping = Mapping
+
+    # To easy checking for this class
+    is_twitter_ocean = True
+
+    def get_field_date(self):
+        """ Field with the update in the JSON items. Now the same in all. """
+        return "@timestamp"
+
+    def _fix_item(self, item):
+        item["uuid"] = item["data"]["id"]
