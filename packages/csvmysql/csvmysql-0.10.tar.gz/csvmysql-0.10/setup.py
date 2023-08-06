5450c0db0be6bcdcd14396e07f440e689252d917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:03 +0000

    Add package csvmysql/0.10/csvmysql-0.10.tar.gz

---
+++ packages/csvmysql/csvmysql-0.10.tar.gz/csvmysql-0.10/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='csvmysql',
+      version='0.10',
+      description='transmit data from csv file to mysql database, date type is not handled in this version',
+      url='https://github.com/ghao2/csvmysql',
+      author='Guoxuan Hao',
+      author_email='haoguoxuan@gmail.com',
+      license='MIT',
+      packages=['csvmysql'],
+      install_requires=[
+          'pymysql',
+          'pandas',
+          'numpy',
+      ],
+      zip_safe=False)
