5057b7b1c9bc52f09f21d0fe751865b3d782c5f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:20 +0000

    Add package cronner/0.6.2/cronner-0.6.2.tar.gz

---
+++ packages/cronner/cronner-0.6.2.tar.gz/cronner-0.6.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='cronner',
+    version='0.6.2',
+    description='Easily schedule python functions to be run under the cron daemon.',
+    py_modules=['cronner'],
+    test_suite='test_cronner',
+    url='https://github.com/liambuchanan/cronner',
+    license='MIT',
+    author='Liam Buchanan',
+    author_email='liam.buchanan@gmail.com',
+    keywords='cron crontab',
+)
