f94502f8a1483b2a58f9293120effe8a293aeb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:19 +0000

    Add package pwclip/1.2.5/pwclip-1.2.5.tar.gz

---
+++ packages/pwclip/pwclip-1.2.5.tar.gz/pwclip-1.2.5/pwclip/lib/system/xlib/__init__.py
@@ -0,0 +1,6 @@
+"""x window related functions"""
+from system.xlib.xinput import xinput
+from system.xlib.xmsgok import xmsgok
+from system.xlib.xyesno import xyesno
+from system.xlib.xnotify import xnotify
+from system.xlib.xgetpass import xgetpass
