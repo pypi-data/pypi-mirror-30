d15da675e2e45216a1d6dfdbba5d0fb9f0e5e8d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:29 +0000

    Add package django-foreignform/0.0.4/django-foreignform-0.0.4.tar.gz

---
+++ packages/django-foreignform/django-foreignform-0.0.4.tar.gz/django-foreignform-0.0.4/foreignform/mixins.py
@@ -0,0 +1,39 @@
+import json
+
+from django.core.exceptions import FieldDoesNotExist
+
+
+class ForeignFormAdminMixin(object):
+    foreignform_field = None
+    foreignform_foreign_key = None
+    change_form_template = 'foreignform/change.html'
+    add_form_template = 'foreignform/add.html'
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        instance = self.model.objects.get(id=object_id)
+        fk = getattr(instance, self.foreignform_foreign_key, None)
+
+        if not fk:
+            raise FieldDoesNotExist(
+                'Field {} does not exist on model. Check the '
+                'foreignform_foreign_key attribute on your foreignform '
+                'ModelAdmin class.'.format(
+                    self.foreignform_foreign_key
+                )
+            )
+
+        extra_context = extra_context or {}
+        extra_context['foreignform_field'] = self.foreignform_field
+        extra_context['foreignform_JSONSchema'] = json.dumps(fk.jsonSchema)
+        extra_context['foreignform_UISchema'] = json.dumps(fk.uiSchema)
+        return super().change_view(
+            request, object_id, form_url, extra_context=extra_context,
+        )
+
+    def add_view(self, request, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['foreignform_field'] = self.foreignform_field
+        extra_context['foreignform_foreign_key'] = self.foreignform_foreign_key
+        return super().add_view(
+            request, form_url, extra_context=extra_context,
+        )
