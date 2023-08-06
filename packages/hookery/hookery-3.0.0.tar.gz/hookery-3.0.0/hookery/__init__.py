88694a2e00ee6038151822171617de0e171de4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:57 +0000

    Add package hookery/3.0.0/hookery-3.0.0.tar.gz

---
+++ packages/hookery/hookery-3.0.0.tar.gz/hookery-3.0.0/hookery/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '3.0.0'
+
+from .base import ClassHook, GlobalHook, Handler, Hook, InstanceHook, hookable
+
+__all__ = [
+    'ClassHook',
+    'GlobalHook',
+    'Handler',
+    'Hook',
+    'InstanceHook',
+    'hookable',
+]
