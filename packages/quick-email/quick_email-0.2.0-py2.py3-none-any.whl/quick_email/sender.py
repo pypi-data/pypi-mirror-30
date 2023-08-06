61088d243d91cb798578528b06d1253dbf78811d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:42 +0000

    Add package quick-email/0.2.0/quick_email-0.2.0-py2.py3-none-any.whl

---
+++ packages/quick-email/quick_email-0.2.0-py2.py3-none-any.whl/quick_email/sender.py
@@ -0,0 +1,31 @@
+import smtplib
+
+from email.utils import COMMASPACE
+
+def send_msg(msg, host, port, username=None, password=None, is_tls=False):
+    smtp = smtplib.SMTP(host, port)
+    if is_tls:
+        smtp.starttls()
+
+    if username and password:
+        smtp.login(username, password)
+
+    _all_recipients = all_recipients(msg)
+
+    smtp.sendmail(msg[u'From'], _all_recipients, msg.as_string())
+    smtp.quit()
+
+
+def all_recipients(msg):
+    all_recipients = set()
+
+    if u'To' in msg:
+        all_recipients.update(msg[u'To'].split(COMMASPACE))
+
+    if u'CC' in msg:
+        all_recipients.update(msg[u'CC'].split(COMMASPACE))
+
+    if u'BCC' in msg:
+        all_recipients.update(msg[u'BCC'].split(COMMASPACE))
+
+    return all_recipients
