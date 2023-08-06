aad39231a15ae64ee55ccbc0e6c3ca07b68de5ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:24 +0000

    Add package flashfocus/0.1.7/flashfocus-0.1.7.tar.gz

---
+++ packages/flashfocus/flashfocus-0.1.7.tar.gz/flashfocus-0.1.7/setup.py
@@ -0,0 +1,50 @@
+import os
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+"""Location of the README file"""
+README = 'README.txt'
+
+
+"""Github username"""
+USERNAME = 'fennerm'
+
+
+"""Package name"""
+NAME = 'flashfocus'
+
+
+def long_description(readme=README):
+    """Extract the long description from the README."""
+    with open(readme, 'r') as f:
+        long_description = f.read()
+    return long_description
+
+
+def url(name=NAME, username=USERNAME):
+    """Generate the package url from the package name."""
+    return '/'.join(['http://github.com', username, name])
+
+
+setup(
+    name=NAME,
+    version='0.1.7',
+    author='Fenner Macrae',
+    author_email='fmacrae.dev@gmail.com',
+    description=("Simple focus animations for tiling window managers"),
+    long_description=long_description(),
+    license='MIT',
+    url='https://www.github.com/fennerm/flashfocus',
+    py_modules=['flashfocus'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    install_requires=['xcffib', 'tendo', 'click', 'cffi'],
+    packages=find_packages(exclude=["*test*"]),
+    keywords='xorg flash focus i3 bpswm awesomewm',
+    entry_points='''
+        [console_scripts]
+        flashfocus=flashfocus.cli:cli
+    ''',
+)
