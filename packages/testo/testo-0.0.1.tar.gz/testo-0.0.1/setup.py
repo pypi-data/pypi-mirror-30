706f387bba02af1aa9f05be0fdc85c428aacd348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:08 +0000

    Add package testo/0.0.1/testo-0.0.1.tar.gz

---
+++ packages/testo/testo-0.0.1.tar.gz/testo-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='testo',
+	version='0.0.1',
+	description='CI framework',
+	url='https://github.com/log0div0/testo',
+	author='Sergey Lvov',
+	author_email='log0div0@gmail.com',
+	license='MIT',
+	packages=['testo'],
+	zip_safe=False)
