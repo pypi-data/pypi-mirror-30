7cf7a54a67352a5fa982b433e7e0bd47ab2a0393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:52 +0000

    Add package mosaiq/0.1/mosaiq-0.1.tar.gz

---
+++ packages/mosaiq/mosaiq-0.1.tar.gz/mosaiq-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='mosaiq',
+    version='0.1',
+    scripts=['mosaiq.py'],
+    url=['https://github.com/jdonaldson/mosaiq-python'],
+    author=["Justin Donaldson"],
+    author_email=['jdonaldson@gmail.com']
+)
