9a2748e41da0c36ca420bde8e2b08465d63a7fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:33 +0000

    Add package pyganalytics/0.0.8/pyganalytics-0.0.8.tar.gz

---
+++ packages/pyganalytics/pyganalytics-0.0.8.tar.gz/pyganalytics-0.0.8/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+from pip.req import parse_requirements
+
+reqs = parse_requirements("requirements.txt", session='hack')
+reqs = [str(ir.req) for ir in reqs]
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='pyganalytics',
+    version='0.0.8',
+    description='Easily get data from Google Analytics',
+    long_description=readme,
+    author='Dacker',
+    author_email='hello@dacker.co',
+    url='https://github.com/dacker-team/pyganalytics',
+    license=license,
+    keywords='get data google analytics easy',
+    packages=find_packages(exclude=('tests', 'docs')),
+    python_requires='>=3',
+    install_requires=reqs,
+
+)
