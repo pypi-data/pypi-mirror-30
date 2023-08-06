afb434d74907922a2eb9e2f5c96a1b08c453f4bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:58 +0000

    Add package multdict/0.0.2/multdict-0.0.2.tar.gz

---
+++ packages/multdict/multdict-0.0.2.tar.gz/multdict-0.0.2/multdict/__init__.py
@@ -0,0 +1,20 @@
+def multdict(idict, num):
+    """
+    >>> idict = {'a': ['a'], 'b': ['b']}
+    >>> odict = multdict(idict, 2)
+    >>> assert odict == {'a0': ['a'], 'b0': ['b'], 'a1': ['a'], 'b1': ['b']}
+    >>> assert odict['a0'] is odict['a1']
+
+    >>> assert multdict(idict, 1) is idict
+    """
+    assert num >= 0
+    if num == 1:
+        return idict
+
+    odict = {}
+    for i in range(num):
+        for ikey, value in idict.items():
+            okey = ikey + str(i)
+            assert okey not in odict
+            odict[okey] = value
+    return odict
