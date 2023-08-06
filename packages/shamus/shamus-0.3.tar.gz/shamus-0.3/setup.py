25720dbcf36188bf3fcd4ba7c50bb0019848414a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:14 +0000

    Add package shamus/0.3/shamus-0.3.tar.gz

---
+++ packages/shamus/shamus-0.3.tar.gz/shamus-0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shamus',
+    version='0.3',
+    description='Small python decorator for basic method time and memory usage.',
+    long_description=open('README.md').read(),
+    keywords='decorator memory time method',
+    url='https://github.com/marinko-peso/shamus',
+    author='Marinko Peso',
+    author_email='marinko.peso@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=['psutil'],
+    zip_safe=False
+)
