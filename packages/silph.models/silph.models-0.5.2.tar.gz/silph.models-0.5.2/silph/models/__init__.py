87924ce61bba3135e2d5c8a1c629d123c73bafa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:17 +0000

    Add package silph.models/0.5.2/silph.models-0.5.2.tar.gz

---
+++ packages/silph.models/silph.models-0.5.2.tar.gz/silph.models-0.5.2/silph/models/__init__.py
@@ -0,0 +1,26 @@
+
+from .events import (
+    Event,
+    EventCommunities,
+    EventStaff,
+    EventCheckin,
+)
+
+from .communities import (
+    Community,
+    CommunityAdmin,
+    DiscordMeta,
+    TelegramMeta,
+)
+
+from .users import (
+    User,
+    UserLogin,
+    UnlockedAvatar,
+    UserBadge,
+)
+
+from .meetings import (
+    Meeting,
+    TravelersMet,
+)
