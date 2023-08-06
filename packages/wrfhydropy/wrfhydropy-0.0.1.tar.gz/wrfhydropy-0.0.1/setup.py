59a76a62563bc8b252bffeb72ac375ab9a1a9a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:21 +0000

    Add package wrfhydropy/0.0.1/wrfhydropy-0.0.1.tar.gz

---
+++ packages/wrfhydropy/wrfhydropy-0.0.1.tar.gz/wrfhydropy-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wrfhydropy',
+    version='0.0.1',
+    packages=['wrfhydropy'],
+    url='https://github.com/NCAR/wrf_hydro_py',
+    license='MIT',
+    install_requires=['pandas','f90nml','deepdiff','pathlib','xarray','datetime','pytest','pytest-datadir-ng'],
+    author='Joe Mills',
+    author_email='jmills@ucar.edu',
+    description='Crude API for the WRF-Hydro model',
+)
