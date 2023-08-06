543a4482a678ad5208b57f53999761614aba13ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:44 +0000

    Add package gdby/0.14/gdby-0.14.tar.gz

---
+++ packages/gdby/gdby-0.14.tar.gz/gdby-0.14/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages  
+  
+setup(  
+      name='gdby',   #名称  
+      version='0.14',  #版本  
+      description="a tool for generating special db format yaml", #描述  
+      keywords='',  
+      author='sea',  #作者  
+      author_email='sealevelnn@163.com', #作者邮箱  
+      packages=["generate_yaml"],  
+      install_requires=[      #需求的第三方模块  
+        'requests',  
+      ],  
+      entry_points={
+        'console_scripts': [
+            'gdby=generate_yaml.generate_yaml:generate_yaml'
+        ]
+    }
+)  
