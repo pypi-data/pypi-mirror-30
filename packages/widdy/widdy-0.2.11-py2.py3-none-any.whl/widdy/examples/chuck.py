892538adc3a138b5fa9dfc31c2f43cdac7d95118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:30 +0000

    Add package widdy/0.2.11/widdy-0.2.11-py2.py3-none-any.whl

---
+++ packages/widdy/widdy-0.2.11-py2.py3-none-any.whl/widdy/examples/chuck.py
@@ -0,0 +1,38 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from builtins import super
+from future import standard_library
+standard_library.install_aliases()
+import requests, sys
+from html.parser import HTMLParser
+import widdy
+from widdy import styles
+
+class Chuck(widdy.App):
+    def get_joke(app):
+        joke = requests.get('http://api.icndb.com/jokes/random').json()
+        return HTMLParser().unescape(joke['value']['joke']).encode('utf-8')
+
+    def next_joke(app):
+        app.update_text('Getting new quote...')
+        app.draw_screen()
+        joke = app.get_joke()
+        app.update_text(joke)
+
+    def __init__(app):
+        h = widdy.Header("Random Quotes")
+        m = widdy.Menu(('R', styles.GREEN_BOLD, 'new quote'))
+        t, app.update_text = widdy.Text('Press (R) to get your first quote...')
+        b = widdy.LineBox(t)
+        f = widdy.Frame(h,b,m)
+        handlers = widdy.Handlers(('R', app.next_joke))
+        super().__init__(f, handlers=handlers)
+
+def main(argv=None): Chuck().run()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+    sys.exit(0)
+
