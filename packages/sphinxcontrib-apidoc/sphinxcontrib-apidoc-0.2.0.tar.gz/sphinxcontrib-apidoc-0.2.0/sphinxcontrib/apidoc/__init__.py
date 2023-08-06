82a996a09935a6bc12a024a7d91f40110a826cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:25 +0000

    Add package sphinxcontrib-apidoc/0.2.0/sphinxcontrib-apidoc-0.2.0.tar.gz

---
+++ packages/sphinxcontrib-apidoc/sphinxcontrib-apidoc-0.2.0.tar.gz/sphinxcontrib-apidoc-0.2.0/sphinxcontrib/apidoc/__init__.py
@@ -0,0 +1,33 @@
+"""
+    sphinxcontrib.apidoc
+    ~~~~~~~~~~~~~~~~~~~~
+
+    A Sphinx extension for running 'sphinx-apidoc' on each build.
+
+    :copyright: Copyright 2018 by Stephen Finucane <stephen@that.guru>
+    :license: BSD, see LICENSE for details.
+"""
+
+import pbr.version
+
+from sphinxcontrib.apidoc import ext
+
+__version__ = pbr.version.VersionInfo('apidoc').version_string()
+
+if False:
+    # For type annotation
+    from typing import Any, Dict  # noqa
+    from sphinx.application import Sphinx  # noqa
+
+
+def setup(app):
+    # type: (Sphinx) -> Dict[unicode, Any]
+    app.setup_extension('sphinx.ext.autodoc')  # We need autodoc to function
+
+    app.connect('builder-inited', ext.builder_inited)
+    app.add_config_value('apidoc_module_dir', None, 'env', [str])
+    app.add_config_value('apidoc_output_dir', 'api', 'env', [str])
+    app.add_config_value('apidoc_excluded_paths', [], 'env', [[str]])
+    app.add_config_value('apidoc_separate_modules', False, 'env', [bool])
+
+    return {'version': __version__, 'parallel_read_safe': True}
