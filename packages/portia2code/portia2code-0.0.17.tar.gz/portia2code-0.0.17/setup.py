e11b93ed37e7b93b2c3272068eb5f75729ce6cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:39 +0000

    Add package portia2code/0.0.17/portia2code-0.0.17.tar.gz

---
+++ packages/portia2code/portia2code-0.0.17.tar.gz/portia2code-0.0.17/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from portia2code import __version__ as version
+
+install_requires = [
+    'Scrapy',
+    'slybot>=0.13.0', 
+    'dateparser',
+    'six==1.10.0',
+    'w3lib',
+    'scrapely',
+    'autoflake==0.6.6',
+    'autopep8==1.2.2'
+]
+
+setup(
+    name='portia2code',
+    version=version,
+    license='BSD',
+    description='Convert portia spider definitions to python scrapy spiders',
+    author='Scrapinghub',
+    author_email='info@scrapinghub.com',
+    maintainer='Ruairi Fahy',
+    maintainer_email='ruairi@scrapinghub.com',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    platforms=['Any'],
+    scripts=['bin/portia_porter'],
+    install_requires=install_requires,
+    url='https://github.com/scrapinghub/portia2code',
+    download_url = 'https://github.com/scrapinghub/portia2code/tarball/portia2code-{}'.format(version),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
