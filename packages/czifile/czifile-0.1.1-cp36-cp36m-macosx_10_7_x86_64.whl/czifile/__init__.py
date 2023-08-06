3d14fee8d4721c61a255aca5fd0d92d5e4622370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:29 +0000

    Add package czifile/0.1.1/czifile-0.1.1-cp36-cp36m-macosx_10_7_x86_64.whl

---
+++ packages/czifile/czifile-0.1.1-cp36-cp36m-macosx_10_7_x86_64.whl/czifile/__init__.py
@@ -0,0 +1,8 @@
+from .czifile import imread, CziFile, Segment, xml_reader, match_filename
+
+__version__ = '0.1.1'
+__all__ = (
+    'imread', 'CziFile', 'Segment', 'xml_reader', 'match_filename'
+)
+
+
