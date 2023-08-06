80c49c923bfbcbb010a3083099915bf8392cee64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:13 +0000

    Add package azure-cli-feedback/2.1.1/azure_cli_feedback-2.1.1-py2.py3-none-any.whl

---
+++ packages/azure-cli-feedback/azure_cli_feedback-2.1.1-py2.py3-none-any.whl/azure/cli/command_modules/feedback/__init__.py
@@ -0,0 +1,26 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from azure.cli.core import AzCommandsLoader
+from azure.cli.core.commands import CliCommandType
+
+import azure.cli.command_modules.feedback._help  # pylint: disable=unused-import
+
+
+class FeedbackCommandsLoader(AzCommandsLoader):
+
+    def __init__(self, cli_ctx=None):
+        super(FeedbackCommandsLoader, self).__init__(cli_ctx=cli_ctx)
+
+    def load_command_table(self, args):
+        custom_feedback = CliCommandType(operations_tmpl='azure.cli.command_modules.feedback.custom#{}')
+
+        with self.command_group('', custom_feedback) as g:
+            g.command('feedback', 'handle_feedback')
+
+        return self.command_table
+
+
+COMMAND_LOADER_CLS = FeedbackCommandsLoader
