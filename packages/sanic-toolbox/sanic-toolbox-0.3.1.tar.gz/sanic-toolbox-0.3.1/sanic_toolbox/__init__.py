9247075f39a513255f2c3d90173a202f135c1338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:48 +0000

    Add package sanic-toolbox/0.3.1/sanic-toolbox-0.3.1.tar.gz

---
+++ packages/sanic-toolbox/sanic-toolbox-0.3.1.tar.gz/sanic-toolbox-0.3.1/sanic_toolbox/__init__.py
@@ -0,0 +1,22 @@
+import logging
+
+from .blueprint import _Blueprint as Blueprint
+from .blueprint import _Sanic as Sanic
+from .blueprint import bp_middleware
+from .lazyapp import lazyapp
+from .utils import load_module, search_modules
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__version__ = '0.3.1'
+__description__ = 'A general utility toolbox for Sanic to make middleware workflows simple and presumable, without monkey patching, for plugins and applications'
+
+
+__all__ = [
+    'lazyapp',
+    'Blueprint',
+    'Sanic',
+    'bp_middleware',
+    'search_modules',
+    'load_module',
+]
