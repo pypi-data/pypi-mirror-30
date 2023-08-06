d99e1d4e13ace030ec7011f4a4f92bd5724c589b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:14 +0000

    Add package django-json-secrets/0.1/django-json-secrets-0.1.tar.gz

---
+++ packages/django-json-secrets/django-json-secrets-0.1.tar.gz/django-json-secrets-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='django-json-secrets',
+    version='0.1',
+    description='JSON secrets to Django settings module',
+    author='lhy',
+    author_email='dev@lhy.kr',
+    license='MIT',
+    packages=[
+        'djs',
+    ],
+    zip_safe=True,
+)
