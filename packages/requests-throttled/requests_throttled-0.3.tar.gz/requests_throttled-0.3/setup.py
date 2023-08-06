da443ee3ee6bec797454b40ba513767509808305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:24 +0000

    Add package requests-throttled/0.3/requests_throttled-0.3.tar.gz

---
+++ packages/requests-throttled/requests_throttled-0.3.tar.gz/requests_throttled-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='requests_throttled',
+      version='0.3',
+      description='Throttling requests.Session.',
+      url='http://github.com/zaquest/requests_throttled',
+      author='zaquest',
+      author_email='web.elektro.net@gmail.com',
+      license='MIT',
+      install_requires=['requests'],
+      py_modules=['requests_throttled'],
+      zip_safe=True)
