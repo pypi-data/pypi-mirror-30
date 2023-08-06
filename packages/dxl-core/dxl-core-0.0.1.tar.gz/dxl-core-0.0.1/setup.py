07d3c15dd7362e687c11f4e879f6dbd4072d4f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:54 +0000

    Add package dxl-core/0.0.1/dxl-core-0.0.1.tar.gz

---
+++ packages/dxl-core/dxl-core-0.0.1.tar.gz/dxl-core-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(name='dxl-core',
+      version='0.0.1',
+      description='Core utility library.',
+      url='https://github.com/Hong-Xiang/dxcore',
+      author='Hong Xiang',
+      author_email='hx.hongxiang@gmail.com',
+      license='MIT',
+      packages=['dxl.core'],
+      package_dir={'': 'src/python'},
+      install_requires=[],
+      zip_safe=False)
