fa39ae0fdfe7774a641f65fbb5635e43d3e09e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:45 +0000

    Add package wradlib/1.0.0/wradlib-1.0.0-py3.6.egg

---
+++ packages/wradlib/wradlib-1.0.0-py3.6.egg/wradlib/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM WRADLIB SETUP.PY
+short_version = '1.0.0'
+version = '1.0.0'
+full_version = '1.0.0'
+git_revision = 'b5577d6c5797b787d4683dccb720e327c629113c'
+release = True
+if not release:
+    version = full_version
