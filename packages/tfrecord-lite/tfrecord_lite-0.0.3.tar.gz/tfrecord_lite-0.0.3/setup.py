118e54d790f3e147854d152308cb480a9b725100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:34 +0000

    Add package tfrecord-lite/0.0.3/tfrecord_lite-0.0.3.tar.gz

---
+++ packages/tfrecord-lite/tfrecord_lite-0.0.3.tar.gz/tfrecord_lite-0.0.3/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+setup(
+    name = 'tfrecord_lite',
+    version = '0.0.3',
+    description = 'A lightweight tfrecord parser',
+    url = 'https://github.com/jongwook/tfrecord_lite',
+    author = 'Jong Wook Kim',
+    author_email = 'jongwook@nyu.edu',
+    license = 'MIT',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords = 'tfrecord',
+    project_urls = {
+        'Source': 'https://github.com/jongwook/tfrecord_lite',
+        'Tracker': 'https://github.com/jongwook/tfrecord_lite/issues'
+    },
+    install_requires = ['numpy'],
+    python_requires = '>=3.6',
+    package_data = {
+        'tfrecord_lite': ['src/*', 'tfrecrod_lite.pyx']
+    },
+    ext_modules = cythonize(Extension(
+        'tfrecord_lite',
+        sources=[
+            'tfrecord_lite.pyx',
+            'src/tfrecord_lite.cpp',
+            'src/example.pb.c',
+            'src/feature.pb.c',
+            'src/pb_common.c',
+            'src/pb_decode.c',
+            'src/pb_encode.c'],
+        language='c++',
+        include_dirs=['src/']
+    ))
+)
