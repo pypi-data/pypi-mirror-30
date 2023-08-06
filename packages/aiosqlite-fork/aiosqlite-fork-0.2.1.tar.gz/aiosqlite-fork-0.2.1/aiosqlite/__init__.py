c092d7899e573c63fc3bfb12740c389b452f311b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:50 +0000

    Add package aiosqlite-fork/0.2.1/aiosqlite-fork-0.2.1.tar.gz

---
+++ packages/aiosqlite-fork/aiosqlite-fork-0.2.1.tar.gz/aiosqlite-fork-0.2.1/aiosqlite/__init__.py
@@ -0,0 +1,19 @@
+# Copyright 2017 John Reese
+# Licensed under the MIT license
+
+'''asyncio bridge to sqlite3'''
+
+from sqlite3 import sqlite_version, sqlite_version_info
+
+from .core import connect, Connection, Cursor
+
+__version__ = '0.2.0'
+__all__ = [
+    '__version__',
+    'sqlite_version',
+    'sqlite_version_info',
+
+    'connect',
+    'Connection',
+    'Cursor',
+]
