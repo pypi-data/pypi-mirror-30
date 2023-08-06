c444817002e8e8d210dd6691ca8cf26d34a254d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:44 +0000

    Add package fb-scrape-public/1.0.10/fb_scrape_public-1.0.10.tar.gz

---
+++ packages/fb-scrape-public/fb_scrape_public-1.0.10.tar.gz/fb_scrape_public-1.0.10/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+  name = 'fb_scrape_public',
+  packages = ['fb_scrape_public'], # this must be the same as the name above
+  version = '1.0.10',
+  description = 'fb_scrape_public',
+  author = 'Deen Freelon',
+  author_email = 'dfreelon@gmail.com',
+  url = 'https://github.com/dfreelon/fb_scrape_public/', # use the URL to the github repo
+  download_url = 'https://github.com/dfreelon/fb_scrape_public/archive/master.tar.gz', # FIXED
+  keywords = ['facebook', 'data collection', 'CSV','big data'], # arbitrary keywords
+)
