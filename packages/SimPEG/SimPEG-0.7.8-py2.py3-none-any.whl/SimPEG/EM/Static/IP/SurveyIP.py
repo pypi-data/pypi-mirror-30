0818286b223a47032da6291f799b6fc279a5534d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:17 +0000

    Add package SimPEG/0.7.8/SimPEG-0.7.8-py2.py3-none-any.whl

---
+++ packages/SimPEG/SimPEG-0.7.8-py2.py3-none-any.whl/SimPEG/EM/Static/IP/SurveyIP.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from SimPEG.EM.Base import BaseEMSurvey
+from SimPEG.EM.Static.DC.SrcDC import BaseSrc
+from SimPEG.EM.Static.DC.RxDC import BaseRx
+from SimPEG.EM.Static.DC import Survey as SurveyDC
+
+
+class Survey(SurveyDC):
+
+    def __init__(self, srcList, **kwargs):
+        self.srcList = srcList
+        SurveyDC.__init__(self, srcList, **kwargs)
+
+    def dpred(self, m=None, f=None):
+        """
+            Predicted data.
+
+            .. math::
+                d_\\text{pred} = Pf(m)
+        """
+        return self.prob.Jvec(m, m, f=f)
