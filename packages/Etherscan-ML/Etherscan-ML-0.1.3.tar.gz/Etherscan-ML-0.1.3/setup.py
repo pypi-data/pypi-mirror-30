8482bb3fe73f24f61b9f1ec519c38f63904bb72a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:08 +0000

    Add package Etherscan-ML/0.1.3/Etherscan-ML-0.1.3.tar.gz

---
+++ packages/Etherscan-ML/Etherscan-ML-0.1.3.tar.gz/Etherscan-ML-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Etherscan-ML',
+    version='0.1.3',
+    packages=['wallet-tools', 'erc20-tools', 'ether-tools','etherscan'],
+    url='https://github.com/elyselam/Etherscan-ML',
+    license='MIT',
+    author='elyse lam', 
+    author_email='Elyse.p.Lam@gmail.com',
+    description='Blockchain Machine Learning and Data Processing Library for Etherscan',
+    long_description='Machine Learning and Data Analysis for Etherscan using Bash, Tensorflow, R,  and other blockchain data science tools for analyzing Ether and Ethereum ERC-20 tokens and wallets.'
+)
