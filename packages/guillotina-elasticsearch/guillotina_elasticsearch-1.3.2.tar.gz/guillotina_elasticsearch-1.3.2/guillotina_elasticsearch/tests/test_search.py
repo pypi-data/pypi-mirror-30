0388c2f055bf7415dc910c0bd996a1250c881cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:46 +0000

    Add package guillotina-elasticsearch/1.3.2/guillotina_elasticsearch-1.3.2.tar.gz

---
+++ packages/guillotina-elasticsearch/guillotina_elasticsearch-1.3.2.tar.gz/guillotina_elasticsearch-1.3.2/guillotina_elasticsearch/tests/test_search.py
@@ -0,0 +1,42 @@
+import json
+import time
+
+
+async def test_indexing_and_search(es_requester):
+    async with es_requester as requester:
+        resp, status = await requester(
+            'POST',
+            '/db/guillotina/',
+            data=json.dumps({
+                '@type': 'Example',
+                'title': 'Item1',
+                'id': 'item1',
+                'categories': [{
+                    'label': 'term1',
+                    'number': 1.0
+                }, {
+                    'label': 'term2',
+                    'number': 2.0
+                }]
+            })
+        )
+        assert status == 201
+        time.sleep(1)
+        resp, status = await requester(
+            'POST',
+            '/db/guillotina/@search',
+            data=json.dumps({})
+        )
+        assert resp['items_count'] == 1
+        assert resp['member'][0]['path'] == '/item1'
+
+        # try removing now...
+        await requester('DELETE', '/db/guillotina/item1')
+        time.sleep(1)
+
+        resp, status = await requester(
+            'POST',
+            '/db/guillotina/@search',
+            data=json.dumps({})
+        )
+        assert resp['items_count'] == 0
