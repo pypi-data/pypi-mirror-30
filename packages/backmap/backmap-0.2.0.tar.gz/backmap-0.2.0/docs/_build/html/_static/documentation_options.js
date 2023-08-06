8a8a217c5471fa6b44713469525009cf6f3c3f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:22 +0000

    Add package backmap/0.2.0/backmap-0.2.0.tar.gz

---
+++ packages/backmap/backmap-0.2.0.tar.gz/backmap-0.2.0/docs/_build/html/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: 'v0.2.0+0.gb0af918.dirty',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
