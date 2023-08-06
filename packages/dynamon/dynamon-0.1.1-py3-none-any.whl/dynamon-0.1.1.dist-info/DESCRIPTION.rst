4fb82a73f1b610c5fcde1fc42d368ed844e3373e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:09 +0000

    Add package dynamon/0.1.1/dynamon-0.1.1-py3-none-any.whl

---
+++ packages/dynamon/dynamon-0.1.1-py3-none-any.whl/dynamon-0.1.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+Python API for dynamon.io
+
+GitHub: https://github.com/dynamon-io/dynamon-python
+
---
+++ packages/dynamon/dynamon-0.1.1-py3-none-any.whl/dynamon/__init__.py
@@ -0,0 +1,4 @@
+from dynamon.dynamon import push, clear
+
+path = None  # If not set by user a random string is generated
+cache_timeout = 1
