b0a766a693b52ba75c7307795ec487fa5e3fa64d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:33 +0000

    Add package over-stats/0.3.1/over_stats-0.3.1-py3-none-any.whl

---
+++ packages/over-stats/over_stats-0.3.1-py3-none-any.whl/over_stats/errors.py
@@ -0,0 +1,11 @@
+class InvalidArgument(Exception):
+    pass
+
+class UnexpectedBehaviour(Exception):
+    pass
+
+class PlayerNotFound(Exception):
+    pass
+
+class DataNotFound(Exception):
+    pass
