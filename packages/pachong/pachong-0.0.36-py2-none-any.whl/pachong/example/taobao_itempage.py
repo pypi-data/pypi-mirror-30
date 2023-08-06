f876ce1520a06164702bd25fa95840ead5ac3daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:22 +0000

    Add package pachong/0.0.36/pachong-0.0.36-py2-none-any.whl

---
+++ packages/pachong/pachong-0.0.36-py2-none-any.whl/pachong/example/taobao_itempage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Cheng Chen
+# @Email : cchen224@uic.edu
+# @Time  : 3/6/18
+# @File  : [pachong] taobao_itempage.py
+
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from pachong.crawler.taobao import Taobao
+from pachong.database.mongodb import MongoDB
+from pachong.fetcher import Browser
+
+# args = sys.argv[1:]
+# nfile = int(args[0])
+
+# fetcher = Browser(proxy='localhost:3128')
+fetcher = Browser('firefox')
+# fetcher = Browser(proxy='13.56.211.230:3128')
+crawler = Taobao('wanghong', 'taobao_items', fetcher=fetcher, Database=MongoDB).import_input(input_list=['559667455412'])#.import_input('itemids{}.txt'.format(nfile))
+    # .login()
+
+crawler.crawl('itempage')
