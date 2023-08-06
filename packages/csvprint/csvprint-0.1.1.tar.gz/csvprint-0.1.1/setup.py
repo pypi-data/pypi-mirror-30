4aa472674abe13b1da87abf5b9cd357b947aa28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:33 +0000

    Add package csvprint/0.1.1/csvprint-0.1.1.tar.gz

---
+++ packages/csvprint/csvprint-0.1.1.tar.gz/csvprint-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='csvprint',
+    version='0.1.1',
+    description='Pretty-printer for csv files, including in Markdown format',
+    packages=['csvprint'],
+    license='MIT',
+    scripts=['bin/csvprint'],
+    author='Vegard Stikbakke',
+    author_email='vegard.stikbakke@gmail.com',
+    url='https://github.com/vegarsti/csvprint',
+    keywords='csv',
+)
