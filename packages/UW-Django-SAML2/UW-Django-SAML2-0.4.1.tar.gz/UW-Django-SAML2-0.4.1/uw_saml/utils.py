e3d0a1ff4b25d9f159584282acd1c7c548af5bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:26 +0000

    Add package UW-Django-SAML2/0.4.1/UW-Django-SAML2-0.4.1.tar.gz

---
+++ packages/UW-Django-SAML2/UW-Django-SAML2-0.4.1.tar.gz/UW-Django-SAML2-0.4.1/uw_saml/utils.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+
+
+def get_user(request):
+    """
+    Return the user login stored in session.samlUserdata, identified in
+    settings by either 'uwnetid' (default) or 'eppn'.
+    """
+    saml_data = request.session.get('samlUserdata')
+
+    if saml_data is not None:
+        user_attr = getattr(settings, 'SAML_USER_ATTRIBUTE', 'uwnetid')
+        user = saml_data.get(user_attr, [])
+        if len(user):
+            return user[0]
+
+
+def is_member_of_group(request, group_id):
+    """
+    Utility function that checks whether the user is a member of the group
+    identified by the passed group_id.
+    """
+    saml_data = request.session.get('samlUserdata')
+
+    if saml_data is not None:
+        if group_id in saml_data.get('isMemberOf', []):
+            return True
+
+    return False
