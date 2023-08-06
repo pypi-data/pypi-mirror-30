a0a4ae8738b2864083293e98b920ccd51cf07495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:08 +0000

    Add package pytablereader/0.16.0/pytablereader-0.16.0.tar.gz

---
+++ packages/pytablereader/pytablereader-0.16.0.tar.gz/pytablereader-0.16.0/setup.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+import os.path
+import sys
+
+import setuptools
+
+
+MODULE_NAME = "pytablereader"
+REQUIREMENT_DIR = "requirements"
+
+pkg_info = {}
+
+
+class ReleaseCommand(setuptools.Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        tag = "v{}".format(pkg_info["__version__"])
+
+        print("Pushing git tags: {}".format(tag))
+
+        os.system("git tag {}".format(tag))
+        os.system("git push --tags")
+
+
+with open(os.path.join(MODULE_NAME, "__version__.py")) as f:
+    exec(f.read(), pkg_info)
+
+with open("README.rst") as fp:
+    long_description = fp.read()
+
+with open(os.path.join("docs", "pages", "introduction", "summary.txt")) as f:
+    summary = f.read()
+
+with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
+    install_requires = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
+    tests_requires = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "docs_requirements.txt")) as f:
+    docs_requires = [line.strip() for line in f if line.strip()]
+
+setuptools_require = ["setuptools>=20.2.2"]
+needs_pytest = set(["pytest", "test", "ptr"]).intersection(sys.argv)
+pytest_runner_require = ["pytest-runner"] if needs_pytest else []
+
+setuptools.setup(
+    name=MODULE_NAME,
+    version=pkg_info["__version__"],
+    url="https://github.com/thombashi/{}".format(MODULE_NAME),
+
+    author=pkg_info["__author__"],
+    author_email=pkg_info["__email__"],
+    description=summary,
+    include_package_data=True,
+    keywords=[
+        "table", "reader", "pandas",
+        "CSV", "Excel", "HTML", "JSON", "LTSV", "Markdown", "MediaWiki", "TSV",
+        "SQLite",
+    ],
+    license=pkg_info["__license__"],
+    long_description=long_description,
+    packages=setuptools.find_packages(exclude=["test*"]),
+
+    install_requires=setuptools_require + install_requires,
+    setup_requires=setuptools_require + pytest_runner_require,
+    tests_require=tests_requires,
+    extras_require={
+        "test": tests_requires,
+        "docs": docs_requires,
+    },
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Database",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    cmdclass={
+        "release": ReleaseCommand,
+    })
