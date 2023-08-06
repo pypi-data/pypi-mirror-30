b4f7b1abf1fcc201e7cb3315c8c2cd169b8e47c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:49 +0000

    Add package MiscMod/0.1/MiscMod-0.1.tar.gz

---
+++ packages/MiscMod/MiscMod-0.1.tar.gz/MiscMod-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='MiscMod',
+    version='0.1',
+    description='Random modules in a package, becuase why not?',
+    author='MrCobalt',
+    author_email='anemail@idontwanttotellyou.com',
+    packages=['mathy', 'test'],
+    scripts=['__init__.py']
+    )
