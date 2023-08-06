ae9e96115e532054f252d778f3b8ef6a354f8184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:25 +0000

    Add package nifi-deploy/0.1.7/nifi-deploy-0.1.7.tar.gz

---
+++ packages/nifi-deploy/nifi-deploy-0.1.7.tar.gz/nifi-deploy-0.1.7/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+from nifi_deploy import __version__
+
+setup(
+    name='nifi-deploy',
+    version=__version__,
+    description='Easy CLI tool for exporting and importing Nifi templates to and from XML files.',
+    long_description='Easy CLI tool for exporting and importing Nifi templates to and from XML files.',
+    # url='',
+    author='Mads H. Jakobsen',
+    author_email='mads@hgaard.net',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: System :: Archiving :: Packaging',
+        'Topic :: System :: Systems Administration',
+    ],
+    keywords='nifi deploy template nipyapi cli automation',
+    packages=['nifi_deploy',],
+    install_requires=[
+        'nipyapi==0.8.0',
+        'requests==2.18.4',
+    ],
+    entry_points={
+        'console_scripts': [
+            'nifi-deploy = nifi_deploy.nifi_deploy:cli',
+        ]
+    }
+)
