071b06ba00b9df95472bf20b347e66874e7f1df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:01 +0000

    Add package RSSIngest/0.0.7/RSSIngest-0.0.7.tar.gz

---
+++ packages/RSSIngest/RSSIngest-0.0.7.tar.gz/RSSIngest-0.0.7/setup.py
@@ -0,0 +1,27 @@
+"""
+Module for ingesting news articles from various sources' RSS feeds.
+"""
+
+from sys import prefix
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="RSSIngest",
+    version="0.0.7",
+    description="Module for ingesting news articles from various sources' RSS feeds.",
+    long_description=long_description,
+    author="Ash Prince",
+    author_email="i7629228@bournemouth.ac.uk",
+    packages=find_packages(),
+    install_requires=["pymongo", 'nltk', 'beautifulsoup4', 'PyYAML', 'feedparser'],
+    scripts=["RSSIngest/bin/article_interpreter", "RSSIngest/bin/rss_ingest"],
+    include_package_data=True,
+    package_data={"outlets": [path.join(here, "RSSIngest/config/config.yaml")]}
+)
