8e27962c0a09503fa9929500abf8a36325d0c4b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:12 +0000

    Add package infdata/1.0.11/infdata-1.0.11.tar.gz

---
+++ packages/infdata/infdata-1.0.11.tar.gz/infdata-1.0.11/infdata/settings.py
@@ -0,0 +1,8 @@
+INITIAL_SERVERS = [
+    ('LOCALHOST', 'http://0.0.0.0:8000'),
+    ('DEVELOPMENT', 'https://dev.wfx.io'),
+    ('Dublin', 'https://wefindx.com.io'),
+    ('Shanghai', 'https://wefindx.io'),
+]
+
+INITIAL_CURRENT = 'LOCALHOST'
