f6813a54a52fad98827982bf6e17cf8697fb3a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:34 +0000

    Add package cacahuate/1.0.0/cacahuate-1.0.0.tar.gz

---
+++ packages/cacahuate/cacahuate-1.0.0.tar.gz/cacahuate-1.0.0/cacahuate/settings.py
@@ -0,0 +1,38 @@
+import os
+import logging
+
+base_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+
+# Testing and log stuff
+TESTING = False
+LOG_LEVEL = logging.INFO
+
+# Where to store xml files
+XML_PATH = os.path.join(base_dir, 'xml')
+
+# Rabbitmq
+RABBIT_HOST = 'localhost'
+RABBIT_QUEUE = 'cacahuate_process'
+RABBIT_NOTIFY_EXCHANGE = 'cacahuate_notify'
+RABBIT_CONSUMER_TAG = 'cacahuate_consumer_1'
+RABBIT_NO_ACK = True
+
+# Mongodb
+MONGO_DBNAME = 'cacahuate'
+MONGO_HISTORY_COLLECTION = 'history'
+
+# Time stuff
+TIMEZONE = 'UTC'
+
+# Supported commands for cacahuate
+COMMANDS = [
+    'step',
+]
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
+
+LDAP_URI = "ldap://localhost:389"
+LDAP_SSL = True
+LDAP_DOMAIN = "local"
