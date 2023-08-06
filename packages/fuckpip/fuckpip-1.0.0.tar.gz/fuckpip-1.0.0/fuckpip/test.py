593b45932c604f3d3147a06fc990b895d79867e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:09 +0000

    Add package fuckpip/1.0.0/fuckpip-1.0.0.tar.gz

---
+++ packages/fuckpip/fuckpip-1.0.0.tar.gz/fuckpip-1.0.0/fuckpip/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin python
+# coding: utf-8
+
+def fuck():
+    print('fuck')
+
+def love():
+    print('love')
+
---
+++ packages/fuckpip/fuckpip-1.0.0.tar.gz/fuckpip-1.0.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='fuckpip',
+    version='1.0.0',
+    author='2627866800',
+    author_email='2627866800@qq.com',
+    url='https://test.fstljx.com',
+    description=u'测试pip安装包',
+    packages=['fuckpip'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'fuck=fuckpip.test:fuck',
+            'love=fuckpip.test:love'
+        ]
+    }
+)
