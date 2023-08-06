6e2b42e999dc0f879e37fcda7e1a599936996b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:53 +0000

    Add package python-telegram-handler-proxy/1.0.2/python-telegram-handler-proxy-1.0.2.tar.gz

---
+++ packages/python-telegram-handler-proxy/python-telegram-handler-proxy-1.0.2.tar.gz/python-telegram-handler-proxy-1.0.2/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+with open('README.rst') as readme:
+    with open('HISTORY.rst') as history:
+        long_description = readme.read() + '\n\n' + history.read()
+
+VERSION = '1.0.2'
+
+setup(
+    install_requires=['requests'],
+    name='python-telegram-handler-proxy',
+    version=VERSION,
+    packages=['telegram_handler_proxy'],
+    keywords=['telegram', 'logging', 'handler', 'bot', 'proxy'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: System :: Logging'
+    ],
+    long_description=long_description,
+    license='MIT License',
+    author='NickSablukov',
+    author_email='dessanndes@gmail.com',
+    description='Fork a python logging handler that sends logs via Telegram Bot Api with proxy',
+)
