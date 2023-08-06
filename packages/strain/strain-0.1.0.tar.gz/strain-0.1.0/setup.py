54b0d07231700f7cd4fb1e7afcb113bfda7e5c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:02 +0000

    Add package strain/0.1.0/strain-0.1.0.tar.gz

---
+++ packages/strain/strain-0.1.0.tar.gz/strain-0.1.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+
+from setuptools import setup, find_packages
+
+readme = """Implementation of strains for III-V materials."""
+
+with open('./strain/_version.py', 'r') as f:
+    match_expr = "__version__[^'\"]+(['\"])([^'\"]+)"
+    version = re.search(match_expr, f.read()).group(2).strip()
+
+setup(
+    name='strain',
+    version=version,
+    author='Dominik Gresch',
+    author_email='greschd@gmx.ch',
+    install_requires=['numpy', 'pymatgen', 'fsc.export'],
+    extras_require={
+        'dev': [
+            'pytest', 'pre-commit', 'yapf==0.21.0', 'prospector', 'sphinx',
+            'sphinx-rtd-theme<0.3'
+        ]
+    },
+    long_description=readme,
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English', 'Operating System :: Unix',
+        'Development Status :: 3 - Alpha', 'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    license='GPL',
+    packages=find_packages()
+)
