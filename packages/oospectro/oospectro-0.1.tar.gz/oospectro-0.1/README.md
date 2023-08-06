8bc131000e947a1612e5ae66938da137f53b4f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:10 +0000

    Add package oospectro/0.1/oospectro-0.1.tar.gz

---
+++ packages/oospectro/oospectro-0.1.tar.gz/oospectro-0.1/README.md
@@ -0,0 +1,17 @@
+
+# Purpose
+
+Get thicknesses from Ocean Optics spectra.
+
+# Install
+
+## Dependencies
+
+* numpy
+* scipy
+* cython
+* matplotlib
+
+Installing scipy >= 1.1.0 will provide better performances.
+If scipy < 1.1.0 is install, then a non-optimized copy of scipy algorithms is used internally.
+
---
+++ packages/oospectro/oospectro-0.1.tar.gz/oospectro-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+with open('oospectro/__init__.py') as fid:
+    for line in fid:
+        if line.startswith('__version__'):
+            VERSION = line.strip().split()[-1][1:-1]
+            break
+
+
+setup(
+    name='oospectro',
+    version=VERSION,
+    url='https://github.com/sciunto-org/oospectro',
+    maintainer='F. Boulogne',
+    maintainer_email='devel@sciunto.org',
+    license='BSD',
+    platforms=['all'],
+    description='A library that calculates thicknesses from ocean optics spectra.',
+    packages=['oospectro',],
+    provides=['oospectro',],
+    install_requires = ['numpy', 'scipy', 'matplotlib', 'scikit-learn']
+    )
