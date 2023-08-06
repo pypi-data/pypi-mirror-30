9989d702f988ad095e9bdae6ae36c05f1b491abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:47 +0000

    Add package cjl-test/0.1/cjl-test-0.1.tar.gz

---
+++ packages/cjl-test/cjl-test-0.1.tar.gz/cjl-test-0.1/cjltest/models.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SimpleCNN(nn.Module):
+    """ CNN Network architecture. """
+
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+# Linear Regression Model
+class LinearRegression(nn.Module):
+    def __init__(self):
+        super(LinearRegression, self).__init__()
+        self.linear = nn.Linear(1, 1)  # input and output is 1 dimension
+
+    def forward(self, x):
+        return self.linear(x)
