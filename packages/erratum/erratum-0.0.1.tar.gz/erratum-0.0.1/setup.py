321ba6168fc7c1b78b71162f14812d911fad6573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:40 +0000

    Add package erratum/0.0.1/erratum-0.0.1.tar.gz

---
+++ packages/erratum/erratum-0.0.1.tar.gz/erratum-0.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+
+exec(open("erratum/version.py", 'r').read())
+
+
+setup(name="erratum",
+      version=__version__,
+      description="User friendly errors",
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Documentation',
+          'Topic :: Documentation :: Sphinx',
+          'Topic :: Software Development :: Documentation'
+      ],
+      author="Alex Carney",
+      author_email="alcarneyme@gmail.com",
+      license='MIT',
+      packages=find_packages(exclude="tests"),
+      python_requires='>=3.0',
+      entry_points={
+          'console_scripts': [
+              'erratum.document = erratum.document:main'
+          ]
+      },
+      zip_safe=False)
