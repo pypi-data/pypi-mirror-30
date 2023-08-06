7c59287d3be9b67e24e3e9af3b0e50adfae90cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:30 +0000

    Add package resolwe/8.0.0a7/resolwe-8.0.0a7-py3-none-any.whl

---
+++ packages/resolwe/resolwe-8.0.0a7-py3-none-any.whl/resolwe/elastic/fields.py
@@ -0,0 +1,60 @@
+"""Elastic search fields for Resolwe."""
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import elasticsearch_dsl as dsl
+
+# pylint: disable=invalid-name
+# Process type analyzer. During indexing we tokenize by type paths, during search,
+# we do not tokenize at all.
+process_type_tokenizer = dsl.tokenizer('process_type_tokenizer', type='path_hierarchy', delimiter=':')
+process_type_analyzer = dsl.analyzer('process_type_analyzer', tokenizer=process_type_tokenizer, filter=['lowercase'])
+process_type_search_analyzer = dsl.analyzer('process_type_search_analyzer', tokenizer='keyword', filter=['lowercase'])
+
+# Name analyzer.
+name_analyzer = dsl.analyzer(
+    'name_analyzer',
+    type='pattern',
+    # The pattern matches token separators.
+    pattern=r'''
+          ([^\p{L}\d]+)                 # swallow non letters and numbers,
+        | (?<=\D)(?=\d)                 # or non-number followed by number,
+        | (?<=\d)(?=\D)                 # or number followed by non-number,
+    ''',
+    flags='CASE_INSENSITIVE|COMMENTS',
+    lowercase=True,
+)
+# pylint: enable=invalid-name
+
+
+class RawKeywordSubfieldMixin(object):
+    """String field with a 'raw' subfield (e.g. for sorting)."""
+
+    def __init__(self, *args, **kwargs):
+        """Construct field."""
+        kwargs.setdefault('fields', {})['raw'] = {'type': 'keyword'}
+        super(RawKeywordSubfieldMixin, self).__init__(*args, **kwargs)
+
+
+class Name(RawKeywordSubfieldMixin, dsl.Text):
+    """Field for names supporting term matches.
+
+    Includes a 'raw' subfield for sorting.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Construct field."""
+        kwargs.setdefault('analyzer', name_analyzer)
+        super(Name, self).__init__(*args, **kwargs)
+
+
+class ProcessType(RawKeywordSubfieldMixin, dsl.Text):
+    """Field for process type supporting hierarchical type matches.
+
+    Includes a 'raw' subfield for sorting.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Construct field."""
+        kwargs.setdefault('analyzer', process_type_analyzer)
+        kwargs.setdefault('search_analyzer', process_type_search_analyzer)
+        super(ProcessType, self).__init__(*args, **kwargs)
