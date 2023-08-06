b906d5f2ff92706723d7d289e294ec22320baacb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:01 +0000

    Add package My-First-BOT/0.4/My-First-BOT-0.4.tar.gz

---
+++ packages/My-First-BOT/My-First-BOT-0.4.tar.gz/My-First-BOT-0.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='My-First-BOT',
+    packages=['package1', 'package2'],
+    # scripts=['runner'],
+    version='0.4',
+    description='Test BOT',
+    author='elsiehsieh',
+    author_email='8chingen8@gmail.com',
+    url='https://github.com/elsiehsieh/pip_project_1.git',
+    download_url='https://github.com/elsiehsieh/pip_project_1/tarball/v0.2',
+    keywords=['chatbot'],
+    classifiers=[]
+)
