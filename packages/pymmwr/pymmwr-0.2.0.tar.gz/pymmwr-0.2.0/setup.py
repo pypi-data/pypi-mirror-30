4bc29f3a8ec45d4fe242de23c8e336d4205793ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:40 +0000

    Add package pymmwr/0.2.0/pymmwr-0.2.0.tar.gz

---
+++ packages/pymmwr/pymmwr-0.2.0.tar.gz/pymmwr-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="pymmwr",
+    version="0.2.0",
+    description="MMWR weeks for Python",
+    long_description=readme,
+    author="Abhinav Tushar",
+    author_email="lepisma@fastmail.com",
+    url="https://github.com/reichlab/pymmwr",
+    install_requires=["attrs"],
+    keywords="mmwr week converter",
+    packages=find_packages(),
+    classifiers=(
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only"
+    ))
