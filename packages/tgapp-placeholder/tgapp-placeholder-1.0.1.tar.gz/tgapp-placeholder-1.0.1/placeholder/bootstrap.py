85c8a305f04c69d9028734426943b6f68f66aef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:31 +0000

    Add package tgapp-placeholder/1.0.1/tgapp-placeholder-1.0.1.tar.gz

---
+++ packages/tgapp-placeholder/tgapp-placeholder-1.0.1.tar.gz/tgapp-placeholder-1.0.1/placeholder/bootstrap.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Setup the placeholder application"""
+from __future__ import print_function
+
+def bootstrap(command, conf, vars):
+    print('Bootstrapping placeholder...')
