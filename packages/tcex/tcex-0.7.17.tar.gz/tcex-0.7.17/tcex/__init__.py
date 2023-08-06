3d21eda39bebe311f43437f4038f1f9e69fd3377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:14 +0000

    Add package tcex/0.7.17/tcex-0.7.17.tar.gz

---
+++ packages/tcex/tcex-0.7.17.tar.gz/tcex-0.7.17/tcex/__init__.py
@@ -0,0 +1,12 @@
+__author__ = 'ThreatConnect (support@threatconnect.com)'
+__version__ = '0.7.17'
+__license__ = 'Apache License, Version 2'
+
+try:
+    from .tcex import TcEx
+except ImportError as e:
+    print('Error: {}'.format(e))
+    print('Try running tc_lib')
+
+from .tcex_local import TcExLocal
+from .argparser import ArgParser
