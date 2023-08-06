4be5e0709d1b1766b2cb82b597ff5cca195a46ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:36 +0000

    Add package astropixie-widgets/0.1.0/astropixie-widgets-0.1.0.tar.gz

---
+++ packages/astropixie-widgets/astropixie-widgets-0.1.0.tar.gz/astropixie-widgets-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='astropixie-widgets',
+      version='0.1.0',
+      description='LSST EPO ',
+      url='https://github.com/lsst-epo/vela/astropixie-hr',
+      author='LSST EPO Team',
+      author_email='epo-team@lists.lsst.org',
+      license='MIT',
+      packages=['astropixie_widgets'],
+      include_package_data=True,
+      package_data={'astropixie': ['sample_data/*']},
+      install_requires=[
+          'astropixie',
+          'astropy==3.0',
+          'astroquery',
+          'bokeh>=0.12.15',
+          # 'ipyaladin',
+          'numpy==1.14.1',
+          'pandas',
+          'pytest',
+          'scipy'
+      ])
