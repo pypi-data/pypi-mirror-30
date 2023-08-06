d8c33b38a37818b0e803f319b2232ad887c24e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:33 +0000

    Add package osc-gen/0.1.1/osc_gen-0.1.1-py3-none-any.whl

---
+++ packages/osc-gen/osc_gen-0.1.1-py3-none-any.whl/osc_gen/visualise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+""" Plot waveforms """
+
+import matplotlib.pyplot as plt
+
+
+def plot_wave(wave):
+    """ Plot a single wave """
+
+    plt.plot(wave)
+    plt.show()
+    plt.gcf().clear()
+
+
+def plot_wavetable(wavetable, title=''):
+    """ Plot all waves in a wavetable """
+
+    for wave in wavetable.waves:
+        plt.plot(wave)
+
+    plt.title(title)
+    plt.show()
+    plt.gcf().clear()
