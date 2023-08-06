e340704ff18b173cd3341dbd6d5f97beb96f5b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:05 +0000

    Add package django-pre-post/1.1.1/django_pre_post-1.1.1.tar.gz

---
+++ packages/django-pre-post/django_pre_post-1.1.1.tar.gz/django_pre_post-1.1.1/setup.py
@@ -0,0 +1,33 @@
+
+import os
+
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+except:  # noqa: E722
+    from distutils.core import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def read_requirements(fname):
+    f = open(os.path.join(os.path.dirname(__file__), fname))
+    return filter(lambda f: f != '', map(lambda f: f.strip(), f.readlines()))
+
+
+setup(
+    zip_safe=False,
+    name="django_pre_post",
+    version="1.1.1",
+    author="Silly Inventor",
+    author_email="SillyInventor@gmail.com",
+    description="This package provides a framework for surveys and questionnaires",
+    keywords="test, pre-post, questionnaire, survey",
+    packages=find_packages(),
+    long_description=read('README.md'),
+    install_requires=read_requirements('requirements.txt'),
+    test_suite="dummy",
+    include_package_data=True,
+)
