fc18dbbd56d98ecd43b29284190c7e8e2c6fda67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:30 +0000

    Add package tesolib/0.0.2/tesolib-0.0.2.tar.gz

---
+++ packages/tesolib/tesolib-0.0.2.tar.gz/tesolib-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+
+# Tesodev LTD STI 2018
+# Please read README for detailed information
+# This place is not for (very) long description
+# But there should be some head information about the project
+# Library starts below
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "tesolib",
+    version = "0.0.2",
+    author = "Tesodev",
+    author_email = "kenan.kurt@tesodev.com",
+    description = ("Test script for publish "
+                                   "packages in PIPY"),
+    license = "BSD",
+    keywords = "tesodev, tesolib",
+    url = "http://packages.python.org/tesolib",
+    #packages=[''],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
