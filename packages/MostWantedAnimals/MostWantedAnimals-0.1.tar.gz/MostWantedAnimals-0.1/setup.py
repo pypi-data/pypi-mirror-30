b6105a7f83f0d7326fe1a4de962f6f038bd2ad45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:24 +0000

    Add package MostWantedAnimals/0.1/MostWantedAnimals-0.1.tar.gz

---
+++ packages/MostWantedAnimals/MostWantedAnimals-0.1.tar.gz/MostWantedAnimals-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='MostWantedAnimals',
+      version='0.1',
+      description='Lame',
+      url='http://github.com/shankar-ray/Animals',
+      author='shankar-ray',
+      author_email='shankar.ray030@gmail.com',
+      license='MIT',
+      packages=['MostWantedAnimals'],
+      zip_safe=False)
