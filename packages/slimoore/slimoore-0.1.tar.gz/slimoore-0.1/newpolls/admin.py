f6e37d47dc5eb2a384c4e6e30a7ea1caf093af0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:29 +0000

    Add package slimoore/0.1/slimoore-0.1.tar.gz

---
+++ packages/slimoore/slimoore-0.1.tar.gz/slimoore-0.1/newpolls/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    change_list_template = 'newpolls/base.html'
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    change_list_template = 'newpolls/change_list.html'
+
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
+
