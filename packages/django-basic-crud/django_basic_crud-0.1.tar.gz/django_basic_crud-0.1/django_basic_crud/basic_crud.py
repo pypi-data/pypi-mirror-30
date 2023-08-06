2573af4a630075113b3ee13320c23f891c72930a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:34 +0000

    Add package django-basic-crud/0.1/django_basic_crud-0.1.tar.gz

---
+++ packages/django-basic-crud/django_basic_crud-0.1.tar.gz/django_basic_crud-0.1/django_basic_crud/basic_crud.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.http import JsonResponse
+from django.shortcuts import redirect, render, get_object_or_404
+
+
+# Create a basic object
+def new_object(request, form, operation, template_name, redirect_success):
+    context = dict()
+
+    object_form = form(request.POST or None, request.FILES or None, prefix='object')
+
+    if request.method == 'POST':
+        if object_form.is_valid():
+            object_form.save()
+            return redirect(redirect_success)
+        else:
+            context['errors'] = object_form.errors
+
+    context['edit'] = False
+    context['form'] = object_form
+    context['operation'] = operation
+
+    return render(request=request, template_name=template_name, context=context)
+
+
+# Update a basic object
+def update_object(request, pk, object_model, form, operation, template_name, redirect_success):
+    context = dict()
+
+    object_instance = get_object_or_404(object_model, pk=pk)
+    object_form = form(request.POST or None, request.FILES or None, instance=object_instance)
+
+    if request.method == 'POST':
+        if object_form.is_valid():
+            object_form.save()
+
+            return redirect(redirect_success)
+        else:
+            context['errors'] = object_form.errors
+    else:
+        object_form = form(instance=object_instance)
+
+    context['form'] = object_form
+    context['operation'] = operation
+    context['edit'] = True
+    return render(request=request, template_name=template_name, context=context)
+
+
+# Delete ajax object
+def delete_object(request, pk, object_model):
+    if request.method == 'POST':
+        try:
+            object_instance = object_model.objects.get(pk=pk)
+        except object_model.DoesNotExist:
+            return JsonResponse('Does Not Exist', status=404, safe=False)
+        object_instance.delete()
+        return JsonResponse('Success', status=200, safe=False)
+    else:
+        return JsonResponse('Bad Request', status=405, safe=False)
---
+++ packages/django-basic-crud/django_basic_crud-0.1.tar.gz/django_basic_crud-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'django_basic_crud',
+  packages = ['django_basic_crud'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A simple Django app to basic CRUD.',
+  author = 'Ricardo Pizano',
+  author_email = 'rikymon2@hotmail.com',
+  url = 'https://github.com/RicardoPizano/django_basic_crud', # use the URL to the github repo
+  download_url = 'https://github.com/RicardoPizano/django_basic_crud/tree/master/dist/django_basic_crud-0.1.tar.gz',
+  keywords = [],
+  classifiers = [],
+)
