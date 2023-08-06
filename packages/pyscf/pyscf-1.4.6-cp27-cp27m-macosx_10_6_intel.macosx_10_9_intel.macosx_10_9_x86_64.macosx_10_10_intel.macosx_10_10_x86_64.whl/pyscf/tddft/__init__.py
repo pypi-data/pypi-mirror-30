eb55af8a529522767e12a94c1e56a18214f049eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:31 +0000

    Add package pyscf/1.4.6/pyscf-1.4.6-cp27-cp27m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl

---
+++ packages/pyscf/pyscf-1.4.6-cp27-cp27m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl/pyscf/tddft/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#
+# Author: Qiming Sun <osirpt.sun@gmail.com>
+#
+
+from pyscf.tddft import rks
+from pyscf.tddft import rhf
+from pyscf.tddft.rhf import TDHF, CIS, RPA
+from pyscf.tddft.rks import dRPA, dTDA
+
+def TD(mf):
+    from pyscf import scf
+    mf = scf.addons.convert_to_rhf(mf)
+    return TDDFT(mf)
+
+def TDA(mf):
+    from pyscf import scf
+    mf = scf.addons.convert_to_rhf(mf)
+    if hasattr(mf, 'xc'):
+        return rks.TDA(mf)
+    else:
+        return rhf.TDA(mf)
+
+def TDDFT(mf):
+    from pyscf import scf
+    mf = scf.addons.convert_to_rhf(mf)
+    if hasattr(mf, 'xc'):
+        if mf._numint.libxc.is_hybrid_xc(mf.xc):
+            return rks.TDDFT(mf)
+        else:
+            return rks.TDDFTNoHybrid(mf)
+    else:
+        return rhf.TDHF(mf)
