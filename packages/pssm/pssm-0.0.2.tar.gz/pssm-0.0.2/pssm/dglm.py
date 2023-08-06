f0da483d5c4ee177508655c13e55e9356c856841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:03 +0000

    Add package pssm/0.0.2/pssm-0.0.2.tar.gz

---
+++ packages/pssm/pssm-0.0.2.tar.gz/pssm-0.0.2/pssm/dglm.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy.stats import multivariate_normal as mvn
+
+
+class DGLM:
+    pass
+
+
+class NormalDLM(DGLM):
+    def __init__(self, structure, V):
+        self._structure = structure
+        self._V = np.matrix([[V]])
+
+    def state(self, previous):
+        mean = np.squeeze(np.asarray(np.dot(self._structure.G, previous)))
+        return mvn(mean=mean, cov=self._structure.W).rvs()
+
+    def observation(self, state):
+        mean = np.dot(self._structure.F, state)
+        return mvn(mean=mean, cov=self._V).rvs()
