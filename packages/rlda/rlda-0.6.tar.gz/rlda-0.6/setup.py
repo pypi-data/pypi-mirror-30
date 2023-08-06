f96e9808c9918d3678d284a96dfecefbcc0e68f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:21 +0000

    Add package rlda/0.6/rlda-0.6.tar.gz

---
+++ packages/rlda/rlda-0.6.tar.gz/rlda-0.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='rlda',
+      version='0.6',
+      description='A module to use robust lda topics for the study of text',
+      url='http://github.com/CasAndreu/rlda',
+      author='Andreu Casas',
+      author_email='acasas2@uw.edu',
+      license='MIT',
+      packages=['rlda'],
+      install_requires=[
+          'nltk',
+          'sklearn',
+          'numpy',
+          're',
+          'json',
+          'string',
+          'tqdm',
+          'lda',
+          'textmining'
+      ],
+      zip_safe=False,
+      include_package_data=True)
