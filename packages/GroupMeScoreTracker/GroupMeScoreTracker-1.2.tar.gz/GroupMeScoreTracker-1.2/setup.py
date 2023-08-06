8e8349c13ea08e0877629eb9aeea706aba3cd0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:36 +0000

    Add package GroupMeScoreTracker/1.2/GroupMeScoreTracker-1.2.tar.gz

---
+++ packages/GroupMeScoreTracker/GroupMeScoreTracker-1.2.tar.gz/GroupMeScoreTracker-1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='GroupMeScoreTracker',
+    version='1.2',
+    url='https://github.com/cheeseypi/GroupMeScoreTracker',
+    download_url='https://github.com/cheeseypi/GroupMeScoreTracker/archive/v1.0.tar.gz',
+    author='Matthew Doto',
+    author_email='matt@mattdoto.com',
+    packages=['scoretracker'],
+    license='LICENSE.txt',
+    description='A score tracker for GroupMe, allowing competition between friends',
+    long_description=open('README.rst').read(),
+    install_requires=open('requirements.txt').readlines()
+)
