07485701897e99225d4416c5fb5b85828bf1ca68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:24 +0000

    Add package snipsneopixel/0.1.0/snipsneopixel-0.1.0.tar.gz

---
+++ packages/snipsneopixel/snipsneopixel-0.1.0.tar.gz/snipsneopixel-0.1.0/snipsneopixel/snipsneopixel.py
@@ -0,0 +1,43 @@
+# -*-: coding utf-8 -*-
+""" Neopixel skill for Snips. """
+
+from __future__ import unicode_literals
+from lights import Animator, Animation
+import atexit
+
+
+class SnipsNeopixel:
+    """NeoPixel skill for Snips."""
+
+    def __init__(self, arduino_port='/dev/ttyACM0', debug=False, locale=None):
+        try:
+            self.animator = Animator(arduino_port, debug)
+            self.animator.open()
+            self.animator.run(Animation.waking_up)
+            atexit.register(self.close)
+        except Exception as ex:
+            print(ex)
+            self.animator = None
+            pass
+
+    def stand_by(self):
+        self.animator.run(Animation.standby)
+
+    def listening(self):
+        self.animator.run(Animation.listening)
+
+    def waking_up(self):
+        self.animator.run(Animation.waking_up)
+
+    def error(self):
+        self.animator.run(Animation.error)
+
+    def none(self):
+        self.animator.run(Animation.none)
+
+    def close(self):
+        self.none()
+        self.animator.close()
+
+
+
