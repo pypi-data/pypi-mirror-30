28c4069f8becabe3507ceb1045b2bf1c0e30a363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:24 +0000

    Add package pyspedas/0.2.2/pyspedas-0.2.2-py2.py3-none-any.whl

---
+++ packages/pyspedas/pyspedas-0.2.2-py2.py3-none-any.whl/pyspedas/__init__.py
@@ -0,0 +1,5 @@
+from core import load_data, spd_version
+from spd_prefs import get_spedas_prefs
+from spd_helpers import download_files, url_exists, find_latest_url_version, get_probes, get_instruments
+from spd_dates import validate_date, get_date_list, get_dates
+from spd_themis import themis_filename, load_themis
