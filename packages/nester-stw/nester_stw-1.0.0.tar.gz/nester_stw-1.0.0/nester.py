a3c005a636b006e7048bd9a8fce8d83ca586a5c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:35 +0000

    Add package nester-stw/1.0.0/nester_stw-1.0.0.tar.gz

---
+++ packages/nester-stw/nester_stw-1.0.0.tar.gz/nester_stw-1.0.0/nester.py
@@ -0,0 +1,8 @@
+"""this is the standard way to include a multiple-line comment in your code"""
+#this is the standard way to include a multiple-line comment in your code
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item)
+        else:
+            print(each_item)
---
+++ packages/nester-stw/nester_stw-1.0.0.tar.gz/nester_stw-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'nester_stw',
+        version     = '1.0.0',
+        py_modules  = ['nester'],
+        author      = 'sitengwei',
+        author_email= '32553411@qq.com',
+        url         = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+    )
