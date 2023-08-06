5c4fb9047d4635ee193bca71519deb1e0e23b351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:17 +0000

    Add package custom-inherit/2.2.0/custom_inherit-2.2.0.tar.gz

---
+++ packages/custom-inherit/custom_inherit-2.2.0.tar.gz/custom_inherit-2.2.0/setup.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+from distutils.core import setup
+from setuptools import find_packages
+import custom_inherit
+
+ver = custom_inherit.__version__
+setup(name='custom_inherit',
+      version= ver,
+      description='A Python package that provides customized docstring inheritance\
+       schemes between derived classes and their parents.',
+      author='@meowklaski',
+      author_email="rsoklaski@gmail.com",
+      url='https://github.com/meowklaski/custom_inherit',
+      download_url="https://github.com/meowklaski/custom_inherit/tarball/"+ver,
+      packages=find_packages(),
+      license="MIT"
+      )
