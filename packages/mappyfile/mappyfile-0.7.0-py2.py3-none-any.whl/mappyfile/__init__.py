1857a061be64d1f70e2d71079f20916965f961d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:05 +0000

    Add package mappyfile/0.7.0/mappyfile-0.7.0-py2.py3-none-any.whl

---
+++ packages/mappyfile/mappyfile-0.7.0-py2.py3-none-any.whl/mappyfile/__init__.py
@@ -0,0 +1,28 @@
+import logging
+import pkg_resources
+import sys
+from types import ModuleType
+# allow high-level functions to be accessed directly from the mappyfile module
+from mappyfile.utils import open, load, loads, find, findall, dumps, dump, save, write, dictfind
+
+__version__ = "0.7.0"
+
+__all__ = ['open', 'load', 'loads', 'find', 'findall', 'dumps', 'dump', 'save', 'write', 'dictfind']
+
+
+plugins = ModuleType('mappyfile.plugins')
+sys.modules['mappyfile.plugins'] = plugins
+
+for ep in pkg_resources.iter_entry_points(group='mappyfile.plugins'):
+    setattr(plugins, ep.name, ep.load())
+
+# Set default logging handler to avoid "No handler found" warnings.
+
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger("mappyfile").addHandler(NullHandler())
