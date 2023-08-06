792554b83b5dc1969352e2d7c3eb79d98a46ef78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:52 +0000

    Add package lingcorpora/1.0/lingcorpora-1.0.tar.gz

---
+++ packages/lingcorpora/lingcorpora-1.0.tar.gz/lingcorpora-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='lingcorpora',
+      version='1.0',
+      description='API for corpora',
+      url='https://github.com/lingcorpora/lingcorpora.py',
+      author='Alexey Koshevoy, Artem Kopetsky, Ekaterina Gerasimenko, Maria Terekhina',
+      license='MIT',
+      author_email='alexeykochevoy@gmail.com',
+      packages=['lingcorpora'],
+      zip_safe=False,
+      keywords = ['corpora', 'api', 'language'],
+      install_requires=['bs4', 'requests', 'lxml', 'tqdm'])
