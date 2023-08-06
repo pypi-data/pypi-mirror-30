5bf2686d3a2e1d14a8a9004ec35f6b9bbbe8bfc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:01 +0000

    Add package mdt/0.11.4/mdt-0.11.4.tar.gz

---
+++ packages/mdt/mdt-0.11.4.tar.gz/mdt-0.11.4/mdt/data/components/standard/composite_models/relaxometry.py
@@ -0,0 +1,31 @@
+from mdt.component_templates.composite_models import CompositeModelTemplate
+
+__author__ = 'Francisco.Lagos'
+
+
+class S0_T1_GRE(CompositeModelTemplate):
+
+    description = '''Inversion recovery model. 
+    
+    This is made to model the MI-EPI sequence, a multi inversion recovery epi (Renvall et Al. 2016). 
+    The Model is based on Stikov et al.'s three parameter model.
+    '''
+    model_expression = 'S0 * ExpT1DecIR'
+    inits = {'ExpT1DecIR.T1': 3.0}
+    upper_bounds = {'ExpT1DecIR.T1': 6.0}
+
+
+class S0T2_exvivo(CompositeModelTemplate):
+
+    name = 'S0-T2-ExVivo'
+    description = 'Models the unweighted signal (aka. b0) with an extra T2.'
+    model_expression = 'S0 * ExpT2Dec'
+    upper_bounds = {'ExpT2Dec.T2': 0.5}
+
+
+class S0T2(CompositeModelTemplate):
+
+    name = 'S0-T2'
+    description = 'Models the unweighted signal (aka. b0) with an extra T2.'
+    model_expression = 'S0 * ExpT2Dec'
+    upper_bounds = {'ExpT2Dec.T2': 2}
