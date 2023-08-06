194d71de1aec1ca652629b202a6c198571e72557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:01 +0000

    Add package elastic-apm/2.1.1/elastic_apm-2.1.1-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/elastic-apm/elastic_apm-2.1.1-cp27-cp27mu-manylinux1_x86_64.whl/elasticapm/contrib/django/apps.py
@@ -0,0 +1,61 @@
+from functools import partial
+
+from django.apps import AppConfig
+
+from elasticapm.contrib.django.client import get_client
+
+
+class ElasticAPMConfig(AppConfig):
+    name = 'elasticapm.contrib.django'
+    label = 'elasticapm.contrib.django'
+    verbose_name = 'ElasticAPM'
+
+    def __init__(self, *args, **kwargs):
+        super(ElasticAPMConfig, self).__init__(*args, **kwargs)
+        self.client = None
+
+    def ready(self):
+        self.client = get_client()
+        register_handlers(self.client)
+        if self.client.config.instrument:
+            instrument(self.client)
+        else:
+            self.client.logger.debug("Skipping instrumentation. INSTRUMENT is set to False.")
+
+
+def register_handlers(client):
+    from django.core.signals import got_request_exception
+    from elasticapm.contrib.django.handlers import exception_handler
+
+    # Connect to Django's internal signal handler
+    dispatch_uid = 'elasticapm-exceptions'
+    got_request_exception.disconnect(dispatch_uid=dispatch_uid)
+    got_request_exception.connect(partial(exception_handler, client), dispatch_uid=dispatch_uid, weak=False)
+
+    # If we can import celery, register ourselves as exception handler
+    try:
+        import celery  # noqa F401
+        from elasticapm.contrib.celery import register_exception_tracking
+
+        try:
+            register_exception_tracking(client)
+        except Exception as e:
+            client.logger.exception('Failed installing django-celery hook: %s' % e)
+    except ImportError:
+        client.logger.debug("Not instrumenting Celery, couldn't import")
+
+
+def instrument(client):
+    """
+    Auto-instruments code to get nice spans
+    """
+    from elasticapm.instrumentation.control import instrument
+
+    instrument()
+    try:
+        import celery  # noqa F401
+        from elasticapm.contrib.celery import register_instrumentation
+
+        register_instrumentation(client)
+    except ImportError:
+        client.logger.debug("Not instrumenting Celery, couldn't import")
