8d981a17616c8b10f9fef9715b7410ae748a274f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:18 +0000

    Add package peegee/0.1.1/peegee-0.1.1.tar.gz

---
+++ packages/peegee/peegee-0.1.1.tar.gz/peegee-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name = 'peegee',
+    packages = ['peegee'],
+    version = '0.1.1',
+    description = 'A pythonic PostgreSQL client based on psycopg2 suit only for Python3',
+    install_requires=['psycopg2',],
+    author = 'scku',
+    author_email = 'scku208@gmail.com',
+    url = 'https://github.com/scku208/peegee',
+    download_url = 'https://github.com/scku208/peegee/archive/0.1.1.tar.gz',
+    keywords = ['postgresql', 'psycopg2', 'database'],
+    classifiers = [],
+)
