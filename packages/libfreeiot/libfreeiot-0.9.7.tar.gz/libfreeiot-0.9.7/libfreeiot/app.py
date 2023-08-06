49017f7f57f8506fb48bd39f5b3dfd5078cc6b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:50 +0000

    Add package libfreeiot/0.9.7/libfreeiot-0.9.7.tar.gz

---
+++ packages/libfreeiot/libfreeiot-0.9.7.tar.gz/libfreeiot-0.9.7/libfreeiot/app.py
@@ -0,0 +1,33 @@
+"""
+    Main Entry for FreeIOT
+
+    Author: Noah Gao
+    Updated at: 2018-02-23
+"""
+import os
+from libfreeiot.core import create_app
+
+def create_flask_app():
+    """ Function for create flask application instance """
+    return create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+def run(port = int(os.environ.get("APP_PORT")),
+    host = os.environ.get('APP_HOST', '127.0.0.1'),
+    adapters = None,
+    app = None):
+    """ Main Method for running application """
+    if app is None:
+        app = create_flask_app() # Create Flask Application
+    scope = dict()
+
+    # 代入数据库句柄到 Adapter 作用域
+    from .core import mongo
+    scope["mongo"] = mongo
+
+    if adapters is None:
+        adapters = [] # Create adapter group if not provided
+    for adapter in adapters:
+        adapter.init(app, scope) # Initialize all adapters
+        adapter.run() # Run all adapters
+
+    app.run(debug=os.environ.get("APP_DEBUG") == "true", port=port, host=host) # Start API services
