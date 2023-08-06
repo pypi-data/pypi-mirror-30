2897bfff53178a27f212802fb50a53ee59841ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:35 +0000

    Add package slimoore/0.1.1/slimoore-0.1.1.tar.gz

---
+++ packages/slimoore/slimoore-0.1.1.tar.gz/slimoore-0.1.1/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='slimoore',
+    version='0.1.1',
+    packages=find_packages(),
+    include_package_data=True,
+    # license='BSD License',  # example license
+    description='A simple Django app to conduct Web-based polls.',
+    long_description=README,
+    url='https://https://github.com/lizidashen/',
+    author='sinf',
+    author_email='xinguanhua@gmail.com',
+    # packages=['slimoore'],
+    package_dir={'slimoore': 'newpolls'},
+)
