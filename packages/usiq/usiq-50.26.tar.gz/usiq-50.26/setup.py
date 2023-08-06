7459bec51edae04772bf40dc96e3227ba7b7062d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:11 +0000

    Add package usiq/50.26/usiq-50.26.tar.gz

---
+++ packages/usiq/usiq-50.26.tar.gz/usiq-50.26/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools.command.install import install as _install
+
+class install(_install):
+    def pre_install_script(self):
+        pass
+
+    def post_install_script(self):
+        pass
+
+    def run(self):
+        self.pre_install_script()
+
+        _install.run(self)
+
+        self.post_install_script()
+
+if __name__ == '__main__':
+    setup(
+        name = 'usiq',
+        version = '50.26',
+        description = '',
+        long_description = '',
+        author = '',
+        author_email = '',
+        license = '',
+        url = '',
+        scripts = ['scripts/usiq'],
+        packages = ['usiq'],
+        namespace_packages = [],
+        py_modules = [],
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Programming Language :: Python'
+        ],
+        entry_points = {},
+        data_files = [],
+        package_data = {},
+        install_requires = [
+            'docopt',
+            'logbook',
+            'mutagen',
+            'pyyaml'
+        ],
+        dependency_links = [],
+        zip_safe = True,
+        cmdclass = {'install': install},
+        keywords = '',
+        python_requires = '',
+        obsoletes = [],
+    )
---
+++ packages/usiq/usiq-50.26.tar.gz/usiq-50.26/usiq/renamer.py
@@ -0,0 +1,60 @@
+import os
+import string
+import unicodedata
+from . import parser
+
+
+def create_filename(tags, pattern):
+    fields = parser.get_fields(pattern)
+    for field, formatter in fields.items():
+        if formatter:
+            pattern = pattern.replace(
+                '<{}.{}>'.format(field, formatter),
+                format_filename(getattr(tags[field], formatter)()))
+        else:
+            pattern = pattern.replace(
+                '<{}>'.format(field),
+                format_filename(tags[field]))
+    return os.path.expanduser(pattern)
+
+
+def format_filename(filename):
+    filename = remove_accents(filename)
+    filename = filename.replace('[', '(').replace(']', ')')
+    filename = format_quotes(filename)
+    valid_chars = '-_.() {}{}'.format(string.ascii_letters, string.digits)
+    filename = ''.join(c if c in valid_chars else '_' for c in filename)
+    return filename
+
+
+def remove_accents(filename):
+    return (unicodedata.normalize('NFKD', filename)
+            .encode('ascii', 'ignore')
+            .decode('utf-8', 'ignore'))
+
+
+def format_quotes(filename):
+    if '"' not in filename:
+        return filename
+
+    if len([c for c in filename if c == '"']) == 1:
+        return filename.replace('"', 'in')
+
+    output = []
+    last_char = ''
+    open_quote = False
+    for i, c in enumerate(filename):
+        if c == '"':
+            if open_quote:
+                # Assume that we definitely want to close any quote first
+                open_quote = False
+            elif last_char == ' ':
+                # This is almost definitely an opening quote
+                open_quote = True
+            elif last_char.isdigit():
+                # Almost sure to be inches
+                output.append('in')
+        else:
+            output.append(c)
+        last_char = c
+    return ''.join(output)
