307458d83cfc46b931fc244ed3d657e2cf96506f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:39 +0000

    Add package guildai/0.3.1/guildai-0.3.1-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/guildai/guildai-0.3.1-cp36-cp36m-manylinux1_x86_64.whl/guildai-0.3.1.data/purelib/guild/tensorboard.py
@@ -0,0 +1,205 @@
+# Copyright 2017-2018 TensorHub, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+
+import logging
+import os
+import sys
+
+import pkg_resources
+from werkzeug import serving
+
+# Proactively check imports for tensorflow and tensorboard. Check
+# tensorflow first because import problems with tensorboard are likely
+# a result of TensorFlow not being installed.
+
+# pylint: disable=unused-import,wrong-import-order
+
+import tensorflow as tf
+from tensorboard import version
+
+log = logging.getLogger("guild")
+
+# Check tensorboard version against our requirements. Indicate error
+# as ImportError with the current version and unmet requirement as
+# additional arguments.
+
+_req = pkg_resources.Requirement.parse("tensorflow-tensorboard >= 1.5.0")
+
+if version.VERSION not in _req:
+    log.warning(
+        "installed version of tensorboard (%s) does not meet the "
+        "requirement %s", version.VERSION, _req)
+
+# pylint: disable=wrong-import-position
+
+from tensorboard import util as tf_util
+from tensorboard.backend import application
+from tensorboard.plugins.audio import audio_plugin
+from tensorboard.plugins.core import core_plugin
+from tensorboard.plugins.distribution import distributions_plugin
+from tensorboard.plugins.graph import graphs_plugin
+from tensorboard.plugins.histogram import histograms_plugin
+from tensorboard.plugins.image import images_plugin
+from tensorboard.plugins.profile import profile_plugin
+from tensorboard.plugins.projector import projector_plugin
+from tensorboard.plugins.scalar import scalars_plugin
+from tensorboard.plugins.text import text_plugin
+
+from guild import util
+
+DEFAULT_RELOAD_INTERVAL = 5
+MIN_MONITOR_INTERVAL = 5
+
+class RunsMonitor(util.LoopingThread):
+
+    STOP_TIMEOUT = 5
+
+    def __init__(self, list_runs_cb, logdir, interval):
+        """Create a RunsMonitor.
+
+        Note that run links are created initially by this
+        function. Any errors result from user input will propagate
+        during this call. Similar errors occuring after the monitor is
+        started will be logged but will not propagate.
+        """
+        interval = min(interval, MIN_MONITOR_INTERVAL)
+        super(RunsMonitor, self).__init__(
+            cb=self.run_once,
+            interval=interval,
+            stop_timeout=self.STOP_TIMEOUT)
+        self.logdir = logdir
+        self.list_runs_cb = list_runs_cb
+        self.run_once(exit_on_error=True)
+
+    def run_once(self, exit_on_error=False):
+        log.debug("Refreshing runs")
+        try:
+            runs = self.list_runs_cb()
+        except SystemExit as e:
+            if exit_on_error:
+                raise
+            log.error(
+                "An error occurred while reading runs. "
+                "Use --debug for details.")
+            log.debug(e)
+        else:
+            self._refresh_run_links(runs)
+
+    def _refresh_run_links(self, runs):
+        to_delete = os.listdir(self.logdir)
+        for run in runs:
+            link_name = self._format_run_name(run)
+            link_path = os.path.join(self.logdir, link_name)
+            if not os.path.exists(link_path):
+                log.debug("Linking %s to %s", link_name, run.path)
+                util.symlink(run.path, link_path)
+            try:
+                to_delete.remove(link_name)
+            except ValueError:
+                pass
+        for link_name in to_delete:
+            log.debug("Removing %s", link_name)
+            os.remove(os.path.join(self.logdir, link_name))
+
+    @staticmethod
+    def _format_run_name(run):
+        model = run.opref.model_name
+        op_name = run.opref.op_name
+        started = util.format_timestamp(run.get("started"))
+        if util.PLATFORM == "Windows":
+            fmt = "{short_id} {model} {op_name} {started}"
+            started = started.replace(":", "_")
+        else:
+            fmt = "{short_id} {model}:{op_name} {started}"
+        return fmt.format(
+            short_id=run.short_id,
+            model=model,
+            op_name=op_name,
+            started=started)
+
+def create_app(logdir, reload_interval, path_prefix=""):
+    plugins = [
+        core_plugin.CorePlugin,
+        scalars_plugin.ScalarsPlugin,
+        images_plugin.ImagesPlugin,
+        audio_plugin.AudioPlugin,
+        graphs_plugin.GraphsPlugin,
+        distributions_plugin.DistributionsPlugin,
+        histograms_plugin.HistogramsPlugin,
+        projector_plugin.ProjectorPlugin,
+        text_plugin.TextPlugin,
+        profile_plugin.ProfilePlugin,
+    ]
+    return application.standard_tensorboard_wsgi(
+        logdir=os.path.expanduser(logdir),
+        purge_orphaned_data=False,
+        reload_interval=reload_interval,
+        plugins=plugins,
+        db_uri="",
+        assets_zip_provider=None,
+        path_prefix=path_prefix,
+        window_title="")
+
+def setup_logging():
+    tf_util.setup_logging()
+    _filter_logging()
+
+def _filter_logging():
+    warn0 = tf.logging.warn
+    tf.logging.warn = (
+        lambda *args, **kw: _filter_warn(warn0, *args, **kw)
+    )
+    tf.logging.warning = tf.logging.warn
+
+def _filter_warn(warn0, msg, *args, **kw):
+    skip = [
+        "Found more than one graph event per run",
+        "Found more than one metagraph event per run",
+        "Deleting accumulator",
+    ]
+    for s in skip:
+        if msg.startswith(s):
+            return
+    warn0(msg, *args, **kw)
+
+def run_simple_server(tb_app, host, port, ready_cb):
+    server, _ = make_simple_server(tb_app, host, port)
+    url = util.local_server_url(host, port)
+    sys.stderr.write(
+        "TensorBoard %s at %s (Press CTRL+C to quit)\n"
+        % (version.VERSION, url))
+    sys.stderr.flush()
+    if ready_cb:
+        ready_cb(url)
+    server.serve_forever()
+
+def make_simple_server(app, host, port):
+    server = serving.make_server(host, port, app, threaded=True)
+    server.daemon_threads = True
+    server.handle_error = _handle_error
+    tensorboard_url = "http://%s:%s" % (host, port)
+    return server, tensorboard_url
+
+def _handle_error(request, _client_address):
+    log.exception("HTTP serving error: %s", request)
+
+def serve_forever(logdir, host, port,
+         reload_interval=DEFAULT_RELOAD_INTERVAL,
+         ready_cb=None):
+    app = create_app(logdir, reload_interval)
+    setup_logging()
+    run_simple_server(app, host, port, ready_cb)
---
+++ packages/guildai/guildai-0.3.1-cp36-cp36m-manylinux1_x86_64.whl/guildai-0.3.1.data/purelib/guild/tests/ops.md
@@ -0,0 +1,230 @@
+# Operations
+
+Operation support is implemented by the `op` module:
+
+    >>> import guild.op
+
+For our tests, we'll use a helper function that returns an instance of
+`guild.guildfile.OpDef`:
+
+    >>> import guild.guildfile
+
+    >>> def OpDef(cmd, name="op"):
+    ...     data = [
+    ...       {
+    ...         "model": "model",
+    ...         "operations": {
+    ...           name: {
+    ...             "cmd": cmd
+    ...           }
+    ...         }
+    ...       }
+    ...     ]
+    ...     gf = guild.guildfile.Guildfile(data, "<string>")
+    ...     return gf.models["model"].get_operation(name)
+
+We'll also create a helper function that returns and instance of
+`guild.op.Operation` given arguments to `OpDef` above:
+
+    >>> def Operation(*args, **kw):
+    ...     model = None # not used
+    ...     return guild.op.Operation(model, OpDef(*args, **kw))
+
+Note that the `"test"` argument is an operation reference, which is
+not used in our tests.
+
+## Command specs
+
+Command specs are used to generate Python commands. The first part of
+the spec is used as the Python script or module. It can be a module
+name with or without a py extension.
+
+Here's an operation with a simple "train" cmd:
+
+    >>> op = Operation(cmd="train")
+    >>> op.cmd_args
+    ['...python...', '-um', 'guild.op_main', 'train']
+
+NOTE: The above formatting, with the line feed after '-u' is required
+when running tests in Python 3. The regex that formats unicode refs as
+strings is fooled by the example. We need to break the line as a work
+around.
+
+Command specs may contain additional arguments, which will be included
+in the Python command.
+
+    >>> op = Operation(cmd="train epoch=10 tags='tag1 tag2'")
+    >>> op.cmd_args
+    ['...python...', '-um', 'guild.op_main', 'train', 'epoch=10',
+     'tags=tag1 tag2']
+
+NOTE: The above formatting, with the line feed after '-u' is required
+when running tests in Python 3. The regex that formats unicode refs as
+strings is fooled by the example. We need to break the line as a work
+around.
+
+Command specs cannot be empty:
+
+    >>> Operation(cmd="")
+    Traceback (most recent call last):
+    InvalidCmd
+
+## Flag args
+
+Flags are defined in guildfiles (defaults) and also provided as
+command line arguments to the run command. `_flag_args` returns a list
+of command line arg for a map of flag values.
+
+We'll create a helper function to get the args:
+
+    >>> class FlagDefProxy(object):
+    ...
+    ...     def __init__(self, name):
+    ...         self.name = name
+    ...         self.choices = []
+    ...         self.arg_name = None
+    ...         self.arg_skip = False
+
+    >>> class OpDefProxy(object):
+    ...
+    ...     def __init__(self, flag_vals):
+    ...         self._flag_vals = flag_vals
+    ...
+    ...     def flag_values(self):
+    ...         return self._flag_vals
+    ...
+    ...     def get_flagdef(self, name):
+    ...         return FlagDefProxy(name)
+
+    >>> def flag_args(flag_vals, cmd_args=None):
+    ...     from guild import util
+    ...     resolved_flag_vals = util.resolve_all_refs(flag_vals)
+    ...     cmd_args = cmd_args or []
+    ...     flags, _flag_map = guild.op._flag_args(
+    ...         resolved_flag_vals,
+    ...         OpDefProxy(flag_vals),
+    ...         cmd_args)
+    ...     return flags
+
+Empty flags:
+
+    >>> flag_args({})
+    []
+
+Single flag:
+
+    >>> flag_args({"epochs": 100})
+    ['--epochs', '100']
+
+Multiple flags are returned in sorted order:
+
+    >>> flag_args({"epochs": 100, "data": "my-data"})
+    ['--data', 'my-data', '--epochs', '100']
+
+If a flag value is None, the flag will not be included as an option.
+
+    >>> flag_args({"test": None, "batch-size": 50})
+    ['--batch-size', '50']
+
+If a flag value is True, the flag will be listed a flag option.
+
+    >>> flag_args({"test": True, "batch-size": 50})
+    ['--batch-size', '50', '--test']
+
+The second argument to the `_flag_args` function is a list of command
+arguments. The function uses this list to check for shadowed flags. A
+shadowed flag is a flag that is defined directly in the operation
+`cmd` spec as an option. Guild prevents redefining command options
+with flags.
+
+Consider an operation definition that looks like this:
+
+    operation:
+      train:
+        cmd: train --epochs=100
+
+The cmd arg in this case are:
+
+    >>> cmd_args = ["train", "--epochs=1000"]
+
+Given this cmd, `_flag_args` prevents the `epochs` option from being
+redefined and logs a warning. Let's capture output to verify.
+
+    >>> log_capture = LogCapture()
+    >>> with log_capture:
+    ...   flag_args({"epochs": 100, "batch-size": 50}, cmd_args)
+    ['--batch-size', '50']
+
+    >>> log_capture.print_all()
+    WARNING: ignoring flag 'epochs = 100' because it's shadowed in the operation cmd
+
+Flags args may contain references to flags.
+
+    >>> flag_args({"a": 1, "b": "b-${a}"})
+    ['--a', '1', '--b', 'b-1']
+
+## Command args
+
+Command args are created using `_cmd_args`. These are simply parsed
+parts from a command string. However, like flag values, command args
+may contain references to flag values.
+
+    >>> guild.op._cmd_args([], {})
+    []
+
+    >>> guild.op._cmd_args(["foo", "--bar"], {})
+    ['foo', '--bar']
+
+    >>> guild.op._cmd_args(["foo", "--a=${a}"], {"a": 1})
+    ['foo', '--a=1']
+
+    >>> guild.op._cmd_args(["foo", "--a=${a}"], {"a": "foo-${b}-bar", "b": 2})
+    ['foo', '--a=foo-2-bar']
+
+## Operation flags
+
+Operation flags may be defined in two places:
+
+- Within the operation itself
+- Within the operation model
+
+Flags defined in the operation override flags defined in the model.
+
+For our tests we'll use the train operation:
+
+    >>> opdef = OpDef("train")
+
+We can get the flags defined for this op using the `all_flag_values`
+method:
+
+    >>> opdef.flag_values()
+    {}
+
+Our sample operations aren't initialized with any flags, so we expect
+this list to be empty.
+
+Let's add some flags, starting with the operation model. We'll use the
+`set_flag_value` method:
+
+    >>> opdef.modeldef.set_flag_value("epochs", 100)
+
+And now enumerate flag values for the operation:
+
+    >>> opdef.flag_values()
+    {'epochs': 100}
+
+Let's define the same flag at the operation level:
+
+    >>> opdef.set_flag_value("epochs", 200)
+    >>> opdef.flag_values()
+    {'epochs': 200}
+
+Here are a couple additional flags, one defined in the model and the
+other in the operations:
+
+    >>> opdef.set_flag_value("batch-size", 50)
+    >>> opdef.modeldef.set_flag_value("learning-rate", 0.1)
+    >>> pprint(opdef.flag_values())
+    {'batch-size': 50,
+     'epochs': 200,
+     'learning-rate': 0.1}
