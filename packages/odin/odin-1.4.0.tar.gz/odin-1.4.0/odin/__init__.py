d2110a71d1e09a77cadded48b90ff75dce60bfb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:28 +0000

    Add package odin/1.4.0/odin-1.4.0.tar.gz

---
+++ packages/odin/odin-1.4.0.tar.gz/odin-1.4.0/odin/__init__.py
@@ -0,0 +1,17 @@
+# Disable logging if an explicit handler is not added
+import logging
+logging.getLogger('odin.registration').addHandler(logging.NullHandler())
+
+__authors__ = "Tim Savage"
+__author_email__ = "tim@savage.company"
+__copyright__ = "Copyright (C) 2014 Tim Savage"
+__version__ = "1.0"
+
+from odin.fields import *  # noqa
+from odin.fields.composite import *  # noqa
+from odin.fields.future import *  # noqa
+from odin.fields.virtual import *  # noqa
+from odin.mapping import *  # noqa
+from odin.resources import Resource  # noqa
+from odin.adapters import ResourceAdapter  # noqa
+from odin.proxy import ResourceProxy  # noqa
