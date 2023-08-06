835de5dc8ec81b2555a656bda5df617453d53d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:09 +0000

    Add package youtube-sm/1.0.4/youtube_sm-1.0.4.tar.gz

---
+++ packages/youtube-sm/youtube_sm-1.0.4.tar.gz/youtube_sm-1.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+params = dict()
+params['entry_points'] = {'console_scripts': ['youtube-sm = youtube_sm.commands:main']}
+
+setup(
+	name='youtube_sm',
+	version='1.0.4',
+	author='Sawyerf',
+	author_email='sawyer.flink@protonmail.ch',
+	description='Youtube subscription manager',
+	long_description='Command-line program to analyze yours subscriptions and yours playlists from youtube.com',
+	license='MLP-2.0',
+	packages=find_packages(),
+	classifiers=[
+		'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+		'Environment :: Console',
+		'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+		'Operating System :: Microsoft :: Windows',
+		'Operating System :: POSIX :: Linux',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3 :: Only'
+	],
+	**params
+)
