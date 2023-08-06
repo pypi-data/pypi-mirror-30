33fd3fbfaaf5003f1e2e4e11b9c0fc05d6f14b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:57 +0000

    Add package ssl-scanner/1.1/ssl_scanner-1.1.tar.gz

---
+++ packages/ssl-scanner/ssl_scanner-1.1.tar.gz/ssl_scanner-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ssl_scanner',
+      version='1.1',
+      description='Save it.',
+      url='http://mad.com',
+      author='Flying Circus',
+      author_email='hooman@example.com',
+      license='BPUT',
+      packages=['ssl_scanner'],
+      zip_safe=False)
