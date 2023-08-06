aaca833cc5b0942fceb80538cb5c54a4f2b0719d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:53 +0000

    Add package bigbang/0.1.7/bigbang-0.1.7.tar.gz

---
+++ packages/bigbang/bigbang-0.1.7.tar.gz/bigbang-0.1.7/bigbang/__init__.py
@@ -0,0 +1,9 @@
+from markdown import markdown
+
+
+__version__ = "0.1.7"
+
+def joke():
+    return markdown(u'Wenn ist das Nunst\u00fcck git und Slotermeyer?'
+                    u'Ja! ... **Beiherhund** das Oder die Flipperwaldt '
+                    u'gersput.')
---
+++ packages/bigbang/bigbang-0.1.7.tar.gz/bigbang-0.1.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from bigbang import __version__ as version
+
+
+setup(
+    name='bigbang',
+    version=version,
+    description='Library to create development environment',
+    author='Sergio Fabian Flores Mendoza',
+    author_email='favian.flone92@gmail.com',
+    url='https://github.com/Fabfm4/bigbang-python',
+    license='MIT',
+    packages=["bigbang"],
+    install_requires=[
+        'markdown',
+    ],
+    zip_safe=False,
+)
