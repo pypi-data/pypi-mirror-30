8213d6ac30fe5519967febbc4e4e8a66427d7a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:02 +0000

    Add package dummyPy/0.1/dummyPy-0.1.zip

---
+++ packages/dummyPy/dummyPy-0.1.zip/dummyPy-0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'dummyPy',
+  packages = ['dummyPy'],
+  version = '0.1',
+  description = 'A python module to transform categorical variables to one hot encoded vectors.\
+                 It can handle categorical variables of a dataset that cannot be fit into memory.\
+                 It also works well with the train test framework common in machine learning tasks.',
+  author = 'Yashu Seth',
+  author_email = 'yashuseth2503@gmail.com',
+  url = 'https://github.com/yashu-seth/dummyPy',
+  download_url = 'https://github.com/yashu-seth/dummyPy/archive/0.1.tar.gz',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
