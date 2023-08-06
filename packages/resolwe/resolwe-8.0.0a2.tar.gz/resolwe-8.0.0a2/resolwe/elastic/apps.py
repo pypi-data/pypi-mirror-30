a09f6bdb2b4a8f8fc9db10f893a14c6ce83591d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:14 +0000

    Add package resolwe/8.0.0a2/resolwe-8.0.0a2.tar.gz

---
+++ packages/resolwe/resolwe-8.0.0a2.tar.gz/resolwe-8.0.0a2/resolwe/elastic/apps.py
@@ -0,0 +1,35 @@
+"""Application configuration."""
+import sys
+
+from django.apps import AppConfig
+
+
+class ElasticConfig(AppConfig):
+    """Application configuration."""
+
+    name = 'resolwe.elastic'
+
+    def ready(self):
+        """Perform application initialization."""
+        # Initialize the type extension composer.
+        from . composer import composer
+        composer.discover_extensions()
+
+        is_testing = sys.argv[1:2] == ['test']
+        is_migrating = sys.argv[1:2] == ['migrate']
+        if is_testing or is_migrating:
+            # Do not register signals and ES indices when:
+            # * testing - `index_builder` shouldn't be imported to early
+            #   in tests
+            # * migrating - model instances used during migrations do
+            #   not contain the full functionality of models and things
+            #   like content types don't work correctly and signals are
+            #   not versioned so they are guaranteed to work only with
+            #   the last version of the model
+            return
+
+        # Connect all signals
+        from . import signals  # pylint: disable=unused-variable
+
+        # Register ES indices
+        from . builder import index_builder  # pylint: disable=unused-variable
