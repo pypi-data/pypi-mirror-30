af6389e85f64b7414a3f404807260cf80da21f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:16 +0000

    Add package ICS-VSBIO/0.0.4.4/ICS_VSBIO-0.0.4.4.tar.gz

---
+++ packages/ICS-VSBIO/ICS_VSBIO-0.0.4.4.tar.gz/ICS_VSBIO-0.0.4.4/ICS_VSBIO/VSBIO/include/VSBIO/VSBStruct.h
@@ -0,0 +1,54 @@
+#ifndef _VSBStruct
+#define _VSBStruct
+
+#ifdef _WIN32
+// Visual studio has extremely poor support for C99 pre-2010
+typedef signed char int8_t;
+typedef short int16_t;
+typedef int int32_t;
+typedef __int64 int64_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned __int64 uint64_t;
+#else
+#include <inttypes.h>
+#endif
+
+typedef struct _icsSpyMessageVSB
+{
+	uint32_t StatusBitField;
+	uint32_t StatusBitField2;
+	uint32_t TimeHardware;
+	uint32_t TimeHardware2;
+	uint32_t TimeSystem;
+	uint32_t TimeSystem2;
+	uint8_t TimeStampHardwareID;
+	uint8_t TimeStampSystemID;
+	uint8_t NetworkID;
+	uint8_t NodeID;
+	uint8_t Protocol;
+	uint8_t MessagePieceID;
+	uint8_t ExtraDataPtrEnabled;
+	uint8_t NumberBytesHeader;
+	uint8_t NumberBytesData;
+	uint8_t NetworkID2;
+	int16_t DescriptionID;
+	uint32_t ArbIDOrHeader;
+	uint8_t Data[8];
+	union {
+		struct
+		{
+			uint32_t StatusBitField3;
+			uint32_t StatusBitField4;
+		};
+		uint8_t AckBytes[8];
+	};
+	uint32_t ExtraDataPtr;
+	uint8_t MiscData;
+	uint8_t Reserved[3];
+} icsSpyMessageVSB;
+#define icsSpyMessageVSB_SIZE 64
+
+#endif
