e0d9933ebfb443743d3e7b67166bb59345615bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:04 +0000

    Add package scriptedforms/0.9.1.dev0/scriptedforms-0.9.1.dev0.tar.gz

---
+++ packages/scriptedforms/scriptedforms-0.9.1.dev0.tar.gz/scriptedforms-0.9.1.dev0/scriptedforms/e2e/src/variable-table.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+import pandas as pd
+
+from IPython.display import display, Markdown
+
+display(Markdown(
+    'Loaded variable-table.py at {}'
+    .format(str(datetime.now()))
+))
+
+input_types_1 = {
+    '1st': 'toggle',
+    '3rd': 'number',
+    '4th': 'dropdown',
+    '5th': 'dropdown',
+    '6th': 'readonly'
+}
+
+input_types_2 = {
+    '1st': 'tick',
+    '2nd': 'string',
+    '3rd': 'readonly',
+    '4th': 'readonly',
+    '5th': 'readonly',
+    '6th': 'readonly'
+}
+
+dropdown_items = {
+  '4th': ['apple', 'orange', 'pear'],
+  '5th': {
+    'a': ['sally', 'margaret', 'annita'],
+    'b': ['george', 'philip', 'simpson'],
+    'c': ['red', 'green', 'blue']
+  }
+}
+
+table = None
+
+
+def load_table():
+    global table
+    table = pd.read_csv('variable-table.csv', index_col=0)
+    display(Markdown(
+        'Loaded variable-table.csv at {}'
+        .format(str(datetime.now()))
+    ))
