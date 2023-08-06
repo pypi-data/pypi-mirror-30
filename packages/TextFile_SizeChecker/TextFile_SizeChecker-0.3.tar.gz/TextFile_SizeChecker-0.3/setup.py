e4980d3b3a985f273783139f98b90c4ff8df2730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:39 +0000

    Add package TextFile_SizeChecker/0.3/TextFile_SizeChecker-0.3.tar.gz

---
+++ packages/TextFile_SizeChecker/TextFile_SizeChecker-0.3.tar.gz/TextFile_SizeChecker-0.3/setup.py
@@ -0,0 +1,40 @@
+"""
+TextFileSizeChecker
+-------------
+
+Simple script for calculating the number of lines, words and
+characters in a text file.
+
+You can get it by downloading it directly or by typing:
+
+.. code:: bash
+
+    $ pip install TextFile_SizeChecker
+
+After it is installed you can start it by simply typing in your terminal:
+
+.. code:: bash
+
+    $ size-checker 'name_of_txt_file_to_read'
+
+"""
+
+
+from setuptools import setup
+
+setup(name='TextFile_SizeChecker',
+      version='0.3',
+      description='Calculate the number of lines, words and characters in a text file',
+      long_description=__doc__,
+      long_description_content_type='text/markdown',
+      url="https://github.com/urosjevremovic/txt-file-size-checker",
+      license='MIT',
+      author='Uros Jevremovic',
+      author_email='jevremovic.uros91@gmail.com',
+      packages=['TextFileSizeChecker'],
+      entry_points={
+          "console_scripts": ["size-checker=TextFileSizeChecker.txt_file_size_checker:main"],
+      },
+      )
+
+__author__ = 'Uros Jevremovic'
