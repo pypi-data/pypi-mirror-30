1afc1504216f9c0a7fd12b2fda9142d96d06117d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:07 +0000

    Add package timeunit/1.1.0/timeunit-1.1.0.tar.gz

---
+++ packages/timeunit/timeunit-1.1.0.tar.gz/timeunit-1.1.0/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+version = '1.1.0'
+
+setup(name='timeunit',
+      version=version,
+      description="timeunit",
+      long_description='',
+      keywords='timeunit',
+      author='Ralph-Wang',
+      author_email='ralph.wang1024@gmail.com',
+      url='https://github.com/Ralph-Wang/timeunit-python',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      py_modules=['timeunit'],
+      zip_safe=False,
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
