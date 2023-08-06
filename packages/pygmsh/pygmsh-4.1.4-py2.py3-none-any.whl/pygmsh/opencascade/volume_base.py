76e9fa14e240aa9f14f5d8b865b20a6e46dfd01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:46 +0000

    Add package pygmsh/4.1.4/pygmsh-4.1.4-py2.py3-none-any.whl

---
+++ packages/pygmsh/pygmsh-4.1.4-py2.py3-none-any.whl/pygmsh/opencascade/volume_base.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+
+
+class VolumeBase(object):
+    _ID = 0
+    dimension = 3
+
+    def __init__(self, is_list=False, id0=None):
+        isinstance(id0, str)
+        self.is_list = is_list
+        if id0:
+            self.id = id0
+        else:
+            self.id = 'v{}'.format(VolumeBase._ID)
+            VolumeBase._ID += 1
+        if is_list:
+            self.id += '[]'
+        return
+
+    def char_length_code(self, char_length):
+        if char_length is None:
+            return []
+
+        return [
+            'pts_{}[] = PointsOf{{Volume{{{}}};}};'.format(
+                self.id, self.id
+                ),
+            'Characteristic Length{{pts_{}[]}} = {};'.format(
+                self.id, char_length
+                ),
+            ]
