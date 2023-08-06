6556d8ef48f35c3bc4461093bdeb80bae2e16db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:31 +0000

    Add package lidssdk/0.0.2/lidssdk-0.0.2.tar.gz

---
+++ packages/lidssdk/lidssdk-0.0.2.tar.gz/lidssdk-0.0.2/lidssdk/help.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import sys
+
+def output():
+    print("Hello world")
+
+
+def main():
+    output()
+    print(sys.aegv[1:])
+
+if __name__ == '__main__':
+    main()
