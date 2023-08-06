0e823b2dab9059160a65f86fa505a4cd2323ec94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:31 +0000

    Add package deos/0.1/deos-0.1.tar.gz

---
+++ packages/deos/deos-0.1.tar.gz/deos-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='deos',
+      version='0.1',
+      description='decentralized operating system',
+      url='https://github.com/desantis/python-deos',
+      author='Andrew T. DeSantis',
+      author_email='atd@deos.org',
+      license='MIT',
+      packages=['deos'],
+      zip_safe=False)
