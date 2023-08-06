eee783291af08fcc24c30bf1a315ca8dfdb59d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:39 +0000

    Add package socketio/0.0.2/socketio-0.0.2.tar.gz

---
+++ packages/socketio/socketio-0.0.2.tar.gz/socketio-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+import socketio
+
+setup(
+    name='socketio',
+    version=socketio.__version__,
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    install_requires=[
+        'panoramisk==1.1',
+        'setuptools==3.3',
+        'netifaces==0.10.6'
+    ],
+    author="EvgrDan",
+    url="http://zvonobot.ru"
+)
