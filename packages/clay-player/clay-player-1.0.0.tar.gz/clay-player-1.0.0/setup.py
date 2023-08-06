2e23fcce1297d4418ea6a91d1fdb973179f4a7db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:51 +0000

    Add package clay-player/1.0.0/clay-player-1.0.0.tar.gz

---
+++ packages/clay-player/clay-player-1.0.0.tar.gz/clay-player-1.0.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from clay.meta import VERSION
+
+setup(
+    name='clay-player',
+    version=VERSION,
+    description='Command Line Player for Google Play Music',
+    author='Andrew Dunai',
+    author_email='a@dun.ai',
+    url='https://github.com/and3rson/clay',
+    install_requires=[
+        'gmusicapi',
+        'PyYAML',
+        'urwid',
+        'codename'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'clay=clay.app:main'
+        ]
+    },
+    package_data={
+        'clay': ['config.yaml', 'colours.yaml'],
+    },
+)
