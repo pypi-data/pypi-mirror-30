f50f2045839e6664be2a6af602de14eab0f5a0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:17 +0000

    Add package biopandas/0.2.3/biopandas-0.2.3.tar.gz

---
+++ packages/biopandas/biopandas-0.2.3.tar.gz/biopandas-0.2.3/biopandas/__init__.py
@@ -0,0 +1,8 @@
+# BioPandas
+# Author: Sebastian Raschka <mail@sebastianraschka.com>
+# License: BSD 3 clause
+# Project Website: http://rasbt.github.io/biopandas/
+# Code Repository: https://github.com/rasbt/biopandas
+
+__version__ = '0.2.3'
+__author__ = "Sebastian Raschka <mail@sebastianraschka.com>"
