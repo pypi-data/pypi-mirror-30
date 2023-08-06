6bd6c418f14f1b6403d2c75c208a22f8a7efab2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:26 +0000

    Add package mdt/0.11.1/mdt-0.11.1.tar.gz

---
+++ packages/mdt/mdt-0.11.1.tar.gz/mdt-0.11.1/mdt/data/components/standard/compartment_models/CylinderGPD.py
@@ -0,0 +1,23 @@
+from mdt.component_templates.compartment_models import CompartmentTemplate
+
+__author__ = 'Robbert Harms'
+__date__ = "2015-06-21"
+__maintainer__ = "Robbert Harms"
+__email__ = "robbert.harms@maastrichtuniversity.nl"
+
+
+class CylinderGPD(CompartmentTemplate):
+
+    parameters = ('g', 'G', 'Delta', 'delta', 'd', 'theta', 'phi', 'R')
+    dependencies = ('MRIConstants',
+                    'NeumannCylindricalRestrictedSignal',
+                    'SphericalToCartesian')
+    cl_code = '''
+        mot_float_type b = pown(GAMMA_H * delta * G, 2) * (Delta - (delta/3.0));
+
+        mot_float_type lperp = NeumannCylindricalRestrictedSignal(Delta, delta, d, R, G);
+
+        mot_float_type gn2 = pown(dot(g, SphericalToCartesian(theta, phi)), 2);
+
+        return exp( ((1 - gn2) * lperp) + (-b * d * gn2));
+    '''
