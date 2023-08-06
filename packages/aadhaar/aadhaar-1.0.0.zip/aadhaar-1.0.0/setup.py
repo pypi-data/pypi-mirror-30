01b062d124fe5a54811baa5803c1afc494f17340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:43 +0000

    Add package aadhaar/1.0.0/aadhaar-1.0.0.zip

---
+++ packages/aadhaar/aadhaar-1.0.0.zip/aadhaar-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='aadhaar',
+    version='1.0.0',
+    packages=['aadhaar'],
+    url='https://github.com/pupattan/aadhaar.git',
+    license='MIT',
+    author='pulakpattanayak',
+    author_email='pulak.pattanayak@gmail.com',
+    description='Api for aadhaar'
+)
