073932bf038a06f1fa7283cf9ed04ee1272821b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:29 +0000

    Add package blurr-dev/0.415/blurr-dev-0.415.tar.gz

---
+++ packages/blurr-dev/blurr-dev-0.415.tar.gz/blurr-dev-0.415/blurr/cli/cli.py
@@ -0,0 +1,18 @@
+from typing import Dict, Any
+
+from blurr.cli.out import Out
+from blurr.cli.transform import transform
+from blurr.cli.validate import validate_command
+
+
+def cli(arguments: Dict[str, Any], out: Out) -> int:
+    if arguments['validate']:
+        return validate_command(arguments['<DTC>'], out)
+    elif arguments['transform']:
+        source = []
+        if arguments['--source'] is not None:
+            source = arguments['--source'].split(',')
+        elif arguments['<raw-json-files>'] is not None:
+            source = arguments['<raw-json-files>'].split(',')
+        return transform(arguments['--streaming-dtc'],
+                         arguments['--window-dtc'], source, out)
