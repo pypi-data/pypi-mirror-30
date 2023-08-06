1f2a90da301095f458594eaa1790c895914d7113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:25 +0000

    Add package aiohttp_cas/0.2.2/aiohttp_cas-0.2.2.tar.gz

---
+++ packages/aiohttp_cas/aiohttp_cas-0.2.2.tar.gz/aiohttp_cas-0.2.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='aiohttp_cas',
+      version='0.2.2',
+      description="CAS 1/2/3 authentication middleware for aiohttp.web",
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Topic :: Internet :: WWW/HTTP'
+                  ],
+       author='G. Ryan Sablosky',
+       author_email='gsablosky@bard.edu',
+       url='https://github.com/bard-it/aiohttp_cas/',
+       download_url='https://github.com/bard-it/aiohttp_cas/archive/0.2.2.tar.gz',
+       license='MIT',
+       packages=['aiohttp_cas'],
+       install_requires=[
+           'aiohttp>=2,<3.2',
+           'lxml',
+           'aiohttp_session',],
+       include_package_data=True)
