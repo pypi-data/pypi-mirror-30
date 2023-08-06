d5b805082e55714f8b3ed1aa6071cd370bc72912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:28 +0000

    Add package tornado-sqlalchemy/0.4.1/tornado_sqlalchemy-0.4.1.tar.gz

---
+++ packages/tornado-sqlalchemy/tornado_sqlalchemy-0.4.1.tar.gz/tornado_sqlalchemy-0.4.1/setup.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+import codecs
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import setup, Command
+
+
+pwd = os.path.abspath(os.path.dirname(__file__))
+
+
+with codecs.open(os.path.join(pwd, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+def install_requires():
+    assert sys.version_info.major > 2, 'Sorry, Python 2 is not supported'
+
+    return [
+        'tornado >= 4.0',
+        'SQLAlchemy >= 1.0'
+    ]
+
+
+class PublishCommand(Command):
+    """Support setup.py publish.
+
+    https://github.com/kennethreitz/setup.py/blob/master/setup.py
+    """
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.sep.join(('.', 'dist')))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel distribution…')
+        os.system(
+            '{0} setup.py sdist bdist_wheel'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
+
+setup(
+    name='tornado_sqlalchemy',
+    version='0.4.1',
+    description='SQLAlchemy helpers for working in Tornado',
+    long_description=long_description,
+    author='Siddhant Goel',
+    author_email='siddhantgoel@gmail.com',
+    license='MIT',
+    url='https://github.com/siddhantgoel/tornado-sqlalchemy',
+    packages=['tornado_sqlalchemy'],
+    keywords=['tornado', 'sqlalchemy'],
+    install_requires=install_requires(),
+    cmdclass={
+        'publish': PublishCommand
+    }
+)
