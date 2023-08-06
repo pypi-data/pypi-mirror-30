ae94471ba2284126b21c493b143ffc5b658f65bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:24 +0000

    Add package django-ohm2-accounts-light/0.4.5/django-ohm2-accounts-light-0.4.5.tar.gz

---
+++ packages/django-ohm2-accounts-light/django-ohm2-accounts-light-0.4.5.tar.gz/django-ohm2-accounts-light-0.4.5/ohm2_accounts_light/errors.py
@@ -0,0 +1,12 @@
+from django.utils.translation import ugettext as _
+
+BASE_ERROR_CODE = 245696
+
+NO_PASSWORD_RESET_FOUND = {
+	"code" : BASE_ERROR_CODE | 1,
+	"message" : _("No password reset found"),
+}
+THE_PASSWORD_IS_NOT_SECURE = {
+	"code" : BASE_ERROR_CODE | 2,
+	"message" : _("The password is not secure"),
+}
