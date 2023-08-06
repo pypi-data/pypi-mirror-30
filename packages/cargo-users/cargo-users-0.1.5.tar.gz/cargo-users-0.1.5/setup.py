6d41c75a220401991b5d7465b17f553a8daa9814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:36 +0000

    Add package cargo-users/0.1.5/cargo-users-0.1.5.tar.gz

---
+++ packages/cargo-users/cargo-users-0.1.5.tar.gz/cargo-users-0.1.5/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3 -S
+import os
+import uuid
+from setuptools import setup
+from pip.req import parse_requirements
+from pkgutil import walk_packages
+
+
+PKG = 'cargo_users'
+PKG_NAME = 'cargo-users'
+PKG_VERSION = '0.1.5'
+
+pathname = os.path.dirname(os.path.realpath(__file__))
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements(pathname + "/requirements.txt",
+                                  session=uuid.uuid1())
+
+
+def find_packages(prefix=""):
+    path = [prefix]
+    yield prefix
+    prefix = prefix + "."
+    for _, name, ispkg in walk_packages(path, prefix):
+        if ispkg:
+            yield name
+
+
+setup(
+    name=PKG_NAME,
+    version=PKG_VERSION,
+    description='User models and controllers based on the Cargo ORM.',
+    author='Jared Lunde',
+    author_email='jared.lunde@gmail.com',
+    url='https://github.com/jaredlunde/cargo-users',
+    license="MIT",
+    install_requires=[str(ir.req) for ir in install_reqs],
+    packages=list(find_packages(PKG))
+)
