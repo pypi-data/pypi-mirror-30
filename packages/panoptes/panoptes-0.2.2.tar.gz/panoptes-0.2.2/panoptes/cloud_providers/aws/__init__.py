1f3e9e491b635ee683a26ba57aa5c2c564140969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:50 +0000

    Add package panoptes/0.2.2/panoptes-0.2.2.tar.gz

---
+++ packages/panoptes/panoptes-0.2.2.tar.gz/panoptes-0.2.2/panoptes/cloud_providers/aws/__init__.py
@@ -0,0 +1,10 @@
+""" Panoptes - Cloud Providers - AWS
+
+Responsible to import analysis functions from AWS.
+"""
+
+from panoptes.cloud_providers.aws import aws_panoptes
+
+
+if __name__ == "__main__":
+    pass
