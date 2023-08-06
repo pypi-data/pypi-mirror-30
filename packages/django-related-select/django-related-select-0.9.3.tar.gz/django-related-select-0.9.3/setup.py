bbe44e3f4534732b390ead7204119994a2f3174a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:33 +0000

    Add package django-related-select/0.9.3/django-related-select-0.9.3.tar.gz

---
+++ packages/django-related-select/django-related-select-0.9.3.tar.gz/django-related-select-0.9.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='django-related-select',
+    packages=['related_select'],
+    version='0.9.3',
+    description='Class-based View and django form field for related select boxes',
+    author='Zach McCormick',
+    author_email='zachary.tyler.mccormick@gmail.com',
+    url='https://github.com/zachmccormick/django-related-select',
+    keywords=['django', 'related', 'dependent', 'forms'],
+    classifiers=[],
+    install_requires=[
+        'django'
+    ],
+    test_suite='tests',
+    include_package_data=True
+)
