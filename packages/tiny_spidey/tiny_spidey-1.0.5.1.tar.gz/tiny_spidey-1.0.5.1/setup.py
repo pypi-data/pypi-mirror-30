60bc3a8f7e33bc6d2634da8b55921d417955d425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:35 +0000

    Add package tiny_spidey/1.0.5.1/tiny_spidey-1.0.5.1.tar.gz

---
+++ packages/tiny_spidey/tiny_spidey-1.0.5.1.tar.gz/tiny_spidey-1.0.5.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tiny_spidey',
+	  version='1.0.5.1',
+	  description='A really basic web crawler.',
+	  url='https://github.com/simpko/tiny_spidey',
+	  author='Joonho Ko',
+	  author_email='joonhok@mit.edu',
+	  license='MIT',
+	  packages=['tiny_spidey'],
+	  zip_safe=False)
