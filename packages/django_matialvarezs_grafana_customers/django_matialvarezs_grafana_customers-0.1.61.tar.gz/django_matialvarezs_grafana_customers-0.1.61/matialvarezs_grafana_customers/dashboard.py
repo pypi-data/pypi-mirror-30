6fb2824000a9b075db067dd18115c9111005843e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:42 +0000

    Add package django_matialvarezs_grafana_customers/0.1.61/django_matialvarezs_grafana_customers-0.1.61.tar.gz

---
+++ packages/django_matialvarezs_grafana_customers/django_matialvarezs_grafana_customers-0.1.61.tar.gz/django_matialvarezs_grafana_customers-0.1.61/matialvarezs_grafana_customers/dashboard.py
@@ -0,0 +1,10 @@
+import json
+from . import base_dashboard,api
+def create_dashboard(title_dashboard,panels):
+    dashboard = base_dashboard.get_dashboard(title_dashboard,panels)
+    #base['panels'] = panels
+    for panel in panels:
+        dashboard['panels'].append(panel)
+    print("DASHBOARD COMPLETO:   ",dashboard)
+    api.create_dashboard(dashboard)
+    #return base
