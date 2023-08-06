4ec6fc40ddcff31c1500cfed9320fb382d9f05b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:35 +0000

    Add package easyrest/0.1.5/easyrest-0.1.5.tar.gz

---
+++ packages/easyrest/easyrest-0.1.5.tar.gz/easyrest-0.1.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import os
+
+
+setup(name='easyrest',
+      version='0.1.5',
+      description='Wrap python urllib to quickly access Rest API',
+      author='Eric He',
+      author_email='unklhe@gmail.com',
+      url='https://github.com/unklhe/easy-rest',
+      packages=find_packages(),
+      zip_safe=False,
+      license='ASL 2.0',
+      install_requires=[
+      ]
+      )
