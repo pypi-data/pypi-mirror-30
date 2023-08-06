7ea04a0fc2196414170d68095d81c7aa26271cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:43 +0000

    Add package django-docker-engine/0.0.49/django_docker_engine-0.0.49.tar.gz

---
+++ packages/django-docker-engine/django_docker_engine-0.0.49.tar.gz/django_docker_engine-0.0.49/django_docker_engine/container_managers/base.py
@@ -0,0 +1,33 @@
+import abc
+import sys
+
+"""
+This provides an interface which satisfies both local and remote Docker use.
+The methods are based on a subset of those provided by
+https://docker-py.readthedocs.io/en/stable/containers.html
+and implementers should return the same kind of results as the the SDK returns.
+
+The plan is to write a wrapper for AWS ECS which will conform to the same
+interface.
+"""
+
+
+if sys.version_info >= (3, 4):
+    ABC = abc.ABC
+else:
+    ABC = abc.ABCMeta('ABC', (), {})
+
+
+class BaseManager(ABC):
+
+    @abc.abstractmethod
+    def run(self, image_name, cmd, **kwargs):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def get_url(self, container_name):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def list(self, filters={}):
+        raise NotImplementedError()
