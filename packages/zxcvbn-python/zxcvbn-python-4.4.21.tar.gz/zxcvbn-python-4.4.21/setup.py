4fa2f7c0955fa1df68c3c16da9afea3750b49244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:35 +0000

    Add package zxcvbn-python/4.4.21/zxcvbn-python-4.4.21.tar.gz

---
+++ packages/zxcvbn-python/zxcvbn-python-4.4.21.tar.gz/zxcvbn-python-4.4.21/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name='zxcvbn-python',
+    version='4.4.21',
+    packages=['zxcvbn'],
+    url='https://github.com/dwolfhub/zxcvbn-python',
+    download_url='https://github.com/dwolfhub/zxcvbn-python/tarball/v4.4.21',
+    license='MIT',
+    author='Daniel Wolf',
+    author_email='danielrwolf5@gmail.com',
+    description=long_description,
+    long_description=long_description,
+    keywords=['zxcvbn', 'password', 'security'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
