6b7a5296d6839e39981ef140f6fbee628020fd21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:02 +0000

    Add package asit/0.4/asit-0.4.tar.gz

---
+++ packages/asit/asit-0.4.tar.gz/asit-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = 'asit',
+    version = '0.4',
+    description = 'Advanced System Information Toolbox Python Library',
+    author = 'Ali Gholami', 
+    author_email = 'aligholami7596@gmail.com',
+    url = 'https://github.com/aligholamee/asit',
+    keywords = 'SYSTEM INFO CPU MEMORY OS PYTHON',
+    platforms = ["any"],
+    install_requires = [
+        'codecov'
+    ],
+    python_requires = '>3.5',
+    license = 'MIT'
+)
