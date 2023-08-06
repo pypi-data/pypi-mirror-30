d03823c0369ac81ad829740c4e7af0b0a05abc64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:40 +0000

    Add package django-ohm2-currencies-light/0.2.1/django-ohm2-currencies-light-0.2.1.tar.gz

---
+++ packages/django-ohm2-currencies-light/django-ohm2-currencies-light-0.2.1.tar.gz/django-ohm2-currencies-light-0.2.1/ohm2_currencies_light/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from . import models as ohm2_currencies_light_models
+from . import settings
+
+
+
+#class Serializer(serializers.ModelSerializer):
+#	class Meta:
+#		model = models.Model
+#		fields = (
+#			'identity',
+#			'created',
+#			'last_update',
+#		)
