d1b4a53dfd73eed2f65d272c886e6d7169ae8f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:04 +0000

    Add package sqlalchemy-seeder/0.3.0/sqlalchemy-seeder-0.3.0.tar.gz

---
+++ packages/sqlalchemy-seeder/sqlalchemy-seeder-0.3.0.tar.gz/sqlalchemy-seeder-0.3.0/README.md
@@ -0,0 +1,24 @@
+# sqlalchemy-seeder
+Seed SQLAlchemy database with a simple data format. Supports references to other entities (and their fields)
+that are defined alongside it or persisted in the database.
+
+## Requirements & Installation
+
+Runs on Python 2.7 or Python 3
+
+#### Dependencies
+* sqlalchemy
+* jsonschema
+* pyyaml
+    
+#### Installation
+`pip install sqlalchemy-seeder`
+
+## Documentation
+    
+http://sqlalchemy-seeder.readthedocs.io/en/latest/
+
+## Issues
+Submit issues on [Github Issues](https://github.com/RiceKab/sqlalchemy-jsonseeder/issues).
+## License
+MIT License
---
+++ packages/sqlalchemy-seeder/sqlalchemy-seeder-0.3.0.tar.gz/sqlalchemy-seeder-0.3.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sqlalchemy-seeder',
+    version='0.3.0',
+    packages=find_packages(exclude=["tests"]),
+    package_data={"sqlalchemyseeder": ["resources/*"]},
+    url='https://github.com/RiceKab/sqlalchemy-seeder',
+    license='MIT',
+    author='Kevin CY Tang',
+    author_email='kevin@cyborn.be',
+    keywords='sqlalchemy json yaml seed',
+    description="Tool for creating (and persisting) SQLAlchemy entities from a simple data format.",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ],
+    install_requires=['SQLAlchemy', 'jsonschema', 'pyyaml'],
+    test_requires=["pytest"],
+    python_requires='>=2.7'
+)
