eb4ddf6292a5cea7117afe0f46ff143beff9dcfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:20 +0000

    Add package asfpy/0.1/asfpy-0.1.tar.gz

---
+++ packages/asfpy/asfpy-0.1.tar.gz/asfpy-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='asfpy',
+      version='0.1',
+      description='ASF Common Python Methods',
+      url='https://svn.apache.org/repos/infra/infrastructure/trunk/projects/asfpy/',
+      author='ASF Infrastructure',
+      author_email='root@apache.org',
+      license='Apache',
+      packages=['asfpy'],
+      zip_safe=False)
+      
