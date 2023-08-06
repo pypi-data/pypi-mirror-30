0e451e34a3dc50598b5c7b7a7a3a314e15b6d9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:51 +0000

    Add package abodepy/0.13.0a1/abodepy-0.13.0a1-py3-none-any.whl

---
+++ packages/abodepy/abodepy-0.13.0a1-py3-none-any.whl/abodepy/devices/light.py
@@ -0,0 +1,38 @@
+"""Abode switch device."""
+
+from abodepy.devices.switch import AbodeSwitch
+import abodepy.helpers.constants as CONST
+
+
+class AbodeLight(AbodeSwitch):
+    """Class for lights (dimmers)."""
+
+    def set_color(self, _color):
+        """Set the color of the light."""
+        # Not implemented
+        return self.has_color
+
+    @property
+    def brightness(self):
+        """Get light brightness."""
+        return self._json_state.get(CONST.BRIGHTNESS_KEY)
+
+    @property
+    def color(self):
+        """Get light color."""
+        return self._json_state.get(CONST.COLOR_KEY)
+
+    @property
+    def has_brightness(self):
+        """Device has brightness."""
+        return self.brightness
+
+    @property
+    def has_color(self):
+        """Device has color."""
+        return self.color is True
+
+    @property
+    def is_dimmable(self):
+        """Device is dimmable."""
+        return 'Dimmer' in self._type
