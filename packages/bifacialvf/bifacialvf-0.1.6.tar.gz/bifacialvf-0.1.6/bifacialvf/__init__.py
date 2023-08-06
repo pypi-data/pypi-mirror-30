6b29064c9ee15a15fd85e0219fa57b571a088e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:27 +0000

    Add package bifacialvf/0.1.6/bifacialvf-0.1.6.tar.gz

---
+++ packages/bifacialvf/bifacialvf-0.1.6.tar.gz/bifacialvf-0.1.6/bifacialvf/__init__.py
@@ -0,0 +1,6 @@
+from bifacialvf import simulate  # main program
+from vf import getBackSurfaceIrradiances, getFrontSurfaceIrradiances, getGroundShadeFactors  # main subroutines
+from vf import getSkyConfigurationFactors, trackingBFvaluescalculator, rowSpacing # helper functions
+from sun import hrSolarPos, perezComp, solarPos, sunIncident # solar position and value
+from loadVFresults import loadVFresults # utility for reading result files
+from readepw import readepw
