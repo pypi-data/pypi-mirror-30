60b4e9be407c87e58bdacef627668289da29a8ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:48 +0000

    Add package soba/2.2/soba-2.2-py3-none-any.whl

---
+++ packages/soba/soba-2.2-py3-none-any.whl/soba/launchers/visual.py
@@ -0,0 +1,41 @@
+from soba.visualization.drawModelBack import BackEndVisualization
+from mesa.visualization.ModularVisualization import ModularServer
+import os
+import soba
+import tornado
+
+"""
+In the file visual.py is implemented the execution with visual representation:
+	Methods: 
+		-run: Execute the simulation with visual representation.
+
+"""
+
+def run(model, parameters, visual, back = False):
+	"""
+	Execute the simulation with visual representation.
+		Args:
+			model: Model that is simulated.
+			visualJS: JS files with the visualization elements that are included in the JavaScript browser visualization template.
+			params: Parameters loaded in the models about the agents and anything else.
+	"""
+
+	backEndVisualization = BackEndVisualization(int(parameters['width']), int(parameters['height']), 500, 500, visual)
+
+	path = os.path.abspath(soba.__file__)
+	path = path.rsplit('/', 1)[0]
+
+	local_handler = (r'/local/(.*)', tornado.web.StaticFileHandler,
+                     {"path": path})
+	external_handler = (r'/external/(.*)', tornado.web.StaticFileHandler,
+                     {"path": ""})
+
+	ModularServer.handlers = ModularServer.handlers[:-1]
+	ModularServer.handlers = ModularServer.handlers + [local_handler] + [external_handler]
+	if back != False:
+		server = ModularServer(model, [backEndVisualization, back], name="Simulation", model_params=parameters)
+	else:
+		server = ModularServer(model, [backEndVisualization], name="Simulation", model_params=parameters)
+
+	server.port = 7777
+	server.launch()
