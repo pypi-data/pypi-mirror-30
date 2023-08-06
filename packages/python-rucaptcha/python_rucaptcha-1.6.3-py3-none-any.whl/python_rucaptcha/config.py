bf58ec5034fef4755e28a01407182cc432d488cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:06 +0000

    Add package python-rucaptcha/1.6.3/python_rucaptcha-1.6.3-py3-none-any.whl

---
+++ packages/python-rucaptcha/python_rucaptcha-1.6.3-py3-none-any.whl/python_rucaptcha/config.py
@@ -0,0 +1,10 @@
+# Сервер для отправки данных
+url_request_2captcha = "http://2captcha.com/in.php"
+# Сервер для получения ответа
+url_response_2captcha = "http://2captcha.com/res.php"
+# Сервер для отправки данных
+url_request_rucaptcha = "http://rucaptcha.com/in.php"
+# Сервер для получения ответа
+url_response_rucaptcha = "http://rucaptcha.com/res.php"
+# ключ приложения
+app_key = "1899"
