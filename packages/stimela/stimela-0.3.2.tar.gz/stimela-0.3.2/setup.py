8a65df00e4ab998bdacc8384fa9a546d41891c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:10 +0000

    Add package stimela/0.3.2/stimela-0.3.2.tar.gz

---
+++ packages/stimela/stimela-0.3.2.tar.gz/stimela-0.3.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+
+try:
+  from setuptools import setup
+except ImportError as e:
+  from distutils.core import setup
+
+from stimela_misc import version
+
+setup(name = "stimela",
+    version = version.version,
+    description = "Dockerized radio interferometry scripting framework",
+    author = "Sphesihle Makhathini",
+    author_email = "sphemakh@gmail.com",
+    url = "https://github.com/sphemakh/Stimela",
+    packages = ["stimela","stimela_misc", "stimela/cargo","stimela/utils", "stimela/cargo/cab"],
+    package_data = { "stimela/cargo" : ["cab/*/Dockerfile",
+                                   "base/*/Dockerfile",
+                                   "cab/*/src/*.py",
+                                   "cab/*/src/*.sh",
+                                   "cab/*/src/*.json",
+                                   "cab/*/xvfb.init.d",
+                                   "cab/*/parameters.json",
+                                   "cab/*/src/tdlconf.profiles"]},
+    install_requires = ["pyyaml"],
+    scripts = ["bin/" + i for i in os.listdir("bin")],
+    classifiers = [],
+     )
