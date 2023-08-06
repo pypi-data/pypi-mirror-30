555f04eb1b9ab53be12ad960f8d5c770364cc721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:07 +0000

    Add package aiodisk/0.1.alpha2/aiodisk-0.1.alpha2.tar.gz

---
+++ packages/aiodisk/aiodisk-0.1.alpha2.tar.gz/aiodisk-0.1.alpha2/aiodisk/aiodisk.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+
+import os
+import sys
+from optparse import OptionParser
+
+from .googledrive import GoogleDrive
+
+__version__ = "0.1"
+
+parser = OptionParser()
+parser.add_option("-q", "--quiet",
+                  action="store_false", dest="verbose", default=True,
+                  help="don't print status messages to stdout")
+parser.add_option("-m", "--mime",
+                  action="store", dest="mimeoption", default=False,
+                  help="Mime type search input Ex: image/jpeg for jpg/jpeg files")
+parser.add_option("-n", "--name-search",
+                  action="store", dest="filename", default=False,
+                  help="Search only on file names")
+parser.add_option("-f", "--full-text-search",
+                  action="store", dest="fulltext", default=False,
+                  help="Search in file and file names")
+parser.add_option("-d", "--disk",
+                  action="store", dest="disk", default=False,
+                  help="Select a disk. Avaiable options: 'google-drive, mega'")
+
+(options, args) = parser.parse_args()
+DEFAULT_PAGE_SIZE_GOOGLE = 100
+
+
+def checkGoogleAPI():
+    if os.path.exists("client_secret.json"):
+        return 1
+    else:
+        return 0
+
+
+def main():
+    gDrive = False
+    mega = False
+    if options.disk == "google-drive":
+        # Show Google Drive Panel
+        if not checkGoogleAPI():
+            print("Google client_secret.json not found. Please check your configuration.")
+            main()
+        else:
+            Drive = GoogleDrive()
+            items = Drive.getWebContentLink(options.fulltext)
+            for item in items:
+                print('{0}&name={1}'.format(item['webContentLink'],item['name']))
+
+    elif options.disk == "mega":
+        # Show Mega Panel
+        nothing = 1
+    else:
+        sys.exit(0)
+
+
+
---
+++ packages/aiodisk/aiodisk-0.1.alpha2.tar.gz/aiodisk-0.1.alpha2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='aiodisk',
+    packages=['aiodisk'],  # this must be the same as the name above
+    version='0.1.alpha2',
+    description='Access all cloud services in one script',
+    author='Oguz BAKIR',
+    author_email='worms298@gmail.com',
+    url='https://github.com/oguzbakir/AioDisk',  # use the URL to the github repo
+    download_url='https://github.com/oguzbakir/aiodisk/archive/0.1.tar.gz',
+    keywords=['drive', 'mega', 'cloud', 'cloudaio'],  # arbitrary keywords
+    classifiers=['Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.6'],
+)
