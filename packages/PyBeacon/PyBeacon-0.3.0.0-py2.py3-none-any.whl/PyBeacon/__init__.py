db8856a6487ce8d99300d49cb585388f876002d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:44 +0000

    Add package PyBeacon/0.3.0.0/PyBeacon-0.3.0.0-py2.py3-none-any.whl

---
+++ packages/PyBeacon/PyBeacon-0.3.0.0-py2.py3-none-any.whl/PyBeacon/__init__.py
@@ -0,0 +1,6 @@
+"""Module configuration."""
+__title__ = 'PyBeacon'
+__version__ = '0.3.0.0'
+__build__ = 0x016
+__author__ = 'forksociety'
+__license__ = 'GNU Affero General Public License v3 or later (AGPLv3+)'
