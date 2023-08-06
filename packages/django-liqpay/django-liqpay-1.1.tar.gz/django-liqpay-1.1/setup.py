9ca12381d71d2511c05d0cd53d8cd3be6b6343e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:58 +0000

    Add package django-liqpay/1.1/django-liqpay-1.1.tar.gz

---
+++ packages/django-liqpay/django-liqpay-1.1.tar.gz/django-liqpay-1.1/setup.py
@@ -0,0 +1,28 @@
+
+from setuptools import setup, find_packages
+
+
+__version__ = '1.1'
+
+
+with open('requirements.txt') as f:
+    requires = f.read().splitlines()
+
+
+url = 'https://github.com/pmaigutyak/mp-liqpay'
+
+
+setup(
+    name='django-liqpay',
+    version=__version__,
+    description='Django liqpay app',
+    long_description=open('README.md').read(),
+    author='Paul Maigutyak',
+    author_email='pmaigutyak@gmail.com',
+    url=url,
+    download_url='%s/archive/%s.tar.gz' % (url, __version__),
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    install_requires=requires
+)
