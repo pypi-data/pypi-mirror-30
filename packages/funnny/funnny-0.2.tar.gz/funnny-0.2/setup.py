359ce45093474d13d057bd4432ac452b6cf3dca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:24 +0000

    Add package funnny/0.2/funnny-0.2.tar.gz

---
+++ packages/funnny/funnny-0.2.tar.gz/funnny-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funnny',
+      version='0.2',
+      description='The funniest joke in the world',
+      url='http://github.com/jeremyzhang2008/funniestjoke',
+      author='Jeremy Zhang',
+      author_email='jeremy.zhangzq@gmail.com',
+      license='MIT',
+      packages=['funnny'],
+      zip_safe=False)
