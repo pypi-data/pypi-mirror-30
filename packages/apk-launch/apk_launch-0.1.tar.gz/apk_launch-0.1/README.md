144bc90f550153b650f5e8ff244ea6ca4bc2511e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:16 +0000

    Add package apk-launch/0.1/apk_launch-0.1.tar.gz

---
+++ packages/apk-launch/apk_launch-0.1.tar.gz/apk_launch-0.1/README.md
@@ -0,0 +1,24 @@
+# apk_launch
+
+Tools to perform various tasks on Android projects from the command line
+
+## antbuilder.py
+TODO: Add script info 
+
+## apk_analyzer.py
+TODO: Add script info 
+
+## apk_launch.py
+TODO: Add script info 
+
+## apk_stats.py
+TODO: Add script info 
+
+## apk_utils.py
+TODO: Add script info 
+
+## copy_aosp_apks.py
+TODO: Add script info 
+
+## mycommand.py
+TODO: Add script info 
---
+++ packages/apk-launch/apk_launch-0.1.tar.gz/apk_launch-0.1/src/apk_stats/__main__.py
@@ -0,0 +1,23 @@
+"""
+Entrypoint module, in case you use `python -mapk_stats`.
+
+    Copyright (C) 2018 Zach Yannes
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+# pylint: disable=relative-import
+import apk_stats
+
+if __name__ == '__main__':
+    apk_stats.main()
