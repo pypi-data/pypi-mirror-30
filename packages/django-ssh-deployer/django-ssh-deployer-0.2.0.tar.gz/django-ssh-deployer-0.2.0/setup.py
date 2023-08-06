949d8f4e80a19b828f71f81450d22b588481a575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:02 +0000

    Add package django-ssh-deployer/0.2.0/django-ssh-deployer-0.2.0.tar.gz

---
+++ packages/django-ssh-deployer/django-ssh-deployer-0.2.0.tar.gz/django-ssh-deployer-0.2.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+setup(
+    name='django-ssh-deployer',
+    version="0.2.0",
+    description='This package provides Django management commands to deploy your site over SSH via Paramiko.',
+    long_description='This package provides Django management commands to deploy your site over SSH via Paramiko.',
+    author='Tim Allen',
+    author_email='tim@pyphilly.org',
+    url='https://github.com/FlipperPA/django-ssh-deployer',
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'Django',
+        'paramiko',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Framework :: Django',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
