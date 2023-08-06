ba24974b1924eb5b545242e849dbfaae0c61b364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:27 +0000

    Add package roro_ioc/0.1.8/roro_ioc-0.1.8.tar.gz

---
+++ packages/roro_ioc/roro_ioc-0.1.8.tar.gz/roro_ioc-0.1.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+def readfile(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+readme = readfile('README.md')
+
+setup(
+    name='roro_ioc',
+    packages=['roro_ioc'],
+    version='0.1.8',
+    description='IOC Injection for python',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Twiggle',
+    author_email='oren@twiggle.com',
+    url='https://github.com/twgOren/roroioc',
+    keywords=['ioc'],
+    classifiers=[],
+    install_requires=[
+        'attrs',
+        'typing',
+        'cached_property',
+        'werkzeug',
+    ]
+)
