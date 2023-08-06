301b140d71b5c9ceb25625f9d295ab766bf1e77b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:18 +0000

    Add package appsecrets/0.1/appsecrets-0.1.tar.gz

---
+++ packages/appsecrets/appsecrets-0.1.tar.gz/appsecrets-0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+description = """
+See `github repo <https://github.com/pior/appsecrets>`_ for information.
+"""
+
+
+setup(
+    name='appsecrets',
+    version='0.1',
+    description='Manage your application secrets (with Google Cloud KMS)',
+    long_description=description,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords='secrets kms crypto',
+    author="Pior Bastida",
+    author_email="pior@pbastida.net",
+    url="https://github.com/pior/appsecrets",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['google-api-python-client'],
+    entry_points={
+        'console_scripts': ['appsecrets = appsecrets.cli:main'],
+    },
+)
