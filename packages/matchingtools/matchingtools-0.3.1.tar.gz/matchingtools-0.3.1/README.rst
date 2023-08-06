c413df98446a486ab1dcbd2a802aa8ccf0d542e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:35 +0000

    Add package matchingtools/0.3.1/matchingtools-0.3.1.tar.gz

---
+++ packages/matchingtools/matchingtools-0.3.1.tar.gz/matchingtools-0.3.1/README.rst
@@ -0,0 +1,36 @@
+MatchingTools is a Python library for doing symbolic calculations in
+effective field theory.
+
+It provides the tools to create a Lagrangian and integrate out heavy
+fields at the tree level. It also includes functions for applying
+customizable transformations (for example, Fierz identities or
+simplification using equations of motion of the light fields) to the
+effective Lagrangian to simplify it or write it in terms of a chosen
+effective operator basis.
+
+Installation
+============
+
+To install MatchingTools using `pip`_ do::
+
+  pip install matchingtools
+
+The source can be downloaded from the `GitHub repository`_.
+To install MatchingTools from a local copy of the project open a terminal
+in the top level directory and type::
+
+  python setup.py install
+
+.. _pip: https://pypi.python.org/pypi/pip/
+
+.. _GitHub repository: https://github.com/jccriado/matchingtools
+  
+Documentation
+=============
+
+Read the documentation at: http://matchingtools.readthedocs.io/en/latest/.
+
+Citation
+========
+
+If you use this work, please cite: https://arxiv.org/abs/1710.06445.
---
+++ packages/matchingtools/matchingtools-0.3.1.tar.gz/matchingtools-0.3.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='matchingtools',
+
+    version='0.3.1',
+
+    description='A library for symbolic Effective Field Theory calculations',
+    long_description=long_description,
+
+    url='https://github.com/jccriado/matchingtools',
+
+    author='Juan Carlos Criado Alamo',
+    author_email='jccriadoalamo@ugr.es',
+
+    license='MIT',
+
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Physics'],
+
+    keywords=['effective field theory symbolic tree matching integration'],
+
+    packages=['matchingtools', 'matchingtools.extras']
+)
