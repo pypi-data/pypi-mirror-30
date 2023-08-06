8d684970d33c928f6886716b58e8b9ace34c13a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:23 +0000

    Add package mrivis/0.2.8/mrivis-0.2.8.tar.gz

---
+++ packages/mrivis/mrivis-0.2.8.tar.gz/mrivis-0.2.8/mrivis/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for mrivis."""
+
+__author__ = """Pradeep Reddy Raamana"""
+__email__ = 'raamana@gmail.com'
+
+__all__ = ['checkerboard', 'color_mix', 'voxelwise_diff', 'collage', 'aseg_on_mri', 'color_maps']
+
+from sys import version_info
+if version_info.major > 2:
+    from mrivis.mrivis import checkerboard, color_mix, voxelwise_diff, collage, aseg_on_mri
+    from mrivis import color_maps
+else:
+    # from .mrivis import checkerboard
+    raise NotImplementedError('mrivis requires Python 3+.')
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
---
+++ packages/mrivis/mrivis-0.2.8.tar.gz/mrivis-0.2.8/tests/test_mrivis.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `mrivis` package."""
+
+import pytest
+
+from mrivis import mrivis
