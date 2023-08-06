1e76fdfc713d76e83525ca4a9ee715323c95eb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:08 +0000

    Add package onegov.winterthur/0.1.6/onegov.winterthur-0.1.6.tar.gz

---
+++ packages/onegov.winterthur/onegov.winterthur-0.1.6.tar.gz/onegov.winterthur-0.1.6/onegov/winterthur/models/address.py
@@ -0,0 +1,48 @@
+from onegov.core.orm import Base
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import Text
+
+
+class WinterthurAddress(Base):
+
+    __tablename__ = 'winterthur_addresses'
+
+    #: the adress id
+    id = Column(Integer, nullable=False, primary_key=True)
+
+    #: the street
+    street_id = Column(Integer, nullable=False)
+    street = Column(Text, nullable=False)
+
+    #: the house
+    house_number = Column(Integer, nullable=False)
+    house_extra = Column(Text, nullable=True)
+
+    #: the place
+    zipcode = Column(Integer, nullable=False)
+    zipcode_extra = Column(Integer, nullable=True)
+
+    place = Column(Text, nullable=False)
+
+    #: the district
+    district = Column(Text, nullable=False)
+
+    #: the neighbourhood
+    neighbourhood = Column(Text, nullable=False)
+
+    @classmethod
+    def as_addressless(cls, street_id, street):
+        return cls(
+            street_id=street_id,
+            street=street,
+            house_number=-1,
+            zipcode=-1,
+            place="",
+            district="",
+            neighbourhood="",
+        )
+
+    @property
+    def is_addressless(self):
+        return self.house_number == -1
