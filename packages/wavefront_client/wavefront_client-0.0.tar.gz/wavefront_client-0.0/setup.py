123f1ee89960ab72930b243f92df107bf015a2cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:26 +0000

    Add package wavefront_client/0.0/wavefront_client-0.0.tar.gz

---
+++ packages/wavefront_client/wavefront_client-0.0.tar.gz/wavefront_client-0.0/setup.py
@@ -0,0 +1,20 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='UNKNOWN',
+    author_email='UNKNOWN',
+    description='Wavefront Public API',
+    install_requires=['wavefront-api-client'],
+    long_description='''Use `wavefront-api-client <https://pypi.python.org/pypi/wavefront-api-client/>`_ instead.''',
+    maintainer='UNKNOWN',
+    maintainer_email='UNKNOWN',
+    name='wavefront_client',
+    platforms=['all'],
+    py_modules=['wheel-platform-tag-is-broken-on-empty-wheels-see-issue-141'],
+    url='https://pypi.python.org/pypi/wavefront-api-client/',
+    version="0.0",
+    zip_safe=False,
+)
