b6beb78e2a24e1a3b93dc3ba471ed16e05838b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:12 +0000

    Add package valley/1.5.1/valley-1.5.1.tar.gz

---
+++ packages/valley/valley-1.5.1.tar.gz/valley-1.5.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+
+version = '1.5.1'
+
+LONG_DESCRIPTION = """
+=======================
+Valley
+=======================
+
+Python extensible schema validations and declarative syntax helpers.
+
+
+"""
+
+setup(
+    name='valley',
+    version=version,
+    description="""Python extensible schema validations and declarative syntax helpers.""",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Environment :: Web Environment",
+    ],
+    keywords='validations,schema,declarative',
+    author='Brian Jinwright',
+    author_email='opensource@capless.io',
+    maintainer='Brian Jinwright',
+    packages=find_packages(),
+    url='https://github.com/capless/valley',
+    license='GNU GPL V3',
+    install_requires=[str(ir.req) for ir in install_reqs],
+    include_package_data=True,
+)
