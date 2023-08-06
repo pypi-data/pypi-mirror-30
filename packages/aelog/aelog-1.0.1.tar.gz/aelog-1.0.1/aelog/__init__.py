e3584271ef41b5fa88364926b2e21538ad6790d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:00 +0000

    Add package aelog/1.0.1/aelog-1.0.1.tar.gz

---
+++ packages/aelog/aelog-1.0.1.tar.gz/aelog-1.0.1/aelog/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""
+@author: guoyanfeng
+@software: PyCharm
+@time: 18-3-24 下午10:22
+"""
+
+from .log import *
+from .aelog import *
+
+__version__ = "1.0.1"
