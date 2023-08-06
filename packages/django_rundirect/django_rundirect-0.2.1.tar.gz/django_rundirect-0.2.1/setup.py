703a068db939a69d73a73537d7c031dcd7b90e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:01 +0000

    Add package django_rundirect/0.2.1/django_rundirect-0.2.1.tar.gz

---
+++ packages/django_rundirect/django_rundirect-0.2.1.tar.gz/django_rundirect-0.2.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+long_description = ""
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    pass
+
+sdict = {
+    'name': 'django_rundirect',
+    'version': "0.2.1",
+    'license': 'MIT',
+    'packages': ['django_rundirect',
+                 'django_rundirect.management',
+                 'django_rundirect.management.commands'],
+    'package_dir': {'django_rundirect': 'django_rundirect'}, 
+    'zip_safe': False,
+    'install_requires': ['django'],
+    'author': 'Lichun',
+    'long_description': long_description,
+    'url': 'https://github.com/socrateslee/django_rundirect',
+    'classifiers': [
+        'Environment :: Console',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python']
+}
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(**sdict)
