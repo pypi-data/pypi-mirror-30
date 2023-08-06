240203ad85f438ddc26c8ce9e8032453fa055b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:24 +0000

    Add package fwipy/0.1.1/fwipy-0.1.1-py2.py3-none-any.whl

---
+++ packages/fwipy/fwipy-0.1.1-py2.py3-none-any.whl/fwipy/fwi_base.py
@@ -0,0 +1,20 @@
+﻿# -*- coding: utf-8 -*-
+
+import logging
+import requests
+
+from requests.auth import HTTPBasicAuth
+
+class Fwi(object):
+    """Base class for all fwi objects"""
+
+    def __init__(self, **kwargs):
+        self.host = kwargs.get('host', '127.0.0.1')
+        self.debug = kwargs.get('debug')
+        self.port = str(kwargs.get('port', '10561'))
+        self.username = kwargs.get('username')
+        self.password = kwargs.get('password')
+
+    def _make_request(self, payload):
+        resp = requests.get(payload, auth=HTTPBasicAuth(self.username, self.password))
+        return resp
