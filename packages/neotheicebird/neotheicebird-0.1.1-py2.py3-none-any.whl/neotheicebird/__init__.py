d73d0177a08f64e6855e281a8677ae331a07525e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:05 +0000

    Add package neotheicebird/0.1.1/neotheicebird-0.1.1-py2.py3-none-any.whl

---
+++ packages/neotheicebird/neotheicebird-0.1.1-py2.py3-none-any.whl/neotheicebird/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+    neotheicebird
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Its a package about me. Other developers can know about me simply by executing:
+
+    pip install neotheicebird
+    import neotheicebird
+
+    :authors: 2018 by Prashanth Gandhiraj, see AUTHORS
+    :license: CC0 1.0, see LICENSE file for details
+"""
+
+__version__ = "0.1.0"
+
+# About me
+about_me = """Hello. I am @neotheicebird a.k.a Prashanth. 
+I am a Python Developer. I work freelance. 
+I started Spacetime Education, a python classroom in my city, Chennai. Checkout https://spacetime.education.
+Tags: Programmer, Minimalist, Lifelong learner, Webaholic, Friendly introvert, Non-linear thinker, Unapologetic questioner, Millennial."""
+
+print(about_me)
