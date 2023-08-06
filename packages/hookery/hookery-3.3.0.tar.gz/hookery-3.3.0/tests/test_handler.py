60b0d864ed8121fc3ba006eaa2877bf1e56dbac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:09 +0000

    Add package hookery/3.3.0/hookery-3.3.0.tar.gz

---
+++ packages/hookery/hookery-3.3.0.tar.gz/hookery-3.3.0/tests/test_handler.py
@@ -0,0 +1,43 @@
+import inspect
+
+from hookery.base import GlobalHook, Handler, InstanceHook
+
+
+def test_handler_from_func():
+    def f(a, b):
+        return a - b
+
+    h = Handler(f, GlobalHook('hook1'))
+    assert callable(h)
+    assert h.__name__ == 'f'
+    assert h.name == 'f'
+    assert h.hook_name == 'hook1'
+    assert not h.is_generator
+    assert h(c=8, b=10, a=25) == 15
+    assert h(b=5, a=10) == 5
+
+
+def test_handler_from_generator():
+    def g(a, b):
+        yield 2 * a
+        yield 3 * b
+
+    hook2 = GlobalHook('hook2')
+    handler = Handler(g, hook2)
+    assert handler.hook_name == 'hook2'
+    assert handler.is_generator
+    assert inspect.isgenerator(handler())
+    assert list(handler(a=5, b=6)) == [10, 18]
+    assert hook2.get_result_from_handler(handler, a=5, b=6) == [10, 18]
+
+
+def test_instance_hook_handler_from_bound_method():
+    class C:
+        def parse(self, value):
+            assert isinstance(self, C)
+            return value * 5
+
+    hook = InstanceHook('before')
+    handler = Handler(C().parse, hook=hook)
+    assert handler(value=6) == 30
+    assert hook.get_result_from_handler(handler, value=6) == 30
