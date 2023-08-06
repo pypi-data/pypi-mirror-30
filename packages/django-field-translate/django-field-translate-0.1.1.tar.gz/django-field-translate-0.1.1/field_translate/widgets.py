f390edc4aa7ef3600a56a134a83e4ecf321cb510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:45 +0000

    Add package django-field-translate/0.1.1/django-field-translate-0.1.1.tar.gz

---
+++ packages/django-field-translate/django-field-translate-0.1.1.tar.gz/django-field-translate-0.1.1/field_translate/widgets.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django import forms
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+
+
+class ModelMultiChoiceWidget(forms.Widget):
+    template_name = 'admin/checkbox_select_with_lang.html'
+    queryset = None
+
+    class Media:
+        js = [
+            'http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js',
+            'js/model_multi_choice_widget.js'
+        ]
+
+    def __init__(self, queryset):
+        super(ModelMultiChoiceWidget, self).__init__()
+        self.queryset = queryset
+
+    def get_options(self, context):
+        options = []
+        for obj in self.queryset:
+            options.append(
+                (
+                    None,
+                    [
+                        {
+                            u'index': len(options),
+                            u'name': context['widget']['name'],
+                            u'template_name': u'admin/checkbox_option.html',
+                            u'type': u'checkbox',
+                            u'selected': False,
+                            u'attrs': {
+                                u'id': 'id_' + context['widget']['name'] + '_' + str(len(options)),
+                            },
+                            u'value': obj.pk,
+                            u'label': obj.__unicode__,
+                            u'lang': obj.lang,
+                        }
+                    ],
+                    len(options)
+                )
+            )
+        return options
+
+    def render(self, name, value, attrs=None, renderer=None):
+        context = self.get_context(name, value, attrs)
+
+        context['widget']['optgroups'] = self.get_options(context)
+
+        return mark_safe(
+            render_to_string(
+                self.template_name,
+                context
+
+            )
+        )
