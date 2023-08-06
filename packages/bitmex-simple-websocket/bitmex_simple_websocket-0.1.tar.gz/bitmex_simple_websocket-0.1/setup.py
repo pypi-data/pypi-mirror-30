2845896191bb873448f5834ca269da4e5e15cd1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:56 +0000

    Add package bitmex-simple-websocket/0.1/bitmex_simple_websocket-0.1.tar.gz

---
+++ packages/bitmex-simple-websocket/bitmex_simple_websocket-0.1.tar.gz/bitmex_simple_websocket-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+requires = ["requests>=2.14.2"]
+
+
+setup(
+    name='bitmex_simple_websocket',
+    version='0.1',
+    description='BitMEX simple websocket client',
+    url='https://github.com/qLethon/bitmex_simple_websocket',
+    author='qLethon',
+    author_email='qLethon@gmail.com',
+    license='MIT',
+    install_requires=[
+        'websocket-client==0.46.0',
+    ],
+)
