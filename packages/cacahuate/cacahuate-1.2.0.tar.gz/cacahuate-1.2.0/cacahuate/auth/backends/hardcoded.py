4c5d921eba05b69f228597784f6c2c44b55ca5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:51 +0000

    Add package cacahuate/1.2.0/cacahuate-1.2.0.tar.gz

---
+++ packages/cacahuate/cacahuate-1.2.0.tar.gz/cacahuate-1.2.0/cacahuate/auth/backends/hardcoded.py
@@ -0,0 +1,31 @@
+from cacahuate.auth.base import BaseAuthProvider, BaseUser
+from cacahuate.errors import AuthenticationError
+
+
+class HardcodedUser(BaseUser):
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def get_identifier(self):
+        return self.username
+
+
+class HardcodedAuthProvider(BaseAuthProvider):
+
+    def authenticate(self, **credentials):
+        if 'username' not in credentials or \
+           'password' not in credentials:
+            raise AuthenticationError
+
+        username = credentials['username']
+        password = credentials['password']
+
+        if username != 'juan' or password != '123456':
+            raise AuthenticationError
+
+        return HardcodedUser(
+            username=username,
+            password=password,
+        )
