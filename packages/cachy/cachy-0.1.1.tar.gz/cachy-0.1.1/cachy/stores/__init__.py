05e72388dd365f1279c692426d2a7bef9ef26463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:28 +0000

    Add package cachy/0.1.1/cachy-0.1.1.tar.gz

---
+++ packages/cachy/cachy-0.1.1.tar.gz/cachy-0.1.1/cachy/stores/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from .database_store import DatabaseStore
+from .dict_store import DictStore
+from .file_store import FileStore
+from .memcached_store import MemcachedStore
+from .redis_store import RedisStore
+from .null_store import NullStore
