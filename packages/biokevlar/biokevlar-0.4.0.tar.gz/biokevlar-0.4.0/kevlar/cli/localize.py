d132b344f3b411c407fc9715fd70fd3430ec5433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:35 +0000

    Add package biokevlar/0.4.0/biokevlar-0.4.0.tar.gz

---
+++ packages/biokevlar/biokevlar-0.4.0.tar.gz/biokevlar-0.4.0/kevlar/cli/localize.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#
+# -----------------------------------------------------------------------------
+# Copyright (c) 2017 The Regents of the University of California
+#
+# This file is part of kevlar (http://github.com/dib-lab/kevlar) and is
+# licensed under the MIT license: see LICENSE.
+# -----------------------------------------------------------------------------
+
+
+def subparser(subparsers):
+    """Define the `kevlar localize` command-line interface."""
+
+    desc = """\
+    Given a reference genome and a contig (or set of contigs) assembled from
+    variant-related reads, retrieve the portion of the reference genome
+    corresponding to the variant. NOTE: this command relies on the `bwa`
+    program being in the PATH environmental variable.
+    """
+
+    subparser = subparsers.add_parser('localize', description=desc)
+    subparser.add_argument('-x', '--max-diff', type=int, metavar='X',
+                           default=10000, help='span of all k-mer starting '
+                           'positions should not exceed X bp; default is '
+                           '10000 (10 kb)')
+    subparser.add_argument('-d', '--delta', type=int, metavar='D',
+                           default=50, help='retrieve the genomic interval '
+                           'from the reference by extending beyond the span '
+                           'of all k-mer starting positions by D bp')
+    subparser.add_argument('-o', '--out', metavar='FILE', default='-',
+                           help='output file; default is terminal (stdout)')
+    subparser.add_argument('-z', '--seed-size', type=int, metavar='Z',
+                           default=51, help='seed size; default is 51')
+    subparser.add_argument('contigs', help='assembled reads in Fasta format')
+    subparser.add_argument('refr', help='BWA indexed reference genome')
