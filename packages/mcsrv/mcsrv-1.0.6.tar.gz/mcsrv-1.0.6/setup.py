c003f15677b1d1df2333a4ced6131e9be8935f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:26 +0000

    Add package mcsrv/1.0.6/mcsrv-1.0.6.tar.gz

---
+++ packages/mcsrv/mcsrv-1.0.6.tar.gz/mcsrv-1.0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='mcsrv',
+    version='1.0.6',
+    install_requires=[
+        "aioredis",
+        "aioamqp",
+        "aiopg",
+    ],
+    description="Package for small service start",
+    author='Stepan Pyzhov',
+    maintainer='Stepan Pyzhov',
+    packages=['mcsrv'],
+    package_dir={'mcsrv': 'src'},
+    url='https://github.com/Turin-tomsk/mcsrv',
+)
