3ff19ac35e874e668246305a9ea7315fbfc8c595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:20 +0000

    Add package smartflow/1.0.0/smartflow-1.0.0.tar.gz

---
+++ packages/smartflow/smartflow-1.0.0.tar.gz/smartflow-1.0.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+__version__ = '1.0.0'
+
+setup(
+    name='smartflow',
+    version=__version__,
+    author='Dylan',
+    author_email='dylan.adv@icloud.com',
+    url='https://github.com/dylan516/smartflow.git',
+    packages=find_packages(),
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    install_requires=['xlrd'],
+    package_data={
+        'smartflow': ['.sys']
+    },
+    description=(
+        'Library for generating and editing smartest7 test flow'
+    ),
+    long_description=(
+        'Library for generating and editing smartest7 test flow'
+    ),
+    platforms=["Any"],
+    license='BSD',
+    keywords=['testflow', 'smart'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    zip_safe=False,
+    include_package_data=True
+)
