e00067045d2203d0e007985e6f2e1ca388a350da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:22 +0000

    Add package pachong/0.0.35/pachong-0.0.35-py2-none-any.whl

---
+++ packages/pachong/pachong-0.0.35-py2-none-any.whl/pachong/crawler/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Cheng Chen
+# @Email : cchen224@uic.edu
+# @Time  : 2/13/18
+# @File  : [pachong] __init__.py
+
+
+from .taobao.taobao import Taobao
+from .weibo.weibo import Weibo
+
+__all__ = ['Weibo', 'Taobao']
