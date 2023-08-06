00e0114ab41f89ee204262eaccc5b3a9c9a48df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:58 +0000

    Add package imcii/0.1.5/imcii-0.1.5.tar.gz

---
+++ packages/imcii/imcii-0.1.5.tar.gz/imcii-0.1.5/setup.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+from setuptools import setup
+
+version = '0.1.5'
+
+entry_points = {
+    'console_scripts': [
+        'imcii=imcii.ascii:main',
+    ],
+}
+
+setup(
+    name='imcii',
+    packages=['imcii'],
+    version=version,
+    description='imcii = Image to ASCII',
+    author='Windard Yang',
+    author_email='windard@qq.com',
+    url='https://windard.com',
+
+    license="MIT",
+    entry_points=entry_points,
+)
