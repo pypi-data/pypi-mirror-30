239beef5b102fe097b5cb9069223d80098bb762d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:26 +0000

    Add package jike/0.3.0/jike-0.3.0.tar.gz

---
+++ packages/jike/jike-0.3.0.tar.gz/jike-0.3.0/jike/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+#         ___ __           __  ___     __
+#        / (_) /_____     /  |/  /__  / /__________
+#   __  / / / //_/ _ \   / /|_/ / _ \/ __/ ___/ __ \
+#  / /_/ / / ,< /  __/  / /  / /  __/ /_/ /  / /_/ /
+#  \____/_/_/|_|\___/  /_/  /_/\___/\__/_/   \____/
+#
+
+"""
+Jike Client
+~~~~~~~~~~~
+
+
+:copyright: (c) 2018 by Soros Liu.
+:license: MIT, see LICENSE for more details.
+"""
+
+from .client import JikeClient
+
+__title__ = 'jike'
+__version__ = '0.3.0'
+__description__ = 'Ⓙ Jike Metro: Unofficial Jike Client'
+__url__ = 'https://github.com/Sorosliu1029/Jike-Metro'
+__author__ = 'Soros Liu'
+__author_email__ = 'soros.liu1029@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 Soros Liu'
+__keywords__ = 'jike crawl'
