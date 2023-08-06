117e20da293e2cc6405d89d84408122243839441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:44 +0000

    Add package bitexen_client/0.0.2a0/bitexen_client-0.0.2a0.tar.gz

---
+++ packages/bitexen_client/bitexen_client-0.0.2a0.tar.gz/bitexen_client-0.0.2a0/bitexen_client/settings.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+
+import importlib
+import os
+import sys
+
+from bitexen_client.utils.dotdict import dotdict
+
+def import_path(fullpath):
+    """
+    Import a file with full path specification. Allows one to
+    import from anywhere, something __import__ does not do.
+    """
+    path, filename = os.path.split(fullpath)
+    filename, ext = os.path.splitext(filename)
+    sys.path.insert(0, path)
+    module = importlib.import_module(filename, path)
+    importlib.reload(module)  # Might be out of date
+    del sys.path[0]
+    return module
+
+
+userSettings = import_path(os.path.join('.', 'settings'))
+
+settings = {}
+settings.update(vars(userSettings))
+
+settings = dotdict(settings)
