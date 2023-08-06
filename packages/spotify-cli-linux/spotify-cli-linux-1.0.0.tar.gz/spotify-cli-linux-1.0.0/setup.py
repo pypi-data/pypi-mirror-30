60d10b04a1e355ae2240c760ddc9cf50ddc060ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:16 +0000

    Add package spotify-cli-linux/1.0.0/spotify-cli-linux-1.0.0.tar.gz

---
+++ packages/spotify-cli-linux/spotify-cli-linux-1.0.0.tar.gz/spotify-cli-linux-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'spotify-cli-linux',
+    version = '1.0.0',
+    description = 'A command-line interface to Spotify on Linux',
+    author = 'pwittchen',
+    author_email = 'piotr.wittchen@gmail.com',
+    url = 'https://github.com/pwittchen/spotify-cli-linux',
+    license = 'Apache 2.0',
+    packages = ['spotifycli'],
+    entry_points = {
+       "console_scripts": ['spotifycli = spotifycli.spotifycli:main']
+    },
+)
