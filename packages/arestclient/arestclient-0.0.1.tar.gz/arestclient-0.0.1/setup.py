724603c4a6dd1b090a097e565e643598ed1e32aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:40 +0000

    Add package arestclient/0.0.1/arestclient-0.0.1.tar.gz

---
+++ packages/arestclient/arestclient-0.0.1.tar.gz/arestclient-0.0.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="arestclient",
+    version="0.0.1",
+    author="Gene Wood",
+    author_email='gene_wood@cementhorizon.com',
+    url="https://pypi.python.org/pypi/arestclient",
+    description="Agnostic REST Client",
+    long_description="This is a placeholder for the future renamed agithub package",
+    license="MIT",
+    install_requires=['agithub'],
+)
