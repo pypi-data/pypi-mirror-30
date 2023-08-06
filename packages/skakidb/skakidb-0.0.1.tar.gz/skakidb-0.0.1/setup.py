0c64c7b8ffdf353199477365bfecd6a74fc6d090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:25 +0000

    Add package skakidb/0.0.1/skakidb-0.0.1.tar.gz

---
+++ packages/skakidb/skakidb-0.0.1.tar.gz/skakidb-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="skakidb",
+    version="0.0.1" ,
+    author="a6446834521"  ,
+    author_email="a6446834521@gmail.com" ,
+    packages=["skakidb"] ,
+    scripts=["skaki_cli.py"] ,
+    url="https://github.com/8269437145/skakidb-cli/" ,
+    description="skakidbcli" ,
+
+)
