76d5ba6373286fdbcf1031e4f74e82f0ec4dfb4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:30 +0000

    Add package oosheet/1.3/oosheet-1.3.tar.gz

---
+++ packages/oosheet/oosheet-1.3.tar.gz/oosheet-1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import os
+
+setup(name = 'oosheet',
+      version = '1.3',
+      description = 'LibreOffice Spreadsheet scripting library',
+      long_description = open(os.path.join(os.path.dirname(__file__), "README")).read(),
+      author = "Luis Fagundes",
+      author_email = "lhfagundes@gmail.com",
+      license = "The MIT License",
+      packages = find_packages(),
+      entry_points = {
+          'console_scripts': [
+              'oosheet-pack = oosheet:pack',
+              'oosheet-launch = oosheet:launch',
+              ]
+          },
+      # Why isn't install_requires working?
+      #install_requires = ['uno'],
+      classifiers = [
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Office/Business :: Financial :: Spreadsheet',
+        ],
+      url = 'http://oosheet.readthedocs.io/',
+      
+)
