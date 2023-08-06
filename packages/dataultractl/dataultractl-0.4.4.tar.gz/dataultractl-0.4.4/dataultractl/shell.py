5f130909392eb577961744c9d0b93c2869fb8610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:15 +0000

    Add package dataultractl/0.4.4/dataultractl-0.4.4.tar.gz

---
+++ packages/dataultractl/dataultractl-0.4.4.tar.gz/dataultractl-0.4.4/dataultractl/shell.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+from .client import Client
+
+
+
+def main():
+    # try:
+    #     Client().main(sys.argv[1:])
+    # except Exception as e:
+    #     msg = e.message
+    #     if not msg or msg == '':
+    #         msg = u"异常"
+    #     print "ERROR: %s" % msg
+    #     # print msg
+    #     sys.exit(1)
+    Client().main(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
