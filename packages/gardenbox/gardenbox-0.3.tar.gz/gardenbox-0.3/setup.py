9d5e954f99dbd93ae924cbd7fcb2f0d2f8adb7ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:53 +0000

    Add package gardenbox/0.3/gardenbox-0.3.tar.gz

---
+++ packages/gardenbox/gardenbox-0.3.tar.gz/gardenbox-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='gardenbox',
+      version='0.3',
+      description='Shared package for GardenBox usage',
+      url='https://gitlab.imerir.com/anatoli/garden-box',
+      author='Imerir',
+      author_email='anthony.natoli@imerir.com',
+      license='MIT',
+      packages=['gardenbox'],
+      zip_safe=False)
+
