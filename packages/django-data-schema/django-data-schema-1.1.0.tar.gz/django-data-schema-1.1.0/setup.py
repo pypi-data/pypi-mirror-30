519fd9103e5bd47f82946b90b73cd29ff87da087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:17 +0000

    Add package django-data-schema/1.1.0/django-data-schema-1.1.0.tar.gz

---
+++ packages/django-data-schema/django-data-schema-1.1.0.tar.gz/django-data-schema-1.1.0/setup.py
@@ -0,0 +1,59 @@
+# import multiprocessing to avoid this bug (http://bugs.python.org/issue15881#msg170215)
+import multiprocessing
+assert multiprocessing
+import re
+from setuptools import setup, find_packages
+
+
+def get_version():
+    """
+    Extracts the version number from the version.py file.
+    """
+    VERSION_FILE = 'data_schema/version.py'
+    mo = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', open(VERSION_FILE, 'rt').read(), re.M)
+    if mo:
+        return mo.group(1)
+    else:
+        raise RuntimeError('Unable to find version string in {0}.'.format(VERSION_FILE))
+
+
+setup(
+    name='django-data-schema',
+    version=get_version(),
+    description='Schemas over dictionaries and models in Django',
+    long_description=open('README.md').read(),
+    url='https://github.com/ambitioninc/django-data-schema',
+    author='Wes Kendall',
+    author_email='opensource@ambition.com',
+    keywords='Django Data Schema',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+    ],
+    license='MIT',
+    install_requires=[
+        'Django>=1.10',
+        'django-manager-utils>=1.1.1',
+        'fleming>=0.4.6',
+        'python-dateutil>=2.2',
+    ],
+    tests_require=[
+        'django-dynamic-fixture',
+        'psycopg2',
+        'django-nose',
+        'mock',
+    ],
+    test_suite='run_tests.run_tests',
+    include_package_data=True,
+)
