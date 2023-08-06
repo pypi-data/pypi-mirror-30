1c6ad3dc5df4c03d5e1f13aaf6026288fceb99e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:33 +0000

    Add package masonite-entry/0.1.1/masonite-entry-0.1.1.tar.gz

---
+++ packages/masonite-entry/masonite-entry-0.1.1.tar.gz/masonite-entry-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="masonite-entry",
+    version='0.1.1',
+    packages=[
+        'entry',
+        'entry.api',
+        'entry.commands',
+        'entry.migrations',
+        'entry.providers',
+    ],
+    install_requires=[
+        'masonite>=1.5,<= 1.5.99',
+    ],
+    include_package_data=True,
+)
