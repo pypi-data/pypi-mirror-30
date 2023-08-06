0ed9003c006b41237acba0124cbde7bcefe005ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:22 +0000

    Add package basic-crud/0.1/basic-crud-0.1.tar.gz

---
+++ packages/basic-crud/basic-crud-0.1.tar.gz/basic-crud-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'basic-crud',
+  packages = ['basic-crud'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A simple Django app to basic CRUD.',
+  author = 'Ricardo Pizano',
+  author_email = 'rikymon2@hotmail.com',
+  url = 'https://github.com/RicardoPizano/basic-crud', # use the URL to the github repo
+  download_url = 'https://github.com/RicardoPizano/basic-crud/tree/master/dist/django-basic-crud-0.1.tar.gz',
+  keywords = [],
+  classifiers = [],
+)
