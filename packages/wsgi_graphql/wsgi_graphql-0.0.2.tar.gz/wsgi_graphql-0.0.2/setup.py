58f217049408c129db80c868129e01805fdd0ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:46 +0000

    Add package wsgi_graphql/0.0.2/wsgi_graphql-0.0.2.tar.gz

---
+++ packages/wsgi_graphql/wsgi_graphql-0.0.2.tar.gz/wsgi_graphql-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+version = '0.0.2'
+url = 'https://github.com/moritzmhmk/wsgi-graphql'
+download_url = '{}/archive/v{}.tar.gz'.format(url, version)
+
+setup(
+    name='wsgi_graphql',
+    py_modules=['wsgi_graphql'],
+    version=version,
+    description='Create a WSGI Application for a GraphQL schema.',
+    long_description=open('README.rst').read(),
+    author='Moritz K.',
+    author_email='moritzmhmk@googlemail.com',
+    url=url,
+    download_url=download_url,
+    install_requires=['graphql-server-core'],
+    license='MIT',
+    keywords=['graphql', 'wsgi', 'api', 'rest'],
+    classifiers=[],
+)
