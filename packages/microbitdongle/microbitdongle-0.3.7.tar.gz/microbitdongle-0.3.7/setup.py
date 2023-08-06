f32bd3f3edbc2659d34e671271db17d185da537a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:18 +0000

    Add package microbitdongle/0.3.7/microbitdongle-0.3.7.tar.gz

---
+++ packages/microbitdongle/microbitdongle-0.3.7.tar.gz/microbitdongle-0.3.7/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='microbitdongle',
+    decription='allows you to be able to communicate with a microbit over serial',
+    version='0.3.7',
+    author='Luke Spademan',
+    author_email='info@lukespademan.com',
+    packages=['microbitdongle'],
+    install_requires=[package for package in open('requirements.txt').read().split("\n")],
+    long_description = long_description,
+    url='https://github.com/lukespademan/microbitdongle',
+    download_url ='https://github.com/lukespademan/microbitdongle/archive/0.1.tar.gz'
+)
