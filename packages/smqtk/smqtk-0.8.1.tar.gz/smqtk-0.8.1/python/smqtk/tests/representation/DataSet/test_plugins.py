e609a14631e136905b003b40e9c36a04b321d1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:59 +0000

    Add package smqtk/0.8.1/smqtk-0.8.1.tar.gz

---
+++ packages/smqtk/smqtk-0.8.1.tar.gz/smqtk-0.8.1/python/smqtk/tests/representation/DataSet/test_plugins.py
@@ -0,0 +1,9 @@
+from smqtk.representation.data_set import get_data_set_impls
+
+
+def test_plugin_getter():
+    c = get_data_set_impls()
+    assert isinstance(c, dict)
+    # The following implementations at least should always be available.
+    assert 'DataFileSet' in c
+    assert 'DataMemorySet' in c
