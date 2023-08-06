9fff9ca47a7754ac0af46f33a329c3c6425fcbd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:04 +0000

    Add package ThreadDownloader8460/1.0.2/ThreadDownloader8460-1.0.2.tar.gz

---
+++ packages/ThreadDownloader8460/ThreadDownloader8460-1.0.2.tar.gz/ThreadDownloader8460-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+packages = find_packages()
+
+setup(
+    name='ThreadDownloader8460',
+    version='1.0.2',
+    description="My first python distribution",
+    packages=packages
+)
