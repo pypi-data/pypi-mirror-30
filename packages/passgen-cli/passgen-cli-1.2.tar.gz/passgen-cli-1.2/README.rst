4c97a2778e35efd0051bef174d5f3409bec0eb63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:57 +0000

    Add package passgen-cli/1.2/passgen-cli-1.2.tar.gz

---
+++ packages/passgen-cli/passgen-cli-1.2.tar.gz/passgen-cli-1.2/README.rst
@@ -0,0 +1,7 @@
+Simple CLI password generator. Written in Python
+
+Installation: `pip install passgen-cli`
+Example: `passgen-cli -l 13`
+
+If you just want to execute passgen.py by itself as a script, you can use passgen.py as a normal script, i.e. as the command line front end to your application. Set permissions via `$ chmod u+x passgen_cli.py` and execute it:
+`./passgen_cli.py -l`
---
+++ packages/passgen-cli/passgen-cli-1.2.tar.gz/passgen-cli-1.2/passgen-cli/passgen_cli.py
@@ -0,0 +1,30 @@
+import random
+import string
+import os
+import click
+
+@click.command()
+@click.option('-l', default=12, type=int, help='length of the password. Default is 12 characters')
+
+def pass_gen(l):
+	symb = ''
+	letters = ''
+	digits = ''
+
+	symb = ''.join(set(random.choice(string.punctuation) for x in range(100)))
+	letters = ''.join(set(random.choice(string.ascii_letters) for x in range(100)))
+	digits = ''.join(set(random.choice(string.digits) for x in range(10)))
+
+
+	chars_combination = letters + digits + symb
+
+	password = ''
+	if l <=0:
+	    click.echo('Invalid value for "-l" argument. Please provide value greater than 0.')
+	else:
+		password = ''.join(random.choice(chars_combination) for x in range(l))
+		click.echo(password)
+
+
+if __name__ == '__main__':
+    pass_gen()
