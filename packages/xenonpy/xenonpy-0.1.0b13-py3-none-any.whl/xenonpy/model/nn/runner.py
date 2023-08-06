8d50eae41683921ac72c037a1126b4681efffad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:08 +0000

    Add package xenonpy/0.1.0b13/xenonpy-0.1.0b13-py3-none-any.whl

---
+++ packages/xenonpy/xenonpy-0.1.0b13-py3-none-any.whl/xenonpy/model/nn/runner.py
@@ -0,0 +1,374 @@
+# Copyright 2018 TsumiNa. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+from datetime import datetime as dt
+from warnings import warn
+
+import numpy as np
+import torch as tc
+import torch.nn as nn
+from pandas import DataFrame as df
+from scipy.stats import pearsonr
+from sklearn.base import BaseEstimator
+from sklearn.externals import joblib
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
+from torch.autograd import Variable as Var
+
+from .checker import Checker
+from .wrap import Init
+from ...preprocess.data_select import DataSplitter
+from ...preprocess.transform import Scaler
+from ...utils.functional import Stopwatch
+
+
+class ModelRunner(BaseEstimator):
+    """
+    Run model.
+    """
+
+    def __init__(self, epochs=2000, *,
+                 xy_scaler=None,
+                 ctx='cpu',
+                 check_step=100,
+                 log_step=0,
+                 work_dir=None,
+                 verbose=True,
+                 additional_info=None
+                 ):
+        """
+
+        Parameters
+        ----------
+        epochs: int
+        log_step: int
+        ctx: str
+        check_step: int
+        work_dir: str
+        verbose: bool
+            Print :class:`ModelRunner` environment.
+        """
+        self._add_info = additional_info if additional_info else {}
+        self._verbose = verbose
+        self._check_step = check_step
+        self._ctx = ctx
+        self._epochs = epochs
+        self._log_step = log_step
+        self._work_dir = work_dir
+        self._checker = None
+        self._splitter = None
+        self._model = None
+        self._model_name = None
+        self._loss_func = None
+        self._optim = None
+        self._lr = None
+        self._lr_scheduler = None
+        if xy_scaler:
+            self._x_scaler, self._y_scaler = self._check_xy_scaler(xy_scaler)
+        else:
+            self._x_scaler, self._y_scaler = None, None
+
+        if self._verbose:
+            print('Runner environment:')
+            print('Running dir: {}'.format(self._work_dir))
+            print('Epochs: {}'.format(self._epochs))
+            print('Context: {}'.format(self._ctx.upper()))
+            print('Check step: {}'.format(self._check_step))
+            print('Log step: {}\n'.format(self._log_step))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        del self
+
+    def __call__(self, model, name=None, *,
+                 init_weight=Init.uniform(scale=0.1),
+                 loss_func=None,
+                 optim=None,
+                 lr=0.001,
+                 lr_scheduler=None):
+
+        def _init_weight(m):
+            if isinstance(m, nn.Linear):  # fixme: not only linear
+                init_weight(m.weight)
+
+        # model must inherit form nn.Module
+        if not isinstance(model, nn.Module):
+            raise ValueError(
+                'Runner need a `torch.nn.Module` instance as first parameter but got {}'.format(type(model)))
+
+        if not name:
+            from hashlib import md5
+            sig = md5(model.__str__().encode()).hexdigest()
+            name = sig
+        if init_weight:
+            if self._verbose:
+                print('init weight with {}'.format(init_weight))
+            model.apply(_init_weight)
+        self._checker = Checker(name, self._work_dir)
+        self._model = model
+        self._model_name = name
+        self._loss_func = loss_func
+        self._optim = optim
+        self._lr = lr
+        self._lr_scheduler = lr_scheduler
+        self._checker.init_model = model
+        self._checker.save(runner=dict(
+            add_info=self._add_info,
+            verbose=self._verbose,
+            check_step=self._check_step,
+            ctx=self._ctx,
+            epochs=self._epochs,
+            log_step=self._log_step,
+            work_dir=self._work_dir,
+            model_name=self._model_name,
+            loss_func=self._loss_func,
+            optim=self._optim,
+            lr=self._lr,
+            lr_scheduler=self._lr_scheduler
+        ))
+
+    @staticmethod
+    def _check_xy_scaler(scaler):
+        if isinstance(scaler, (tuple, list)):
+            if not (isinstance(scaler[0], Scaler) and isinstance(scaler[1], Scaler)):
+                raise TypeError('must be a Scaler but got ({}, {})'.format(type(scaler[0]), type(scaler[1])))
+            if scaler[0].value.shape[0] != scaler[1].value.shape[0]:
+                raise ValueError('X y should have same row size (shape[0])')
+            return scaler[0], scaler[1]
+        else:
+            raise ValueError('parameter scaler must be a tuple or list with size 2 but got {}'.format(scaler))
+
+    @property
+    def xy_scaler(self):
+        return self._x_scaler, self._y_scaler
+
+    @xy_scaler.setter
+    def xy_scaler(self, scaler):
+        self._x_scaler, self._y_scaler = self._check_xy_scaler(scaler)
+
+    @staticmethod
+    def _d2tv(data):
+        if isinstance(data, df):
+            data = tc.from_numpy(data.as_matrix()).type(tc.FloatTensor)
+        elif isinstance(data, np.ndarray):
+            data = tc.from_numpy(data).type(tc.FloatTensor)
+        else:
+            raise TypeError('need to be <numpy.ndarray> or <pandas.DataFrame> but got {}'.format(type(data)))
+        return Var(data, requires_grad=False)
+
+    @staticmethod
+    def metrics(y_true, y_pred):
+        mae = mean_absolute_error(y_true, y_pred)
+        rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+        r2 = r2_score(y_true, y_pred)
+        pr, p_val = pearsonr(y_true, y_pred)
+        return dict(
+            mae=mae,
+            rmse=rmse,
+            r2=r2,
+            pearsonr=pr,
+            p_value=p_val
+        )
+
+    def _train(self, x_train, y_train):
+        stopwatch = Stopwatch()
+        # optimization
+        optim = self._optim(self._model.parameters(), lr=self._lr)
+
+        # adjust learning rate
+        scheduler = self._lr_scheduler(optim) if self._lr_scheduler else None
+
+        # start
+        loss, y_pred = None, None
+        print('Model name: {}\n'.format(self._model_name))
+        print('=======start training=======')
+
+        for t in range(self._epochs):
+            if scheduler and not isinstance(scheduler, tc.optim.lr_scheduler.ReduceLROnPlateau):
+                scheduler.step()
+            y_pred = self._model(x_train)
+            loss = self._loss_func(y_pred, y_train)
+            if scheduler and isinstance(scheduler, tc.optim.lr_scheduler.ReduceLROnPlateau):
+                scheduler.step(loss)
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+
+            if self._log_step > 0 and t % self._log_step == 0:
+                print('at step[{}/{}], Loss={:.4f}, elapsed time: {}'.format(t, self._epochs,
+                                                                             loss.data[0],
+                                                                             stopwatch.count))
+            if self._check_step > 0 and t % self._check_step == 0:
+                self._checker(model_state=self._model.state_dict(), epochs=t)
+
+        print('\n=======over training=======')
+
+        # save last results
+        self._checker(model_state=self._model.state_dict(), epochs=self._epochs)
+        y_train, y_pred = y_train.cpu().data.numpy(), y_pred.cpu().data.numpy()
+        metrics = self.metrics(y_train, y_pred)
+
+        if self._verbose:
+            print('Final loss={:.4f}\n'.format(loss.data[0]))
+            print('Mean absolute error with train data: %s' % metrics['mae'])
+            print('Root mean squared error with train data: %s' % metrics['rmse'])
+
+        return y_train, y_pred, metrics
+
+    def fit(self, *xy, test_size=0.2):
+        """
+        Fit Neural Network model
+
+        Parameters
+        ----------
+        xy: None or list or ``numpy.ndarray`` or ``pandas.DataFrame``.
+            Training data.
+            If None, will split data in ``self.xy_scaler`` with ``test_size=0.2``.
+            If ``self.xy_scaler``  is None, raise ``ValueError``.
+            If list, use as indices to sample training data from `self.xy_scaler``.
+            If ``self.xy_scaler``  is None, raise ``ValueError``.
+            Also can input x y training data directly in ``numpy.ndarray`` or ``pandas.DataFrame``.
+        test_size: float, int, None
+            If float, should be between 0.0 and 1.0 and represent the proportion
+            of the dataset to include in the test split. If int, represents the
+            absolute number of test samples. If None, the value is set to the
+            complement of the train size. By default, the value is set to 0.2.
+        Returns
+        -------
+        self:
+            returns an instance of self.
+        """
+        if len(xy) == 0:
+            if self._x_scaler and self._y_scaler:
+                ds = DataSplitter(self._x_scaler.np_value, test_size=test_size)
+                x_train, y_train = ds.split_data(self._x_scaler.np_value, self._y_scaler.np_value, test=False)
+                self._checker.save_data(x_scaler=self._x_scaler, y_scaler=self._y_scaler, splitter=ds)
+                self._splitter = ds
+            else:
+                raise ValueError('No data for training, please set ``xy_scaler`` or input training data directly')
+        else:
+            if len(xy) != 2:
+                raise ValueError('xy size must be 2 but got {}'.format(len(xy)))
+            x_train, y_train = xy
+            self._checker.save_data(x_train=x_train, y_train=y_train)
+
+        # transform to torch tensor
+        x_train = self._d2tv(x_train)
+        y_train = self._d2tv(y_train)
+
+        # if use CUDA acc
+        if self._ctx.lower() == 'gpu':
+            if tc.cuda.is_available():
+                self._model.cuda()
+                x_train = x_train.cuda()
+                y_train = y_train.cuda()
+            else:
+                warn('No cuda environment, use cpu fallback.', RuntimeWarning)
+        else:
+            self._model.cpu()
+
+        # train
+        result = self._train(x_train, y_train)
+
+        # move back to cpu
+        self._model.cpu()
+        self._checker.trained_model = self._model
+        self._checker.describe = dict(self._add_info,
+                                      structure=str(self._model),
+                                      running_at=self._work_dir,
+                                      created_at=dt.now().strftime('-%Y-%m-%d_%H-%M-%S_%f'))
+
+        return result
+
+    def predict(self, *xy):
+        save_data = False
+        if len(xy) == 0:
+            if self._x_scaler and self._y_scaler:
+                x_test, y_test = self._splitter.split_data(self._x_scaler.np_value, self._y_scaler.np_value,
+                                                           train=False)
+            else:
+                raise ValueError('No data for test, please set ``xy_scaler`` or input test data directly')
+        else:
+            if len(xy) != 2:
+                raise ValueError('xy size must be 2 but got {}'.format(len(xy)))
+            x_test, y_test = xy
+            save_data = True
+        # prepare x
+        x_test_ = self._d2tv(x_test)
+
+        # if use CUDA acc
+        if self._ctx.lower() == 'gpu':
+            if tc.cuda.is_available():
+                self._model.cuda()
+                x_test_ = x_test_.cuda()
+            else:
+                warn('No cuda environment, use cpu fallback.', RuntimeWarning)
+        else:
+            self._model.cpu()
+        # prediction
+        y_pred = self._model(x_test_).cpu().data.numpy()
+        metrics = self.metrics(y_test, y_pred)
+
+        if save_data:
+            self._checker.add_predict(
+                x_test=x_test,
+                y_test=y_test,
+                y_pred=y_pred,
+                metrics=metrics
+            )
+        else:
+            self._checker.add_predict(
+                y_pred=y_pred,
+                metrics=metrics
+            )
+
+        return y_test, y_pred, metrics
+
+    @classmethod
+    def from_checker(cls, name, path=None):
+        checker = Checker.load(name, path)
+        runner = checker.last('runner')
+        ret = cls()
+        ret._add_info = runner['add_info']
+        ret._verbose = runner['verbose']
+        ret._check_step = runner['check_step']
+        ret._ctx = runner['ctx']
+        ret._epochs = runner['epochs']
+        ret._log_step = runner['log_step']
+        ret._work_dir = runner['work_dir']
+        ret._model_name = runner['model_name']
+        ret._loss_func = runner['loss_func']
+        ret._optim = runner['optim']
+        ret._lr = runner['lr']
+        ret._lr_scheduler = runner['lr_scheduler']
+        try:
+            ret._x_scaler = checker.last('x_scaler')
+        except FileExistsError:
+            pass
+        try:
+            ret._y_scaler = checker.last('y_scaler')
+        except FileExistsError:
+            pass
+        try:
+            ret._splitter = checker.last('splitter')
+        except FileExistsError:
+            pass
+        ret._checker = checker
+        ret._model = checker.trained_model if checker.trained_model else checker.init_model
+        return ret
+
+    def dump(self, fpath, **kwargs):
+        """
+        Save model into pickled file with at ``fpath``.
+        Some additional description can be given from ``kwargs``.
+
+        Parameters
+        ----------
+        fpath: str
+            Path with name of pickle file.
+        kwargs: dict
+            Additional description
+        """
+        val = dict(model=self._model, **kwargs)
+        joblib.dump(val, fpath)
---
+++ packages/xenonpy/xenonpy-0.1.0b13-py3-none-any.whl/xenonpy/preprocess/transform.py
@@ -0,0 +1,190 @@
+# Copyright 2018 TsumiNa. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+import numpy as np
+from pandas import DataFrame, Series
+from scipy.special import inv_boxcox
+from scipy.stats import boxcox
+from sklearn.base import BaseEstimator, TransformerMixin
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+
+class BoxCox(BaseEstimator, TransformerMixin):
+    """
+    Box-cox transform.
+
+    References
+    ----------
+    G.E.P. Box and D.R. Cox, “An Analysis of Transformations”,
+    Journal of the Royal Statistical Society B, 26, 211-252 (1964).
+    """
+
+    def __init__(self, shift=1e-9):
+        """
+        Parameters
+        ----------
+        shift: float
+            Guarantee that all variables > 0
+        """
+        self._shift = shift
+        self._min = []
+        self._lmd = []
+        self._shape = None
+
+    def _check_type(self, x, check_shape=True):
+        if isinstance(x, list):
+            x = np.array(x, dtype=np.float)
+        elif isinstance(x, (DataFrame, Series)):
+            x = x.values
+        if not isinstance(x, np.ndarray):
+            raise TypeError(
+                'parameter `X` should be a `DataFrame`, `Series`, `ndarray` or list object '
+                'but got {}'.format(type(x)))
+        if check_shape and x.shape != self._shape:
+            raise ValueError('parameter `X` should have shape {} but got {}'.format(
+                self._shape, x.shape))
+        return x
+
+    def fit(self, x):
+        self._shape = x.shape
+        return self
+
+    def transform(self, x):
+        """
+
+        Parameters
+        ----------
+        x
+
+        Returns
+        -------
+        DataFrame
+            Box-Cox transformed data.
+        """
+        x = self._check_type(x)
+        if len(x.shape) == 1:
+            x_, lmd = self._box_cox(x)
+            self._lmd.append(lmd)
+            return x_
+
+        xs = []
+        for col in x.T:
+            x_, lmd = self._box_cox(col)
+            self._lmd.append(lmd)
+            xs.append(x_.reshape(-1, 1))
+        df = np.concatenate(xs, axis=1)
+        return df
+
+    def _box_cox(self, series):
+        if series.min() != series.max():
+            self._min.append(series.min())
+            with np.errstate(all='raise'):
+                tmp = series - series.min() + self._shift
+                try:
+                    return boxcox(tmp)
+                except FloatingPointError:
+                    return boxcox(tmp, 0.), 0.
+        self._min.append(series.min())
+        tmp = series - series.min() + self._shift
+        return boxcox(tmp, 0.), 0.
+
+    def inverse_transform(self, x):
+        """
+        Scale back the data to the original representation.
+
+        Parameters
+        ----------
+        x: DataFrame, Series, ndarray, list
+            The data used to scale along the features axis.
+
+        Returns
+        -------
+        DataFrame
+            Inverse transformed data.
+        """
+        x = self._check_type(x, check_shape=False)
+        if len(x.shape) == 1:
+            return  inv_boxcox(x, self._lmd[0]) - self._shift + self._min[0]
+
+        xs = []
+        for i, col in enumerate(x.T):
+            x_ = inv_boxcox(col, self._lmd[i]) - self._shift + self._min[i]
+            xs.append(x_.reshape(-1, 1))
+        df = np.concatenate(xs, axis=1)
+        return df
+
+
+class Scaler(object):
+    """
+    A value-matrix container for data transform.
+    """
+
+    def __init__(self, value):
+        """
+        Parameters
+        ----------
+        value: DataFrame
+            Inner data.
+        """
+        if isinstance(value, (Series, list, np.ndarray)):
+            self.__value = DataFrame(data=value)
+        elif isinstance(value, DataFrame):
+            self.__value = value
+        else:
+            raise TypeError(
+                'value must be list, dict, tuple, Series, ndarray or DataFrame but got {}'.format(type(value)))
+        self._index = self.__value.index
+        self._columns = self.__value.columns
+        self.__now = self.__value.values
+        self.__inverse_chain = []
+
+    def box_cox(self, *args, **kwargs):
+        return self._scale(BoxCox, *args, **kwargs)
+
+    def min_max(self, *args, **kwargs):
+        return self._scale(MinMaxScaler, *args, **kwargs)
+
+    def standard_scale(self, *args, **kwargs):
+        return self._scale(StandardScaler, *args, **kwargs)
+
+    def _scale(self, scaler, *args, **kwargs):
+        scaler = scaler(*args, **kwargs)
+        self.__now = scaler.fit_transform(self.__now)
+        self.__inverse_chain.append(scaler.inverse_transform)
+        return self
+
+    @property
+    def value(self):
+        """
+        Return scaled values as Dataframe object.
+
+        Returns
+        -------
+        DataFrame
+            Scaled value. If your need value as ndarray object, please use ``np_value``
+        """
+        return DataFrame(self.__now, index=self._index, columns=self._columns)
+
+    @property
+    def np_value(self):
+        """
+        Return scaled values as ndarray object
+
+        Returns
+        -------
+        ndarray
+            Scaled value. If your need value as Dataframe object, please use ``value``
+        """
+        return self.__now
+
+    def inverse(self, data):
+        if len(self.__inverse_chain) == 0:
+            return data
+        for inv in self.__inverse_chain[::-1]:
+            data = inv(data)
+        return data
+
+    def reset(self):
+        self.__now = self.__value
