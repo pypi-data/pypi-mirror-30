c8442519e956f8ece99d334eb15309102743ff16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:31 +0000

    Add package py-evm/0.2.0a14/py-evm-0.2.0a14.tar.gz

---
+++ packages/py-evm/py-evm-0.2.0a14.tar.gz/py-evm-0.2.0a14/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='py-evm',
+    # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
+    version='0.2.0-alpha.14',
+    description='Python implementation of the Ethereum Virtual Machine',
+    long_description_markdown_filename='README.md',
+    author='Piper Merriam',
+    author_email='piper@pipermerriam.com',
+    url='https://github.com/ethereum/py-evm',
+    include_package_data=True,
+    py_modules=['evm', 'trinity', 'p2p'],
+    install_requires=[
+        "aiohttp>=2.3.1,<3.0.0",
+        "async_lru>=0.1.0,<1.0.0",
+        "cryptography>=2.0.3,<3.0.0",
+        "cytoolz>=0.9.0,<1.0.0",
+        "eth-bloom>=1.0.0,<2.0.0",
+        "eth-utils>=1.0.1,<2.0.0",
+        "pyethash>=0.1.27,<1.0.0",
+        "py-ecc>=1.4.2,<2.0.0",
+        "rlp>=0.4.7,<1.0.0",
+        "eth-keys>=0.2.0b3,<1.0.0",
+        "trie>=1.3.0,<2.0.0",
+        "eth-tester==0.1.0b21",
+        "web3>=4.0.0b6,<5.0.0",
+    ],
+    extras_require={
+        'coincurve': [
+            "coincurve>=7.0.0,<8.0.0",
+        ],
+        'leveldb': [
+            "plyvel==1.0.4",
+        ],
+        'trinity': [
+            "plyvel==1.0.4",
+            "coincurve>=7.0.0,<8.0.0",
+            "web3>=4.0.0b11,<5.0.0",
+        ],
+        'p2p': [
+            "aiohttp>=2.3.1,<3.0.0",
+            "async_lru>=0.1.0,<1.0.0",
+            "pysha3>=1.0.0,<2.0.0",
+        ],
+    },
+    setup_requires=['setuptools-markdown'],
+    license='MIT',
+    zip_safe=False,
+    keywords='ethereum blockchain evm',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+    ],
+    # trinity
+    entry_points={
+        'console_scripts': ['trinity=trinity:main'],
+    },
+)
