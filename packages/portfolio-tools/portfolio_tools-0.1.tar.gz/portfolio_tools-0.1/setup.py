0fe73cdfa5d439444865eb5e133c75da4203036c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:38 +0000

    Add package portfolio-tools/0.1/portfolio_tools-0.1.tar.gz

---
+++ packages/portfolio-tools/portfolio_tools-0.1.tar.gz/portfolio_tools-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='portfolio_tools',
+    version='0.1',
+    description='Tools for working with portfolios of securities',
+    author='Pete Massaro',
+    author_email='peter.j.massaro@gmail.com',
+    license='MIT',
+    packages=['portfolio_tools'],
+    zip_safe=False)
