786a069ddd6f0e78176ef441f940f9977414503e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:17 +0000

    Add package amzsear/2.0.1/amzsear-2.0.1.tar.gz

---
+++ packages/amzsear/amzsear-2.0.1.tar.gz/amzsear-2.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'amzsear',
+  packages = find_packages(),
+  version = '2.0.1',
+  description = 'The unofficial Amazon search CLI & Python API',
+  author = "Asher Silvers",
+  author_email = "ashersilvers@gmail.com",
+  license='MIT',
+  url = 'https://github.com/asherAgs/amzSear', 
+  keywords = 'amazon search product products python',
+  classifiers = [],
+  entry_points={
+    'console_scripts': [
+      'amzsear = amzsear.cli.cli:run',
+     ],
+  },
+)
