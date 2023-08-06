5f3826a4b7fc4fa0dfec9b864c2472cfa3be6a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:20 +0000

    Add package bgen-reader/1.1.2/bgen-reader-1.1.2.tar.gz

---
+++ packages/bgen-reader/bgen-reader-1.1.2.tar.gz/bgen-reader-1.1.2/bgen_reader/__init__.py
@@ -0,0 +1,18 @@
+"""
+*******************
+bgen_reader package
+*******************
+
+Bgen file format reader.
+
+"""
+
+from __future__ import absolute_import as _
+
+from .bgen_reader import convert_to_dosage, read_bgen, create_metadata_file
+from .testit import test
+
+__version__ = "1.1.2"
+
+__all__ = ['__version__', 'test', 'read_bgen',
+           'convert_to_dosage', 'create_metadata_file']
