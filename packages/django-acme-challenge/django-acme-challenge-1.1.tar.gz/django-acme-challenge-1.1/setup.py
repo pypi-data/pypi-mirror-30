bda86219d3a8ab833a76b52e22c09e2dc4bf7904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:04 +0000

    Add package django-acme-challenge/1.1/django-acme-challenge-1.1.tar.gz

---
+++ packages/django-acme-challenge/django-acme-challenge-1.1.tar.gz/django-acme-challenge-1.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+
+
+# Utility function to read README file
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-acme-challenge',
+    version='1.1',
+    description="A quick tool to serve the acme-challenge verification page.",
+    author='Benjamin W Stookey',
+    author_email='ben.stookey@gmail.com',
+    url='https://github.com/jamstooks/django-acme-challenge',
+    long_description=read("README.md"),
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+    ],
+    test_suite='tests.main',
+    install_requires=['Django',],
+)
