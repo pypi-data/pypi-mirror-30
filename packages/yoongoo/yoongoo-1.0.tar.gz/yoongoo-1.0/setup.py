89563f71addb7f4a404950e977288e5d37b056c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:50 +0000

    Add package yoongoo/1.0/yoongoo-1.0.tar.gz

---
+++ packages/yoongoo/yoongoo-1.0.tar.gz/yoongoo-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'yoongoo',
+    version = '1.0',
+    description = '',
+    author = 'gdyoon',
+    author_email = 'gdyoon@naver.com'
+)
