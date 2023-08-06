0555139175ab94977e576f2222cd2dc2f88abd81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:04 +0000

    Add package pydevrant/0.21/pydevrant-0.21.tar.gz

---
+++ packages/pydevrant/pydevrant-0.21.tar.gz/pydevrant-0.21/pydevrant/__init__.py
@@ -0,0 +1,6 @@
+# Written by Sergio La Rosa (sergio.larosa89@gmail.com)
+# and John Leonardo (hey@jdleo.me)
+# Part of "pydevrant" package
+# https://github.com/SergioLaRosa/pydevrant
+
+from .basic import *
