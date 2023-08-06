e2724489a76b9a7cce02e5b9b542f69d43a32d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:18 +0000

    Add package sense2vec/1.0.0a0/sense2vec-1.0.0a0.tar.gz

---
+++ packages/sense2vec/sense2vec-1.0.0a0.tar.gz/sense2vec-1.0.0a0/sense2vec/spacy_pipeline.py
@@ -0,0 +1,21 @@
+import spacy
+
+
+def transform_doc(doc):
+    '''Optimized for the Reddit vectors data download'''
+    for ent in doc.ents:
+        ent.merge(ent.root.tag_, ent.text, ent.label_)
+    for np in doc.noun_chunks:
+        while len(np) > 1 and np[0].dep_ not in ('advmod', 'amod', 'compound'):
+            np = np[1:]
+        np.merge(np.root.tag_, np.text, np.root.ent_type_)
+
+
+def represent_word(word):
+    if word.like_url:
+        return '%%URL|X'
+    text = re.sub(r'\s', '_', word.text)
+    tag = LABELS.get(word.ent_type_, word.pos_)
+    if not tag:
+        tag = '?'
+    return text + '|' + tag
