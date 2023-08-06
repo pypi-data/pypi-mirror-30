54629288a4fa3948ed6ed15e91446b8ceceb9c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:10 +0000

    Add package signalr-client-aio/0.0.1.6.1/signalr-client-aio-0.0.1.6.1.tar.gz

---
+++ packages/signalr-client-aio/signalr-client-aio-0.0.1.6.1.tar.gz/signalr-client-aio-0.0.1.6.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+install_requires = \
+    ['requests>=2.18.4',
+     'uvloop>=0.9.1',
+     'websockets>=4.0.1'
+     ]
+
+setup(
+    name='signalr-client-aio',
+    version='0.0.1.6.1',
+    author='Stanislav Lazarov',
+    author_email='s.a.lazarov@gmail.com',
+    license='MIT',
+    url='https://github.com/slazarov/python-signalr-client',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=install_requires,
+    description='Simple python SignalR client using asyncio.',
+    download_url='https://github.com/slazarov/python-signalr-client.git',
+    keywords=['signalr', 'sginalr-weboscket', 'signalr-client', 'signalr-asyncio', 'signalr-aio'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
