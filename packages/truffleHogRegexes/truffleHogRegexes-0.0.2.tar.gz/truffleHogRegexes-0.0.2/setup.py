fe467c15540dddcde705bd1e109766fd864f56b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:56 +0000

    Add package truffleHogRegexes/0.0.2/truffleHogRegexes-0.0.2.tar.gz

---
+++ packages/truffleHogRegexes/truffleHogRegexes-0.0.2.tar.gz/truffleHogRegexes-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='truffleHogRegexes',
+    version='0.0.2',
+    description='These regexes power truffleHog.',
+    url='https://github.com/dxa4481/truffleHogRegexes',
+    author='Dylan Ayrey',
+    author_email='dylanayrey@gmail.com',
+    license='GNU',
+    packages = find_packages(),
+)
