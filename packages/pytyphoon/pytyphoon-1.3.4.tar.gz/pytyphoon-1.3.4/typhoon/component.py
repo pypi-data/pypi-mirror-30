f10823c9a7b3e0d7851eab21175dfd7b70e6a25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:36 +0000

    Add package pytyphoon/1.3.4/pytyphoon-1.3.4.tar.gz

---
+++ packages/pytyphoon/pytyphoon-1.3.4.tar.gz/pytyphoon-1.3.4/typhoon/component.py
@@ -0,0 +1,18 @@
+import asyncio
+
+
+class BaseComponent:
+
+    def __init__(self, queues_manager):
+        self.queues_manager = queues_manager
+
+    def run(self):
+        self.queues_manager.start()
+
+    def stop(self):
+        self.queues_manager.stop()
+        for task in asyncio.Task.all_tasks():
+            try:
+                task.cancel()
+            except:
+                pass
