6b5d0ac1e90c89b11a9a5dec136fb60e280a2302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:31 +0000

    Add package funniest_kr/0.0.1/funniest_kr-0.0.1.tar.gz

---
+++ packages/funniest_kr/funniest_kr-0.0.1.tar.gz/funniest_kr-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='funniest_kr',
+	version='0.0.1',
+	description='The AaZae gag for koreans',
+	url='https://github.com/bboyseiok/funniest_kr',
+	author='bboyseiok',
+	author_email='bboyseiok@deepest.ai',
+	licence='BSP',
+	packages=['funniest_kr'],
+	zip_safe=False)
