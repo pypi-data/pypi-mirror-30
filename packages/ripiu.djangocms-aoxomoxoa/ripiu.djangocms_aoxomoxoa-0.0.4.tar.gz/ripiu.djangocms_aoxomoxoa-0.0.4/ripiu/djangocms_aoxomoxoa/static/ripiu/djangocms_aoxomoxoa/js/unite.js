cd56de0608360bb46f330ef544d67e8130e86f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:03 +0000

    Add package ripiu.djangocms-aoxomoxoa/0.0.4/ripiu.djangocms_aoxomoxoa-0.0.4.tar.gz

---
+++ packages/ripiu.djangocms-aoxomoxoa/ripiu.djangocms_aoxomoxoa-0.0.4.tar.gz/ripiu.djangocms_aoxomoxoa-0.0.4/ripiu/djangocms_aoxomoxoa/static/ripiu/djangocms_aoxomoxoa/js/unite.js
@@ -0,0 +1,8 @@
+$(window).on("load", function() {
+    $(".ripiu-unite").each(function() {
+      var confId = $(this).data("unite-conf");
+      var conf = JSON.parse($("#" + confId).html());
+      $(this).unitegallery(conf);
+      $(this).trigger("loaded");
+    });
+});
