a7e2581b495a3be88f7b1216102308c4892c0924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:54 +0000

    Add package menuplaceholder/0.1.5/menuplaceholder-0.1.5.tar.gz

---
+++ packages/menuplaceholder/menuplaceholder-0.1.5.tar.gz/menuplaceholder-0.1.5/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='menuplaceholder',
+    version='0.1.5',
+    description='Placeholder items for Mezzanine menus.',
+    author='Peter Murphy',
+    author_email='peterkmurphy@gmail.com',
+#    url='http://pypi.python.org/pypi/glyphviewer/',
+    packages=['menuplaceholder'],
+    package_data={
+        'menuplaceholder': ['templates/*.html', 'templates/**/*.html'],
+    },
+    include_package_data=True,
+#    keywords = 'font glyph web woff ttf otf TrueType OpenType Django',
+    license='LICENSE.txt',
+    classifiers = [
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        ],
+    long_description=open('README.rst').read(),
+    install_requires = ["Django >= 1.0", "Mezzanine >= 4.0"],
+    zip_safe = False,
+)
