a4e8a20ab7591a37d6e5f342968975e04369b6e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:27 +0000

    Add package py-auth0-jwt/0.2.6/py-auth0-jwt-0.2.6.tar.gz

---
+++ packages/py-auth0-jwt/py-auth0-jwt-0.2.6.tar.gz/py-auth0-jwt-0.2.6/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='py-auth0-jwt',
+    version='0.2.6',
+    packages=['pyauth0jwt',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
