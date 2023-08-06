a82d5d426b609df5bb455009fdfe5eeb4228425d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:35 +0000

    Add package dnaconverter2/0.0.1/dnaconverter2-0.0.1.tar.gz

---
+++ packages/dnaconverter2/dnaconverter2-0.0.1.tar.gz/dnaconverter2-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dnaconverter2',
+    version='0.0.1',
+    description='an algorithm to encode any file into a sequence of ACGT',
+    license='MIT',
+    packages=['dnaconverter2'],
+    author='Anshul Khanna',
+    author_email='anshul17khanna@gmail.com',
+    keywords=['dnaconverter'],
+    url=''
+)
