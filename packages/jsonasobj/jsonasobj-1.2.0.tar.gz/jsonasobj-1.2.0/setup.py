9227b9f9031c10858894d08aa7448180887e8ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:19 +0000

    Add package jsonasobj/1.2.0/jsonasobj-1.2.0.tar.gz

---
+++ packages/jsonasobj/jsonasobj-1.2.0.tar.gz/jsonasobj-1.2.0/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+jsonasobj
+====
+
+jsonasobj is an extension to the core python json library that treats name/value pairs
+as first class attributes whenever possible
+"""
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+import jsonasobj
+
+
+# with open('README.md') as file:
+#     long_description = file.read()
+long_description = """an extension to the core python json library that treats name/value pairs
+as first class attributes whenever possible """
+
+setup(
+    name='jsonasobj',
+    version=jsonasobj.__version__,
+    description='JSON as python objects',
+    long_description=long_description,
+    author='Harold Solbrig',
+    author_email='solbrig.harold@mayo.edu',
+    url='http://github.com/hsolbrig/jsonasobj',
+    packages=['jsonasobj'],
+    tests_require=['yadict-compare'],
+    package_dir={'': 'src'},
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
---
+++ packages/jsonasobj/jsonasobj-1.2.0.tar.gz/jsonasobj-1.2.0/src/jsonasobj/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from jsonasobj.jsonobj import JsonObj, loads, load
+from jsonasobj.extendednamespace import ExtendedNamespace
+
+__copyright__ = 'Copyright (c) 2018, Mayo Clinic'
+__license__ = 'New BSD license'
+__version__ = '1.2.0'
+
+__all__ = ['extendednamespace', 'JsonObj', 'load', 'loads']
