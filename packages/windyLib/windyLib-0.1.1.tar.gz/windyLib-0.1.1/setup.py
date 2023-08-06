21d0866edd1a9d2f37c6052e0cdc7ccfa355e528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:42 +0000

    Add package windyLib/0.1.1/windyLib-0.1.1.tar.gz

---
+++ packages/windyLib/windyLib-0.1.1.tar.gz/windyLib-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name = "windyLib",
+    version = "0.1.1",
+    description = "weather data access and analysis from www.windy.com",
+    author = "Louis He",
+    author_email = "1726110778@qq.com",
+    url = "https://github.com/Louis-He/windyLib",
+    packages= find_packages(),
+    long_description = """weather data access and analysis from www.windy.com"""
+)
