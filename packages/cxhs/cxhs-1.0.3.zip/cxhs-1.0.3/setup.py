71ee51ee04abf26da300ad9552615d0f7dbce317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:45 +0000

    Add package cxhs/1.0.3/cxhs-1.0.3.zip

---
+++ packages/cxhs/cxhs-1.0.3.zip/cxhs-1.0.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# ...
+
+#from distutils.core import setup
+from setuptools import setup
+
+setup(name='cxhs',
+      version='1.0.3',
+      description='cxhs: a python haystack client',
+      author='Connexx Energy, Peter Michalek',
+      author_email='connexxion@connexxenergy.com, peter@michalek.org',
+      maintainer='Connexx Energy',
+      maintainer_email='connexxion@connexxenergy.com',
+      url='http://www.connexxenergy.com/',
+      packages=['cxhs'],
+      long_description="Python haystack client",
+      license="Apache License, Version 2.0",
+      platforms=["any"],
+     )
