2fa5956d966b01d19c682e17460da2f2d91bf295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:49 +0000

    Add package JYTools/1.1.8/JYTools-1.1.8.tar.gz

---
+++ packages/JYTools/JYTools-1.1.8.tar.gz/JYTools-1.1.8/JYTools/JYWorker/UploadLog.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+from time import sleep
+from ._redis import RedisWorker
+
+__author__ = '鹛桑够'
+
+
+class UploadLogWorker(RedisWorker):
+
+    expect_params_type = dict
+
+    def upload_log(self, key, log_path, timestamp):
+        return True
+
+    def handler_task_exception(self, e):
+        self.push_task(self.current_task.task_key, self.current_task.task_params)
+
+    def handle_task(self, key, params):
+        log_path = params["log_path"]
+        timestamp = params["timestamp"]
+        upload_r = self.upload_log(key, log_path, timestamp)
+        if upload_r is True:
+            self.task_log("Upload ", log_path, " Success")
+            l = self.stat_man.remove_queue_task(self.work_tag, key)
+            sleep(2)
+            return
+        self.task_log("Upload ", log_path, " Fail")
+        self.push_task(key, params)
