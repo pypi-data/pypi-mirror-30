01bace772116c4ffe998ac45d3dcd7252a73bac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:45 +0000

    Add package ybc-rmb/1.0.0/ybc_rmb-1.0.0.tar.gz

---
+++ packages/ybc-rmb/ybc_rmb-1.0.0.tar.gz/ybc_rmb-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ybc_rmb',
+  packages = ['ybc_rmb'],
+  version = '1.0.0',
+  description = 'YBC Module',
+  long_description='YBC Module',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python'],
+  license='MIT',
+  install_requires=['ybc_face','ybc_speech','ybc_box']
+)
---
+++ packages/ybc-rmb/ybc_rmb-1.0.0.tar.gz/ybc_rmb-1.0.0/ybc_rmb/ybc_test.py
@@ -0,0 +1,11 @@
+import ybc_box
+import ybc_face
+import ybc_speech
+
+
+def main():
+    ybc_box.msgbox('测试Msg Box')
+
+
+if __name__ == '__main__':
+    main()
