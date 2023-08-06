e02dc2d18b412ffaa731b038c9ffa1610b30689d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:46 +0000

    Add package MultiRunner/4.2/MultiRunner-4.2.tar.gz

---
+++ packages/MultiRunner/MultiRunner-4.2.tar.gz/MultiRunner-4.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name = "MultiRunner",
+    version = "4.2",
+    description= "Change class name, change some method to private method.",
+    long_description= "This is a package for multi-process running, dedicated to my girlfriend 10L",
+    url='https://github.com/Statham-stone/MultiRunner',
+    author = "Statham",
+    author_email = "statham.stone@gmail.com",
+    packages=[""]
+)
