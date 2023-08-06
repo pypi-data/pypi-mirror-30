33d186b5de3d8513e1ce7a77c3eae669b1e90586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:43 +0000

    Add package ta/0.1.4/ta-0.1.4.tar.gz

---
+++ packages/ta/ta-0.1.4.tar.gz/ta-0.1.4/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+    name = 'ta',
+    packages = ['ta'],
+    version = '0.1.4',
+    description='Technical Analysis Library in Python',
+    long_description='It is a technical analysis library to financial time series datasets. You can use to do feature engineering. It is builded on pandas python library.',
+    author = 'Dario Lopez Padial (Bukosabino)',
+    author_email = 'bukosabino@gmail.com',
+    url = 'https://github.com/bukosabino/ta',
+    maintainer='Dario Lopez Padial (Bukosabino)',
+    maintainer_email='bukosabino@gmail.com',
+    install_requires=[
+        'numpy',
+        'pandas'
+    ],
+    download_url = 'https://github.com/bukosabino/ta/tarball/0.1.4',
+    keywords = ['technical analysis', 'python3', 'pandas', 'numpy',
+                'fundamental analysis'],
+    license='The MIT License (MIT)',
+    classifiers = [],
+)
---
+++ packages/ta/ta-0.1.4.tar.gz/ta-0.1.4/ta/utils.py
@@ -0,0 +1,11 @@
+import math
+import pandas as pd
+
+
+def dropna(df):
+    """Drop rows with "Nans" values
+    """
+    df = df[df < math.exp(709)] # big number
+    df = df[df != 0.0]
+    df = df.dropna()
+    return df
