1b04aefae42d25028e1626eb6de9839e00bf5dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:25 +0000

    Add package deos.net/0.1/deos.net-0.1.tar.gz

---
+++ packages/deos.net/deos.net-0.1.tar.gz/deos.net-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='deos.net',
+      version='0.1',
+      description='deos.net',
+      url='https://github.com/desantis/python-deos.net',
+      author='Andrew T. DeSantis',
+      author_email='atd@deos.org',
+      license='MIT',
+      packages=['deos.net'],
+      zip_safe=False)
