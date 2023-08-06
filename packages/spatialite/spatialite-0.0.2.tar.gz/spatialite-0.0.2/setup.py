0703a06402f91dfa3dc2df07d0e001f3a937e0c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:54 +0000

    Add package spatialite/0.0.2/spatialite-0.0.2.tar.gz

---
+++ packages/spatialite/spatialite-0.0.2.tar.gz/spatialite-0.0.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+
+cwd = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(cwd, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='spatialite',
+    version='0.0.2',
+
+    description='Wrapper of sqlite3 module which adds SpatiaLite support.',
+    long_description=long_description,
+    url='https://github.com/malexer/spatialite',
+
+    author='Alex (Oleksii) Markov',
+    author_email='alex@markovs.me',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Database :: Database Engines/Servers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    keywords=('sqlite sqlite3 spatialite'),
+
+    packages=['spatialite'],
+)
