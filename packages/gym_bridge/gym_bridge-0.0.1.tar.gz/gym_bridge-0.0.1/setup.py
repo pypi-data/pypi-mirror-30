728c921c3cf7db8428b84dd6f03eb5ee32a5a5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:50 +0000

    Add package gym_bridge/0.0.1/gym_bridge-0.0.1.tar.gz

---
+++ packages/gym_bridge/gym_bridge-0.0.1.tar.gz/gym_bridge-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gym_bridge',
+      version='0.0.1',
+      author='Taylor Denouden',
+      author_email='taylordenouden@gmail.com',
+      url = 'https://github.com/tayden/gym-bridge',
+      install_requires=['gym', 'six', 'numpy']  # And any other dependencies foo needs
+)
