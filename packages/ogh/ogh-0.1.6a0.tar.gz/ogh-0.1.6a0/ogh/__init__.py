23cb0336250f18e55a340fcb0a6b845c21a732fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:42 +0000

    Add package ogh/0.1.6a0/ogh-0.1.6a0.tar.gz

---
+++ packages/ogh/ogh-0.1.6a0.tar.gz/ogh-0.1.6a0/ogh/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import (absolute_import,
+                        division,
+                        print_function,
+                        unicode_literals)
+
+from ogh.ogh import *  # noqa
+
+__author__ = 'Jimmy Phuong'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
