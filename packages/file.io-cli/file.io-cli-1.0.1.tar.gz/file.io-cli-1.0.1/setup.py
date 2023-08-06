528231201884d8cb1c3cfe35af6651eb20f63888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:36 +0000

    Add package file.io-cli/1.0.1/file.io-cli-1.0.1.tar.gz

---
+++ packages/file.io-cli/file.io-cli-1.0.1.tar.gz/file.io-cli-1.0.1/setup.py
@@ -0,0 +1,22 @@
+
+import setuptools
+
+with open('requirements.txt') as fp:
+  install_reqs = fp.readlines()
+
+setuptools.setup(
+  name = 'file.io-cli',
+  version = '1.0.1',
+  author = 'Niklas Rosenstein',
+  author_email = 'rosensteinniklas@gmail.com',
+  license = 'MIT',
+  description = 'Command-line tool to upload files to https://file.io',
+  url = 'https://github.com/NiklasRosenstein/file.io-cli',
+  install_requires = install_reqs,
+  py_modules = ['file_io_cli'],
+  entry_points = dict(
+    console_scripts = [
+      'file.io = file_io_cli:_entry_point'
+    ]
+  )
+)
