fb501952c013656dd9962136494d5c6849402a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:40 +0000

    Add package physical-device-manager/1.0.6/physical-device-manager-1.0.6.tar.gz

---
+++ packages/physical-device-manager/physical-device-manager-1.0.6.tar.gz/physical-device-manager-1.0.6/setup.py
@@ -0,0 +1,38 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    readme_file_path = os.path.join(os.path.dirname(__file__), fname)
+
+    if os.path.exists(readme_file_path) and os.path.isfile(readme_file_path):
+        readme_file = open(readme_file_path)
+        return readme_file.read()
+    else:
+        return "The SoftFIRE Physical Device Manager"
+
+
+setup(
+    name="physical-device-manager",
+    version="1.0.6",
+    author="SoftFIRE",
+    author_email="softfire@softfire.eu",
+    description="The SoftFIRE Physical Device Manager",
+    license="Apache 2",
+    keywords="python vnfm nfvo open baton openbaton sdk experiment manager softfire tosca openstack rest",
+    url="http://softfire.eu/",
+    packages=find_packages(),
+    scripts=["physical-device-manager"],
+    install_requires=[
+        'softfire-sdk==1.1.4',
+        'requests',
+        'PyYAML'
+    ],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
