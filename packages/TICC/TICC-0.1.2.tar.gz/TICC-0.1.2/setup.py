40375e341a67de5dfcd30c9aae41971fd26a970d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:35 +0000

    Add package TICC/0.1.2/TICC-0.1.2.tar.gz

---
+++ packages/TICC/TICC-0.1.2.tar.gz/TICC-0.1.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+	name='TICC',
+	version='0.1.2',
+	description='Solver for Toeplitz Inverse Covariance-based Clustering (TICC)',
+	url='https://github.com/davidhallac/ticc',
+	py_modules=['ticc/ticc']
+	)
