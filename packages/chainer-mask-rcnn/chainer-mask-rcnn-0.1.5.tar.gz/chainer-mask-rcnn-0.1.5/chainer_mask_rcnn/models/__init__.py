bf91f3a1266c1418005b55d51582d15eeb0c71ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:10 +0000

    Add package chainer-mask-rcnn/0.1.5/chainer-mask-rcnn-0.1.5.tar.gz

---
+++ packages/chainer-mask-rcnn/chainer-mask-rcnn-0.1.5.tar.gz/chainer-mask-rcnn-0.1.5/chainer_mask_rcnn/models/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+from .faster_rcnn_resnet import FasterRCNNResNet
+from .mask_rcnn import MaskRCNN
+from .mask_rcnn_resnet import MaskRCNNResNet
+from .mask_rcnn_train_chain import MaskRCNNTrainChain
+from .mask_rcnn_vgg import MaskRCNNVGG16
+from .mask_rcnn_vgg import VGG16RoIHead
+from .rpn_train_chain import RPNTrainChain
