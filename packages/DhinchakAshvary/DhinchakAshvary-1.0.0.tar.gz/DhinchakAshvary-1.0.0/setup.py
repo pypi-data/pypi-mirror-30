7999cf3685e6c22cab8bf7050f655110397490c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:48 +0000

    Add package DhinchakAshvary/1.0.0/DhinchakAshvary-1.0.0.tar.gz

---
+++ packages/DhinchakAshvary/DhinchakAshvary-1.0.0.tar.gz/DhinchakAshvary-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+packages = find_packages()
+
+setup(
+    name="DhinchakAshvary",
+    version="1.0.0",
+    description="my first python library",
+    packages=packages
+)
