48778dcb04ce659b8c1985c9af8efffda8b8c572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:34 +0000

    Add package ybc-browser/1.0.0/ybc_browser-1.0.0.tar.gz

---
+++ packages/ybc-browser/ybc_browser-1.0.0.tar.gz/ybc_browser-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'ybc_browser',
+  packages = ['ybc_browser'],
+  version = '1.0.0',
+  description = 'ybc_browser',
+  long_description='ybc_browser',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'browser', 'python3','python'],
+  license='MIT',
+  install_requires=[
+        'ybc_pinyin'
+    ],
+)
