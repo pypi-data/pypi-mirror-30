1c9d849a7481ff4fee25caec6abc90d402cf6051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:52 +0000

    Add package LIMBR/0.2.8/LIMBR-0.2.8.tar.gz

---
+++ packages/LIMBR/LIMBR-0.2.8.tar.gz/LIMBR-0.2.8/LIMBR/__init__.py
@@ -0,0 +1,10 @@
+from LIMBR import imputable
+from LIMBR import sva
+from LIMBR import old_fashioned
+from LIMBR import simulations
+
+"""
+The package LIMBR consists of three modules.  imputable performs imputation of missing data using K nearest neighbors.  sva performs modelling and removal of batch effects. old_fashioned provides normalization methods without 
+batch effect removal as a baseline for the results of sva.
+
+"""
