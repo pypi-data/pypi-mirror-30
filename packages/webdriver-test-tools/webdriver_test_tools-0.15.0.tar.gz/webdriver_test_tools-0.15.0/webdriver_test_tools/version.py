dae9af4cc09477951d625e41548f0a3da8b7b265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:49 +0000

    Add package webdriver-test-tools/0.15.0/webdriver_test_tools-0.15.0.tar.gz

---
+++ packages/webdriver-test-tools/webdriver_test_tools-0.15.0.tar.gz/webdriver_test_tools-0.15.0/webdriver_test_tools/version.py
@@ -0,0 +1,11 @@
+"""Version information about the package
+
+:var __version__: The version number for the package
+:var __devstatus__: PyPI classifier for current development status on the project
+"""
+
+__version__ = '0.15.0'
+
+# Development Status classifier
+__devstatus__ = 'Development Status :: 2 - Pre-Alpha'
+
