0f2938f9fe9b9f6d3d7dd07544a37f938419bf05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:21 +0000

    Add package fortnite/0.0.5/fortnite-0.0.5.tar.gz

---
+++ packages/fortnite/fortnite-0.0.5.tar.gz/fortnite-0.0.5/pfaw/core.py
@@ -0,0 +1,81 @@
+from . import constants
+from . import objects
+import requests
+
+
+class Fortnite:
+    def __init__(self, fortnite_token, launcher_token, password, email):
+        password_response = requests.post(constants.token, headers={'Authorization': f'basic {launcher_token}'},
+                                          data={'grant_type': 'password', 'username': '{}'.format(email),
+                                                'password': '{}'.format(password), 'includePerms': True}).json()
+        access_token = password_response.get('access_token')
+
+        exchange_response = requests.get(constants.exchange,
+                                         headers={'Authorization': 'bearer {}'.format(access_token)}).json()
+        code = exchange_response.get('code')
+
+        token_response = requests.post(constants.token, headers={'Authorization': 'basic {}'.format(fortnite_token)},
+                                       data={'grant_type': 'exchange_code', 'exchange_code': '{}'.format(code),
+                                             'includePerms': True, 'token_type': 'egl'}).json()
+
+        self.access_token = token_response.get('access_token')
+        self.refresh_token = token_response.get('refresh_token')
+        self.expires_at = token_response.get('expires_at')
+        self.session = Session(self.access_token)
+
+    def player(self, username):
+        """Return object containing player name and id"""
+        response = self.session.get(constants.player.format(username))
+        return objects.Player(response)
+
+    def battle_royale_stats(self, username, mode, platform):
+        """Return object containing Battle Royale stats
+
+        mode can be solo, duo, squad, or all.
+        platform can be pc, ps4, or xb1.
+
+        Example: Fortnite.battle_royale_stats('Jimmy', 'solo', 'pc')
+
+        """
+
+        player_id = self.player(username).id
+        response = self.session.get(constants.battle_royale.format(player_id))
+        return objects.BattleRoyale(response=response, mode=mode.lower(), platform=platform.lower())
+
+    def friends(self, username):
+        """Return list of player ids. This method only works for the authenticated account."""
+        player_id = self.player(username).id
+        response = self.session.get(constants.friends.format(player_id))
+        return [friend.get('accountId') for friend in response]
+
+    def store(self, rw=-1):
+        """Return current store items. This method only works for the authenticated account."""
+        response = self.session.get(constants.store.format(rw))
+        return objects.Store(response)
+
+    @staticmethod
+    def news():
+        """Get the current news on fortnite."""
+        response = requests.get(constants.news, headers={'Accept-Language': 'en'})
+        return objects.News(status=response.status_code, response=response.json())
+
+    @staticmethod
+    def server_status():
+        """Check the status of the Fortnite servers. Returns True if up and False if down."""
+        response = requests.get(constants.status)
+        if response.json()[0]['status'] == 'UP':
+            return True
+        else:
+            return False
+
+
+class Session:
+    def __init__(self, access_token):
+        self.session = requests.Session()
+        self.session.headers.update({'Authorization': f'bearer {access_token}'})
+
+    def get(self, endpoint):
+        response = self.session.get(endpoint)
+        if response.status_code != 200:
+            response.raise_for_status()
+        return response.json()
---
+++ packages/fortnite/fortnite-0.0.5.tar.gz/fortnite-0.0.5/pfaw/objects.py
@@ -0,0 +1,135 @@
+class Base:
+    def __init__(self, response):
+        self.response = response
+
+
+class Player(Base):
+    def __init__(self, response):
+        super().__init__(response)
+        self.name = self.response.get('displayName')
+        self.id = self.response.get('id')
+
+
+class BattleRoyale:
+    def __init__(self, response, mode, platform):
+        self.score = 0
+        self.matches = 0
+        self.time = 0
+        self.kills = 0
+        self.wins = 0
+        self.top3 = 0
+        self.top5 = 0
+        self.top6 = 0
+        self.top10 = 0
+        self.top12 = 0
+        self.top25 = 0
+
+        if mode == 'solo':
+            mode = '_p2'
+        elif mode == 'duo':
+            mode = '_p10'
+        elif mode == 'squad':
+            mode = '_p9'
+        elif mode == 'all':
+            mode = '_p'
+
+        for i in response:
+            if mode in i['name'] and platform in i['name']:
+                if 'score_' in i['name']:
+                    self.score += i['value']
+                elif 'matchesplayed_' in i['name']:
+                    self.matches += i['value']
+                elif 'minutesplayed_' in i['name']:
+                    self.time += i['value']
+                elif 'kills_' in i['name']:
+                    self.kills += i['value']
+                elif 'placetop1_' in i['name']:
+                    self.wins += i['value']
+                elif 'placetop3_' in i['name']:
+                    self.top3 += i['value']
+                elif 'placetop5_' in i['name']:
+                    self.top5 += i['value']
+                elif 'placetop6_' in i['name']:
+                    self.top6 += i['value']
+                elif 'placetop10_' in i['name']:
+                    self.top10 += i['value']
+                elif 'placetop12_' in i['name']:
+                    self.top12 += i['value']
+                elif 'placetop25_' in i['name']:
+                    self.top25 += i['value']
+
+
+class Store(Base):
+    def __init__(self, response):
+        super().__init__(response)
+        self.refresh_interval_hrs = self.response.get('refreshIntervalHrs')
+        self.daily_purchase_hrs = self.response.get('dailyPurchaseHrs')
+        self.expiration = self.response.get('expiration')
+        self.storefronts = self.storefront_list()
+
+    def storefront_list(self):
+        return [StoreFront(response) for response in self.response.get('storefronts')]
+
+
+class StoreFront(Base):
+    def __init__(self, response):
+        super().__init__(response)
+        self.name = self.response.get('name')
+        self.catalog_entries = self.catalog_entry_list()
+
+    def catalog_entry_list(self):
+        return [CatalogEntry(response) for response in self.response.get('catalogEntries')]
+
+
+class CatalogEntry(Base):
+    def __init__(self, response):
+        super().__init__(response)
+        self.offer_id = self.response.get('offerId')
+        self.dev_name = self.response.get('devName')
+        self.offer_type = self.response.get('offerType')
+        self.prices = self.price_list()
+        self.title = self.response.get('title')
+        self.description = self.response.get('description')
+        self.refundable = self.response.get('refundable')
+
+    def price_list(self):
+        return [Price(response) for response in self.response.get('prices')]
+
+
+class Price(Base):
+    def __init__(self, response):
+        super().__init__(response)
+        self.currency_type = self.response.get('currencyType')
+        self.regular_price = self.response.get('regularPrice')
+        self.final_price = self.response.get('finalPrice')
+        self.sale_expiration = self.response.get('saleExpiration')
+        self.base_price = self.response.get('basePrice')
+
+
+class News:
+    def __init__(self, status, response):
+        self.status = status
+
+        common = response.get('athenamessage').get('overrideablemessage')
+        if common.get('message') is not None:
+            self.common = [common.get('message')]
+        elif common.get('messages') is not None:
+            self.common = common.get('messages')
+        else:
+            self.common = None
+
+        br = response.get('battleroyalenews').get('news')
+        if br.get('message') is not None:
+            self.br = [br.get('message')]
+        elif br.get('messages') is not None:
+            self.br = br.get('messages')
+        else:
+            self.br = None
+
+        login = response.get('loginmessage').get('loginmessage')
+        if login.get('message') is not None:
+            self.login = [login.get('message')]
+        elif login.get('messages') is not None:
+            self.login = login.get('messages')
+        else:
+            self.login = None
