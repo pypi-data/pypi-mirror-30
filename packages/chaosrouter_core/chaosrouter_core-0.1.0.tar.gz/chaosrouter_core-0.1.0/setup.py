92357dbef1998fdaa5e305fd52bb66065a5690ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:18 +0000

    Add package chaosrouter_core/0.1.0/chaosrouter_core-0.1.0.tar.gz

---
+++ packages/chaosrouter_core/chaosrouter_core-0.1.0.tar.gz/chaosrouter_core-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name                        = "chaosrouter_core",
+    packages                    = [
+        "chaosrouter_core",
+    ],
+    package_dir                 = {
+        "chaosrouter_core":	".",
+    },
+    install_requires            = [
+        'restruct',
+    ],
+    version                     = "0.1.0",
+    include_package_data        = True,
+    author                      = "Matthew Brisebois",
+    author_email                = "matthew@webheroes.ca",
+    url                         = "https://github.com/webheroesinc/",
+    license                     = "Dual License; GPLv3 and Proprietary",
+    description                 = "Core suite of basic useful directives",
+    long_description            = """
+    Core suite of basic useful directives
+    """,
+    keywords                    = [""],
+    classifiers                 = [
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "License :: Other/Proprietary License",
+        "Programming Language :: Python :: 3.5",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers"
+    ],
+),
+
