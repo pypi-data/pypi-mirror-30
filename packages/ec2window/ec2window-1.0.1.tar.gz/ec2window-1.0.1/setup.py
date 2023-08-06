b2e7d2365bc419ab671fb0c25ffaa089aa6b826d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:59 +0000

    Add package ec2window/1.0.1/ec2window-1.0.1.tar.gz

---
+++ packages/ec2window/ec2window-1.0.1.tar.gz/ec2window-1.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# pylint: disable=missing-docstring
+
+import setuptools
+
+setuptools.setup(
+    name='ec2window',
+    author='Nicholas Bishop',
+    author_email='nicholasbishop@gmail.com',
+    version='1.0.1',
+    description='Simple EC2 instance context manager',
+    url='https://github.com/nicholasbishop/ec2window')
