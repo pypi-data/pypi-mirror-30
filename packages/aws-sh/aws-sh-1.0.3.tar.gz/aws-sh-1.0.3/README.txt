198114b167454de9af0a6c056c37dabfa3b2e343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:34 +0000

    Add package aws-sh/1.0.3/aws-sh-1.0.3.tar.gz

---
+++ packages/aws-sh/aws-sh-1.0.3.tar.gz/aws-sh-1.0.3/README.txt
@@ -0,0 +1,6 @@
+A shell for aws commands to manage AWS EC2 and S3
+
+install with python setup.py install
+
+Run with:
+	python -m aws-sh
---
+++ packages/aws-sh/aws-sh-1.0.3.tar.gz/aws-sh-1.0.3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="aws-sh",
+
+
+    # Version number (initial):
+    version=open("aws-sh/VERSION.txt").readline().rstrip(),
+
+    # Application author details:
+    author="Steven hk Wong",
+    author_email="steven@wongsrus.net",
+
+    # Packages
+    packages=["aws-sh", "aws-sh/classes"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="http://pypi.python.org/pypi/aws-sh/",
+
+    #
+    license="LICENSE.txt",
+    description="A shell for AWS commands.",
+
+    long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "boto3","aws-cmd",
+    ],
+)
