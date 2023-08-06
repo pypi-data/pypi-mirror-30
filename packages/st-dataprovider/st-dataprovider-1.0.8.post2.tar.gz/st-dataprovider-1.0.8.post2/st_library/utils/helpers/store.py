98f681feedd34301e1358d75000ce9ddb1b5a2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:15 +0000

    Add package st-dataprovider/1.0.8.post2/st-dataprovider-1.0.8.post2.tar.gz

---
+++ packages/st-dataprovider/st-dataprovider-1.0.8.post2.tar.gz/st-dataprovider-1.0.8.post2/st_library/utils/helpers/store.py
@@ -0,0 +1,17 @@
+import os
+
+
+class Store(object):
+    def __init__(self):
+        raise TypeError('This class should not be initialized')
+
+    config_id = os.environ.get('CONFIGURATION_ID')
+
+    if os.environ.get('SEC_REFRESH_TOKEN'):
+        token = os.environ['SEC_REFRESH_TOKEN']
+    else:
+        token = None
+
+    performance_id = os.environ.get('PERFORMANCE_ID')
+
+    token_data = dict()
