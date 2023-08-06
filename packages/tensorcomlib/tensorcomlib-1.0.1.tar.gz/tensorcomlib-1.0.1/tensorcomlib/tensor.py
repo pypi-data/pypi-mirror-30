65d3598f19f638e8506967d03d214ff986c4cb08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:30 +0000

    Add package tensorcomlib/1.0.1/tensorcomlib-1.0.1.tar.gz

---
+++ packages/tensorcomlib/tensorcomlib-1.0.1.tar.gz/tensorcomlib-1.0.1/tensorcomlib/tensor.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class tensor(object):
+
+	data = None
+	shape = None
+	dtype = None
+
+	def __init__(self, data = None, shape = None):
+		super(tensor, self).__init__()
+		self.data = np.array(data)
+		self.shape = tuple(shape)
+		self.dtype = data.dtype
+
+	def size(self):
+		ret = 1
+		for i in range(0,len(self.shape)):
+			ret = ret * self.shape[i]
+		return  ret
+
+	def copy(self):
+		return tensor(self.data.copy(),self.shape)
+
+	def dimsize(self, ind):
+		return self.shape[ind]
+
+	def ndims(self):
+		return len(self.shape)
+
+	def tendtype(self):
+		return self.dtype
