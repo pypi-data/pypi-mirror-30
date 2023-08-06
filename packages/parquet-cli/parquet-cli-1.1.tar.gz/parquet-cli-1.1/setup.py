7ecf493fa99429bdfe10f7a2477876f2dd3bfae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:54 +0000

    Add package parquet-cli/1.1/parquet-cli-1.1.tar.gz

---
+++ packages/parquet-cli/parquet-cli-1.1.tar.gz/parquet-cli-1.1/setup.py
@@ -0,0 +1,37 @@
+
+from setuptools import setup, find_packages
+
+msg = "Command line (CLI) tool to inspect Apache Parquet files on the go"
+
+setup(
+    name='parquet-cli',
+    version='1.1',
+    description=msg,
+    long_description=msg,
+    author='Nar Kumar Chhantyal',
+    author_email='nkchhantyal@gmail.com',
+    url='https://github.com/chhantyal/parquet-cli',
+    license='BSD 3-Clause License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    packages=find_packages(),
+    platforms='any',
+    install_requires=[
+        'pandas==0.22.0',
+        'pyarrow==0.9.0.post1'
+    ],
+    entry_points={
+        'console_scripts': [
+            'parq = parq.main:main',
+        ]
+    },
+    package_data={'example': ['*.parquet']},
+    include_package_data=True,
+)
