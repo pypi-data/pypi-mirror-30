38a2cd3a167927d2754bcab16e49ea56c2aa6ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:42 +0000

    Add package slimoore/0.1.4/slimoore-0.1.4.tar.gz

---
+++ packages/slimoore/slimoore-0.1.4.tar.gz/slimoore-0.1.4/slimoore/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.http import Http404
+from django.urls import reverse
+from django.views import generic
+from django.template import loader
+
+from .models import Question, Choice
+
+
+class IndexView(generic.ListView):
+    template_name = 'slimoore/change_list.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'slimoore/base.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'slimoore/results.html'
+
+
+# def index(request):
+#     # return HttpResponse("Hello, world. You're at the polls index.")
+#
+#     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # output = ', '.join([q.question_text for q in latest_question_list])
+#     # return HttpResponse(output)
+#
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # template = loader.get_template('newpolls/change.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     # return HttpResponse(template.render(context, request))
+#     return render(request, 'newpolls/change.html', context)
+#     # return render(request, "change.html", context)
+#
+#
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'newpolls/base.html', {'question': question})
+#
+#
+# def results(request, question_id):
+#     # response = "You're looking at the results of question %s."
+#     # return HttpResponse(response % question_id)
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'newpolls/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'newpolls/base.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('newpolls:results', args=(question.id,)))
