ce72b13358e694a64ca2125ed7ed694939dd3bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:03 +0000

    Add package mastercard-places/1.0.6/mastercard_places-1.0.6-py2-none-any.whl

---
+++ packages/mastercard-places/mastercard_places-1.0.6-py2-none-any.whl/mastercardplaces/__init__.py
@@ -0,0 +1,11 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardplaces.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardplaces.merchantcategorycodes import *
+from mastercardplaces.merchantindustries import *
+from mastercardplaces.merchantpointofinterest import *
+from mastercardplaces.placebylocationid import *
