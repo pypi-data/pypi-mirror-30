ed294a804d65c77b505eccbbe6b3cf12542a1db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:52 +0000

    Add package void-reconstructor/1.0.2/void_reconstructor-1.0.2.tar.gz

---
+++ packages/void-reconstructor/void_reconstructor-1.0.2.tar.gz/void_reconstructor-1.0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='void_reconstructor',    # This is the name of your PyPI-package.
+    version='1.0.2',# Update the version number for new releases
+    author_email = 'tobias.meier@epfl.ch',
+    packages=['void_reconstructor']                  # The name of your scipt, and also the command you'll be using for calling it
+    )
