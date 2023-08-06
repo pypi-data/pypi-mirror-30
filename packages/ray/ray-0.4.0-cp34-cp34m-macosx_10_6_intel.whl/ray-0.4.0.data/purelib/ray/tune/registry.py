050903a1e730f055cf9047d9b19b51987e8760a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:00 +0000

    Add package ray/0.4.0/ray-0.4.0-cp34-cp34m-macosx_10_6_intel.whl

---
+++ packages/ray/ray-0.4.0-cp34-cp34m-macosx_10_6_intel.whl/ray-0.4.0.data/purelib/ray/tune/registry.py
@@ -0,0 +1,109 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from types import FunctionType
+
+import numpy as np
+
+import ray
+from ray.local_scheduler import ObjectID
+
+TRAINABLE_CLASS = "trainable_class"
+ENV_CREATOR = "env_creator"
+RLLIB_MODEL = "rllib_model"
+RLLIB_PREPROCESSOR = "rllib_preprocessor"
+KNOWN_CATEGORIES = [
+    TRAINABLE_CLASS, ENV_CREATOR, RLLIB_MODEL, RLLIB_PREPROCESSOR]
+
+
+def register_trainable(name, trainable):
+    """Register a trainable function or class.
+
+    Args:
+        name (str): Name to register.
+        trainable (obj): Function or tune.Trainable clsas. Functions must
+            take (config, status_reporter) as arguments and will be
+            automatically converted into a class during registration.
+    """
+
+    from ray.tune.trainable import Trainable, wrap_function
+
+    if isinstance(trainable, FunctionType):
+        trainable = wrap_function(trainable)
+    if not issubclass(trainable, Trainable):
+        raise TypeError(
+            "Second argument must be convertable to Trainable", trainable)
+    _default_registry.register(TRAINABLE_CLASS, name, trainable)
+
+
+def register_env(name, env_creator):
+    """Register a custom environment for use with RLlib.
+
+    Args:
+        name (str): Name to register.
+        env_creator (obj): Function that creates an env.
+    """
+
+    if not isinstance(env_creator, FunctionType):
+        raise TypeError(
+            "Second argument must be a function.", env_creator)
+    _default_registry.register(ENV_CREATOR, name, env_creator)
+
+
+def get_registry():
+    """Use this to access the registry. This requires ray to be initialized."""
+
+    _default_registry.flush_values_to_object_store()
+
+    # returns a registry copy that doesn't include the hard refs
+    return _Registry(_default_registry._all_objects)
+
+
+def _to_pinnable(obj):
+    """Converts obj to a form that can be pinned in object store memory.
+
+    Currently only numpy arrays are pinned in memory, if you have a strong
+    reference to the array value.
+    """
+
+    return (obj, np.zeros(1))
+
+
+def _from_pinnable(obj):
+    """Retrieve from _to_pinnable format."""
+
+    return obj[0]
+
+
+class _Registry(object):
+    def __init__(self, objs=None):
+        self._all_objects = {} if objs is None else objs.copy()
+        self._refs = []  # hard refs that prevent eviction of objects
+
+    def register(self, category, key, value):
+        if category not in KNOWN_CATEGORIES:
+            from ray.tune import TuneError
+            raise TuneError("Unknown category {} not among {}".format(
+                category, KNOWN_CATEGORIES))
+        self._all_objects[(category, key)] = value
+
+    def contains(self, category, key):
+        return (category, key) in self._all_objects
+
+    def get(self, category, key):
+        value = self._all_objects[(category, key)]
+        if type(value) == ObjectID:
+            return _from_pinnable(ray.get(value))
+        else:
+            return value
+
+    def flush_values_to_object_store(self):
+        for k, v in self._all_objects.items():
+            if type(v) != ObjectID:
+                obj = ray.put(_to_pinnable(v))
+                self._all_objects[k] = obj
+                self._refs.append(ray.get(obj))
+
+
+_default_registry = _Registry()
---
+++ packages/ray/ray-0.4.0-cp34-cp34m-macosx_10_6_intel.whl/ray-0.4.0.data/purelib/ray/tune/tune.py
@@ -0,0 +1,94 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import time
+
+from ray.tune import TuneError
+from ray.tune.hyperband import HyperBandScheduler
+from ray.tune.async_hyperband import AsyncHyperBandScheduler
+from ray.tune.median_stopping_rule import MedianStoppingRule
+from ray.tune.trial import Trial, DEBUG_PRINT_INTERVAL
+from ray.tune.log_sync import wait_for_log_sync
+from ray.tune.trial_runner import TrialRunner
+from ray.tune.trial_scheduler import FIFOScheduler
+from ray.tune.web_server import TuneServer
+from ray.tune.variant_generator import generate_trials
+from ray.tune.experiment import Experiment
+
+
+_SCHEDULERS = {
+    "FIFO": FIFOScheduler,
+    "MedianStopping": MedianStoppingRule,
+    "HyperBand": HyperBandScheduler,
+    "AsyncHyperBand": AsyncHyperBandScheduler,
+}
+
+
+def _make_scheduler(args):
+    if args.scheduler in _SCHEDULERS:
+        return _SCHEDULERS[args.scheduler](**args.scheduler_config)
+    else:
+        raise TuneError(
+            "Unknown scheduler: {}, should be one of {}".format(
+                args.scheduler, _SCHEDULERS.keys()))
+
+
+def run_experiments(experiments, scheduler=None, with_server=False,
+                    server_port=TuneServer.DEFAULT_PORT, verbose=True):
+    """Tunes experiments.
+
+    Args:
+        experiments (Experiment | list | dict): Experiments to run.
+        scheduler (TrialScheduler): Scheduler for executing
+            the experiment. Choose among FIFO (default), MedianStopping,
+            AsyncHyperBand, or HyperBand.
+        with_server (bool): Starts a background Tune server. Needed for
+            using the Client API.
+        server_port (int): Port number for launching TuneServer.
+        verbose (bool): How much output should be printed for each trial.
+    """
+
+    # Make sure rllib agents are registered
+    from ray import rllib  # noqa # pylint: disable=unused-import
+
+    if scheduler is None:
+        scheduler = FIFOScheduler()
+
+    runner = TrialRunner(
+        scheduler, launch_web_server=with_server, server_port=server_port)
+
+    if type(experiments) is dict:
+        for name, spec in experiments.items():
+            for trial in generate_trials(spec, name):
+                trial.set_verbose(verbose)
+                runner.add_trial(trial)
+    elif (type(experiments) is list and
+          all(isinstance(exp, Experiment) for exp in experiments)):
+        for experiment in experiments:
+            for trial in experiment.trials():
+                trial.set_verbose(verbose)
+                runner.add_trial(trial)
+    elif isinstance(experiments, Experiment):
+        for trial in experiments.trials():
+            trial.set_verbose(verbose)
+            runner.add_trial(trial)
+
+    print(runner.debug_string(max_debug=99999))
+
+    last_debug = 0
+    while not runner.is_finished():
+        runner.step()
+        if time.time() - last_debug > DEBUG_PRINT_INTERVAL:
+            print(runner.debug_string())
+            last_debug = time.time()
+
+    print(runner.debug_string(max_debug=99999))
+
+    for trial in runner.get_trials():
+        # TODO(rliaw): What about errored?
+        if trial.status != Trial.TERMINATED:
+            raise TuneError("Trial did not complete", trial)
+
+    wait_for_log_sync()
+    return runner.get_trials()
