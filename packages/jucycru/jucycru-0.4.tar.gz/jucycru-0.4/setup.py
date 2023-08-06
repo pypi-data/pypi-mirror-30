e3fd4deae5a25ffd244e43672139fda2b02ddfd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:06 +0000

    Add package jucycru/0.4/jucycru-0.4.tar.gz

---
+++ packages/jucycru/jucycru-0.4.tar.gz/jucycru-0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='jucycru',
+      version='0.4',
+      description='JUICE cruise phase package',
+      url='https://bitbucket.org/btorn/jucycru',
+      author='Benjamin Torn',
+      author_email='btorn@cosmos.esa.int',
+      license='MIT',
+      packages=['jucycru'],
+      install_requires=[
+                        'spiceypy',
+                        'matplotlib',
+                        'numpy',
+                        ],
+      scripts=['bin/distExampleScript.py'],
+      test_suite='nose.collector',
+      test_require=['nose'],
+      zip_safe=False)
