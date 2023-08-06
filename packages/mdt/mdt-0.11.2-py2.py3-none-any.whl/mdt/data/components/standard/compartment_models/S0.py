0645dcf3557aa94d14f6dd354eb7c31190544651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:37 +0000

    Add package mdt/0.11.2/mdt-0.11.2-py2.py3-none-any.whl

---
+++ packages/mdt/mdt-0.11.2-py2.py3-none-any.whl/mdt/data/components/standard/compartment_models/S0.py
@@ -0,0 +1,12 @@
+from mdt.component_templates.compartment_models import CompartmentTemplate
+
+__author__ = 'Robbert Harms'
+__date__ = "2015-06-21"
+__maintainer__ = "Robbert Harms"
+__email__ = "robbert.harms@maastrichtuniversity.nl"
+
+
+class S0(CompartmentTemplate):
+
+    parameters = ('s0',)
+    cl_code = 'return s0;'
