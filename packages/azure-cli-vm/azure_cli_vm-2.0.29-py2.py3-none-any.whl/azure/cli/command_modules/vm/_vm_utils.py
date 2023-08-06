f2e29facf320bbe057a3d6432fa2ac263e30b260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:39 +0000

    Add package azure-cli-vm/2.0.29/azure_cli_vm-2.0.29-py2.py3-none-any.whl

---
+++ packages/azure-cli-vm/azure_cli_vm-2.0.29-py2.py3-none-any.whl/azure/cli/command_modules/vm/_vm_utils.py
@@ -0,0 +1,109 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+import json
+import os
+
+from knack.log import get_logger
+from knack.util import CLIError
+
+logger = get_logger(__name__)
+
+
+MSI_LOCAL_ID = '[system]'
+
+
+def get_target_network_api(cli_ctx):
+    """ Since most compute calls don't need advanced network functionality, we can target a supported, but not
+        necessarily latest, network API version is order to avoid having to re-record every test that uses VM create
+        (which there are a lot) whenever NRP bumps their API version (which is often)!
+    """
+    from azure.cli.core.profiles import get_api_version, ResourceType
+    version = get_api_version(cli_ctx, ResourceType.MGMT_NETWORK)
+    if cli_ctx.cloud.profile == 'latest':
+        version = '2018-01-01'
+    return version
+
+
+def read_content_if_is_file(string_or_file):
+    content = string_or_file
+    if os.path.exists(string_or_file):
+        with open(string_or_file, 'r') as f:
+            content = f.read()
+    return content
+
+
+def _resolve_api_version(cli_ctx, provider_namespace, resource_type, parent_path):
+    from azure.cli.core.commands.client_factory import get_mgmt_service_client
+    from azure.cli.core.profiles import ResourceType
+    client = get_mgmt_service_client(cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES)
+    provider = client.providers.get(provider_namespace)
+
+    # If available, we will use parent resource's api-version
+    resource_type_str = (parent_path.split('/')[0] if parent_path else resource_type)
+
+    rt = [t for t in provider.resource_types  # pylint: disable=no-member
+          if t.resource_type.lower() == resource_type_str.lower()]
+    if not rt:
+        raise CLIError('Resource type {} not found.'.format(resource_type_str))
+    if len(rt) == 1 and rt[0].api_versions:
+        npv = [v for v in rt[0].api_versions if 'preview' not in v.lower()]
+        return npv[0] if npv else rt[0].api_versions[0]
+    else:
+        raise CLIError(
+            'API version is required and could not be resolved for resource {}'
+            .format(resource_type))
+
+
+def log_pprint_template(template):
+    logger.info('==== BEGIN TEMPLATE ====')
+    logger.info(json.dumps(template, indent=2))
+    logger.info('==== END TEMPLATE ====')
+
+
+def check_existence(cli_ctx, value, resource_group, provider_namespace, resource_type,
+                    parent_name=None, parent_type=None):
+    # check for name or ID and set the type flags
+    from azure.cli.core.commands.client_factory import get_mgmt_service_client
+    from msrestazure.azure_exceptions import CloudError
+    from msrestazure.tools import parse_resource_id
+    from azure.cli.core.profiles import ResourceType
+    resource_client = get_mgmt_service_client(cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES).resources
+
+    id_parts = parse_resource_id(value)
+
+    rg = id_parts.get('resource_group', resource_group)
+    ns = id_parts.get('namespace', provider_namespace)
+
+    if parent_name and parent_type:
+        parent_path = '{}/{}'.format(parent_type, parent_name)
+        resource_name = id_parts.get('child_name_1', value)
+        resource_type = id_parts.get('child_type_1', resource_type)
+    else:
+        parent_path = ''
+        resource_name = id_parts['name']
+        resource_type = id_parts.get('type', resource_type)
+    api_version = _resolve_api_version(cli_ctx, provider_namespace, resource_type, parent_path)
+
+    try:
+        resource_client.get(rg, ns, parent_path, resource_type, resource_name, api_version)
+        return True
+    except CloudError:
+        return False
+
+
+def create_keyvault_data_plane_client(cli_ctx):
+    from azure.cli.core._profile import Profile
+
+    def get_token(server, resource, scope):  # pylint: disable=unused-argument
+        return Profile(cli_ctx=cli_ctx).get_login_credentials(resource)[0]._token_retriever()  # pylint: disable=protected-access
+
+    from azure.keyvault import KeyVaultClient, KeyVaultAuthentication
+    return KeyVaultClient(KeyVaultAuthentication(get_token))
+
+
+def get_key_vault_base_url(cli_ctx, vault_name):
+    suffix = cli_ctx.cloud.suffixes.keyvault_dns
+    return 'https://{}{}'.format(vault_name, suffix)
