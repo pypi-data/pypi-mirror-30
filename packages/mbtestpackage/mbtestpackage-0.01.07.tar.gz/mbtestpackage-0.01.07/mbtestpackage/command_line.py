c3c739922fc6f2cd011c98b670d696c80e8ff18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:21 +0000

    Add package mbtestpackage/0.01.07/mbtestpackage-0.01.07.tar.gz

---
+++ packages/mbtestpackage/mbtestpackage-0.01.07.tar.gz/mbtestpackage-0.01.07/mbtestpackage/command_line.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+import mbtestpackage
+def main():
+    mbtestpackage.mytry()
+def main2():
+    mbtestpackage.mytry2()
+def startup():
+    print("starting program!")
+    mbtestpackage.versionchecker()
