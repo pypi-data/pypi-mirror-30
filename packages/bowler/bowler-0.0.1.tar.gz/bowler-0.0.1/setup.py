e20b1a7ad778f3ab97e979d8a74579ba51d53678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:18 +0000

    Add package bowler/0.0.1/bowler-0.0.1.tar.gz

---
+++ packages/bowler/bowler-0.0.1.tar.gz/bowler-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='bowler',
+    description='a hat',
+    version='0.0.1',
+    author='John Reese',
+    author_email='john@noswap.com',
+    url='https://github.com/jreese',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+    ],
+    license='MIT',
+    packages=[],
+    setup_requires=['setuptools>=38.6.0'],
+    install_requires=[
+    ],
+)
