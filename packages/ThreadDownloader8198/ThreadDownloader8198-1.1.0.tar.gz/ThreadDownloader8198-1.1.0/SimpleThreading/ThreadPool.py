792e8788f3015280fa48dffdd218c9cf9c10d4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:08 +0000

    Add package ThreadDownloader8198/1.1.0/ThreadDownloader8198-1.1.0.tar.gz

---
+++ packages/ThreadDownloader8198/ThreadDownloader8198-1.1.0.tar.gz/ThreadDownloader8198-1.1.0/SimpleThreading/ThreadPool.py
@@ -0,0 +1,19 @@
+import time
+from concurrent.futures.thread import ThreadPoolExecutor
+from urllib import request
+
+executor = ThreadPoolExecutor(10)
+
+def downloader(url, path):
+    executor.submit(lambda : request.urlretrieve(url, path))
+
+def multi_downloader(files):
+    for url, path in files:
+        downloader(url, path)
+
+if __name__ == "__main__":
+
+    l = [("https://s.yimg.com/fz/api/res/1.2/hPzIdpFqPTYtITzLJzmg0A--~C/YXBwaWQ9c3JjaGRkO2g9NTEyO3E9ODA7dz01MTI-/http://files.softicons.com/download/tv-movie-icons/poke-balls-icons-by-davi-andrade/png/512x512/Poke%20Ball.png.cf.jpg", "ball.jpg")]
+
+    for url, path in l:
+        downloader(url, path)
