f8af4912cd913b8b22574854881dfeab1f1e08f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:11 +0000

    Add package django_matialvarezs_grafana_customers/0.1.41/django_matialvarezs_grafana_customers-0.1.41.tar.gz

---
+++ packages/django_matialvarezs_grafana_customers/django_matialvarezs_grafana_customers-0.1.41.tar.gz/django_matialvarezs_grafana_customers-0.1.41/matialvarezs_grafana_customers/base_dashboard.py
@@ -0,0 +1,67 @@
+import json
+
+
+def get_base_dashboard():
+    return json.dumps({
+        "annotations": {
+            "list": [
+                {
+                    "builtIn": 1,
+                    "datasource": "-- Grafana --",
+                    "enable": True,
+                    "hide": True,
+                    "iconColor": "rgba(0, 211, 255, 1)",
+                    "name": "Annotations & Alerts",
+                    "type": "dashboard"
+                }
+            ]
+        },
+        "editable": True,
+        "gnetId": None,
+        "graphTooltip": 0,
+        "id": None,
+        "links": [],
+        "panels": [
+
+        ],
+        "refresh": "5s",
+        "schemaVersion": 16,
+        "style": "dark",
+        "tags": [],
+        "templating": {
+            "list": []
+        },
+        "time": {
+            "from": "now-24h",
+            "to": "now"
+        },
+        "timepicker": {
+            "refresh_intervals": [
+                "5s",
+                "10s",
+                "30s",
+                "1m",
+                "5m",
+                "15m",
+                "30m",
+                "1h",
+                "2h",
+                "1d"
+            ],
+            "time_options": [
+                "5m",
+                "15m",
+                "1h",
+                "6h",
+                "12h",
+                "24h",
+                "2d",
+                "7d",
+                "30d"
+            ]
+        },
+        "timezone": "",
+        "title": "SISCORVAC CREADO POR API",
+        "uid": None,
+        "version": 3
+    })
