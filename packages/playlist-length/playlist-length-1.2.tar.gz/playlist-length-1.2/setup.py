65ac938f95eb3c33597af860f1cc95d06fc211c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:08 +0000

    Add package playlist-length/1.2/playlist-length-1.2.tar.gz

---
+++ packages/playlist-length/playlist-length-1.2.tar.gz/playlist-length-1.2/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+
+setup(
+    name='playlist-length',
+    version='1.2',
+    description='A simple tool to get total lenght of videos in a directory',
+    long_description=open('README.md').read().strip(),
+    author='Karan Suthar',
+    author_email='karansthr97@gmail.com',
+    url='http://github.com/karansthr/playlist-length',
+    packages=['playlist_length'],
+    install_requires=["python-magic","tqdm","huepy"],
+    license='MIT License',
+    keywords='videolenght playlist-length',
+    entry_points={
+        'console_scripts': ['playlistlen=playlist_length:main'],
+    },
+)
