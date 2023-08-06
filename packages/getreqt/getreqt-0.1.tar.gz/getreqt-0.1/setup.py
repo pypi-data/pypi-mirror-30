5bb21d670c51fd6e59d4979ed788715cdc0c32b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:35 +0000

    Add package getreqt/0.1/getreqt-0.1.tar.gz

---
+++ packages/getreqt/getreqt-0.1.tar.gz/getreqt-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='getreqt',
+    version='0.1',
+    description='get requirements tree for installed pip packages',
+    url='https://github.com/andermi/getreqt',
+    author='Michael Anderson',
+    author_email='manders9@jhu.edu',
+    license='MIT',
+    install_requires=['asciitree'],
+    scripts=['getreqt'],
+    zip_safe=False
+)
