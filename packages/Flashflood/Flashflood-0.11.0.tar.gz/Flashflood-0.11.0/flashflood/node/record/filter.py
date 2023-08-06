5dc5a3191216c04e735d2d686c059313c9667098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:34 +0000

    Add package Flashflood/0.11.0/Flashflood-0.11.0.tar.gz

---
+++ packages/Flashflood/Flashflood-0.11.0.tar.gz/Flashflood-0.11.0/flashflood/node/record/filter.py
@@ -0,0 +1,23 @@
+#
+# (C) 2014-2017 Seiji Matsuoka
+# Licensed under the MIT License (MIT)
+# http://opensource.org/licenses/MIT
+#
+
+import operator
+
+from flashflood.node.control.filter import Filter, AsyncFilter
+
+
+class FilterRecords(Filter):
+    def __init__(self, key, value, operator=operator.eq, **kwargs):
+        super().__init__(
+            lambda x: operator(x[key], value),
+            residue_counter=None, fields=None, **kwargs)
+
+
+class AsyncFilterRecords(AsyncFilter):
+    def __init__(self, key, value, operator=operator.eq, **kwargs):
+        super().__init__(
+            lambda x: operator(x[key], value),
+            residue_counter=None, fields=None, **kwargs)
