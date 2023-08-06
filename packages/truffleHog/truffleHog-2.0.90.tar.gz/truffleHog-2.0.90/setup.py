bbae5f867800d2562a570a3351c516ccce83479f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:34 +0000

    Add package truffleHog/2.0.90/truffleHog-2.0.90.tar.gz

---
+++ packages/truffleHog/truffleHog-2.0.90.tar.gz/truffleHog-2.0.90/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='truffleHog',
+    version='2.0.90',
+    description='Searches through git repositories for high entropy strings, digging deep into commit history.',
+    url='https://github.com/dxa4481/truffleHog',
+    author='Dylan Ayrey',
+    author_email='dxa4481@rit.edu',
+    license='GNU',
+    packages = ['truffleHog', 'truffleHog.defaultRegexes'],
+    install_requires=[
+        'GitPython == 2.1.1'
+    ],
+    entry_points = {
+      'console_scripts': ['trufflehog = truffleHog.truffleHog:main'],
+    },
+)
