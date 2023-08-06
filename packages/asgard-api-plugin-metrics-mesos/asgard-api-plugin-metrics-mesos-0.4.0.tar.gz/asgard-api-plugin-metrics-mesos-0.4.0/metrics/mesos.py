3c13663e4ca2edd6ce73603b9dd3c6e23129a67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:06 +0000

    Add package asgard-api-plugin-metrics-mesos/0.4.0/asgard-api-plugin-metrics-mesos-0.4.0.tar.gz

---
+++ packages/asgard-api-plugin-metrics-mesos/asgard-api-plugin-metrics-mesos-0.4.0.tar.gz/asgard-api-plugin-metrics-mesos-0.4.0/metrics/mesos.py
@@ -0,0 +1,61 @@
+from metrics.config import MESOS_URL
+from requests import get
+from collections import defaultdict
+
+def get_mesos_slaves():
+    return get("{}/slaves".format(MESOS_URL)).json()
+
+def get_slaves_attr(slaves_state):
+    attrs = defaultdict(set)
+
+    for slave in slaves_state['slaves']:
+        for attr in slave['attributes']:
+            attrs[attr].add(slave['attributes'][attr])
+
+    for attr in attrs:
+        attrs[attr] = list(attrs[attr])
+
+    return dict(attrs)
+
+def get_slaves_with_attr(slaves_state, attrs):
+    slaves = []
+
+    for slave in slaves_state['slaves']:
+        intersection = dict(set(slave['attributes'].items()).intersection(set(attrs.items())))
+
+        if intersection != attrs:
+            continue
+
+        slaves.append(slave)
+
+    return slaves
+
+def get_attr_usage(slaves_state, attrs):
+    slaves = get_slaves_with_attr(slaves_state, attrs)
+
+    usage = 0
+
+    cpu_total = 0
+    ram_total = 0
+
+    cpu_used = 0
+    ram_used = 0
+
+    for slave in slaves:
+        cpu_total = cpu_total + slave["resources"]['cpus']
+        ram_total = ram_total + slave["resources"]['mem']
+
+        cpu_used = cpu_used + slave["used_resources"]['cpus']
+        ram_used = ram_used + slave["used_resources"]['mem']
+
+    ram_total = round(ram_total/1000)
+    ram_used = round(ram_used/1000)
+
+    return {
+        'cpu_total': cpu_total,
+        'ram_total': ram_total,
+        'cpu_used': cpu_used,
+        'ram_used': ram_used,
+        'cpu_pct': round(cpu_used*100/cpu_total, 1),
+        'ram_pct': round(ram_used*100/ram_total, 1)
+    }
