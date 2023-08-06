d8f04dad0d44edd3b37c8e3a799d34b7f1252d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:48 +0000

    Add package thorpy/1.6/thorpy-1.6.tar.gz

---
+++ packages/thorpy/thorpy-1.6.tar.gz/thorpy-1.6/README.txt
@@ -0,0 +1,4 @@
+# Thorpy - Yann Thorimbert - yann.thorimbert@gmail.com
+
+ThorPy is a free GUI library intended for use with pygame. Just extract thorpy folder for use, or see installation explanations on http://www.thorpy.org.
+
---
+++ packages/thorpy/thorpy-1.6.tar.gz/thorpy-1.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+
+setup(name='thorpy',
+      version='1.6',
+      description='pygame GUI library',
+      author='Yann Thorimbert',
+      author_email='yann.thorimbert@gmail.com',
+      url='http://www.thorpy.org/',
+      keywords=['pygame','gui','menus','buttons', 'widgets'],
+      packages=find_packages(),
+      include_package_data=True,
+      license='MIT')
