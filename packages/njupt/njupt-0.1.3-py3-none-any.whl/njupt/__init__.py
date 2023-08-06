e0fa4de5efb63e9aaea32911f4dabe1114a8a69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:55 +0000

    Add package njupt/0.1.3/njupt-0.1.3-py3-none-any.whl

---
+++ packages/njupt/njupt-0.1.3-py3-none-any.whl/njupt/__init__.py
@@ -0,0 +1,3 @@
+# from njupt.models.aolan import Aolan
+from njupt.models.card import Card
+from njupt.models.zhengfang import Zhengfang
---
+++ packages/njupt/njupt-0.1.3-py3-none-any.whl/njupt/exceptions.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+__author__ = 'gaoliang'
+
+
+class NjuptException(Exception):
+    """Base class for exceptions in this module."""
+
+    def __init__(self, *args, **kwargs):
+        super(NjuptException, self).__init__(*args, **kwargs)
+
+
+class AuthenticationException(NjuptException):
+    """An Authentication Exception occurred."""
+
+
+class UnauthorizedError(NjuptException):
+    """Not Unauthorized Error"""
