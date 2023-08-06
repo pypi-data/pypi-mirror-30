203b21719606f811f0a13f6863db1cd7e91e47f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:18 +0000

    Add package pyreemote/0.9.1/pyreemote-0.9.1.tar.gz

---
+++ packages/pyreemote/pyreemote-0.9.1.tar.gz/pyreemote-0.9.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from pyreemote import __LIBRARY_VERSION__
+
+setup(
+    name='pyreemote',
+    version=__LIBRARY_VERSION__,
+    packages=find_packages(),
+    url='https://github.com/gisce/reemote',
+    license='GNU Affero General Public License v3',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    install_requires=[
+        'requests',
+    ],
+    description='Pyhton wrapper for reemote'
+)
