37242a81194ef11cdb78e3350547a1e9175af82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:06 +0000

    Add package sanjaynesterhf/1.2.0/sanjaynesterhf-1.2.0.tar.gz

---
+++ packages/sanjaynesterhf/sanjaynesterhf-1.2.0.tar.gz/sanjaynesterhf-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+	name = 'sanjaynesterhf',
+	version = '1.2.0',
+	py_modules = ['sanjaynesterhf'],
+	author = 'Sanjay',
+	author_email = 'ksanjay.synoverge@gmail.com',
+	description = 'A simple printer of nested lists from HFP',
+)
