718d14d03f8ad41e07d3fa20371b982955c12558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:44 +0000

    Add package django-manager-utils/1.1.0/django_manager_utils-1.1.0-py2.py3-none-any.whl

---
+++ packages/django-manager-utils/django_manager_utils-1.1.0-py2.py3-none-any.whl/manager_utils/tests/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from manager_utils import ManagerUtilsManager
+from timezone_field import TimeZoneField
+
+
+class TestModel(models.Model):
+    """
+    A model for testing manager utils.
+    """
+    int_field = models.IntegerField(null=True, unique=True)
+    char_field = models.CharField(max_length=128, null=True)
+    float_field = models.FloatField(null=True)
+    time_zone = TimeZoneField(default='UTC')
+
+    objects = ManagerUtilsManager()
+
+    class Meta:
+        unique_together = ('int_field', 'char_field')
+
+
+class TestForeignKeyModel(models.Model):
+    """
+    A test model that has a foreign key.
+    """
+    int_field = models.IntegerField()
+    test_model = models.ForeignKey(TestModel, on_delete=models.CASCADE)
+
+    objects = ManagerUtilsManager()
+
+
+class TestPkForeignKey(models.Model):
+    """
+    A test model with a primary key thats a foreign key to another model.
+    """
+    my_key = models.ForeignKey(TestModel, primary_key=True, on_delete=models.CASCADE)
+    char_field = models.CharField(max_length=128, null=True)
+
+    objects = ManagerUtilsManager()
+
+
+class TestPkChar(models.Model):
+    """
+    A test model with a primary key that is a char field.
+    """
+    my_key = models.CharField(max_length=128, primary_key=True)
+    char_field = models.CharField(max_length=128, null=True)
+
+    objects = ManagerUtilsManager()
