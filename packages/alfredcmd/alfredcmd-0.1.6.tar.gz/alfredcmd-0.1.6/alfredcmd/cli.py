a4c6e0785cdba7350b62f6c1de72ab8f8b219b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:41 +0000

    Add package alfredcmd/0.1.6/alfredcmd-0.1.6.tar.gz

---
+++ packages/alfredcmd/alfredcmd-0.1.6.tar.gz/alfredcmd-0.1.6/alfredcmd/cli.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for alfred."""
+import sys
+from alfredcmd import Alfred, AlfredException
+
+
+def main(args=None):
+    """Console script for alfred."""
+    if args is None:
+        args = sys.argv[1:]
+
+    try:
+        al = Alfred()
+        return al.run(args)
+    except AlfredException as e:
+        print('? {}'.format(e))
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))  # pragma: no cover
