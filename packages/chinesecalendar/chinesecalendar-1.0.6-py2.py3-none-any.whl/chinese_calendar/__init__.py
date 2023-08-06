0aabf32c21d663c40a13d234ea806e2f0dd137b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:55 +0000

    Add package chinesecalendar/1.0.6/chinesecalendar-1.0.6-py2.py3-none-any.whl

---
+++ packages/chinesecalendar/chinesecalendar-1.0.6-py2.py3-none-any.whl/chinese_calendar/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from .constants import Holiday
+from .utils import get_holiday_detail, is_holiday, is_workday
+
+__all__ = ['Holiday', 'get_holiday_detail', 'is_holiday', 'is_workday']
