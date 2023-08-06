0744e6c8b35150393adbcdb9e32c13e9d5d36ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:25 +0000

    Add package ThreadDownloader4834/1.0.0/ThreadDownloader4834-1.0.0.tar.gz

---
+++ packages/ThreadDownloader4834/ThreadDownloader4834-1.0.0.tar.gz/ThreadDownloader4834-1.0.0/SimpleThreading/ThreadingByFunction.py
@@ -0,0 +1,24 @@
+from threading import Thread
+import time
+
+def downloader(url):
+    time.sleep(5)
+    print(url,"download complete")
+
+l = ["gfgikoikujhg", "ghjhgh","sdfddsd"]
+
+for item in l:
+    Thread(target=lambda : downloader(item)).start()
+
+# t = Thread(target=downloader).start()
+# t = Thread(target=downloader).start()
+# t = Thread(target=downloader).start()
+# t = Thread(target=downloader).start()
+# t = Thread(target=downloader).start()
+#
+
+# t = Thread(target=downloader).run()
+# t = Thread(target=downloader).run()
+# t = Thread(target=downloader).run()
+# t = Thread(target=downloader).run()
+# t = Thread(target=downloader).run()
