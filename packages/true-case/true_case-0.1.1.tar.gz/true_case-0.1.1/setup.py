8e0a94bef994adfc24f04f14d2c845547d64b35a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:34 +0000

    Add package true-case/0.1.1/true_case-0.1.1.tar.gz

---
+++ packages/true-case/true_case-0.1.1.tar.gz/true_case-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+version = "0.1.1"
+
+setup(
+    name="true_case",
+    version=version,
+    description="Get case statistics for words.",
+    author="Brian Lester",
+    author_email="blester125@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    package_data={
+        'true_case': [
+            'true_case/data/model.p',
+        ],
+    },
+    install_requires=[
+        'tqdm',
+    ],
+    include_package_data=True,
+    keywords=["NLP"]
+)
