968a8fff130e60040d18ba800a5e488d92d91150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:05 +0000

    Add package scopus.wp/0.3.0.70/scopus.wp-0.3.0.70.tar.gz

---
+++ packages/scopus.wp/scopus.wp-0.3.0.70.tar.gz/scopus.wp-0.3.0.70/ScopusWp/main.py
@@ -0,0 +1,8 @@
+from ScopusWp.controller import TopController
+
+import optparse
+
+# TODO: Make a fail proof for the citation comment wordpress not well formed
+# TODO: Make the new citations functionality with update date
+# TODO: Make update date into the main reference
+# TODO: Make error safe for http request pool exceptions wrapper method for requests
