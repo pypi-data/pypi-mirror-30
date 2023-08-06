c03feb1e88bb09fd67b3d134b2881552131f5769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:12 +0000

    Add package pydevrant/0.12/pydevrant-0.12.tar.gz

---
+++ packages/pydevrant/pydevrant-0.12.tar.gz/pydevrant-0.12/pydevrant/__init__.py
@@ -0,0 +1,5 @@
+# Written by Sergio La Rosa (sergio.larosa89@gmail.com)
+# Part of "pydevrant" package
+# https://github.com/SergioLaRosa/pydevrant
+
+from .basic import *
---
+++ packages/pydevrant/pydevrant-0.12.tar.gz/pydevrant-0.12/setup.py
@@ -0,0 +1,24 @@
+# Written by Sergio La Rosa (sergio.larosa89@gmail.com)
+# Part of "pydevrant" package
+# https://github.com/SergioLaRosa/pydevrant
+
+from setuptools import setup
+
+try:
+   import pypandoc
+   long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+   long_description = ''
+
+setup(
+    name='pydevrant',
+    version='0.12',
+    description='Unofficial Python wrapper for the public devRant API',
+    long_description=long_description,
+    url='https://github.com/SergioLaRosa/pydevrant',
+    author='Sergio La Rosa',
+    author_email='sergio.larosa89@gmail.com',
+    license='MIT',
+    packages=['pydevrant'],
+    install_requires=['requests'],
+    zip_safe=False)
