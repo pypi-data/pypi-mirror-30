b527e710de1e1d19bf69dc23b841e58d5aced06b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:09 +0000

    Add package macroecon/0.1.0/macroecon-0.1.0.tar.gz

---
+++ packages/macroecon/macroecon-0.1.0.tar.gz/macroecon-0.1.0/macroecon/__init__.py
@@ -0,0 +1,7 @@
+"""macroecon - Interface with macroeconomic data sources from Brasil, such as IPEA."""
+
+__version__ = '0.1.0'
+__author__ = 'Pedro Correia <pedrocorreia.rs@gmail.com>'
+__all__ = []
+
+from macroecon.ipea_indicadores import ipea_indicadores
---
+++ packages/macroecon/macroecon-0.1.0.tar.gz/macroecon-0.1.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name="macroecon",
+    version="0.1.0",
+    url="https://github.com/pfcor/macroecon.git",
+
+    author="Pedro Correia",
+    author_email="pedrocorreia.rs@gmail.com",
+
+    description="Interface with macroeconomic data sources from Brasil, such as IPEA.",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+)
