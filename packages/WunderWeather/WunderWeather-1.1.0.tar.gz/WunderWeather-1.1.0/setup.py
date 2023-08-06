beef9964da9eb6cc3d73b7125f6d46445006d40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:26 +0000

    Add package WunderWeather/1.1.0/WunderWeather-1.1.0.tar.gz

---
+++ packages/WunderWeather/WunderWeather-1.1.0.tar.gz/WunderWeather-1.1.0/setup.py
@@ -0,0 +1,34 @@
+import io
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+setup(
+  name = 'WunderWeather',
+  packages = ['WunderWeather'],
+  # To include everything defined in MANIFEST.in file
+  include_package_data=True,
+  # to include addl files
+  package_data={
+    '':['*.rst','*.txt'],
+    },
+  install_requires = ['easydict','requests'],
+  version = '1.1.0',
+  description = 'Wrapper for Weather Underground API',
+  long_description = long_description,
+  author = 'Tyler Santos',
+  author_email = '1tsantos7+wunderweather@gmail.com',
+  url = 'http://wunderweather.readthedocs.io/en/latest/',
+  download_url = 'https://github.com/T-Santos/WunderWeather',
+  keywords = ['weather', 'wunderground'],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3.5'],
+    )
