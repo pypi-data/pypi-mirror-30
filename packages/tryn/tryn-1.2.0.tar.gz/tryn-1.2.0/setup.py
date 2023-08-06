cabd8afa53e46b741128ed527c90b65a03d68cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:23 +0000

    Add package tryn/1.2.0/tryn-1.2.0.tar.gz

---
+++ packages/tryn/tryn-1.2.0.tar.gz/tryn-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name='tryn',
+        version='1.2.0',
+        py_modules=['tryn'],
+        author='priscillalu',
+        author_email='priscillaluwe@yeah.net',
+        description='A simple printer of  lists',
+     )
---
+++ packages/tryn/tryn-1.2.0.tar.gz/tryn-1.2.0/tryn.py
@@ -0,0 +1,6 @@
+def print_lol(the_list,level=0):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item)
+        else:
+            print(each_item)
