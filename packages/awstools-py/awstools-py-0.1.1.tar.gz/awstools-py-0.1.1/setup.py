afda6131eccb4c126a17eb61d20aff11148369c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:37 +0000

    Add package awstools-py/0.1.1/awstools-py-0.1.1.tar.gz

---
+++ packages/awstools-py/awstools-py-0.1.1.tar.gz/awstools-py-0.1.1/setup.py
@@ -0,0 +1,39 @@
+import os
+import uuid
+from setuptools import setup
+from pip.req import parse_requirements
+from pkgutil import walk_packages
+
+
+PKG = 'awstools'
+PKG_NAME = 'awstools-py'
+PKG_VERSION = '0.1.1'
+
+pathname = os.path.dirname(os.path.realpath(__file__))
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements(pathname + "/requirements.txt",
+                                  session=uuid.uuid1())
+
+
+def find_packages(prefix=""):
+    path = [prefix]
+    yield prefix
+    prefix = prefix + "."
+    for _, name, ispkg in walk_packages(path, prefix):
+        if ispkg:
+            yield name
+
+
+setup(
+    name=PKG_NAME,
+    version=PKG_VERSION,
+    description='Simple wrappers for boto3 to make life easier',
+    author='Jared Lunde',
+    author_email='jared.lunde@gmail.com',
+    url='https://github.com/jaredlunde/awstools',
+    license="MIT",
+    install_requires=[str(ir.req) for ir in install_reqs],
+    packages=list(find_packages(PKG))
+)
