7ad43a9968560f5ea6468bcb247562274863cefd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:17 +0000

    Add package pocdb-py/0.5/pocdb-py-0.5.tar.gz

---
+++ packages/pocdb-py/pocdb-py-0.5.tar.gz/pocdb-py-0.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pocdb-py',
+      version='0.5',
+      description='pocdb.com Python SDK',
+      url='https://github.com/jeffthorne/pocdb-py',
+      author='Jeff Thorne',
+      author_email='jthorne@u.washington.edu',
+      license='MIT',
+      packages=['pocdb', 'pocdb.models', 'pocdb.utils'],
+      install_requires=['requests'],
+      zip_safe=False)
