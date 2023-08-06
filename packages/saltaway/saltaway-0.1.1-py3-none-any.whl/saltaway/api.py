bcb6d223ec6292e4b505b684c2a6eb03d784cf53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:45 +0000

    Add package saltaway/0.1.1/saltaway-0.1.1-py3-none-any.whl

---
+++ packages/saltaway/saltaway-0.1.1-py3-none-any.whl/saltaway/api.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import concurrent.futures
+
+from .repositories import ArchiveIs, WaybackMachine
+
+
+REPOSITORIES = (
+    ArchiveIs,
+    WaybackMachine,
+)
+
+
+def push(url, exact=False, repos=REPOSITORIES):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_repo = {
+            executor.submit(repo().push, url): repo
+            for repo in repos
+        }
+        for future in concurrent.futures.as_completed(future_to_repo):
+            yield future_to_repo[future], future.result()
