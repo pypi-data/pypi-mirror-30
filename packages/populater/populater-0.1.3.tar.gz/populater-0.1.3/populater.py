e7902c5f23d63409648cb84f9dba913aba4cb0a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:28 +0000

    Add package populater/0.1.3/populater-0.1.3.tar.gz

---
+++ packages/populater/populater-0.1.3.tar.gz/populater-0.1.3/populater.py
@@ -0,0 +1,79 @@
+
+import logging
+import re
+import string
+
+from .				import isolate
+
+log				= logging.getLogger('Populater')
+# log.setLevel(logging.DEBUG)
+
+class Template( object ):
+
+    def __init__(self, text):
+        if type(text) is not str:
+            raise Exception("Template only takes 1 string, not type '{}'".format(type(text)))
+        self.text		= text.strip()
+
+        Populater.before	= self.text
+        
+    def fill(self, s, ctx, fn_ctx=None):
+        text			= s
+        matches			= re.findall("({{([^}]+)}})", s)
+        for match,key in matches:
+            key			= key.strip()
+            value		= self.eval(key, ctx, fn_ctx)
+            if value is None:
+                value		= ""
+            text		= text.replace(match, str(value))
+        return text
+
+    def eval(self, ref, ctx, fn_ctx=None):
+        log.debug("Eval path {}".format(ref))
+        keys			= ref.split('.')
+        for k in keys:
+            log.debug("Extracting path segment {}".format(k))
+            if isinstance(ctx, dict):
+                right		= k.lstrip(string.ascii_letters + string.digits + "_")
+                k		= k.rstrip(right)
+                ctx		= isolate.evaluate("self.get('{}')".format(k), ctx, fn_ctx)
+                if right:
+                    ctx		= isolate.evaluate("self{}".format(right), ctx, fn_ctx)
+            elif isinstance(ctx, object) and ctx is not None:
+                ctx		= isolate.evaluate("self.{}".format(k), ctx, fn_ctx)
+            else:
+                return None
+        return ctx
+
+    def context(self, ctx, fn_ctx=None):
+        text			= self.fill(self.text, ctx, fn_ctx)
+        
+        ref			= text.startswith('<')
+        raw			= text.startswith(':')
+        run			= text.startswith('=')
+        
+        if ref or raw or run:
+            text		= text[1:].strip()
+
+        if ref:
+            value		= self.eval(text, ctx, fn_ctx)
+        elif run:
+            value		= isolate.evaluate(text, ctx, fn_ctx)
+        else:
+            value		= text
+            
+        Populater.after		= value
+        return value
+            
+
+def Populater(data, ctx=None):
+    if not isinstance(data, (object, dict)):
+        raise Exception("Populater can only take complex objects, not type '{}'.  See Populater.template() for other uses.".format(type(data)))
+
+    def wrap(text):
+        return Template(text).context(data, ctx)
+    return wrap
+
+Populater.template		= Template
+Populater.error			= isolate.error
+Populater.method		= isolate.method
