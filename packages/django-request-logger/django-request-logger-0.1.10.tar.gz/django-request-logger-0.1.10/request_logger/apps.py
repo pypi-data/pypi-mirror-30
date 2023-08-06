7672cf422b199e4692c6051054c7b86f28783c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:10 +0000

    Add package django-request-logger/0.1.10/django-request-logger-0.1.10.tar.gz

---
+++ packages/django-request-logger/django-request-logger-0.1.10.tar.gz/django-request-logger-0.1.10/request_logger/apps.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# vi: et sw=2 fileencoding=utf-8
+
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class RequestLoggerConfig(AppConfig):
+  name = "request_logger"
+  verbose_name = "Request logger for django"
+
+
+  # class RequestLoggerConfig
+
+
---
+++ packages/django-request-logger/django-request-logger-0.1.10.tar.gz/django-request-logger-0.1.10/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='django-request-logger',
+      version='0.1.10',
+      description='Django middleware that logs http request information to database.',
+      url='https://git.pispalanit.fi/pit/django-request-logger',
+      author='Joni Saarinen',
+      author_email='joni.saarinen@pispalanit.fi',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'Django',
+      ],
+zip_safe=False)
