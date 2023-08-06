ae1b35bc832001210e5f0a012ec728dacce39de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:53 +0000

    Add package maccabistats/1.6.2/maccabistats-1.6.2-py3-none-any.whl

---
+++ packages/maccabistats/maccabistats-1.6.2-py3-none-any.whl/maccabistats/parse/maccabi_tlv_site/config.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
+from maccabistats.config.config import get_int_from_settings, get_str_from_settings, get_bool_from_settings
+
+# This file contains all the settings.ini file related
+
+section_name_in_settings = "maccabi-site"
+
+max_seasons = "max-seasons-to-crawl"
+season_page_pattern = "season-page-pattern"
+folder_to_save_seasons_html_files = "folder-to-save-seasons-html-files"
+folder_to_save_games_html_files = "folder-to-save-games-html-files"
+should_use_disk_to_crawl_when_available = "use-disk-to-crawl-when-available"
+
+
+def get_should_use_disk_to_crawl_when_available_from_settings():
+    return get_bool_from_settings(section_name_in_settings, should_use_disk_to_crawl_when_available)
+
+
+def get_max_seasons_from_settings():
+    return get_int_from_settings(section_name_in_settings, max_seasons)
+
+
+def get_season_page_pattern_from_settings():
+    return get_str_from_settings(section_name_in_settings, season_page_pattern)
+
+
+def get_folder_to_save_seasons_html_files_from_settings():
+    return get_str_from_settings(section_name_in_settings, folder_to_save_seasons_html_files)
+
+
+def get_folder_to_save_games_html_files_from_settings():
+    return get_str_from_settings(section_name_in_settings, folder_to_save_games_html_files)
---
+++ packages/maccabistats/maccabistats-1.6.2-py3-none-any.whl/maccabistats/test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+from maccabistats.stats.serialized_games import get_maccabi_stats, serialize_maccabi_games
+from maccabistats.maccabilogging import faster_logging
+
+if __name__ == "__main__":
+    #faster_logging()
+    serialize_maccabi_games()
+    a = 6
